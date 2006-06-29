44ea9c7671c0dc849981c8ac95df0cadd7925d16
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-06-29 08:07:00 +0000

    Converted a lot of extensions to the new autoload scheme. Moved a number of extensions to their own directory, apologies for the break in backwards compatibility. This was done because they needed to be split from one file to two, for efficiency, and it makes sense to have two files in their own directory. It probably makes sense to have each extension in its own directory anyway. Tested both the inefficient backwards compatible interface and the present interface. Still more to be converted...
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@15145 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ FindSpam_body.php
@@ -0,0 +1,89 @@
+<?php
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "FindSpam extension\n";
+	exit( 1 );
+}
+
+global $wgMessageCache;
+$wgMessageCache->addMessage( "findspam", "Find spam" );
+
+class FindSpamPage extends SpecialPage
+{
+	function FindSpamPage() {
+		SpecialPage::SpecialPage("FindSpam", "findspam");
+	}
+
+	function execute( $par ) {
+		global $wgRequest, $wgOut, $wgTitle, $wgLocalDatabases, $wgUser;
+		global $wgConf, $wgCanonicalNamespaceNames, $wgLang;
+
+		$this->setHeaders();
+		if ( !$this->userCanExecute( $wgUser ) ) {
+			$this->displayRestrictionError();
+			return;
+		}
+
+		$ip = trim( $wgRequest->getText( 'ip' ) );
+		$encQ = htmlspecialchars( $ip );
+		$action = $wgTitle->getLocalUrl();
+		$ok = wfMsg( "ok" );
+
+		$wgOut->addHTML( "
+<form name=ucf method=post action=\"$action\">
+<label> IP: 
+<input type=text width=50 name=ip value=\"$encQ\" />
+</label>
+<input type=submit name=submit value=\"$ok\"><br /><br />
+</form>" );
+
+		if ( $ip ) {
+			$dbr =& wfGetDB( DB_READ );
+			$s  = '';
+
+			foreach ( $wgLocalDatabases as $db ) {
+				$sql = "SELECT rc_namespace,rc_title,rc_timestamp,rc_user_text,rc_last_oldid FROM $db.recentchanges WHERE rc_ip='" . wfStrencode( $ip ) . 
+				  "' AND rc_this_oldid=0";
+				$res = $dbr->query( $sql, "findspam.php" );
+				list( $site, $lang ) = $wgConf->siteFromDB( $db );
+				if ( $lang == 'meta' ) {
+					$baseUrl = "http://meta.wikimedia.org";
+				} else {
+					$baseUrl = "http://$lang.$site.org";
+				}
+
+				if ( $dbr->numRows( $res ) ) {
+					$s .= "\n$db\n";
+					while ( $row = $dbr->fetchObject( $res ) ) {
+								
+						if ( $row->rc_namespace == 0 ){
+							$title = $row->rc_title;
+						} else {
+							$title = $wgCanonicalNamespaceNames[$row->rc_namespace] . ':' .$row->rc_title;
+						}
+						$encTitle = urlencode( $title );
+						$url = "$baseUrl/wiki/$encTitle";
+						$user = urlencode( $row->rc_user_text );
+						#$rollbackText = wfMsg( 'rollback' );
+						$diffText = wfMsg( 'diff' );
+						#$rollbackUrl = "$baseUrl/w/wiki.phtml?title=$encTitle&action=rollback&from=$user";
+						$diffUrl = "$baseUrl/w/wiki.phtml?title=$encTitle&diff=0&oldid=0";
+						if ( $row->rc_last_oldid ) {
+							$lastLink = "[$baseUrl/w/wiki.phtml?title=$encTitle&oldid={$row->rc_last_oldid}&action=edit last]";
+						}
+
+						$date = $wgLang->timeanddate( $row->rc_timestamp );
+						#$s .= "* $date [$url $title] ([$rollbackUrl $rollbackText] | [$diffUrl $diffText])\n";
+						$s .= "* $date [$url $title] ($lastLink | [$diffUrl $diffText])\n";
+					}
+				}
+			}
+			if ( $s == '' ) {
+				$s = "No contributions found\n";
+			}
+			$wgOut->addWikiText( $s );
+		}
+	}
+}
+
+?>
