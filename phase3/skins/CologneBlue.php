GICodeWarrior/mediawiki-extensions

commit 08666e310147f0f98578286ec2b0c196e6150813

* (bug 6276) Stopped search field from getting too large in Cologne Blue * Fixed duplicate name/id value of "search" in Cologne Blue
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@17122 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/skins/CologneBlue.php
@@ -304,9 +304,9 @@ class SkinCologneBlue extends Skin {
 		$s = "<form id=\"search\" method=\"get\" class=\"inline\" action=\"$action\">";
 		if ( "" != $label ) { $s .= "{$label}: "; }
 
-		$s .= "<input type='text' name=\"search\" size='14' value=\""
+		$s .= "<input type='text' id=\"searchtext\" name=\"searchtext\" size=\"14\" value=\""
 		  . htmlspecialchars(substr($search,0,256)) . "\" />"
-		  . "<br /><input type='submit' name=\"go\" value=\"" . htmlspecialchars( wfMsg( "searcharticle" ) ) . "\" /> <input type='submit' name=\"fulltext\" value=\"" . htmlspecialchars( wfMsg( "search" ) ) . "\" /></form>";
+		  . "<br /><input type='submit' id=\"go\" name=\"go\" value=\"" . htmlspecialchars( wfMsg( "searcharticle" ) ) . "\" /> <input type='submit' id=\"fulltext\" name=\"fulltext\" value=\"" . htmlspecialchars( wfMsg( "search" ) ) . "\" /></form>";
 
 		return $s;
 	}

phase3/skins/common/cologneblue.css
@@ -95,3 +95,4 @@ a.stub, #quickbar a.stub { color:#772233; text-decoration:none; }
 a.new, #quickbar a.new { color: #CC2200; }
 h2, h3, h4, h5, h6 { margin-bottom: 0; }
 small { font-size: 75%; }
+input#searchtext { width: 106px; }

phase3/RELEASE-NOTES
@@ -61,6 +61,8 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * (bug 2241) Fix collision of 'w' and 'd' accesskeys
 * (bug 5795) CSS class added to body based on page name for page-specific
   styling
+* (bug 6276) Stopped search field from getting too large in Cologne Blue
+* Fixed duplicate name/id value of "search" in Cologne Blue
 
 == Languages updated ==
 

phase3/includes/DefaultSettings.php
@@ -1030,7 +1030,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '16';
+$wgStyleVersion = '17';
 
 # Server-side caching:
 

commit 14c912a74ee22e06c7328d0fccc650fb1165e5de

Fix regression caused by r17122 in Cologne Blue search.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@17165 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/skins/CologneBlue.php
@@ -301,12 +301,12 @@ class SkinCologneBlue extends Skin {
 
 		$search = $wgRequest->getText( 'search' );
 		$action = $this->escapeSearchLink();
-		$s = "<form id=\"search\" method=\"get\" class=\"inline\" action=\"$action\">";
+		$s = "<form id=\"searchform\" method=\"get\" class=\"inline\" action=\"$action\">";
 		if ( "" != $label ) { $s .= "{$label}: "; }
 
-		$s .= "<input type='text' id=\"searchtext\" name=\"searchtext\" size=\"14\" value=\""
+		$s .= "<input type='text' id=\"searchInput\" name=\"search\" size=\"14\" value=\""
 		  . htmlspecialchars(substr($search,0,256)) . "\" />"
-		  . "<br /><input type='submit' id=\"go\" name=\"go\" value=\"" . htmlspecialchars( wfMsg( "searcharticle" ) ) . "\" /> <input type='submit' id=\"fulltext\" name=\"fulltext\" value=\"" . htmlspecialchars( wfMsg( "search" ) ) . "\" /></form>";
+		  . "<br /><input type='submit' id=\"searchGoButton\" name=\"go\" value=\"" . htmlspecialchars( wfMsg( "searcharticle" ) ) . "\" /> <input type='submit' id=\"mw-searchButton\" name=\"fulltext\" value=\"" . htmlspecialchars( wfMsg( "search" ) ) . "\" /></form>";
 
 		return $s;
 	}

phase3/skins/common/cologneblue.css
@@ -95,4 +95,4 @@ a.stub, #quickbar a.stub { color:#772233; text-decoration:none; }
 a.new, #quickbar a.new { color: #CC2200; }
 h2, h3, h4, h5, h6 { margin-bottom: 0; }
 small { font-size: 75%; }
-input#searchtext { width: 106px; }
+input#searchInput { width: 106px; }