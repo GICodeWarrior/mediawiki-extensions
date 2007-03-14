GICodeWarrior/mediawiki-extensions

commit 247aed1b71afc88d224d78e8f97a44b9f19a004c

Make Special:Listusers caseinsensitive for first letter User proper XML form functions Attention translators: Slight change in message. A complete new message seem to be overdone.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@20382 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/SpecialListusers.php
@@ -39,8 +39,11 @@ class UsersPager extends AlphabeticPager {
 	function __construct($group=null) {
 		global $wgRequest;
 		$this->requestedGroup = $group != "" ? $group : $wgRequest->getVal( 'group' );
-		$this->requestedUser = $wgRequest->getText( 'username', $this->mOffset );
-		
+		$un = $wgRequest->getText( 'username' );
+		if ( $un != '' ) {
+			$username = Title::makeTitleSafe( NS_USER, $un );
+			$this->requestedUser = $username->getText();
+		}
 		parent::__construct();
 	}
 
@@ -57,9 +60,9 @@ class UsersPager extends AlphabeticPager {
 		if ($this->requestedUser != "") {
 			$conds[] = 'user_name >= ' . wfGetDB()->addQuotes( $this->requestedUser );
 		}
-		
+
 		list ($user,$user_groups) = wfGetDB()->tableNamesN('user','user_groups');
-		
+
 		return array(
 			'tables' => " $user LEFT JOIN $user_groups ON user_id=ug_user ",
 			'fields' => array('user_name',
@@ -70,7 +73,7 @@ class UsersPager extends AlphabeticPager {
 			'conds' => $conds
 		);
 	}
-	
+
 	function formatRow($row) {
 		$userPage = Title::makeTitle(NS_USER, $row->user_name);
 		$name = $this->getSkin()->makeLinkObj( $userPage, htmlspecialchars( $userPage->getText() ) );
@@ -87,7 +90,7 @@ class UsersPager extends AlphabeticPager {
 		} elseif ($row->numgroups == 1 ) { // MAX hack inside query :)
 			$groups[$row->singlegroup] = User::getGroupMember( $row->singlegroup );
 		}
-		
+
 		if ( count($groups) > 0 ) {
 			foreach( $groups as $group => $desc ) {
 				$list[] = User::makeGroupLinkHTML( $group, $desc);
@@ -96,18 +99,19 @@ class UsersPager extends AlphabeticPager {
 		} else {
 			$groups ='';
 		}
+		//$ulink = $skin->userLink( $result->user, $result->user_text ) . ' ' . $skin->userToolLinks( $result->user, $result->user_text );
 		return '<li>' . wfSpecialList ($name, $groups) .'</li>';
 	}
-	
+
 	function getBody() {
 		if (!$this->mQueryDone) {
 			$this->doQuery();
 		}
 		$batch = new LinkBatch;
 		$db = $this->mDb;
-	
+
 		$this->mResult->rewind();
-		
+
 		while ( $row = $this->mResult->fetchObject() ) {
 			$batch->addObj( Title::makeTitleSafe( NS_USER, $row->user_name ) );
 		}
@@ -115,41 +119,45 @@ class UsersPager extends AlphabeticPager {
 		$this->mResult->rewind();
 		return parent::getBody();
 	}
-	
+
 	function getPageHeader( ) {
+		global $wgRequest;
 		$self = $this->getTitle();
 
 		# Form tag
-		$out = wfOpenElement( 'form', array( 'method' => 'post', 'action' => $self->getLocalUrl() ) );
-		
+		$out  = Xml::openElement( 'form', array( 'method' => 'get', 'action' => $self->getLocalUrl() ) ) .
+			'<fieldset>' .
+			Xml::element( 'legend', array(), wfMsg( 'listusers' ) );
+
+		# Username field
+		$out .= Xml::label( wfMsg( 'listusersfrom' ), 'offset' ) . ' ' .
+			Xml::input( 'username', 20, $this->requestedUser ) . ' ';
+
+		if( $this->mLimit )
+			$out .= Xml::hidden( 'limit', $this->mLimit );
+
 		# Group drop-down list
-		$out .= wfElement( 'label', array( 'for' => 'group' ), wfMsg( 'group' ) ) . ' ';
-		$out .= wfOpenElement( 'select', array( 'name' => 'group', 'id' => 'group' ) );
-		$out .= wfElement( 'option', array( 'value' => '' ), wfMsg( 'group-all' ) ); # Item for "all groups"
+		$out .= Xml::label( wfMsg( 'group' ), 'group' ) . ' ' .
+			Xml::openElement('select',  array( 'name' => 'group', 'id' => 'group' ) ) .
+			Xml::option( wfMsg( 'group-all' ), '' );  # Item for "all groups"
+
 		$groups = User::getAllGroups();
 		foreach( $groups as $group ) {
 			$attribs = array( 'value' => $group );
 			if( $group == $this->requestedGroup )
 				$attribs['selected'] = 'selected';
-			$out .= wfElement( 'option', $attribs, User::getGroupName( $group ) );
+			$out .= Xml::option( User::getGroupName( $group ), $attribs['value'], $attribs['selected'] );
 		}
-		$out .= wfCloseElement( 'select' ) . ' ';;# . wfElement( 'br' );
-
-		# Username field
-		$out .= wfElement( 'label', array( 'for' => 'offset' ), wfMsg( 'listusersfrom' ) ) . ' ';
-		$out .= wfElement( 'input', array( 'type' => 'text', 'id' => 'username', 'name' => 'username',
-							'value' => $this->requestedUser ) ) . ' ';
-
-		if( $this->mLimit )
-			$out .= wfElement( 'input', array( 'type' => 'hidden', 'name' => 'limit', 'value' => $this->mLimit ) );
+		$out .= Xml::closeElement( 'select' ) . ' ';
 
 		# Submit button and form bottom
-		$out .= wfElement( 'input', array( 'type' => 'submit', 'value' => wfMsg( 'allpagessubmit' ) ) );
-		$out .= wfCloseElement( 'form' );
+		$out .= Xml::submitButton( wfMsg( 'allpagessubmit' ) ) .
+			'</fieldset>' .
+			Xml::closeElement( 'form' );
 
 		return $out;
 	}
-	
+
 	/**
 	 * Preserve group and username offset parameters when paging
 	 * @return array

phase3/RELEASE-NOTES
@@ -264,7 +264,7 @@ lighter making things easier to read.
 * (bug 8815) Setting password in initUser() breaks LdapAuthentication plugin
 * (bug 9256) Add a quick note to index.php header comments
 * (bug 1196) Add IPv6 support to blocks
-
+* Make Special:Listusers caseinsensitive for first letter
 
 == Languages updated ==
 

phase3/languages/messages/MessagesDe.php
@@ -1198,7 +1198,7 @@ Davon {{PLURAL:$2|hat|haben}} '''$2''' (=$4%) $5-Rechte.",
 # Special:Listusers
 'listusersfrom'      => 'Zeige Benutzer ab:',
 'listusers-submit'   => 'Zeige',
-'listusers-noresult' => 'Keine Benutzer gefunden. Bitte beachte, dass die Groß-/Kleinschreibung des Anfangsbuchstabens zu unterschiedlichen Ergebnissen führt.',
+'listusers-noresult' => 'Keinen Benutzer gefunden.',
 
 # E-mail user
 'mailnologin'     => 'Sie sind nicht angemeldet.',

phase3/languages/messages/MessagesEn.php
@@ -1597,7 +1597,7 @@ You can narrow down the view by selecting a log type, the user name, or the affe
 # Special:Listusers
 'listusersfrom'      => 'Display users starting at:',
 'listusers-submit'   => 'Show',
-'listusers-noresult' => 'No users found. Please check upper-/lowercase variants too.',
+'listusers-noresult' => 'No user found.',
 
 # Email this user
 #

commit 0aa1d34cd466dbf001e402e3d5959271bb978ef2

*Fix initialization regressions in r20382. Also, join on block table to hide hidden names
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@20428 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/SpecialListusers.php
@@ -40,6 +40,7 @@ class UsersPager extends AlphabeticPager {
 		global $wgRequest;
 		$this->requestedGroup = $group != "" ? $group : $wgRequest->getVal( 'group' );
 		$un = $wgRequest->getText( 'username' );
+		$this->requestedUser = '';
 		if ( $un != '' ) {
 			$username = Title::makeTitleSafe( NS_USER, $un );
 			$this->requestedUser = $username->getText();
@@ -54,6 +55,8 @@ class UsersPager extends AlphabeticPager {
 
 	function getQueryInfo() {
 		$conds=array();
+		// don't show hidden names
+		$conds[]='ipb_deleted IS NULL OR ipb_deleted = 0';
 		if ($this->requestedGroup != "") {
 			$conds['ug_group'] = $this->requestedGroup;
 		}
@@ -61,17 +64,18 @@ class UsersPager extends AlphabeticPager {
 			$conds[] = 'user_name >= ' . wfGetDB()->addQuotes( $this->requestedUser );
 		}
 
-		list ($user,$user_groups) = wfGetDB()->tableNamesN('user','user_groups');
+		list ($user,$user_groups,$ipblocks) = wfGetDB()->tableNamesN('user','user_groups','ipblocks');
 
 		return array(
-			'tables' => " $user LEFT JOIN $user_groups ON user_id=ug_user ",
+			'tables' => " $user LEFT JOIN $user_groups ON user_id=ug_user LEFT JOIN $ipblocks ON user_id=ipb_user AND ipb_auto=0 ",
 			'fields' => array('user_name',
 				'MAX(user_id) AS user_id',
 				'COUNT(ug_group) AS numgroups', 
 				'MAX(ug_group) AS singlegroup'),
 			'options' => array('GROUP BY' => 'user_name'), 
 			'conds' => $conds
-		);
+		);	
+		
 	}
 
 	function formatRow($row) {
@@ -144,8 +148,7 @@ class UsersPager extends AlphabeticPager {
 		$groups = User::getAllGroups();
 		foreach( $groups as $group ) {
 			$attribs = array( 'value' => $group );
-			if( $group == $this->requestedGroup )
-				$attribs['selected'] = 'selected';
+			$attribs['selected'] = ( $group == $this->requestedGroup ) ? 'selected' : '';
 			$out .= Xml::option( User::getGroupName( $group ), $attribs['value'], $attribs['selected'] );
 		}
 		$out .= Xml::closeElement( 'select' ) . ' ';