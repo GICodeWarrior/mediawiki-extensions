GICodeWarrior/mediawiki-extensions

commit 0b08e82608e2c38b8f8e57823414d8a2e7532f15

Avoid the duplication of form UI code between Special:Userrights and Special:Makesysop
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@21658 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Makesysop/SpecialMakesysop_body.php
@@ -303,29 +303,8 @@ class MakesysopStewardForm extends UserrightsForm {
 		}
 
 		$groups = $this->getUserGroups( $database, $userid );
-
-		$wgOut->addHTML(
-			Xml::openElement( 'form', array( 'method' => 'post', 'action' => $this->action, 'name' => 'editGroup' ) ) .
-			Xml::hidden( 'user-editname', $username ) .
-			Xml::hidden( 'wpEditToken', $wgUser->editToken( $username ) ) .
-			Xml::openElement( 'fieldset' ) .
-			Xml::element( 'legend', array(), wfMsg( 'userrights-editusergroup' ) ) .
-			$wgOut->parse( wfMsg( 'editinguser', $username ) ) .
-			"<table border='0' align='center'>
-			<tr>
-				<td>" . HTMLSelectGroups( 'member', $this->mName.'-groupsmember', $groups, true, 6 ) . "</td>
-				<td align='right'>" . HTMLSelectGroups( 'available', $this->mName.'-groupsavailable', $groups, true, 6, true) . "</td>
-			</tr><tr>
-				<td>" . $wgOut->parse( wfMsg( 'makesysopreason' ) ) . "</td>
-				<td align='right'>" . Xml::input( 'wpMakesysopReason', 30 ) . "</td>
-			</tr>
-			</table> \n" .
-			$wgOut->parse( wfMsg('userrights-groupshelp') ) .
-			Xml::submitButton( wfMsg( 'saveusergroups' ), array( 'name' => 'saveusergroups' ) ) .
-			Xml::closeElement( 'fieldset' ) .
-			Xml::closeElement( 'form' ) . "\n"
-		);
-
+		
+		$this->showEditUserGroupsForm( $username, $groups );
 	}
 
 	function splitUsername( $username ) {

phase3/includes/SpecialUserrights.php
@@ -138,7 +138,7 @@ class UserrightsForm extends HTMLForm {
 	 * @param string $username Name of the user.
 	 */
 	function editUserGroupsForm($username) {
-		global $wgOut, $wgUser;
+		global $wgOut;
 
 		$user = User::newFromName($username);
 		if( is_null( $user ) ) {
@@ -150,7 +150,11 @@ class UserrightsForm extends HTMLForm {
 		}
 
 		$groups = $user->getGroups();
-
+		$this->showEditUserGroupsForm( $username, $groups );
+	}
+	
+	function showEditUserGroupsForm( $username, $groups ) {
+		global $wgOut, $wgUser;
 		$wgOut->addHTML(
 			Xml::openElement( 'form', array( 'method' => 'post', 'action' => $this->action, 'name' => 'editGroup' ) ) .
 			Xml::hidden( 'user-editname', $username ) .

commit 7522679da04cbf934eb9396158b4d66e09d59ec7

* (Bug 9693) fix regression, broken since r21658
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@21683 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Makesysop/SpecialMakesysop_body.php
@@ -229,7 +229,7 @@ class MakesysopStewardForm extends UserrightsForm {
 		$this->mPosted = $request->wasPosted();
 		$this->mRequest =& $request;
 		$this->mName = 'userrights';
-		$this->mReason = $request->getText( 'wpMakesysopReason' );
+		$this->mReason = $request->getText( 'user-reason' );
 		$titleObj = Title::makeTitle( NS_SPECIAL, 'Makesysop' );
 		$this->action = $titleObj->escapeLocalURL();
 