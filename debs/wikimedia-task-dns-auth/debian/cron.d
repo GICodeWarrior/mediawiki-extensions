4feb5722fa42ad8ff244d4db239ce2e3b9d5c04e
emil <emil@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-08 16:36:20 +0000

    fixing a directory name of this extension as all its files refer to regexBlock/ while it was uploaded as RegexBlock/
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@27320 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- debs/wikimedia-task-dns-auth/debian/cron.d
+++
@@ -1,2 +0,0 @@
-07 04 * * *     pdns    rsync -qt rsync://countries-ns.mdc.dk/zone/zz.countries.nerd.dk.rbldnsd \
-                        /etc/powerdns/zz.countries.nerd.dk.rbldnsd && pdns_control rediscover > /dev/null
