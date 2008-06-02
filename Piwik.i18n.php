d94f789507fd618548e28bdcf5833052a41782ee
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-02 14:52:25 +0000

    Renamed (capital "P" in directory name for consistency)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@35719 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Piwik.i18n.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Internationalisation file for extension Piwik Integration.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Isb1009
+ */
+$messages['en'] = array(
+	'piwik-desc' => 'Inserts Piwik script into MediaWiki pages for tracking and adds a [[Special:Piwik|special page]] with some stats',
+	'piwik' => 'Piwik',
+	'lastvisits' => 'Last Visits',
+	'countries' => 'Countries',
+	'browsers' => 'Browsers',
+);
+
+/** Catalan (Català)
+ * @author Isb1009
+ */
+$messages['ca'] = array(
+	'piwik-desc' => 'Insereix el codi de Piwik dins les pàgines del MediaWiki per a fer les estadístiques',
+	'piwik' => 'Piwik',
+	'lastvisits' => 'Darreres visites',
+	'countries' => 'Països',
+	'browsers' => 'Navegadors',
+);
+
+/** German (Deutsch)
+ * @author Raimond Spekking
+ */
+$messages['de'] = array(
+	'piwik-desc' => 'Fügt ein Piwik-Skript zum Tracking in MediaWiki-Seiten ein und ergänzt eine [[Special:Piwik|Spezialseite]] mit einigen Statistiken',
+	'piwik'      => 'Piwik',
+	'lastvisits' => 'Letzte Besuche',
+	'countries'  => 'Länder',
+	'browsers'   => 'Browser',
+);
+
+/** Spanish (Castellano)
+ * @author Isb1009
+ */
+$messages['es'] = array(
+	'piwik-desc' => 'Inseta el código de Piwik en las páginas de la wiki MediaWiki para hacer las estadísticas',
+	'piwik' => 'Piwik',
+	'lastvisits' => 'Últimas visitas',
+	'countries' => 'Países',
+	'browsers' => 'Navegadores',
+);
