GICodeWarrior/mediawiki-extensions

commit e8ad7c531f4fc08193fc6817cd478448d90b1f8a

Add array support to wfArrayToCGI(): when receiving parameters in the form of &foo[]=bar&foo[]=baz, PHP automatically sets $_GET['foo'] = array(bar, baz); When feeding the original query back to wfArrayToCGI(), it used to choke on that. Now wfArrayToCGI() actually converts array('bar', 'baz'); back to the &foo[]=bar&foo[]=baz form.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@35298 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/GlobalFunctions.php
@@ -999,7 +999,13 @@ function wfArrayToCGI( $array1, $array2 = NULL )
 			if ( '' != $cgi ) {
 				$cgi .= '&';
 			}
-			$cgi .= urlencode( $key ) . '=' . urlencode( $value );
+			if(is_array($value))
+				foreach($value as $v)
+					$cgi .= urlencode( $key . '[]' ) . '=' .
+						urlencode( $v );
+			else
+				$cgi .= urlencode( $key ) . '=' .
+					urlencode( $value );
 		}
 	}
 	return $cgi;

commit 07dd87f2aa5a4b61c6a81a7f66ea7e36b9cfcd29

Fixing regression from r35298: wasn't adding enough ampersands.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@35783 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/GlobalFunctions.php
@@ -1000,9 +1000,16 @@ function wfArrayToCGI( $array1, $array2 = NULL )
 				$cgi .= '&';
 			}
 			if(is_array($value))
+			{
+				$firstTime = true;
 				foreach($value as $v)
-					$cgi .= urlencode( $key . '[]' ) . '=' .
+				{
+					$cgi .= ($firstTime ? '' : '&') .
+						urlencode( $key . '[]' ) . '=' .
 						urlencode( $v );
+					$firstTime = false;
+				}
+			}
 			else
 				$cgi .= urlencode( $key ) . '=' .
 					urlencode( $value );