GICodeWarrior/mediawiki-extensions

commit 9f4a082f3506e370b9402e33852421f022e36818

Use the proper Title methods in ApiQueryBase::titleToKey() and keyToTitle(). This fixes bug 14651 for much more than just apprefix.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@36678 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/api/ApiQueryBase.php
@@ -319,14 +319,13 @@ abstract class ApiQueryBase extends ApiBase {
 	}
 
 	/**
-	 * This is a very simplistic utility function
-	 * to convert a non-namespaced title string to a db key.
-	 * It will replace all ' ' with '_'
+	 * Convert a title to a DB key
 	 * @param string $title Page title with spaces
 	 * @return string Page title with underscores
 	 */
 	public static function titleToKey($title) {
-		return str_replace(' ', '_', $title);
+		$t = Title::newFromText($title);
+		return $t->getDbKey();
 	}
 
 	/**
@@ -335,7 +334,8 @@ abstract class ApiQueryBase extends ApiBase {
 	 * @return string Page title with spaces
 	 */
 	public static function keyToTitle($key) {
-		return str_replace('_', ' ', $key);
+		$t = Title::newFromDbKey($key);
+		return $t->getPrefixedText();
 	}
 
 	/**

phase3/RELEASE-NOTES
@@ -469,6 +469,7 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * Added nocreate parameter to action=edit
 * (bug 14402) Added maxage and smaxage parameters to api.php
 * Added bkip parameter to list=blocks
+* (bug 14651) apprefix and similar parameters are now canonicalized
 
 === Languages updated in 1.13 ===
 

commit 0a4299365f581da0f35f2bdaf6efbe9a0cf8b731

Fix fatal errors introduced in r36678
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@36719 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/api/ApiQueryBase.php
@@ -325,6 +325,8 @@ abstract class ApiQueryBase extends ApiBase {
 	 */
 	public static function titleToKey($title) {
 		$t = Title::newFromText($title);
+		if(!$t)
+			$this->dieUsageMsg('invalidtitle', $title);
 		return $t->getDbKey();
 	}
 
@@ -335,6 +337,9 @@ abstract class ApiQueryBase extends ApiBase {
 	 */
 	public static function keyToTitle($key) {
 		$t = Title::newFromDbKey($key);
+		# This really shouldn't happen but we gotta check anyway
+		if(!$t)
+			$this->dieUsageMsg('invalidtitle', $key);
 		return $t->getPrefixedText();
 	}
 