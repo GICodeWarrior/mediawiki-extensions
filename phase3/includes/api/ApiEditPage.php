GICodeWarrior/mediawiki-extensions

commit dcd871cc87ae82276961c2cdaf35cc5b94406889

(bug 19090) Added watchlist parameter, deprecated watch and unwatch parameter in action=edit
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@53266 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/api/ApiEditPage.php
@@ -169,18 +169,37 @@ class ApiEditPage extends ApiBase {
 		else
 			$reqArr['wpSection'] = '';
 
-		if($params['watch'])
+		// Handle watchlist settings
+		switch ($params['watchlist']) 
+		{
+			case 'watch':
+				$watch = true;
+				break;
+			case 'unwatch':
+				$watch = false;
+				break;
+			case 'preferences':
+				if ($titleObj->exists())
+					$watch = $wgUser->getOption('watchdefault');
+				else
+					$watch = $wgUser->getOption('watchcreations');
+				break;
+			case 'nochange':
+			default:
+				$watch = $titleObj->userIsWatching();
+		}
+		// Deprecated parameters
+		if ($params['watch']) 
+		{
 			$watch = true;
-		else if($params['unwatch'])
+			$this->setWarning('The watch parameter has been deprecated.');
+		}
+		elseif ($params['unwatch']) 
+		{
 			$watch = false;
-		else if($titleObj->userIsWatching())
-			$watch = true;
-		else if($wgUser->getOption('watchdefault'))
-			$watch = true;
-		else if($wgUser->getOption('watchcreations') && !$titleObj->exists())
-			$watch = true;
-		else
-			$watch = false;
+			$this->setWarning('The unwatch parameter has been deprecated.');
+		}
+		
 		if($watch)
 			$reqArr['wpWatchthis'] = '';
 
@@ -318,6 +337,15 @@ class ApiEditPage extends ApiBase {
 			'captchaid' => null,
 			'watch' => false,
 			'unwatch' => false,
+			'watchlist' => array(
+				ApiBase :: PARAM_DFLT => 'preferences',
+				ApiBase :: PARAM_TYPE => array(
+					'watch', 
+					'unwatch', 
+					'preferences', 
+					'nochange'
+				),
+			),
 			'md5' => null,
 			'prependtext' => null,
 			'appendtext' => null,
@@ -349,8 +377,9 @@ class ApiEditPage extends ApiBase {
 			'recreate' => 'Override any errors about the article having been deleted in the meantime',
 			'createonly' => 'Don\'t edit the page if it exists already',
 			'nocreate' => 'Throw an error if the page doesn\'t exist',
-			'watch' => 'Add the page to your watchlist',
-			'unwatch' => 'Remove the page from your watchlist',
+			'watch' => 'DEPRECATED! Add the page to your watchlist',
+			'unwatch' => 'DEPRECATED! Remove the page from your watchlist',
+			'watchlist' => 'Unconditionally add or remove the page from your watchlist, use preferences or do not change watch',
 			'captchaid' => 'CAPTCHA ID from previous request',
 			'captchaword' => 'Answer to the CAPTCHA',
 			'md5' => array(	'The MD5 hash of the text parameter, or the prependtext and appendtext parameters concatenated.',

phase3/RELEASE-NOTES
@@ -296,6 +296,8 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * (bug 19528) Added XSLT parameter to API queries in format=xml
 * (bug 19040) Fix prependtext and appendtext in combination with section 
   parameter in action=edit
+* (bug 19090) Added watchlist parameter, deprecated watch and unwatch 
+  parameter in action=edit
 
 === Languages updated in 1.16 ===
 

commit 003b7e2abc67e1ebe3940b2eb8bf5868562bf24e

API: (bug 20816) Editing a page with &watchlist=preferences (default) unwatches it if you're watching it but don't have watching preferences set. This is a regression from r53266
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@56940 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/api/ApiEditPage.php
@@ -180,7 +180,7 @@ class ApiEditPage extends ApiBase {
 				break;
 			case 'preferences':
 				if ($titleObj->exists())
-					$watch = $wgUser->getOption('watchdefault');
+					$watch = $wgUser->getOption('watchdefault') || $titleObj->userIsWatching();
 				else
 					$watch = $wgUser->getOption('watchcreations');
 				break;