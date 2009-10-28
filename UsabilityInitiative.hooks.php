GICodeWarrior/mediawiki-extensions

commit 127452f5bda535f25584d8e128d0346db21303f1

Changed click handlers to mousedown - improves precieved performance.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@57836 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

js/plugins/jquery.wikiEditor.toolbar.js
@@ -326,25 +326,23 @@ fn : {
 			.text( label )
 			.attr( 'rel', id )
 			.data( 'context', context )
-			.click(
-				function() {
-					
-					$(this).parent().parent().find( '.page' ).hide();
-					$(this).parent().parent().find( '.page-' + $(this).attr( 'rel' ) ).show();
-					$(this).siblings().removeClass( 'current' );
-					$(this).addClass( 'current' );
-					var section = $(this).parent().parent().attr( 'rel' );
-					
-					//click tracking
-					if($.trackAction != undefined){
-						$.trackAction(section + '.' + $(this).attr('rel'));
-					}
-					
-					$.cookie(
-						'wikiEditor-' + $(this).data( 'context' ).instance + '-booklet-' + section + '-page',
-						$(this).attr( 'rel' )
-					);
-				} );
+			.bind( 'mousedown', function() {
+				$(this).parent().parent().find( '.page' ).hide();
+				$(this).parent().parent().find( '.page-' + $(this).attr( 'rel' ) ).show();
+				$(this).siblings().removeClass( 'current' );
+				$(this).addClass( 'current' );
+				var section = $(this).parent().parent().attr( 'rel' );
+				
+				//click tracking
+				if($.trackAction != undefined){
+					$.trackAction(section + '.' + $(this).attr('rel'));
+				}
+				
+				$.cookie(
+					'wikiEditor-' + $(this).data( 'context' ).instance + '-booklet-' + section + '-page',
+					$(this).attr( 'rel' )
+				);
+			} );
 	},
 	buildPage : function( context, id, page ) {
 		var $page = $( '<div />' ).attr( {
@@ -451,7 +449,7 @@ fn : {
 					.attr( 'href', '#' )
 					.text( $.wikiEditor.autoMsg( section, 'label' ) )
 					.data( 'context', context )
-					.click( function() {
+					.bind( 'mousedown', function() {
 						var $sections = $(this).data( 'context' ).$ui.find( '.sections' );
 						var $section =
 							$(this).data( 'context' ).$ui.find( '.section-' + $(this).parent().attr( 'rel' ) );

commit 0c104bd42b6dcc97c4eeeac548d6da86888fb0d1

EditToolbar: (bug 21323) Clicking tabs makes browser jump to top of page; regression from r57836
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@58253 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

UsabilityInitiative.hooks.php
@@ -70,15 +70,15 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 6 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 16 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 12 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 16 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 17 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 6 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 22 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 51 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 52 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 51 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 52 ),
 			),
 		),
 	);

js/plugins.combined.js
@@ -2118,8 +2118,8 @@ fn : {
 							'wikiEditor-' + $(this).data( 'context' ).instance + '-toolbar-section',
 							show ? $section.attr( 'rel' ) : null
 						);
-						return false;
 					} )
+					.click( function() { return false; } )
 			);
 	},
 	buildSection : function( context, id, section ) {

js/plugins/jquery.wikiEditor.toolbar.js
@@ -488,8 +488,8 @@ fn : {
 							'wikiEditor-' + $(this).data( 'context' ).instance + '-toolbar-section',
 							show ? $section.attr( 'rel' ) : null
 						);
-						return false;
 					} )
+					.click( function() { return false; } )
 			);
 	},
 	buildSection : function( context, id, section ) {