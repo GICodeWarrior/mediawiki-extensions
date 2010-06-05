72c796ecda4cdb343deacf03353d99c92aeef274
tisane <tisane@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-05 16:45:57 +0000

    Change name. Integration->InterwikiIntegration
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@67408 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ InterwikiIntegration.i18n.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Internationalisation file for the InterwikiIntegration extension
+ * @addtogroup Extensions
+ */
+ 
+$messages = array();
+ 
+/* English
+ * @author Tisane
+ */
+$messages['en'] = array(
+	'integration' => 'InterwikiIntegration',
+	'integration-desc' => 'Comprehensive interwiki integration',
+        'populateintegrationtable' => 'Populate integration table',
+        'integration-setuptext' => '$1\'s tables have been configured. Be sure '
+            .'to configure the tables on your other wikis as well.'
+);
+
+$aliases = array(); 
+
+$aliases['en'] = array(
+ 	'PopulateInterwikiIntegrationTable' => array( 'PopulateInterwikiIntegrationTable' ),
+ 	);
+
