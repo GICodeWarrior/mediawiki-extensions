GICodeWarrior/mediawiki-extensions

commit 09d68cad023912690cb45ab1e426384edab9d400

Fixed a bunch of silly instances of [^!=]==\s*(true|false)
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@67707 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/db/DatabasePostgres.php
@@ -181,7 +181,7 @@ class DatabasePostgres extends DatabaseBase {
 		$this->mConn = pg_connect( $connectString );
 		$phpError = $this->restoreErrorHandler();
 
-		if ( $this->mConn == false ) {
+		if ( !$this->mConn ) {
 			wfDebug( "DB connection error\n" );
 			wfDebug( "Server: $server, Database: $dbName, User: $user, Password: " . substr( $password, 0, 3 ) . "...\n" );
 			wfDebug( $this->lastError()."\n" );
@@ -309,7 +309,7 @@ class DatabasePostgres extends DatabaseBase {
 				$connectVars['password'] = $password;
 
 				@$this->mConn = pg_connect( $this->makeConnectionString( $connectVars ) );
-				if ( $this->mConn == false ) {
+				if ( $this->mConn ) {
 					print "<b>FAILED TO CONNECT!</b></li>";
 					dieout("</ul>");
 				}
@@ -1355,7 +1355,7 @@ SQL;
 		echo "<li>Populating interwiki table... ";
 		## Avoid the non-standard "REPLACE INTO" syntax
 		$f = fopen( "../maintenance/interwiki.sql", 'r' );
-		if ($f == false ) {
+		if ( $f ) {
 			print "<b>FAILED</b></li>";
 			dieout( "Could not find the interwiki.sql file" );
 		}

phase3/config/Installer.php
@@ -1330,7 +1330,7 @@ if( $conf->posted && ( 0 == count( $errs ) ) ) {
 		$localSettings = str_replace( "\r\n", "\n", $localSettings );
 		$f = fopen( "LocalSettings.php", 'xt' );
 
-		if( $f == false ) {
+		if( !$f ) {
 			print( "</li>\n" );
 			dieout( "<p>Couldn't write out LocalSettings.php. Check that the directory permissions are correct and that there isn't already a file of that name here...</p>\n" .
 			"<p>Here's the file that would have been written, try to paste it into place manually:</p>\n" .

phase3/includes/BagOStuff.php
@@ -102,7 +102,7 @@ abstract class BagOStuff {
 	}
 
 	public function add( $key, $value, $exptime = 0 ) {
-		if ( $this->get( $key ) == false ) {
+		if ( !$this->get( $key ) ) {
 			$this->set( $key, $value, $exptime );
 
 			return true;

phase3/includes/EditPage.php
@@ -2628,7 +2628,7 @@ INPUTS
 	}
 
 	function getBaseRevision() {
-		if ( $this->mBaseRevision == false ) {
+		if ( !$this->mBaseRevision ) {
 			$db = wfGetDB( DB_MASTER );
 			$baseRevision = Revision::loadFromTimestamp(
 				$db, $this->mTitle, $this->edittime );

phase3/includes/GlobalFunctions.php
@@ -1500,7 +1500,7 @@ function wfMerge( $old, $mine, $yours, &$result ){
 	pclose( $handle );
 	unlink( $mytextName ); unlink( $oldtextName ); unlink( $yourtextName );
 
-	if ( $result === '' && $old !== '' && $conflict == false ) {
+	if ( $result === '' && $old !== '' && !$conflict ) {
 		wfDebug( "Unexpected null result from diff3. Command: $cmd\n" );
 		$conflict = true;
 	}

phase3/includes/ImageGallery.php
@@ -183,7 +183,7 @@ class ImageGallery
 	 * @param $f Boolean: set to false to disable.
 	 */
 	function setShowBytes( $f ) {
-		$this->mShowBytes = ( $f == true);
+		$this->mShowBytes = $f;
 	}
 
 	/**
@@ -193,7 +193,7 @@ class ImageGallery
 	 * @param $f Boolean: set to false to disable.
 	 */
 	function setShowFilename( $f ) {
-		$this->mShowFilename = ( $f == true);
+		$this->mShowFilename = $f;
 	}
 
 	/**

phase3/includes/OutputPage.php
@@ -39,6 +39,7 @@ class OutputPage {
 	var $mParseWarnings = array();
 	var $mSquidMaxage = 0;
 	var $mRevisionId = null;
+	var $mPageIcons = array();
 	protected $mTitle = null;
 
 	/**
@@ -1079,6 +1080,7 @@ class OutputPage {
 		$this->addCategoryLinks( $parserOutput->getCategories() );
 		$this->mNewSectionLink = $parserOutput->getNewSection();
 		$this->mHideNewSectionLink = $parserOutput->getHideNewSection();
+		$this->mPageIcons = $parserOutput->getPageIcons();
 
 		$this->mParseWarnings = $parserOutput->getWarnings();
 		if ( !$parserOutput->isCacheable() ) {

phase3/includes/SkinTemplate.php
@@ -230,6 +230,21 @@ class SkinTemplate extends Skin {
 		$tpl->set( 'pageclass', $this->getPageClasses( $this->mTitle ) );
 		$tpl->set( 'skinnameclass', ( 'skin-' . Sanitizer::escapeClass( $this->getSkinName() ) ) );
 
+		$icons = '';
+		foreach( $out->mPageIcons as $icon ) {
+			list( $file, $alt ) = $icon;
+			$fileAttr = array( 'src' => $file->createThumb( 16 ) );
+			if( $alt != '' ) {
+				$msg = wfMsg( $alt );
+				if( !wfEmptyMsg( $alt ) ) {
+					$alt = $msg;
+				}
+				$fileAttr['alt'] = htmlspecialchars( $alt );
+			}
+			$icons .= Html::element( 'img', $fileAttr ) . "&nbsp;";
+		}
+		$tpl->set( 'pageicons', $icons );
+
 		$nsname = MWNamespace::exists( $this->mTitle->getNamespace() ) ?
 					MWNamespace::getCanonicalName( $this->mTitle->getNamespace() ) :
 					$this->mTitle->getNsText();

commit 12502e0aec3b291d5913ce1a57df8419d41ee863

Fixed a fatal installer failure on PG introduced in r67707. 1.16 is unaffected
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@68716 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/db/DatabasePostgres.php
@@ -1355,7 +1355,7 @@ SQL;
 		echo "<li>Populating interwiki table... ";
 		## Avoid the non-standard "REPLACE INTO" syntax
 		$f = fopen( "../maintenance/interwiki.sql", 'r' );
-		if ( $f ) {
+		if ( !$f ) {
 			print "<b>FAILED</b></li>";
 			dieout( "Could not find the interwiki.sql file" );
 		}