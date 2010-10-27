55258d8bb919b24383ce785fd06428e5e4a5572f
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-27 12:56:19 +0000

    Remove unused global. Fix typo, wrap some lines
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75536 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WikiBhasha/WikiBhashaSpecial.php
+++ extensions/WikiBhasha/WikiBhashaSpecial.php
@@ -3,7 +3,7 @@
 /*
 * 
 * We need to overload the special page constructor to initialize our own data also if we want to change the behavior of the SpecialPage class itself. 
-* it will ececute when called from the child class
+* it will execute when called from the child class
 * 
 */
 
@@ -21,7 +21,7 @@ class WikiBhasha extends SpecialPage {
 	 * @param $par Parameters passed to the page
 	 */
 	function execute( $par ) {
-		global $wgRequest, $wgOut;
+		global $wgOut;
 		$this->setHeaders();
 		$wgOut->addHTML( '<h2>message goes here</h2>' );
 	}
