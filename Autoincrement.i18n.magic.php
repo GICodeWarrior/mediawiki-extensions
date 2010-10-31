cbece93c30711131f5dbc10f658caaf67dbb39d6
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75726 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Autoincrement.i18n.magic.php
+++ Autoincrement.i18n.magic.php
@@ -2,54 +2,67 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
 	'autoincrement' => array( 0, 'AUTOINCREMENT' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'autoincrement'   => array( '0', 'زيادة_تلقائية', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'زيادة_تلقائية', 'AUTOINCREMENT' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'autoincrement'   => array( '0', 'زيادة_تلقائية', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'زيادة_تلقائية', 'AUTOINCREMENT' ),
 );
 
+/** Chechen (Нохчийн) */
 $magicWords['ce'] = array(
-	'autoincrement'   => array( '0', 'ШАЙОККХОЙО', 'АВТОИНКРЕМЕНТ', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'ШАЙОККХОЙО', 'АВТОИНКРЕМЕНТ', 'AUTOINCREMENT' ),
 );
 
+/** Spanish (Español) */
 $magicWords['es'] = array(
-	'autoincrement'   => array( '0', 'AUTOINCREMENTO', 'AUTOINCREMENTAR', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'AUTOINCREMENTO', 'AUTOINCREMENTAR', 'AUTOINCREMENT' ),
 );
 
+/** Japanese (日本語) */
 $magicWords['ja'] = array(
-	'autoincrement'   => array( '0', 'オートインクリメント', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, '自動インクリメント', 'オートインクリメント' ),
 );
 
+/** Korean (한국어) */
 $magicWords['ko'] = array(
-	'autoincrement'   => array( '0', '번호', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, '번호' ),
 );
 
+/** Malayalam (മലയാളം) */
 $magicWords['ml'] = array(
-	'autoincrement'   => array( '0', 'സ്വയംകൂടുക', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'സ്വയംകൂടുക' ),
 );
 
+/** Marathi (मराठी) */
 $magicWords['mr'] = array(
-	'autoincrement'   => array( '0', 'स्वयंवाढ', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'स्वयंवाढ', 'AUTOINCREMENT' ),
 );
 
+/** Nedersaksisch (Nedersaksisch) */
 $magicWords['nds-nl'] = array(
-	'autoincrement'   => array( '0', 'AUTEMATISCHOPHOGEN', 'AUTOMATISCHOPHOGEN', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'AUTEMATISCHOPHOGEN', 'AUTOMATISCHOPHOGEN', 'AUTOINCREMENT' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'autoincrement'   => array( '0', 'AUTOMATISCHOPHOGEN', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'AUTOMATISCHOPHOGEN' ),
 );
 
+/** Russian (Русский) */
 $magicWords['ru'] = array(
-	'autoincrement'   => array( '0', 'АВТОИНКРЕМЕНТ', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'АВТОИНКРЕМЕНТ', 'AUTOINCREMENT' ),
 );
 
+/** Swedish (Svenska) */
 $magicWords['sv'] = array(
-	'autoincrement'   => array( '0', 'ÖKAAUTOMATISKT', 'AUTOINCREMENT' ),
+	'autoincrement' => array( 0, 'ÖKAAUTOMATISKT', 'AUTOINCREMENT' ),
 );
