8571cae12bce9cfceabf05d4f52678eb4ab44501
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75726 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- I18nTags.magic.php
+++ I18nTags.magic.php
@@ -2,122 +2,157 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
 	'languagename' => array( 0, 'languagename' ),
 );
 
+/** Afrikaans (Afrikaans) */
 $magicWords['af'] = array(
-	'languagename'   => array( '0', 'taalnaam', 'languagename' ),
+	'languagename' => array( 0, 'taalnaam', 'languagename' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'languagename'   => array( '0', 'اسم_اللغة', 'languagename' ),
+	'languagename' => array( 0, 'اسم_اللغة', 'languagename' ),
 );
 
+/** Aramaic (ܐܪܡܝܐ) */
 $magicWords['arc'] = array(
-	'languagename'   => array( '0', 'ܫܡܐ_ܕܠܫܢܐ', 'languagename' ),
+	'languagename' => array( 0, 'ܫܡܐ_ܕܠܫܢܐ', 'languagename' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'languagename'   => array( '0', 'اسم_اللغة', 'languagename' ),
+	'languagename' => array( 0, 'اسم_اللغة', 'languagename' ),
 );
 
+/** Breton (Brezhoneg) */
 $magicWords['br'] = array(
-	'languagename'   => array( '0', 'anvyezh', 'languagename' ),
+	'languagename' => array( 0, 'anvyezh', 'languagename' ),
 );
 
+/** Bosnian (Bosanski) */
 $magicWords['bs'] = array(
-	'languagename'   => array( '0', 'imejezika', 'languagename' ),
+	'languagename' => array( 0, 'imejezika' ),
 );
 
+/** Chechen (Нохчийн) */
 $magicWords['ce'] = array(
-	'languagename'   => array( '0', 'меттацlе', 'названиеязыка', 'languagename' ),
+	'languagename' => array( 0, 'меттацlе', 'названиеязыка', 'languagename' ),
 );
 
+/** Czech (Česky) */
 $magicWords['cs'] = array(
-	'languagename'   => array( '0', 'názevjazyka', 'languagename' ),
+	'languagename' => array( 0, 'názevjazyka', 'languagename' ),
 );
 
+/** Esperanto (Esperanto) */
 $magicWords['eo'] = array(
-	'languagename'   => array( '0', 'nomolingvo', 'lingvonomo', 'languagename' ),
+	'languagename' => array( 0, 'nomolingvo', 'lingvonomo' ),
 );
 
+/** Spanish (Español) */
 $magicWords['es'] = array(
-	'languagename'   => array( '0', 'nombreidioma', 'nombredeidioma', 'languagename' ),
+	'languagename' => array( 0, 'nombreidioma', 'nombredeidioma', 'languagename' ),
 );
 
+/** Estonian (Eesti) */
 $magicWords['et'] = array(
-	'languagename'   => array( '0', 'keelenimetus', 'languagename' ),
+	'languagename' => array( 0, 'keelenimetus' ),
 );
 
+/** French (Français) */
 $magicWords['fr'] = array(
-	'languagename'   => array( '0', 'nomlangue', 'languagename' ),
+	'languagename' => array( 0, 'nomlangue', 'languagename' ),
 );
 
+/** Franco-Provençal (Arpetan) */
 $magicWords['frp'] = array(
-	'languagename'   => array( '0', 'nom_de_lengoua', 'nomlangue', 'languagename' ),
+	'languagename' => array( 0, 'nom_de_lengoua', 'nomlangue', 'languagename' ),
 );
 
+/** Italian (Italiano) */
 $magicWords['it'] = array(
-	'languagename'   => array( '0', 'nomelingua', 'languagename' ),
+	'languagename' => array( 0, 'nomelingua' ),
 );
 
+/** Japanese (日本語) */
 $magicWords['ja'] = array(
-	'languagename'   => array( '0', '言語名', 'languagename' ),
+	'languagename' => array( 0, '言語名' ),
 );
 
+/** Colognian (Ripoarisch) */
 $magicWords['ksh'] = array(
-	'languagename'   => array( '0', 'ShproocheName', 'languagename' ),
+	'languagename' => array( 0, 'ShproocheName', 'languagename' ),
 );
 
+/** Ladino (Ladino) */
 $magicWords['lad'] = array(
-	'languagename'   => array( '0', 'nombredelingua', 'nombreidioma', 'nombredeidioma', 'languagename' ),
+	'languagename' => array( 0, 'nombredelingua', 'nombreidioma', 'nombredeidioma', 'languagename' ),
 );
 
+/** Malayalam (മലയാളം) */
 $magicWords['ml'] = array(
-	'languagename'   => array( '0', 'ഭാഷാനാമം', 'languagename' ),
+	'languagename' => array( 0, 'ഭാഷാനാമം' ),
 );
 
+/** Marathi (मराठी) */
 $magicWords['mr'] = array(
-	'languagename'   => array( '0', 'भाषानाव', 'languagename' ),
+	'languagename' => array( 0, 'भाषानाव', 'languagename' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'languagename'   => array( '0', 'taalnaam', 'languagename' ),
+	'languagename' => array( 0, 'taalnaam' ),
 );
 
+/** Norwegian Nynorsk (‪Norsk (nynorsk)‬) */
 $magicWords['nn'] = array(
-	'languagename'   => array( '0', 'språknamn', 'languagename' ),
+	'languagename' => array( 0, 'språknamn', 'languagename' ),
 );
 
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬) */
 $magicWords['no'] = array(
-	'languagename'   => array( '0', 'språknavn', 'languagename' ),
+	'languagename' => array( 0, 'språknavn', 'languagename' ),
 );
 
+/** Pashto (پښتو) */
 $magicWords['ps'] = array(
-	'languagename'   => array( '0', 'ژبنوم', 'languagename' ),
+	'languagename' => array( 0, 'ژبنوم', 'languagename' ),
 );
 
+/** Romanian (Română) */
 $magicWords['ro'] = array(
-	'languagename'   => array( '0', 'numelimba', 'languagename' ),
+	'languagename' => array( 0, 'numelimba', 'languagename' ),
 );
 
+/** Russian (Русский) */
 $magicWords['ru'] = array(
-	'languagename'   => array( '0', 'названиеязыка', 'languagename' ),
+	'languagename' => array( 0, 'названиеязыка', 'languagename' ),
 );
 
+/** Swedish (Svenska) */
 $magicWords['sv'] = array(
-	'languagename'   => array( '0', 'språknamn', 'languagename' ),
+	'languagename' => array( 0, 'språknamn', 'languagename' ),
 );
 
+/** Turkish (Türkçe) */
 $magicWords['tr'] = array(
-	'languagename'   => array( '0', 'diladı', 'dilismi', 'lisanadı', 'lisanismi', 'languagename' ),
+	'languagename' => array( 0, 'diladı', 'dilismi', 'lisanadı', 'lisanismi', 'languagename' ),
 );
 
+/** Ukrainian (Українська) */
 $magicWords['uk'] = array(
-	'languagename'   => array( '0', 'назвамови', 'названиеязыка', 'languagename' ),
+	'languagename' => array( 0, 'назвамови', 'названиеязыка', 'languagename' ),
 );
 
+/** Vietnamese (Tiếng Việt) */
+$magicWords['vi'] = array(
+	'languagename' => array( 0, 'tênngônngữ' ),
+);
+
+/** Simplified Chinese (‪中文(简体)‬) */
 $magicWords['zh-hans'] = array(
-	'languagename'   => array( '0', '语言名称', 'languagename' ),
+	'languagename' => array( 0, '语言名称', 'languagename' ),
 );
