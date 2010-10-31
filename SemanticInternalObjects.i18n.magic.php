2ac6d6e9a605b5a847e892af2ddd7aa1f8bbc7cd
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75726 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticInternalObjects.i18n.magic.php
+++ SemanticInternalObjects.i18n.magic.php
@@ -2,18 +2,27 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
 	'set_internal' => array( 0, 'set_internal' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'set_internal'   => array( '0', 'ضبط_داخلي', 'set_internal' ),
+	'set_internal' => array( 0, 'ضبط_داخلي', 'set_internal' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'set_internal'   => array( '0', 'ضبط_داخلي', 'set_internal' ),
+	'set_internal' => array( 0, 'ضبط_داخلي', 'set_internal' ),
 );
 
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'set_internal' => array( 0, '内部設定' ),
+);
+
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'set_internal'   => array( '0', 'intern_instellen', 'set_internal' ),
+	'set_internal' => array( 0, 'intern_instellen' ),
 );
