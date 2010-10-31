78dee9c9da08246c897e1494503cd2644bfd0546
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75726 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SmoothGallery.i18n.magic.php
+++ SmoothGallery.i18n.magic.php
@@ -2,30 +2,42 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
 	'sgallery' => array( 0, 'sgallery' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'sgallery'   => array( '0', 'معرض_سلس', 'sgallery' ),
+	'sgallery' => array( 0, 'معرض_سلس', 'sgallery' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'sgallery'   => array( '0', 'معرض_سلس', 'sgallery' ),
+	'sgallery' => array( 0, 'معرض_سلس', 'sgallery' ),
 );
 
+/** Spanish (Español) */
 $magicWords['es'] = array(
-	'sgallery'   => array( '0', 'galerías', 'galerias', 'sgallery' ),
+	'sgallery' => array( 0, 'galerías', 'galerias', 'sgallery' ),
 );
 
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'sgallery' => array( 0, 'Sギャラリー' ),
+);
+
+/** Marathi (मराठी) */
 $magicWords['mr'] = array(
-	'sgallery'   => array( '0', 'मृदूदिर्घा', 'sgallery' ),
+	'sgallery' => array( 0, 'मृदूदिर्घा', 'sgallery' ),
 );
 
+/** Nedersaksisch (Nedersaksisch) */
 $magicWords['nds-nl'] = array(
-	'sgallery'   => array( '0', 'suutstalling', 'sgalerij', 'sgallery' ),
+	'sgallery' => array( 0, 'suutstalling', 'sgalerij', 'sgallery' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'sgallery'   => array( '0', 'sgalerij', 'sgallery' ),
+	'sgallery' => array( 0, 'sgalerij' ),
 );
