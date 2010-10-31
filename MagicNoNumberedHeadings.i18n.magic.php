f18a7e7d02f0a1ea9abfbb7883b2efc14695c878
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75726 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MagicNoNumberedHeadings.i18n.magic.php
+++ MagicNoNumberedHeadings.i18n.magic.php
@@ -2,62 +2,77 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
 	'MAG_NONUMBEREDHEADINGS' => array( 0, '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', 'لا_عناوين_مرقمة', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, 'لا_عناوين_مرقمة', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__لا_عناوين_مرقمة__', 'لا_عناوين_مرقمة', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__لا_عناوين_مرقمة__', 'لا_عناوين_مرقمة', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Chechen (Нохчийн) */
 $magicWords['ce'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__ТЕРАХЬДОЦУШКОРТА__', '__БЕЗНОМЕРОВЗАГОЛОВКОВ__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__ТЕРАХЬДОЦУШКОРТА__', '__БЕЗНОМЕРОВЗАГОЛОВКОВ__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** German (Deutsch) */
 $magicWords['de'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__KEINEÜERSCHRIFTENNUMMERIERUNG__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__KEINEÜERSCHRIFTENNUMMERIERUNG__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Spanish (Español) */
 $magicWords['es'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__ENCABEZADOSNOENUMERADOS__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__ENCABEZADOSNOENUMERADOS__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Colognian (Ripoarisch) */
 $magicWords['ksh'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__ÖVERSCHRIFTENITNUMMERIERE__', '__KEINEÜERSCHRIFTENNUMMERIERUNG__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__ÖVERSCHRIFTENITNUMMERIERE__', '__KEINEÜERSCHRIFTENNUMMERIERUNG__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Malayalam (മലയാളം) */
 $magicWords['ml'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__ക്രമസംഖ്യാരഹിതതലക്കെട്ടുകൾ__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__ക്രമസംഖ്യാരഹിതതലക്കെട്ടുകൾ__' ),
 );
 
+/** Marathi (मराठी) */
 $magicWords['mr'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__विनाअंकमथळे__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__विनाअंकमथळे__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Nedersaksisch (Nedersaksisch) */
 $magicWords['nds-nl'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__GIENENUMMERDEKOPJES__', '__GEENGENUMMERDEKOPPEN__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__GIENENUMMERDEKOPJES__', '__GEENGENUMMERDEKOPPEN__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__GEENGENUMMERDEKOPPEN__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__GEENGENUMMERDEKOPPEN__' ),
 );
 
+/** Russian (Русский) */
 $magicWords['ru'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__БЕЗНОМЕРОВЗАГОЛОВКОВ__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__БЕЗНОМЕРОВЗАГОЛОВКОВ__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Swedish (Svenska) */
 $magicWords['sv'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__INTENUMRERADERUBRIKER__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__INTENUMRERADERUBRIKER__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Turkish (Türkçe) */
 $magicWords['tr'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__NUMARALIBAŞLIKYOK__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__NUMARALIBAŞLIKYOK__', '__NONUMBEREDHEADINGS__' ),
 );
 
+/** Ukrainian (Українська) */
 $magicWords['uk'] = array(
-	'MAG_NONUMBEREDHEADINGS'   => array( '0', '__БЕЗНОМЕРІВЗАГОЛОВКІВ__', '__БЕЗНОМЕРОВЗАГОЛОВКОВ__', '__NONUMBEREDHEADINGS__' ),
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__БЕЗНОМЕРІВЗАГОЛОВКІВ__', '__БЕЗНОМЕРОВЗАГОЛОВКОВ__', '__NONUMBEREDHEADINGS__' ),
 );
