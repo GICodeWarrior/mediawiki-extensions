81e8ac81994030f19c72c51ceb0bd1586accb12b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-31 19:49:33 +0000

    Add a Toolserver tool from User:Kolossos. It shows OpenStreetMap maps rendered on the Toolserver in Wikipedia pages.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75740 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ToolserverTools/KolossosKmlOnOpenlayers/kml-on-ol-source.i18n.php.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Internationalisation for OpenStreetMapIntegration in Wikipedia
+ * Source: http://toolserver.org/~kolossos/openlayers/kml-on-ol-source.i18n.php
+ * Used in program http://toolserver.org/~kolossos/openlayers/kml-on-ol-source.php
+ * Example of a map: http://toolserver.org/~kolossos/openlayers/kml-on-ol.php?lang=en&uselang=de&zoom=6&lat=48.85338&lon=2.36958
+ * with lang=en the language of the layer
+ * with uselang=de the language of the menu
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Kolossos
+ */
+$messages['en'] = array(
+    'ts-kml-on-ol-options' => 'Options',
+    'ts-kml-on-ol-languages' => 'Languages',
+    'ts-kml-on-ol-thumbnails' => 'Thumbnails',
+    'ts-kml-on-ol-help' => 'Help',
+    'ts-kml-on-ol-all' => 'All',
+    'ts-kml-on-ol-map-by' => 'Map by',
+    'ts-kml-on-ol-openstreetmap' => 'OpenStreetMap',
+    'ts-kml-on-ol-wikipedia' => 'Wikipedia'
+);
+
+/** Deutsch
+ * @author Kolossos
+ */
+$messages['de'] = array(
+    'ts-kml-on-ol-options' => 'Optionen',
+    'ts-kml-on-ol-languages' => 'Sprachauswahl',
+    'ts-kml-on-ol-thumbnails' => 'Miniaturbilder',
+    'ts-kml-on-ol-help' => 'Hilfe',
+    'ts-kml-on-ol-all' => 'Alle',
+    'ts-kml-on-ol-map-by' => 'Karte von',
+    'ts-kml-on-ol-openstreetmap' => 'OpenStreetMap',
+    'ts-kml-on-ol-wikipedia' => 'Wikipedia'
+);
+
