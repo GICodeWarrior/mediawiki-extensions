e7a97bb8ec40a89e5e5ea390af91332d4be4220a
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75726 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiArticleFeeds.i18n.magic.php
+++ WikiArticleFeeds.i18n.magic.php
@@ -2,30 +2,37 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
 	'itemtags' => array( 0, 'itemtags' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'itemtags'   => array( '0', 'وسوم_المدخلة', 'itemtags' ),
+	'itemtags' => array( 0, 'وسوم_المدخلة', 'itemtags' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'itemtags'   => array( '0', 'وسوم_المدخلة', 'itemtags' ),
+	'itemtags' => array( 0, 'وسوم_المدخلة', 'itemtags' ),
 );
 
+/** Japanese (日本語) */
 $magicWords['ja'] = array(
-	'itemtags'   => array( '0', 'アイテムタグ', 'itemtags' ),
+	'itemtags' => array( 0, 'アイテムタグ' ),
 );
 
+/** Marathi (मराठी) */
 $magicWords['mr'] = array(
-	'itemtags'   => array( '0', 'आयटमखूणा', 'कलमखूणा', 'itemtags' ),
+	'itemtags' => array( 0, 'आयटमखूणा', 'कलमखूणा', 'itemtags' ),
 );
 
+/** Nedersaksisch (Nedersaksisch) */
 $magicWords['nds-nl'] = array(
-	'itemtags'   => array( '0', 'onderwarpetiket', 'itemlabels', 'itemtags' ),
+	'itemtags' => array( 0, 'onderwarpetiket', 'itemlabels', 'itemtags' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'itemtags'   => array( '0', 'itemlabels', 'itemtags' ),
+	'itemtags' => array( 0, 'itemlabels' ),
 );
