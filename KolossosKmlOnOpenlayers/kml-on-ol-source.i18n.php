8c089b5263acc1f1fac2c942d05cf288e374bd8b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-01 20:51:37 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-01 20:01:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75828 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- KolossosKmlOnOpenlayers/kml-on-ol-source.i18n.php
+++ KolossosKmlOnOpenlayers/kml-on-ol-source.i18n.php
@@ -27,17 +27,159 @@ $messages['en'] = array(
     'ts-kml-on-ol-wikipedia' => 'Wikipedia'
 );
 
-/** Deutsch
+/** Message documentation (Message documentation)
+ * @author EugeneZelenko
+ */
+$messages['qqq'] = array(
+	'ts-kml-on-ol-options' => '{{Identical|Options}}',
+	'ts-kml-on-ol-languages' => '{{Identical|Language}}',
+	'ts-kml-on-ol-thumbnails' => '{{Identical|Thumbnail}}',
+	'ts-kml-on-ol-help' => '{{Identical|Help}}',
+	'ts-kml-on-ol-all' => '{{Identical|All}}',
+);
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'ts-kml-on-ol-options' => 'Устаноўкі',
+	'ts-kml-on-ol-languages' => 'Мовы',
+	'ts-kml-on-ol-thumbnails' => 'Мініятуры',
+	'ts-kml-on-ol-help' => 'Дапамога',
+	'ts-kml-on-ol-all' => 'Усе',
+	'ts-kml-on-ol-map-by' => 'Мапа',
+);
+
+/** Czech (Česky)
+ * @author Mormegil
+ */
+$messages['cs'] = array(
+	'ts-kml-on-ol-options' => 'Možnosti',
+	'ts-kml-on-ol-languages' => 'Jazyky',
+	'ts-kml-on-ol-thumbnails' => 'Náhledy',
+	'ts-kml-on-ol-help' => 'Nápověda',
+	'ts-kml-on-ol-all' => 'Všechny',
+	'ts-kml-on-ol-map-by' => 'Mapa z',
+);
+
+/** German (Deutsch)
  * @author Kolossos
  */
 $messages['de'] = array(
-    'ts-kml-on-ol-options' => 'Optionen',
-    'ts-kml-on-ol-languages' => 'Sprachauswahl',
-    'ts-kml-on-ol-thumbnails' => 'Miniaturbilder',
-    'ts-kml-on-ol-help' => 'Hilfe',
-    'ts-kml-on-ol-all' => 'Alle',
-    'ts-kml-on-ol-map-by' => 'Karte von',
-    'ts-kml-on-ol-openstreetmap' => 'OpenStreetMap',
-    'ts-kml-on-ol-wikipedia' => 'Wikipedia'
+	'ts-kml-on-ol-options' => 'Optionen',
+	'ts-kml-on-ol-languages' => 'Sprachauswahl',
+	'ts-kml-on-ol-thumbnails' => 'Miniaturbilder',
+	'ts-kml-on-ol-help' => 'Hilfe',
+	'ts-kml-on-ol-all' => 'Alle',
+	'ts-kml-on-ol-map-by' => 'Karte von',
+	'ts-kml-on-ol-openstreetmap' => 'OpenStreetMap',
+	'ts-kml-on-ol-wikipedia' => 'Wikipedia',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'ts-kml-on-ol-options' => 'Optiones',
+	'ts-kml-on-ol-languages' => 'Linguas',
+	'ts-kml-on-ol-thumbnails' => 'Miniaturas',
+	'ts-kml-on-ol-help' => 'Adjuta',
+	'ts-kml-on-ol-all' => 'Totes',
+	'ts-kml-on-ol-map-by' => 'Carta per',
+);
+
+/** Japanese (日本語)
+ * @author 青子守歌
+ */
+$messages['ja'] = array(
+	'ts-kml-on-ol-options' => 'オプション',
+	'ts-kml-on-ol-languages' => '言語',
+	'ts-kml-on-ol-thumbnails' => 'サムネイル',
+	'ts-kml-on-ol-help' => 'ヘルプ',
+	'ts-kml-on-ol-all' => 'すべて',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'ts-kml-on-ol-options' => 'Optiounen',
+	'ts-kml-on-ol-languages' => 'Sproochen',
+	'ts-kml-on-ol-thumbnails' => 'Miniaturbiller',
+	'ts-kml-on-ol-help' => 'Hëllef',
+	'ts-kml-on-ol-all' => 'All',
+	'ts-kml-on-ol-map-by' => 'Kaart vum',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'ts-kml-on-ol-options' => 'Нагодувања',
+	'ts-kml-on-ol-languages' => 'Јазици',
+	'ts-kml-on-ol-thumbnails' => 'Минијатури',
+	'ts-kml-on-ol-help' => 'Помош',
+	'ts-kml-on-ol-all' => 'Сите',
+	'ts-kml-on-ol-map-by' => 'Карта:',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'ts-kml-on-ol-options' => 'Instellingen',
+	'ts-kml-on-ol-languages' => 'Talen',
+	'ts-kml-on-ol-thumbnails' => 'Miniaturen',
+	'ts-kml-on-ol-help' => 'Help',
+	'ts-kml-on-ol-all' => 'Alle',
+	'ts-kml-on-ol-map-by' => 'Kaart door',
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Nghtwlkr
+ */
+$messages['no'] = array(
+	'ts-kml-on-ol-options' => 'Alternativer',
+	'ts-kml-on-ol-languages' => 'Språk',
+	'ts-kml-on-ol-thumbnails' => 'Miniatyrbilder',
+	'ts-kml-on-ol-help' => 'Hjelp',
+	'ts-kml-on-ol-all' => 'Alle',
+	'ts-kml-on-ol-map-by' => 'Kart av',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'ts-kml-on-ol-options' => 'Opcje',
+	'ts-kml-on-ol-languages' => 'Języki',
+	'ts-kml-on-ol-thumbnails' => 'Miniatury',
+	'ts-kml-on-ol-help' => 'Pomoc',
+	'ts-kml-on-ol-all' => 'Wszystko',
+	'ts-kml-on-ol-map-by' => 'Mapa',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'ts-kml-on-ol-options' => 'Opções',
+	'ts-kml-on-ol-languages' => 'Línguas',
+	'ts-kml-on-ol-thumbnails' => 'Miniaturas',
+	'ts-kml-on-ol-help' => 'Ajuda',
+	'ts-kml-on-ol-all' => 'Todas',
+	'ts-kml-on-ol-map-by' => 'Mapa de',
+);
+
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages['te'] = array(
+	'ts-kml-on-ol-options' => 'ఎంపికలు',
+	'ts-kml-on-ol-languages' => 'భాషలు',
+	'ts-kml-on-ol-thumbnails' => 'నఖచిత్రాలు',
+	'ts-kml-on-ol-help' => 'సహాయం',
+	'ts-kml-on-ol-all' => 'అన్నీ',
+	'ts-kml-on-ol-map-by' => 'పటపు సౌజన్యం',
 );
 
