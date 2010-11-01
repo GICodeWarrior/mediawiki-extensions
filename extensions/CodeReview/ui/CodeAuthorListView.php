f42562a41a33e751696c5c4aea2c6ee2e12c9b25
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-01 16:54:03 +0000

    Need a way to show unlinked accounts
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75802 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/ui/CodeAuthorListView.php
+++ extensions/CodeReview/ui/CodeAuthorListView.php
@@ -8,9 +8,10 @@ class CodeAuthorListView extends CodeView {
 	}
 
 	function execute() {
-		global $wgOut, $wgLang;
+		global $wgOut, $wgLang, $wgRequest;
 		$authors = $this->mRepo->getAuthorList();
 		$repo = $this->mRepo->getName();
+		$hideLinked = $wgRequest->getBool( 'unlinkedonly' );
 		$text = wfMsg( 'code-authors-text' ) . "\n\n";
 		$text .= '<strong>' . wfMsg( 'code-author-total', $wgLang->formatNum( $this->mRepo->getAuthorCount() ) )  . "</strong>\n";
 
@@ -22,15 +23,18 @@ class CodeAuthorListView extends CodeView {
 
 		foreach ( $authors as $committer ) {
 			if ( $committer ) {
-				$wgOut->addHTML( "<tr><td>" );
 				$author = $committer["author"];
 				$text = "[[Special:Code/$repo/author/$author|$author]]";
 				$user = $this->mRepo->authorWikiUser( $author );
 				if ( $user ) {
+					if( $hideLinked ) {
+						continue;
+					}
 					$title = htmlspecialchars( $user->getUserPage()->getPrefixedText() );
 					$name = htmlspecialchars( $user->getName() );
 					$text .= " ([[$title|$name]])";
 				}
+				$wgOut->addHTML( "<tr><td>" );
 				$wgOut->addWikiText( $text );
 				$wgOut->addHTML( "</td><td>{$wgLang->timeanddate( $committer['lastcommit'], true )}</td></tr>" );
 			}
