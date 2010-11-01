629cc4439cc1df0c24e289cfc2da7bcaea1b2f7f
churchofemacs <churchofemacs@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-01 13:22:32 +0000

    removing option to configure message title via wgGroupsSidebar -- this is an unneeded feature
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75784 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/GroupsSidebar/GroupsSidebar.body.php
+++ extensions/GroupsSidebar/GroupsSidebar.body.php
@@ -5,14 +5,13 @@ class GroupsSidebar {
 	 * Gets called by Hook SkinBuildSidebar
 	 */
 	function efHideSidebar( $skin, &$bar ) {
-		global $wgGroupsSidebar, $wgUser;
-		if ( is_null($wgGroupsSidebar) )
-			return true;
-		foreach ( $wgGroupsSidebar as $group => $sectiontitle ) {
-			if (in_array($group, $wgUser->getEffectiveGroups())) {
-				$message = 'sidebar-'.$sectiontitle;
+		global $wgUser;
+		foreach ( $wgUser->getEffectiveGroups() as $group ) {
+			$message = 'sidebar-'.$group;
+			# addToSidebar currently won't throw errors if we call it
+			# with nonexisting pages, but better check and be sure
+			if ( Title::newFromText( $message, NS_MEDIAWIKI )->exists() )
 				$skin->addToSidebar( &$bar, $message );
-			}
 		}
 		return true;
 	}
--- extensions/GroupsSidebar/GroupsSidebar.php
+++ extensions/GroupsSidebar/GroupsSidebar.php
@@ -10,7 +10,7 @@ $wgExtensionCredits['parserhook'][] = array(
 	'author' => 'Church of emacs',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:GroupsSidebar',
 	'descriptionmsg' => 'groupssidebar-desc',
-	'version' => '0.1',
+	'version' => '0.1.1',
 );
 
 $dir = dirname(__FILE__) . '/';
@@ -18,4 +18,3 @@ $dir = dirname(__FILE__) . '/';
 $wgAutoloadClasses['GroupsSidebar'] = $dir . 'GroupsSidebar.body.php'; # Tell MediaWiki to load the extension body.
 $wgHooks['SkinBuildSidebar'][] = array(new GroupsSidebar(), 'efHideSidebar');
 $wgExtensionMessagesFiles['GroupsSidebar'] = $dir . 'GroupsSidebar.i18n.php';
-$wgGroupsSidebar = array();
