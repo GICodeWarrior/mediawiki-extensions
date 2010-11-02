708e5fa5bb3074ef6a735a0b831ef37e6cc8dff1
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-02 17:14:55 +0000

    New version: 0.6.2
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75860 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SemanticInternalObjects/SemanticInternalObjects.php
+++ extensions/SemanticInternalObjects/SemanticInternalObjects.php
@@ -9,7 +9,7 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'SIO_VERSION', '0.6.1' );
+define( 'SIO_VERSION', '0.6.2' );
 
 $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'parserhook'][] = array(
 	'path' => __FILE__,
