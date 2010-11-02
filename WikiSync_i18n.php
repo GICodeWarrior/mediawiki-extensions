d550372da976934d21a666eeebcd915a010555e3
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-02 19:22:38 +0000

    First round of i18n tweaks for consistency
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75867 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiSync_i18n.php
+++ WikiSync_i18n.php
@@ -11,7 +11,7 @@ $messages = array();
  */
 $messages['en'] = array(
 	'wikisync' => 'Wiki synchronization',
-	'wikisync-desc' => 'Provides a [[Special:WikiSync|special page]] to synchronize recent changes of two wikis - local one and remote one.',
+	'wikisync-desc' => 'Provides a [[Special:WikiSync|special page]] to synchronize recent changes of two wikis - local one and remote one',
 	'wikisync_direction' => 'Please choose the direction of synchronization',
 	'wikisync_local_root' => 'Local wiki site root',
 	'wikisync_remote_root' => 'Remote wiki site root',
@@ -19,7 +19,7 @@ $messages['en'] = array(
 	'wikisync_clear_log' => 'Clear log',
 	'wikisync_login_to_remote_wiki' => 'Login to remote wiki',
 	'wikisync_remote_wiki_root' => 'Remote wiki root',
-	'wikisync_remote_wiki_example' => 'path to api.php, for example: http://www.mediawiki.org/w',
+	'wikisync_remote_wiki_example' => 'Path to api.php, for example: http://www.mediawiki.org/w',
 	'wikisync_remote_wiki_user' => 'Remote wiki user name',
 	'wikisync_remote_wiki_pass' => 'Remote wiki password',
 	'wikisync_remote_login_button' => 'Log in',
@@ -29,25 +29,37 @@ $messages['en'] = array(
 	'wikisync_log_uploaded_by' => 'Uploaded by [[Special:WikiSync]]',
 	'wikisync_api_result_unknown_action' => 'Unknown API action',
 	'wikisync_api_result_exception' => 'Exception occured in local API call',
-	'wikisync_api_result_noaccess' => 'Only members of the following groups can perform this action: ($1)',
+	'wikisync_api_result_noaccess' => 'Only members of the following groups can perform this action: $1',
 	'wikisync_api_result_invalid_parameter' => 'Invalid value of parameter',
 	'wikisync_api_result_http' => 'HTTP error while querying data from remote API',
 	'wikisync_api_result_Unsupported' => 'Your version of MediaWiki is unsupported (less than 1.15)',
-	'wikisync_api_result_NoName' => 'You didn\'t set the lgname parameter',
+	'wikisync_api_result_NoName' => 'You did not set the lgname parameter',
 	'wikisync_api_result_Illegal' => 'You provided an illegal username',
-	'wikisync_api_result_NotExists' => 'The username you provided doesn\'t exist',
-	'wikisync_api_result_EmptyPass' => 'You didn\'t set the lgpassword parameter or you left it empty',
+	'wikisync_api_result_NotExists' => 'The username you provided does not exist',
+	'wikisync_api_result_EmptyPass' => 'You did not set the lgpassword parameter or you left it empty',
 	'wikisync_api_result_WrongPass' => 'The password you provided is incorrect',
 	'wikisync_api_result_WrongPluginPass' => 'Same as WrongPass, returned when an authentication plugin rather than MediaWiki itself rejected the password',
 	'wikisync_api_result_CreateBlocked' => 'The wiki tried to automatically create a new account for you, but your IP address has been blocked from account creation',
-	'wikisync_api_result_Throttled' => 'You\'ve logged in too many times in a short time.',
+	'wikisync_api_result_Throttled' => 'You have logged in too many times in a short time.',
 	'wikisync_api_result_Blocked' => 'User is blocked',
 	'wikisync_api_result_mustbeposted' => 'The login module requires a POST request',
 	'wikisync_api_result_NeedToken' => 'Either you did not provide the login token or the sessionid cookie. Request again with the token and cookie given in this response',
-	'wikisync_api_result_no_import_rights' => 'This user is not allowed to import xml dump files',
+	'wikisync_api_result_no_import_rights' => 'This user is not allowed to import XML dump files',
 	'wikisync_api_result_Success' => 'Successfully logged into remote wiki site',
-	'wikisync_js_last_op_error' => "Last operation returned an error\nCode: $1\nMsg: $2\nPress [OK] to retry last operation",
-	'wikisync_js_synchronization_confirmation' => "Are you sure you want to synchronize\nfrom $1\nto $2\nstarting from revision $3?",
+	'wikisync_js_last_op_error' => 'Last operation returned an error.
+
+Code: $1
+
+Message: $2
+
+Press [OK] to retry last operation',
+	'wikisync_js_synchronization_confirmation' => 'Are you sure you want to synchronize
+
+from $1
+	
+to $2
+	
+starting from revision $3?',
 	'wikisync_js_synchronization_success' => 'Synchronization was completed successfully',
 	'wikisync_js_already_synchronized' => 'Source and destination wikis seems to be already synchronized',
 	'wikisync_js_sync_to_itself' => 'You cannot synchronize the wiki to itself',
