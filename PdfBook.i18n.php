a993e616fc3944a9105d11457a5dee3baa9c67aa
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-03 20:28:35 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-03 20:05:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75955 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PdfBook.i18n.php
+++ PdfBook.i18n.php
@@ -19,3 +19,30 @@ $messages['en'] = array(
 	'pdfbook-desc' => 'Composes a book from pages in a category and exports as a PDF book',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'pdfbook-action' => 'Als PDF-Datei ausgeben',
+	'pdfbook-log' => '$1 wurde als Zusammenstellung in einer PDF-Datei erstellt',
+	'pdfbook-desc' => 'Ermöglicht die Erstellung von PDF-Dateien einzelner Seiten oder eine Zusammenstellung aller in einer Kategorie vorhandenen Seiten in einer PDF-Datei',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'pdfbook-action' => 'Imprimir como PDF',
+	'pdfbook-log' => '$1 exportado como libro en PDF',
+	'pdfbook-desc' => 'Composición dun libro a partir das páxinas presentes nunha categoría e exportación como un libro en PDF',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'pdfbook-action' => 'Imprimer como PDF',
+	'pdfbook-log' => '$1 exportate como libro in PDF',
+	'pdfbook-desc' => 'Compone un libro ex paginas in un categoria e exporta lo como libro in PDF',
+);
+
