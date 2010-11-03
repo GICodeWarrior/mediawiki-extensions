b56a2069cba0c326ba87b075139792d6d709c9c0
nad <nad@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-03 01:15:57 +0000

    add i18n
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75894 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/PdfBook/PdfBook.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Internationalisation file for the extension PdfBook
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Aran Dunkley
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array();
+
+/** English
+ * @author Aran Dunkley
+ */
+$messages['en'] = array(
+	'pdfbook-action' => "Print as PDF",
+	'pdfbook-log' => "$1 exported as a PDF book",
+	'pdfbook-desc' => "Composes a book from articles in a category and exports as a PDF book",
+);
