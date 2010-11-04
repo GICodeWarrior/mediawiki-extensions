5595bea59f5342e2197eda02095e8f6f286e2ee0
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-04 23:55:29 +0000

    Kill numerous wfLoadExtensionMessages calls
    344 usages left...
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76064 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SpecialEmergencyDeSysop.php
+++ SpecialEmergencyDeSysop.php
@@ -6,8 +6,6 @@ if ( ! defined( 'MEDIAWIKI' ) )
 class SpecialEmergencyDeSysop extends SpecialPage {
 
 	function __construct() {
-		wfLoadExtensionMessages( 'EmergencyDeSysop' );
-		parent::__construct( 'EmergencyDeSysop' );
 		parent::__construct( "EmergencyDeSysop", "emergencydesysop" );
 	}
 	
