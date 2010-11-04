32cc6503892a26ce485e935a8a1753a9e218b8ee
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-04 23:55:29 +0000

    Kill numerous wfLoadExtensionMessages calls
    344 usages left...
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76064 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- FlagPage.hooks.php
+++ FlagPage.hooks.php
@@ -1,15 +1,12 @@
 <?php
 
 class FlagPageTabInstaller {
-	function __construct( ) { }
-
 	function insertTab( $skin, &$content_actions ) {
 		global $wgTitle;
 		if ( !$wgTitle->exists() ) {
 			return false;
 		}
 		$linkParam = "page=" . $wgTitle->getPrefixedURL();
-		wfLoadExtensionMessages( 'FlagPage' );
 		$special = SpecialPage::getTitleFor( 'FlagPage' );
 		$content_actions['flagpage'] = array(
 			'class' => false,
