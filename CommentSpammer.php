ec92e32bec7289c277e43a5222f02b976434e738
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-04 23:55:29 +0000

    Kill numerous wfLoadExtensionMessages calls
    344 usages left...
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76064 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- CommentSpammer.php
+++ CommentSpammer.php
@@ -14,7 +14,6 @@ if ( ! defined( 'MEDIAWIKI' ) ) {
 	die( 'This is only valid as a MediaWiki extension' );
 }
 
-$wgExtensionFunctions[] = 'efCommentSpammer';
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'CommentSpammer',
@@ -25,10 +24,6 @@ $wgExtensionCredits['other'][] = array(
 
 $dir = dirname(__FILE__) . '/';
 $wgExtensionMessagesFiles['CommentSpammer'] = $dir . 'CommentSpammer.i18n.php';
-
-function efCommentSpammer() {
-	wfLoadExtensionMessages( 'CommentSpammer' );
-}
 
 /**
  * Add the hook on which we trigger.
