8ee06ccf9d2310c60d84bc8d0a21c2362dfa6e72
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-04 21:19:12 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-04 21:05:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76039 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PdfBook.i18n.php
+++ PdfBook.i18n.php
@@ -37,6 +37,15 @@ $messages['gl'] = array(
 	'pdfbook-desc' => 'Composición dun libro a partir das páxinas presentes nunha categoría e exportación como un libro en PDF',
 );
 
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'pdfbook-action' => 'As PDF-Datei uusgee',
+	'pdfbook-log' => '$1 exportiert as PDF-Buech',
+	'pdfbook-desc' => 'Stellt e Buech vu Syte us ere Kategori zämme un exportiert s as PDF-Buech',
+);
+
 /** Interlingua (Interlingua)
  * @author McDutchie
  */
@@ -44,5 +53,59 @@ $messages['ia'] = array(
 	'pdfbook-action' => 'Imprimer como PDF',
 	'pdfbook-log' => '$1 exportate como libro in PDF',
 	'pdfbook-desc' => 'Compone un libro ex paginas in un categoria e exporta lo como libro in PDF',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'pdfbook-action' => 'Als PDF drécken',
+	'pdfbook-log' => '$1 gouf als PDF-Buch exportéiert',
+	'pdfbook-desc' => 'Setzt e Buch aus Säiten an eng Kategorie an exportéiert se als PDF-Buch',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'pdfbook-action' => 'Испечати како PDF',
+	'pdfbook-log' => 'Извоз на $1 како PDF-книга',
+	'pdfbook-desc' => 'Составува книга од страници во извесна категорија и ја извезува во PDF-формат',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'pdfbook-action' => 'Afdrukken als PDF',
+	'pdfbook-log' => '$1 is geëxporteerd als PDF-boek',
+	'pdfbook-desc' => "Maakt een boek van pagina's in een categorie en maakt een export als PDF-boek",
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Nghtwlkr
+ */
+$messages['no'] = array(
+	'pdfbook-action' => 'Skriv ut som PDF',
+	'pdfbook-log' => '$1 eksportert som en PDF-bok',
+	'pdfbook-desc' => 'Komponerer en bok fra sider i en kategori og eksporterer dem som en PDF-bok',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'pdfbook-action' => 'Drukuj do PDF',
+	'pdfbook-log' => 'wyeksportowano $1 jako książkę w formacie PDF',
+	'pdfbook-desc' => 'Tworzenie książki ze stron kategorii i eksportowanie w formacie PDF',
+);
+
+/** Brazilian Portuguese (Português do Brasil)
+ * @author Garrasdalua
+ */
+$messages['pt-br'] = array(
+	'pdfbook-action' => 'Imprima como PDF',
+	'pdfbook-log' => '$1 exportado como um livro PDF',
+	'pdfbook-desc' => 'Componha um livro com paginas de uma categoria e o exporte como um livro PDF',
 );
 
