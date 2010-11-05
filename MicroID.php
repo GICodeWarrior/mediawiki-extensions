021698d8225ed93354a9ae5773a62597fcc6c41d
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-05 00:11:57 +0000

    Kill numerous wfLoadExtensionMessages calls more
    200 usages left...
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76065 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MicroID.php
+++ MicroID.php
@@ -44,7 +44,6 @@ $wgExtensionMessagesFiles['MicroID'] = dirname(__FILE__) . '/MicroID.i18n.php';
 function setupMicroID() {
 
 	global $wgOut, $wgRequest, $wgHooks;
-	wfLoadExtensionMessages( 'MicroID' );
 
 	$wgHooks['UserToggles'][] = 'MicroIDUserToggle';
 
