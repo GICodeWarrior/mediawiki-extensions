333ae21532296a7d52f0abca8c97e361db127e40
wikibhasha.msr <wikibhasha.msr@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-08 11:03:23 +0000

    Adding GNU General Public License info. to the php extension scripts
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76297 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiBhasha.i18n.php
+++ WikiBhasha.i18n.php
@@ -1,6 +1,26 @@
 <?php
 /*
 *
+*   WikiBhasha
+*   Copyright (C) 2010, Microsoft
+*   
+*   This program is free software; you can redistribute it and/or
+*   modify it under the terms of the GNU General Public License version 2
+*   as published by the Free Software Foundation.
+*   
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*   
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*
+*/
+
+/*
+*
 *	If you want your extension to be used on wikis that have a multi-lingual readership, we will need to add internationalization support to the extension. 
 *
 *	1.For any text string displayed to the user, define a message. MediaWiki supports parameterized messages and that feature should be used when a message is dependent on information generated at runtime. Assign each message a lowercase message id.
