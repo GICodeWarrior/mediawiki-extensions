899db1445d16ee543e388ddcd02db72e57889c4d
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-11 01:35:06 +0000

    ProxyConnect: added missing semicolon, made Special:ProxyConnect an unlisted special page, added version number to extension credits, tweaked coding style
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76490 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ProxyConnect/ProxyConnect.php
+++ extensions/ProxyConnect/ProxyConnect.php
@@ -1,27 +1,28 @@
 <?php
-if ( ! defined( 'MEDIAWIKI' ) )
-	die();
-    
-/**#@+
+/**
  * Implements the server side functionality of Proxy Connect
- * 
- * @package MediaWiki
- * @subpackage Extensions
  *
- * @link http://www.mediawiki.org/wiki/Extension:ProxyConnect Documentation
- *
- *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
  * @author Travis Derouin <travis@wikihow.com>
+ * @link http://www.mediawiki.org/wiki/Extension:ProxyConnect Documentation
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
+
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['special'][] = array(
 	'name' => 'ProxyConnect',
+	'version' => '1.0',
 	'author' => 'Travis Derouin',
 	'description' => 'Implements the server side functionality of Proxy Connect',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:ProxyConnect',
 );
 
-
+// Set up the new special page
 $wgSpecialPages['ProxyConnect'] = 'ProxyConnect';
 $wgAutoloadClasses['ProxyConnect'] = dirname( __FILE__ ) . '/ProxyConnect.body.php';
