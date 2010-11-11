44896a3fd83db076e2e646e0f1ae556ddc2744d7
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-11 01:35:06 +0000

    ProxyConnect: added missing semicolon, made Special:ProxyConnect an unlisted special page, added version number to extension credits, tweaked coding style
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76490 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ProxyConnect.body.php
+++ ProxyConnect.body.php
@@ -1,21 +1,31 @@
-<?
-class ProxyConnect extends SpecialPage {
+<?php
 
-    function __construct() {
-		parent::__construct( 'ProxyConnect' )
-    }
+class ProxyConnect extends UnlistedSpecialPage {
 
-    function execute ($par) {
+	/**
+	 * Constructor -- set up the new special page
+	 */
+	public function __construct() {
+		parent::__construct( 'ProxyConnect' );
+	}
+
+	/**
+	 * Show the new special page
+	 *
+	 * @param $par Mixed: parameter passed to the special page or null
+	 */
+	public function execute( $par ) {
 		global $wgUser, $wgOut;
 		$wgOut->disable();
-		header("Content-type: text/plain;");
-		if ($wgUser->getID() == 0)
+		header( 'Content-type: text/plain;' );
+		if ( $wgUser->getID() == 0 ) {
 			return;
-		$result = "";
+		}
+		$result = '';
 		$result .= "UniqueID={$wgUser->getID()}\n";
 		$result .= "Name={$wgUser->getName()}\n";
 		$result .= "Email={$wgUser->getEmail()}\n";
-		wfDebug("ProxyConnect: returning $result\n");
+		wfDebug( "ProxyConnect: returning $result\n" );
 		echo $result;
 		return;
 	}
