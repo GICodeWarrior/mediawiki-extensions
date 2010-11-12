174e227ecdc567d7e11e2648d99da21979900e45
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-12 17:15:01 +0000

    * Made the special page extend SpecialPage and use $wgSpecialPages instead of using deprecated SpecialPage::addPage() * Added aliases file for special page * Removed usage of deprecated method OutputPage::fatalError() * WikiError -> Status * Hardcoded method name -> __METHOD__ * Corrected TodoTemplate to output all HTML in the correct order * $wgTitle -> $skin->getTitle() in SkinTemplateTabs hook
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76582 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Todo.alias.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aliases for special pages of Todo extension.
+ *
+ * @file
+ */
+
+$specialPageAliases = array();
+
+/**
+ * English
+ */
+$specialPageAliases['en'] = array(
+	'Todo' => array( 'Todo' ),
+);
