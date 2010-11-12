ba35ad1ee3135dd97764d61791c62d954673f3b3
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-12 21:24:36 +0000

    jQueryified things, made the most recent bar the current one on load.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76598 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ContributionReporting/FundraiserStatistics.js
+++ extensions/ContributionReporting/FundraiserStatistics.js
@@ -1,22 +1,36 @@
 /* JavaScript */
 
-var currentViewID = 'fundraiserstats-view-box-0';
-function replaceView( newLayerID ) {
-	var currentLayer = document.getElementById( currentViewID );
-	var newLayer = document.getElementById( newLayerID );
-	currentLayer.style.display = 'none';
-	newLayer.style.display = 'block';
-	currentViewID = newLayerID;
-}
-var currentChartID = 'fundraiserstats-chart-totals';
-function replaceChart( newLayerID ) {
-	var currentLayer = document.getElementById( currentChartID );
-	var currentTab = document.getElementById( currentChartID + '-tab' );
-	var newLayer = document.getElementById( newLayerID );
-	var newTab = document.getElementById( newLayerID + '-tab' );
-	currentLayer.style.display = 'none';
-	currentTab.setAttribute( 'class', 'fundraiserstats-chart-tab-normal' );
-	newLayer.style.display = 'block';
-	newTab.setAttribute( 'class', 'fundraiserstats-chart-tab-current' );
-	currentChartID = newLayerID;
-}
+$j( document ).ready( function() {
+	
+	var currentViewID = 'fundraiserstats-view-box-0';
+	function replaceView( newLayerID ) {
+		var currentLayer = document.getElementById( currentViewID );
+		var newLayer = document.getElementById( newLayerID );
+		currentLayer.style.display = 'none';
+		newLayer.style.display = 'block';
+		currentViewID = newLayerID;
+	}
+	
+	var currentChartID = 'fundraiserstats-chart-totals';
+	function replaceChart( newLayerID ) {
+		var currentLayer = document.getElementById( currentChartID );
+		var currentTab = document.getElementById( currentChartID + '-tab' );
+		var newLayer = document.getElementById( newLayerID );
+		var newTab = document.getElementById( newLayerID + '-tab' );
+		currentLayer.style.display = 'none';
+		currentTab.setAttribute( 'class', 'fundraiserstats-chart-tab-normal' );
+		newLayer.style.display = 'block';
+		newTab.setAttribute( 'class', 'fundraiserstats-chart-tab-current' );
+		currentChartID = newLayerID;
+	}
+
+	$j( '.fundraiserstats-bar' ).hover( function() {
+		replaceView( $j(this).attr( 'rel' ) )
+	} );
+	$j( '.fundraiserstats-chart-tab' ).click( function() {
+		replaceChart( $j(this).attr( 'rel' ) )
+	} );
+	$j( '.fundraiserstats-current' ).each( function() {
+		replaceView( $j(this).attr( 'rel' ) )
+	} );
+} );
