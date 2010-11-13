26a8114d61017f8560ae8660c1495c75bb5b7ce7
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-13 20:42:57 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-13 20:25:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76645 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ProxyConnect.i18n.php
+++ ProxyConnect.i18n.php
@@ -12,3 +12,17 @@ $messages['en'] = array(
 	'proxyconnect-desc' => 'Implements the server side functionality of Proxy Connect',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'proxyconnect-desc' => 'Ermöglicht die serverseitige Funktion Proxy Connect',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'proxyconnect-desc' => 'Ја спроведува функцијата за опслужувачката страна на Поврзување преку застапник',
+);
+
