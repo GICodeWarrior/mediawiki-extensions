771ed3c650eb2e08d71d627064381cb1f6fb967d
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-18 08:26:13 +0000

    Move span out of the message, Some tweaks to messages and alias file for consistency Add extension to Translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@76940 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/FlagPage/FlagPage.i18n.php
+++ extensions/FlagPage/FlagPage.i18n.php
@@ -1,26 +1,27 @@
 <?php
 $messages = array();
  
-/* *** English *** */
+/** English
+ * @author Church of emacs
+ */
 $messages['en'] = array( 
 	'flagpage' => 'Flag a page',
-	'flagpage-desc' => "Flag page with predefined templates",
-	'flagpage-templatelist' => "<!--
+	'flagpage-desc' => 'Flag page with predefined templates',
+	'flagpage-templatelist' => '<!--
 Edit this page to configure templates to use.
 Examples:
 * [[Template:Unsourced|The article does not cite any references]]
 * [[Template:NPOV|The article is written in a biased way]]
 * [[Template:Delete|The article should be deleted]]
-
--->",
+-->',
 	'flagpage-nopageselectedtitle' => 'No page selected',
 	'flagpage-nopageselected' => 'You did not specify a page',
 	'flagpage-emptylisttitle' => 'No templates configured',
 	'flagpage-emptylist' => 'You need to configure your lists of templates. Edit [[{{ns:8}}:flagpage-templatelist]] to do so now.',
 	'flagpage-preview' => 'Preview of the selected template:',
-	'flagpage-confirmsave' => 'Please confirm your changes. ',
+	'flagpage-confirmsave' => 'Please confirm your changes.',
 	'flagpage-submitbutton' => 'Save page with this template',
-	'flagpage-nonexistent'=> '<span class="plainlinks">The page “$1” does not exist. Perhaps it has been [{{fullurl:Special:Log|page=$1}} moved or deleted].</span>',
+	'flagpage-nonexistent'=> 'The page “$1” does not exist. Perhaps it has been [{{fullurl:Special:Log|page=$1}} moved or deleted].',
 	'flagpage-summary' => 'Added template [[$1]] using FlagPage',
 	'flagpage-success' => '[[$1]] has been added to the page [[$2]].',
 	'flagpage-tab' => 'Flag'
