f67a51936affb06f6789bbdd6428f94e1b41f558
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-24 21:26:50 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-24 21:05:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77248 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OpenGraphMeta.i18n.php
+++ OpenGraphMeta.i18n.php
@@ -22,10 +22,66 @@ $messages['de'] = array(
 	'opengraphmeta-desc' => 'Fügt „OpenGraph“-Meta-Elemente hinzu, die von FaceBooks Schaltfläche „Gefällt mir“ verwendet werden',
 );
 
+/** French (Français)
+ * @author Peter17
+ */
+$messages['fr'] = array(
+	'opengraphmeta-desc' => 'Ajoute des méta-étiquettes OpenGraph utilisées par le bouton « j’aime » de Facebook',
+);
+
 /** Upper Sorbian (Hornjoserbsce)
  * @author Michawiki
  */
 $messages['hsb'] = array(
 	'opengraphmeta-desc' => 'Přidawa metafafličku OpenGraph, kotrež so přez tłóčatko "Lubi so mi" we FaceBooku wužiwaja',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'opengraphmeta-desc' => 'Adde meta-etiquettas OpenGraph usate per le button "Like" (Me place) de Facebook',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'opengraphmeta-desc' => 'Menambahkan tag meta OpenGraph untuk digunakan oleh tombol "Suka" FaceBook',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'opengraphmeta-desc' => 'Додава метаознаки за OpenGraph што ги користи копчето „Ми се допаѓа“ на Facebook',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'opengraphmeta-desc' => 'Dodaje meta znacznik OpenGraph wykorzystywany przez FaceBookowy przycisk „Lubię to”',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'opengraphmeta-desc' => 'A gionta ij meta tag OpenGraph dovrà dal boton Like ëd Facebook',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'opengraphmeta-desc' => 'Adiciona as meta tags OpenGraph usadas pelo botão Like do Facebook',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'opengraphmeta-desc' => 'Добавляет мета-теги OpenGraph, используемые кнопкой Like FaceBook',
 );
 
