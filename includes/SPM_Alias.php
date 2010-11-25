4bbef8290d3fc3cbb0f1179e2871e77d3307ac6b
fde <fde@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-25 15:18:15 +0000

    Added new extension Semantic Project Management. Semantic Project Management is an extension to MediaWiki that offers two new result formats for Semantic MediaWiki's inline queries.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77292 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ includes/SPM_Alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Alias file for the Semantic Project Management extension
+ *
+ * @addtogroup Extensions
+*/
+$aliases = array();
+ 
+/** English */
+$aliases['en'] = array(
+    'SemanticProjectManagement' => array( 'SemanticProjectManagement' ),
+);
---
+++ includes/SPM_Messages.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Internationalization file for the Semantic Project Management extension
+ *
+ * @addtogroup Extensions
+*/
+
+// FIXME: Can be enabled when new style magic words are used (introduced in r52503)
+// require_once( dirname( __FILE__ ) . '/SRF_Magic.php' );
+
+$messages = array();
+
+/** English
+ */
+$messages['en'] = array(
+	
+	'spm_wbs_link'     => 'MS Project XML export',
+	'spm_printername_wbs' => 'MS Project export',
+	'spm-name' => 'Semantic Project Management',
+	'spm-desc' => 'Project Management Formats for Semantic MediaWiki inline queries',
+    'semanticprojectmanagement-desc' => 'Project Management Formats for Semantic MediaWiki inline queries',
+    'semanticprojectmanagement' => 'Semantic Project Management'
+);
