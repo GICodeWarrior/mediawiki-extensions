255bf29ebab25f435d2e344669a5d94059746413
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-25 17:19:37 +0000

    Add i18n file Add extension to translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77295 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SNMPquery/SNMPquery.i18n.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Internationalisation for SNMPquery extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Rudy Rucker, Jr.'
+ */
+$messages['en'] = array(
+	'snmpquery-desc' => 'Add SNMPget and SNMPwalk functions and return the data into the wiki',
+);
+
+
