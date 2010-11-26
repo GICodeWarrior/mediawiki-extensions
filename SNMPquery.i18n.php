91d524ca52e4c62e9ec4b75ef5a1d49f80b1e330
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-26 21:52:21 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-26 21:27:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77345 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SNMPquery.i18n.php
+++ SNMPquery.i18n.php
@@ -15,4 +15,80 @@ $messages['en'] = array(
 	'snmpquery-desc' => 'Add SNMPget and SNMPwalk functions and return the data into the wiki',
 );
 
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'snmpquery-desc' => 'Дадае функцыі SNMPget і SNMPwalk і вяртае зьместкі ў вікі',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'snmpquery-desc' => 'Erweitert den Parser um die Funktionen SNMPget und SNMPwalk',
+);
+
+/** French (Français)
+ * @author Peter17
+ */
+$messages['fr'] = array(
+	'snmpquery-desc' => 'Ajoute les fonctions SNMPget et SNMPwalk et renvoie les données dans le wiki',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'snmpquery-desc' => 'Fiegt d Funktione SNMPget un SNMPwalk zue un schickt d Date retur in s Wiki',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'snmpquery-desc' => 'Funkcije SNMPget a SNMPwalk přidać a daty do wikija wróćić',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'snmpquery-desc' => 'Adde le functiones SNMPget and SNMPwalk e retorna le datos in le wiki',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'snmpquery-desc' => 'Tambahkan fungsi SNMPget dan SNMPwalk dan kembalikan data ke wiki',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'snmpquery-desc' => 'Додај функции SNMPget и SNMPwalk и врати ги податоците во викито',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'snmpquery-desc' => 'Gionta le funsion SNMPget e SNMPwalk e artorna ij dat ant la wiki',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'snmpquery-desc' => 'Acrescentar as funções SNMPget e SNMPwalk e devolver os dados à wiki',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'snmpquery-desc' => 'Добавляет функции SNMPget и SNMPwalk и возвращает данные в вики',
+);
 
