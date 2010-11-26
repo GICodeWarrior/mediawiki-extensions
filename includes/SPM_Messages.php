9249a25615bb502374eaec38c5ad8a3a7a34c018
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-26 21:52:21 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-26 21:27:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77345 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/SPM_Messages.php
+++ includes/SPM_Messages.php
@@ -19,3 +19,58 @@ $messages['en'] = array(
 	'right-SPMsetup' => 'Edit basic semantic project management settings'
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'spm_wbs_link' => '„MS Project“-XML-Export',
+	'spm_printername_wbs' => '„MS Project“-Export',
+	'spm-name' => 'Semantic Project Management',
+	'semanticprojectmanagement-desc' => 'Stellt Semantic MediaWiki zusätzliche, beim Projektmanagement nützliche, Ausgabeformate für eingebettete Abfragen zur Verfügung',
+	'semanticprojectmanagement' => 'Semantisches Projektmanagement',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'spm_wbs_link' => '„MS Project“-XML-Export',
+	'spm_printername_wbs' => '„MS Project“-Export',
+	'spm-name' => 'Semantic Project Management',
+	'semanticprojectmanagement-desc' => 'Projäktmanagement-Format fir Semantic MediaWiki inline-Abfroge',
+	'semanticprojectmanagement' => 'Semantisch Projäktmanagement',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'spm_wbs_link' => 'Exportation in XML de MS Project',
+	'spm_printername_wbs' => 'Exportation in MS Project',
+	'spm-name' => 'Gestion semantic de projectos',
+	'semanticprojectmanagement-desc' => 'Formatos de gestion de projectos pro incorporar consultas de Semantic MediaWiki',
+	'semanticprojectmanagement' => 'Gestion semantic de projectos',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'spm_wbs_link' => 'Ekpor XML MS Project',
+	'spm_printername_wbs' => 'Ekspor MS Project',
+	'spm-name' => 'Manajemen Proyek Semantik',
+	'semanticprojectmanagement-desc' => 'Format manajemen proyek untuk pertanyaan sisip MediaWiki Semantik',
+	'semanticprojectmanagement' => 'Manajemen Proyek Semantik',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'spm_wbs_link' => 'Exportação em XML do MS Project',
+	'spm_printername_wbs' => 'Exportação do MS Project',
+	'spm-name' => 'Gestão de Projectos Semântica',
+	'semanticprojectmanagement-desc' => 'Formatos de Gestão de Projectos, para consultas em linha do MediaWiki Semântico',
+	'semanticprojectmanagement' => 'Gestão de Projectos Semântica',
+);
+
