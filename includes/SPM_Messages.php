205592df5691c226355b153a1e730d7dc8577e06
fde <fde@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-11-26 16:43:48 +0000

    Added message for user group rights page Corrected wrong version Fixed  that extension is displayed in the semantic extension group
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77333 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/SPM_Messages.php
+++ includes/SPM_Messages.php
@@ -5,8 +5,6 @@
  * @addtogroup Extensions
 */
 
-// FIXME: Can be enabled when new style magic words are used (introduced in r52503)
-// require_once( dirname( __FILE__ ) . '/SRF_Magic.php' );
 
 $messages = array();
 
@@ -17,7 +15,7 @@ $messages['en'] = array(
 	'spm_printername_wbs' => 'MS Project export',
 	'spm-name' => 'Semantic Project Management',
 	'semanticprojectmanagement-desc' => 'Project Management Formats for Semantic MediaWiki inline queries',
-	'semanticprojectmanagement' => 'Semantic Project Management'
-	# 'right-SPMsetup' => '', FIXME: Add a description of this user right. It is shown at [[Special:ListGroupRights]]
+	'semanticprojectmanagement' => 'Semantic Project Management',
+	'right-SPMsetup' => 'Edit basic semantic project management settings'
 );
 
