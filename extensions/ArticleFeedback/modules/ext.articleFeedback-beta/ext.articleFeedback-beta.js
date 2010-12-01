33ebff34c9758223470f5c502fe66726723f57be
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-01 23:08:26 +0000

    Moved the tabs back over to the left side
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77548 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ArticleFeedback/modules/ext.articleFeedback-beta/ext.articleFeedback-beta.js
+++ extensions/ArticleFeedback/modules/ext.articleFeedback-beta/ext.articleFeedback-beta.js
@@ -2,74 +2,6 @@
  * Script for Article Feedback (beta)
  */
 
-/* 
- * This is the desired output...
- * 
-<div class="articleFeedback">
-	<div class="articleFeedback-panel" rel="form">
-		<div class="articleFeedback-buffer">
-			<div class="articleFeedback-title"></div>
-			<div class="articleFeedback-instructions"></div>
-			<div style="clear:both;"></div>
-			<table>
-				<tr>
-					<td><div class="articleFeedback-label" rel="wellsourced"></div></td>
-					<td><div class="articleFeedback-control" rel="wellsourced"></div></td>
-					<td><div class="articleFeedback-label" rel="complete"></div></td>
-					<td><div class="articleFeedback-control" rel="complete"></div></td>
-				</tr>
-				<tr>
-					<td><div class="articleFeedback-label" rel="neutral"></div></td>
-					<td><div class="articleFeedback-control" rel="neutral"></div></td>
-					<td><div class="articleFeedback-label" rel="readable"></div></td>
-					<td><div class="articleFeedback-control" rel="readable"></div></td>
-				</tr>
-			</table>
-			<hr noshade size="1" />
-			<div class="articleFeedback-notice"></div>
-			<div class="articleFeedback-submit"><button type="button"></button></div>
-			<div style="clear:both;"></div>
-		</div>
-	</div>
-	<div class="articleFeedback-tabs">
-		<div class="articleFeedback-tab" rel="form">
-			<div class="articleFeedback-buffer"></div>
-		</div>
-		<div class="articleFeedback-tab" rel="chart">
-			<div class="articleFeedback-buffer"></div>
-		</div>
-	</div>
-	<div class="articleFeedback-panel" rel="report">
-		<div class="articleFeedback-buffer">
-			<div class="articleFeedback-title"></div>
-			<div style="clear:both;"></div>
-			<table>
-				<tr>
-					<td><div class="articleFeedback-label" rel="wellsourced"></div></td>
-					<td><div class="articleFeedback-chart" rel="wellsourced"></div></td>
-					<td><div class="articleFeedback-count" rel="wellsourced"></div></td>
-				</tr>
-				<tr>
-					<td><div class="articleFeedback-label" rel="complete"></div></td>
-					<td><div class="articleFeedback-chart" rel="complete"></div></td>
-					<td><div class="articleFeedback-count" rel="complete"></div></td>
-				</tr>
-				<tr>
-					<td><div class="articleFeedback-label" rel="neutral"></div></td>
-					<td><div class="articleFeedback-chart" rel="neutral"></div></td>
-					<td><div class="articleFeedback-count" rel="neutral"></div></td>
-				</tr>
-				<tr>
-					<td><div class="articleFeedback-label" rel="readable"></div></td>
-					<td><div class="articleFeedback-chart" rel="readable"></div></td>
-					<td><div class="articleFeedback-count" rel="readable"></div></td>
-				</tr>
-			</table>
-		</div>
-	</div>
-</div>
-*/
-
 ( function( $, mw ) {
 
 $.articleFeedback = {
@@ -82,12 +14,6 @@ $.articleFeedback = {
 			context.$ui
 				.addClass( 'articleFeedback articleFeedback-form' )
 				.append( '\
-					<div class="articleFeedback-panel" rel="form">\
-						<div class="articleFeedback-buffer"></div>\
-					</div>\
-					<div class="articleFeedback-panel" rel="report">\
-						<div class="articleFeedback-buffer"></div>\
-					</div>\
 					<div class="articleFeedback-tabs">\
 						<div class="articleFeedback-tab articleFeedback-tab-current" rel="form">\
 							<div class="articleFeedback-buffer"></div>\
@@ -95,6 +21,12 @@ $.articleFeedback = {
 						<div class="articleFeedback-tab" rel="report">\
 							<div class="articleFeedback-buffer"></div>\
 						</div>\
+					</div>\
+					<div class="articleFeedback-panel" rel="form">\
+						<div class="articleFeedback-buffer"></div>\
+					</div>\
+					<div class="articleFeedback-panel" rel="report">\
+						<div class="articleFeedback-buffer"></div>\
 					</div>\
 				' )
 				// Handles
