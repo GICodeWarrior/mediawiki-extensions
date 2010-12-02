8281a0cf39e13df6afd46cfa9e732881cb718744
diederik <diederik@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-02 17:13:42 +0000

    * Added wrapper functionality to drop MongoDB collections.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77590 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/editor_trends/database/db.py
+++ tools/editor_trends/database/db.py
@@ -32,6 +32,23 @@ def init_mongo_db(db):
     return db
 
 
+def drop_collection(dbname, collection):
+    db = init_mongo_db(dbname)
+    db.drop_collection(collection)
+
+
+def get_collections(dbname):
+    db = init_mongo_db(dbname)
+    return db.collection_names()
+
+
+def cleanup_database(dbname):
+    coll = get_collections(dbname)
+    for c in coll:
+        if not c.startswith('system'):
+            drop_collection(dbname, c)
+
+
 def remove_documents_from_mongo_db(collection, ids):
     collection.remove(ids)
 
@@ -49,38 +66,38 @@ def add_index_to_collection(db, collection, key):
     mongo.collection.ensure_index(key)
 
 
-def init_database(db=None):
-    '''
-    This function initializes the connection with a sqlite db.
-    If the database already exists then it returns False to indicate
-    that the db already exists, else it returns True to indicate
-    that it's an empty database without tables.
-    '''
-    if db == None:
-        db = settings.DATABASE_NAME
-
-    return sqlite.connect(db, check_same_thread=False)
-
-
-def create_tables(cursor, tables):
-    '''
-    Tables is expected to be a dictionary, with key
-    table name and value another dictionary. This second
-    dictionary contains variable names and datatypes.
-    '''
-    for table in tables:
-        vars = '('
-        for var, datatype in tables[table]:
-            vars = vars + '%s %s,' % (var, datatype)
-        vars = vars[:-1]
-        vars = vars + ')'
-        cursor.execute('CREATE TABLE IF NOT EXISTS ? ?' % (table, vars))
-
-
-def debug():
-    connection = init_database()
-    cursor = connection.cursor()
-    create_tables(cursor, settings.TABLES)
+#def init_database(db=None):
+#    '''
+#    This function initializes the connection with a sqlite db.
+#    If the database already exists then it returns False to indicate
+#    that the db already exists, else it returns True to indicate
+#    that it's an empty database without tables.
+#    '''
+#    if db == None:
+#        db = settings.DATABASE_NAME
+#
+#    return sqlite.connect(db, check_same_thread=False)
+#
+#
+#def create_tables(cursor, tables):
+#    '''
+#    Tables is expected to be a dictionary, with key
+#    table name and value another dictionary. This second
+#    dictionary contains variable names and datatypes.
+#    '''
+#    for table in tables:
+#        vars = '('
+#        for var, datatype in tables[table]:
+#            vars = vars + '%s %s,' % (var, datatype)
+#        vars = vars[:-1]
+#        vars = vars + ')'
+#        cursor.execute('CREATE TABLE IF NOT EXISTS ? ?' % (table, vars))
+#
+#
+#def debug():
+#    connection = init_database()
+#    cursor = connection.cursor()
+#    create_tables(cursor, settings.TABLES)
 
 
 if __name__ == '__main__':
