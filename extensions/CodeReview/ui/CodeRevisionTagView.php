42428ff100d48fa252b2f475129afe5e8fcc1496
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-04 18:43:43 +0000

    Useful comment to go with r77733
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77734 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/ui/CodeRevisionTagView.php
+++ extensions/CodeReview/ui/CodeRevisionTagView.php
@@ -19,6 +19,8 @@ class SvnRevTagTablePager extends SvnRevTablePager {
 
 	function getQueryInfo() {
 		$info = parent::getQueryInfo();
+		//Don't change table order, see http://www.mediawiki.org/wiki/Special:Code/MediaWiki/77733
+		//Bug in mysql 4 allowed incorrect table ordering joins to work
 		$info['tables'] = array_merge(  array( 'code_tags' ), $info['tables'] );
 		$info['conds'][] = 'cr_repo_id=ct_repo_id';
 		$info['conds'][] = 'cr_id=ct_rev_id';
