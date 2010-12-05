1b0442fd2ba5df94fb4fd62b2ec192b70404688f
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-05 19:26:53 +0000

    Fix title case, add extension to Translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77821 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- DSMW.i18n.php
+++ DSMW.i18n.php
@@ -14,10 +14,8 @@ $messages = array();
  */
 $messages['en'] = array(
 	'dsmw-desc' => 'Allows to create a network of Semantic MediaWiki servers that share common semantic wiki pages. ([http://www.dsmw.org www.dsmw.org])',
-
 	'specialpages-group-dsmw_group' => 'Distributed Semantic MediaWiki',
-	
-	'dsmw-special-admin' => 'DSMW Admin functions',
-	'dsmw-special-settings' => 'DSMW Settings',
+	'dsmw-special-admin' => 'DSMW admin functions',
+	'dsmw-special-settings' => 'DSMW settings',
 	'dsmw-special-exhibits' => 'DSMW general exhibits',
 );
