bef24d7dbdd51819457fec2c863a4505f6954917
dantman <dantman@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-06 08:23:22 +0000

    Commit p2wiki skin, a port of WordPress' P2 theme.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77863 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ p2wiki/P2wiki.i18n.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Internationalisation file for the p2wiki skin
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Daniel Friesen
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
+
+$messages = array();
+
+/** English
+ * @author Daniel Friesen
+ */
+$messages['en'] = array(
+	'skinname-p2wiki' => "P2 wiki",
+);
