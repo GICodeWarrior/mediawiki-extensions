2cee14455a882c8844ee3c9324fc049d889e85f3
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-06 20:33:01 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-12-06 20:22:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77902 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- HolekCiteGen/allMessages.php
+++ HolekCiteGen/allMessages.php
@@ -73,6 +73,7 @@ $messages['en'] = array(
 );
 
 /** Message documentation (Message documentation)
+ * @author EugeneZelenko
  * @author Holek
  */
 $messages['qqq'] = array(
@@ -92,23 +93,28 @@ $messages['qqq'] = array(
 	'ts-citegen-Skin-skins' => 'Skins',
 	'ts-citegen-Skin-outputformat' => 'Output format',
 	'ts-citegen-Template-lang' => 'Template language',
-	'ts-citegen-Sources-title' => 'Sources section title',
+	'ts-citegen-Sources-title' => 'Sources section title
+{{Identical|Source}}',
 	'ts-citegen-Sourcer-text' => 'An explanation test for sources section',
 	'ts-citegen-Sidebar-title' => 'Shortened title used for mini-generator',
 	'ts-citegen-Sidebar-add-Firefox' => "Caption of generator addition to Firefox's sidebar",
 	'ts-citegen-Sidebar-add-Opera' => "Caption of generator addition to Opera's Hotlist",
-	'ts-citegen-Sidebar-add-IE-Mac' => "Caption of generator addition to Mac IE's Page Holder",
+	'ts-citegen-Sidebar-add-IE-Mac' => "Caption of generator addition to Mac IE's Page Holder. Page Holder is a Macintosh IE version of Firefox's Sidebar and Opera's Hotlist.",
 	'ts-citegen-Sidebar-add-IE-Mac-details' => "Details on generator addition to Mac IE's Page Holder",
-	'ts-citegen-Tools' => 'Tools portlet section',
-	'ts-citegen-Other-languages' => 'Other languages section',
+	'ts-citegen-Tools' => 'Tools portlet section
+{{Identical|Tools}}',
+	'ts-citegen-Other-languages' => 'Other languages section
+{{Identical|Otherlanguages}}',
 	'ts-citegen-Save-it' => 'Link to itself/current query',
-	'ts-citegen-Errors-title' => 'Errors section title',
+	'ts-citegen-Errors-title' => 'Errors section title
+{{Identical|Error}}',
 	'ts-citegen-Unavailable-API' => 'Error message: Wikimedia API is unavailable',
 	'ts-citegen-Unavailable-SQL' => 'Error message: Toolserver database is unavailable. %s is an error message',
 	'ts-citegen-base-disabled' => 'Error message: A book database is unavailable. <tt>%s</tt> is the name of the database.',
 );
 
 /** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author EugeneZelenko
  * @author Wizardist
  */
 $messages['be-tarask'] = array(
@@ -131,21 +137,34 @@ $messages['be-tarask'] = array(
 	'ts-citegen-Sources-title' => 'Крыніцы',
 	'ts-citegen-Sourcer-text' => 'Ніжэй пададзены сьпіс выкарыстаных крыніцаў.',
 	'ts-citegen-Sidebar-title' => 'Генэратар цытаваньняў',
-	'ts-citegen-Sidebar-text' => 'Гэтая вэрсія генэратара выкарыстоўвае налады поўнай вэрсіі генэратара. Гэтак, калі вы зьмяняеце моўную вэрсію шаблёну, рабіце гэта праз генэратар. Налады захоўваюцца ў cookies.',
 	'ts-citegen-Sidebar-add-Firefox' => 'Дадаць да бакавой панэлі',
 	'ts-citegen-Sidebar-add-Opera' => 'Дадаць да панэлі Opera',
 	'ts-citegen-Sidebar-add-IE-Mac' => 'Дадаць да Page Holder',
-	'ts-citegen-Sidebar-add-IE-Mac-details' => "Як толькі старонка загрузіцца, адкрыйце Page Holder, націсьніце 'Add', і скарыстайцеся Page Holder Favorites лоя захаваньня старонкі.",
+	'ts-citegen-Sidebar-add-IE-Mac-details' => "Як толькі старонка загрузіцца, адкрыйце Page Holder, націсьніце 'Add' і выкарыстоўвайце кнопку Page Holder Favorites лля захаваньня старонкі.",
 	'ts-citegen-Tools' => 'Інструмэнты',
 	'ts-citegen-Other-languages' => 'На іншых мовах',
-	'ts-citegen-Save-it' => 'Бягучы запыт',
+	'ts-citegen-Save-it' => 'Цяперашні запыт',
 	'ts-citegen-Errors-title' => 'Памылкі',
 	'ts-citegen-Unavailable-API' => 'Памылка: Wikimedia API недаступны',
-	'ts-citegen-Unavailable-SQL' => 'Памылка: база зьвестак Toolserver недаступная. MySQL адказаў: %s',
+	'ts-citegen-Unavailable-SQL' => 'Памылка: база зьвестак Toolserver недаступная. адказ MySQL: %s',
 	'ts-citegen-base-disabled' => 'Памылка: база зьвестак %s недаступная.',
 );
 
+/** Breton (Brezhoneg)
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'ts-citegen-Tools' => 'Ostilhoù',
+	'ts-citegen-Other-languages' => 'Yezhoù all',
+	'ts-citegen-Save-it' => 'Reked red',
+	'ts-citegen-Errors-title' => 'Fazioù',
+	'ts-citegen-Unavailable-API' => "Fazi : N'haller ket tizhout API wikimedia",
+	'ts-citegen-Unavailable-SQL' => "Fazi : N'haller ket tizhout diaz roadennoù ar servijer ostilhoù. Kemenn MySQL : %s",
+	'ts-citegen-base-disabled' => "Fa zi : N'haller ket tizhout diaz roadennoù %s.",
+);
+
 /** German (Deutsch)
+ * @author Holek
  * @author Kghbln
  */
 $messages['de'] = array(
@@ -164,6 +183,28 @@ $messages['de'] = array(
 	'ts-citegen-Skins' => 'Ausgabe',
 	'ts-citegen-Skin-skins' => 'Benutzeroberflächen',
 	'ts-citegen-Skin-outputformat' => 'Ausgabeformat',
+	'ts-citegen-Template-lang' => 'Sprache der Vorlage',
+	'ts-citegen-Sources-title' => 'Quellen',
+	'ts-citegen-Sourcer-text' => 'Unterhalb ist die Liste der verwenden Quellen verfügbar.',
+	'ts-citegen-Sidebar-title' => 'Quellennachweisgenerator',
+	'ts-citegen-Sidebar-add-Firefox' => 'Zu den Lesezeichen hinzufügen',
+	'ts-citegen-Sidebar-add-Opera' => 'Zu den Lesezeichen hinzufügen',
+	'ts-citegen-Sidebar-add-IE-Mac' => 'Zu den Favoriten hinzufügen',
+	'ts-citegen-Sidebar-add-IE-Mac-details' => 'Sobald die Seite geladen wurde, öffne bitte die Favoritenleiste, klicke danach auf „Hinzufügen“ und nutze anschließend den Favoritenknopf, um sie als Favorit zu speichern.',
+	'ts-citegen-Tools' => 'Werkzeuge',
+	'ts-citegen-Other-languages' => 'Andere Sprachen',
+	'ts-citegen-Save-it' => 'Aktuelle Abfrage',
+	'ts-citegen-Errors-title' => 'Fehler',
+	'ts-citegen-Unavailable-API' => 'Fehler: Die Programmierschnittstelle zur Wikipedia ist nicht verfübar.',
+	'ts-citegen-Unavailable-SQL' => 'Fehler: Die Datenbank des Toolservers ist nicht verfügbar. MySQL erzeugte: %s',
+	'ts-citegen-base-disabled' => 'Fehler: %s-Datenbank ist nicht verfügbar.',
+);
+
+/** British English (British English)
+ * @author Holek
+ */
+$messages['en-gb'] = array(
+	'ts-citegen-Sidebar-add-IE-Mac-details' => "Once the page has loaded, open your Page Holder, click 'Add' then use the Page Holder Favourites button to store it as a Page Holder Favourite.",
 );
 
 /** Upper Sorbian (Hornjoserbsce)
@@ -171,7 +212,7 @@ $messages['de'] = array(
  */
 $messages['hsb'] = array(
 	'ts-citegen-Title' => 'Generator citatowych předłohow',
-	'ts-citegen-Send' => 'Pósłać',
+	'ts-citegen-Send' => 'Wupłodźić',
 	'ts-citegen-Input-title' => 'Zapodaće',
 	'ts-citegen-Option-append-author-link' => 'Awtorowe wikiwotkazy do předłohi připowěsnyć',
 	'ts-citegen-Option-append-newlines' => 'Za kóždym parametrom nowe linki připowěsnyć',
@@ -199,12 +240,95 @@ $messages['hsb'] = array(
 	'ts-citegen-base-disabled' => 'Zmylk: Datowa banka %s njesteji k dispoziciji.',
 );
 
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'ts-citegen-Title' => 'Pembuat templat kutipan',
+	'ts-citegen-Send' => 'Buat',
+	'ts-citegen-Input-title' => 'Masukan',
+	'ts-citegen-Input-text' => 'Ini adalah pembuat templat kutipan. Anda dapat dengan cepat mengisi templat kutipan dalam berbagai edisi bahasa Wikipedia dengan menggunakannya. Silakan isi data (%s) pada kolom-kolom di bawah ini dan skrip akan mencoba untuk menyelesaikan templat. Ingat, bidang apa pun yang Anda pilih untuk memasukkan data bukan masalah. Secara otomatis skrip akan mencocokkan templat yang tepat untuk masukan yang diberikan.',
+	'ts-citegen-Option-append-author-link' => 'Tambahkan pranala penulis ke dalam templat',
+	'ts-citegen-Option-append-newlines' => 'Tambahkan baris baru setelah setiap parameter',
+	'ts-citegen-Option-add-references' => 'Tambahkan tag <ref> di sekitar templat kutipan',
+	'ts-citegen-Option-add-list' => 'Buat daftar wiki tempat kutipan',
+	'ts-citegen-Output-title' => 'Hasil',
+	'ts-citegen-Output-select-disclaimer' => 'Ingat bahwa memilih bahasa templat tidak menjamin bahwa templat tertentu tersedia dalam bahasa Anda. Bidang ini mencantumkan daftar bahasa yang tersedia dari setiap template yang didukung, yaitu dapat menampilkan Perancis, karena hanya {{Cite book}} yang didukung.',
+	'ts-citegen-Wrong-input' => '%s: tidak dikenali sebagai masukan yang benar.',
+	'ts-citegen-Parsers' => 'Parser',
+	'ts-citegen-Skins' => 'Keluaran',
+	'ts-citegen-Skin-skins' => 'Kulit',
+	'ts-citegen-Skin-outputformat' => 'Format keluaran',
+	'ts-citegen-Template-lang' => 'Bahasa templat',
+	'ts-citegen-Sources-title' => 'Sumber',
+	'ts-citegen-Sourcer-text' => 'Berikut adalah daftar sumber',
+	'ts-citegen-Sidebar-title' => 'Pembuat kutipan',
+	'ts-citegen-Sidebar-add-Firefox' => 'Tambahkan ke bilah sisi',
+	'ts-citegen-Sidebar-add-Opera' => 'Tambahkan ke Hotlist',
+	'ts-citegen-Sidebar-add-IE-Mac' => 'Tambahkan ke Page Holder',
+	'ts-citegen-Sidebar-add-IE-Mac-details' => "Setelah halaman dimuat, buka Page Holder Anda, klik 'Tambah', kemudian gunakan tombol Favorit Page Holder untuk menyimpannya sebagai Favorit Page Holder.",
+	'ts-citegen-Tools' => 'Peralatan',
+	'ts-citegen-Other-languages' => 'Bahasa lain',
+	'ts-citegen-Save-it' => 'Kueri saat ini',
+	'ts-citegen-Errors-title' => 'Galat',
+	'ts-citegen-Unavailable-API' => 'Galat: API Wikimedia tidak tersedia',
+	'ts-citegen-Unavailable-SQL' => 'Galat: Basis data Toolserver tidak tersedia. Tanggapan MySQL: %s',
+	'ts-citegen-base-disabled' => 'Galat: Basis data %s tidak tersedia.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'ts-citegen-Template-lang' => 'Sprooch vun der Schabloun',
+	'ts-citegen-Other-languages' => 'Aner Sproochen',
+	'ts-citegen-Errors-title' => 'Feeler',
+	'ts-citegen-Unavailable-API' => 'Feeler: Wikimedia API ass net disponibel',
+	'ts-citegen-base-disabled' => 'Feeler: %s Datebank ass net disponibel.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'ts-citegen-Title' => 'Создавач на шаблони за цитирање',
+	'ts-citegen-Send' => 'Создај',
+	'ts-citegen-Input-title' => 'Внос',
+	'ts-citegen-Input-text' => 'Ова е создавач на шаблони за цитати. Овозможува брзо пополнување на шаблоните за цитирање на разни јазични изданија на Википедија. Пополнете ги податоците (%s) во долунаведените полиња, а самата скрипта ќе се обиде да ги доврши шаблоните. Запомнете: не е важно во кои полиња ги внесувате податоците. Самата скрипта ќе го изнајде точниот шаблон за внесеното.',
+	'ts-citegen-Option-append-author-link' => 'Додај викиврски за авторот во шаблонот',
+	'ts-citegen-Option-append-newlines' => 'Додај нови редови по секој параметар',
+	'ts-citegen-Option-add-references' => 'Додај ознаки <ref> околу шаблоните за цитирање',
+	'ts-citegen-Option-add-list' => 'Создај викисписок на шаблони за цитирање',
+	'ts-citegen-Output-title' => 'Исход',
+	'ts-citegen-Output-select-disclaimer' => 'Запомнете: изборот на јазик на шаблонот не ви гарантира дека тој шаблон е достапен на вашиот јазик. Ова поле ги наведува достапните јазици за секој поддржан шаблон. (т.е. може да прикаже француски бидејќи е поддржан само {{Cite book}}.)',
+	'ts-citegen-Wrong-input' => '%s: не е утврден како исправен внос.',
+	'ts-citegen-Parsers' => 'Парсери',
+	'ts-citegen-Skins' => 'Извод',
+	'ts-citegen-Skin-skins' => 'Рува',
+	'ts-citegen-Skin-outputformat' => 'Формат на изводот',
+	'ts-citegen-Template-lang' => 'Јазик на шаблонот',
+	'ts-citegen-Sources-title' => 'Извори',
+	'ts-citegen-Sourcer-text' => 'Подолу има список на користени извори.',
+	'ts-citegen-Sidebar-title' => 'Создавач на цитати',
+	'ts-citegen-Sidebar-add-Firefox' => 'Додај во страничната лента',
+	'ts-citegen-Sidebar-add-Opera' => 'Додај во Hotlist',
+	'ts-citegen-Sidebar-add-IE-Mac' => 'Додај во Page Holder',
+	'ts-citegen-Sidebar-add-IE-Mac-details' => 'Откако ќе се вчита страницата, отворете го вашиот Page Holder, сиснете на „Додај“, и потоа со копчето за Page Holder Favorites зачувајте ја како таква.',
+	'ts-citegen-Tools' => 'Алатки',
+	'ts-citegen-Other-languages' => 'Други јазици',
+	'ts-citegen-Save-it' => 'Тековно барање',
+	'ts-citegen-Errors-title' => 'Грешки',
+	'ts-citegen-Unavailable-API' => 'Грешка: API на Викимедија е недостапен',
+	'ts-citegen-Unavailable-SQL' => 'Грешка: Базата на Toolserver е недостапна. MySQL даде: %s',
+	'ts-citegen-base-disabled' => 'Грешка: Базата на %s е недостапна.',
+);
+
 /** Polish (Polski)
  * @author Holek
  */
 $messages['pl'] = array(
 	'ts-citegen-Title' => 'Generator szablonów cytowania',
-	'ts-citegen-Send' => 'Wyślij',
+	'ts-citegen-Send' => 'Generuj',
 	'ts-citegen-Input-title' => 'Dane wejściowe',
 	'ts-citegen-Input-text' => 'To jest generator szablonów cytowania. Za pomocą tego narzędzia możesz szybko uzupełnić różne szablony cytowania dostępne w różnych edycjach językowych Wikipedii. Wpisz w polach poniżej odpowiednie dane (%s), a skrypt postara się wypełnić odpowiednie szablony według danych wejściowych. Pamiętaj, że nie ma znaczenia, w których polach, co wpiszesz. Skrypt automatycznie dopasowuje szablony do wprowadzonych danych.',
 	'ts-citegen-Option-append-author-link' => 'Dołączaj linki do artykułów o odpowiednich autorach',
@@ -220,7 +344,7 @@ $messages['pl'] = array(
 	'ts-citegen-Skin-outputformat' => 'Dla botów',
 	'ts-citegen-Template-lang' => 'Język szablonu',
 	'ts-citegen-Sources-title' => 'Źródła',
-	'ts-citegen-Sourcer-text' => 'Poniżej podane są strony, z których korzystano przy pobieraniu informacji o książkach. Każdy z linków przenosi',
+	'ts-citegen-Sourcer-text' => 'Poniżej podane są strony, z których korzystano przy pobieraniu informacji o książkach.',
 	'ts-citegen-Sidebar-title' => 'Generator cytowań',
 	'ts-citegen-Sidebar-add-Firefox' => 'Dodaj do panelu bocznego',
 	'ts-citegen-Sidebar-add-Opera' => 'Dodaj do panelu Opery',
@@ -231,7 +355,14 @@ $messages['pl'] = array(
 	'ts-citegen-Save-it' => 'Samowywołanie (zapisz tę stronę)',
 	'ts-citegen-Errors-title' => 'Błędy',
 	'ts-citegen-Unavailable-API' => 'Błąd: API Wikipedii jest niedostępne',
-	'ts-citegen-Unavailable-SQL' => 'Błąd: Dostęp do bazy danych SQL niemożliwy',
+	'ts-citegen-Unavailable-SQL' => 'Błąd: Dostęp do bazy danych niemożliwy. MySQL zwróciło: %s',
 	'ts-citegen-base-disabled' => 'Błąd: Baza %s jest niedostępna',
+);
+
+/** Portuguese (Português)
+ * @author Giro720
+ */
+$messages['pt'] = array(
+	'ts-citegen-Title' => 'Gerador de predefinição de citação',
 );
 
