e289f6dd9ced05d1c1087f661c551ae47cf826c4
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-06 09:42:21 +0000

    FileRepo stuff no longer uses WikiError since ages
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77868 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikilog/maintenance/wikilogImportDocumentation.php
+++ extensions/Wikilog/maintenance/wikilogImportDocumentation.php
@@ -168,7 +168,7 @@ class WikilogImportDocumentation
 					$revision->getText(), 0, false, $this->mTimestamp,
 					$this->mUser);
 
-				if ( WikiError::isError( $archive ) || !$archive->isGood() ) {
+				if ( !$archive->isGood() ) {
 					$this->output( " failed.\n" );
 					return false;
 				} else {
