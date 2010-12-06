64b15b0bcc1942ec6266db7dd17d84db0686a05c
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-06 23:21:05 +0000

    Basic star rating functionality working
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77931 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- modules/ext.articleFeedback-beta/ext.articleFeedback-beta.js
+++ modules/ext.articleFeedback-beta/ext.articleFeedback-beta.js
@@ -6,6 +6,23 @@
 
 $.articleFeedback = {
 	'fn': {
+		'updateRating': function() {
+			$(this)
+				.find( 'label' )
+					.removeClass( 'articleFeedback-rating-label-new' )
+					.each( function() {
+						if ( $( 'input#' + $(this).attr( 'for' ) + ':checked' ).length ) {
+							$(this)
+								.prevAll( 'label' )
+									.andSelf()
+										.addClass( 'articleFeedback-rating-label-full' )
+										.end()
+									.end()
+								.nextAll( 'label' )
+									.removeClass( 'articleFeedback-rating-label-full' );
+						}
+					} );
+		},
 		'build': function( context ) {
 			context.$ui
 				.addClass( 'articleFeedback articleFeedback-form' )
@@ -30,22 +47,22 @@ $.articleFeedback = {
 			<div class="articleFeedback-rating" rel="wellsourced">\
 				<span class="articleFeedback-label" title-msg="field-wellsourced-tip"><msg key="field-wellsourced-label" /></span>\
 				<div class="articleFeedback-rating-fields articleFeedback-form"><input type="radio" /><input type="radio" /><input type="radio" /><input type="radio" /><input type="radio" /></div>\
-				<div class="articleFeedback-rating-labels articleFeedback-form"><label class="articleFeedback-rating-label-first"></label><label></label><label></label><label></label><label class="articleFeedback-rating-label-last"></label></div>\
+				<div class="articleFeedback-rating-labels articleFeedback-form"><label></label><label></label><label></label><label></label><label></label></div>\
 			</div>\
 			<div class="articleFeedback-rating" rel="neutral">\
 				<span class="articleFeedback-label" title-msg="field-neutral-tip"><msg key="field-neutral-label" /></span>\
 				<div class="articleFeedback-rating-fields articleFeedback-form"><input type="radio" /><input type="radio" /><input type="radio" /><input type="radio" /><input type="radio" /></div>\
-				<div class="articleFeedback-rating-labels articleFeedback-form"><label class="articleFeedback-rating-label-first"></label><label></label><label></label><label></label><label class="articleFeedback-rating-label-last"></label></div>\
+				<div class="articleFeedback-rating-labels articleFeedback-form"><label></label><label></label><label></label><label></label><label></label></div>\
 			</div>\
 			<div class="articleFeedback-rating" rel="complete">\
 				<span class="articleFeedback-label" title-msg="field-complete-tip"><msg key="field-complete-label" /></span>\
 				<div class="articleFeedback-rating-fields articleFeedback-form"><input type="radio" /><input type="radio" /><input type="radio" /><input type="radio" /><input type="radio" /></div>\
-				<div class="articleFeedback-rating-labels articleFeedback-form"><label class="articleFeedback-rating-label-first"></label><label></label><label></label><label></label><label class="articleFeedback-rating-label-last"></label></div>\
+				<div class="articleFeedback-rating-labels articleFeedback-form"><label></label><label></label><label></label><label></label><label></label></div>\
 			</div>\
 			<div class="articleFeedback-rating" rel="readable">\
 				<span class="articleFeedback-label" title-msg="field-readable-tip"><msg key="field-readable-label" /></span>\
 				<div class="articleFeedback-rating-fields articleFeedback-form"><input type="radio" /><input type="radio" /><input type="radio" /><input type="radio" /><input type="radio" /></div>\
-				<div class="articleFeedback-rating-labels articleFeedback-form"><label class="articleFeedback-rating-label-first"></label><label></label><label></label><label></label><label class="articleFeedback-rating-label-last"></label></div>\
+				<div class="articleFeedback-rating-labels articleFeedback-form"><label></label><label></label><label></label><label></label><label></label></div>\
 			</div>\
 			<div style="clear:both;"></div>\
 		</div>\
@@ -71,11 +88,37 @@ $.articleFeedback = {
 				' )
 				.localize( { 'prefix': 'articlefeedback-beta-' } )
 				.find( '[title]' )
-					.tipsy( { 'gravity': 'nw', 'fade': true, 'center': false } )
+					.tipsy( {
+						'gravity': 'nw',
+						'center': false,
+						'fade': true,
+						'delayIn': 300,
+						'delayOut': 100
+					} )
 					.end()
 				// Hide report elements initially
 				.find( '.articleFeedback-report' )
 					.hide()
+					.end()
+				// Connect labels and fields
+				.find( '.articleFeedback-rating' )
+					.each( function() {
+						var rel = $(this).attr( 'rel' );
+						$(this)
+							.find( '.articleFeedback-rating-fields input' )
+								.attr( 'name', rel )
+								.each( function( i ) {
+									$(this)
+										.val( i + 1 )
+										.attr( 'id', 'articleFeedback-rating-field-' + rel + '-' + ( i + 1 ) );
+								} )
+								.end()
+							.find( '.articleFeedback-rating-labels label' )
+								.each( function( i ) {
+									$(this)
+										.attr( 'for', 'articleFeedback-rating-field-' + rel + '-' + ( i + 1 ) );
+								} );
+					} )
 					.end()
 				// Setup tab behavior
 				.find( '.articleFeedback-tab' )
@@ -95,7 +138,21 @@ $.articleFeedback = {
 								} );
 						e.preventDefault();
 						return false;
-					} );
+					} )
+					.end()
+				// Setup rating behavior
+				.find( '.articleFeedback-rating-labels label' )
+					.hover(
+						function() {
+							$(this).prevAll( 'label' ).andSelf()
+								.addClass( 'articleFeedback-rating-label-new' );
+						},
+						function() {
+							$.articleFeedback.fn.updateRating.call(
+								$(this).closest( '.articleFeedback-rating' )
+							);
+						}
+					);
 		}
 	}
 };
