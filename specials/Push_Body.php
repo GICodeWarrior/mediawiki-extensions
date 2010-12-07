fce12e53b81bfcb15769cc772a31e85514803985
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-07 07:52:25 +0000

    Added AdminLinks hook, removed not needed code and added special page stub
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@77961 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ specials/Push_Body.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * A special page that allows pushing one or more pages to one or more targets.
+ * 
+ * @since 0.1
+ * 
+ * @author Jeroen De Dauw
+ */
+class SpecialPush extends SpecialPage {
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @since 0.1
+	 */
+	public function __construct() {
+		parent::__construct( 'Push', 'push' );
+	}
+	
+	/**
+	 * @see SpecialPage::getDescription
+	 */
+	public function getDescription() {
+		return wfMsg( 'special-' . strtolower( $this->mName ) );
+	}
+	
+	/**
+	 * Main method.
+	 * 
+	 * @since 0.1 
+	 * 
+	 * @param string $arg
+	 */
+	public function execute( $arg ) {
+		global $wgOut, $wgUser;
+		
+		$wgOut->setPageTitle( wfMsg( 'special-push' ) );
+		
+		// If the user is authorized, display the page, if not, show an error.
+		if ( !$this->userCanExecute( $wgUser ) ) {
+			$this->displayRestrictionError();
+			return;
+		} 
+
+		// TODO
+	}	
+	
+}
