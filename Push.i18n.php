8c5f2f786586ef5d1a46e4ae55b17b764ee30bbc
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-08 22:35:58 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-12-08 20:15:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78100 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Push.i18n.php
+++ Push.i18n.php
@@ -55,8 +55,93 @@ To push pages, enter the titles in the text box below, one title per line and hi
 	# 'right-pushadmin' => '', // Please add a description of this userright which is shown at [[Special:ListGroupRights]]
 );
 
+/** Message documentation (Message documentation) */
 $messages['qqq'] = array(
-	'push-import-revision-message' => '$3 is [[MediaWiki:Push-import-revision-comment]] or empty.', 
+	'push-import-revision-message' => '$3 is [[MediaWiki:Push-import-revision-comment]] or empty.',
 	'push-remote-page-link' => '$1: page name, $2: wiki name',
 	'push-remote-page-link-full' => '$1: page name, $2: wiki name',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'push-desc' => 'Ermöglicht den einfachen Transfer von Inhalten eines Wikis in ein anderes',
+	'push-tab-text' => 'Transferieren',
+	'push-button-text' => 'Transferieren',
+	'push-tab-desc' => 'Dieser Reiter ermöglicht den Transfer des aktuellen Seiteninhalts in ein oder mehrere andere Wikis.',
+	'push-button-pushing' => 'Transferiere',
+	'push-button-completed' => 'Transfer abgeschlossen',
+	'push-button-failed' => 'Transfer fehlgeschlagen',
+	'push-tab-title' => 'Transferiere $1',
+	'push-targets' => 'Transferziele',
+	'push-add-target' => 'Transferziel hinzufügen',
+	'push-import-revision-message' => 'Import von $1 durch Benutzer $2. $3',
+	'push-import-revision-comment' => 'Letzter Kommentar: $1',
+	'push-tab-no-targets' => 'Es sind keine Transferziele vorhanden. Es müssen welche in der Datei LocalSettings.php definiert werden.',
+	'push-tab-push-to' => 'Transferiere nach $1',
+	'push-remote-pages' => 'Entfernte Seiten',
+	'push-remote-page-link' => 'Seite $1 auf Wiki $2',
+	'push-remote-page-link-full' => 'Seite $1 auf Wiki $2 ansehen',
+	'push-targets-total' => 'Es {{PLURAL:$1|ist|sind}} insgesamt $1 {{PLURAL:$1|Transferziel|Transferziele}} vorhanden.',
+	'push-button-all' => 'Alle transferieren',
+	'special-push' => 'Seiten transferieren',
+	'push-special-description' => 'Diese Spezialseite ermöglicht es den Inhalt einer oder mehrerer Seiten zu einem oder mehreren anderen Wikis zu transferieren.
+
+Um Seiten zu transferieren, sind deren Titel im Eingabefeld unten anzugeben (ein Titel pro Zeile). Klicke danach auf „{{int:push-special-button-text}}“. Es kann etwas dauern, bis der Transfer abgeschlossen ist.',
+	'push-special-pushing-desc' => 'Transferiere $2 {{PLURAL:$2|Seite|Seiten}} nach $1 …',
+	'push-special-button-text' => 'Seiten transferieren',
+	'push-special-target-is' => 'Zielwiki: $1',
+	'push-special-select-targets' => 'Zielwikis:',
+	'push-special-item-getting' => '$1: Seite wird geholt',
+	'push-special-item-pushing-to' => '$1: Transferiere nach $2',
+	'push-special-item-completed' => '$1: Transfer abgeschlossen',
+	'push-special-item-failed' => '$1: Transfer fehlgeschlagen. Grund: $2',
+	'push-special-push-done' => 'Transfer abgeschlossen',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'push-desc' => 'Мал додаток за пренесување на содржини од едно на други викија',
+	'push-tab-text' => 'Пренеси',
+	'push-button-text' => 'Пренеси',
+	'push-tab-desc' => 'Ова јазиче ви овозможува да ја пренесете тековната ревизија на страницава на едно или повеќе викија',
+	'push-button-pushing' => 'Пренесувам',
+	'push-button-completed' => 'Преносот заврши',
+	'push-button-failed' => 'Преносот не успеа',
+	'push-tab-title' => 'Пренесување на $1',
+	'push-targets' => 'Пренеси цели',
+	'push-add-target' => 'Додај цел',
+	'push-import-revision-message' => 'Увоз од $1. Увозник: $2. $3',
+	'push-import-revision-comment' => 'Последен коментар: $1',
+	'push-tab-no-targets' => 'Нема целни места за пренос. Додајте места во вашата податотека LocalSettings.php.',
+	'push-tab-push-to' => 'Пренеси во $1',
+	'push-remote-pages' => 'Далечински страници',
+	'push-remote-page-link' => '$1 на $2',
+	'push-remote-page-link-full' => 'Преглед на $1 на $2',
+	'push-targets-total' => 'Има вкупно $1 {{PLURAL:$1|цел|цели}}.',
+	'push-button-all' => 'Пренеси сè',
+	'special-push' => 'Пренесување страници',
+	'push-special-description' => 'Оваа страница ви овозможува да пренесете содржини од една или повеќе страници од едно вики во едно или повеќе викија што работат на МедијаВики.
+
+За да пренесете, внесете ги насловите во полето подолу, по едно во секој ред, па стиснете на „Пренеси сè“. Ова може да потрае.',
+	'push-special-pushing-desc' => 'Пренесувам $2 {{PLURAL:$2|страница|страници}} во $1...',
+	'push-special-button-text' => 'Пренеси',
+	'push-special-target-is' => 'Целно вики: $1',
+	'push-special-select-targets' => 'Целни викија:',
+	'push-special-item-getting' => '$1: Ја преземам страницата',
+	'push-special-item-pushing-to' => '$1: Пренесуавам во $2',
+	'push-special-item-completed' => '$1: Преносот заврши',
+	'push-special-item-failed' => '$1: Преносот не успеа: $2',
+	'push-special-push-done' => 'Преносот заврши',
+);
+
+/** Russian (Русский)
+ * @author Lockal
+ */
+$messages['ru'] = array(
+	'push-import-revision-comment' => 'Последний комментарий: $1',
+);
+
