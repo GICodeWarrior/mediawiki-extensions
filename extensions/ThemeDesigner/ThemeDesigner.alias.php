2bba8d96a2666eaf45ac3552b04e007f72578149
dantman <dantman@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-12 19:02:11 +0000

    Commit experimental ThemeDesigner extension.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78270 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ThemeDesigner/ThemeDesigner.alias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aliases for Special:ThemeDesigner
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author Daniel Friesen
+ */
+$specialPageAliases['en'] = array(
+	'ThemeDesigner' => array( 'ThemeDesigner' ),
+);
+
