98587cc972ecb2493cadba638b39009753c25bda
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-13 20:58:38 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-12-13 20:33:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78330 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SVGEdit.i18n.php
+++ SVGEdit.i18n.php
@@ -28,7 +28,35 @@ $messages['qqq'] = array(
 	'svgedit-desc' => 'Extension description displayed on [[Special:Version]]',
 );
 
-/** Esperanto
+/** Breton (Brezhoneg)
+ * @author Y-M D
+ */
+$messages['br'] = array(
+	'svgedit-editor-save-close' => 'Enrollañ ha serriñ',
+	'svgedit-editor-close' => 'Nullañ',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'svgedit-editbutton-edit' => 'Datei bearbeiten',
+	'svgedit-editor-save-close' => 'Speichern und schließen',
+	'svgedit-editor-close' => 'Abbrechen',
+	'svgedit-desc' => 'Ermöglicht das Bearbeiten einer SVG-Datei mit [http://code.google.com/p/svg-edit/ SVG-edit]',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'svgedit-editbutton-edit' => 'Kreslanku wobźěłaś',
+	'svgedit-editor-save-close' => 'Składowaś a zacyniś',
+	'svgedit-editor-close' => 'Pśetergnuś',
+	'svgedit-desc' => 'Zmóžnja wobźěłowanje SVG-kreslankow z [http://code.google.com/p/svg-edit/ SVG-Edit] we wobglědowaku',
+);
+
+/** Esperanto (Esperanto)
  * @author Brion Vibber <brion@pobox.com>
  */
 $messages['eo'] = array(
@@ -36,5 +64,75 @@ $messages['eo'] = array(
 	'svgedit-editor-save-close' => 'Konservi kaj reveni',
 	'svgedit-editor-close' => 'Malfermi',
 	'svgedit-desc' => 'Redakti desegnojn SVG en foliumilo per [http://code.google.com/p/svg-edit/ SVG-Edit]',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'svgedit-editbutton-edit' => 'Datei bearbeite',
+	'svgedit-editor-save-close' => 'Spychere un zuemache',
+	'svgedit-editor-close' => 'Abbräche',
+	'svgedit-desc' => 'Macht a Bearbeite vun ere SVG-Datei mit [http://code.google.com/p/svg-edit/ SVG-edit] megli',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'svgedit-editbutton-edit' => 'Rysowanku wobdźěłać',
+	'svgedit-editor-save-close' => 'Składować a začinić',
+	'svgedit-editor-close' => 'Přetorhnyć',
+	'svgedit-desc' => 'Zmóžnja wobdźěłanje SVG-rysowanki z [http://code.google.com/p/svg-edit/ SVG-Edit] we wobhladowaku',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'svgedit-editbutton-edit' => 'Modificar designo',
+	'svgedit-editor-save-close' => 'Salveguardar e clauder',
+	'svgedit-editor-close' => 'Cancellar',
+	'svgedit-desc' => 'Modification in le navigator de designos SVG con [http://code.google.com/p/svg-edit/ SVG-Edit]',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'svgedit-editbutton-edit' => 'Sunting gambar',
+	'svgedit-editor-save-close' => 'Simpan dan tutup',
+	'svgedit-editor-close' => 'Batalkan',
+	'svgedit-desc' => 'Penyuntingan gambar SVG via peramban dengan [http://code.google.com/p/svg-edit/ SVG-Edit]',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'svgedit-editbutton-edit' => 'Уреди',
+	'svgedit-editor-save-close' => 'Зачувај и затвори',
+	'svgedit-editor-close' => 'Откажи',
+	'svgedit-desc' => 'Уредување на SVG-цртежи во самиот прелистувач со [http://code.google.com/p/svg-edit/ SVG-Edit]',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'svgedit-editbutton-edit' => 'Tekening bewerken',
+	'svgedit-editor-save-close' => 'Opslaan en sluiten',
+	'svgedit-editor-close' => 'Annuleren',
+	'svgedit-desc' => 'In de browser bewerken van SVG-tekeningen met [http://code.google.com/p/svg-edit/ SVG-Edit]',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'svgedit-editbutton-edit' => 'Редактировать рисунок',
+	'svgedit-editor-save-close' => 'Сохранить и закрыть',
+	'svgedit-editor-close' => 'Отмена',
+	'svgedit-desc' => 'Редактирование SVG-изображений в браузере с помощью [http://code.google.com/p/svg-edit/ SVG-Edit]',
 );
 
