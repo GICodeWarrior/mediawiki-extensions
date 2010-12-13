1d051c4b7d6dcc50bd69da7938416007107cacfb
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-13 20:58:38 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-12-13 20:33:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78330 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- CategoryMembersOnSidebar.i18n.php
+++ CategoryMembersOnSidebar.i18n.php
@@ -15,3 +15,45 @@ $messages['en'] = array(
 	'categorymembersonsidebar-desc' => 'Adds category members on the sidebar',
 );
 
+/** Bosnian (Bosanski)
+ * @author CERminator
+ */
+$messages['bs'] = array(
+	'categorymembersonsidebar-desc' => 'Dodaje članove kategorije na alatnu traku sa strane',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'categorymembersonsidebar-desc' => 'Ermöglicht die Anzeige der einer Kategorie zugeordneten Seiten in der Seitenleiste',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'categorymembersonsidebar-desc' => 'Macht d Aazeig vu dr Syte, wu in ere Kategori din sin, in dr Syteleischte megli',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'categorymembersonsidebar-desc' => 'Adde membros de categorias al barra lateral',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'categorymembersonsidebar-desc' => 'Додава членови на категорија во страничната лента',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'categorymembersonsidebar-desc' => 'Добавляет членов категории на боковую панель',
+);
+
