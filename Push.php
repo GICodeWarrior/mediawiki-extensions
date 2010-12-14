485a8a86992d7654186b944aaf952e6c95c8df09
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-14 09:45:05 +0000

    Updated INSTALL and incremented version number
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78362 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Push.php
+++ Push.php
@@ -22,7 +22,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define( 'Push_VERSION', '0.5 alpha' );
+define( 'Push_VERSION', '0.5 rc' );
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
