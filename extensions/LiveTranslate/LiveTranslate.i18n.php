94671ecf63d433b17afc728e9ff03e7552a92cb2
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-15 10:55:36 +0000

    Adding stub for new LiveTranslate extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78431 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/LiveTranslate/LiveTranslate.i18n.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Internationalization file for the Live Translate extension.
+ *
+ * @file LiveTranslate.i18n.php
+ * @ingroup LiveTranslate
+ *
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'lte-desc' => 'Enables live translation of page content using the Google Translate service.',
+
+
+);
