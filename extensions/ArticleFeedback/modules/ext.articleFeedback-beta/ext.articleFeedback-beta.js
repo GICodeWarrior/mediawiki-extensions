5a1d1835e350a3cde4591c674da300467dcb0618
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-15 23:18:51 +0000

    Refactoring things - step 1 of 3, removing alpha
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78463 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ArticleFeedback/modules/ext.articleFeedback-beta/ext.articleFeedback-beta.js
+++ extensions/ArticleFeedback/modules/ext.articleFeedback-beta/ext.articleFeedback-beta.js
@@ -362,6 +362,35 @@ $.fn.articleFeedback = function( method, data ) {
 	return $(this);
 };
 
-$( '#catlinks' ).before( $( '<div></div>' ).articleFeedback() );
+$( '#catlinks' )
+	.before(
+		$( '<div></div>' )
+			.articleFeedback(/* {
+				'ratings': {
+					'wellsourced': {
+						'id': '1',
+						'label': 'article-feedback-beta-wellsourced-field-wellsourced-label',
+						'tip': 'article-feedback-beta-wellsourced-field-wellsourced-tip',
+					},
+					'neutral': {
+						'id': '2',
+						'label': 'article-feedback-beta-wellsourced-field-neutral-label',
+						'tip': 'article-feedback-beta-wellsourced-field-neutral-tip',
+					},
+					'complete': {
+						'id': '3',
+						'label': 'article-feedback-beta-wellsourced-field-complete-label',
+						'tip': 'article-feedback-beta-wellsourced-field-complete-tip',
+					},
+					'readable': {
+						'id': '4',
+						'label': 'article-feedback-beta-wellsourced-field-readable-label',
+						'tip': 'article-feedback-beta-wellsourced-field-readable-tip',
+					}
+				}
+			}
+			*/
+		)
+	);
 
 } )( jQuery, mediaWiki );
