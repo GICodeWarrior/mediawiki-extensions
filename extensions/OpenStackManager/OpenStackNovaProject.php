f8c603ab8b905336d2544457490533e60504c97d
laner <laner@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-16 08:52:32 +0000

    * Adding missing special page and class for projects * UI improvements for Instance management * Added instance name support via "DisplayName" property in OpenStack * Added a VM and VM_talk namespace; 498 and 499 respectively (500+ is for nova project namespaces)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78479 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/OpenStackManager/OpenStackNovaProject.php
@@ -0,0 +1,222 @@
+<?php
+
+class OpenStackNovaProject {
+
+	var $projectname;
+	var $projectDN;
+	var $projectInfo;
+
+	function __construct( $projectname ) {
+		$this->projectname = $projectname;
+		$this->connect();
+		$this->fetchProjectInfo();
+	}
+
+	function connect() {
+		global $wgAuth;
+		global $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword;
+
+		$wgAuth->connect();
+		$wgAuth->bindAs( $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword );
+	}
+
+	function fetchProjectInfo() {
+		global $wgAuth;
+		global $wgOpenStackManagerLDAPProjectBaseDN;
+		global $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword;
+
+		$result = @ldap_search( $wgAuth->ldapconn, $wgOpenStackManagerLDAPProjectBaseDN, '(&(cn=' . $this->projectname . ')(projectManager=*))' );
+		$this->projectInfo = @ldap_get_entries( $wgAuth->ldapconn, $result );
+		$this->projectDN = $this->projectInfo[0]['dn'];
+	}
+
+	function getProjectName() {
+		return $this->projectname;
+	}
+
+	function getMembers() {
+		$members = array();
+		if ( isset( $this->projectInfo[0]['member'] ) ) {
+			$memberdns = $this->projectInfo[0]['member'];
+			array_shift( $memberdns );
+			foreach ( $memberdns as $memberdn ) {
+				$member = explode( '=', $memberdn );
+				$member = explode( ',', $member[1] );
+				$member = $member[0];
+				$members[] = $member;
+			}
+		}
+
+		return $members;
+	}
+
+	function deleteMember( $username ) {
+		global $wgAuth;
+
+		if ( isset( $this->projectInfo[0]['member'] ) ) {
+			$members = $this->projectInfo[0]['member'];
+			array_shift( $members );
+			$user = new OpenStackNovaUser( $username );
+			if ( ! $user->userDN ) {
+				$wgAuth->printDebug( "Failed to find userDN in deleteMember", NONSENSITIVE );
+				return false;
+			}
+			$index = array_search( $user->userDN, $members );
+			if ( $index === false ) {
+				$wgAuth->printDebug( "Failed to find userDN in member list", NONSENSITIVE );
+				return false;
+			}
+			unset( $members[$index] );
+			$values['member'] = array();
+			foreach ( $members as $member ) {
+				$values['member'][] = $member;
+			}
+			$success = @ldap_modify( $wgAuth->ldapconn, $this->projectDN, $values );
+			if ( $success ) {
+				$wgAuth->printDebug( "Successfully removed $user->userDN from $this->projectDN", NONSENSITIVE );
+				return true;
+			} else {
+				$wgAuth->printDebug( "Failed to remove $user->userDN from $this->projectDN", NONSENSITIVE );
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	function addMember( $username ) {
+		global $wgAuth;
+
+		$members = array();
+		if ( isset( $this->projectInfo[0]['member'] ) ) {
+			$members = $this->projectInfo[0]['member'];
+			array_shift( $members );
+		}
+		$user = new OpenStackNovaUser( $username );
+		if ( ! $user->userDN ) {
+			$wgAuth->printDebug( "Failed to find userDN in addMember", NONSENSITIVE );
+			return false;
+		}
+		$members[] = $user->userDN;
+		$values['member'] = $members;
+		$success = @ldap_modify( $wgAuth->ldapconn, $this->projectDN, $values );
+		if ( $success ) {
+			$wgAuth->printDebug( "Successfully added $user->userDN to $this->projectDN", NONSENSITIVE );
+			return true;
+		} else {
+			$wgAuth->printDebug( "Failed to add $user->userDN to $this->projectDN", NONSENSITIVE );
+			return false;
+		}
+	}
+
+	static function getAllProjects() {
+		global $wgAuth;
+		global $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword;
+		global $wgOpenStackManagerLDAPProjectBaseDN;
+
+		$wgAuth->connect();
+		$wgAuth->bindAs( $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword );
+
+		$projects = array();
+		$result = @ldap_search( $wgAuth->ldapconn, $wgOpenStackManagerLDAPProjectBaseDN, '(projectManager=*)' );
+		if ( $result ) {
+			$entries = @ldap_get_entries( $wgAuth->ldapconn, $result );
+			if ( $entries ) {
+				# First entry is always a count
+				array_shift( $entries );
+				foreach ( $entries as $entry ) {
+					$project = new OpenStackNovaProject( $entry['cn'][0] );
+					array_push( $projects, $project );
+				}
+			}
+		}
+
+		return $projects;
+	}
+
+	static function createProject( $projectname ) {
+		global $wgAuth;
+		global $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword;
+		global $wgOpenStackManagerLDAPProjectBaseDN;
+
+		$wgAuth->connect();
+		$wgAuth->bindAs( $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword );
+
+		$project['objectclass'][] = 'novaproject';
+		$project['objectclass'][] = 'groupofnames';
+		$project['objectclass'][] = 'posixgroup';
+		$project['cn'] = $projectname;
+		$project['projectmanager'] = $wgOpenStackManagerLDAPUser;
+		$project['gidnumber'] = OpenStackNovaUser::getNextIdNumber( $wgAuth, 'gidnumber' );
+		$dn = 'cn=' . $projectname . ',' . $wgOpenStackManagerLDAPProjectBaseDN;
+
+		$success = @ldap_add( $wgAuth->ldapconn, $dn, $project );
+		if ( $success ) {
+			$wgAuth->printDebug( "Successfully added project $projectname", NONSENSITIVE );
+			return true;
+		} else {
+			$wgAuth->printDebug( "Failed to add project $projectname", NONSENSITIVE );
+			return false;
+		}
+	}
+
+	static function deleteProject( $projectname ) {
+		global $wgAuth;
+		global $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword;
+
+
+		$wgAuth->connect();
+		$wgAuth->bindAs( $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword );
+
+		$project = new OpenStackNovaProject( $projectname );
+		if ( ! $project ) {
+			return false;
+		}
+		$dn = $project->projectDN;
+
+		# Projects can have roles as sub entries, delete them first
+		$result = ldap_list( $wgAuth->ldapconn, $dn, 'objectclass=*' );
+		$roles = ldap_get_entries( $wgAuth->ldapconn, $result );
+		array_shift( $roles );
+		foreach ( $roles as $role ) {
+			$success = @ldap_delete( $wgAuth->ldapconn, $role[0]['dn'] );
+			if ( ! $success ) {
+				return false;
+			}
+		}
+		$success = @ldap_delete( $wgAuth->ldapconn, $dn );
+		if ( $success ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	static function addNamespaces() {
+		global $wgAuth;
+		global $wgOpenStackManagerLDAPProjectBaseDN;
+		global $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword;
+		global $wgOpenStackManagerLDAPDomain;
+		global $wgExtraNamespaces;
+
+		$wgAuth->connect( $wgOpenStackManagerLDAPDomain );
+		$wgAuth->bindAs( $wgOpenStackManagerLDAPUser, $wgOpenStackManagerLDAPUserPassword );
+
+		$result = @ldap_search( $wgAuth->ldapconn, $wgOpenStackManagerLDAPProjectBaseDN, 'projectmanager=*' );
+		$entries = ldap_get_entries( $wgAuth->ldapconn, $result );
+		if ( $entries ) {
+			array_shift($entries);
+			foreach ( $entries as $entry ) {
+				$id = (int)$entry['gidnumber'][0];
+				$talkid = $id + 1;
+				$name = ucwords( $entry['cn'][0] );
+				$wgAuth->printDebug( "Adding namespace $name", NONSENSITIVE );
+				$wgExtraNamespaces[$id] = $name;
+				$wgExtraNamespaces[$talkid] = $name . '_talk';
+			}
+		} else {
+			$wgAuth->printDebug( "Failed to find projects", NONSENSITIVE );
+		}
+	}
+
+}
---
+++ extensions/OpenStackManager/SpecialNovaProject.php
@@ -0,0 +1,305 @@
+<?php
+class SpecialNovaProject extends SpecialPage {
+
+	var $userNova, $adminNova;
+
+	function __construct() {
+		parent::__construct( 'NovaProject' );
+
+		global $wgOpenStackManagerNovaAdminKeys;
+
+		$this->userLDAP = new OpenStackNovaUser();
+		$adminCredentials = $wgOpenStackManagerNovaAdminKeys;
+		$this->adminNova = new OpenStackNovaController( $adminCredentials );
+	}
+
+	public function isRestricted() {
+		return true;
+	}
+
+#	public function userCanExecute( $user ) {
+#		global $wgRequest;
+#
+#		#$project = $wgRequest->getVal('project');
+#		#if ( $project && ! $this->userLDAP->inProject( $project ) ) {
+#		#	return false;
+#		#}
+#		return true;
+#	}
+ 
+	function execute( $par ) {
+		global $wgRequest, $wgUser;
+
+		wfLoadExtensionMessages('OpenStackManager');
+
+		#if ( ! $wgUser->isAllowed( 'manageproject' ) ) {
+		#	return false;
+		#}
+		if ( ! $wgUser->isLoggedIn() ) {
+			return false;
+		}
+
+		$action = $wgRequest->getVal('action');
+		if ( $action == "create" ) {
+			$this->createProject();
+		} else if ( $action == "delete" ) {
+			$this->deleteProject();
+		} else if ( $action == "addmember" ) {
+			$this->addMember();
+		} else if ( $action == "deletemember" ) {
+			$this->deleteMember();
+		} else {
+			$this->listProjects();
+		}
+	}
+
+	function notLoggedIn() {
+	        global $wgOut;
+
+	        $this->setHeaders();
+	        $wgOut->setPagetitle("Not logged in");
+	        $wgOut->addHTML('<p>You must be logged in to perform this action</p>');
+	}
+
+	function noCredentials() {
+	        global $wgOut;
+
+	        $this->setHeaders();
+	        $wgOut->setPagetitle("No Nova credentials found for your account");
+	        $wgOut->addHTML('<p>There were no Nova credentials found for your user account. Please ask a Nova administrator to create credentials for you.</p>');
+	}
+
+	function createProject() { 
+		global $wgRequest, $wgOut;
+
+		$this->setHeaders();
+		$wgOut->setPagetitle("Create Project");
+ 
+		$projectInfo = Array(); 
+		$projectInfo['projectName'] = array(
+			'type' => 'text',
+			'label-message' => 'projectname',
+			'default' => '',
+			'section' => 'project/info',
+		);
+
+		$projectInfo['action'] = array(
+			'type' => 'hidden',
+			'default' => 'create',
+		);
+
+		$projectForm = new SpecialNovaProjectForm( $projectInfo, 'novaprojectform' );
+		$projectForm->setTitle( SpecialPage::getTitleFor( 'NovaProject' ));
+		$projectForm->setSubmitID( 'novaproject-form-createprojectsubmit' );
+		$projectForm->setSubmitCallback( array( $this, 'tryCreateSubmit' ) );
+		$projectForm->show();
+
+		return true;
+	}
+
+	function addMember() { 
+		global $wgRequest, $wgOut;
+
+		$this->setHeaders();
+		$wgOut->setPagetitle("Add projectmember");
+ 
+		$project = $wgRequest->getText('projectname');
+		$projectInfo = Array(); 
+		$projectInfo['member'] = array(
+			'type' => 'text',
+			'label-message' => 'member',
+			'default' => '',
+			'section' => 'project/info',
+		);
+		$projectInfo['action'] = array(
+			'type' => 'hidden',
+			'default' => 'addmember',
+		);
+		$projectInfo['projectname'] = array(
+			'type' => 'hidden',
+			'default' => $project,
+		);
+
+		$projectForm = new SpecialNovaProjectForm( $projectInfo, 'novaprojectform' );
+		$projectForm->setTitle( SpecialPage::getTitleFor( 'NovaProject' ));
+		$projectForm->setSubmitID( 'novaproject-form-addmembersubmit' );
+		$projectForm->setSubmitCallback( array( $this, 'tryAddMemberSubmit' ) );
+		$projectForm->show();
+
+		return true;
+	}
+
+	function deleteMember() { 
+		global $wgRequest, $wgOut;
+
+		$this->setHeaders();
+		$wgOut->setPagetitle("Remove project member");
+
+		$member = $wgRequest->getText('member'); 
+		$project = $wgRequest->getText('projectname');
+		if ( ! $wgRequest->wasPosted() ) {
+			$out .= Html::element( 'p', array(), 'Are you sure you wish to remove ' . $member . ' from project ' . $project );
+			$wgOut->addHTML( $out );
+		}
+		$projectInfo = Array(); 
+		$projectInfo['member'] = array(
+			'type' => 'hidden',
+			'default' => $member,
+		);
+		$projectInfo['action'] = array(
+			'type' => 'hidden',
+			'default' => 'deletemember',
+		);
+		$projectInfo['projectname'] = array(
+			'type' => 'hidden',
+			'default' => $project,
+		);
+
+		$projectForm = new SpecialNovaProjectForm( $projectInfo, 'novaprojectform' );
+		$projectForm->setTitle( SpecialPage::getTitleFor( 'NovaProject' ));
+		$projectForm->setSubmitID( 'novaproject-form-deletemembersubmit' );
+		$projectForm->setSubmitCallback( array( $this, 'tryDeleteMemberSubmit' ) );
+		$projectForm->show();
+
+		return true;
+	}
+
+	function deleteProject() {
+		global $wgOut, $wgRequest;
+
+		$this->setHeaders();
+		$wgOut->setPagetitle("Delete project");
+
+		$project = $wgRequest->getText('projectname');
+		if ( ! $wgRequest->wasPosted() ) {
+			$out .= Html::element( 'p', array(), 'Are you sure you wish to delete project "' . $project . '"? This action has reprecusions on all VMs. Do not take this action lightly!' );
+			$wgOut->addHTML( $out );
+		}
+		$projectInfo = Array(); 
+		$projectInfo['projectname'] = array(
+			'type' => 'hidden',
+			'default' => $project,
+		);
+		$projectInfo['action'] = array(
+			'type' => 'hidden',
+			'default' => 'delete',
+		);
+		$projectForm = new SpecialNovaProjectForm( $projectInfo, 'novaproject-form' );
+		$projectForm->setTitle( SpecialPage::getTitleFor( 'NovaProject' ));
+		$projectForm->setSubmitID( 'novaproject-form-deleteprojectsubmit' );
+		$projectForm->setSubmitCallback( array( $this, 'tryDeleteSubmit' ) );
+		$projectForm->setSubmitText( 'confirm' );
+		$projectForm->show();
+
+		return true;
+	}
+
+	function listProjects() {
+		global $wgOut, $wgUser;
+
+		$this->setHeaders();
+		$wgOut->setPagetitle("Project list");
+
+		$out = '';
+		$sk = $wgUser->getSkin();
+		$out .= $sk->link( $this->getTitle(), 'Create a new project', array(), array( 'action' => 'create' ), array() );
+                $projectsOut = Html::element( 'th', array(), 'Project name' );
+                $projectsOut .= Html::element( 'th', array(), 'Members' );
+                $projectsOut .= Html::element( 'th', array(), 'Action' );
+		$projects = OpenStackNovaProject::getAllProjects();
+		foreach ( $projects as $project ) {
+			$projectName = $project->getProjectName();
+			$projectOut = Html::element( 'td', array(), $projectName );
+			$projectMembers = $project->getMembers();
+			$memberOut = '';
+			foreach ( $projectMembers as $projectMember ) {
+				$link = $sk->link( $this->getTitle(), 'remove member', array(), array( 'action' => 'deletemember', 'projectname' => $projectName, 'member' => $projectMember ), array() );
+				$projectMemberOut = htmlentities( $projectMember) . ' (' . $link . ')';
+				$memberOut .= Html::rawElement( 'li', array(), $projectMemberOut );
+			}
+			$memberOut .= '<br />';
+			$memberOut .= $sk->link( $this->getTitle(), 'Add a member', array(), array( 'action' => 'addmember', 'projectname' => $projectName ), array() );
+			$membersOut = Html::rawElement( 'ul', array(), $memberOut );
+			$projectOut .= Html::rawElement( 'td', array(), $membersOut );
+			$link = $sk->link( $this->getTitle(), 'delete project', array(), array( 'action' => 'delete', 'projectname' => $projectName ), array() );
+			$projectOut .= Html::rawElement( 'td', array(), $link );
+			$projectsOut .= Html::rawElement( 'tr', array(), $projectOut );
+		}
+		$out .= Html::rawElement( 'table', array( 'class' => 'wikitable' ), $projectsOut );
+
+		$wgOut->addHTML( $out );
+	}
+
+	function tryCreateSubmit( $formData, $entryPoint = 'internal' ) {
+		global $wgOut, $wgUser;
+
+		$success = OpenStackNovaProject::createProject( $formData['projectName'] );
+		if ( ! $success ) {
+			$out = Html::element( 'p', array(), 'Failed to create project' );
+			return false;
+		}
+		$out = Html::element( 'p', array(), 'Created project' );
+		$out .= '<br />';
+		$sk = $wgUser->getSkin();
+		$out .= $sk->link( $this->getTitle(), 'Back to project list', array(), array(), array() );
+		$wgOut->addHTML( $out );
+
+		return true;
+	}
+
+	function tryDeleteSubmit( $formData, $entryPoint = 'internal' ) {
+		global $wgOut, $wgUser;
+
+		$success = OpenStackNovaProject::deleteProject( $formData['projectname'] );
+		if ( $success ) {
+			$out = Html::element( 'p', array(), 'Successfully deleted project' );
+		} else {
+			$out = Html::element( 'p', array(), 'Failed to delete project' );
+		}
+		$out .= '<br />';
+		$sk = $wgUser->getSkin();
+		$out .= $sk->link( $this->getTitle(), 'Back to project list', array(), array(), array() );
+		$wgOut->addHTML( $out );
+
+		return true;
+	}
+
+	function tryAddMemberSubmit( $formData, $entryPoint = 'internal' ) {
+		global $wgOut, $wgUser;
+
+		$project = new OpenStackNovaProject( $formData['projectname'] );
+		$success = $project->addMember( $formData['member'] );
+		if ( $success ) {
+			$out = Html::element( 'p', array(), 'Successfully added ' . $formData['member'] . ' to ' . $formData['projectname'] );
+		} else {
+			$out = Html::element( 'p', array(), 'Failed to add ' . $formData['member'] . ' to ' . $formData['projectname'] );
+		}
+		$out .= '<br />';
+		$sk = $wgUser->getSkin();
+		$out .= $sk->link( $this->getTitle(), 'Back to project list', array(), array(), array() );
+		$wgOut->addHTML( $out );
+
+		return true;
+	}
+
+	function tryDeleteMemberSubmit( $formData, $entryPoint = 'internal' ) {
+		global $wgOut, $wgUser;
+
+		$project = new OpenStackNovaProject( $formData['projectname'] );
+		$success = $project->deleteMember( $formData['member'] );
+		if ( $success ) {
+			$out = Html::element( 'p', array(), 'Successfully removed ' . $formData['member'] . ' from ' . $formData['projectname'] );
+		} else {
+			$out = Html::element( 'p', array(), 'Failed to remove ' . $formData['member'] . ' from ' . $formData['projectname'] );
+		}
+		$out .= '<br />';
+		$sk = $wgUser->getSkin();
+		$out .= $sk->link( $this->getTitle(), 'Back to project list', array(), array(), array() );
+		$wgOut->addHTML( $out );
+
+		return true;
+	}
+}
+
+class SpecialNovaProjectForm extends HTMLForm {
+}
