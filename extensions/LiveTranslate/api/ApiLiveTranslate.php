3c64286cb161d1508fd256b27385ee33b93ece35
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-16 12:47:00 +0000

    Updated INSTALL and created API module
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78485 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LiveTranslate/api/ApiLiveTranslate.php
+++ extensions/LiveTranslate/api/ApiLiveTranslate.php
@@ -17,9 +17,94 @@ class ApiLiveTranslate extends ApiBase {
 	}
 	
 	public function execute() {
+		$params = $this->extractRequestParams();
 		
+		// In MW 1.17 and above ApiBase::PARAM_REQUIRED can be used, this is for b/c with 1.16.
+		foreach ( array( 'from', 'to', 'words' ) as $requiredParam ) {
+			if ( !isset( $params[$requiredParam] ) ) {
+				$this->dieUsageMsg( array( 'missingparam', $requiredParam ) );
+			}			
+		}
+		
+		$dbr = wfGetDB( DB_SLAVE );
+		
+		foreach ( array_unique( $params['words'] ) as $word ) {
+			$source = $dbr->selectRow(
+				'live_translate',
+				array( 'word_id' ),
+				array(
+					'word_language' => $params['from'],
+					'word_translation' => $word
+				)
+			);
+			
+			if ( $source ) {
+				$destination = $dbr->selectRow(
+					'live_translate',
+					array( 'word_translation' ),
+					array(
+						'word_language' => $params['to'],
+						'word_id' => $source->word_id
+					)
+				);
+
+				if ( $destination ) {
+					$this->getResult()->addValue(
+						'translations',
+						$word,
+						$destination->word_translation
+					);						
+				}
+			}
+		}
 	}
 
+	public function getAllowedParams() {
+		return array(
+			'from' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				//ApiBase::PARAM_REQUIRED => true,
+			),
+			'to' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				//ApiBase::PARAM_REQUIRED => true,
+			),
+			'words' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_ISMULTI => true,
+				//ApiBase::PARAM_REQUIRED => true,
+			),					
+		);
+	}
+	
+	public function getParamDescription() {
+		return array(
+			'from' => 'Source language',
+			'to' => 'Destination language',
+			'words' => 'The words to translate. Delimitered by |',
+		);
+	}
+	
+	public function getDescription() {
+		return array(
+			'Returns the available translations of the provided words in the source language in the destiniation language.'
+		);
+	}
+		
+	public function getPossibleErrors() {
+		return array_merge( parent::getPossibleErrors(), array(
+			array( 'missingparam', 'from' ),
+			array( 'missingparam', 'to' ),
+			array( 'missingparam', 'words' ),
+		) );
+	}
+
+	protected function getExamples() {
+		return array(
+			'api.php?action=livetranslate&from=English&to=Dutch&words=In|Your|Example',
+		);
+	}	
+	
 	public function getVersion() {
 		return __CLASS__ . ': $Id$';
 	}		
