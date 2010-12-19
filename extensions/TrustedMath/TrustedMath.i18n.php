a6a9792c4f34ab9183873ea749bb0b85cdf24f68
btongminh <btongminh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-19 18:34:12 +0000

    Add extension to use latex for people who don't like camels.
    Documentation and code comments will follow later. Also need to check what a proper way of namespace initialization is.
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78602 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/TrustedMath/TrustedMath.i18n.php
@@ -0,0 +1,12 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'trustedmath-namespace' => 'Math',
+	'trustedmath-talk-namespace' => 'Math_talk',
+	'trustedmath-permission-error' => 'Untrusted math is disabled on this wiki',
+	'trustedmath-path-error' => 'Error creating the math directory',
+	'trustedmath-convert-error' => 'Error creating math image in $1',
+	
+);
