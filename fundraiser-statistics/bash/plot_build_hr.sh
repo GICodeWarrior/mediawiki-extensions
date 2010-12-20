feb60fc5614053e3444927b9df7acdf398a0116a
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-20 08:55:01 +0000

    plot_build_hr.sh copying to wrong directory - fixed
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@78620 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fundraiser-statistics/bash/plot_build_hr.sh
+++ fundraiser-statistics/bash/plot_build_hr.sh
@@ -4,9 +4,9 @@ cd /home/rfaulk/fundraiser-statistics/fundraiser-scripts
 
 python run_plots.py
 
-cp /home/rfaulk/fundraiser-scripts/report_LP_metrics_don_per_view.png /srv/org.wikimedia.fundraising/stats/
-cp /home/rfaulk/fundraiser-scripts/report_total_amounts_by_day.png /srv/org.wikimedia.fundraising/stats/
-cp /home/rfaulk/fundraiser-scripts/report_banner_metrics_don_per_imp.png /srv/org.wikimedia.fundraising/stats/
-cp /home/rfaulk/fundraiser-scripts/report_total_amounts_by_hr.png /srv/org.wikimedia.fundraising/stats/
-cp /home/rfaulk/fundraiser-scripts/report_banner_metrics_click_rate.png /srv/org.wikimedia.fundraising/stats/
-cp /home/rfaulk/fundraiser-scripts/report_LP_metrics_conversion_rate.png /srv/org.wikimedia.fundraising/stats/
+cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/report_LP_metrics_don_per_view.png /srv/org.wikimedia.fundraising/stats/
+cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/report_total_amounts_by_day.png /srv/org.wikimedia.fundraising/stats/
+cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/report_banner_metrics_don_per_imp.png /srv/org.wikimedia.fundraising/stats/
+cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/report_total_amounts_by_hr.png /srv/org.wikimedia.fundraising/stats/
+cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/report_banner_metrics_click_rate.png /srv/org.wikimedia.fundraising/stats/
+cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/report_LP_metrics_conversion_rate.png /srv/org.wikimedia.fundraising/stats/
