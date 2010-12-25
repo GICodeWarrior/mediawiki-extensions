745acd89fd059b72d4f0980bbeea235b4d15466b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-25 20:55:55 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-12-25 20:33:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79002 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Special404.i18n.php
+++ Special404.i18n.php
@@ -22,3 +22,45 @@ Maybe you would like to look at:
 * [[{{int:mainpage}}|The main page]]',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'special404-desc' => 'Ermöglicht eine benutzerdefinierte Zielseite für den Fall das der HTTP-Fehler „404 Not Found“ auftritt',
+	'error404' => '404 Not Found',
+	'special404-body' => 'Die gewünschte URL wurde nicht gefunden.
+
+Sollte tatsächlich {{fullurl:$1}} aufgerufen werden?
+
+Vielleicht sollte stattdessen folgende Seite aufgerufen werden:
+* [[{{int:mainpage}}|Hauptseite]]',
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Nghtwlkr
+ */
+$messages['no'] = array(
+	'special404-desc' => 'Tilbyr en 404 spesialdestinasjonsside for 404 Ikke funnet-feil',
+	'error404' => '404 Ikke funnet',
+	'special404-body' => 'URLen du søkte ble ikke funnet.
+
+Mente du å skrive {{fullurl:$1}}?
+
+Kanskje du vil se på:
+* [[{{int:mainpage}}|Hovedsiden]]',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'special404-desc' => 'Udostępnia specjalną stronę wyświetlaną przy wystąpieniu błędu 404 oznaczającego brak strony',
+	'error404' => '404 Nie znaleziono',
+	'special404-body' => 'Wybrany adres URL nie istnieje.
+
+Czy na pewno chodziło o {{fullurl:$1}}?
+
+Może chcesz zobaczyć
+* [[{{int:mainpage}}|stronę główną]]',
+);
+
