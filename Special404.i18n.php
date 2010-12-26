6eace9fe869610de05e3dc66907e9d219dcba526
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-26 20:38:54 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-12-26 20:27:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79023 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Special404.i18n.php
+++ Special404.i18n.php
@@ -22,6 +22,20 @@ Maybe you would like to look at:
 * [[{{int:mainpage}}|The main page]]',
 );
 
+/** Breton (Brezhoneg)
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'special404-desc' => 'Pourchas a ra ur bajenn dal 404 dibar evit ar fazioù dianav',
+	'error404' => '404 dianav',
+	'special404-body' => "N'eo ket bet kavet an URL goulennet ganeoc'h.
+
+Ha fellout a rae deoc'h skrivañ {{fullurl:$1}}?
+
+Marteze ho po c'hoant da sellet ouzh :
+* [[{{int:mainpage}}|Ar bajenn degemer]]",
+);
+
 /** German (Deutsch)
  * @author Kghbln
  */
@@ -34,6 +48,77 @@ Sollte tatsächlich {{fullurl:$1}} aufgerufen werden?
 
 Vielleicht sollte stattdessen folgende Seite aufgerufen werden:
 * [[{{int:mainpage}}|Hauptseite]]',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'special404-desc' => 'Proporciona unha páxina especial de destino para os erros "404 Non atopado"',
+	'error404' => '404 Non atopado',
+	'special404-body' => 'Non se puido atopar o enderezo URL solititado.
+
+Quizais quixo escribir {{fullurl:$1}}?
+
+Seica quere botar un ollo á:
+* [[{{int:mainpage}}|Portada]]',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'special404-desc' => 'Procura un pagina special de destination pro le errores "404 Non trovate"',
+	'error404' => '404 Non trovate',
+	'special404-body' => 'Le URL que tu requestava non esseva trovate.
+
+Voleva tu scriber {{fullurl:$1}}?
+
+Si non, tu pote consultar:
+* [[{{int:mainpage}}|Le pagina principal]]',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'special404-desc' => 'Menyediakan halaman khusus 404 untuk galat 404 Tidak ditemukan',
+	'error404' => '404 Tidak ditemukan',
+	'special404-body' => 'URL yang diminta tidak ditemukan. 
+
+Apakah Anda bermaksud mengetik {{fullurl:$1}}? 
+
+Mungkin Anda ingin melihat: 
+* [[{{int:mainpage}}|Halaman utama]]',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'special404-desc' => 'Erméiglecht eng 404 Spezialzilsäit fir de Fall wou de Feeler "404 Net fonnt" geschitt',
+	'error404' => '404 Net fonnt',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'special404-desc' => 'Овозможува одредница за специјална страница за грешки од видот „404 не е пронајденo“',
+	'error404' => '404 не е пронајденo',
+	'special404-body' => 'URL-адресата што ја побаравте не е пронајдена.
+
+Да не мислевте на {{fullurl:$1}}?
+
+Можеби би сакале да ја погледате:
+* [[{{int:mainpage}}|Главната страница]]',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'error404' => '404 Niet gevonden',
 );
 
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
@@ -62,5 +147,19 @@ Czy na pewno chodziło o {{fullurl:$1}}?
 
 Może chcesz zobaczyć
 * [[{{int:mainpage}}|stronę główną]]',
+);
+
+/** Russian (Русский)
+ * @author Byulent
+ */
+$messages['ru'] = array(
+	'special404-desc' => 'Предоставляет специальную страницу "404" для ошибок "404 Not Found"',
+	'error404' => '404 Не найдено',
+	'special404-body' => 'Запрошенный URL-адрес не найден.
+
+Вы хотели написать {{fullurl:$1}}?
+
+Может быть, вы бы хотели посмотреть:
+* [[{{int:mainpage}}|The Main page]]',
 );
 
