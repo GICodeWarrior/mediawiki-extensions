fcda9512e408068b2cff4228ec0c9314c57a1723
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-27 14:24:22 +0000

    * Use MediaWiki: instead of int: for main page link. * Tweak extension description
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79060 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Special404.i18n.php
+++ Special404.i18n.php
@@ -12,14 +12,14 @@ $messages = array();
  * @author Daniel Friesen
  */
 $messages['en'] = array(
-	'special404-desc' => 'Provides a 404 special page destination for 404 Not found errors',
+	'special404-desc' => 'Provides a destination for 404 Not found errors',
 	'error404' => '404 Not found',
 	'special404-body' => 'The URL you requested was not found.
 
 Did you mean to type {{fullurl:$1}}?
 
 Maybe you would like to look at:
-* [[{{int:mainpage}}|The main page]]',
+* [[{{MediaWiki:Mainpage}}|The main page]]',
 );
 
 /** Breton (Brezhoneg)
