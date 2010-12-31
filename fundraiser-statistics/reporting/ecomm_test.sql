1d8014adef482dee190cdd3aa1bf02f4dac2835b
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-12-31 17:46:29 +0000

    added banners and amt100 to the reporting
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79357 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fundraiser-statistics/reporting/ecomm_test.sql
+++ fundraiser-statistics/reporting/ecomm_test.sql
@@ -1,11 +1,13 @@
 
 select 
+SUBSTRING_index(substring_index(utm_source, '.', 2),'.',1) as banner,
 SUBSTRING_index(substring_index(utm_source, '.', 2),'.',-1) as landing_page,
 utm_campaign,
 count(*) as total_clicks,
 sum(not isnull(contribution_tracking.contribution_id)) as donations,
 sum(converted_amount) AS amount,
 sum(if(converted_amount > 50, 50, converted_amount)) as amount50,
+sum(if(converted_amount > 100, 100, converted_amount)) as amount100,
 -- sum(if(converted_amount > 100, 100, converted_amount)) AS modified_amount, -- truncates donations over 100 
 max(converted_amount) AS max_amt,
 sum(if(right(utm_source,2)='cc',1,0))  as cc_clicks,
@@ -19,7 +21,7 @@ drupal.contribution_tracking LEFT JOIN civicrm.public_reporting
 ON (contribution_tracking.contribution_id = civicrm.public_reporting.contribution_id)
 where ts >=  '%s' and ts < '%s' 
 and (utm_campaign REGEXP '%s')
-group by 1,2
+group by 1,2,3
 
 
 
