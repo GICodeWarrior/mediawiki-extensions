e44b1f9684ef7c82dcb5ea393b2ebe37185f3775
btongminh <btongminh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-01 19:54:47 +0000

    Fixed the extension to a working state, except that stuff in the Math namespace is not rendered on view. * Added comments and license * Cleanup temporary files * Kill globals in TrustedMath class
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79436 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- TrustedMath.i18n.php
+++ TrustedMath.i18n.php
@@ -7,6 +7,6 @@ $messages['en'] = array(
 	'trustedmath-talk-namespace' => 'Math_talk',
 	'trustedmath-permission-error' => 'Untrusted math is disabled on this wiki',
 	'trustedmath-path-error' => 'Error creating the math directory',
-	'trustedmath-convert-error' => 'Error creating math image in $1',
+	'trustedmath-convert-error' => 'Error creating math image. "$1" returned <tt><nowiki>$2</nowiki></tt>.',
 	
 );
