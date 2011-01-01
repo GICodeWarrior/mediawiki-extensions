6324ed0c1526a3040987d244eb04276b574a14aa
btongminh <btongminh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-01 19:54:47 +0000

    Fixed the extension to a working state, except that stuff in the Math namespace is not rendered on view. * Added comments and license * Cleanup temporary files * Kill globals in TrustedMath class
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79436 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/TrustedMath/TrustedMathHooks.php
+++ extensions/TrustedMath/TrustedMathHooks.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ * Class to hook the TrustedMath extension into MediaWiki
+ * @author Bryan
+ *
+ */
 class TrustedMathHooks {
-	
+	/**
+	 * TODO fix
+	 */
 	public static function onParserAfterStrip( $parser, &$text, &$stripState ) {
 		global $wgTrustedMathNamespace;
 		
@@ -13,11 +20,14 @@ class TrustedMathHooks {
 		return true;
 	}
 	
-	
+	/**
+	 * Initialize globals to their default values. Hooked by $wgExtensionFunctions.
+	 */
 	public static function initGlobals() {
 		global $wgTrustedMathDirectory, $wgTrustedMathPath;
 		global $wgUploadDirectory, $wgUploadPath;
 		
+		// Use the MediaWiki default path
 		if ( is_null( $wgTrustedMathDirectory ) ) {
 			$wgTrustedMathDirectory = "$wgUploadDirectory/math";
 		}
@@ -25,9 +35,13 @@ class TrustedMathHooks {
 			$wgTrustedMathPath = "$wgUploadPath/math";
 		}
 		
+		// Initialize the namespace for TrustedMath
 		self::initNamespace();
 	}
 	
+	/**
+	 * TODO: check, fix, etc.
+	 */
 	public static function initNamespace() {
 		global $wgExtraNamespaces, $wgNamespaceProtection;
 		global $wgTrustedMathNamespace;
@@ -46,61 +60,107 @@ class TrustedMathHooks {
 		}
 	}
 	
+	/**
+	 * Add the math tag as parser hook
+	 * @param Parser $parser
+	 */
 	public static function onParserFirstCallInit( $parser ) {
 		$parser->setHook( 'math',  __CLASS__ . '::mathTag' );
 		return true;
 	}
 	
+	/**
+	 * Render a <math> tag
+	 * 
+	 * @param string $input
+	 * @param array $args
+	 * @param Parser $parser
+	 * @param mixed $frame
+	 */
 	public static function mathTag( $input, $args, $parser, $frame ) {
 		global $wgTrustedMathNamespace;
 		
 		if ( isset( $args['name'] ) ) {
+			// Safe mode
+			
 			$title = Title::newFromText( $args['name'], $wgTrustedMathNamespace );
 			if ( !self::validateSafeMode( $title ) ) {
+				// Requested title was not in the math namespace
 				return self::getPermissionError( $parser );
 			}
 			
 			$math = TrustedMath::newFromTitle( $title );
 		} else {
-			global $wgTitle; // eeeeeewh
+			// Raw math mode
 			
-			if ( !self::validateSafeMode( $wgTitle ) ) {
+			if ( !self::validateSafeMode( $parser->getTitle() ) ) {
+				// Raw math mode not allowed
 				return self::getPermissionError( $parser );
 			}
 			
 			$math = TrustedMath::newFromText( $input );
 		}
 		
+		// Setup and render
+		self::setupMathRenderer( $math );
 		$status = $math->render();
 		
 		if ( $status->isOk() ) {
 			global $wgTrustedMathPath;
 			
+			// Wrap a the rendered image in an <img> tag
 			return Html::element( 'img', array( 
 				'src' => "{$wgTrustedMathPath}/{$status->value}",
 				'class' => 'tex',
 				'alt' => $math->getText(),
 			) );
 		} else {
+			
+			// Return the error to the user
 			return $parser->recursiveTagParse( '<span class="error">' . 
 				$status->getWikiText() . '</span>', $frame );
 		}
-		
 	}
 	
+	/**
+	 * Check if the content of $title is considered safe
+	 * @param Title $title
+	 * @return bool True if safe
+	 */
 	protected static function validateSafeMode( $title ) {
 		global $wgTrustedMathNamespace;
 		return !( self::safeMode() && 
 			$title->getNamespace() != $wgTrustedMathNamespace );
 	}
-	
+	/**
+	 * Check if unsafe math is allowed
+	 * @return bool True if unsafe math is disallowed
+	 */
 	protected static function safeMode() {
 		global $wgTrustedMathUnsafeMode;
 		return !$wgTrustedMathUnsafeMode;
 	}
 	
+	/**
+	 * Return an HTML fragment to indicate that the user is doing something disallowed
+	 * @param Parser $parser
+	 */
 	protected static function getPermissionError( $parser ) {
 		return $parser->recursiveTagParse( '<span class="error">' . 
 			wfMsg( 'trustedmath-permission-error' ) . '</span>' );
 	}
+	
+	/**
+	 * Initialize the math renderer
+	 * @param TrustedMath $math
+	 */
+	protected static function setupMathRenderer( $math ) {
+		global $wgTrustedMathLatexPath, $wgTrustedMathDviPngPath,
+			$wgTrustedMathEnvironment, $wgTrustedMathDirectory;
+			
+		$math->setRenderers( $wgTrustedMathLatexPath, $wgTrustedMathDviPngPath );
+		$math->setEnvironment( $wgTrustedMathEnvironment );
+		$math->setBasePath( $wgTrustedMathDirectory );				
+	}
 }
+
