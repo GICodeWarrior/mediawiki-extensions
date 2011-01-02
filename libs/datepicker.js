43726baa7484e4510bd60466905f555b65987fb4
foxtrott <foxtrott@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-02 15:16:13 +0000

    registration of initialisation/validation methods for datepicker and regexp
    * Initialisation for datepicker put into its own JS file and registered with SF
    * Validation for regexp registered with SF
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79493 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- libs/datepicker.js
+++ libs/datepicker.js
@@ -6,6 +6,86 @@
  *
  */
 
+function SFI_DP_init ( input_id, params ) {
+
+	var input = jQuery("#" + input_id + "_show");
+
+	input.datepicker( {
+		"showOn": "both", 
+		"buttonImage": params.buttonImage,
+		"buttonImageOnly": false, 
+		"changeMonth": true, 
+		"changeYear": true, 
+		"altFormat": "yy/mm/dd", 
+		// Today button does not work (http://dev.jqueryui.com/ticket/4045)
+		// do not show button panel for now
+		// TODO: show date picker button panel when bug is fixed
+		"showButtonPanel": false, 
+		"firstDay": params.firstDay,
+		"showWeek": params.showWeek,
+		"dateFormat": params.dateFormat,
+		"altField": "#" + input_id,
+		"beforeShowDay": function (date) {return SFI_DP_checkDate("#" + input_id + "_show", date);}
+	} );
+
+	if ( params.minDate ) {
+		input.datepicker( "option", "minDate",
+			jQuery.datepicker.parseDate("yy/mm/dd", params.minDate, null) );
+	}
+
+	if ( params.maxDate ) {
+		input.datepicker( "option", "maxDate",
+			jQuery.datepicker.parseDate("yy/mm/dd", params.maxDate, null) );
+	}
+
+	if ( params.userClass ) {
+		input.datepicker("widget").addClass( params.userClass );
+		jQuery("#" + input_id + "_show + button").addClass( params.userClass );
+	}
+
+	if ( params.disabledDates ) {
+
+		var disabledDates = params.disabledDates.map(function(range) {
+			return [new Date(range[0], range[1], range[2]), new Date(range[3], range[4], range[5])]
+		});
+
+		input.datepicker("option", "disabledDates", disabledDates);
+	}
+
+	if ( params.highlightedDates ) {
+
+		var highlightedDates = params.highlightedDates.map(function(range) {
+			return [new Date(range[0], range[1], range[2]), new Date(range[3], range[4], range[5])]
+		});
+
+		input.datepicker("option", "highlightedDates", highlightedDates);
+	}
+
+	if (params.disabledDays) {
+		input.datepicker("option", "disabledDays", params.disabledDays);
+	}
+
+	if (params.highlightedDays) {
+		input.datepicker("option", "highlightedDays", params.highlightedDays);
+	}
+
+	//input.datepicker("option", "beforeShowDay", function (date) {return SFI_DP_checkDate("#" + input_id + "_show", date);});
+
+	try {
+
+		var re = /\d{4}\/\d{2}\/\d{2}/
+		if ( ! re.test( params.currValue ) ) {
+			throw "Wrong date format!";
+		}
+		input.datepicker( "setDate", jQuery.datepicker.parseDate( "yy/mm/dd", params.currValue, null ) );
+		
+	} catch (e) {
+		input.attr( "value", params.currValue );
+		jQuery( "#" + input_id).attr( "value", params.currValue );
+	}
+
+}
+
 /**
  * Checks a date if it is to be enabled or highlighted
  *
@@ -18,9 +98,9 @@
  */
 function SFI_DP_checkDate( input, date ) {
 
-	enable = true
+	var enable = true
 
-	disabledDates = jQuery( input ).datepicker( "option", "disabledDates" );
+	var disabledDates = jQuery( input ).datepicker( "option", "disabledDates" );
 
 	if ( disabledDates ) {
 		for ( i = 0; i < disabledDates.length; ++i ) {
@@ -31,17 +111,17 @@ function SFI_DP_checkDate( input, date ) {
 		}
 	}
 
-	disabledDays = jQuery( input ).datepicker( "option", "disabledDays" );
+	var disabledDays = jQuery( input ).datepicker( "option", "disabledDays" );
 
 	if ( disabledDays ) {
 		enable = enable && !disabledDays[ date.getDay() ];
 	}
 
-	highlightedDates = jQuery( input ).datepicker( "option", "highlightedDates" );
-	highlight = "";
+	var highlightedDates = jQuery( input ).datepicker( "option", "highlightedDates" );
+	var highlight = "";
 
 	if ( highlightedDates ) {
-		for ( i = 0; i < highlightedDates.length; ++i ) {
+		for ( var i = 0; i < highlightedDates.length; ++i ) {
 			if ( ( date >= highlightedDates[i][0] ) && ( date <= highlightedDates[i][1] ) ) {
 				highlight = "ui-state-highlight";
 				break;
@@ -49,7 +129,7 @@ function SFI_DP_checkDate( input, date ) {
 		}
 	}
 
-	highlightedDays = jQuery( input ).datepicker( "option", "highlightedDays" );
+	var highlightedDays = jQuery( input ).datepicker( "option", "highlightedDays" );
 
 	if ( highlightedDays ) {
 		if ( highlightedDays[ date.getDay() ] ) {
