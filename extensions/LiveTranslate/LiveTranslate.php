c5984561048d5c2ed607f1493e9838e50cf8a88d
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-06 01:22:49 +0000

    Fixed wrong version number and updated release notes
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79698 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LiveTranslate/LiveTranslate.php
+++ extensions/LiveTranslate/LiveTranslate.php
@@ -25,7 +25,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define( 'LiveTranslate_VERSION', '0.2 alpha' );
+define( 'LiveTranslate_VERSION', '0.3 alpha' );
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
