fb37af5b5a58ff8cbd67ee345d60b5d99308a78a
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-06 01:43:05 +0000

    Documentation
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79700 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ui/CodeRepoListView.php
+++ ui/CodeRepoListView.php
@@ -18,6 +18,11 @@ class CodeRepoListView {
 		$wgOut->addWikiText( $text );
 	}
 
+	/**
+	 * @static
+	 * @param  $repo CodeRepository
+	 * @return string
+	 */
 	public static function getNavItem( $repo ) {
 		global $wgLang, $wgUser;
 		$name = $repo->getName();
