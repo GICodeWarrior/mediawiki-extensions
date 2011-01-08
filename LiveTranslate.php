dfba7b85d3b2e72f291c3041eeceefcaf0a71319
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-08 00:07:07 +0000

    Added Google branding thinghy and incremented version number
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79852 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- LiveTranslate.php
+++ LiveTranslate.php
@@ -25,7 +25,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define( 'LiveTranslate_VERSION', '0.3' );
+define( 'LiveTranslate_VERSION', '0.4 alpha' );
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
