aa191818adf8710c4f24dfa32a200bbd4e56ea66
hartman <hartman@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-10 22:30:49 +0000

    Update extensions to use the new style getLongDesc() of r79964
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@79968 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiAtHome.i18n.php
+++ WikiAtHome.i18n.php
@@ -20,11 +20,11 @@ $messages['en'] = array(
 	'wah-short-video' => '$1 video file, $2',
 	'wah-short-general' => '$1 media file, $2',
 
-	'wah-long-audio' => '($1 sound file, length $2, $3)',
-	'wah-long-video' => '($1 video file, length $2, $4×$5 pixels, $3)',
-	'wah-long-multiplexed' => '(multiplexed audio/video file, $1, length $2, $4×$5 pixels, $3 overall)',
-	'wah-long-general' => '(media file, length $2, $3)',
-	'wah-long-error' => '(ffmpeg could not read this file: $1)',
+	'wah-long-audio' => '$1 sound file, length $2, $3',
+	'wah-long-video' => '$1 video file, length $2, $4×$5 pixels, $3',
+	'wah-long-multiplexed' => 'multiplexed audio/video file, $1, length $2, $4×$5 pixels, $3 overall',
+	'wah-long-general' => 'media file, length $2, $3',
+	'wah-long-error' => 'ffmpeg could not read this file: $1',
 
 	'wah-transcode-working' => 'This video is being processed, please try again later',
 	'wah-transcode-helpout' => 'You can help transcode this video by visiting [[Special:WikiAtHome|Wiki@Home]].',
