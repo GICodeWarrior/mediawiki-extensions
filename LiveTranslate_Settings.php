74239ebe001503dfdae531092abe9a72f9c7d54b
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-11 21:13:45 +0000

    Removed
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80040 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- LiveTranslate_Settings.php
+++ LiveTranslate_Settings.php
@@ -32,7 +32,3 @@ $wgGroupPermissions['sysop']['managetms'] = true;
 # Default translation memory type.
 # TMT_LTF, TMT_TMX, TMT_GCSV
 $egLiveTranslateTMT = TMT_LTF;
-
-# The name of the page on which the special words translations dirctionary is defined.
-# NOTICE: This is only used for the initial setup. Use Special:LiveTranslate to modify after installation.
-$egLiveTranslateDirPage = 'Live Translate Dictionary';
