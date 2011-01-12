43fcac11c81edb649061125c3d7d3ac5dd4676b7
laner <laner@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-12 23:38:23 +0000

    Adding in some backend support for security groups
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80132 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/OpenStackManager/OpenStackNovaSecurityGroup.php
@@ -0,0 +1,33 @@
+<?php
+
+# TODO: Make this an abstract class, and make the EC2 API a subclass
+class OpenStackNovaSecurityGroup {
+
+	var $group;
+	var $rules;
+
+	function __construct( $apiInstanceResponse ) {
+		$this->group = $apiInstanceResponse;
+		$this->rules = array();
+		foreach ( $this->group->ipPermissions as $permission ) {
+			$this->rules[] = new OpenStackNovaSecurityGroupRule( $permission );
+		}
+	}
+
+	function getGroupName() {
+		return $this->group->groupName;
+	}
+
+	function getGroupDescription() {
+		return $this->group->groupDescription;
+	}
+
+	function getOwner() {
+		return $this->group->ownerId;
+	}
+
+	function getRules() {
+		return $this->rules;
+	}
+
+}
