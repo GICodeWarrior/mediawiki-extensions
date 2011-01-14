f555d8ba4af6bad8872af4946b19b276f567a21d
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-14 20:32:39 +0000

    ...l
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80317 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wp-photocommons/js/search.js
+++ tools/wp-photocommons/js/search.js
@@ -1,122 +1,124 @@
-window.log = function(a){
-	console.log(a);
-}
-
+// Debug
+window.log = function( a, b ) {
+	console.log( a, b );
+};
 if ( !window.Photocommons ) {
 	window.Photocommons = {};
 }
 
-(function($) {
-$.extend( Photocommons, {
-	
-	getQueryUrl: function( type, args ) {
-		var queries = {
-			'pagesearch' : function(q) {
-				return {
-					'action' : 'opensearch',
-					'search' : q.search
-				};
-			},
-/* http://commons.wikimedia.org/w/api.php
-	? action=query
-	& generator=images
-	& gimlimit=20
-	& indexpageids=1
-	& titles=Cat
-	& redirects=1
-	& prop=imageinfo
-	& iiprop=url
-	& iiurlwidth=200		
-*/	
-			'pageimages' : function(q) {
-				return {
-					'action' : 'query',
-					'generator' : 'images',
-					'gimlimit' : '20',
-					'indexpageids' : '1',
-					'titles' : q.title,
-					'redirects' : '1',
-					'prop' : 'imageinfo',
-					'iiprop' : 'url',
-					'iiurlwidth' : q.width,
-				};
+(function($){
+
+	$.extend( Photocommons, {
+
+		getQueryUrl: function( type, args ) {
+			var queries = {
+				'pagesearch' : function(q) {
+					return {
+						'action' : 'opensearch',
+						'search' : q.search
+					};
+				},
+				/* http://commons.wikimedia.org/w/api.php
+					? action=query
+					& generator=images
+					& gimlimit=20
+					& indexpageids=1
+					& titles=Cat
+					& redirects=1
+					& prop=imageinfo
+					& iiprop=url
+					& iiurlwidth=200
+				*/
+				'pageimages' : function(q) {
+					return {
+						'action' : 'query',
+						'generator' : 'images',
+						'gimlimit' : '20',
+						'indexpageids' : '1',
+						'titles' : q.title,
+						'redirects' : '1',
+						'prop' : 'imageinfo',
+						'iiprop' : 'url',
+						'iiurlwidth' : q.width
+					};
+				}
+			};
+
+			if (!queries[type]) {
+				throw new Error( 'Unknown query type' );
 			}
-		};
-		
-		if (!queries[type]) {
-			throw new Error( 'Unknown query type' );
-		}
-		
-		return Photocommons.makeUrl(queries[type](args));
-	},
-	makeUrl: function( args ) {
-		// default arguments
-		args = $.extend({
-			'format' : 'json',
-			'callback' : '!noencode!?'
-		}, args);
-		
-		var url = 'http://commons.wikimedia.org/w/api.php';
-		var first = true;
-		for (var key in args) {
-			var value = args[key];
-			url += (first) ? '?' : '&';
-			first = false;
-		
-			if (value.indexOf( '!noencode!' ) === 0 && typeof value === 'string' ) {
-				value = value.slice(10);
-			} else {
-				value = encodeURIComponent(value);
-		}
-		
-		url += key + '=' + value;
-		}
-		return url;
-	},
-	init: function() {
 
-		$( '#wp-photocommons-search' ).autocomplete({
-			source : function(request, response) {
-				var url = Photocommons.getQueryUrl( 'pagesearch', {
-					'search' : $( '#wp-photocommons-search' ).val()
-				});
-			
-				$.getJSON(url, function(data) {
-					response(data[1]);
-				});
-			},
-		
-			select : function(event, ui) {
-				$( '#wp-photocommons-images' ).empty();
-				$( '#wp-photocommons-loading' ).show();
-				
-				var url = Photocommons.getQueryUrl( 'pageimages', {
-					'title' : ui.item.value,
-					'width' : '200'
-				});
-				
-				$.getJSON(url, function(data) {
+			return Photocommons.makeUrl(queries[type](args));
+		},
+		makeUrl: function( args ) {
+			// default arguments
+			args = $.extend({
+				'format' : 'json',
+				'callback' : '!noencode!?'
+			}, args);
 
-					if ( !data.query.pageids.length ) {
-						$( '#wp-photocommons-images' ).html( 'No images found :(' );
-					} else {
-						$.each(data.query.pageids, function(key,pageid){
-							var img = data.query.pages[pageid];
-							if ( img.imageinfo && img.imageinfo[0] ) {
-								$( '#wp-photocommons-images' ).append( '<img src="' + img.imageinfo[0].thumburl + '" style="display:none;"/>' ).find( 'img' ).fadeIn();
+			var url = 'http://commons.wikimedia.org/w/api.php';
+			var first = true;
+			for (var key in args) {
+				var value = args[key];
+				url += (first) ? '?' : '&';
+				first = false;
 
-							}
-						})					
-					
-					}				
-					
-					$( '#wp-photocommons-loading' ).hide();
-				});
+				if (value.indexOf( '!noencode!' ) === 0 && typeof value === 'string' ) {
+					value = value.slice(10);
+				} else {
+					value = encodeURIComponent(value);
 			}
-		});
-	}
-});
 
-// Init
-$( document ).ready( Photocommons.init );
+			url += key + '=' + value;
+			}
+			return url;
+		},
+		init: function() {
+
+			$( '#wp-photocommons-search' ).autocomplete({
+				source : function(request, response) {
+					var url = Photocommons.getQueryUrl( 'pagesearch', {
+						'search' : $( '#wp-photocommons-search' ).val()
+					});
+
+					$.getJSON(url, function(data) {
+						response(data[1]);
+					});
+				},
+
+				select : function(event, ui) {
+					$( '#wp-photocommons-images' ).empty();
+					$( '#wp-photocommons-loading' ).show();
+
+					var url = Photocommons.getQueryUrl( 'pageimages', {
+						'title' : ui.item.value,
+						'width' : '200'
+					});
+
+					$.getJSON(url, function(data) {
+
+						if ( !data.query.pageids.length ) {
+							$( '#wp-photocommons-images' ).html( 'No images found :(' );
+						} else {
+							$.each(data.query.pageids, function(key,pageid){
+								var img = data.query.pages[pageid];
+								if ( img.imageinfo && img.imageinfo[0] ) {
+									$( '#wp-photocommons-images' ).append( '<img src="' + img.imageinfo[0].thumburl + '" style="display:none;"/>' ).find( 'img' ).fadeIn();
+
+								}
+							})
+
+						}
+
+						$( '#wp-photocommons-loading' ).hide();
+					});
+				}
+			});
+		}
+	});
+
+	// Init
+	$( document ).ready( Photocommons.init );
+
 })(jQuery);
