b714ccc43824d81985039b8b10912767e1edc639
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-14 14:36:40 +0000

    Version 0.7
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80266 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Push.php
+++ Push.php
@@ -25,7 +25,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define( 'Push_VERSION', '0.7 alpha' );
+define( 'Push_VERSION', '0.7' );
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
