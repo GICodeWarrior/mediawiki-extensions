e21f3f3501e96a5563e5a7ee886b9ae6735bd793
btongminh <btongminh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-14 11:22:22 +0000

    Framework for the fancy uploader
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80251 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ tools/fancy-uploader/fancy-uploader.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class FancyUploader(object):
+
+    def __init__(self):
+        pass
+
+    def convert_json_to_wikitext(self, json):
+        pass
+
+    def upload(self, data):
+        pass
+
