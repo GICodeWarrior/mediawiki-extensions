5c4051e880cc30b081fcaacd1674434a105ec67d
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-14 19:19:44 +0000

    extendn instead of replace
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80300 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wp-photocommons/search.js
+++ tools/wp-photocommons/search.js
@@ -1,117 +1,118 @@
 // Debug
 window.log = (console && console.log) ? console.log : function(){};
 
-if ( typeof Photocommons == 'undefined' ) {
+if (!window.Photocommons) window.Photocommons = {};
 
-	window.Photocommons = {
-		
-		getQueryUrl: function( type, args ) {
-			var queries = {
-				'pagesearch' : function(q) {
-					return {
-						'action' : 'opensearch',
-						'search' : q.search
-					};
-				},
-				
-				'pageimages' : function(q) {
-					return {
-						'action' : 'query',
-						'prop' : 'images',
-						'indexpageids' : '1',
-						'titles' : q.title
-					};
-				},
-				
-				'thumbs' : function(q) {
-					return {
-						'action' : 'query',
-						'prop' : 'imageinfo',
-						'iiprop' : 'url',
-						'iiurlwidth' : q.width,
-						'indexpageids' : '1',
-						'titles' : q.image
-					};
-				}
-			};
-			
-			if (!queries[type]) {
-				throw new Error('Unknown query type');
-			}
-			
-			return Photocommons.makeUrl(queries[type](args));
-		},
-		makeUrl: function( args ) {
-			// default arguments
-			args = $.extend({
-				'format' : 'json',
-				'callback' : '!noencode!?'
-			}, args);
-			
-			var url = 'http://commons.wikimedia.org/w/api.php';
-			var first = true;
-			for (var key in args) {
-				var value = args[key];
-				url += (first) ? '?' : '&';
-				first = false;
-			
-				if (value.indexOf('!noencode!') === 0 && typeof value === 'string') {
-					value = value.slice(10);
-				} else {
-					value = encodeURIComponent(value);
-			}
-			
-			url += key + '=' + value;
-			}
-			return url;
-		},
-		init: function() {
+$.extend( Photocommons, {
 	
-			$('#search').autocomplete({
-				source : function(request, response) {
-					var url = Photocommons.getQueryUrl('pagesearch', {
-						'search' : $('#search').val()
-					});
-				
-					$.getJSON(url, function(data) {
-						response(data[1]);
-					});
-				},
+	getQueryUrl: function( type, args ) {
+		var queries = {
+			'pagesearch' : function(q) {
+				return {
+					'action' : 'opensearch',
+					'search' : q.search
+				};
+			},
 			
-				select : function(event, ui) {
-					$('#images').empty();
-					$('#loading').show();
+			'pageimages' : function(q) {
+				return {
+					'action' : 'query',
+					'prop' : 'images',
+					'indexpageids' : '1',
+					'titles' : q.title
+				};
+			},
+			
+			'thumbs' : function(q) {
+				return {
+					'action' : 'query',
+					'prop' : 'imageinfo',
+					'iiprop' : 'url',
+					'iiurlwidth' : q.width,
+					'indexpageids' : '1',
+					'titles' : q.image
+				};
+			}
+		};
+		
+		if (!queries[type]) {
+			throw new Error('Unknown query type');
+		}
+		
+		return Photocommons.makeUrl(queries[type](args));
+	},
+	makeUrl: function( args ) {
+		// default arguments
+		args = $.extend({
+			'format' : 'json',
+			'callback' : '!noencode!?'
+		}, args);
+		
+		var url = 'http://commons.wikimedia.org/w/api.php';
+		var first = true;
+		for (var key in args) {
+			var value = args[key];
+			url += (first) ? '?' : '&';
+			first = false;
+		
+			if (value.indexOf('!noencode!') === 0 && typeof value === 'string') {
+				value = value.slice(10);
+			} else {
+				value = encodeURIComponent(value);
+		}
+		
+		url += key + '=' + value;
+		}
+		return url;
+	},
+	init: function() {
+
+		$('#search').autocomplete({
+			source : function(request, response) {
+				var url = Photocommons.getQueryUrl('pagesearch', {
+					'search' : $('#search').val()
+				});
+			
+				$.getJSON(url, function(data) {
+					response(data[1]);
+				});
+			},
+		
+			select : function(event, ui) {
+				$('#images').empty();
+				$('#loading').show();
+				
+				var url = Photocommons.getQueryUrl('pageimages', {
+					'title' : ui.item.value
+				});
+				
+				$.getJSON(url, function(data) {
+					var pageid = data.query.pageids[0],
+					query = data.query.pages[pageid].images;
 					
-					var url = Photocommons.getQueryUrl('pageimages', {
-						'title' : ui.item.value
-					});
+					if (!query) {
+						$('#images').html('No images found :(');
+					}
 					
-					$.getJSON(url, function(data) {
-						var pageid = data.query.pageids[0],
-						query = data.query.pages[pageid].images;
-						
-						if (!query) {
-							$('#images').html('No images found :(');
-						}
-						
-						$.each(query, function() {
-							var url = Photocommons.getQueryUrl('thumbs', {
-								width : '200',
-								image : this.title
-							});
-							
-							$.getJSON(url, function(data) {
-								var pageid = data.query.pageids[0],
-								src = data.query.pages[pageid].imageinfo[0].thumburl;
-								$('#images').append('<img src="' + src + '" style="display:none;"/>').find('img').fadeIn();
-							});
+					$.each(query, function() {
+						var url = Photocommons.getQueryUrl('thumbs', {
+							width : '200',
+							image : this.title
 						});
 						
-						$('#loading').hide();
+						$.getJSON(url, function(data) {
+							var pageid = data.query.pageids[0],
+							src = data.query.pages[pageid].imageinfo[0].thumburl;
+							$('#images').append('<img src="' + src + '" style="display:none;"/>').find('img').fadeIn();
+						});
 					});
-				}
-			});
-		}
+					
+					$('#loading').hide();
+				});
+			}
+		});
 	}
-	$( document ).ready( Photocommons.init );
-}
+});
+
+// Init
+$( document ).ready( Photocommons.init );
