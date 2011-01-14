fd88b1a0cccb099718bc66166071446da60c3bcd
diederik <diederik@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-14 20:21:10 +0000

    Configuration uses Singleton pattern to prevent multiple instances.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80312 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/editor_trends/configuration.py
+++ tools/editor_trends/configuration.py
@@ -43,7 +43,23 @@ except ImportError:
     pass
 
 
+class Singleton(type):
+    '''
+    Recipe: http://stackoverflow.com/questions/31875/is-there-a-simple-elegant-way-to-define-singletons-in-python
+    '''
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None
+
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+            return cls.instance
+        else:
+            return cls.instance
+
 class Settings(object):
+        __metaclass__ = Singleton
 
         def __init__(self, debug=True, process_multiplier=1, **kwargs):
             self.debug = debug
