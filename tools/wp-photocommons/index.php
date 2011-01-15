b33de6c9855ed4234cfc57fb2df61534183f5706
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-15 12:32:05 +0000

    meta
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80391 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wp-photocommons/index.php
+++ tools/wp-photocommons/index.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Photocommons
-Plugin URI: http://www.mediawiki.org/wiki/Photocommons
+Plugin Name: PhotoCommons
+Plugin URI: http://www.mediawiki.org/wiki/PhotoCommons
 Description: Search and add free images from Wikimedia Commons directly in your blog 
-Author: Husky (Hay Kranen), Krinkle
+Author: Hay Kranen, Timo Tijhof
 Version: 1.0
-Author URI: http://www.mediawiki.org/wiki/Photocommons
+Author URI: http://www.mediawiki.org/wiki/PhotoCommons
 */
 
 require 'inc/class-photocommons.php';
