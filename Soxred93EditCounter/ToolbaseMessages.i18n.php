e746223b8c0106493cdfe4f6e45059fd4aadf24b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-18 20:33:58 +0000

    Follow-up r80510: Update to current coding standards (spaced -> tabs) etc. Header, constructing of the arrays, Some language tweaks for consistency.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80513 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Soxred93EditCounter/ToolbaseMessages.i18n.php
+++ Soxred93EditCounter/ToolbaseMessages.i18n.php
@@ -1,60 +1,48 @@
 <?php
+$messages = array();
 
-$messages = array (
-  'en' => 
-  array (
-    'toolbase-header-title' => 'X!\'s Tools',
-    'toolbase-header-bugs' => 'Bugs',
-    'toolbase-header-twitter' => 'Twitter',
-    'toolbase-header-sitenotice' => 'Global Toolserver Sitenotice: $1',
-    
-    'toolbase-replag' => 'Server lagged by $1',
-    'toolbase-replag-years' => 'years',
-    'toolbase-replag-months' => 'months',
-    'toolbase-replag-weeks' => 'weeks',
-    'toolbase-replag-days' => 'days',
-    'toolbase-replag-hours' => 'hours',
-    'toolbase-replag-minutes' => 'minutes',
-    'toolbase-replag-seconds' => 'seconds',
-    
-    'toolbase-footer-exectime' => 'Executed in $1 seconds',
-    'toolbase-footer-source' => 'View Source',
-    'toolbase-footer-language' => 'Change language',
-    
-    'toolbase-navigation' => 'Navigation',
-    'toolbase-navigation-homepage' => 'Homepage',
-    'toolbase-navigation-api' => 'API',
-    
-    'toolbase-userid-submit' => 'Get user ID',
-    'toolbase-userid-title' => 'Find a User ID',
-    'toolbase-userid-result' => 'The user ID for <b>$1</b> is <b>$2</b>.',
-    
-    
-    'toolbase-main-title' => 'Welcome!',
-    'toolbase-main-content' => 'Welcome to X!\'s tools! The tool suite is still in the process of being converted to the <a href="$1">Symfony</a> framework. This process will take a while, but it should be working now. 
+/** English
+ * @author soxred93
+ */
+$messages['en'] => array (
+	'toolbase-header-title' => "X!'s Tools",
+	'toolbase-header-bugs' => 'Bugs',
+	'toolbase-header-twitter' => 'Twitter',
+	'toolbase-header-sitenotice' => 'Global Toolserver Sitenotice: $1',
+
+	'toolbase-replag' => 'Server lagged by $1',
+	'toolbase-replag-years' => 'years',
+	'toolbase-replag-months' => 'months',
+	'toolbase-replag-weeks' => 'weeks',
+	'toolbase-replag-days' => 'days',
+	'toolbase-replag-hours' => 'hours',
+	'toolbase-replag-minutes' => 'minutes',
+	'toolbase-replag-seconds' => 'seconds',
+	
+	'toolbase-footer-exectime' => 'Executed in $1 seconds',
+	'toolbase-footer-source' => 'View source',
+	'toolbase-footer-language' => 'Change language',
+	
+	'toolbase-navigation' => 'Navigation',
+	'toolbase-navigation-homepage' => 'Homepage',
+	'toolbase-navigation-api' => 'API',
+	
+	'toolbase-userid-submit' => 'Get user ID',
+	'toolbase-userid-title' => 'Find a user ID',
+	'toolbase-userid-result' => 'The user ID for <b>$1</b> is <b>$2</b>.',
+
+	'toolbase-main-title' => 'Welcome!',
+	'toolbase-main-content' => 'Welcome to X!\'s tools! The tool suite is still in the process of being converted to the <a href="$1">Symfony</a> framework. This process will take a while, but it should be working now. 
 
 For a list of tools that are currently running right now on this framework, see the sidebar to the right.
 
-Bugs can be reported at <a href="$2">Google Code</a>.
-
-
-',
-    'toolbase-main-404' => 'File not found',
-    'toolbase-main-404-content' => '
-Oops! No page was found!
+Bugs can be reported at <a href="$2">Google Code</a>.',
+	'toolbase-main-404' => 'File not found',
+	'toolbase-main-404-content' => 'Oops! No page was found!
 
 Make sure that you typed the URL correctly.
 If you followed a link from somewhere, please <a href="$1">report a bug</a>.
-</ul>
-
-',
-
-
-    'toolbase-form-wiki' => 'Wiki',
-    
-    
-    'toolbase-error-nouser' => '$1 is not a valid user',
-    
-    
-  ),
+</ul>',
+	'toolbase-form-wiki' => 'Wiki',
+	'toolbase-error-nouser' => '$1 is not a valid user',	
 );
