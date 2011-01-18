cbf314216a4dc84def784f8216103ff8e539fdef
soxred93 <soxred93@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-18 19:34:27 +0000

    Updating to most recent i18n for Soxred93s Tools
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80510 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ToolserverTools/Soxred93EditCounter/ToolbaseMessages.i18n.php
+++ extensions/ToolserverTools/Soxred93EditCounter/ToolbaseMessages.i18n.php
@@ -3,46 +3,58 @@
 $messages = array (
   'en' => 
   array (
-    'soxredtoolbase-1' => 'X!\'s Tools',
-    'soxredtoolbase-2' => 'Bugs',
-    'soxredtoolbase-3' => 'Twitter',
-    'soxredtoolbase-4' => 'Global Toolserver Sitenotice:',
-    'soxredtoolbase-5' => 'Server lagged by ',
-    'soxredtoolbase-6' => 'years',
-    'soxredtoolbase-7' => 'months',
-    'soxredtoolbase-8' => 'weeks',
-    'soxredtoolbase-9' => 'days',
-    'soxredtoolbase-10' => 'hours',
-    'soxredtoolbase-11' => 'minutes',
-    'soxredtoolbase-12' => 'seconds',
-    'soxredtoolbase-13' => 'Executed in $1 seconds',
-    'soxredtoolbase-14' => 'Navigation',
-    'soxredtoolbase-15' => 'Homepage',
-    'soxredtoolbase-16' => 'Get user ID',
-    'soxredtoolbase-17' => 'API',
-    'soxredtoolbase-18' => 'View Source',
-    'soxredtoolbase-19' => 'Wiki',
-    'soxredtoolbase-20' => '$1 is not a valid user',
-    'soxredtoolbase-21' => 'Find a User ID',
-    'soxredtoolbase-22' => 'The user ID for <b>$1</b> is <b>$2</b>.',
-    'soxredtoolbase-23' => 'Change language',
-    'soxredtoolbase-24' => 'Welcome!',
-    'soxredtoolbase-25' => 'Welcome to X!\'s tools! The tool suite is still in the process of being converted to the <a href="\\$1">Symfony</a> framework. This process will take a while, but it should be working now. 
+    'toolbase-header-title' => 'X!\'s Tools',
+    'toolbase-header-bugs' => 'Bugs',
+    'toolbase-header-twitter' => 'Twitter',
+    'toolbase-header-sitenotice' => 'Global Toolserver Sitenotice: $1',
+    
+    'toolbase-replag' => 'Server lagged by $1',
+    'toolbase-replag-years' => 'years',
+    'toolbase-replag-months' => 'months',
+    'toolbase-replag-weeks' => 'weeks',
+    'toolbase-replag-days' => 'days',
+    'toolbase-replag-hours' => 'hours',
+    'toolbase-replag-minutes' => 'minutes',
+    'toolbase-replag-seconds' => 'seconds',
+    
+    'toolbase-footer-exectime' => 'Executed in $1 seconds',
+    'toolbase-footer-source' => 'View Source',
+    'toolbase-footer-language' => 'Change language',
+    
+    'toolbase-navigation' => 'Navigation',
+    'toolbase-navigation-homepage' => 'Homepage',
+    'toolbase-navigation-api' => 'API',
+    
+    'toolbase-userid-submit' => 'Get user ID',
+    'toolbase-userid-title' => 'Find a User ID',
+    'toolbase-userid-result' => 'The user ID for <b>$1</b> is <b>$2</b>.',
+    
+    
+    'toolbase-main-title' => 'Welcome!',
+    'toolbase-main-content' => 'Welcome to X!\'s tools! The tool suite is still in the process of being converted to the <a href="$1">Symfony</a> framework. This process will take a while, but it should be working now. 
 
 For a list of tools that are currently running right now on this framework, see the sidebar to the right.
 
-Bugs can be reported at <a href="\\$2">Google Code</a>.
+Bugs can be reported at <a href="$2">Google Code</a>.
 
 
 ',
-    'soxredtoolbase-26' => 'File not found',
-    'soxredtoolbase-27' => '
+    'toolbase-main-404' => 'File not found',
+    'toolbase-main-404-content' => '
 Oops! No page was found!
 
 Make sure that you typed the URL correctly.
-If you followed a link from somewhere, please <a href="\\$1">report a bug</a>.
+If you followed a link from somewhere, please <a href="$1">report a bug</a>.
 </ul>
 
 ',
+
+
+    'toolbase-form-wiki' => 'Wiki',
+    
+    
+    'toolbase-error-nouser' => '$1 is not a valid user',
+    
+    
   ),
 );
