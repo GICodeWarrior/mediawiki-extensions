b8d30e7563cf60a6fe9a9b8796bbc1364f2ccc68
soxred93 <soxred93@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-18 17:34:11 +0000

    Add new message file.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80501 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Soxred93EditCounter/ToolbaseMessages.i18n.php
@@ -0,0 +1,50 @@
+<?php
+
+$messages = array (
+  'en' => 
+  array (
+    'soxredtoolbase-1' => 'Find a User ID',
+    'soxredtoolbase-2' => 'Wiki',
+    'soxredtoolbase-3' => 'Get user ID',
+    'soxredtoolbase-4' => 'X!\'s Tools',
+    'soxredtoolbase-5' => 'Bugs',
+    'soxredtoolbase-6' => 'Twitter',
+    'soxredtoolbase-7' => 'Executed in %count% seconds',
+    'soxredtoolbase-8' => 'Navigation',
+    'soxredtoolbase-9' => 'Homepage',
+    'soxredtoolbase-10' => 'View Source',
+    'soxredtoolbase-11' => 'Change language',
+    'soxredtoolbase-12' => 'The user ID for <b>$1</b> is <b>$2</b>.',
+    'soxredtoolbase-13' => 'Global Toolserver Sitenotice:',
+    'soxredtoolbase-14' => 'Server lagged by ',
+    'soxredtoolbase-15' => 'years',
+    'soxredtoolbase-16' => 'months',
+    'soxredtoolbase-17' => 'weeks',
+    'soxredtoolbase-18' => 'days',
+    'soxredtoolbase-19' => 'hours',
+    'soxredtoolbase-20' => 'minutes',
+    'soxredtoolbase-21' => 'seconds',
+    'soxredtoolbase-22' => '%name% is not a valid user',
+    'soxredtoolbase-23' => 'The user ID for <b>%name%</b> is <b>%id%</b>.',
+    'soxredtoolbase-24' => 'Welcome!',
+    'soxredtoolbase-25' => 'Welcome to X!\'s tools! The tool suite is still in the process of being converted to the <a href="\\$1">Symfony</a> framework. This process will take a while, but it should be working now. 
+
+For a list of tools that are currently running right now on this framework, see the sidebar to the right.
+
+Bugs can be reported at <a href="\\$2">Google Code</a>.
+
+
+',
+    'soxredtoolbase-26' => 'File not found',
+    'soxredtoolbase-27' => '
+Oops! No page was found!
+
+Make sure that you typed the URL correctly.
+If you followed a link from somewhere, please <a href="\\$1">report a bug</a>.
+</ul>
+
+',
+    'soxredtoolbase-28' => 'API',
+    'soxredtoolbase-29' => '$1 is not a valid user',
+  ),
+);
