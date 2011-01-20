8c4195e856ded17b5822083a255520c7fb4bd66b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-20 20:52:42 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-01-20 20:24:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80647 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SubPageList3.i18n.php
+++ SubPageList3.i18n.php
@@ -553,10 +553,11 @@ $messages['zh-hans'] = array(
 
 /** Traditional Chinese (‪中文(繁體)‬)
  * @author Liangent
+ * @author Mark85296341
  * @author Wrightbus
  */
 $messages['zh-hant'] = array(
-	'spl3_debug' => '不明的選項$1的設定值',
-	'spl3_nosubpages' => '$1沒有子頁面來顯示',
+	'spl3_debug' => '不明的選項 $1 的設定值',
+	'spl3_nosubpages' => '$1 沒有子頁面來顯示',
 );
 
