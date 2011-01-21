006841f34eb52a22db9a122d0eb4d1746052e31c
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-21 01:20:51 +0000

    re-adding Debug window.log (removed by accident in merge)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80674 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wp-photocommons/js/search.js
+++ tools/wp-photocommons/js/search.js
@@ -1,6 +1,11 @@
-// Debug
-window.log = function( a, b ) {
-	//console.log( a, b );
+// usage: log('inside coolFunc',this,arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+	log.history = log.history || [];   // store logs to an array for reference
+	log.history.push(arguments);
+	if(this.console){
+		console.log( Array.prototype.slice.call(arguments) );
+	}
 };
 
 if ( !window.PhotoCommons ) {
