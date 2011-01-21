fdedd62a109e13e49509a1c011a66ef8e7d02d64
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-21 23:10:07 +0000

    Importing FolkTagCloud extension from mediawiki.org wiki after adding i18n file and stylizing the thing
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80725 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/FolkTagCloud/FolkTagCloud.i18n.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Internationalization file for the FolkTagCloud extension.
+ *
+ * @file FolkTagCloud.i18n.php
+ * @ingroup FolkTagCloud
+ *
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'folktagcloud-desc' => 'Adds an SMW property for tagging wiki pages with user-defined labels and a parser tag for displaying them in a tag cloud.',
+);
+
