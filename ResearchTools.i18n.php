0e19d3d2cb5deb0534aea421799721cfe27f4c5a
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-22 20:08:34 +0000

    Localisation updates for extensions from translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80761 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ResearchTools.i18n.php
+++ ResearchTools.i18n.php
@@ -19,3 +19,28 @@ $messages['en'] = array(
 	'researchtools-page-clicks' => 'Click Tracking',
 	'researchtools-page-prefs' => 'Preference Statistics',
 );
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'researchtools' => 'Alat Penelitian',
+	'researchtools-desc' => 'Kumpulan alat yang berguna untuk melakukan kajian penelitian pengguna',
+	'researchtools-page-dashboard' => 'Dasbor',
+	'researchtools-page-surveys' => 'Tanggapan Survei',
+	'researchtools-page-clicks' => 'Pelacakan Klik',
+	'researchtools-page-prefs' => 'Statistik Preferensi',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'researchtools' => 'Onderzoekshulpmiddelen',
+	'researchtools-desc' => 'Verzameling hulpmiddelen die handig zijn voor het uitvoeren van gebruikersstudies',
+	'researchtools-page-dashboard' => 'Dashboard',
+	'researchtools-page-surveys' => 'Vragenlijstresultaten',
+	'researchtools-page-clicks' => 'Kliks bijhouden',
+	'researchtools-page-prefs' => 'Voorkeurenstatistieken',
+);
+
