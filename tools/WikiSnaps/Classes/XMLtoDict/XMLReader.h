30aa9e8aa49cf853a37c96cc1febb0047c5b2f3f
hartman <hartman@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-23 23:03:15 +0000

    Add XML to NSDictionary parser.
    Licensed MIT. Made by Troy Brant and adapted by Benoit Caccinolo
    
    http://troybrant.net/blog/2010/09/simple-xml-to-nsdictionary-converter/
    https://github.com/bcaccinolo/XML-to-NSDictionary/tree/
    
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80838 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ tools/WikiSnaps/Classes/XMLtoDict/XMLReader.h
@@ -0,0 +1,19 @@
+//
+//  XMLReader.h
+//
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface XMLReader : NSObject
+{
+    NSMutableArray *dictionaryStack;
+    NSMutableString *textInProgress;
+    NSError **errorPointer;
+}
+
++ (NSDictionary *)dictionaryForXMLData:(NSData *)data error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLString:(NSString *)string error:(NSError **)errorPointer;
+
+@end
