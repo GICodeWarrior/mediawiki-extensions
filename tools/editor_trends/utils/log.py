9f4d5cbedca8d93430a19e0520ae65601b96a761
diederik <diederik@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-24 17:00:09 +0000

    Generic log functionality that can log to Mongo and / or CSV file.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@80868 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/editor_trends/utils/log.py
+++ tools/editor_trends/utils/log.py
@@ -13,12 +13,13 @@ http://www.fsf.org/licenses/gpl.html
 '''
 
 __author__ = '''\n'''.join(['Diederik van Liere (dvanliere@gmail.com)', ])
-__author__email = 'dvanliere at gmail dot com'
+__email__ = 'dvanliere at gmail dot com'
 __date__ = '2011-01-11'
 __version__ = '0.1'
 
-
+import datetime
 import sys
+import progressbar
 sys.path.append('..')
 
 import configuration
@@ -26,10 +27,146 @@ settings = configuration.Settings()
 
 from database import db
 
-def log_to_mongo(project, task, timer, type='start'):
+def log_to_mongo(properties, jobtype, task, timer, event='start'):
     conn = db.init_mongo_db('wikilytics')
+    created = datetime.datetime.now()
+    hash = '%s_%s' % (properties.project, properties.hash)
     coll = conn['jobs']
-    if type == 'start':
-        coll.save({'project': project, 'tasks': {task: {'start': timer.t0, 'in_progress': True}}})
-    if type == 'finish':
-        coll.save({'project': project, 'tasks': {task: {'finish': timer.t1, 'in_progress': False}}})
+
+    job = coll.find_one({'hash': hash})
+
+    if job == None:
+        if jobtype == 'dataset':
+            _id = coll.save({'hash': hash, 'created': created, 'finished': False,
+                             'language_code': properties.language_code,
+                             'project': properties.short_project,
+                             'in_progress': True, 'jobtype': jobtype,
+                             'tasks': {}})
+
+
+        elif jobtype == 'chart':
+            _id = coll.save({'hash': hash, 'created': created,
+                             'jobtype': jobtype,
+                             'project': properties.project,
+                             'language_code': properties.language_code,
+                             'tasks': {}})
+
+        job = coll.find_one({'_id': _id})
+
+    tasks = job['tasks']
+    t = tasks.get(task, {})
+    if event == 'start':
+        t['start'] = timer.t0
+        t['in_progress'] = True
+        tasks[task] = t
+        coll.update({'hash': hash}, {'$set': {'tasks': tasks}})
+    elif event == 'finish':
+        t['finish'] = timer.t1
+        t['in_progress'] = False
+        tasks[task] = t
+        if task == 'transform' or jobtype == 'chart': #final task, set entire task to finished
+            coll.update({'hash': hash}, {'$set': {'tasks': tasks,
+                                                  'in_progress': False,
+                                                  'finished': True}})
+        else:
+            coll.update({'hash': hash}, {'$set': {'tasks': tasks}})
+
+
+def log_to_csv(logger, settings, **kwargs):
+    '''
+    Writes detailed log information to logs / projectname_date.csv
+    '''
+    message = kwargs.pop('message')
+    verb = kwargs.pop('verb')
+    function = kwargs.pop('function')
+    logger.debug('%s\tStarting %s' \
+        % (datetime.datetime.now(), function.func_name))
+    if message:
+        logger.debug('%s\t%s' % (datetime.datetime.now(), message))
+
+    max_length = max([len(prop) for prop in kwargs if type(prop) != type(True)])
+    max_tabs = max_length // settings.tab_width
+    res = max_length % settings.tab_width
+    if res > 0:
+        max_tabs += 1
+    pos = max_tabs * settings.tab_width
+    for prop in kwargs:
+        if verb:
+            logger.debug('%s\tAction: %s\tSetting: %s' \
+                % (datetime.datetime.now(), verb, kwargs[prop]))
+        else:
+            tabs = (pos - len(prop)) // settings.tab_width
+            res = len(prop) % settings.tab_width
+            if res > 0 or tabs == 0:
+                tabs += 1
+            tabs = ''.join(['\t' for t in xrange(tabs)])
+            logger.debug('%s\t\tKey: %s%sSetting: %s' \
+                % (datetime.datetime.now(),
+                   prop,
+                   tabs,
+                   kwargs[prop]))
+
+
+def init_progressbar_widgets(description):
+    widgets = ['%s: ' % description, progressbar.Percentage(), ' ',
+               progressbar.Bar(marker=progressbar.RotatingMarker()), ' ',
+               progressbar.ETA(), ' ', progressbar.FileTransferSpeed()]
+    return widgets
+
+
+# error tracking related functions
+def track_errors(xml_buffer, error, file, messages):
+    text = extract_offending_string(xml_buffer.getvalue(), error)
+
+    vars = {}
+    vars['file'] = file
+    vars['error'] = error
+    vars['text'] = text
+    #print file, error, text
+    key = remove_error_specific_information(error)
+    if key not in messages:
+        messages[key] = {}
+    if messages[key] == {}:
+        c = 0
+    else:
+        counters = messages[key].keys()
+        counters.sort()
+        counters.reverse()
+        c = counters[-1]
+
+    messages[key][c] = {}
+    for var in vars:
+        messages[key][c][var] = vars[var]
+
+    return messages
+
+
+def report_error_messages(messages, function):
+    store_object(messages, settings.log_location, function.func_name)
+    errors = messages.keys()
+    for error in errors:
+        for key, value in messages[error].iteritems():
+            print error, key, value
+
+
+def remove_error_specific_information(e):
+    pos = e.args[0].find('line')
+    if pos > -1:
+        return e.args[0][:pos]
+    else:
+        return e.args[0]
+
+
+def extract_offending_string(text, error):
+    '''
+    This function determines the string that causes an error when feeding it to
+    the XML parser. This is only useful for debugging purposes.
+    '''
+    location = re.findall(RE_ERROR_LOCATION, error.args[0])
+    if location != []:
+        location = int(location[0]) - 1
+        text = text.split('\n')[location]
+        text = text.decode('utf-8')
+        return text
+    else:
+        return ''
