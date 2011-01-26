9134f895c4a97075f03753072d9e862d6b8fbcbc
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-26 22:01:54 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-01-26 21:38:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81054 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- TrustedMath.i18n.php
+++ TrustedMath.i18n.php
@@ -18,3 +18,38 @@ $messages['en'] = array(
 	'trustedmath-convert-error' => 'Error creating math image. "$1" returned <tt><nowiki>$2</nowiki></tt>.',
 	'trustedmath-not-found' => 'Page [[:$1]] does not exist',
 );
+
+/** French (Français)
+ * @author IAlex
+ * @author Zetud
+ */
+$messages['fr'] = array(
+	'trustedmath-desc' => "Permet d'intégrer des équations LaTeX sur les pages, comme dans le moteur de rendu intégré de mathématiques, mais sans la dépendance d'Ocaml",
+	'trustedmath-permission-error' => 'Les mathématiques non fiables sont désactivées sur ce wiki',
+	'trustedmath-path-error' => 'Erreur de création du répertoire math',
+	'trustedmath-convert-error' => "Erreur de création de l'image des mathématiques. « $1 » a retourné <nowiki><tt>$2</tt></nowiki>.",
+	'trustedmath-not-found' => "La page [[:$1]] n'existe pas",
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'trustedmath-desc' => 'Zmóžnja zasadźenje LaTeX-runicow, podobnje zatwarjenemu math-rysowarjej, ale bjez wotwisnosće wot Ocaml',
+	'trustedmath-permission-error' => 'Njedowěryhódny math bu na tutym wikiju znjemóžnjeny',
+	'trustedmath-path-error' => 'Zmylk při wutworjenju zapisa math',
+	'trustedmath-convert-error' => 'Wobraz při wutworjenju wobraza math: "$1" wróći <tt><nowiki>$2</nowiki></tt>',
+	'trustedmath-not-found' => 'Strona [[:$1]] njeeksistuje',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'trustedmath-desc' => 'Permite incorporar nas páginas equações LaTeX, de forma semelhante à do compositor matemático já incorporado na aplicação, mas sem dependência do Ocaml',
+	'trustedmath-permission-error' => 'Nesta wiki não foi possibilitada matemática não confiável',
+	'trustedmath-path-error' => 'Ocorreu um erro na criação do directório math',
+	'trustedmath-convert-error' => 'Ocorreu um erro durante a criação da imagem. "$1" devolveu <tt><nowiki>$2</nowiki></tt>.',
+	'trustedmath-not-found' => 'A página [[:$1]] não existe',
+);
+
