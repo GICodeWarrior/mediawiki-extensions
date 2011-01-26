2fc5d553316dfc0c32631e71d081201b94835498
liangent <liangent@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-26 13:46:45 +0000

    New extension: EmailUsers
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81021 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ EmailUsers.i18n.php
@@ -0,0 +1,18 @@
+<?php
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+    die();
+}
+
+$messages = array();
+
+$messages['en'] = array(
+    'emailusers-desc' => 'Allowing users to send mails to multiple recipients',
+    
+    'emailusers' => 'E-mail users',
+    'emailusers-target-reverse' => 'Send to all users except for selected ones',
+    
+    'emailusers-norecipient' => 'There is no one you can send mail to.',
+    'emailusers-norecipientselected' => 'No recipient is selected.',
+    'emailusers-toomanyrecipientsselected' => 'Too many recipients are selected.',
+);
