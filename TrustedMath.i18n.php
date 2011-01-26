8a2476891be48398aff9c7f1daa9d36cbe0c931b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-26 11:21:32 +0000

    Add description message Add extension to Translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81019 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- TrustedMath.i18n.php
+++ TrustedMath.i18n.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * Internationalisation file for extension TrustedMath.
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
 $messages = array();
 
+/** English
+ * @author Bryan Tong Minh
+ */
 $messages['en'] = array(
+	'trustedmath-desc' => 'Allows embedding LaTeX equations on pages, similarly to the built-in math renderer, but without dependency on Ocaml',
 	'trustedmath-permission-error' => 'Untrusted math is disabled on this wiki',
 	'trustedmath-path-error' => 'Error creating the math directory',
 	'trustedmath-convert-error' => 'Error creating math image. "$1" returned <tt><nowiki>$2</nowiki></tt>.',
