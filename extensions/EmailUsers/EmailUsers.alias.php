37f2d6ce749313250c1a5d90346e15ee2f106a55
liangent <liangent@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-01-28 18:23:19 +0000

    Add one parameter to a message and translations in zh-hans
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81145 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/EmailUsers/EmailUsers.alias.php
+++ extensions/EmailUsers/EmailUsers.alias.php
@@ -1,12 +1,19 @@
 <?php
 
 $specialPageAliases = array();
- 
+
 /** English
  * @author Liangent
  */
 $specialPageAliases['en'] = array(
     'EmailUsers' => array( 'EmailUsers' ),
+);
+
+/** Chinese (Simplified)
+ * @author Liangent
+ */
+$specialPageAliases['zh-hans'] = array(
+    'EmailUsers' => array( '电邮多个用户' ),
 );
 
 /**
