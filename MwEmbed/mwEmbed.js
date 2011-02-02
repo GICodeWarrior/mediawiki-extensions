c240e5764dbec171e8bfa100bf202fe558c0240c
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-02 23:02:52 +0000

    Moved MwEmbed to MwEmbedSupport to better reflect the nature of the extension which only supports other mwEmbed extension and has no stand alone purpose. It also better reflects the transition nature of the extension as the set of functionality moves into core components, and libraries are updated to use 'core' mediaWiki javascript calls.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81431 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ MwEmbed/mwEmbed.js
@@ -0,0 +1,9 @@
+/**
+* MwEmbed.js is a meta package for the mwEmbed platform.
+* 
+*  For legacy support, this file invokes the mwEmbed platform with debug mode enabled. 
+*  
+*  This mode has been deprecated, you should instead directly request the debug package
+*  
+*  When using mwEmbed in mediaWiki, this file will never be accessed. 
+*/
