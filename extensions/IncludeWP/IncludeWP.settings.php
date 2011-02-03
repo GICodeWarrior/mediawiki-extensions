6af1f6e626bc428db39b2da748b4a839f3cae41d
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-03 15:09:29 +0000

    Adding simple extension to load Wikipedia articles onto other pages
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81452 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/IncludeWP/IncludeWP.settings.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * File defining the settings for the Include WP extension.
+ * More info can be found at http://www.mediawiki.org/wiki/Extension:Include_WP#Settings
+ *
+ *                          NOTICE:
+ * Changing one of these settings can be done by copieng or cutting it,
+ * and placing it in LocalSettings.php, AFTER the inclusion of this extension.
+ *
+ * @file IncludeWP_Settings.php
+ * @ingroup IncludeWP
+ *
+ * @licence GNU GPL v3
+ *
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
---
+++ extensions/IncludeWP/ext.incwp.js
@@ -0,0 +1,109 @@
+/**
+ * JavasSript for the IncludeWP  extension.
+ * @see http://www.mediawiki.org/wiki/Extension:Push
+ * 
+ * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
+ */
+
+(function($) { $( document ).ready( function() {
+	
+	// Compatibility with pre-RL code.
+	// Messages will have been loaded into wgPushMessages.
+	if ( typeof mediaWiki === 'undefined' ) {
+		mediaWiki = new Object();
+		
+		mediaWiki.msg = function() {
+			message = window.wgIncWPMessages[arguments[0]];
+			
+			for ( var i = arguments.length - 1; i > 0; i-- ) {
+				message = message.replace( '$' + i, arguments[i] );
+			}
+			
+			return message;
+		}
+	}
+	
+	var pages = {};
+	
+	$.each($(".includewp-loading"), function(i,v) {
+		loadPage( $(v) );
+	});		
+	
+	function loadPage( sender ) {
+		$.getJSON(
+			sender.attr( 'wiki' ) + '/api.php?callback=?',
+			{
+				'action': 'query',
+				'format': 'json',
+				'prop': 'revisions',
+				'rvprop':'timestamp|content',
+				'titles': sender.attr( 'page' )
+			},
+			function( data ) {
+				if ( data.query ) {
+					for ( pageWikiID in data.query.pages ) {
+						getPlaintext( sender, data.query.pages[pageWikiID].revisions[0]["*"] );
+						break;
+					}
+				}
+				else {
+					sender.html( '<b>' + mediaWiki.msg( 'includewp-loading-failed' ) + '</b>' );
+				}
+			}
+		);		
+	}
+	
+	function getPlaintext( sender, rawWikiText ) {
+		var pageId = sender.attr( 'pageid' );
+		var pageName = sender.attr( 'page' );
+		
+		$.post(
+			wgScriptPath + '/api.php',
+			{
+				'action': 'includewp',
+				'format': 'json',
+				'text': rawWikiText,
+				'pagename': pageName
+			},
+			function( data ) {
+				var plainText = data.shift();
+				if ( plainText ) {
+					sender.slideUp( 'slow' );
+					pages[pageName] = plainText;
+					showPageFragment( pageName, pageId );
+					showCopyright( pageName, pageId );
+				}
+				else {
+					sender.html( '<b>' + mediaWiki.msg( 'includewp-loading-failed' ) + '</b>' );
+				}
+			},
+			'json'
+		);
+	}
+	
+	function showPageFragment( pageName, pageId ) {
+		var paragraphEnd = pages[pageName].search( '</p>' );
+		$( '#includewp-article-' + pageId )
+			.html( pages[pageName].substr( 0, paragraphEnd ) + '</p>' )
+			.append( $( '<a />' ).text( mediaWiki.msg( 'includewp-show-full-page' ) ).addClass( 'incwp-more' ).attr( 'href', '#' ).click( function() { showFullPage( pageName, pageId ) } ) );
+	} 
+
+	function showFullPage( pageName, pageId ) {
+		$( '#includewp-article-' + pageId ).html( pages[pageName] );
+	}
+	
+	function showCopyright( pageName, pageId ) {
+		var licenceHtml = mediaWiki.msg( // TODO: make non WP-specific
+				'includewp-licence-notice',
+				'Wikipedia',
+				'http://en.wikipedia.org/wiki/' + pageName,
+				pageName,
+				'http://creativecommons.org/licenses/by-sa/3.0/',
+				'CC-BY-SA',
+				'http://en.wikipedia.org/wiki/' + pageName + '?action=history'
+		);
+		
+		$( '#includewp-copyright-' + pageId ).html( licenceHtml );
+	}
+	
+} ); })(jQuery);
