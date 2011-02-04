3d136f90ee43853e750ed04a5edf47dcfba33afd
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-04 21:04:49 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-02-04 20:33:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81540 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- IncludeWP.i18n.php
+++ IncludeWP.i18n.php
@@ -32,3 +32,38 @@ $messages['en'] = array(
 	'includewp-include-par-page' => 'The name of the (remote) page you want to display.',
 );
 
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'includewp-desc' => "IncludeWP is 'n liggewig uitbreiding om Wikipedia inhoud by u wiki in te sluit",
+	'includewp-loading-page' => 'Laai bladsy...',
+	'includewp-show-full-page' => 'Wys volledige bladsy',
+	'includewp-loading-failed' => 'Laai van bladsy het gefaal.',
+	'includewp-licence-notice' => 'Bostaande inhoud kom vanaf die $1-artikel <a href="$2">$3</a> gelisensieerd onder die <a href="$4">$5</a>. \'n Volledige lys van outeurs is <a href="$6">hier</a> beskikbaar.',
+	'includewp-parserhook-desc' => 'Ontleder-hoek wat die vertoon van inhoud vanuit Wikipedia-artikels moontlik maak.',
+	'includewp-include-par-page' => 'Die naam van die (afgeleë) bladsy om te vertoon.',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'includewp-desc' => 'Ermöglicht das Einbeziehen von Inhalten der Wikipedia in ein Wiki',
+	'includewp-loading-page' => 'Lade Seite …',
+	'includewp-show-full-page' => 'Seite vollständig anzeigen',
+	'includewp-loading-failed' => 'Das Laden der Seite ist gescheitert.',
+	'includewp-licence-notice' => 'Der obige Inhalt stammt vom $1-Artikel <a href="$2">„$3“</a>, der gemäß <a href="$4">$5</a> lizenziert wurde. Eine vollständige Liste der Autoren ist <a href="$6">hier</a> verfügbar.',
+	'includewp-parserhook-desc' => 'Parserhook der das Anzeigen von Inhalten eines Wikipedia-Artikels ermöglicht.',
+	'includewp-include-par-page' => 'Der Name der Seite, die angezeigt werden soll.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'includewp-loading-page' => "D'Säit gëtt gelueden...",
+	'includewp-show-full-page' => 'Säit ganz weisen',
+	'includewp-loading-failed' => 'D?Säit konnt net geluede ginn.',
+);
+
