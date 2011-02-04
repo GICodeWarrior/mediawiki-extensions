131493779f4675118c982ea22eff38709cdea59f
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-04 19:40:48 +0000

    Shorten extension description and "article" -> "page".
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81533 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- IncludeWP.i18n.php
+++ IncludeWP.i18n.php
@@ -19,15 +19,15 @@ $messages = array();
  * @author Jeroen De Dauw
  */
 $messages['en'] = array(
-	'includewp-desc' => 'IncludeWP is a lightweight extension for including Wikipedia content into your wiki',
+	'includewp-desc' => 'IncludeWP is a lightweight extension for including Wikipedia content',
 
 	'includewp-loading-page' => 'Loading page...',
 	'includewp-show-full-page' => 'Show full page',                                 
 	'includewp-loading-failed' => 'Failed to load page.',
 
-	'includewp-licence-notice' => 'The above content comes from the $1 article <a href="$2">$3</a> licenced under the <a href="$4">$5</a>. A full list of authors can be found <a href="$6">here</a>.',
+	'includewp-licence-notice' => 'The above content comes from the $1 page <a href="$2">$3</a> licenced under the <a href="$4">$5</a>. A full list of authors can be found <a href="$6">here</a>.',
 	
-	'includewp-parserhook-desc' => 'Parser hook that allows displaying content of a Wikipedia article.',
+	'includewp-parserhook-desc' => 'Parser hook that allows displaying content of a Wikipedia page.',
 	'includewp-include-par-page' => 'The name of the (remote) page you want to display.',
 );
 
