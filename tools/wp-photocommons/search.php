f2cca2e248fdcfd34d64176f9ec846ade99325f5
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-04 13:47:27 +0000

    more http://codex.wordpress.org/WordPress_Coding_Standards
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81514 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wp-photocommons/search.php
+++ tools/wp-photocommons/search.php
@@ -1,16 +1,16 @@
 <?php
-$path = dirname($_SERVER['PHP_SELF']);
-// Standalone is used to develop this page directly, without having to load
-// it in admin.
-$standalone = empty($_GET['standalone']);
+$path = dirname( $_SERVER['PHP_SELF'] );
+// Standalone is used to debug/test this page directly,
+// without having to the HEAD/BODY with it in admin.
+$standalone = empty( $_GET['standalone'] );
 
-if ($standalone) : ?>
+if ( $standalone ) : ?>
 <!doctype html>
 <html>
 <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="<?=$dir?>/css/ui-lightness/jquery-ui-1.8.5.custom.css" />
-    <link rel="stylesheet" href="<?=$dir?>/css/jquery.suggestions.css" />
+    <link rel="stylesheet" href="<?php echo $dir; ?>/css/ui-lightness/jquery-ui-1.8.5.custom.css" />
+    <link rel="stylesheet" href="<?php echo $dir; ?>/css/jquery.suggestions.css" />
     <link rel="stylesheet" href="css/search.css" />
 </head>
 <body>
@@ -21,15 +21,15 @@ if ($standalone) : ?>
 
 	<ul id="wp-photocommons-results"></ul>
 
-	<img src="<? echo $path; ?>/img/loading.gif" style="display:none;" id="wp-photocommons-loading" />
+	<img src="<?php echo $path; ?>/img/loading.gif" style="display:none;" id="wp-photocommons-loading" />
 
 	<div id="wp-photocommons-images"></div>
 
 <?php if ($standalone) : ?>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
-	<script src="<? echo $path; ?>/js/jquery-ui-1.8.5.custom.min.js"></script>
-	<script src="<? echo $path; ?>/js/search.js"></script>
-	<script src="<? echo $path; ?>/js/jquery.suggestions.js"></script>
+	<script src="<?php echo $path; ?>/js/jquery-ui-1.8.5.custom.min.js"></script>
+	<script src="<?php echo $path; ?>/js/search.js"></script>
+	<script src="<?php echo $path; ?>/js/jquery.suggestions.js"></script>
 </body>
 </html>
 <?php endif; ?>
