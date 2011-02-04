e436ed3683b6c3abba6854c6daebe8c23fc4f6e5
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-04 13:32:44 +0000

    adding a license in wordpress meta head + GPL header in core class
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81512 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wp-photocommons/index.php
+++ tools/wp-photocommons/index.php
@@ -1,11 +1,12 @@
 <?php
 /*
 Plugin Name: Photocommons
-Plugin URI: http://www.mediawiki.org/wiki/Photocommons
+Plugin URI: http://www.mediawiki.org/wiki/PhotoCommons
 Description: Search and add free images from Wikimedia Commons directly in your blog 
 Author: Hay Kranen, Timo Tijhof
 Version: 0.1-alpha
-Author URI: http://www.mediawiki.org/wiki/Photocommons
+Author URI: http://www.mediawiki.org/wiki/PhotoCommons
+License: GPL2
 */
 
 require 'inc/class-photocommons.php';
