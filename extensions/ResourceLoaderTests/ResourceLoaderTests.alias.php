3a6fd209707a55b8ebf8dc6b6ba9a43a0629e4a7
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-07 19:00:52 +0000

    Really simple base for ResourceLoader tests.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81639 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ResourceLoaderTests/ResourceLoaderTests.alias.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aliases for special pages of ResourceLoaderTests extension.
+ *
+ * @file
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author Nike
+ */
+$specialPageAliases['en'] = array(
+	'ResourceLoaderTests' => array( 'ResourceLoaderTests' ),
+);
