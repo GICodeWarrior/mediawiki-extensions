a7c54a826e508bac1ef9d2037a3a067ff1d89c76
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-08 18:05:57 +0000

    Rem redundant version tag
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81766 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/FolkTagCloud/FolkTagCloud.php
+++ extensions/FolkTagCloud/FolkTagCloud.php
@@ -2,7 +2,6 @@
 /**
  * FolkTagCloud
  * @author Katharina Wäschle
- * @version 1.1
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
