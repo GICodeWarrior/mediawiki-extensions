c834d414132fb709b0da26cc4193ea9085c381ba
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-09 21:25:20 +0000

    * Switched to 'ResourceLoaderGetStartupModules' hook base * Removed old EmbedPlayer and TimedText to make room for new RL1_17 based modules
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81849 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MwEmbedSupport.php
+++ MwEmbedSupport.php
@@ -13,8 +13,11 @@
 
 /* Configuration */
 
-/* Setup */
+// When used as an extension we are not in StandAloneResourceLoaderMode:
+$wgStandAloneResourceLoaderMode = false; 
 
+
+/* Setup */
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'MwEmbedSupport',
@@ -29,13 +32,22 @@ $wgExtensionMessagesFiles['MwEmbedSupport'] = dirname( __FILE__ ) . '/MwEmbedSup
 
 // Add Global MwEmbed Registration hook
 $wgHooks['ResourceLoaderRegisterModules'][] = 'MwEmbedResourceManager::registerModules';
+
 // Add MwEmbed module configuration
 $wgHooks['ResourceLoaderGetConfigVars'][] =  'MwEmbedResourceManager::registerConfigVars';
 
+/* MwEmbed Module Registration */
 
 // Register the core MwEmbed Support Module:
 MwEmbedResourceManager::register( 'extensions/MwEmbedSupport/MwEmbedModules/MwEmbedSupport' );
 
 // Register the MwEmbed 'mediaWiki' Module:
 MwEmbedResourceManager::register( 'extensions/MwEmbedSupport/MwEmbedModules/MwEmbedSupport' );
+
+// Add MwEmbedSupport to Startup:
+function MwUpdateStartupModules( &$modules ){	
+	array_push($modules, 'jquery.triggerQueueCallback', 'jquery.mwEmbedUtil', 'mwEmbedStartup' );		
+	return true;
+}
+$wgHooks['ResourceLoaderGetStartupModules'][] = 'MwUpdateStartupModules';
 
