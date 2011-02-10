d38814d451ca171a6d2ad585bfc7d9a307c39615
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-10 00:20:57 +0000

    moved mwEmbed modules to "resources" folder
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@81858 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MwEmbedModules/MediaWikiSupport/MediaWikiSupport.loader.js
+++ MwEmbedModules/MediaWikiSupport/MediaWikiSupport.loader.js
@@ -2,7 +2,7 @@
 // Wrap in mediaWiki 
 ( function( mw ) {
 
-	// Add MediaWikiSupportPlayer dependency on players with apiTitleKey 
+	// Add MediaWikiSupportPlayer dependency on players with mwtitle 
 	$( mw ).bind( 'EmbedPlayerUpdateDependencies', function( event, embedPlayer, dependencySet ){
 		if( $( embedPlayer ).attr( 'data-mwtitle' ) ){
 			$.merge( dependencySet, ['MediaWikiSupportPlayer'] );
