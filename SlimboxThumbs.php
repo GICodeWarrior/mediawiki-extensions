291a80afdd6a494815b343bd7f7c515d7766b01e
kwisatz <kwisatz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-12 23:21:59 +0000

    Partially working maintenance release of SlimboxThumbs. This is the last version of this extensions with this codebase. Future versions will be redone on a higher level, doing away with the horribly regex kludge.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@82040 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SlimboxThumbs.php
+++ SlimboxThumbs.php
@@ -49,7 +49,6 @@ require_once 'SlimboxThumbs_Settings.php';
  * but they would be called for each image, making the wiki even slower
  */
 if ( $slimboxThumbsFilesDir ) {
-    $slimboxThumbsFilesDir = rtrim( trim( $slimboxThumbsFilesDir ), '/' );  // strip whitespace, then any trailing /
     $wgHooks['BeforeParserrenderImageGallery'][] = 'efSBTTestForGallery'; // this seems to fail on some pages :(
     $hasGallery = true; // temporary fix
     $wgHooks['BeforePageDisplay'][] = 'efSBTAddScripts';
@@ -59,70 +58,38 @@ if ( $slimboxThumbsFilesDir ) {
 function efSBTTestForGallery( $parser, $gallery ) {
         global $hasGallery;
         $hasGallery = $gallery instanceof ImageGallery;
-        return $hasGallery;
+        return true;
 }
 
-function efSBTDebugVar( $varName, $var ) {
-    return "\n\n<!--\n$varName: " . str_replace( '--', '__', print_r( $var, true ) ) . "\n-->\n\n";
-}
-
-// This is a callback function that gets called by efBeforePageDisplay().
-function efRewriteThumbImage( $matches ) {
-    global $wgOut, $slimboxThumbsDebug;
-    
-    if ( $slimboxThumbsDebug ) { global $wgContLang; }
-    
-    $titleObj = Title::newFromText( rawurldecode( $matches[2] ) );
-    $image = wfFindFile( $titleObj, false, false, true ); # # wfFindFile($titleObj,false,false,true) to bypass cache
-        $output =  $matches[1]
-                . ' href="' . $image->getURL() . '" class="image" rel="lightbox" title="'
-                . htmlspecialchars( $wgOut->parse( "'''[[:" . $titleObj->getFullText() . "|" . $titleObj->getText() . "]]:''' " ) . $matches[3] )
-                . '" ' . $matches[4] . $matches[5]   // url, hashpath,/w/thumb.php?f=FoodHacker_01.jpg&amp;width=800
-                . ( $slimboxThumbsDebug ? efDebugVar( '$matches', $matches )
-                        . efSBTDebugVar( '$titleObj', $titleObj )
-                        . efSBTDebugVar( '$image', $image )
-                        . efSBTDebugVar( '$wgContLang->namespaceNames', $wgContLang->namespaceNames ):'' );
-                        
-        return $output;
-}
-
-// Rewrite the gallery code.
-function efRewriteGalleryImage( $matches ) {
-        global $wgOut, $slimboxThumbsDebug, $slimboxDefaultWidth;
-        $titleObj = Title::newFromText( rawurldecode( $matches[2] ) );
-        $image = wfFindFile( $titleObj, false, false, true );
-        $realwidth = (Integer) $image->getWidth();
-        $width = ( $realwidth > $slimboxDefaultWidth ) ? $slimboxDefaultWidth : $realwidth -1;
-        $output = $matches[1]
-                // .' href="'.$image->getURL().'" class="image" rel="lightbox[gallery]" title="'
-                . ' href="/w/thumb.php?f=' . $image->getName() . '&amp;width=' . $width . '" class="image" rel="lightbox[gallery]" title="'
-                . htmlspecialchars( $wgOut->parse( "'''[[:File:" . $titleObj->getFullText() . "|" . $titleObj->getText() . "]]:''' " )
-                . $matches[4] )
-                . '" ' . $matches[3] . $matches[4] . "</div>"
-                . ( $slimboxThumbsDebug ? efDebugVar( '$matches', $matches )
-                        . efSBTDebugVar( '$titleObj', $titleObj )
-                        . efSBTDebugVar( '$image', $image ):'' );
-                        
-        return $output;
-}
-
-/* Add javacsripts and stylesheets */
+/**
+ * Add javascript files and stylesheets.
+ * Also add inline js that regulates the image width.
+ */
 function efSBTAddScripts( $out ) {
-        global $slimboxThumbsFilesDir, $hasGallery;
+        global $hasGallery, $wgVersion, $wgExtensionAssetsPath;
+	$eDir = $wgExtensionAssetsPath .'/SlimboxThumbs/slimbox';
         
         // We don't want to load jQuery if there's no gallery here.
         //if ( !$hasGallery ) return false;
 
-        $out->addScript( '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>' . "\n" );
-        $out->addScript( '<script type="text/javascript" src="' . $slimboxThumbsFilesDir . '/js/slimbox2.js"></script>' . "\n" );
-        $out->addExtensionStyle( $slimboxThumbsFilesDir . '/css/slimbox2.css', 'screen' );
+	// 1.17 will require another setup 
+	// (http://www.mediawiki.org/wiki/JQuery & http://www.mediawiki.org/wiki/ResourceLoader/Default_modules)
+	if ( substr($wgVersion,0,-2) < 1.16 ) {
+		$jQ = '$';
+		$out->addScript( '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>' . "\n" );
+	} else  {
+		$jQ = '$j';
+		$out->includeJQuery(); // includes jquery.min.js
+	}
+        $out->addScript( '<script type="text/javascript" src="'. $eDir .'/js/slimbox2.js"></script>' . "\n" );
+        $out->addExtensionStyle( $eDir . '/css/slimbox2.css', 'screen' );
 
         // use thumb.php to resize pictures if browser window is smaller than the picture itself
-        $out->addInlineScript( '$(document).ready(function(){
-                if($("table.gallery").val() != undefined){
-                        var boxWidth = ($(window).width() - 20);
+        $out->addInlineScript( $jQ.'(document).ready(function(){
+                if('.$jQ.'("table.gallery").val() != undefined){
+                        var boxWidth = ('.$jQ.'(window).width() - 20);
                         var rxp = new RegExp(/([0-9]{2,})$/);
-                        $("a[rel=\'lightbox[gallery]\']").each(function(el){
+                        '.$jQ.'("a[rel=\'lightbox[gallery]\']").each(function(el){
                                 if(boxWidth < Number(this.search.match(rxp)[0])){
                                         this.href = this.pathname+this.search.replace(rxp,boxWidth);
                                 }
@@ -133,8 +100,55 @@ function efSBTAddScripts( $out ) {
         return true;
 }
 
+function efSBTDebugVar( $varName, $var ) {
+    return "\n\n<!--\n$varName: " . str_replace( '--', '__', print_r( $var, true ) ) . "\n-->\n\n";
+}
+
+/** 
+ * This is a callback function that gets called by efBeforePageDisplay().
+ */
+function efRewriteThumbImage( $matches ) {
+	global $wgOut, $slimboxThumbsDebug;
+    
+	if ( $slimboxThumbsDebug ) { global $wgContLang; }
+    
+	$titleObj = Title::newFromText( rawurldecode( 'File:'.$matches[2] ) );
+	$image = wfFindFile( $titleObj, false, false, true ); # # wfFindFile($titleObj,false,false,true) to bypass cache
+	$output =  $matches[1]	// <a (not much else)
+                . 'href="' . $image->getURL() . '" class="image" rel="lightbox" title="'
+                . htmlspecialchars( $wgOut->parse( "'''[[:" . $titleObj->getFullText() . "|" . $titleObj->getText() . "]]:''' " ) )
+                . '">'. $matches[3] . '</a>' //$matches[4]
+                . ( $slimboxThumbsDebug ? efSBTDebugVar( '$matches', $matches )
+                        . efSBTDebugVar( '$titleObj', $titleObj )
+                        . efSBTDebugVar( '$image', $image )
+                        . efSBTDebugVar( '$wgContLang->namespaceNames', $wgContLang->namespaceNames ):'' );
+                        
+        return $output;
+}
+
+// Rewrite the gallery code.
+function efRewriteGalleryImage( $matches ) {
+        global $wgOut, $slimboxThumbsDebug, $slimboxDefaultWidth, $wgScriptPath;
+
+        $titleObj = Title::newFromText( rawurldecode( $matches[2] ) );
+        $image = wfFindFile( $titleObj, false, false, true );
+        $realwidth = (Integer) $image->getWidth();
+        $width = ( $realwidth > $slimboxDefaultWidth ) ? $slimboxDefaultWidth : $realwidth -1;
+        $output = $matches[1]
+                 .' href="'.$image->createThumb($width).'" class="image" rel="lightbox[gallery]" title="'
+                . htmlspecialchars( $wgOut->parse( "'''[[:" . $titleObj->getFullText() . "|" . $titleObj->getText() . "]]:''' " )
+                . $matches[4] )
+                . '" ' . $matches[3] . $matches[4] . "</div>"
+                . ( $slimboxThumbsDebug ? efSBTDebugVar( '$matches', $matches )
+                        . efSBTDebugVar( '$titleObj', $titleObj )
+                        . efSBTDebugVar( '$image', $image ):'' );
+                        
+        return $output;
+}
+
+
 function efSBTAddSlimboxCode( $out, $skin ) {
-        global $slimboxThumbsFilesDir, $wgContLang, $hasGallery;
+        global $wgContLang, $hasGallery;
 
         // We don't want to run regular expressions if there's no gallery here.
         if ( !$hasGallery ) {
@@ -144,34 +158,44 @@ function efSBTAddSlimboxCode( $out, $skin ) {
         # # ideally we'd do this with XPath, but we'd need valid XML for that, so we'll do it with some ugly regexes
         # # (could use a regex to pull out all div.thumb, maybe they're valid XML? ...probably not)
         # # An other alternative would be to use javascript and the DOM
+	# # The jquery lightbox plugin does exactly that.
 
         // regex for thumbnails
-        $pattern = '/(<a[^>]+?)'        // $1: start of opening <a> tag through start of href attribute in <a> tag
-                 . '\s*href="[^"]*(?:' . $wgContLang->namespaceNames[6] . '):' // dont care about start of original link href...
-                 . '([^"\/]+)'           // $2: ...but end is wiki name for the image
-                 . '"\s*class="image"\s*title="'
-                 . '([^"]+)'                              // $3: link title becomes image caption
-                 . '"\s*.'
-                 . '([^>]*>)'                             // $4: remainder of opening <a> tag
-                 . '\s*'
-                 . '(<img[^>]+?class="thumbimage"[^>]*>)' // $5: the img tag itself
-                 . '/x';
-    $thumbnailsDone = preg_replace_callback( $pattern, 'efRewriteThumbImage', $out->getHTML() );
+	$pattern = '/(<a[^>]+?)'     	   // $1: start of opening <a> tag through start of href attribute in <a> tag
+                . '\s*href="[^"]*(?:' . $wgContLang->namespaceNames[6] . '):' // dont care about start of original link href...
+                . '([^"\/]+)'           			// $2: ...but end is wiki name for the image
+                . '"\s*class="image"\s*'			// (1.16 title= gone)
+                . '[^>]*>\s*' 					
+                . '(<img[^>]+?[^>]*>)' 				// $3: the img tag itself (1.16 thumbimage class gone)
+        	//.'(.*<\/*a>)' 					// $4 should get until >, but gets more...
+		.'/xs';
 
-        // regex for galleries
-        $pattern = '/(<div\s*class="gallerybox".+?div\s*class="thumb".+?) # $1: div.gallerybox opening tag through href attribute, so we can keep it intact
-                 \s*href="[^"]+"\s*class="image"\s*                   # this is getting replaced
-                 title="([^"]+)"                                      # $2: link title attribute holds wiki name for the image
-                 ([^>]*>.+?<div\s*class="gallerytext">)               # $3: end of open <a> through start of caption
-                 \s*(?:<p>\s*)?                                       #
-                 (.+?)                                                # $4: caption is raw HTML... (may choke if contains an ending div)
-                 (?:\s*(<\/p>|<br\s*\/?>))?\s*<\/div>                 #
-                /sx';
-        
-    $allDone = preg_replace_callback( $pattern, 'efRewriteGalleryImage', $thumbnailsDone );
+	//by User:Cm
+	/*$pattern = '/(<a[^>]+?)' 				// $1 start of opening <a> tag through start of href attribute in <a> tag
+        	.'\s*href="([^"]*)' 				// $2 link
+        	.'"\s*class="image"\s*title="([^"]+)' 		// $3 title
+        	.'"\s*([^>]*>)'					// $4 end of <a>
+        	.'\s*(<img[^>]+?class="thumbimage"[^>]*>)' 	// $5 img tag
+        	.'(.*<\/*a>)/xs'; 				// $6 anything else
+	*/
 
-    $out->clearHTML();
-    $out->addHTML( $allDone );
+	$thumbnailsDone = preg_replace_callback( $pattern, 'efRewriteThumbImage', $out->getHTML() );
 
-    return true;
+        /**
+	 *  Redone regex for galleries 
+	 * I don't get this, why am I regexxing code I'm creating in the first callback? WTF?
+	 */
+	$pattern = '/(<div\s*class="gallerybox".+?<div\s*class="thumb".+?)'	# $1
+		.'.+?href="\/wiki\/([^"]+)"\s*class="image"'			# $2 (link)
+		.'([^>]*>.+?<div\s*class="gallerytext">)'			# $3 rest
+                .'\s*(?:<p>\s*)?(.+?)'		
+                .'(?:\s*(<\/p>|<br\s*\/?>))?\s*<\/div>'
+                .'/xs';
+
+	$allDone = preg_replace_callback( $pattern, 'efRewriteGalleryImage', $thumbnailsDone );
+
+	$out->clearHTML();
+	$out->addHTML( $thumbnailsDone );
+	//$out->addHTML( $allDone );
+	return true;
 }
