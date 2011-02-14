3194540ccbacc3d53b79c86c38f4467e74660bff
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-14 21:56:30 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-02-14 21:31:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@82148 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- GeeQuBox.i18n.php
+++ GeeQuBox.i18n.php
@@ -17,3 +17,24 @@ $messages['en'] = array(
 	'geequbox-desc' => 'Generates lightbox effects using the jQuery lightbox plugin'
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'geequbox-desc' => 'Ermöglicht unter Verwendung von jQuery lightBox die Nutzung von Lightbox-Funktionen',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'geequbox-desc' => 'Wutworja efekty swětłoweho kašćika (Lightbox) z pomocu tykača jQuery lightbox',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'geequbox-desc' => 'Создава ефекти на светлосна кутија (Lightbox) користејќи го приклучокот за jQuery',
+);
+
