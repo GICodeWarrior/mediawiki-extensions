59bed9fca71e095deb9e27279fc0568ef92f46a3
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-15 21:04:37 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-02-15 20:47:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@82192 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- GeeQuBox.i18n.php
+++ GeeQuBox.i18n.php
@@ -17,6 +17,13 @@ $messages['en'] = array(
 	'geequbox-desc' => 'Generates lightbox effects using the jQuery lightbox plugin'
 );
 
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author EugeneZelenko
+ */
+$messages['be-tarask'] = array(
+	'geequbox-desc' => 'Стварае эфэкты падсьветкі з выкарыстаньнем дапаўненьня падсьветкі jQuery',
+);
+
 /** German (Deutsch)
  * @author Kghbln
  */
@@ -31,10 +38,24 @@ $messages['hsb'] = array(
 	'geequbox-desc' => 'Wutworja efekty swětłoweho kašćika (Lightbox) z pomocu tykača jQuery lightbox',
 );
 
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'geequbox-desc' => 'Määt et müjjelesch, met <i lang="en">jQuery lightBox</i> de Funkjohne un Efäkte vun <i lang="en">Lightbox</i> ze bruche.',
+);
+
 /** Macedonian (Македонски)
  * @author Bjankuloski06
  */
 $messages['mk'] = array(
 	'geequbox-desc' => 'Создава ефекти на светлосна кутија (Lightbox) користејќи го приклучокот за jQuery',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'geequbox-desc' => 'Voegt lightboxeffecten toe met behulp van de jQuery lightboxplug-in',
 );
 
