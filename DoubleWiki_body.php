0c512f1baafe036cf39623b11ba197d82bd0eaf3
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-15 22:11:54 +0000

    Break out of the foreach loop in addMatchedText after we've matched the target language and done whatever we needed to do. Per chad
    Remove some commented out/debug code
    
    Normalise comments
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@82199 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- DoubleWiki_body.php
+++ DoubleWiki_body.php
@@ -19,12 +19,12 @@
 
 class DoubleWiki {
 
-	/*
+	/**
 	 * Tags that must be closed. (list copied from Sanitizer.php)
 	 */
 	var $tags = '/<\/?(b|del|i|ins|u|font|big|small|sub|sup|h1|h2|h3|h4|h5|h6|cite|code|em|s|strike|strong|tt|tr|td|var|div|center|blockquote|ol|ul|dl|table|caption|pre|ruby|rt|rb|rp|p|span)([\s](.*?)>|>)/i';
 
-	/*
+	/**
 	 * Read the list of matched phrases and add tags to the html output.
 	 */
 	function addMatchingTags ( &$text, $lang ) {
@@ -34,7 +34,7 @@ class DoubleWiki {
 			return;
 		}
 		$text = str_replace( $m[1], '', $text );
-		$line_pattern = "/\s*([^:\n]*?)\s*:\s*([^:\n]*?)\s*\n/i";
+		$line_pattern = '/\s*([^:\n]*?)\s*:\s*([^:\n]*?)\s*\n/i';
 		$items = array();
 		preg_match_all( $line_pattern, $m[1], $items, PREG_SET_ORDER );
 		foreach ( $items as $n => $i ) {
@@ -66,6 +66,7 @@ class DoubleWiki {
 		foreach ( $out->mLanguageLinks as $l ) {
 			$nt = Title::newFromText( $l );
 			$iw = $nt->getInterwiki();
+
 			if ( $iw === $match_request ) {
 				$url =  $nt->getFullURL();
 				$myURL = $out->getTitle()->getLocalURL();
@@ -74,22 +75,23 @@ class DoubleWiki {
 
 				$translation = Http::get( wfAppendQuery( $url, array( 'action' => 'render' ) ) );
 				if ( $translation !== null ) {
-					# first find all links that have no 'class' parameter.
-					# these links are local so we add '?match=xx' to their url,
-					# unless it already contains a '?'
+					/* first find all links that have no 'class' parameter.
+					 * these links are local so we add '?match=xx' to their url,
+					 * unless it already contains a '?'
+					 */
 					$translation = preg_replace(
 						"/<a href=\"http:\/\/([^\"\?]*)\"(([\s]+)(c(?!lass=)|[^c\>\s])([^\>\s]*))*\>/i",
 						"<a href=\"http://\\1?match={$wgContLanguageCode}\"\\2>", $translation );
-					# now add class='extiw' to these links
+					// now add class='extiw' to these links
 					$translation = preg_replace(
 						"/<a href=\"http:\/\/([^\"]*)\"(([\s]+)(c(?!lass=)|[^c\>\s])([^\>\s]*))*\>/i",
 						"<a href=\"http://\\1\" class=\"extiw\"\\3>", $translation );
-					# use class='extiw' for images too
+					// use class='extiw' for images too
 					$translation = preg_replace(
 						"/<a href=\"http:\/\/([^\"]*)\"([^\>]*)class=\"image\"([^\>]*)\>/i",
 						"<a href=\"http://\\1\"\\2class=\"extiw\"\\3>", $translation );
 
-					# add prefixes to internal links, in order to prevent duplicates
+					// add prefixes to internal links, in order to prevent duplicates
 					$translation = preg_replace( "/<a href=\"#(.*?)\"/i", "<a href=\"#l_\\1\"",
 								    $translation );
 					$translation = preg_replace( "/<li id=\"(.*?)\"/i", "<li id=\"l_\\1\"",
@@ -97,14 +99,17 @@ class DoubleWiki {
 					$text = preg_replace( "/<a href=\"#(.*?)\"/i", "<a href=\"#r_\\1\"", $text );
 					$text = preg_replace( "/<li id=\"(.*?)\"/i", "<li id=\"r_\\1\"", $text );
 
-					# add ?match= to local links of the local wiki
+					// add ?match= to local links of the local wiki
 					$text = preg_replace( "/<a href=\"\/([^\"\?]*)\"/i",
 							"<a href=\"/\\1?match={$match_request}\"", $text );
 
-					# do the job
+					// do the job
 					$text = $this->matchColumns ( $text, $myLanguage, $myURL, $wgContLanguageCode,
 							       $translation, $languageName, $url, $match_request );
 				}
+
+				//Break once we've matched a language link, no point in continuing processing
+				break;
 			}
 		}
 		return true;
@@ -134,12 +139,12 @@ class DoubleWiki {
 			$tag = $left_tags[1][$i];
 			$left_chunk .= $left_slices[$i];
 
-			# if we are at the end of the loop, finish quickly
+			// if we are at the end of the loop, finish quickly
 			if ( $i == $leftSliceCount - 1 ) {
 				$right_chunk .= $right_text;
 				$found = true;
 			} else {
-				# look for requested tag in the text
+				// look for requested tag in the text
 				$a = strpos ( $right_text, $tag );
 				if ( $a ) {
 					$found = true;
@@ -151,8 +156,6 @@ class DoubleWiki {
 						$right_chunk .= $m[0];
 						$right_text = substr( $right_text, strlen( $m[0] ) );
 					}
-				# } else {
-				#	print "<br/>tag not found ".$tag;
 				}
 			}
 
@@ -161,7 +164,6 @@ class DoubleWiki {
 				$left_bits  = $this->find_paragraphs( $left_chunk );
 				$right_bits = $this->find_paragraphs( $right_chunk );
 
-				// $body .= "<tr style=\"background-color:#ffdddd;\"><td>".count($left_bits)."</td><td>".count($right_bits)."</td></tr>\n";
 				// Do not align paragraphs if counts are different
 				if ( count( $left_bits ) != count( $right_bits ) ) {
 					$left_bits = array( $left_chunk );
@@ -196,11 +198,11 @@ class DoubleWiki {
 		return $head . $body . "</table>" ;
 	}
 
-	/*
+	/**
 	 * Split text and return a set of html-balanced paragraphs
 	 */
 	function find_paragraphs( $text ) {
-		$result = Array();
+		$result = array();
 		$bits = preg_split( $this->tags, $text );
 		$m = array();
 		preg_match_all( $this->tags, $text, $m, PREG_SET_ORDER );
@@ -226,7 +228,7 @@ class DoubleWiki {
 		return $result;
 	}
 
-	/*
+	/**
 	 * Split text and return a set of html-balanced slices
 	 */
 	function find_slices( $left_text ) {
@@ -237,7 +239,7 @@ class DoubleWiki {
 		preg_match_all( $tag_pattern, $left_text,  $left_tags, PREG_PATTERN_ORDER );
 		$n = count( $left_slices );
 
-		/*
+		/**
 		 * Make slices that are full paragraphs
 		 * If two slices correspond to the same paragraph, the second one will be empty
 		 */
@@ -250,7 +252,7 @@ class DoubleWiki {
 			}
 		}
 
-		/*
+		/**
 		 * Keep only slices that contain balanced html
 		 * If a slice is unbalanced, we merge it with the next one.
 		 * The first and last slices are compensated.
