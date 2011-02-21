8b053a0009071c6581f6e9d8135602656bdc37a6
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-21 18:00:11 +0000

    RL support check tweak
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@82563 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Push/Push.php
+++ extensions/Push/Push.php
@@ -98,7 +98,7 @@ $egPushJSMessages = array(
 );
 
 // For backward compatibility with MW < 1.17.
-if ( is_callable( array( 'OutputPage', 'addModules' ) ) ) {
+if ( defined( 'MW_SUPPORTS_RESOURCE_MODULES' ) ) {
 	$moduleTemplate = array(
 		'localBasePath' => dirname( __FILE__ ),
 		'remoteBasePath' => $egPushScriptPath,
