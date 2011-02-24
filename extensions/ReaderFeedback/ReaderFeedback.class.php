25ead4c1397e18e44c9362b350d0070cb759b636
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-24 23:04:05 +0000

    Fixed timestamp use in getAverageRating
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@82755 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ReaderFeedback/ReaderFeedback.class.php
+++ extensions/ReaderFeedback/ReaderFeedback.class.php
@@ -89,6 +89,8 @@ class ReaderFeedback {
 	public static function getAverageRating( $article, $tag, $forUpdate=false ) {
 		global $wgFeedbackAge;
 		$cutoff_unixtime = time() - $wgFeedbackAge;
+		// rfh_date is always MW format on all dbms
+		$encCutoff = $db->addQuotes( wfTimestamp( TS_MW, $cutoff_unixtime ) );
 		$db = $forUpdate ? wfGetDB( DB_MASTER ) : wfGetDB( DB_SLAVE );
 		$row = $db->selectRow( 'reader_feedback_history', 
 			array('SUM(rfh_total)/SUM(rfh_count) AS ave, SUM(rfh_count) AS count'),
