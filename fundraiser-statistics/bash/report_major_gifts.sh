0b60b12743484930af91d4d0ede2f8326a2fc2d6
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-02-28 22:50:15 +0000

    fixed call in major gifts bash call.  disabled sql statement output.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@82974 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fundraiser-statistics/bash/report_major_gifts.sh
+++ fundraiser-statistics/bash/report_major_gifts.sh
@@ -2,7 +2,7 @@
 
 cd /home/rfaulk/fundraiser-statistics/fundraiser-scripts
 
-python mine_contacts.py c
+python mine_contacts.py a
 
 cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/html/report_ecomm_by_contact.html /srv/org.wikimedia.fundraising/stats/
 
