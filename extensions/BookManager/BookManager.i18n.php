6a43682286133485e62ace51f3504c21071c9bc9
raylton.sousa <raylton.sousa@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-02 12:43:32 +0000

    trying to add extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@83079 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/BookManager/BookManager.i18n.php
@@ -0,0 +1,16 @@
+<?php
+$messages = array();
+$messages['en'] = array(
+    'BookManager' => '<ul class="book-navigation">
+<li class="prev">[[$1|$2]]</li>
+<li class="index"> [[$3|$4]]</li>
+<li class="next">[[$5|$6]]</li>
+</ul>',
+);
+$messages['pt'] = array(
+    'BookManager' => '<ul class="book-navigation">
+<li class="prev">[[$1|$2]]</li>
+<li class="index"> [[$3|$4]]</li>
+<li class="next">[[$5|$6]]</li>
+</ul>',
+);
