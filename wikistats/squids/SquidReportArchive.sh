027e8a137107dcc4f0726ae8562a979e9e7c8c5c
ezachte <ezachte@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-05 16:03:09 +0000

    run and pack traffic by country reports
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@83293 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- wikistats/squids/SquidReportArchive.sh
+++ wikistats/squids/SquidReportArchive.sh
@@ -1,10 +1,20 @@
 #! /bin/sh
 ulimit -v 4000000
+
 home="/a/ezachte"
+month=2011-02
+
 # perl $home/SquidReportArchive.pl -m 201007 > SquidReportArchiveLog.txt
 # after further automating SquidScanCountries.sh:
-perl $home/SquidReportArchive.pl -c 201101 >> SquidReportArchiveLog.txt # -c for per country reports
-perl $home/SquidReportArchive.pl -m 201101 >> SquidReportArchiveLog.txt
-tar -cf reports.tar /a/ezachte/*.htm 
-bzip2 reports.tar
-mv reports.tar.bz2 /a/ezachte
+
+#perl $home/SquidReportArchive.pl -c $month >> SquidReportArchiveLog.txt # -c for per country reports
+#perl $home/SquidReportArchive.pl -m $month >> SquidReportArchiveLog.txt
+#perl $home/SquidCountryScan.pl # start in July 2009
+
+cp /a/ezachte/$month/$month-html.tar ./reports_traffic_$month.tar 
+tar -cf reports_countries_$month.tar SquidReportPage*.htm 
+
+bzip2 -f reports_traffic_$month.tar
+bzip2 -f reports_countries_$month.tar
+
+tar -cf reports_$month.tar reports_*_$month.tar.bz2
