90374e0ae7f3df5b32d08353a380d3ef4a4fb01c
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-06 03:31:37 +0000

    added file I forgot to svn add earlier
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@83363 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ArrayExtension/ArrayExtension.i18n.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Internationalization file for the Include WP extension.
+ *
+ * @since 1.3.4
+ *
+ * @file ArrayExtension.i18n.php
+ * @ingroup ArrayExtension
+ *
+ * @licence MIT License
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'arrayext-desc' => 'Store and compute named arrays',
+);
