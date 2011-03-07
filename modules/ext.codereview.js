618e8081d590401d8377355a3c2123d43a7acb24
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-07 22:43:43 +0000

    Follow-up: r83352; simplifying. No need for entire frame if there's only one thing
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@83488 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- modules/ext.codereview.js
+++ modules/ext.codereview.js
@@ -1,18 +1,3 @@
-( function( $ ) {
-// Create or extend the object
-window.CodeReview = $.extend( window.CodeReview, {
-
-	focusReplyArea : function() {
-		var $replyArea = $('.mw-codereview-post-comment textarea:first');
-		if ( $replyArea.size() ) {
-			$replyArea[0].focus();	
-		}
-	}
-
-});
-
-$( document ).ready( function() {
-	CodeReview.focusReplyArea();
-});
-
-})( jQuery );
+jQuery( document ).ready( function( $ ) {
+	$( '.mw-codereview-post-comment textarea:first' ).focus();
+} );
