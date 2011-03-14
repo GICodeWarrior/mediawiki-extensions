9002aafdc6eb282624e1b99610dc08ecc8131e9e
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-14 18:51:03 +0000

    migrating major donor table access to local folder
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@83942 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fundraiser-statistics/bash/report_major_gifts.sh
+++ fundraiser-statistics/bash/report_major_gifts.sh
@@ -5,6 +5,6 @@ cd /home/rfaulk/fundraiser-statistics/fundraiser-scripts
 python mine_contacts.py a
 python mine_contacts.py c
 
-cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/html/report_ecomm_by_amount.html /srv/org.wikimedia.fundraising/stats/
-cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/html/report_ecomm_by_contact.html /srv/org.wikimedia.fundraising/stats/
+#cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/html/report_ecomm_by_amount.html /srv/org.wikimedia.fundraising/stats/
+#cp /home/rfaulk/fundraiser-statistics/fundraiser-scripts/html/report_ecomm_by_contact.html /srv/org.wikimedia.fundraising/stats/
 
