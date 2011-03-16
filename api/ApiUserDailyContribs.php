696c36d48dd024d13aab3cd4fd2897ef94e0a226
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-16 22:21:15 +0000

    Fixup indenting
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84127 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- api/ApiUserDailyContribs.php
+++ api/ApiUserDailyContribs.php
@@ -2,32 +2,30 @@
 
 class ApiUserDailyContribs extends ApiBase {
 
-	
 	public function execute() {
-	 $params = $this->extractRequestParams();
-	 $result = $this->getResult();
+		$params = $this->extractRequestParams();
+		$result = $this->getResult();
 
-	 $userName = $params['user'];
-	 $days = $params['daysago'];	
-	 $user = User::newFromName($userName);
-	 $now = time();
-	 $result->addValue( array( 'query', $this->getModuleName() ),
-				 'totalEdits', 
-	 			($user->getEditCount() == NULL)?0:$user->getEditCount() );
-	 $result->addValue( array( 'query', $this->getModuleName() ),
-				'registration', $user->getRegistration() );
-	 $result->addValue( array( 'query', $this->getModuleName() ),
-				'timeFrameEdits', getUserEditCountSince( $now - ($days * 60 *60 *24)  ));
-	 
+		$userName = $params['user'];
+		$days = $params['daysago'];
+		$user = User::newFromName($userName);
+		$now = time();
+		$result->addValue( array( 'query', $this->getModuleName() ),
+			'totalEdits',
+			($user->getEditCount() == NULL)?0:$user->getEditCount() );
+		$result->addValue( array( 'query', $this->getModuleName() ),
+			'registration', $user->getRegistration() );
+		$result->addValue( array( 'query', $this->getModuleName() ),
+			'timeFrameEdits', getUserEditCountSince( $now - ($days * 60 *60 *24)  ));
 	}
-	 
-	 public function getAllowedParams() {
+
+	public function getAllowedParams() {
 		return array(
 			'user' => null,
 			'daysago' => null,
 		);
-	 }
-	 
+	}
+
 	public function getParamDescription() {
 		return array(
 			'user' => 'Username to query',
@@ -48,5 +46,4 @@ class ApiUserDailyContribs extends ApiBase {
 	}
 	 //2009-05-01-18-35-35
 	 //YYYY-MM-DD-HH-MM-SS
-	 
-	}
+}
