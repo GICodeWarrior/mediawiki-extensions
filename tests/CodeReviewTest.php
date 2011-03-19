166238bd915a3b41a0539b026706680e9495c45b
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-19 20:48:59 +0000

    Expand unit tests with bug references
    Some overlaps in bugs...
    
    A lot of it boils down to the same issue though
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84344 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tests/CodeReviewTest.php
+++ tests/CodeReviewTest.php
@@ -19,8 +19,24 @@ class CodeReviewTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( '[http://foo http://foo]', $formatter->link( 'http://foo' ) );
 		$this->assertEquals( '[http://example.com/bugzilla/123 bug 123]', $formatter->link( 'bug 123' ) );
 		$this->assertEquals( '[[Special:Code/Test/456|r456]]', $formatter->link( 'r456' ) );
-		// fails, bug 23203 and so on
-		// $this->assertEquals( '[http://example.org foo http://example.org foo]', $formatter->link( '[http://example.org foo http://example.org foo]' ) );
+
+		// fails, 18614
 		// $this->assertEquals( '[http://foo.bar r123]', $formatter->link( '[http://foo.bar r123]' ) );
+		// $this->assertEquals( '[[foo|bug 1234]]', $formatter->link( '[[foo|bug 1234]]' ) );
+		// $this->assertEquals( '[[bugzilla:19359|bug 19359]]', $formatter->link( '[[bugzilla:19359|bug 19359]]' ) );
+		
+		// fails, bug 19299
+		// $this->assertEquals( '[[bugzilla:18989|Bug 18989]]', $formatter->link( '[[bugzilla:18989|Bug 18989]]' ) );
+		// $this->assertEquals( '[http://www.mediawiki.org/wiki/Special:Code/MediaWiki/75762#code-comments r75762 CR comments]',
+		//	$formatter->link( [http://www.mediawiki.org/wiki/Special:Code/MediaWiki/75762#code-comments r75762 CR comments]' ) );
+
+		// fails, bug 24279
+		// $this->assertEquals( '[http://svn.wikimedia.org/viewvc/mediawiki/trunk/phase3/includes/api/ApiUpload.php?pathrev=70049&r1=70048&r2=70049 ViewVC]',
+		//	$formatter->link( '[http://svn.wikimedia.org/viewvc/mediawiki/trunk/phase3/includes/api/ApiUpload.php?pathrev=70049&r1=70048&r2=70049 ViewVC]' ) );
+		// $this->assertEquals( '<nowiki>bug 24027</nowiki>', $formatter->link( '<nowiki>bug 24027</nowiki>' ) );
+		// $this->assertEquals( 'http://bugzilla.org/13518#c9', $formatter->link( 'bug 13518#c9' ) );
+		// $this->assertEquals( 'http://bugzilla.org/13518#c9', $formatter->link( 'bug 13518 comment 9' ) );
+
+		// $this->assertEquals( '', $formatter->link( '' ) );
 	}
 }
