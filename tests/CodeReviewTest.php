b1429a4d26dc646d7963e019f640f550f7702260
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-19 21:10:32 +0000

    Fixup tests to match input a bit better
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84346 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tests/CodeReviewTest.php
+++ tests/CodeReviewTest.php
@@ -7,7 +7,7 @@ class CodeReviewTest extends PHPUnit_Framework_TestCase {
 		$row->repo_name = 'Test';
 		$row->repo_path = 'somewhere';
 		$row->repo_viewvc = 'http://example.com/view/';
-		$row->repo_bugzilla = 'http://example.com/bugzilla/$1';
+		$row->repo_bugzilla = 'http://example.com/$1';
 
 		return CodeRepository::newFromRow( $row );
 	}
@@ -17,7 +17,7 @@ class CodeReviewTest extends PHPUnit_Framework_TestCase {
 		$formatter = new CodeCommentLinkerWiki( $repo );
 
 		$this->assertEquals( '[http://foo http://foo]', $formatter->link( 'http://foo' ) );
-		$this->assertEquals( '[http://example.com/bugzilla/123 bug 123]', $formatter->link( 'bug 123' ) );
+		$this->assertEquals( '[http://example.com/123 bug 123]', $formatter->link( 'bug 123' ) );
 		$this->assertEquals( '[[Special:Code/Test/456|r456]]', $formatter->link( 'r456' ) );
 
 		// fails, 18614
@@ -26,16 +26,18 @@ class CodeReviewTest extends PHPUnit_Framework_TestCase {
 		// $this->assertEquals( '[[bugzilla:19359|bug 19359]]', $formatter->link( '[[bugzilla:19359|bug 19359]]' ) );
 		
 		// fails, bug 19299
-		// $this->assertEquals( '[http://www.mediawiki.org/wiki/Special:Code/MediaWiki/75762#code-comments r75762 CR comments]',
-		//	$formatter->link( [http://www.mediawiki.org/wiki/Special:Code/MediaWiki/75762#code-comments r75762 CR comments]' ) );
+		 $this->assertEquals( '[http://www.mediawiki.org/wiki/Special:Code/MediaWiki/75762#code-comments r75762 CR comments]',
+			$formatter->link( '[http://www.mediawiki.org/wiki/Special:Code/MediaWiki/75762#code-comments r75762 CR comments]' ) );
 
 		// fails, bug 24279
 		// $this->assertEquals( '[http://svn.wikimedia.org/viewvc/mediawiki/trunk/phase3/includes/api/ApiUpload.php?pathrev=70049&r1=70048&r2=70049 ViewVC]',
 		//	$formatter->link( '[http://svn.wikimedia.org/viewvc/mediawiki/trunk/phase3/includes/api/ApiUpload.php?pathrev=70049&r1=70048&r2=70049 ViewVC]' ) );
 		// $this->assertEquals( '<nowiki>bug 24027</nowiki>', $formatter->link( '<nowiki>bug 24027</nowiki>' ) );
-		// $this->assertEquals( 'http://bugzilla.org/13518#c9', $formatter->link( 'bug 13518#c9' ) );
-		// $this->assertEquals( 'http://bugzilla.org/13518#c9', $formatter->link( 'bug 13518 comment 9' ) );
+		// $this->assertEquals( 'http://example.com/13518#c9', $formatter->link( 'bug 13518#c9' ) );
+		// $this->assertEquals( 'http://example.com/13518#c9', $formatter->link( 'bug 13518 comment 9' ) );
 
+		// $this->assertEquals( '[[Special:Code/Test/10234#c5]]', $formatter->link( 'r10234#c5' ) );
+		
 		// $this->assertEquals( '', $formatter->link( '' ) );
 	}
 }
