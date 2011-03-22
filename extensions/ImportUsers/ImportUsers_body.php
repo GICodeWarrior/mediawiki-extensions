00fa05cc4d37cee2d282b38eb9c259458dd84466
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-22 21:21:56 +0000

    ImportUsers: *added version number to Special:Version credits *renamed some functions as per our coding conventions + fixed a typo in function name *whitespace tweaks *moved some hardcoded colons to the i18n file for better i18n *documentation
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84559 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ImportUsers/ImportUsers_body.php
+++ extensions/ImportUsers/ImportUsers_body.php
@@ -1,14 +1,21 @@
 <?php
 
 class SpecialImportUsers extends SpecialPage {
-	function __construct() {
-		parent::__construct('ImportUsers' , 'import_users' );
+
+	/**
+	 * Constructor -- set up the new special page
+	 */
+	public function __construct() {
+		parent::__construct( 'ImportUsers', 'import_users' );
 	}
 
-	function execute( $par ) {
+	/**
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the special page or null
+	 */
+	public function execute( $par ) {
 		global $wgOut, $wgUser;
-
-		$wgOut->setArticleRelated( false );
 
 		wfLoadExtensionMessages( 'ImportUsers' );
 
@@ -17,16 +24,16 @@ class SpecialImportUsers extends SpecialPage {
 			return;
 		}
 
+		$this->setHeaders();
 
-		$wgOut->setPagetitle( wfMsg( 'importusers' ) );
-		if (IsSet($_FILES['users_file'])) {
-			$wgOut->addHTML( $this->AnalizeUsers($_FILES['users_file'],IsSet($_POST['replace_present'])) );
+		if ( isset( $_FILES['users_file'] ) ) {
+			$wgOut->addHTML( $this->analyzeUsers( $_FILES['users_file'], isset( $_POST['replace_present'] ) ) );
 		} else {
-			$wgOut->addHTML( $this->MakeForm() );
+			$wgOut->addHTML( $this->makeForm() );
 		}
 	}
 
-	function MakeForm() {
+	function makeForm() {
 		global $wgLang;
 
 		$titleObj = SpecialPage::getTitleFor( 'ImportUsers' );
@@ -35,52 +42,59 @@ class SpecialImportUsers extends SpecialPage {
 			wfMsg( 'importusers-login-name' ),
 			wfMsg( 'importusers-password' ),
 			wfMsg( 'importusers-email' ),
-			wfMsg( 'importusers-realname' ) ) );
-		$output ='<form enctype="multipart/form-data" method="post"  action="'.$action.'">';
-		$output.='<dl><dt>'. wfMsg( 'importusers-form-file' ) . '</dt><dd>' . $fileFormat . '.</dd></dl>';
-		$output.='<fieldset><legend>' . wfMsg('importusers-uploadfile') . '</legend>';
-		$output.='<table border=0 a-valign=center width=100%>';
-		$output.='<tr><td align=right width=160>'.wfMsg( 'importusers-form-caption' ).': </td><td><input name="users_file" type="file" size=40 /></td></tr>';
-		$output.='<tr><td align=right></td><td><input name="replace_present" type="checkbox" />'.wfMsg( 'importusers-form-replace-present' ).'</td></tr>';
-		$output.='<tr><td align=right></td><td><input type="submit" value="'.wfMsg( 'importusers-form-button' ).'" /></td></tr>';
-		$output.='</table>';
-		$output.='</fieldset>';
-		$output.='</form>';
+			wfMsg( 'importusers-realname' )
+		) );
+		$output = '<form enctype="multipart/form-data" method="post"  action="' . $action . '">';
+		$output .= '<dl><dt>' . wfMsg( 'importusers-form-file' ) . '</dt><dd>' . $fileFormat . '.</dd></dl>';
+		$output .= '<fieldset><legend>' . wfMsg( 'importusers-uploadfile' ) . '</legend>';
+		$output .= '<table border="0" a-valign="center" width="100%">';
+		$output .= '<tr><td align="right" width="160">' . wfMsg( 'importusers-form-caption' ) .
+			' </td><td><input name="users_file" type="file" size=40 /></td></tr>';
+		$output .= '<tr><td align="right"></td><td><input name="replace_present" type="checkbox" />' .
+			wfMsg( 'importusers-form-replace-present' ) . '</td></tr>';
+		$output .= '<tr><td align="right"></td><td><input type="submit" value="' . wfMsg( 'importusers-form-button' ) . '" /></td></tr>';
+		$output .= '</table>';
+		$output .= '</fieldset>';
+		$output .= '</form>';
 		return $output;
 	}
 
-	function AnalizeUsers($fileinfo,$replace_present) {
-		global $IP, $wgOut;
+	function analyzeUsers( $fileinfo, $replace_present ) {
+		$summary = array(
+			'all' => 0,
+			'added' => 0,
+			'updated' => 0
+		);
+		$filedata = explode( "\n", rtrim( file_get_contents( $fileinfo['tmp_name'] ) ) );
+		$output = '<h2>' . wfMsg( 'importusers-log' ) . '</h2>';
 
-		require_once "$IP/includes/User.php";
-
-		$summary=array('all'=>0,'added'=>0,'updated'=>0);
-		$filedata=explode("\n",rtrim(file_get_contents($fileinfo['tmp_name'])));
-		$output='<h2>'.wfMsg( 'importusers-log' ).'</h2>';
-
-		foreach ($filedata as $line=>$newuserstr) {
-			$newuserarray=explode(',', trim( $newuserstr ) );
-			if (count($newuserarray)<2) {
-				$output.= wfMsg( 'importusers-user-invalid-format', $line+1 ) . '<br />';
+		foreach ( $filedata as $line => $newuserstr ) {
+			$newuserarray = explode( ',', trim( $newuserstr ) );
+			if ( count( $newuserarray ) < 2 ) {
+				$output .= wfMsg( 'importusers-user-invalid-format', $line + 1 ) . '<br />';
 				continue;
 			}
-			if (!IsSet($newuserarray[2])) $newuserarray[2]='';
-			if (!IsSet($newuserarray[3])) $newuserarray[3]='';
-			$NextUser=User::newFromName( $newuserarray[0] );
-			$NextUser->setEmail( $newuserarray[2] );
-			$NextUser->setRealName( $newuserarray[3] );
-			$uid=$NextUser->idForName();
-			if ($uid===0) {
-				$NextUser->addToDatabase();
-				$NextUser->setPassword( $newuserarray[1] );
-				$NextUser->saveSettings();
-				$output.= wfMsg( 'importusers-user-added', $newuserarray[0] ) . '<br />';
+			if ( !isset( $newuserarray[2] ) ) {
+				$newuserarray[2] = '';
+			}
+			if ( !isset( $newuserarray[3] ) ) {
+				$newuserarray[3] = '';
+			}
+			$nextUser = User::newFromName( $newuserarray[0] );
+			$nextUser->setEmail( $newuserarray[2] );
+			$nextUser->setRealName( $newuserarray[3] );
+			$uid = $nextUser->idForName();
+			if ( $uid === 0 ) {
+				$nextUser->addToDatabase();
+				$nextUser->setPassword( $newuserarray[1] );
+				$nextUser->saveSettings();
+				$output .= wfMsg( 'importusers-user-added', $newuserarray[0] ) . '<br />';
 				$summary['added']++;
 			} else {
-				if ($replace_present) {
-					$NextUser->setPassword( $newuserarray[1] );
-					$NextUser->saveSettings();
-					$output.= wfMsg( 'importusers-user-present-update', $newuserarray[0] ).'<br />';
+				if ( $replace_present ) {
+					$nextUser->setPassword( $newuserarray[1] );
+					$nextUser->saveSettings();
+					$output .= wfMsg( 'importusers-user-present-update', $newuserarray[0] ).'<br />';
 					$summary['updated']++;
 				} else {
 					$output .= wfMsg( 'importusers-user-present-no-update', $newuserarray[0] ) . '<br />';
@@ -89,10 +103,10 @@ class SpecialImportUsers extends SpecialPage {
 			$summary['all']++;
 		}
 
-		$output.='<b>'.wfMsg( 'importusers-log-summary' ).'</b><br />';
-		$output.=wfMsg( 'importusers-log-summary-all' ).' : '.$summary['all'].'<br />';
-		$output.=wfMsg( 'importusers-log-summary-added' ).' : '.$summary['added'].'<br />';
-		$output.=wfMsg( 'importusers-log-summary-updated' ).' : '.$summary['updated'];
+		$output .= '<b>' . wfMsg( 'importusers-log-summary' ) . '</b><br />';
+		$output .= wfMsg( 'importusers-log-summary-all', $summary['all'] ) . '<br />';
+		$output .= wfMsg( 'importusers-log-summary-added', $summary['added'] ) . '<br />';
+		$output .= wfMsg( 'importusers-log-summary-updated', $summary['updated'] );
 
 		return $output;
 	}
