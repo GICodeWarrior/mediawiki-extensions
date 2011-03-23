c6e4c0286cb15e3355c8c8b880ad297ea9bd4ae7
junaidpv <junaidpv@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-23 15:14:44 +0000

    Narayam: Add a checkbox under 'Advanced options' section of 'Editing' tab toallow user to disable Narayam IME.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84597 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Narayam.hooks.php
+++ Narayam.hooks.php
@@ -1,32 +1,50 @@
 <?php
+
 /**
  * Hooks for Narayam extension
  * @file
  * @ingroup Extensions
  */
 class NarayamHooks {
-	public static function addModules( $out, $skin ) {
-		$schemes = array_values( self::getSchemes () );
-		if ( count( $schemes ) ) {
-			$out->addModules( $schemes );
-			$out->addModules( 'ext.narayam' );
+
+	protected static $disabled = false;
+
+	public static function addModules($out, $skin) {
+		global $wgUser;
+		if ($wgUser->getOption('narayamDisable')) {
+			// User disabled Narayam
+			return true;
+		}
+		$schemes = array_values(self::getSchemes ());
+		if (count($schemes)) {
+			$out->addModules($schemes);
+			$out->addModules('ext.narayam');
 		}
 		return true;
 	}
-	
-	public static function addConfig( &$vars ) {
-		global $wgNarayamEnabledByDefault, $wgNarayamShortcutKey;
+
+	public static function addConfig(&$vars) {
+		global $wgNarayamEnabledByDefault, $wgNarayamShortcutKey, $wgUser;
+		if ($wgUser->getOption('narayamDisable')) {
+			// User disabled Narayam
+			return true;
+		}
 		$vars['wgNarayamEnabledByDefault'] = $wgNarayamEnabledByDefault;
 		$vars['wgNarayamShortcutKey'] = $wgNarayamShortcutKey;
-		
+
 		return true;
 	}
-	
-	public static function addVariables( &$vars ) {
+
+	public static function addVariables(&$vars) {
+		global $wgUser;
+		if ($wgUser->getOption('narayamDisable')) {
+			// User disabled Narayam
+			return true;
+		}
 		$vars['wgNarayamAvailableSchemes'] = self::getSchemes(); // Note: scheme names must be keys, not values
 		return true;
 	}
-	
+
 	/**
 	 * Get the available schemes for the user and content language
 	 * @return array( scheme name => module name )
@@ -34,10 +52,21 @@ class NarayamHooks {
 	protected static function getSchemes() {
 		global $wgLanguageCode, $wgLang, $wgNarayamSchemes;
 		$userlangCode = $wgLang->getCode();
-		$contlangSchemes = isset( $wgNarayamSchemes[$wgLanguageCode] ) ?
-			$wgNarayamSchemes[$wgLanguageCode] : array();
-		$userlangSchemes = isset( $wgNarayamSchemes[$userlangCode] ) ?
-			$wgNarayamSchemes[$userlangCode] : array();
+		$contlangSchemes = isset($wgNarayamSchemes[$wgLanguageCode]) ?
+				$wgNarayamSchemes[$wgLanguageCode] : array();
+		$userlangSchemes = isset($wgNarayamSchemes[$userlangCode]) ?
+				$wgNarayamSchemes[$userlangCode] : array();
 		return $userlangSchemes + $contlangSchemes;
 	}
+
+	public static function addPreference($user, &$preferences) {
+		// A checkbox in preferences to diable Narayam
+		$preferences['narayamDisable'] = array(
+			'type' => 'toggle',
+			'label-message' => 'narayam-disable-preference', // a system message
+			'section' => 'editing/advancedediting',	// under 'Advanced options' section of 'Editing' tab
+		);
+		return true;
+	}
+
 }
