48e499a9b7f46727a586fb96e510ca77e2f41fc8
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-23 21:58:41 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-03-23 21:48:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84639 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PrintVersion.i18n.php
+++ PrintVersion.i18n.php
@@ -20,13 +20,44 @@ $messages['en'] = array(
 	'printversion-inexistent-book' => 'Book not found. Please enter the name of an existing book to get its print version.',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ * @author Purodha
+ */
+$messages['de'] = array(
+	'printversion' => 'Druckversion',
+	'printversion-desc' => 'Stellt eine Spezialseite zum [[Special:PrintVersion|Erstellen einer Druckversion]] eines Buchs zur Verfügung',
+	'printversion-no-book' => 'Bitte den Namen des Buchs angeben, für das eine Druckversion erstellt werden soll.',
+	'printversion-inexistent-book' => 'Das Buch wurde nicht gefunden. Bitte den Namen eines vorhandenen Buchs angeben, für das eine Druckversion erstellt werden soll.',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'printversion' => 'Dröck_Version',
+	'printversion-desc' => 'Määd en [[Special:PrintVersion|Dröck_Version]] vun enem Booch.',
+	'printversion-no-book' => 'Bes esu jood un donn dä  Name vun enem Booch aanjävve, öm aan däm sing Dröck_Version ze kumme.',
+	'printversion-inexistent-book' => 'Mer han dat Booch nit jefonge. Bes esu jood un donn ene reshteteje Name vun enem Booch aanjävve, wadd et jit, öm aan däm sing Dröck_Version draan ze kumme.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'printversion' => 'Верзија за печатење',
+	'printversion-desc' => 'Создава [[Special:PrintVersion|верзија за печатење]] на книга',
+	'printversion-no-book' => 'Внесете го името на книгата за да ја добиете верзијата за печатење',
+	'printversion-inexistent-book' => 'Не ја најдов книгата. Внесете име на постоечка книга за да ја добиете нејзината верзија за печатење.',
+);
+
 /** Portuguese (Português)
  * @author Raylton P. Sousa <raylton.sousa@gmail.com>
-*/
+ */
 $messages['pt'] = array(
-	'printversion'                 => 'Versão para impressão',
-	'printversion-desc'            => '[[Special:PrintVersion|Gera uma versão para impressão de um livro]]',
-	'printversion-no-book'         => 'Por favor informe o nome de um livro para obter sua versão para impressão.',
+	'printversion' => 'Versão para impressão',
+	'printversion-desc' => '[[Special:PrintVersion|Gera uma versão para impressão de um livro]]',
+	'printversion-no-book' => 'Por favor informe o nome de um livro para obter sua versão para impressão.',
 	'printversion-inexistent-book' => 'Livro não encontrado. Por favor informe o nome de um livro existente para obter sua versão para impressão.',
 );
 
