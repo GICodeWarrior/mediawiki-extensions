013a51b819c5b4d31169dd9015249e6998a1e0e7
purodha <purodha@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-23 20:52:22 +0000

    Better wording of English message.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84630 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BookManager/PrintVersion.i18n.php
+++ extensions/BookManager/PrintVersion.i18n.php
@@ -4,18 +4,25 @@
  *
  * @file
  * @ingroup Extensions
+ * @author Raylton P. Sousa <raylton.sousa@gmail.com>
  */
 
 $messages = array();
 
+/** English
+ * @author Raylton P. Sousa <raylton.sousa@gmail.com>
+ * @author Purodha
+ */
 $messages['en'] = array(
 	'printversion'                 => 'Print version',
 	'printversion-desc'            => 'Generates a [[Special:PrintVersion|print version]] of a book',
-	'printversion-no-book'         => 'Please inform the name of a book to get its print version.',
-	'printversion-inexistent-book' => 'Book not found. Please inform the name of an existing book to get its print version.',
+	'printversion-no-book'         => 'Please enter the name of a book to get its print version.',
+	'printversion-inexistent-book' => 'Book not found. Please enter the name of an existing book to get its print version.',
 );
 
-/** Portuguese (Português) */
+/** Portuguese (Português)
+ * @author Raylton P. Sousa <raylton.sousa@gmail.com>
+*/
 $messages['pt'] = array(
 	'printversion'                 => 'Versão para impressão',
 	'printversion-desc'            => '[[Special:PrintVersion|Gera uma versão para impressão de um livro]]',
