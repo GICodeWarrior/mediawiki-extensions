61c1cea58e994724912251ada06608de48768564
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-24 17:04:02 +0000

    Remove wfLoadExtensionMessages() in all trunk extensions (do not backport) * This function has been a no-op since 1.16 * As of 1.18, this throws warnings to all callers * It will be removed *entirely* in 1.20 according to documentation * I skipped Semantic* on purpose
    This means: extensions IN TRUNK now require AT LEAST MediaWiki 1.16 or above.
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84686 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- JSKit.php
+++ JSKit.php
@@ -70,7 +70,7 @@ function efJSKitSetup( &$parser ) {
 	if( $wgJSKitTypes['reviews'] ) {
 		$parser->setHook( 'review', 'efJSKitReview' );
 	}
-	wfLoadExtensionMessages( 'JSKit' );
+	
 	return true;
 }
 
