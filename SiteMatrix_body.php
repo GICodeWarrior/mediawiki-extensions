0d61540176057663e0ddaeac84dbdead3d7edab8
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-24 23:47:43 +0000

    Followup r84671, match implementation match
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84719 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SiteMatrix_body.php
+++ SiteMatrix_body.php
@@ -248,7 +248,7 @@ class SiteMatrix {
 	 * @param array $results
 	 * @return void
 	 */
-	public static function APIQuerySiteInfoGeneralInfo( &$module, &$results ) {
+	public static function APIQuerySiteInfoGeneralInfo( $module, &$results ) {
 		global $wgDBname, $wgConf;
 
 		$matrix = new SiteMatrix();
