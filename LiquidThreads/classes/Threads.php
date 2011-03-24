5e4f9e8359a5e3c787c2abfa03e67568d85b01a0
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-24 17:04:02 +0000

    Remove wfLoadExtensionMessages() in all trunk extensions (do not backport) * This function has been a no-op since 1.16 * As of 1.18, this throws warnings to all callers * It will be removed *entirely* in 1.20 according to documentation * I skipped Semantic* on purpose
    This means: extensions IN TRUNK now require AT LEAST MediaWiki 1.16 or above.
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84686 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- LiquidThreads/classes/Threads.php
+++ LiquidThreads/classes/Threads.php
@@ -59,7 +59,6 @@ class Threads {
 	public static function createTalkpageIfNeeded( $talkpage ) {
 		if ( ! $talkpage->exists() ) {
 			try {
-				wfLoadExtensionMessages( 'LiquidThreads' );
 				$talkpage->doEdit(
 					"",
 					wfMsgForContent( 'lqt_talkpage_autocreate_summary' ),
@@ -186,7 +185,6 @@ class Threads {
 	}
 
 	static function newThreadTitle( $subject, $article ) {
-		wfLoadExtensionMessages( 'LiquidThreads' );
 
 		$base = $article->getTitle()->getPrefixedText() . "/$subject";
 
@@ -198,7 +196,6 @@ class Threads {
 	}
 
 	static function newReplyTitle( $thread, $user ) {
-		wfLoadExtensionMessages( 'LiquidThreads' );
 		$topThread = $thread->topmostThread();
 
 		$base = $topThread->title()->getText() . '/'
