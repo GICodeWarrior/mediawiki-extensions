e39b0e43dd69771687cb19cfdbc7e579d497e1aa
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-24 17:04:02 +0000

    Remove wfLoadExtensionMessages() in all trunk extensions (do not backport) * This function has been a no-op since 1.16 * As of 1.18, this throws warnings to all callers * It will be removed *entirely* in 1.20 according to documentation * I skipped Semantic* on purpose
    This means: extensions IN TRUNK now require AT LEAST MediaWiki 1.16 or above.
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84686 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- CategoryWatch.php
+++ CategoryWatch.php
@@ -234,7 +234,7 @@ class CategoryWatch {
 function wfSetupCategoryWatch() {
 	global $wgCategoryWatch;
 
-	wfLoadExtensionMessages( 'CategoryWatch' );
+	
 
 	# Instantiate the CategoryWatch singleton now that the environment is prepared
 	$wgCategoryWatch = new CategoryWatch();
