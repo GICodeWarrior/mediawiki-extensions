4c1c7f76928251d2f6c97f7991d716f285c12a0f
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-28 19:04:54 +0000

    Adding user bucket support
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84915 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ patches/ClickTrackingUserProperties.sql
@@ -0,0 +1,21 @@
+--
+-- Schema for ClickTrackingUserProperties
+--
+
+CREATE TABLE IF NOT EXISTS /*_*/click_tracking_user_properties (
+
+	-- session id from clicktracking table
+	session_id varbinary(255) NOT NULL,
+
+	-- property name
+	property_name varbinary(255),
+
+	-- property value
+	property_value varbinary(255),
+	
+	-- property version
+	property_version INTEGER
+		
+) /*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/click_tracking_user_properties_session_idx ON /*_*/click_tracking_user_properties (session_id);
