c33d96523f0b9db1d46fd633b01000c86fe1237f
raylton.sousa <raylton.sousa@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-28 15:49:11 +0000

    adding keyboard navigation
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@84906 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- bookmanager.js
+++ bookmanager.js
@@ -1,1 +1,16 @@
 /*Script here */
+/* Navegação pelas setas do teclado */
+	$nav = $( ' .mw-book-navigation ' );
+	$prev = $nav.find( ' .mw-prev a' );
+	$next = $nav.find( ' .mw-next a' );
+	if ( $prev[0] ) {
+		$(document).bind('keydown', 'left', function(){
+			location.href = $prev[0].href;
+		});
+	}
+	if ( $next[0] ) {
+		$(document).bind('keydown', 'right', function(){
+			 location.href = $next[0].href;
+		});
+	}
+
