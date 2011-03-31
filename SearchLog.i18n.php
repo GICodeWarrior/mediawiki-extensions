1d180a2feeb581399d9be060bcbf90b3758f2f2a
purodha <purodha@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-03-31 17:41:36 +0000

    Minor: alter "unicode" -> "Unicode" in message text.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@85074 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SearchLog.i18n.php
+++ SearchLog.i18n.php
@@ -8,13 +8,14 @@ $messages = array();
 /**
  * English
  * @author Nad
+ * @author Purodha
  */
 $messages['en'] = array(
 	'searchlog'            => 'Search Log',
 	'searchlog-desc'       => 'Logs usage of the search box and allows reporting of keyword totals during given time periods',
 	'searchlog-cantopen'   => 'Could not open log file <tt>$1</tt>',
 	'searchlog-heading'    => 'Search keywords used over $1 period',
-	'searchlog-unicode'    => 'Display raw unicode',
+	'searchlog-unicode'    => 'Display raw Unicode',
 	'searchlog-timeperiod' => 'Select time period:',
 	'searchlog-entire'     => 'Entire log',
 	'searchlog-phrase'     => 'Search phrase',
