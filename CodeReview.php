05b704a80bae10554d39b77596692f182aecb00e
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-02 23:08:49 +0000

    Make update row check same as the one inserted
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@85218 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- CodeReview.php
+++ CodeReview.php
@@ -251,7 +251,7 @@ function efCodeReviewSchemaUpdates( $updater ) {
 				"$base/archives/codereview-cp_action_char.sql", true ) );
 		}
 
-		if ( !$updater->updateRowExists( 'make code_prop_changes char' ) ) {
+		if ( !$updater->updateRowExists( 'make code_prop_changes varchar' ) ) {
 			$updater->addExtensionUpdate( array( 'modifyField', 'code_prop_changes', 'cpc_attrib',
 				"$base/archives/codereview-cpc_attrib_varchar.sql", true ) );
 		}
