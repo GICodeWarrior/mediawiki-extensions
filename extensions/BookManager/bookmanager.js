db689614dff861e1d0a9c18d68ce48121fb3b285
raylton.sousa <raylton.sousa@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-03 10:16:41 +0000

    *Decreasing CSS *Making JavaScript work better *Correcting bug that made ​​the system message did not work
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@85237 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BookManager/bookmanager.js
+++ extensions/BookManager/bookmanager.js
@@ -1,16 +1,16 @@
-/*Script here */
-/* Navegação pelas setas do teclado */
+/* Navigation by arrow keys */
+$(function() {
 	$nav = $( ' .mw-book-navigation ' );
-	$prev = $nav.find( ' .mw-prev a' );
-	$next = $nav.find( ' .mw-next a' );
-	if ( $prev[0] ) {
+	$prev = $nav.find( ' .mw-prev a ' );
+	$next = $nav.find( ' .mw-next a ' );
+	if ( $prev.length ) {
 		$(document).bind('keydown', 'left', function(){
 			location.href = $prev[0].href;
 		});
 	}
-	if ( $next[0] ) {
+	if ( $next.length ) {
 		$(document).bind('keydown', 'right', function(){
 			 location.href = $next[0].href;
 		});
 	}
-
+});
