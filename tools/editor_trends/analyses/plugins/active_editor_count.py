f9d40777ded407112be1b7a51ae6eb530b73bd98
diederik <diederik@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-05 17:30:01 +0000

    Active editor count plugin works with new version of Wikilytics.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@85448 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/editor_trends/analyses/plugins/active_editor_count.py
+++ tools/editor_trends/analyses/plugins/active_editor_count.py
@@ -12,17 +12,24 @@ See the GNU General Public License for more details, at
 http://www.fsf.org/licenses/gpl.html
 '''
 
-__author__ = '''\n'''.join(['Diederik van Liere (dvanliere@gmail.com)', ])
+__author__ = '''\n'''.join(['Diederik van Liere (dvanliere@gmail.com)'])
 __email__ = 'dvanliere at gmail dot com'
 __date__ = '2011-01-25'
 __version__ = '0.1'
 
+from datetime import datetime
 
 def active_editor_count(var, editor, **kwargs):
-    monthly_edits = editor['monthly_edits']
-    for year in xrange(ds.count.min_year, var.max_year):
-        for month in xrange(1, 13):
-            if monthly_edits[str(year)][str(month)] >= var.cutoff:
-                datum = datetime.date(year, month, 1)
-                var.add(datum, {0:1})
+    '''
+    If you have questions about how to use this plugin, please visit:
+    http://meta.wikimedia.org/wiki/Wikilytics_Plugins
+    '''
+    edits = editor['edit_count']
+    years = edits.keys()
+    for year in years:
+        months = edits[year].keys()
+        for month in months:
+            if edits[year][month] >= var.cutoff:
+                date = datetime(int(year), int(month), 1)
+                var.add(date, 1)
     return var
