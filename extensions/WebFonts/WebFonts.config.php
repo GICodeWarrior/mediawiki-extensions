aa4c7c0ea437af8f813cc17cbf501262ba5fbd37
santhosh <santhosh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-06 16:50:17 +0000

    Initial set of files for WebFonts Extension http://www.mediawiki.org/wiki/Extension:WebFonts.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@85549 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/WebFonts/WebFonts.config.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Configuration file for webfonts
+ * First font is the default font for the language
+ * @file
+ * @ingroup Extensions
+ */
+$fontsPath = "$wgScriptPath/extensions/WebFonts/fonts";
+$wgWebFonts = array(
+	'fonts' => array(
+		'RufScript' => array(
+			'eot' => "$fontsPath/en/Rufscript.eot",
+			'ttf' => "$fontsPath/en/Rufscript.ttf",
+			'woff' => "$fontsPath/en/Rufscript.woff",
+		),
+
+		'Perizia' => array(
+			'eot' => "$fontsPath/en/Perizia.eot",
+			'ttf' => "$fontsPath/en/Perizia.ttf",
+			'woff' => "$fontsPath/en/Perizia.woff",
+		),
+		'Ubuntu' => array(
+			'eot' => "$fontsPath/en/ubuntu-r-webfont.eot",
+			'ttf' => "$fontsPath/en/ubuntu-r.ttf",
+			'woff' => "$fontsPath/en/ubuntu-r-webfont.woff",
+			'svg' => "$fontsPath/en/ubuntu-r-webfont.svg",
+		),
+		'Dyuthi' => array(
+			'eot' => "$fontsPath/ml/Dyuthi.eot",
+			'ttf' => "$fontsPath/ml/Dyuthi.ttf",
+			'woff' => "$fontsPath/ml/Dyuthi.woff",
+			'size' => 32,
+			'normalization' => array(
+				"ൾ" => "ള്‍",
+				"ൻ" => "ന്‍",
+				"ർ" => "ര്‍",
+				"ൺ "=> "ണ്‍",
+				"ൽ" => "ല്‍",
+				"ൿ" => "ക്‍ ",
+				"ൻ‍റ" => "ന്റ",
+				"ന്‍റെ" => "ന്റെ"
+			)
+		),
+
+		'Meera' => array(
+			'eot' => "$fontsPath/ml/Meera.eot",
+			'ttf' => "$fontsPath/ml/Meera.ttf",
+			'woff' => "$fontsPath/ml/Meera.woff",
+			'size' => 20,
+			'normalization' => array(
+				"ൾ" => "ള്‍",
+				"ൻ" => "ന്‍",
+				"ർ" => "ര്‍",
+				"ൺ "=> "ണ്‍",
+				"ൽ" => "ല്‍",
+				"ൿ" => "ക്‍ ",
+				"ൻ‍റ" => "ന്റ",
+				"ന്‍റെ" => "ന്റെ"
+			)
+		),
+
+		'Rachana' => array(
+			'eot' => "$fontsPath/ml/Rachana.eot",
+			'ttf' => "$fontsPath/ml/Rachana.ttf",
+			'woff' => "$fontsPath/ml/Rachana.woff",
+			'normalization' => array(
+				"ൾ" => "ള്‍",
+				"ൻ" => "ന്‍",
+				"ർ" => "ര്‍",
+				"ൺ "=> "ണ്‍",
+				"ൽ" => "ല്‍",
+				"ൿ" => "ക്‍ ",
+				"ൻ‍റ" => "ന്റ",
+				"ന്‍റെ" => "ന്റെ"
+			)
+		),
+		'RaghuMalayalam' => array(
+			'eot' => "$fontsPath/ml/RaghuMalayalam.eot",
+			'ttf' => "$fontsPath/ml/RaghuMalayalam.ttf",
+			'woff' => "$fontsPath/ml/RaghuMalayalam.woff",
+			'normalization' => array(
+				"ൾ" => "ള്‍",
+				"ൻ" => "ന്‍",
+				"ർ" => "ര്‍",
+				"ൺ "=> "ണ്‍",
+				"ൽ" => "ല്‍",
+				"ൿ" => "ക്‍ ",
+				"ൻ‍റ" => "ന്റ",
+				"ന്‍റെ" => "ന്റെ"
+			)
+		),
+	),
+
+	'languages' => array(
+		'en' => array( 'RufScript', 'Perizia', 'Ubuntu' ),
+		'ml' => array( 'Meera', 'Rachana' , 'Dyuthi', 'RaghuMalayalam'),
+	),
+);
---
+++ extensions/WebFonts/WebFonts.i18n.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Internationalisation file for extension webfonts.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author santhosh
+*/
+$messages['en'] = array(
+	'webfonts'         => 'WebFonts',
+	'webfonts-desc'    => 'Embed fonts on mediawiki pages',
+	'webfonts-load'    => 'Select font',
+	'webfonts-disable-preference' => 'Disable Font Embedding(WebFonts)',
+
+);
+
+
+/** Malayalam 
+ * @author Santhosh
+ */
+$messages['ml'] = array(
+	'webfonts'         => 'വെബ്ഫോണ്ടുകള്‍',
+	'webfonts-desc'    => 'മീഡിയവിക്കി താളുകളില്‍ ഫോണ്ടുകള്‍ എംബെഡ് ചെയ്യുക',
+	'webfonts-load'    => 'ഫോണ്ടുകള്‍',
+	'webfonts-disable-preference' => 'ഫോണ്ടു് എംബഡ് ചെയ്യാതിരിക്കുക(വെബ്ഫോണ്ട്)',
+);
+
