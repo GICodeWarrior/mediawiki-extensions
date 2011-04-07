96c2aa71d7081ec241ab6d8c0773952bc239eaca
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-07 22:41:35 +0000

    added documentation, more descriptive variables, and fixed some issues in the logic
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@85649 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/editor_trends/analyses/plugins/taxonomy_burnout.py
+++ tools/editor_trends/analyses/plugins/taxonomy_burnout.py
@@ -18,44 +18,109 @@ __email__ = 'dvanliere at wikimedia dot org'
 __date__ = '2011-01-25'
 __version__ = '0.1'
 
+from datetime import date
 
-def taxonomy_burnout(var, editor, **kwargs):
-    '''
+'''
+    taxonomy_burnout
+    ================
+    
+    This is a Taxonomy project metric.  The "Editor Burnout" metric is intended to measure 
+    editors that make a large number of edits in a single month and then make relatively few
+    in the following months.  The exact number of edits and months are defined by the 
+    following gloabal variables:
+    
+    ONE_MONTH_EDIT_COUNT_CUTOFF
+    MONTHS_AFTER_CUTOFF_TO_RECORD_BURNOUT
+    
+    While the average number of edits per month beyond MONTHS_AFTER_CUTOFF_TO_RECORD_BURNOUT 
+    that classify an editor as a "burned out" is defined in:
+    
+    CUTOFF_RATE_FOR_BURNOUT_EDITORS
+    
+    More documentation may be found at: 
+    
+    http://meta.wikimedia.org/wiki/Contribution_Taxonomy_Project/Research_Questions
+    
     If you have questions about how to use this plugin, please visit:
     http://meta.wikimedia.org/wiki/Wikilytics_Plugins
-    '''
+    
+'''
+def taxonomy_burnout(var, editor, **kwargs):
+    
+    """ These parameters can be used to tune the burnout analyses """
+    global MONTHS_AFTER_CUTOFF_TO_RECORD_BURNOUT
+    global CUTOFF_RATE_FOR_BURNOUT_EDITORS
+    global ONE_MONTH_EDIT_COUNT_CUTOFF
+    
+    MONTHS_AFTER_CUTOFF_TO_RECORD_BURNOUT = 6
+    CUTOFF_RATE_FOR_BURNOUT_EDITORS = 10
+    ONE_MONTH_EDIT_COUNT_CUTOFF = 149
+    
+    
+    """ Record editor properties """
     new_wikipedian = editor['new_wikipedian']
     edits = editor['edit_count']
-    cutoff = kwargs.get('cutoff', 149)
+    cutoff = kwargs.get('cutoff', ONE_MONTH_EDIT_COUNT_CUTOFF)
     username = editor['username']
 
+    made_cutoff = False
     burnout = False
-    sum = 0.0
-    count = 0.0
+    
+    total_edits_since_cutoff = 0.0
+    total_months_since_cutoff = 0.0
+    
+    zero_edit_months = dict()
 
+    """ 
+        new_wikipedian is False if the user is not classified as a wikipedian 
+        and returns the date of their becoming a Wikipedian otherwise 
+        
+        In that case iterate through the months to present trying to find:
+            (1) If they meet ONE_MONTH_EDIT_COUNT_CUTOFF 
+            (2) If the fall into the burnout category thereafter
+    """ 
+            
     if new_wikipedian:
         years = edits.keys()
+        
         for year in years:
             months = edits[year].keys()
+            
             for month in months:
-                try:
-                    if edits[year][month].get('0', 0) > cutoff:
-                        burnout = True
-                    if burnout == True:
-                        n = edits[year][month].get('0', 0)
-                        sum += n
-                        if n > 0:
-                            count += 1.0
-                except (AttributeError, KeyError):
-                    print 'Editor %s does not have data for year: %s and month %s. Data: %s' \
-                    % (username, year, month, edits[year])
-
-        if burnout and sum / count > 10:
-            avg_edit = sum / count
+                
+                if edits[year][month].get('0', 0) > cutoff:
+                    made_cutoff = True
+                if made_cutoff == True:
+                    
+                    """ Count every month after the cutoff is made """
+                    total_months_since_cutoff += 1.0
+                    
+                    """ Handle cases where no edits were recorded for a given month """
+                    try:
+                        edit_count_month = edits[year][month].get('0', 0)
+                    except (AttributeError, KeyError):
+                        edit_count_month = 0
+                        
+                        """ Record that this editor had no edits on this month """
+                        try:
+                            zero_edit_months[username].append(date(year, month, 1))
+                        except KeyError:
+                            zero_edit_months[username] = list()
+                            zero_edit_months[username].append(date(year, month, 1))
+                            
+                    total_edits_since_cutoff = total_edits_since_cutoff + edit_count_month
+                            
+                    if total_edits_since_cutoff / total_months_since_cutoff < CUTOFF_RATE_FOR_BURNOUT_EDITORS and \
+                    total_months_since_cutoff > MONTHS_AFTER_CUTOFF_TO_RECORD_BURNOUT and burnout == False:
+                         burnout = True
+                         burnout_date = date(year, month, 1)
+                             
+        if burnout:
+            avg_edit = total_edits_since_cutoff / total_months_since_cutoff
 
             try:
-                var.add(new_wikipedian, avg_edit, {'username' : username})
-            except Exception, error:
+                var.add(burnout_date, avg_edit, {'username' : username})
+            except Exception as error:
                 print 'user: %s error: %s' % (username, error)
 
     return var
