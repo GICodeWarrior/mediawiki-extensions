a275f4df50ebf0303142775c683fb9b1acabbae2
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-14 09:47:46 +0000

    CustomUserSignup: initial cleanup
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@86036 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CustomUserSignup/CustomUserSignup.hooks.php
+++ extensions/CustomUserSignup/CustomUserSignup.hooks.php
@@ -7,42 +7,50 @@
  */
 
 class CustomUserSignupHooks {
-	
-	public static function userCreateForm(&$template){
-		if(isset($_GET["campaign"])){
-			$campaign = $_GET["campaign"];
+
+	public static function userCreateForm( &$template ) {
+		if( isset( $_GET['campaign'] ) ) {
+			$campaign = $_GET['campaign'];
 			$newTemplate;
-			if(get_class($template) == "UserloginTemplate"){
+			if( get_class( $template ) == 'UserloginTemplate' ) {
 				$newTemplate = new CustomUserloginTemplate();
-				$template->data["action"] = "{$template->data["action"]}&campaign=$campaign";
-				$template->data["link"] =
-					preg_replace("/type\=signup/", "campaign=$campaign&amp;type=signup", $template->data["link"]);
-			}
-			else if(get_class($template) == "UsercreateTemplate"){
+				$template->data['action'] = "{$template->data['action']}&campaign=$campaign";
+				$template->data['link'] =
+					preg_replace(
+						'/type\=signup/',
+						"campaign=$campaign&amp;type=signup",
+						$template->data['link']
+					);
+			} elseif( get_class( $template ) == 'UsercreateTemplate' ) {
 				$newTemplate = new CustomUsercreateTemplate();
-				$template->data["action"] = "{$template->data["action"]}&campaign=$campaign";
-				$template->data["link"] =
-					preg_replace("/type\=login\&amp;/", "type=login&amp;campaign=$campaign&amp;", $template->data["link"]);
+				$template->data['action'] = "{$template->data['action']}&campaign=$campaign";
+				$template->data['link'] =
+					preg_replace(
+						'/type\=login\&amp;/',
+						"type=login&amp;campaign=$campaign&amp;",
+						$template->data['link']
+					);
+			} else {
+				return true;
 			}
-			else return true;
-			
+
 			$newTemplate->data = $template->data;
 			$newTemplate->translator = $template->translator;
 			$template = $newTemplate;
 		}
-		
-		return true;	
-	}
-	
-	public static function welcomeScreen(&$welcome_creation_msg, &$injected_html){
-		if(isset($_GET["campaign"])){
-			$campaign = $_GET["campaign"];
-			
-			if(wfMsg( "customusertemplate-$campaign-welcomecreation") != "&lt;customusertemplate-$campaign-welcomecreation&gt;"){
-				$welcome_creation_msg = "customusertemplate-$campaign-welcomecreation";	
-			}
-		}	
+
 		return true;
 	}
-	
+
+	public static function welcomeScreen( &$welcomeCreationMsg, &$injected_html ) {
+		if( isset( $_GET['campaign'] ) ) {
+			$campaign = $_GET['campaign'];
+
+			if( wfMsg( "customusertemplate-$campaign-welcomecreation" ) != "&lt;customusertemplate-$campaign-welcomecreation&gt;" ) {
+				$welcomeCreationMsg = "customusertemplate-$campaign-welcomecreation";
+			}
+		}
+		return true;
+	}
+
 }
