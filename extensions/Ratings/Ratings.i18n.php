25083ee701824e1a1afcaaf9ff1c6a7a3976c51a
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-14 10:13:19 +0000

    Better extension description, Fix typo. Add extension to Translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@86040 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Ratings/Ratings.i18n.php
+++ extensions/Ratings/Ratings.i18n.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Internationalization file for the Include WP extension.
+ * Internationalization file for the Ratings extension.
  *
  * @since 0.1
  *
- * @file IncludeWP.i18n.php
- * @ingroup IncludeWP
+ * @file Ratings.i18n.php
+ * @ingroup Ratings
  *
  * @licence GNU GPL v3 or later
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
@@ -18,11 +18,11 @@ $messages = array();
  * @author Jeroen De Dauw
  */
 $messages['en'] = array(
-	'ratings-desc' => 'Simple rating extension for MediaWiki',
+	'ratings-desc' => 'Allows users to rate different "properties" of pages',
 
 	// Rating stars
 	'ratings-par-page' => 'The page the rating applies to.',
-	'ratings-par-tag' => 'Tha rating tag. The tag indicates what "property" of the page gets rated.',
+	'ratings-par-tag' => 'The rating tag. The tag indicates what "property" of the page gets rated.',
 	'ratings-par-showdisabled' => 'Show ratings when the user can not vote (in read-only mode).',
 	'ratings-par-incsummary' => 'Show a summary of the current votes above the rating element?',
 
