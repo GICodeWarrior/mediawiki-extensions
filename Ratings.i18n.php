70481d322746686c35a2ec773b12a223028198ac
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-15 20:02:58 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-04-15 19:40:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@86136 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Ratings.i18n.php
+++ Ratings.i18n.php
@@ -38,6 +38,16 @@ $messages['qqq'] = array(
 	'ratings-desc' => '{{desc}}',
 );
 
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author Wizardist
+ */
+$messages['be-tarask'] = array(
+	'ratings-desc' => 'Дазваляе карыстальнікам ацэньваць розныя «ўласьцівасьці» старонкі',
+	'ratings-par-showdisabled' => 'Паказваць рэйтынг, калі карыстальнік ня можа галасаваць (толькі для чытаньня)',
+	'ratings-current-score' => 'Бягучы карыстальніцкі рэйтынг: $1 ($2 {{PLURAL:2|ацэнка|ацэнкі|ацэнак}})',
+	'ratings-no-votes-yet' => 'Ніхто яшчэ не даваў ацэнак.',
+);
+
 /** German (Deutsch)
  * @author Kghbln
  */
@@ -51,6 +61,19 @@ $messages['de'] = array(
 	'ratings-no-votes-yet' => 'Bislang wurde dies von niemanden bewertet.',
 );
 
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'ratings-desc' => 'Permitter al usatores de evalutar diverse "proprietates" de paginas',
+	'ratings-par-page' => 'Le pagina al qual le evalutation pertine.',
+	'ratings-par-tag' => 'Le etiquetta de evalutation. Le etiquetta specifica le "proprietate" del pagina que es evalutate.',
+	'ratings-par-showdisabled' => 'Monstrar le evalutationes si le usator non pote votar (in modo de lectura sol).',
+	'ratings-par-incsummary' => 'Monstrar un summario del votos actual supra le elemento de evalutation?',
+	'ratings-current-score' => 'Evalutation actual del usator: $1 ($2 {{PLURAL:$2|evalutation|evalutationes}})',
+	'ratings-no-votes-yet' => 'Nemo ha ancora evalutate isto.',
+);
+
 /** Luxembourgish (Lëtzebuergesch)
  * @author Robby
  */
@@ -58,5 +81,31 @@ $messages['lb'] = array(
 	'ratings-desc' => 'Erméiglecht et Benotzer fir verschidden "Eegeschaften" vu Säiten ze bewäerten',
 	'ratings-par-showdisabled' => 'Bewäertunge weisen wann de Benotzer net mat ofstëmme kann (Read-Only Modus)',
 	'ratings-no-votes-yet' => 'Et huet nach keen dëst bewäert.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'ratings-desc' => 'Им овозможува на корисниците да оценуваат разни „својства“ на страниците',
+	'ratings-par-page' => 'На која страница се однесува оценкава.',
+	'ratings-par-tag' => 'Ознака за оценка. Ознаката покажува кое „својство“ на страницата се оценува.',
+	'ratings-par-showdisabled' => 'Прикажувај оценки кога корисникот не може да гласа (во режим „само читање“).',
+	'ratings-par-incsummary' => 'Да прикажувам опис на тековните гласови над елементот за оценка?',
+	'ratings-current-score' => 'Тековна корисничка оценка: $1 ($2 {{PLURAL:$2|оценка|оценки}})',
+	'ratings-no-votes-yet' => 'Сè уште никој го нема оценето ова.',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'ratings-desc' => 'Nagpapahintulot sa mga tagagamit na antasan ang iba\'t ibang mga "pag-aari" ng mga pahina',
+	'ratings-par-page' => 'Ang pahinang malalapatan ng kaantasan.',
+	'ratings-par-tag' => 'Ang tatak ng antas. Nagpapahiwatig ang tatak ng kung anong "pag-aari" ng pahina ang aantasan.',
+	'ratings-par-showdisabled' => 'Ipakita ang mga antas kapag hindi makakaboto ang tagagamit (nasa pamamaraang mababasa lamang).',
+	'ratings-par-incsummary' => 'Magpakita ng isang buod ng pangkasalukuyang mga boto na nasa itaas ng sangkap na antas?',
+	'ratings-current-score' => 'Pangkasalukuyang antas ng tagagamit: $1 ($2 {{PLURAL:$2|antas|mga antas}})',
+	'ratings-no-votes-yet' => 'Wala pang isang nag-aantas nito.',
 );
 
