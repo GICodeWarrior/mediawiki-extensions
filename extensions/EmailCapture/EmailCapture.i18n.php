b402cba2f730db3117f8cdf2232ab81ae6e39625
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-15 07:30:06 +0000

    Tweak messages.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@86107 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/EmailCapture/EmailCapture.i18n.php
+++ extensions/EmailCapture/EmailCapture.i18n.php
@@ -12,20 +12,21 @@ $messages = array();
  * @author Trevor Parscal
  */
 $messages['en'] = array(
-	'emailcapture' => 'E-mail Capture',
+	'emailcapture' => 'E-mail address capture',
 	'emailcapture-desc' => 'Capture e-mail addresses, and allow users to verify them through e-mail',
 	'emailcapture-failure' => "Your e-mail was '''not''' verified.",
-	'emailcapture-response-subject' => '{{SITENAME}} E-mail Verification',
+	'emailcapture-response-subject' => '{{SITENAME}} e-mail address verification',
 	'emailcapture-response-body' => 'To verify your e-mail address, following this link:
-	$1
+$1
 
 You can also visit:
-	$2
+$2
+
 and enter the following verification code:
-	$3
+$3
 
 Thank you for verifying your e-mail address.',
-	'emailcapture-success' => 'Your e-mail was successfully verified.',
+	'emailcapture-success' => 'Your e-mail address was successfully verified.',
 	'emailcapture-instructions' => 'To verify your e-mail address, enter the code that was emailed to you and click verify.',
 	'emailcapture-verify' => 'Verification code:',
 	'emailcapture-submit' => 'Verify e-mail address',
