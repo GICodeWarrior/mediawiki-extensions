e1514c0298f72e4eef236242c2b5f540fcb0fe0c
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-19 14:22:05 +0000

    EmailCapture fixes (also untested): * Fix indices in SQL schema (PRIMARY KEY instead of one-field UNIQUE INDEX, and fix index naming) * Fix parameter order for body message, was wrong * Use getFullUrl() instead of link(). The previous code was wrong because ** link() wants a Title, not a string, so it would just return <!-- ERROR -->Special:EmailCapture ** even if called right, it would return an HTML <a> tag, and you're sending the e-mail as text/plain ** using getTitleFor() also produces nice localized special page titles * Add aliases file
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@86396 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/EmailCapture/sql/CreateEmailCaptureTable.sql
+++ extensions/EmailCapture/sql/CreateEmailCaptureTable.sql
@@ -1,7 +1,7 @@
 -- Captured email addresses
 CREATE TABLE IF NOT EXISTS /*_*/email_capture (
   -- Email address
-  ec_email varbinary(255) NOT NULL DEFAULT '',
+  ec_email varbinary(255) PRIMARY KEY NOT NULL DEFAULT '',
   -- Additional information
   ec_info blob NOT NULL DEFAULT '',
   -- Verification code
@@ -9,5 +9,4 @@ CREATE TABLE IF NOT EXISTS /*_*/email_capture (
   -- Verified
   ec_verified boolean DEFAULT 0
 ) /*$wgDBTableOptions*/;
-CREATE UNIQUE INDEX /*i*/ac_email_key ON /*_*/email_capture (ec_email);
-CREATE INDEX /*i*/ac_code_verified_key ON /*_*/email_capture (ec_code, ec_verified);
+CREATE INDEX /*i*/ec_code_verified ON /*_*/email_capture (ec_code, ec_verified);
