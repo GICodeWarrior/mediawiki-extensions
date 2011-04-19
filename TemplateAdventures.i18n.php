6dc9b3a57e862039bf1b90f683356e2b6b53e153
svip <svip@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-19 22:19:31 +0000

    Follow up to r86340.  Added more functionality to Citation.php.  STill not done.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@86440 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- TemplateAdventures.i18n.php
+++ TemplateAdventures.i18n.php
@@ -18,6 +18,7 @@ $messages['en'] = array (
 	# citation messages
 	'ta-citeetal'            => "$1 ''et al''.",   # $1 = list of authors
 	'ta-citecoauthors'       => '$1$2 $3',         # $1 = authors, $2 = separator
+	'ta-citealonedate'       => '$1',              # $1 = date
 	'ta-citeauthordate'      => '$1 ($2)',         # $1 = authors or separator
 	'ta-citeauthoryearnote'  => '$1 [$2]',         # $1 = date
 	'ta-citeeditorsplural'   => '$1, eds',         # $1 = editors
@@ -31,10 +32,15 @@ $messages['en'] = array (
 	'ta-citeinlanguage'      => '$1 (in $2)',      # $1 = title/link
 	'ta-citetitletrans'      => '"$1 [$2]"',       # $1 = title/link, $2 = transtitle
 	'ta-citeformatrender'    => '$1 ($2)',         # $1 = title/link
-	'ta-newspublisherplace'  => "''$1'' ($2)",
-	'ta-newspublisher'       => "''$1''",
+	'ta-citenewspublisherplace'  => "''$1'' ($2)",
+	'ta-citenewspublisher'       => "''$1''",
 	'ta-citeperiodical'      => "''$1''",
 	'ta-citeperiodicaltitle' => "''$1''",
+	'ta-citebooktitle'       => "''$1''",
+	'ta-citebookpubplace'    => '$1: $2',          # $1 = location, $2 = publisher
+	'ta-citebookpublisher'   => '$1',
+	'ta-citebookpage'        => 'p. $1',
+	'ta-citebookisbn'        => '[[Special:BookSources/$1|ISBN $2]]',
 	'ta-series'              => '$1',
 	'ta-citepublicationplaceandpublisher'   => '($1: $2)',
 	'ta-citepublicationplace'   => '($1)',
@@ -50,6 +56,8 @@ $messages['en'] = array (
 	'ta-citepublicationdate' => '$1',
 	'ta-citeretrievedupper'  => 'Retrieved $1',   
 	'ta-citeretrievedlower'  => 'retrieved $1',
+	'ta-citejournal'         => "''$1''",
+	'ta-citejournalpage'     => "''$1'': $2",
 	
 	# citation span messages
 	'ta-citationspan'        => '<span class="citation $2">$1</span>',
