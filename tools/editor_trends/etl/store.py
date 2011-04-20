17f01c7b8553baf6386ab2e0374384a02a863019
diederik <diederik@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-20 22:38:19 +0000

    Storing of article information now uses multiprocessing as well.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@86559 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/editor_trends/etl/store.py
+++ tools/editor_trends/etl/store.py
@@ -19,7 +19,6 @@ __version__ = '0.1'
 
 from Queue import Empty
 import multiprocessing
-import os
 import progressbar
 
 from utils import file_utils
@@ -38,7 +37,8 @@ class Storer(consumers.BaseConsumer):
     The treshold is currently more than 9 edits and is not yet configurable. 
     '''
     def run(self):
-        db = storage.Database(self.rts.storage, self.rts.dbname, self.rts.editors_raw)
+        db = storage.init_database(self.rts.storage, self.rts.dbname,
+                                   self.rts.editors_raw)
         editor_cache = cache.EditorCache(db)
         prev_editor = -1
         while True:
@@ -61,59 +61,49 @@ class Storer(consumers.BaseConsumer):
                     if prev_editor != editor and prev_editor != -1:
                         editor_cache.add(prev_editor, 'NEXT')
 
-                    data = self.prepare_data(line)
+                    data = prepare_data(line)
                     #print editor, data['username']
                     editor_cache.add(editor, data)
                     prev_editor = editor
             fh.close()
             self.result.put(True)
 
-    def prepare_data(self, line):
-        article_id = int(line[1])
-        username = line[3].encode('utf-8')
-        ns = int(line[4])
-        date = text_utils.convert_timestamp_to_datetime_utc(line[6])
-        md5 = line[7]
-        revert = int(line[8])
-        bot = int(line[9])
-        cur_size = int(line[10])
-        delta = int(line[11])
 
-        data = {'date': date,
-                 'article': article_id,
-                 'username': username,
-                 'ns': ns,
-                 'hash': md5,
-                 'revert':revert,
-                 'cur_size':cur_size,
-                 'delta':delta,
-                 'bot':bot
-                }
-        return data
-
-
-def store_articles(rts):
+def prepare_data(line):
     '''
-    This function reads titles.csv and stores it in a separate collection.
-    Besides containing the title of an article, it also includes:
-    * namespace
-    * category (if any)
-    * article id
+    Prepare a single line to store in the database, this entails converting
+    to proper variable and taking care of the encoding.
     '''
-    db = storage.Database(rts.storage, rts.dbname, rts.articles_raw)
-    db.drop_collection()
-    db.add_index('id')
-    db.add_index('title')
-    db.add_index('ns')
-    db.add_index('category')
+    article_id = int(line[1])
+    username = line[3].encode('utf-8')
+    ns = int(line[4])
+    date = text_utils.convert_timestamp_to_datetime_utc(line[6])
+    md5 = line[7]
+    revert = int(line[8])
+    bot = int(line[9])
+    cur_size = int(line[10])
+    delta = int(line[11])
 
-    location = os.path.join(rts.input_location, rts.language.code, rts.project.name, 'txt')
-    files = file_utils.retrieve_file_list(rts.txt, extension='csv', mask='titles')
+    data = {'date': date,
+            'article': article_id,
+            'username': username,
+            'ns': ns,
+            'hash': md5,
+            'revert':revert,
+            'cur_size':cur_size,
+            'delta':delta,
+            'bot':bot
+    }
+    return data
 
-    print 'Storing articles...'
-    for file in files:
-        print 'Processing %s...' % file
-        fh = file_utils.create_txt_filehandle(rts.txt, file, 'r', 'utf-8')
+
+def store_articles(tasks, rts):
+    while True:
+        filename = tasks.get(block=False)
+        if filename == None:
+            break
+        print 'Processing %s...' % filename
+        fh = file_utils.create_txt_filehandle(rts.txt, filename, 'r', 'utf-8')
         for line in fh:
             line = line.strip()
             line = line.split('\t')
@@ -132,25 +122,26 @@ def store_articles(rts):
     print 'Done storing articles...'
 
 
-def launcher(rts):
+def launcher_articles(rts):
     '''
-    This is the main entry point and creates a number of workers and launches
-    them. 
+    This function reads titles.csv and stores it in a separate collection.
+    Besides containing the title of an article, it also includes:
+    * namespace
+    * category (if any)
+    * article id
     '''
-    store_articles(rts)
-    print 'Input directory is: %s ' % rts.sorted
-    db = storage.Database(rts.storage, rts.dbname, rts.editors_raw)
+    db = storage.init_database(rts.storage, rts.dbname, rts.articles_raw)
     db.drop_collection()
-    db.add_index('editor')
+    db.add_index('id')
+    db.add_index('title')
+    db.add_index('ns')
+    db.add_index('category')
 
-    files = file_utils.retrieve_file_list(rts.sorted, 'csv')
-    pbar = progressbar.ProgressBar(maxval=len(files)).start()
-
+    files = file_utils.retrieve_file_list(rts.txt, extension='csv',
+                                          mask='articles')
     tasks = multiprocessing.JoinableQueue()
-    result = multiprocessing.JoinableQueue()
 
-    consumers = [Storer(rts, tasks, result) for
-                 x in xrange(rts.number_of_processes)]
+    print 'Storing articles...'
 
     for filename in files:
         tasks.put(filename)
@@ -158,23 +149,58 @@ def launcher(rts):
     for x in xrange(rts.number_of_processes):
         tasks.put(None)
 
-    for w in consumers:
-        w.start()
+    storers = Process(target=store_articles, args=[tasks, rts] for
+                      x in xrange(rts.number_of_processes))
 
-    ppills = rts.number_of_processes
-    while True:
-        while ppills > 0:
-            try:
-                res = result.get(block=False)
-                if res == True:
-                    pbar.update(pbar.currval + 1)
-                else:
-                    ppills -= 1
-            except Empty:
-                pass
-        break
+    for storer in storers:
+        storer.start()
 
     tasks.join()
+
+
+def launcher(rts):
+    '''
+    This is the main entry point and creates a number of workers and launches
+    them. 
+    '''
+    launcher_articles(rts)
+#    print 'Input directory is: %s ' % rts.sorted
+#    db = storage.init_database(rts.storage, rts.dbname, rts.editors_raw)
+#    db.drop_collection()
+#    db.add_index('editor')
+#
+#    files = file_utils.retrieve_file_list(rts.sorted, 'csv')
+#    pbar = progressbar.ProgressBar(maxval=len(files)).start()
+#
+#    tasks = multiprocessing.JoinableQueue()
+#    result = multiprocessing.JoinableQueue()
+#
+#    storers = [Storer(rts, tasks, result) for
+#                 x in xrange(rts.number_of_processes)]
+#
+#    for filename in files:
+#        tasks.put(filename)
+#
+#    for x in xrange(rts.number_of_processes):
+#        tasks.put(None)
+#
+#    for storer in storers:
+#        storer.start()
+#
+#    ppills = rts.number_of_processes
+#    while True:
+#        while ppills > 0:
+#            try:
+#                res = result.get(block=False)
+#                if res == True:
+#                    pbar.update(pbar.currval + 1)
+#                else:
+#                    ppills -= 1
+#            except Empty:
+#                pass
+#        break
+#
+#    tasks.join()
 
 
 if __name__ == '__main__':
