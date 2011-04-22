1a566a52a80f8fad473c03734829377d5b8d19da
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-22 18:57:03 +0000

    Added mock methods for data query functions with comments on how they should work.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@86723 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SpecialArticleFeedback.php
+++ SpecialArticleFeedback.php
@@ -18,6 +18,72 @@ class SpecialArticleFeedback extends SpecialPage {
 		global $wgUser, $wgOut, $wgRequest;
 
 		$this->setHeaders();
+
+		$dailyHighsAndLows = $this->getDailyHighsAndLows();
+		$weeklyMostChanged = $this->getWeeklyMostChanged();
+		$recentLows = $this->getRecentLows();
+
+		// TODO: Use lists to generate tables
+
 		$wgOut->addHtml( '<div class="articleFeedback-dashboard"><h2>Hello dashboard!</h2></div>' );
+	}
+
+	/* Protected Methods */
+
+	/**
+	 * Gets a list of articles which were rated exceptionally high or low.
+	 * 
+	 * - Based on average of all rating categories
+	 * - Gets 50 highest rated and 50 lowest rated
+	 * - Only consider articles with 10+ ratings in the last 24 hours
+	 * 
+	 * This data should be updated daily, ideally though a scheduled batch job
+	 */
+	protected function getDailyHighsAndLows() {
+		return array(
+			array(
+				'title' => 'Main Page',
+				'ratings' => array( 4, 3, 2, 1 ),
+				'all' => 2.5
+			)
+		);
+	}
+
+	/**
+	 * Gets a list of articles which have quickly changing ratings.
+	 * 
+	 * - Based on any rating category
+	 * - Gets 50 most improved and 50 most worsened
+	 * - Only consider articles with 100+ ratings in the last 7 days
+	 * 
+	 * This data should be updated daily, ideally though a scheduled batch job
+	 */
+	protected function getWeeklyMostChanged() {
+		return array(
+			array(
+				'title' => 'Main Page',
+				'ratings' => array( 4, 3, 2, 1 ),
+				'average' => 2.5
+			)
+		);
+	}
+
+	/**
+	 * Gets a list of articles which have recently recieved exceptionally low ratings.
+	 * 
+	 * - Based on any rating category
+	 * - Gets 50 most improved and 50 most worsened
+	 * - Only consider articles which were rated lower than 3 for 7 out of the last 10 ratings
+	 * 
+	 * This data should be updated whenever article ratings are changed, ideally through a hook
+	 */
+	protected function getRecentLows() {
+		return array(
+			array(
+				'title' => 'Main Page',
+				'ratings' => array( 4, 3, 2, 1 ),
+				'average' => 2.5
+			)
+		);
 	}
 }
