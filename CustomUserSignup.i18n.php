7a862155ca7681f22aee25e1af92301169f31595
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-27 20:22:02 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-04-27 20:00:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87040 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- CustomUserSignup.i18n.php
+++ CustomUserSignup.i18n.php
@@ -15,3 +15,116 @@ $messages['en'] = array(
 	'customusersignup' => 'Custom User Signup',
 	'customusersignup-desc' => 'Allows for customizable messages during the account creation process',
 );
+
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author EugeneZelenko
+ */
+$messages['be-tarask'] = array(
+	'customusersignup-desc' => 'Дазваляе нестандартныя паведамленьні падчас працэсу стварэньня рахунку',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'customusersignup-desc' => 'Ermöglicht die Ausgabe angepasster Benutzernachrichten im Verlauf der Benutzerkontenerstellung',
+);
+
+/** French (Français)
+ * @author IAlex
+ */
+$messages['fr'] = array(
+	'customusersignup-desc' => 'Permet des messages variés au cours du processus de création de compte',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'customusersignup-desc' => 'לאפשר הודעות מותאמות אישית בזמן תהליך יצירת חשבון',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'customusersignup-desc' => 'Permitter messages personalisabile durante le processo de creation de contos',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'customusersignup-desc' => 'Menyediakan pesan terkustomisasi saat proses pembuatan akun',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'customusersignup-desc' => 'Дава можност за приказ ан прилагодливи пораки во текот на создавањето на сметки',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'customusersignup-desc' => 'Maakt het mogelijk aangepaste berichten weer te geven tijdens het aanmaken van gebruikers',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'customusersignup-desc' => 'A përmëtt ëd përsonalisé ij mëssagi an mente dël process ëd creassion dël cont',
+);
+
+/** Tarandíne (Tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'customusersignup-desc' => "Permette de fa messagge personalizzate duranne 'u processe de ccrejazione d'u cunde utende",
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'customusersignup-desc' => 'Включает настраиваемые сообщения в процессе создания учётной записи',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'customusersignup-desc' => 'Umožňuje prispôsobovať správy pri vytváraní účtu',
+);
+
+/** Slovenian (Slovenščina)
+ * @author Dbc334
+ */
+$messages['sl'] = array(
+	'customusersignup-desc' => 'Omogoča prilagodljiva sporočila med postopkom ustvarjanja računa',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'customusersignup-desc' => 'Nagpapahintulot ng mga mensaheng mapapasadya habang isinasagawa ang paglikha ng akawnt',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Minh Nguyen
+ */
+$messages['vi'] = array(
+	'customusersignup-desc' => 'Cho phép tùy biến các thông điệp trong quá trình mở tài khoản',
+);
+
+/** Simplified Chinese (‪中文(简体)‬)
+ * @author 阿pp
+ */
+$messages['zh-hans'] = array(
+	'customusersignup-desc' => '允许在邮件帐户创建过程中自定义消息',
+);
+
