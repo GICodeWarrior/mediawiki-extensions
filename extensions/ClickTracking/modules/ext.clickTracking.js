fa5e1705846284d2dca63d9ac8e0011f0084f9b0
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-04-27 21:44:02 +0000

    AFT/ClickTracking JsPerf + JsLint fixes: * Combining (function($){}(jQuery); and $(document).ready (check [[Conventions]] for details) * Cache $(this) instead of re-initializing the same object multiple times. * Don't use .each() for plugings that do this already internally. (ie. .remove(), .css() etc.) * Make use of jQuery chaining where possible ($(...).foo().bar().more().calls(); etc.); * Delete $.json_encode, using jquery.json module instead * Use dot.notation instead array['brackets'] whenever possible. * Using Array literal instead of Object constructor (var = []; / var = new Array();)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87046 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ClickTracking/modules/ext.clickTracking.js
+++ extensions/ClickTracking/modules/ext.clickTracking.js
@@ -2,16 +2,14 @@
  * JavaScript for ClickTracking extension
  */
 
-( function( $ ) {
+jQuery( function( $ ) {
 	// Add click tracking hooks to the sidebar
-	$( document ).ready( function() {
-		$( '#p-logo a, #p-navigation a, #p-interaction a, #p-tb a' ).each( function() {
-			var href = $( this ).attr( 'href' );
-			// Only modify local URLs
-			if ( href.length > 0 && href[0] == '/' && ( href.length == 1 || href[1] != '/' ) ) {
-				var id = 'leftnav-' + skin + '-' + ( $( this ).attr( 'id' ) || $( this ).parent().attr( 'id' ) );
-				$( this ).attr( 'href', $.trackActionURL( href, id ) );
-			}
-		} );
+	$( '#p-logo a, #p-navigation a, #p-interaction a, #p-tb a' ).each( function() {
+		var $el = $(this), href = $el.attr( 'href' );
+		// Only modify local URLs
+		if ( href.length > 0 && href[0] == '/' && ( href.length == 1 || href[1] != '/' ) ) {
+			var id = 'leftnav-' + skin + '-' + ( $el.attr( 'id' ) || $el.parent().attr( 'id' ) );
+			$el.attr( 'href', $.trackActionURL( href, id ) );
+		}
 	} );
-} )( jQuery );
+} )();
