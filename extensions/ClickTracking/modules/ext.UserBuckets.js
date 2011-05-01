eae0a2f0095b1066841029ccd904178e189fb555
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-01 13:33:56 +0000

    Follow-up r86961: Code conventions; line-wrap around 80-100 chars; using isFunction instead of typeof check
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87186 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ClickTracking/modules/ext.UserBuckets.js
+++ extensions/ClickTracking/modules/ext.UserBuckets.js
@@ -21,25 +21,25 @@ $.setupActiveBuckets = function(){
 	var buckets = $.getBuckets();
 	for ( var iter in mw.activeCampaigns ) {
 		var campaign = mw.activeCampaigns[iter];
-		// if bucket has been set, or bucket version is out of date,
-		// set up a user bucket
-		if(campaign.all){
+		// If bucket has been set, or bucket version is out of date,
+		// set up a user bucket.
+		if ( campaign.all ){
 			campaign.all();
 		}
 		
-		if(campaign.preferences && !campaign.preferences.setBuckets){
+		if ( campaign.preferences && !campaign.preferences.setBuckets ) {
 			continue;
 		}
 		
-		if(!buckets || !buckets[campaign.name] || buckets[campaign.name][1] < campaign.version){
-			//add up all rates
+		if ( !buckets || !buckets[campaign.name] || buckets[campaign.name][1] < campaign.version){
+			// Add up all rates
 			var bucketTotal = 0;
 			for ( var rate in campaign.rates ){
 				bucketTotal += campaign.rates[rate];
 			}
 			
-			//give the user a random number in those rates
-			var currentUser = Math.floor(Math.random() * (bucketTotal+1));
+			// Give the user a random number in those rates
+			var currentUser = Math.floor( Math.random() * (bucketTotal+1) );
 			
 			// recurse through the rates until we get into the range the user falls in,
 			// assign them to that range
@@ -47,21 +47,28 @@ $.setupActiveBuckets = function(){
 			var next_val = 0;
 			for( rate in campaign.rates ){
 				next_val += campaign.rates[rate];
-				if(prev_val <= currentUser && currentUser < next_val){
-					$.setBucket(campaign.name, rate, campaign.version);
+				if ( prev_val <= currentUser && currentUser < next_val ){
+					$.setBucket( campaign.name, rate, campaign.version );
 					break;
 				}
 				prev_val = next_val;
 			}
 		}
 		
-		// do the actual code in the campaign based on the bucket
-		if($.getBuckets() && $.getBuckets()[campaign.name] && $.getBuckets()[campaign.name][0] != "none"){
-			if(typeof(campaign[$.getBuckets()[campaign.name][0]]) == "function"){
-				campaign[$.getBuckets()[campaign.name][0]](); //function to execute
-			}
-			if ( campaign.allActive ) {
-				campaign.allActive();
+		// Execute the actual code in the campaign based on the bucket
+		if ( $.getBuckets() && $.getBuckets()[campaign.name] ) {
+
+			var campaignBucket = $.getBuckets()[campaign.name][0];
+			if ( campaignBucket != 'none' ) {
+						
+				// Function to execute
+				var func = campaign[campaignBucket];
+				if ( $.isFunction( func ) ) {
+					func();
+				}
+				if ( campaign.allActive ) {
+					campaign.allActive();
+				}
 			}
 		}
 		
