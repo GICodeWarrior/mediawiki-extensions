8eb54115de58d040e337af5c6f4ae9d03a2aab83
awjrichards <awjrichards@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-03 01:19:26 +0000

    Adding sql to create table for article feedback stats relating to high/low article ratings; Adding maintenance script to periodically populate high/low article feedback stats table
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87310 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ArticleFeedback/sql/AddStatsHighsLowsTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE /*_*/article_feedback_stats_highs_lows (
+	afshl_id integer unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	afshl_page_id integer unsigned NOT NULL,
+	-- combined average rating
+	afshl_avg_overall double unsigned NOT NULL,
+	-- json object of rating key => avg value
+	afshl_avg_ratings varbinary(255) NOT NULL,
+	-- timestamp of insertion job	
+	afshl_ts binary(14) NOT NULL
+) /*$wgDBTableOptions*/;
