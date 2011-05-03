9a55d4d73c325e2e99a9ac23fd559cd5b5b4c871
preilly <preilly@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-03 16:58:48 +0000

    fix spaces to tabs issue
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87335 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/PatchOutputMobile/PatchOutputMobile.i18n.php
+++ extensions/PatchOutputMobile/PatchOutputMobile.i18n.php
@@ -2,7 +2,7 @@
 
 $messages = array();
 $messages['en'] = array(
-    'patch_output_mobile_show' => 'show',
-    'patch_output_mobile_hide' => 'hide',
+	'patch_output_mobile_show' => 'show',
+	'patch_output_mobile_hide' => 'hide',
 	'patch_output_mobile_back_to_top' => 'Jump Back A Section',
 );
