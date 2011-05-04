71cd1ed17ebee52255d7b6d01ec26d3f59ffac27
awjrichards <awjrichards@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-04 01:25:26 +0000

    Follow up r87310; Adding index to timestamp files in article_feedback and article_feedback_stats_highs_lows tables; Now calculating rating averages in PHP rather than relying on MySQL in populateAFStatistics.php and removed associated cruft
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87379 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- sql/AddStatsHighsLowsTable.sql
+++ sql/AddStatsHighsLowsTable.sql
@@ -1,4 +1,4 @@
-CREATE TABLE /*_*/article_feedback_stats_highs_lows (
+CREATE TABLE IF NOT EXISTS /*_*/article_feedback_stats_highs_lows (
 	afshl_id integer unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	afshl_page_id integer unsigned NOT NULL,
 	-- combined average rating
@@ -8,3 +8,4 @@ CREATE TABLE /*_*/article_feedback_stats_highs_lows (
 	-- timestamp of insertion job	
 	afshl_ts binary(14) NOT NULL
 ) /*$wgDBTableOptions*/;
+CREATE INDEX /*i*/ afshl_timestamp ON /*_*/article_feedback_stats_highs_lows (afshl_ts);
