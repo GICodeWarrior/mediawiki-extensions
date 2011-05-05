00ec1212b0f1e3608de8ef0c4672664f3f88600b
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-05 02:55:51 +0000

    Fixed some register_globals vulnerabilities.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87477 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- templates/HtmlUiFieldset.php
+++ templates/HtmlUiFieldset.php
@@ -1,3 +1,4 @@
+<?php if ( !defined( 'MEDIAWIKI' ) ) exit; ?>
 <fieldset class="htmlUiFieldset" rel="<?php echo $id ?>">
 	<?php foreach( $elements as $element ): ?>
 	<?php echo $element->render(); ?>
