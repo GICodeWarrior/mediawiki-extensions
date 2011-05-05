3adc84c72d1c2d2c194ea7628123402b35d30135
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-05 20:38:04 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-05 20:28:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87509 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WhitelistPages.i18n.php
+++ WhitelistPages.i18n.php
@@ -20,3 +20,38 @@ $messages['de'] = array(
 	'whitelistpages-desc' => 'Ermöglicht das [[MediaWiki:Public read whitelist|Freigeben]] bestimmter Seiten auf geschlossenen Wikis, so dass diese von anonymen Benutzern eingesehen werden können',
 );
 
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'whitelistpages-desc' => 'Permitte adder paginas al [[MediaWiki:Public read whitelist|lista blanc]] in un wiki private, de sorta que usatores anonyme pote leger iste paginas',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'whitelistpages-desc' => 'Овозможува заведување на страници на приватно вики во [[MediaWiki:Public read whitelist|бел список]], овозможувајќи им на анонимните корисници да ги читаат наведените страници',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'whitelistpages-desc' => "Maakt het mogelijk pagina's in een niet-openbare wiki op de [[MediaWiki:Public read whitelist|witte lijst]] te zetten zodat anonieme gebruikers die kunnen bekijken",
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'whitelistpages-desc' => 'Nas wikis privadas, permite a inclusão de páginas numa [[MediaWiki:Public read whitelist|lista branca pública]], para que os utilizadores anónimos possam lê-las',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'whitelistpages-desc' => 'Позволяет добавлять страницы частной вики [[MediaWiki:Public read whitelist|в белый список]], чтобы анонимные участники могли их просматривать',
+);
+
