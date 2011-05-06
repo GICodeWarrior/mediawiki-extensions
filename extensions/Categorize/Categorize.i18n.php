d96b64c507eccd587819039a7c89982fcdf41484
faurethomas <faurethomas@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-06 14:12:00 +0000

    update headers and i18n file
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87539 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Categorize/Categorize.i18n.php
+++ extensions/Categorize/Categorize.i18n.php
@@ -1,9 +1,9 @@
 <?php
 
-# Internationalisation file for the AjaxSuggest extension for MediaWiki
-/* CategorySuggest Mediawiki Extension
+# Internationalisation file for the Categorize extension for MediaWiki
+/* Categorize Mediawiki Extension
  *
- * @author Andreas Rindler (mediawiki at jenandi dot com)
+ * @author Andreas Rindler (mediawiki at jenandi dot com) and Thomas Fauré (faure dot thomas at gmail dot com)
  * @licence GNU General Public Licence 2.0 or later
  * @description 
  *
@@ -11,33 +11,19 @@
 $messages = array();
 
 $messages['en'] = array(
-	'categorize-title'       => 'Knowledge qualification (beta version)',
+	'categorize-title'       => 'Knowledge qualification',
 	'categorize-subtitle'    => '',
-	'categorize-footer'      => 'If you want to add a label, please contact us at <a href=\'yoann.richard@morpho.com\'>Yoann Richard</a>, <a href=\'philippe.clemot@morpho.com\'>Philippe Cl&eacute;mot</a> or <a href=\'thomas.faure@morpho.com\'>Thomas Faur&eacute;</a>.',
+	'categorize-footer'      => 'If you want to add a label, please contact us at ...',
 	'categorize-boxlabel'    => 'Cat&eacute;gories',
 	'categorize-taglabel'    => 'Cat&eacute;gories populaires',
-	'categorize-advice'      => 'Click on the labels you want to attribute to this article:',
-	'categorize1'            => 'Product',
-	'categorize2'            => 'COTS',
-	'categorize3'            => 'System',
-	'categorize4'            => 'Infrastructure',
-	'categorize5'            => 'Business',
-	'categorize6'            => 'Tools',
-	'categorize7'            => 'WikiMorpho'
+	'categorize-advice'      => 'Click on the labels you want to attribute to this article:'
 	);
 $messages['fr'] = array(
-	'categorize-title'       => 'Qualification des savoirs (beta version)',
+	'categorize-title'       => 'Qualification des savoirs',
 	'categorize-subtitle'    => '',
-	'categorize-footer'      => 'Si vous souhaitez ajouter un label, contactez <a href=\'yoann.richard@morpho.com\'>Yoann Richard</a>, <a href=\'philippe.clemot@morpho.com\'>Philippe Cl&eacute;mot</a> ou <a href=\'thomas.faure@morpho.com\'>Thomas Faur&eacute;</a>.',
+	'categorize-footer'      => 'Si vous souhaitez ajouter un label, contactez ...',
 	'categorize-boxlabel'    => 'Cat&eacute;gories',
 	'categorize-taglabel'    => 'Cat&eacute;gories populaires',
-	'categorize-advice'      => 'Cliquez sur les labels que vous souhaitez attribuer &agrave; cet article :',
-	'categorize1'            => 'Produit',
-	'categorize2'            => 'COTS',
-	'categorize3'            => 'Syst&egrave;me',
-	'categorize4'            => 'Infrastructure',
-	'categorize5'            => 'M&eacute;tier',
-	'categorize6'            => 'Outils',
-	'categorize7'            => 'WikiMorpho'
+	'categorize-advice'      => 'Cliquez sur les labels que vous souhaitez attribuer &agrave; cet article :'
 	);
 ?>
--- extensions/Categorize/Categorize.js
+++ extensions/Categorize/Categorize.js
@@ -1,6 +1,6 @@
-/* CategorySuggest Mediawiki Extension
+/* Categorize Mediawiki Extension
  *
- * @author Andreas Rindler (mediawiki at jenandi dot com)
+ * @author Andreas Rindler (mediawiki at jenandi dot com) and Thomas Fauré (faure dot thomas at gmail dot com)
  * @credits Jared Milbank, Leon Weber <leon.weber@leonweber.de> & Manuel Schneider <manuel.schneider@wikimedia.ch>, Daniel Friesen http://wiki-tools.com
  * @licence GNU General Public Licence 2.0 or later
  * @description Adds input box to edit and upload page which allows users to assign categories to the article. When a user starts typing the name of a category, the extension queries the database to find categories that match the user input."
