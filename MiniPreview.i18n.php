57f117a035a033069403692cd0da6d7d40f78ab9
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-07 21:26:26 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-07 20:56:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87662 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MiniPreview.i18n.php
+++ MiniPreview.i18n.php
@@ -107,9 +107,10 @@ $messages['bcl'] = array(
 /** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
  * @author EugeneZelenko
  * @author Jim-by
+ * @author Wizardist
  */
 $messages['be-tarask'] = array(
-	'minipreview-desc' => 'Гэтае пашырэньне паказвае каля выявы невялікія выявы іншых мэдыя-файлаў з той жа катэгорыі альбо выкарыстаных на тых жа старонках',
+	'minipreview-desc' => 'Гэтае пашырэньне паказвае каля выявы невялікія выявы іншых мэдыяфайлаў з той жа катэгорыі альбо выкарыстаных на тых жа старонках',
 	'minipreview-files_in_category' => "Гэта катэгорыя ўтрымлівае '''$1''' {{PLURAL:$1|файл|файлы|файлаў}}.",
 	'minipreview-files_in_gallery' => "Гэтая галерэя ўтрымлівае '''$1''' {{PLURAL:$1|файл|файлы|фалаў}}.",
 	'minipreview-no_more_files_here' => 'Няма файлаў у гэтым накірунку.',
