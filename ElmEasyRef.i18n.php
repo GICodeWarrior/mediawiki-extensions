f269f255f569b5f2bd40da04f8e398ab5ab33c66
nord <nord@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-07 18:59:20 +0000

    Commit ElmEasyRef extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87650 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ ElmEasyRef.i18n.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Internationalisation file for extension ElmEasyRef.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Elancev Michael
+ */
+$messages['en'] = array(
+	'elm-easyref-ref'    => 'Reference $1',
+	'elm-easyref-close'  => 'Close',
+);
+
+
+/** Russian (Русский)
+ * @author Elancev Michael
+ */
+$messages['ru'] = array(
+	'elm-easyref-ref'    => 'Ссылка $1',
+	'elm-easyref-close'  => 'Закрыть',
+);
