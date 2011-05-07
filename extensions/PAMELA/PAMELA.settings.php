d358dee73cfb1ef9415483dca9201b74f6ad1bb4
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-07 00:14:02 +0000

    typo fixes
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87613 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/PAMELA/PAMELA.settings.php
+++ extensions/PAMELA/PAMELA.settings.php
@@ -5,7 +5,7 @@
  * More info can be found at http://www.mediawiki.org/wiki/Extension:PAMELA#Settings
  *
  *                          NOTICE:
- * Changing one of these settings can be done by copieng or cutting it,
+ * Changing one of these settings can be done by copying or cutting it,
  * and placing it in LocalSettings.php, AFTER the inclusion of PAMELA.
  *
  * @since 0.1
@@ -24,5 +24,5 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 # The location of the PAMELA webservice read API.
 $egPamAPIURL = '';
 
-# The interval between refreshes of data in the widgets, in miliseconds.
+# The interval between refreshes of data in the widgets, in milliseconds.
 $egPamRefreshInterval = 60000;
