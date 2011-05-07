445af13a8d479505dc6adb17adb1f47d41c57c6e
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-07 20:16:08 +0000

    Followup r87650: Add description message Add extension to Translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87655 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ElmEasyRef/ElmEasyRef.i18n.php
+++ extensions/ElmEasyRef/ElmEasyRef.i18n.php
@@ -13,10 +13,10 @@ $messages = array();
  * @author Elancev Michael
  */
 $messages['en'] = array(
+	'elm-easyref-desc'   => "Add popup field to display reference's content",
 	'elm-easyref-ref'    => 'Reference $1',
 	'elm-easyref-close'  => 'Close',
 );
-
 
 /** Russian (Русский)
  * @author Elancev Michael
@@ -25,3 +25,4 @@ $messages['ru'] = array(
 	'elm-easyref-ref'    => 'Ссылка $1',
 	'elm-easyref-close'  => 'Закрыть',
 );
+
