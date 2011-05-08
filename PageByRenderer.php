fb15d01d59d42990d9863f1a03dc8535d52b82bd
dantman <dantman@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-08 03:16:03 +0000

    Modify callers of makeLinkObj, many of these seam to be passing unescaped text which is actually treated as html and needs to be escaped.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87673 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PageByRenderer.php
+++ PageByRenderer.php
@@ -155,7 +155,7 @@ class PageByRenderer {
 
 		if ($this->showfirst) {
 			$firstuser = Title::makeTitle(NS_USER, $first->rev_user_text);
-			$ulink =  $sk->makeLinkObj($firstuser, $first->rev_user_text);
+			$ulink =  $sk->makeLinkObj($firstuser, htmlspecialchars($first->rev_user_text));
 			$date = $this->showtime ? $wgContLang->timeanddate($first->rev_timestamp) : $wgContLang->date($first->rev_timestamp);
 			$diff = $this->title->getLocalURL('diff=' . $first->rev_id);
 			$comment = htmlspecialchars( $first->rev_comment );
@@ -176,7 +176,7 @@ class PageByRenderer {
 				}
 				else {
 					$cuser = Title::makeTitle(NS_USER, $u['name']);
-					$ulink =  $sk->makeLinkObj($cuser, $u['name']);
+					$ulink =  $sk->makeLinkObj($cuser, htmlspecialchars($u['name']));
 				}
 
 				if ($contributors !== '') $contributors .= ', ';
@@ -199,7 +199,7 @@ class PageByRenderer {
 
 		if (!$this->showfirst || $edits > 1) {
 			$lastuser = Title::makeTitle(NS_USER, $last->rev_user_text);
-			$ulink =  $sk->makeLinkObj($lastuser, $last->rev_user_text);
+			$ulink =  $sk->makeLinkObj($lastuser, htmlspecialchars($last->rev_user_text));
 			$date = $this->showtime ? $wgContLang->timeanddate($last->rev_timestamp) : $wgContLang->date($last->rev_timestamp);
 			$diff = $this->title->getLocalURL('diff=' . $last->rev_id);
 			$comment = htmlspecialchars( $last->rev_comment );
