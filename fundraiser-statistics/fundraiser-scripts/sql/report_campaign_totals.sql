e4375dcc703377c891722c0c8952770cf2a11730
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-09 23:51:54 +0000

    - added new field to query indocating whether a report exists
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87792 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fundraiser-statistics/fundraiser-scripts/sql/report_campaign_totals.sql
+++ fundraiser-statistics/fundraiser-scripts/sql/report_campaign_totals.sql
@@ -6,7 +6,8 @@ select
 faulkner.test.test_name,
 drupal.contribution_tracking.utm_campaign,
 sum(not isnull(contribution_tracking.contribution_id)) as donations,
-min(receive_date) as earliest_timestamp
+min(receive_date) as earliest_timestamp,
+if(not isnull(faulkner.test.test_name),1,0) as report_exists
 
 from 
 drupal.contribution_tracking left join civicrm.civicrm_contribution ON (drupal.contribution_tracking.contribution_id = civicrm.civicrm_contribution.id)
