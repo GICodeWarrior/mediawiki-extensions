78fae6214beb23b9de59e1cccb6f1d1939f7690c
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-14 00:11:22 +0000

    Localisation updates for special page aliases for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88028 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ArticleFeedback.alias.php
+++ ArticleFeedback.alias.php
@@ -8,9 +8,42 @@
 
 $specialPageAliases = array();
 
-/** English
- * @author Trevor Parscal
- */
+/** English (English) */
 $specialPageAliases['en'] = array(
 	'ArticleFeedback' => array( 'ArticleFeedback' ),
 );
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'ArticleFeedback' => array( 'تعليقات_المقالة' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'ArticleFeedback' => array( 'ОценувањеНаСтатија' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'ArticleFeedback' => array( 'ലേഖനാഭിപ്രായം' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'ArticleFeedback' => array( 'Terugkoppeling' ),
+);
+
+/** Vietnamese (Tiếng Việt) */
+$specialPageAliases['vi'] = array(
+	'ArticleFeedback' => array( 'Phản_hồi_bài' ),
+);
+
+/** Simplified Chinese (‪中文(简体)‬) */
+$specialPageAliases['zh-hans'] = array(
+	'ArticleFeedback' => array( '文章反馈' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
