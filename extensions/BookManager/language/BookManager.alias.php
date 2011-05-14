f480a0bd0e817e41f09a596f127109e9799319e6
raylton.sousa <raylton.sousa@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-14 12:28:46 +0000

    Reorganizing directories
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88057 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/BookManager/language/BookManager.alias.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Aliases for Special:PrintVersion
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Raylton P. Sousa <raylton.sousa@gmail.com>
+ */
+
+$specialPageAliases = array();
+
+/** English 
+ * @author Raylton P. Sousa <raylton.sousa@gmail.com>
+ * @author Helder.wiki
+ */
+$specialPageAliases['en'] = array(
+	'PrintVersion' => array( 'Print Version' ),
+);
+
+/** Portuguese (Português)
+ * @author Raylton P. Sousa <raylton.sousa@gmail.com>
+ * @author Helder.wiki
+ */
+$specialPageAliases['pt'] = array(
+	'PrintVersion' => array( 'Versão para impressão' ),
+);
