89ab1989332639adddf7d0c3b49bf26e9725b9ef
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-14 00:11:22 +0000

    Localisation updates for special page aliases for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88028 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Todo.alias.php
+++ Todo.alias.php
@@ -7,9 +7,47 @@
 
 $specialPageAliases = array();
 
-/**
- * English
- */
+/** English (English) */
 $specialPageAliases['en'] = array(
 	'Todo' => array( 'Todo' ),
 );
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'Todo' => array( 'للعمل' ),
+);
+
+/** Breton (Brezhoneg) */
+$specialPageAliases['br'] = array(
+	'Todo' => array( 'DaOber' ),
+);
+
+/** French (Français) */
+$specialPageAliases['fr'] = array(
+	'Todo' => array( 'ÀFaire', 'À_faire' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'Todo' => array( 'PouFè' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'Todo' => array( 'Задачи' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'Todo' => array( 'ചെയ്യേണ്ടവ' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'Todo' => array( 'TeDoen' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
