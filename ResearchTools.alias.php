e4aaf171e04d954678adc664dc0cdefe4470a116
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-14 00:11:22 +0000

    Localisation updates for special page aliases for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88028 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ResearchTools.alias.php
+++ ResearchTools.alias.php
@@ -8,9 +8,47 @@
 
 $specialPageAliases = array();
 
-/** English
- * @author Trevor Parscal
- */
+/** English (English) */
 $specialPageAliases['en'] = array(
 	'ResearchTools' => array( 'ResearchTools' ),
 );
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'ResearchTools' => array( 'أدوات_البحث' ),
+);
+
+/** Breton (Brezhoneg) */
+$specialPageAliases['br'] = array(
+	'ResearchTools' => array( 'OstilhoùKlask' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'ResearchTools' => array( 'ZoutiPouChache' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'ResearchTools' => array( 'АлаткиЗаИстражување' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'ResearchTools' => array( 'ഗവേഷണോപകരണങ്ങൾ' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'ResearchTools' => array( 'Onderzoekshulpmiddelen' ),
+);
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = array(
+	'ResearchTools' => array( 'AraştırmaAraçları' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
