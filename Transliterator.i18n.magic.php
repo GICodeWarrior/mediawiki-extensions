bbb1e07a5d9e425b81c6261076480a61cde03dc3
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-14 00:23:17 +0000

    Localisation updates for magic word aliases for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88029 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Transliterator.i18n.magic.php
+++ Transliterator.i18n.magic.php
@@ -1,65 +1,109 @@
 <?php
+/**
+ *
+ */
 
 $magicWords = array();
 
-/**
- * English
- */
+/** English (English) */
 $magicWords['en'] = array(
-	'transliterate' => array( '0', 'transliterate' ),
-	// NOTE: only the first case-sensitive synonym is considered.
+	'transliterate' => array( 'transliterate' ),
 	'tr_prefix' => array( '1', 'Transliterator:' ),
-	'tr_decompose' => array( '0', '__DECOMPOSE__' ),
+	'tr_decompose' => array( '__DECOMPOSE__' ),
 );
 
+/** Afrikaans (Afrikaans) */
 $magicWords['af'] = array(
-	'transliterate'   => array( '0', 'translitereer', 'transliterate' ),
-	'tr_prefix'       => array( '1', 'Translitereerder:', 'Transliterator:' ),
+	'transliterate' => array( 'translitereer', 'transliterate' ),
+	'tr_prefix' => array( '1', 'Translitereerder:', 'Transliterator:' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'transliterate'   => array( '0', 'ترجمة_حرفية', 'transliterate' ),
-	'tr_prefix'       => array( '1', 'مترجم_حرفي:', 'Transliterator:' ),
-	'tr_decompose'    => array( '0', '__تحلل__', '__DECOMPOSE__' ),
+	'transliterate' => array( 'ترجمة_حرفية' ),
+	'tr_prefix' => array( 'مترجم_حرفي:' ),
+	'tr_decompose' => array( '__تحلل__' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'transliterate'   => array( '0', 'ترجمة_حرفية', 'transliterate' ),
-	'tr_prefix'       => array( '1', 'مترجم_حرفي:', 'Transliterator:' ),
-	'tr_decompose'    => array( '0', '__تحلل__', '__DECOMPOSE__' ),
+	'transliterate' => array( 'ترجمة_حرفية', 'transliterate' ),
+	'tr_prefix' => array( '1', 'مترجم_حرفي:', 'Transliterator:' ),
+	'tr_decompose' => array( '__تحلل__', '__DECOMPOSE__' ),
 );
 
+/** Breton (Brezhoneg) */
 $magicWords['br'] = array(
-	'transliterate'   => array( '0', 'treuzlizherennañ', 'transliterate' ),
+	'transliterate' => array( 'treuzlizherennañ' ),
 );
 
+/** Chechen (Нохчийн) */
 $magicWords['ce'] = array(
-	'transliterate'   => array( '0', 'хийцайозанца', 'транслитерация', 'transliterate' ),
+	'transliterate' => array( 'хийцайозанца', 'транслитерация', 'transliterate' ),
 );
 
+/** Spanish (Español) */
 $magicWords['es'] = array(
-	'transliterate'   => array( '0', 'transliterar', 'transliterate' ),
+	'transliterate' => array( 'transliterar', 'transliterate' ),
 );
 
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'transliterate' => array( '翻字' ),
+	'tr_prefix' => array( '翻字機能:' ),
+	'tr_decompose' => array( '__分解__' ),
+);
+
+/** Macedonian (Македонски) */
+$magicWords['mk'] = array(
+	'transliterate' => array( 'транслитерирај' ),
+	'tr_prefix' => array( 'Траслитератор:' ),
+	'tr_decompose' => array( '__РАЗЛОЖИ__' ),
+);
+
+/** Malayalam (മലയാളം) */
 $magicWords['ml'] = array(
-	'transliterate'   => array( '0', 'ലിപിമാറ്റംചെയ്യുക', 'ലിപ്യന്തരണം', 'ലിപിമാറ്റം', 'transliterate' ),
-	'tr_prefix'       => array( '1', 'ലിപിമാറ്റയുപകരണം:', 'Transliterator:' ),
-	'tr_decompose'    => array( '0', '__ശിഥിലീകരിക്കുക__', '__DECOMPOSE__' ),
+	'transliterate' => array( 'ലിപിമാറ്റംചെയ്യുക', 'ലിപ്യന്തരണം', 'ലിപിമാറ്റം' ),
+	'tr_prefix' => array( 'ലിപിമാറ്റയുപകരണം:' ),
+	'tr_decompose' => array( '__ശിഥിലീകരിക്കുക__' ),
 );
 
+/** Marathi (मराठी) */
 $magicWords['mr'] = array(
-	'transliterate'   => array( '0', 'लिप्यांतर', 'transliterate' ),
+	'transliterate' => array( 'लिप्यांतर', 'transliterate' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'transliterate'   => array( '0', 'translitereren', 'transliterate' ),
+	'transliterate' => array( 'translitereren' ),
 );
 
+/** Oriya (ଓଡ଼ିଆ) */
+$magicWords['or'] = array(
+	'transliterate' => array( 'ଟ୍ରାନ୍ସଲିଟରେସନ' ),
+	'tr_prefix' => array( 'ଟ୍ରାନ୍ସଲିଟରେସନକାରି' ),
+);
+
+/** Portuguese (Português) */
 $magicWords['pt'] = array(
-	'transliterate'   => array( '0', 'transliterar', 'transliterate' ),
-	'tr_prefix'       => array( '1', 'Transliterador:', 'Transliterator:' ),
+	'transliterate' => array( 'transliterar', 'transliterate' ),
+	'tr_prefix' => array( '1', 'Transliterador:', 'Transliterator:' ),
 );
 
+/** Russian (Русский) */
 $magicWords['ru'] = array(
-	'transliterate'   => array( '0', 'транслитерация', 'transliterate' ),
+	'transliterate' => array( 'транслитерация' ),
+	'tr_prefix' => array( 'Транслитератор:' ),
+	'tr_decompose' => array( '__РАЗОБРАТЬ__' ),
+);
+
+/** Serbian Cyrillic ekavian (‪Српски (ћирилица)‬) */
+$magicWords['sr-ec'] = array(
+	'transliterate' => array( 'пресловљавање' ),
+	'tr_prefix' => array( 'Пресловљавач:' ),
+);
+
+/** Vietnamese (Tiếng Việt) */
+$magicWords['vi'] = array(
+	'transliterate' => array( 'chuyểntự' ),
 );
