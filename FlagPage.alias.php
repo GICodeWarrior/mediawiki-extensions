964e06cdf55197c530ad9d0d790b27c5d8db6f08
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-14 00:11:22 +0000

    Localisation updates for special page aliases for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88028 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- FlagPage.alias.php
+++ FlagPage.alias.php
@@ -4,19 +4,55 @@ $specialPageAliases = array();
 /** English
  * @author Church of emacs
  */
+
+$specialPageAliases = array();
+
+/** English (English) */
 $specialPageAliases['en'] = array(
-    'FlagPage' => array( 'FlagPage' ),
+	'FlagPage' => array( 'FlagPage' ),
 );
- 
-/** German (Deutsch)
- * @author Church of emacs
- */
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'FlagPage' => array( 'علم_الصفحة' ),
+);
+
+/** German (Deutsch) */
 $specialPageAliases['de'] = array(
-	'FlagPage' => array( 'Seite markieren'),
+	'FlagPage' => array( 'Seite_markieren' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'FlagPage' => array( 'MakePaj' ),
+);
+
+/** Luxembourgish (Lëtzebuergesch) */
+$specialPageAliases['lb'] = array(
+	'FlagPage' => array( 'Säit_markéieren' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'FlagPage' => array( 'ОзначиСтраница' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'FlagPage' => array( 'പതാകാതാൾ' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'FlagPage' => array( 'PaginaMarkeren' ),
+);
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = array(
+	'FlagPage' => array( 'Sayfaİşaretle', 'SayfayıBayrakla' ),
 );
 
 /**
  * For backwards compatibility with MediaWiki 1.15 and earlier.
  */
 $aliases =& $specialPageAliases;
-
