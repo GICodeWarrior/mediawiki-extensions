662ccaa3cb98ca6f900be20908021875a8da615e
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-14 00:11:22 +0000

    Localisation updates for special page aliases for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88028 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- CreateRedirect.alias.php
+++ CreateRedirect.alias.php
@@ -8,11 +8,84 @@
 
 $specialPageAliases = array();
 
-/** English
- * @author Marco Zafra
- */
+/** English (English) */
 $specialPageAliases['en'] = array(
 	'CreateRedirect' => array( 'CreateRedirect' ),
+);
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'CreateRedirect' => array( 'تحويل_تصنيف' ),
+);
+
+/** Breton (Brezhoneg) */
+$specialPageAliases['br'] = array(
+	'CreateRedirect' => array( 'KrouiñAdkas' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'CreateRedirect' => array( 'Weiterleitung_erstellen' ),
+);
+
+/** Esperanto (Esperanto) */
+$specialPageAliases['eo'] = array(
+	'CreateRedirect' => array( 'Kreu_alidirekton' ),
+);
+
+/** Persian (فارسی) */
+$specialPageAliases['fa'] = array(
+	'CreateRedirect' => array( 'ایجاد_تغییر_مسیر' ),
+);
+
+/** French (Français) */
+$specialPageAliases['fr'] = array(
+	'CreateRedirect' => array( 'CréerRedirection', 'Créer_redirection' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'CreateRedirect' => array( 'FèRedireksyon' ),
+);
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = array(
+	'CreateRedirect' => array( 'リダイレクト作成' ),
+);
+
+/** Khmer (ភាសាខ្មែរ) */
+$specialPageAliases['km'] = array(
+	'CreateRedirect' => array( 'បង្កើតការបញ្ចូនបន្ត' ),
+);
+
+/** Luxembourgish (Lëtzebuergesch) */
+$specialPageAliases['lb'] = array(
+	'CreateRedirect' => array( 'Viruleedung_uleeën' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'CreateRedirect' => array( 'СоздајПренасочување' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'CreateRedirect' => array( 'തിരിച്ചുവിടൽസൃഷ്ടിക്കുക' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'CreateRedirect' => array( 'DoorverwijzingAanmaken' ),
+);
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = array(
+	'CreateRedirect' => array( 'YönlendirmeOluştur' ),
+);
+
+/** Simplified Chinese (‪中文(简体)‬) */
+$specialPageAliases['zh-hans'] = array(
+	'CreateRedirect' => array( '创建重定向' ),
 );
 
 /**
