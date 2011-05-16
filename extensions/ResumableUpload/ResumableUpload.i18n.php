465964775eecb520117e1d1687c98fb696c8400e
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-16 14:23:42 +0000

    Renamed FirefoggChunkedUpload to ResumableUpload Modified protocol to include total filesize in initial request, include byte offset on every chunk, and  more strict parameter checks ( just an initial rename not yet working or tested )
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88236 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ResumableUpload/ResumableUpload.i18n.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Internationalisation file for extension Resumable Uploading.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'resumableupload-desc' => "Resumable Uploading support",
+);
