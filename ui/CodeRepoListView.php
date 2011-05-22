2ea19f0cf3494e15a455438979edc6b5699fc98e
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-22 17:57:15 +0000

    Less Special hardcoding
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88586 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ui/CodeRepoListView.php
+++ ui/CodeRepoListView.php
@@ -37,29 +37,31 @@ class CodeRepoListView {
 	public static function getNavItem( $repo ) {
 		global $wgLang, $wgUser;
 		$name = $repo->getName();
-		$links[] = "[[Special:Code/$name/comments|" . wfMsgHtml( 'code-notes' ) . "]]";
-		$links[] = "[[Special:Code/$name/statuschanges|" . wfMsgHtml( 'code-statuschanges' ) . "]]";
+
+		$code = SpecialPage::getTitleFor( 'Code', $name );
+		$links[] = "[[$code/comments|" . wfMsgHtml( 'code-notes' ) . "]]";
+		$links[] = "[[$code/statuschanges|" . wfMsgHtml( 'code-statuschanges' ) . "]]";
 		if ( $wgUser->getId() ) {
 			$author = $repo->wikiUserAuthor( $wgUser->getName() );
 			if ( $author !== false ) {
-				$links[] = "[[Special:Code/$name/author/$author|" . wfMsgHtml( 'code-mycommits' ) . "]]";
+				$links[] = "[[$code/author/$author|" . wfMsgHtml( 'code-mycommits' ) . "]]";
 			}
 		}
 
 		if ( $wgUser->isAllowed( 'codereview-post-comment' ) ) {
 			$userName = $wgUser->getName();
-			$links[] = "[[Special:Code/$name/comments/author/$userName|" . wfMsgHtml( 'code-mycomments' ) . "]]";
+			$links[] = "[[$code/comments/author/$userName|" . wfMsgHtml( 'code-mycomments' ) . "]]";
 		}
 
-		$links[] = "[[Special:Code/$name/tag|" . wfMsgHtml( 'code-tags' ) . "]]";
-		$links[] = "[[Special:Code/$name/author|" . wfMsgHtml( 'code-authors' ) . "]]";
-		$links[] = "[[Special:Code/$name/status|" . wfMsgHtml( 'code-status' ) . "]]";
-		$links[] = "[[Special:Code/$name/releasenotes|" . wfMsgHtml( 'code-releasenotes' ) . "]]";
-		$links[] = "[[Special:Code/$name/stats|" . wfMsgHtml( 'code-stats' ) . "]]";
+		$links[] = "[[$code/tag|" . wfMsgHtml( 'code-tags' ) . "]]";
+		$links[] = "[[$code/author|" . wfMsgHtml( 'code-authors' ) . "]]";
+		$links[] = "[[$code/status|" . wfMsgHtml( 'code-status' ) . "]]";
+		$links[] = "[[$code/releasenotes|" . wfMsgHtml( 'code-releasenotes' ) . "]]";
+		$links[] = "[[$code/stats|" . wfMsgHtml( 'code-stats' ) . "]]";
 		if( $wgUser->isAllowed( 'repoadmin' ) ) {
 			$links[] = "[[Special:RepoAdmin/$name|" . wfMsgHtml( 'repoadmin-nav' ) . "]]";
 		}
-		$text = "'''[[Special:Code/$name|$name]]''' " . wfMsg( 'parentheses', $wgLang->pipeList( $links ) );
+		$text = "'''[[$code|$name]]''' " . wfMsg( 'parentheses', $wgLang->pipeList( $links ) );
 		return $text;
 	}
 }
