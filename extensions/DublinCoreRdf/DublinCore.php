ca0abc83eb90195fc965955615d79333b8831f98
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-25 23:55:15 +0000

    * (bug 29144) Move action=dublincore and action=creativecommons to extensions
    Moved dublincore out to extension
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88868 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/DublinCoreRdf/DublinCore.php
@@ -0,0 +1,25 @@
+<?php
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'DublinCore',
+	'url' => '',
+	'author' => 'Evan Prodromou',
+);
+
+$wgHooks['MediaWikiPerformAction'][] = 'efDublinCorePreformAction';
+
+					$rdf = new DublinCoreRdf( $article );
+					$rdf->show();
+
+$wgAutoloadClasses['DublinCoreRdf'] = $dir . 'DublinCoreRdf_body.php';
+
+function efDublinCorePreformAction( $output, $article, $title, $user, $request, $mediaWiki ) {
+	if ( $mediaWiki->getAction() !== 'dublincore' ) {
+		return true;
+	}
+
+	$rdf = new DublinCoreRdf( $article );
+	$rdf->show();
+	return false;
+}
