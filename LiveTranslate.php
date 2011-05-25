f7f40b15a73df33b9f72093a5fdb331afe0c2c4d
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-25 22:53:49 +0000

    version nr fail fix
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88859 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- LiveTranslate.php
+++ LiveTranslate.php
@@ -24,7 +24,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define( 'LiveTranslate_VERSION', '0.1' );
+define( 'LiveTranslate_VERSION', '1.0' );
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
