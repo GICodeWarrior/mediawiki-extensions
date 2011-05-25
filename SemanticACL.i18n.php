a2b64488480bd57833d25469e9ba795a26c8c1e2
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-25 20:35:59 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-25 20:01:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88831 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticACL.i18n.php
+++ SemanticACL.i18n.php
@@ -16,3 +16,119 @@ $messages['en'] = array(
 	'sacl-property-editable-wl-user' => 'Editable by user',
 );
 
+/** Message documentation (Message documentation)
+ * @author Kghbln
+ */
+$messages['qqq'] = array(
+	'right-sacl-exempt' => 'ACL = Access Control List',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'sacl-desc' => 'Ermöglicht mit semantischen Attributen verknüpfte Zugriffsbeschränkungen auf Seiten',
+	'sacl-denied' => 'Du befindest dich nicht auf der Liste der für diese Seite Zugriffsberechtigten.',
+	'right-sacl-exempt' => 'Ausgenommen von Zugriffsbeschränkungen auf semantische Attribute',
+	'sacl-property-visibility' => 'Sichtbar für',
+	'sacl-property-visibility-wl-group' => 'Sichtbar für Benutzergruppe',
+	'sacl-property-visibility-wl-user' => 'Sichtbar für Benutzer',
+	'sacl-property-editable' => 'Bearbeitbar von',
+	'sacl-property-editable-wl-group' => 'Bearbeitbar von Benutzergruppe',
+	'sacl-property-editable-wl-user' => 'Bearbeitbar von Benutzer',
+);
+
+/** Persian (فارسی)
+ * @author Ebraminio
+ */
+$messages['fa'] = array(
+	'sacl-desc' => 'اجازه می‌دهد محدودیت‌های دسترسی با مشخصات معنایی مدیاویکی تنظیم شوند',
+	'sacl-denied' => 'شما در فهرست دسترسی به این صفحه نیستید.',
+	'right-sacl-exempt' => 'معاف از فهرست کنترل دسترسی (ACL) معنایی',
+	'sacl-property-visibility' => 'قابل نمایش برای',
+	'sacl-property-visibility-wl-group' => 'قابل نمایش برای گروه',
+	'sacl-property-visibility-wl-user' => 'قابل نمایش برای کاربر',
+	'sacl-property-editable' => 'قابل ویرایش برای',
+	'sacl-property-editable-wl-group' => 'قابل ویرایش برای گروه',
+	'sacl-property-editable-wl-user' => 'قابل ویرایش برای کاربر',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'sacl-desc' => 'Permite definir restricións de acceso coas propiedades de Semantic MediaWiki',
+	'sacl-denied' => 'Non está na lista de acceso desta páxina.',
+	'right-sacl-exempt' => 'Exento da lista de control de acceso semántica',
+	'sacl-property-visibility' => 'Visible para',
+	'sacl-property-visibility-wl-group' => 'Visible para o grupo',
+	'sacl-property-visibility-wl-user' => 'Visible para o usuario',
+	'sacl-property-editable' => 'Editable por',
+	'sacl-property-editable-wl-group' => 'Editable polo grupo',
+	'sacl-property-editable-wl-user' => 'Editable polo usuario',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'sacl-desc' => 'Permitte definir restrictiones de accesso con proprietates de Semantic MediaWiki',
+	'sacl-denied' => 'Tu non es in le lista de accesso de iste pagina.',
+	'right-sacl-exempt' => 'Exempte del controlo de accesso de Semantic MediaWiki',
+	'sacl-property-visibility' => 'Visibile pro',
+	'sacl-property-visibility-wl-group' => 'Visibile pro le gruppo',
+	'sacl-property-visibility-wl-user' => 'Visibile pro le usator',
+	'sacl-property-editable' => 'Modificabile per',
+	'sacl-property-editable-wl-group' => 'Modificabile per le gruppo',
+	'sacl-property-editable-wl-user' => 'Modificabile per le usator',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'sacl-denied' => 'Dir sidd net op der Lëscht vun deenen déi dës Säit benotzen däerfen',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'sacl-desc' => 'Овозможува поставање на ограничувања на пристапот со својства на Семантичко МедијаВики',
+	'sacl-denied' => 'Не сте на списокот за пристап до оваа страница.',
+	'right-sacl-exempt' => 'Изземен од семантичкиот список за конрола на пристап',
+	'sacl-property-visibility' => 'Видливо за',
+	'sacl-property-visibility-wl-group' => 'Видливо за групата',
+	'sacl-property-visibility-wl-user' => 'Видливо за корисникот',
+	'sacl-property-editable' => 'Уредливо за',
+	'sacl-property-editable-wl-group' => 'Уредливо за групата',
+	'sacl-property-editable-wl-user' => 'Уредливо за корисникот',
+);
+
+/** Dutch (Nederlands)
+ * @author McDutchie
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'sacl-property-visibility-wl-group' => 'Zichtbaar voor groep',
+	'sacl-property-visibility-wl-user' => 'Zichtbaar voor gebruiker',
+	'sacl-property-editable' => 'Bewerkbaar door',
+	'sacl-property-editable-wl-group' => 'Bewerkbaar door groep',
+	'sacl-property-editable-wl-user' => 'Bewerkbaar door gebruiker',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'sacl-desc' => 'Permite definir restrições de acesso com as propriedades do MediaWiki Semântico',
+	'sacl-denied' => 'Não está na lista de acesso desta página.',
+	'right-sacl-exempt' => 'Isenta das Listas de Controle de Acesso Semânticas',
+	'sacl-property-visibility' => 'Visível para',
+	'sacl-property-visibility-wl-group' => 'Visível para o grupo',
+	'sacl-property-visibility-wl-user' => 'Visível para o utilizador',
+	'sacl-property-editable' => 'Editável por',
+	'sacl-property-editable-wl-group' => 'Editável pelo grupo',
+	'sacl-property-editable-wl-user' => 'Editável pelo utilizador',
+);
+
