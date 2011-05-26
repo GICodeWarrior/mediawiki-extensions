6e1953b8066a59116e8f20bf0bfbb9cddd43462d
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-26 19:03:17 +0000

    Added documentation to wiki.AnnotationRenderer
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88915 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/lib/wiki.AnnotationRenderer.js
+++ parsers/wikidom/lib/wiki.AnnotationRenderer.js
@@ -10,6 +10,21 @@ wiki.AnnotationRenderer = function() {
 
 	/* Methods */
 
+	/**
+	 * Adds a set of insertions around a range of text.
+	 * 
+	 * Insertions for the same range will be nested in order of declaration.
+	 * @example
+	 *     ar = new wiki.AnnotationRenderer();
+	 *     ar.wrapWithText( { 'offset': 1, 'length': 1 }, '[', ']' );
+	 *     ar.wrapWithText( { 'offset': 1, 'length': 1 }, '{', '}' );
+	 *     // Outputs: "a[{b}]c"
+	 *     console.log( ar.apply( 'abc' ) );
+	 * 
+	 * @param range Object: Range to insert text around
+	 * @param pre String: Text to insert before range
+	 * @param post String: Text to insert after range
+	 */
 	this.wrapWithText = function( range, pre, post ) {
 		var start = range.offset;
 		if ( !( start in insertions ) ) {
@@ -25,12 +40,27 @@ wiki.AnnotationRenderer = function() {
 		}
 	};
 
+	/**
+	 * Adds a set of opening and closing XML tags around a range of text.
+	 * 
+	 * This is a convenience function, and has the same nesting behavior as wrapWithText.
+	 * 
+	 * @param range Object: Range to insert XML tags around
+	 * @param tag String: XML tag name
+	 * @param attributes Object: XML tag attributes (optional)
+	 */
 	this.wrapWithXml = function( range, tag, attributes ) {
 		that.wrapWithText(
 			range, wiki.util.xml.open( tag, attributes ), wiki.util.xml.close( tag )
 		);
 	};
 
+	/**
+	 * Applies insertions to text.
+	 * 
+	 * @param text String: Text to apply insertions to
+	 * @return String: Wrapped text
+	 */
 	this.apply = function( text ) {
 		var out = '';
 		for ( var i = 0, iMax = text.length; i <= iMax; i++ ) {
