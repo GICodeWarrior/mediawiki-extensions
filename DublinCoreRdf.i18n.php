138c5e437c4c210f496cd7c8ef06504dec2d392c
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-29 21:50:09 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-29 21:31:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89138 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- DublinCoreRdf.i18n.php
+++ DublinCoreRdf.i18n.php
@@ -16,3 +16,24 @@ $messages['en'] = array(
 	'dublincorerdf-desc' => 'Dublin Core RDF-metadata',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'dublincorerdf-desc' => 'Stellt RDF-Metadaten zu Dublin Core bereit',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'dublincorerdf-desc' => 'Metadatos RDF de Dublin Core',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'dublincorerdf-desc' => 'Metadados RDF Dublin Core',
+);
+
