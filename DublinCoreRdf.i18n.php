76de02aa82c07cdca2ab649ae7a57f4e87ae2917
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-30 20:28:52 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-30 20:19:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89186 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- DublinCoreRdf.i18n.php
+++ DublinCoreRdf.i18n.php
@@ -16,6 +16,13 @@ $messages['en'] = array(
 	'dublincorerdf-desc' => 'Dublin Core RDF-metadata',
 );
 
+/** Danish (Dansk)
+ * @author Peter Alberti
+ */
+$messages['da'] = array(
+	'dublincorerdf-desc' => 'Dublin Core RDF-metadata',
+);
+
 /** German (Deutsch)
  * @author Kghbln
  */
@@ -23,11 +30,32 @@ $messages['de'] = array(
 	'dublincorerdf-desc' => 'Stellt RDF-Metadaten zu Dublin Core bereit',
 );
 
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'dublincorerdf-desc' => 'Metadatos RDF de Dublin Core',
+);
+
 /** Interlingua (Interlingua)
  * @author McDutchie
  */
 $messages['ia'] = array(
 	'dublincorerdf-desc' => 'Metadatos RDF de Dublin Core',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'dublincorerdf-desc' => 'RDF-метаподатоци од Dublin Core',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'dublincorerdf-desc' => 'Dublin Core RDF-metadata',
 );
 
 /** Portuguese (Português)
