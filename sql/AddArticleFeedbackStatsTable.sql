c428a76e70bdf40cb4159620c90319a38cca0a88
awjrichards <awjrichards@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-01 18:32:17 +0000

    Added 'problem articles' view to dashboard; refactored dashboard code (populateAFStatistics.php, primarily); Added new schema sql scripts as well as sql migration script
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89277 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ sql/AddArticleFeedbackStatsTable.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS article_feedback_stats;
+CREATE TABLE IF NOT EXISTS /*_*/article_feedback_stats (
+	afs_page_id integer unsigned NOT NULL,
+	-- data point to be used for ordering this data
+	afs_orderable_data double unsigned NOT NULL,
+	-- json object of stat data
+	afs_data varbinary(255) NOT NULL,
+	afs_stats_type_id integer unsigned NOT NULL,
+	-- timestamp of insertion job	
+	afs_ts binary(14) NOT NULL
+) /*$wgDBTableOptions*/;
+CREATE UNIQUE INDEX /*i*/ afs_page_ts_type ON /*_*/ article_feedback_stats( afs_page_id, afs_ts, afs_stats_type_id );
+CREATE INDEX /*i*/ afs_ts_avg_overall ON /*_*/article_feedback_stats (afs_ts, afs_orderable_data);
