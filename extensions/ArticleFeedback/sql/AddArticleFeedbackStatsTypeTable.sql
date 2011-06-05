ebb1a13b30b5431340423e77590b073f4d536dfa
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-05 10:59:44 +0000

    Fix syntax in .sql (spaces), crashed updater (follow-up r89277)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89494 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ArticleFeedback/sql/AddArticleFeedbackStatsTypeTable.sql
+++ extensions/ArticleFeedback/sql/AddArticleFeedbackStatsTypeTable.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS /*_*/ article_feedback_stats_types (
+CREATE TABLE IF NOT EXISTS /*_*/article_feedback_stats_types (
 	afst_id integer unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	afst_type varbinary(255) NOT NULL
 ) /*$wgDBTableOptions*/;
-CREATE UNIQUE INDEX /*i*/ afst_type ON /*_*/ article_feedback_stats_types( afst_type );
+CREATE UNIQUE INDEX /*i*/afst_type ON /*_*/article_feedback_stats_types( afst_type );
 
 -- Pre-populate table with stat types
 INSERT INTO article_feedback_stats_types ( afst_type ) VALUES ( 'highs_and_lows' );
