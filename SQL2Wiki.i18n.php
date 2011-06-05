19fdf902dea43b2822954407460ea36bd038d4d4
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-05 19:21:51 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-06-05 19:19:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89522 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SQL2Wiki.i18n.php
+++ SQL2Wiki.i18n.php
@@ -19,3 +19,15 @@ with error message: "$2"!',
 	'sql2wiki-err-feature_not_supported' => 'SQL2Wiki: Feature is not supported by databases of type $1!',
 );
 
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'sql2wiki-desc' => 'SQL-gegevens rechtstreeks in de pagina-inhoud weergeven',
+	'sql2wiki-cache_refresh' => 'Verversen',
+	'sql2wiki-err-invalid_db_id' => 'SQL2Wiki: Ontbrekend of ongeldig databasenummer',
+	'sql2wiki-err-invalid_type' => 'SQL2Wiki: Ontbrekend of ongeldig type',
+	'sql2wiki-err-failed_to_connect' => 'SQL2Wiki: verbinding maken met $1 is mislukt!',
+	'sql2wiki-err-feature_not_supported' => 'SQL2Wiki: De functie wordt niet ondersteund door databases van het type $1!',
+);
+
