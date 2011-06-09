86cc07be88094b243c383c14a9cf51ea7db570a8
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-09 20:58:25 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-06-09 20:44:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89795 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Notificator.i18n.php
+++ Notificator.i18n.php
@@ -25,6 +25,7 @@ $messages['en'] = array(
 	'notificator-notified-already' => '$1 has been notified about this page or page change before.',
 );
 
+/** Message documentation (Message documentation) */
 $messages['qqq'] = array(
 	'notificator' => 'Name',
 	'notificator-desc' => '{{desc}}',
@@ -48,6 +49,33 @@ $messages['qqq'] = array(
 	'notificator-notified-already' => 'Message on special page',
 );
 
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'notificator' => 'Kennisgewer',
+	'notificator-desc' => "Stel iemand per e-pos in kennis oor veranderinge aan 'n bladsy as 'n knoppie op die bladsy gedruk word.",
+	'notificator-db-table-does-not-exist' => 'Die tabel "notificator" bestaan ​​nie in die databasis nie. Die update.php onderhoud-skrip moet geloop word alvorens die Notificator uitbreiding gebruik kan word.',
+	'notificator-e-mail-address' => 'E-posadres',
+	'notificator-notify' => 'In kennis stel',
+	'notificator-notify-address-or-name' => 'Stel $1 in kennis',
+	'notificator-revs-not-from-same-title' => "Wysigings vir ID's is nie van dieselfde titel/page nie",
+	'notificator-return-to' => 'Keer terug na',
+	'notificator-special-page-accessed-directly' => "Die spesiale bladsy kan nie direk aangevra word. Dit is bedoel om gebruik te word deur 'n kennisgewing-knoppie.",
+	'notificator-e-mail-address-invalid' => 'Die verskafde e-posadres is ongeldig.',
+	'notificator-notification-not-sent' => 'Kennisgewing is <em>nie</em> gestuur <em>nie</em>.',
+	'notificator-change-tag' => 'verandering',
+	'notificator-new-tag' => 'nuut',
+	'notificator-notification-text-changes' => '$1 wil u in kennis stel van die volgende wysigings aan $2:',
+	'notificator-notification-text-new' => '$1 wil u in kennis van $2.',
+	'notificator-following-e-mail-sent-to' => 'Die volgende e-pos is aan <em>$1</em> gestuur:',
+	'notificator-subject' => 'Onderwerp:',
+	'notificator-error-sending-e-mail' => "Daar was 'n fout met die stuur van die kennisgewing per e-pos aan <em>$1</em>.",
+	'notificator-error-parameter-missing' => 'Fout: Vermiste parameter.',
+	'notificator-notified-already' => '$1 is reeds in kennis gestel oor hierdie bladsy of veranderinge.',
+);
+
+/** German (Deutsch) */
 $messages['de'] = array(
 	'notificator' => 'Notificator',
 	'notificator-desc' => 'Sendet Benachrichtungs-Mails über Seitenänderungen, wenn ein Knopf auf der entsprechenden Seite betätigt wird.',
@@ -70,3 +98,58 @@ $messages['de'] = array(
 	'notificator-error-parameter-missing' => 'Fehler: Fehlender Parameter.',
 	'notificator-notified-already' => '$1 wurde bereits zu dieser Seite oder Seitenänderung benachrichtigt.',
 );
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'notificator' => 'Известувач',
+	'notificator-desc' => 'Испраќа известувања по е-пошта за измени во страници, ако корисникот стисне на такво копче на дадена страница.',
+	'notificator-db-table-does-not-exist' => 'Во базата не постои табела наречена „notificator“. За да можете да го користите додатокот „Известувач“, најпрвин ќе треба да ја пуштите скриптата за одржување „update.php“.',
+	'notificator-e-mail-address' => 'е-пошта',
+	'notificator-notify' => 'Извести',
+	'notificator-notify-address-or-name' => 'Извести го $1',
+	'notificator-revs-not-from-same-title' => 'Назнаките на ревизиите не се од ист наслов/страница',
+	'notificator-return-to' => 'Назад на',
+	'notificator-special-page-accessed-directly' => 'До оваа специјална страница не може да се дојде директно. Наменета е да се користи преку копчето за Известувач.',
+	'notificator-e-mail-address-invalid' => 'Наведената е-пошта е неважечка.',
+	'notificator-notification-not-sent' => 'Известувањето <em>не е</em> испратено.',
+	'notificator-change-tag' => 'измена',
+	'notificator-new-tag' => 'ново',
+	'notificator-notification-text-changes' => '$1 сака да ве извести за следниве измени $2:',
+	'notificator-subject' => 'Наслов:',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'notificator-subject' => 'Onderwerp:',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'notificator' => 'Notificador',
+	'notificator-desc' => 'Envia, por correio electrónico, uma notificação de que uma página foi alterada, quando um botão dessa página é clicado.',
+	'notificator-db-table-does-not-exist' => 'Não existe uma tabela "notificator" na base de dados. Para poder usar a extensão Notificador, tem de executar o ficheiro de comandos de manutenção update.php.',
+	'notificator-e-mail-address' => 'endereço de correio electrónico',
+	'notificator-notify' => 'Notificar',
+	'notificator-notify-address-or-name' => 'Notificar $1',
+	'notificator-revs-not-from-same-title' => 'As identificações das revisões não pertencem à mesma página ou título de página',
+	'notificator-return-to' => 'Voltar a',
+	'notificator-special-page-accessed-directly' => 'Não pode aceder directamente a esta página especial. Ela é utilizada através de um botão Notificador.',
+	'notificator-e-mail-address-invalid' => 'O endereço de correio electrónico fornecido é inválido.',
+	'notificator-notification-not-sent' => 'A notificação <em>não foi</em> enviada.',
+	'notificator-change-tag' => 'alterada',
+	'notificator-new-tag' => 'nova',
+	'notificator-notification-text-changes' => 'A $1 pretende notificar as seguintes alterações a $2:',
+	'notificator-notification-text-new' => 'A $1 pretende fazer uma notificação acerca de $2.',
+	'notificator-following-e-mail-sent-to' => 'A seguinte mensagem foi enviada por correio electrónico para <em>$1</em>:',
+	'notificator-subject' => 'Assunto:',
+	'notificator-error-sending-e-mail' => 'Ocorreu um erro ao enviar a notificação por correio electrónico a <em>$1</em>.',
+	'notificator-error-parameter-missing' => 'Erro: Parâmetro em falta.',
+	'notificator-notified-already' => '$1 já foi notificado acerca desta página ou da alteração desta página.',
+);
+
