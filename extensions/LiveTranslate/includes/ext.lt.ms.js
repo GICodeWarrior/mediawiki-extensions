8a81e15de504c9d7e4c275f410820a827feb39b1
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-09 16:29:22 +0000

    some work on ms translation functionality
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89774 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LiveTranslate/includes/ext.lt.ms.js
+++ extensions/LiveTranslate/includes/ext.lt.ms.js
@@ -1,3 +1,27 @@
 /**
+ * JavasSript for the Microsoft translation functionality in the Live Translate extension.
+ * @see http://www.mediawiki.org/wiki/Extension:Live_Translate
  * 
+ * @licence GNU GPL v3 or later
+ * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
  */
+
+(function( $ ){ window.translationService = function() {
+	
+	var self = this;
+	
+	this.done = function( targetLang ){};
+	
+	/**
+	 * Translates a single DOM element using Microsoft Translate.
+	 * Loops through child elements and recursively calls itself to translate these.
+	 * 
+	 * @param {jQuery} element
+	 * @param {string} sourceLang
+	 * @param {string} targetLang
+	 */
+	this.translateElement = function( element, sourceLang, targetLang ) {
+		
+	};
+	
+}; })( jQuery );
