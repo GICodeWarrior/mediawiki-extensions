205e0172f2500c74c75d872fc2d77f3abf880c51
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-10 06:03:10 +0000

    Followup r89768: Add alias file. svn add forgotten.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89815 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Notificator/Notificator.alias.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'Notificator' => array( 'Notificator' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
