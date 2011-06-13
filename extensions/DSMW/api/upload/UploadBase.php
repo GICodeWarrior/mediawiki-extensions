fa9e2d67973a23367aa731458d24acc0f5e1d22e
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-13 19:00:49 +0000

    (bug 28556) Remove MacBinary * IE5 for Mac is history.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89995 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/DSMW/api/upload/UploadBase.php
+++ extensions/DSMW/api/upload/UploadBase.php
@@ -230,7 +230,6 @@ abstract class UploadBase {
 	 */
 	protected function verifyFile() {
 		$this->mFileProps = File::getPropsFromPath( $this->mTempPath, $this->mFinalExtension );
-		$this->checkMacBinary();
 
 		# magically determine mime type
 		$magic = MimeMagic::singleton();
@@ -877,30 +876,6 @@ abstract class UploadBase {
 			wfDebug( __METHOD__ . ": FOUND VIRUS! scanner feedback: $output \n" );
 			return $output;
 		}
-	}
-
-	/**
-	 * Check if the temporary file is MacBinary-encoded, as some uploads
-	 * from Internet Explorer on Mac OS Classic and Mac OS X will be.
-	 * If so, the data fork will be extracted to a second temporary file,
-	 * which will then be checked for validity and either kept or discarded.
-	 */
-	private function checkMacBinary() {
-		$macbin = new MacBinary( $this->mTempPath );
-		if ( $macbin->isValid() ) {
-			$dataFile = tempnam( wfTempDir(), 'WikiMacBinary' );
-			$dataHandle = fopen( $dataFile, 'wb' );
-
-			wfDebug( __METHOD__ . ": Extracting MacBinary data fork to $dataFile\n" );
-			$macbin->extractData( $dataHandle );
-
-			$this->mTempPath = $dataFile;
-			$this->mFileSize = $macbin->dataForkLength();
-
-			// We'll have to manually remove the new file if it's not kept.
-			$this->mRemoveTempFile = true;
-		}
-		$macbin->close();
 	}
 
 	/**
