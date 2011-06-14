9bc55346862d158bdabb56cd797f8baee5980a90
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-14 17:23:32 +0000

    fix syntax error that caused all other extension messages to be broken
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90059 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MoodBar.i18n.php
+++ MoodBar.i18n.php
@@ -1,5 +1,5 @@
 <?php
 
 $messages['en'] = array(
-	'moodbar-desc' => 'Allows specified users to send their "mood" back to the site operator.",
+	'moodbar-desc' => 'Allows specified users to send their "mood" back to the site operator.',
 );
