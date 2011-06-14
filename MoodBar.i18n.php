278d20fd45eb1454a126f97aa6375be55f161a1f
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-14 17:25:33 +0000

    Fixup wgExtensionMessagesFiles and .i18n to not overwrite other extension messages files
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90060 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MoodBar.i18n.php
+++ MoodBar.i18n.php
@@ -1,5 +1,9 @@
 <?php
+$messages = array();
 
+/** English
+ * @author Andrew Garrett
+ */
 $messages['en'] = array(
 	'moodbar-desc' => 'Allows specified users to send their "mood" back to the site operator.',
 );
