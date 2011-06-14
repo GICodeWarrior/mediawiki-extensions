f4efe1226eb20227cac5d8a398cdcd873d134e3d
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-14 18:42:38 +0000

    Tweak message for consistency Add extension to Translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90071 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MoodBar/MoodBar.i18n.php
+++ extensions/MoodBar/MoodBar.i18n.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Internationalisation file for MoodBar extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+ 
 $messages = array();
-
+ 
 /** English
  * @author Andrew Garrett
  */
+
 $messages['en'] = array(
-	'moodbar-desc' => 'Allows specified users to send their "mood" back to the site operator.',
+	'moodbar-desc' => 'Allows specified users to send their "mood" back to the site operator',
 );
+
