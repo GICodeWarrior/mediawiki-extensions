c4cd6e886656d7f1074ac6c3fee11f0e0474ae04
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-15 20:04:37 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-06-15 19:51:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90142 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OracleTextSearch.i18n.php
+++ OracleTextSearch.i18n.php
@@ -13,11 +13,50 @@ $messages['en'] = array(
 	'oracletextsearch-desc' => 'Search content/metadata of uploaded files using Oracle Text indexing',
 );
 
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'oracletextsearch' => 'OracleTeksSoektogte',
+	'oracletextsearch-desc' => 'Deursoek inhoud/metadata van opgelaaide lêers met behulp van Oracle se teks indekse',
+);
+
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author Wizardist
+ */
+$messages['be-tarask'] = array(
+	'oracletextsearch' => 'OracleTextSearch',
+);
+
 /** German (Deutsch)
  * @author Kghbln
  */
 $messages['de'] = array(
 	'oracletextsearch' => '„Oracle TEXT“-Suche',
 	'oracletextsearch-desc' => 'Ermöglicht das Durchsuchen von Inhalt und Metadaten hochgeladener Dateien unter Verwendung der „Oracle TEXT“-Indexierung',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'oracletextsearch' => 'Procura de texto Oracle',
+	'oracletextsearch-desc' => 'Procurar nos contidos ou metadatos dos ficheiros cargados mediante a indexación de texto Oracle',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'oracletextsearch' => 'Пребарување со Oracle Text',
+	'oracletextsearch-desc' => 'Пребарување на содржини/метаподатоци на подигнати податотеки користејќи го индексирањето на Oracle Text',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'oracletextsearch' => 'Pesquisa de Texto da Oracle',
+	'oracletextsearch-desc' => 'Pesquisar o conteúdo e os metadados dos ficheiros usando a indexação Oracle Text',
 );
 
