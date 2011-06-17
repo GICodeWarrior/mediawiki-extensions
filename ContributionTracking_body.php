14ca13a5a2cc148724d37a0643c919c2765b1360
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-17 16:25:46 +0000

    Swap else if for elseif
    Trimming trailing whitespace also
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90285 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ContributionTracking_body.php
+++ ContributionTracking_body.php
@@ -31,20 +31,20 @@ class ContributionTracking extends UnlistedSpecialPage {
 		require_once( 'countryCodes.inc' );
 		global $wgRequest, $wgOut, $wgContributionTrackingPayPalIPN, $wgContributionTrackingReturnToURLDefault,
 			$wgContributionTrackingPayPalRecurringIPN, $wgContributionTrackingPayPalBusiness;
-		
+
 		if ( !preg_match( '/^[a-z-]+$/', $language ) ) {
 			$language = 'en';
 		}
 		$this->lang = Language::factory( $language );
-		
+
 		$this->setHeaders();
-		
+
 		$gateway = $wgRequest->getText( 'gateway' );
 		if( !in_array( $gateway, array( 'paypal', 'moneybookers' ) ) ) {
 			$wgOut->showErrorPage( 'contrib-tracking-error', 'contrib-tracking-error-text' );
 			return;
 		}
-		
+
 		$db = contributionTrackingConnection();
 
 		$ts = $db->timestamp();
@@ -53,7 +53,7 @@ class ContributionTracking extends UnlistedSpecialPage {
 		if($owa_ref != null  && !is_numeric($owa_ref)){
 			$owa_ref = $this->get_owa_ref_id($owa_ref);
 		}
-		
+
 		// Translate the shipping country from a code to a country name
 		$country = '';
 		$code = $wgRequest->getText('country2');
@@ -76,20 +76,20 @@ class ContributionTracking extends UnlistedSpecialPage {
 			'owa_ref' => $owa_ref,
 			'ts' => $ts,
 		);
-		
+
 		// Make all empty strings NULL
 		foreach ($tracked_contribution as $key => $value) {
 			if ($value === '') {
 				$tracked_contribution[$key] = null;
 			}
 		}
-		
+
 		// Store the contribution data
 		if ( !$wgRequest->getVal( 'contribution_tracking_id', 0 )) {
 			$db->insert( 'contribution_tracking', $tracked_contribution );
 		}
 		$contribution_tracking_id = $wgRequest->getVal( 'contribution_tracking_id', $db->insertId());
-		
+
 		$returnText = $wgRequest->getText( 'returnto', "Donate-thanks/$language" );
 		$returnTitle = Title::newFromText( $returnText );
 		if( $returnTitle ) {
@@ -97,13 +97,13 @@ class ContributionTracking extends UnlistedSpecialPage {
 		} else {
 			$returnto = $wgContributionTrackingReturnToURLDefault . "/$language";
 		}
-		
+
 		// Set the action and tracking ID fields
 		$repost = array();
 		$action = 'http://wikimediafoundation.org/';
 		$amount_field_name = 'amount'; // the amount fieldname may be different depending on the service
 		if ( $gateway == 'paypal' ) {
-			
+
 			$action = 'https://www.paypal.com/cgi-bin/webscr';
 
 			// Premiums
@@ -114,25 +114,25 @@ class ContributionTracking extends UnlistedSpecialPage {
 				$repost['os1'] = $wgRequest->getText( 'premium_language' );
 				$repost['no_shipping'] = 2;
 			}
-			
+
 			// PayPal
 			$repost['business'] = $wgContributionTrackingPayPalBusiness;
 			$repost['item_number'] = 'DONATE';
 			$repost['no_note'] = '0';
 			$repost['return'] = $returnto;
 			$repost['currency_code'] = $wgRequest->getText( 'currency_code', 'USD' );
-			
+
 			// additional fields to pass to PayPal from single-step credit card form
 			$repost[ 'first_name' ] = $wgRequest->getText( 'fname', null );
 			$repost[ 'last_name' ] = $wgRequest->getText( 'lname', null );
 			$repost[ 'email' ] = $wgRequest->getText( 'email', null );
-			
+
 			// if this is a recurring donation, we have add'l fields to send to paypal
 			if ( $wgRequest->getText( 'recurring_paypal' ) == 'true' ) {
 				$repost[ 't3' ] = "M"; // The unit of measurement for for p3 (M = month)
 				$repost[ 'p3' ] = '1'; // Billing cycle duration
 				$repost[ 'srt' ] = '12'; // # of billing cycles
-				$repost[ 'src' ] = '1'; // Make this 'recurring' 
+				$repost[ 'src' ] = '1'; // Make this 'recurring'
 				$repost[ 'sra' ] = '1'; // Turn on re-attempt on failure
 				$repost[ 'cmd' ] = '_xclick-subscriptions';
 				$amount_field_name = 'a3';
@@ -144,7 +144,7 @@ class ContributionTracking extends UnlistedSpecialPage {
 				$repost['item_name'] = $this->msg( 'contrib-tracking-item-name-onetime' );
 			}
 		}
-		else if ( $gateway == 'moneybookers' ) {
+		elseif ( $gateway == 'moneybookers' ) {
 			$action = 'https://www.moneybookers.com/app/payment.pl';
 
 			// Tracking
@@ -160,28 +160,28 @@ class ContributionTracking extends UnlistedSpecialPage {
 		} else {
 			throw new MWException( "This shouldn't happen, we validated the gateway earlier." );
 		}
-		
+
 		// Normalized amount
 		$repost[ $amount_field_name ] = $wgRequest->getVal( 'amount' );
 		if ( $wgRequest->getVal( 'amountGiven' ) ) {
 			$repost[ $amount_field_name ] = $wgRequest->getVal( 'amountGiven' );
 		}
-		
+
 		// Tracking
 		$repost['custom'] = $contribution_tracking_id;
-		
+
 		$wgOut->addWikiText( "{{2009/Donate-banner/$language}}" );
 		$wgOut->addHTML( $this->msgWiki( 'contrib-tracking-submitting' ) );
-		
+
 		// Output the repost form
 		$output = '<form method="post" name="contributiontracking" action="' . $action . '">';
 
 		foreach ( $repost as $key => $value ) {
 			$output .= '<input type="hidden" name="' . htmlspecialchars($key) . '" value="' . htmlspecialchars($value) . '" />';
 		}
-		
+
 		$output .= $this->msgWiki( 'contrib-tracking-redirect' );
-		
+
 		// Offer a button to post the form if the user has no Javascript support
 		$output .= '<noscript>';
 		$output .= $this->msgWiki( 'contrib-tracking-continue' );
