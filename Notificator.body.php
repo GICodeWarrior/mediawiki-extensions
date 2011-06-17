5c83da433a1fe12ff80a9f7b60b1b5c2b565d06b
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-17 16:27:04 +0000

    Swap else if for elseif
    Trimming trailing whitespace also
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90287 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Notificator.body.php
+++ Notificator.body.php
@@ -80,27 +80,27 @@ public static function checkEmailAddress( $string ) {
 			// local part length exceeded
 			$isValid = false;
 		}
-		else if ( $domainLen < 1 || $domainLen > 255 ) {
+		elseif ( $domainLen < 1 || $domainLen > 255 ) {
 			// domain part length exceeded
 			$isValid = false;
 		}
-		else if ( $local[0] == '.' || $local[$localLen -1] == '.' ) {
+		elseif ( $local[0] == '.' || $local[$localLen -1] == '.' ) {
 			// local part starts or ends with '.'
 			$isValid = false;
 		}
-		else if ( preg_match( '/\\.\\./', $local ) ) {
+		elseif ( preg_match( '/\\.\\./', $local ) ) {
 			// local part has two consecutive dots
 			$isValid = false;
 		}
-		else if ( !preg_match( '/^[A-Za-z0-9\\-\\.]+$/', $domain ) ) {
+		elseif ( !preg_match( '/^[A-Za-z0-9\\-\\.]+$/', $domain ) ) {
 			// character not valid in domain part
 			$isValid = false;
 		}
-		else if ( preg_match( '/\\.\\./', $domain ) ) {
+		elseif ( preg_match( '/\\.\\./', $domain ) ) {
 			// domain part has two consecutive dots
 			$isValid = false;
 		}
-		else if ( !preg_match( '/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+		elseif ( !preg_match( '/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
 			str_replace( "\\\\", "", $local ) ) ) {
 			// character not valid in local part unless
 			// local part is quoted
