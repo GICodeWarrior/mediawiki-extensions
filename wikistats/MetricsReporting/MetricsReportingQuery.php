355d1ac0150e452562d0a4adb1bc796f1539066f
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-17 21:55:22 +0000

    naming issues in last commit...this is the proper code'
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90321 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- wikistats/MetricsReporting/MetricsReportingQuery.php
+++ wikistats/MetricsReporting/MetricsReportingQuery.php
@@ -1,113 +1,19 @@
-<?php 
-
-$wgeAnalyticsFieldNames = array(
-	'select_regions' => "REGION",
-	'select_countries' => "COUNTRY",
-	'select_web_projects' => "WEBPROJ",
-	'select_wikis' => "WIKI",
-	'select_editors' => "EDITOR",
-	'select_edits' => "EDITS",
-	'select_platform' => "MOBILE",
-);
-
-$wgeAnalyticsFieldReturns = array(
-	'select_regions' => "region",
-	'select_countries' => "country",
-	'select_web_projects' => "web project",
-	'select_wikis' => "wiki",
-	'select_editors' => "editors",
-	'select_edits' => "edits",
-	'select_platform' => "mobile",
-);
-
-
-$wgeAnalyticsValidParams = array(
-
-"select_regions" => array(
-	"AS" => "Asia Pacific",
-	"C" => "China",
-	"EU" => "Europe",
-	"I"  => "India",
-	"LA" => "Latin-America",
-	"MA" => "Middle-East/Africa",
-	"NA" => "North-America",
-	"US" => "United States",
-	"W" => "World",
-),
-//"select_countries" => array(),
-//"select_web_properties"=> array(),
-"select_projects"=> array(
-	"wb" => "Wikibooks",
-	"wk" => "Wiktionary",
-	"wn" => "Wikinews",
-	"wp" => "Wikipedia",
-	"wq" => "Wikiquote",
-	"ws" => "Wikisource",
-	"wv" => "Wikiversity",
-	"co" => "Commons",
-	"wx" => "Other projects", 
-),
-//"select_wikis"=> array(),
-"select_editors"=> array(
-	"A" => "Anonymous",
-	"R" => "Registered User",
-	"B" => "Bot",
-),
-"select_edits"=> array(
-	"M" => "Manual",
-	"B" => "Bot",
-),
-"select_platform"=> array(
-	"M" => "Moblie",
-	"N" => "Non-Mobile",
-),
-
-);
-
-
-$wgAutoloadClasses['ApiAnalytics'] = $dir . '/api/ApiAnalytics.php';
-$wgAutoloadClasses['ApiAnalyticsMetric'] = $dir . '/api/ApiAnalyticsMetric.php';
-$wgAutoloadClasses['MetricsReportingQuery'] = $dir . '/MetricsReportingQuery.php';
-
-
-$dir = dirname( __FILE__ );
-
-
-$wgeAnalyticsMetricsList = array();
-$metricsdir = $dir."/metrics";
-$dh = opendir($metricsdir);
-while( ($file = readdir($dh)) !== false){
-	if(filetype($dir.$file) == "file" ){
-		$file_path_parts = pathinfo($dir.$file);
-		if($file_path_parts['extension'] == 'php'){
-			$wgAutoloadClasses["ApiAnalyticsMetric{$file_path_parts['filename']}"] = $dir.$file;
-			$wgeAnalyticsMetricsList[] = $file_path_parts['filename'];
-		}
+<?php
+class MetricsReportingQuery{
+	
+	public $query = "";
+	public $queryProperties = array();
+	
+	public function __construct(){
 	}
-}
 
-
-$wgAPIModules['analytics'] = 'ApiAnalytics';
-
-
-
-function wfAnalyticsMetricConnection() {
-	global $wgeAnalyticsMetricDBserver, $wgeAnalyticsMetricDBname;
-	global $wgeAnalyticsMetricDBuser, $wgeAnalyticsMetricDBpassword;
-	
-	static $db;
-	
-	if ( !$db ) {
-		$db = new DatabaseMysql(
-		$wgeAnalyticsMetricDBserver,
-		$wgeAnalyticsMetricDBuser,
-		$wgeAnalyticsMetricDBpassword,
-		$wgeAnalyticsMetricDBname );
-		$db->query( "SET names utf8" );
+	public function __construct($initial_query, $initial_prop_name, $initial_prop_value){
+		$this->query = $initial_query;
+		$this->queryProperties = array($initial_prop_name => $initial_prop_value);
 	}
 	
-	return $db;
+	public function mergeWith(MetricsReportingQuery $queryObj){
+		$this->query = $this->query.$queryObj->query;
+		$this->queryProperties = array_merge($this->queryProperties, $queryObj->queryProperties);
+	}
 }
-
-
-class 
