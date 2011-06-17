1d55793b3f0a71d494764621c3648b5b1b6eb26d
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-17 02:00:03 +0000

    Created MetricsReportingQuery object, added more readable comments
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90251 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- wikistats/MetricsReporting/api/ApiAnalytics.php
+++ wikistats/MetricsReporting/api/ApiAnalytics.php
@@ -10,39 +10,79 @@ class ApiAnalytics extends ApiBase {
 	
 	public function execute() {
 		
-			global $wgeAnalyticsMetricsList;
+			global $wgeAnalyticsMetricsList, $wgeAnalyticsFieldReturns;
 			$params = $this->extractRequestParams();
 			$result = $this->getResult();
 
 			foreach($wgeAnalyticsMetricsList as $metricName){
+				//find the metric requested
 				if($metricName == $params["metric"]){
+					
+					//instantiate the class and an empty array of metricsreportingquery objects
 					$metricClassName = "ApiAnalyticsMetric$metricName";
 					$metric = new $metricClassName;
 					$filterParams = $metric->getAllowedFilterParams();
 					$queries = array();
+					
+					//check to see which filters are set
 					foreach($filterParams as $filterParam){
 						if(isset( $params["$filterParam"])){
-							$queries= array_merge($queries, $this->collectQueryFilters($filterParam, $params["$filterParam"]));
-						}
-					}
+							$filter_results = $this->collectQueryFilters($filterParam, $params["$filterParam"]);
+							
+							//if multiple results returned, merge each of the query objects with whatever exists
+							if (count($filter_results) > 1){
+								foreach ($filter_results as $result){
+									foreach ($queries as $queryObj){
+										$result->merge($queryObj);
+									}
+								}
+								$queries = $filter_results;		
+							}
+							//else just merge with the only object
+							else{
+								foreach($queries as &$queryObj){
+									$queryObj->merge($filter_results[0]);
+								}
+							}
+						} //endif
+					}//end filters
 					
+					//calculate time
 					$range = " DATE = {$params["months"]}"; //TODO: query
-					
+					$minDate = $params["months"];
 					if(strpos($params["months"],";") !== FALSE){
 						$rangeVals = explode($params["months"], ";");
 						$range = " DATE >= $rangeVals[0]  AND DATE <= $rangeVals[1]";
+						$minDate = $rangeVals[0];
+					}
+					$timeStart = 0;
+					if(($timeStart = strtotime($str)) !== false){
+						$result->addValue("","timeStart",strfmttime("%Y%m%d%H%M%S",$timeStart));
 					}
 					
-					foreach($queries as &$query){
-						$query = $query . " AND " . $range;
+					//foreach query
+					foreach($queries as &$queryObj){
+						$queryObj->query = $queryObj->query . " AND " . $range;
+						
+						foreach($queryObj->queryProperties as $filterName => $filterVal){
+							$result->addValue("", $filterName, $filterVal); //filters
+						}
 						
 						if($metric->canBeNormalized){
-							$result->addValue("","normalized","false");
 							//TODO: modify queries for normalization
 							if(isset($params["normalized"]) && (strpos($params["normalized"], "true") !== FALSE)){
 							//is normalized
-							$result->addValue("","normalized","true");
+								$result->addValue("","normalized","true");
+							} else {
+								$result->addValue("","normalized","false");	
 							}
+							
+							
+							if(isset($params["modailty"]) && (strpos($params["modality"], "indexed") !== FALSE)){
+								//modify query for indexed
+								$result->addValue("","modality","indexed");
+							}
+							
 						}
 							
 					}
@@ -51,8 +91,6 @@ class ApiAnalytics extends ApiBase {
 					
 					$dbr = wfAnalyticsMetricConnection();
 					//TODO: build return object from queries
-					
-					
 					
 				}
 			}
@@ -65,7 +103,7 @@ class ApiAnalytics extends ApiBase {
 		
 		//get rid of any potential whitespace
 		$param = preg_replace('/\s\s+/', '', $fieldVal);
-		$returnQueries = array();
+		$returnQueries = array(new MetricsReportingQuery);
 		
 		if(!isset ($wgeAnalyticsFieldNames["$field"]) ){
 			return $returnQueries;
@@ -85,17 +123,20 @@ class ApiAnalytics extends ApiBase {
 				foreach($and_params as $and_param){
 						$this->validate_atomic_param($and_param, $field);
 					}
-					$returnQueries[] = "$filedName = $sub_query ";  //TODO: This is not what the query means
+					$returnQueries[] = new MetricsReportingQuery("$filedName = $sub_query ",
+					$fieldName,$sub_query);;  //TODO: This is not what the query means
 			}
 			else{
 				if(!$topval){
 					//TODO:check for {project}:{lang} (ie wp:en) style here
 					
 					$this->validate_atomic_param($sub_query, $field);
-					$returnQueries[] = "$fieldName = $sub_query ";		
+					$returnQueries[] = new MetricsReportingQuery("$fieldName = $sub_query ", //TODO: this might be what this query means
+					$fieldName, $sub_query);		
 				}
 				else{
-					$returnQueries[] = "$fieldName < $topval"; //TODO: This is not what this query means
+					$returnQueries[] = new MetricsReportingQuery("$fieldName < $topval", //TODO: This is not what this query means
+					$fieldName, $topval); 
 				}
 			}
 		}
