8bedee4064a607dd3187bdde52cea54692881219
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-20 19:01:37 +0000

    ParserPlayground: split node tree view setup to its own jquery-friendly module
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90478 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- modules/ext.parserPlayground.js
+++ modules/ext.parserPlayground.js
@@ -20,111 +20,6 @@
  */
 (function( $ ) {
 
-function htmlEscape(str) {
-    return mw.html.escape( str );
-}
-
-/**
- * Render an XML tree into this thingy.
- * @param {DOMNode} node
- * @param {jQuery} dest a list object!
- * @param {HashMap} inspectorMap
- */
-function renderXmlTree(node, dest, inspectorMap) {
-    if (node.nodeType == Node.ELEMENT_NODE) {
-        var base = '<span style="color:#808">' + htmlEscape(node.nodeName) + '</span>',
-            str = '&lt;' + base,
-            closer;
-        $.each(node.attributes, function(i, attr) {
-            str += ' ' + htmlEscape(attr.nodeName) + '=<span style="color:#008">"' + htmlEscape(htmlEscape(attr.nodeValue)) + '"</span>';
-        });
-        if (node.childNodes.length == 0) {
-            str += ' /&gt;';
-            dest.append('<li>' + str + '</li>');
-        } else {
-            str += '&gt;';
-            closer = '&lt;/' + base + '&gt;';
-            var chunk = $('<li>' +
-                          '<div class="mw-pp-node">' + str + '</div>' +
-                          '<ul></ul>' +
-                          '<div class="mw-pp-node">' + closer + '</div>' +
-                          '</li>');
-            var sublist = chunk.find('ul');
-            dest.append(chunk);
-            $.each(node.childNodes, function(i, child) {
-                renderXmlTree(child, sublist);
-            });
-        }
-    } else if (node.nodeType == Node.TEXT_NODE) {
-        dest.append($('<li style="white-space: pre-wrap; font-family: monospace"></li>').text(node.textContent));
-    }
-}
-
-/**
- * Render a JSON tree into this thingy.
- * @param {mixed} node
- * @param {jQuery} dest a list object!
- * @param {HashMap} inspectorMap
- */
-function renderJsonTree(node, dest, inspectorMap) {
-    var type = (typeof node);
-    var chunk, item, sublist;
-    if (type == 'object' && node === null) {
-        dest.append('null');
-    } else if (type == 'object' && node instanceof Array) {
-        chunk = $('<div>' +
-                  '<span class="mw-pp-node">[</span>' +
-                  '<ul></ul>' +
-                  '<span class="mw-pp-node">]</span>' +
-                  '</div>');
-        sublist = chunk.find('ul');
-        $.each(node, function(i, val) {
-            item = $('<li></li>');
-            renderJsonTree(val, item, inspectorMap);
-            sublist.append(item);
-        });
-        dest.append(chunk);
-    } else if (type == 'object') {
-        chunk = $('<div class="parseNode">' +
-                  '<span class="mw-pp-node">{</span>' +
-                  '<ul></ul>' +
-                  '<span class="mw-pp-node">}</span>' +
-                  '</div>');
-        chunk.data('parseNode', node); // assign the node for the tree inspector
-        if (inspectorMap) {
-            // save reverse lookup for the inspector
-            inspectorMap.put(node, chunk[0]);
-        }
-        sublist = chunk.find('ul'); // hack
-        $.each(node, function(key, val) {
-            var item = $('<li><span style="color:#808">' + htmlEscape('' + key) + '</span>:&nbsp;</li>');
-            renderJsonTree(val, item, inspectorMap);
-            sublist.append(item);
-        });
-        dest.append(chunk);
-    } else if (type == 'string') {
-        dest.append(htmlEscape(JSON.stringify(node))); // easy way to escape :)
-    } else {
-        dest.append(htmlEscape('' + node));
-    }
-}
-
-/**
- * Render a JSON or XML tree into this thingy.
- * @param {mixed} node
- * @param {jQuery} dest a list object!
- * @param {HashMap} inspectorMap
- */
-function renderTree(node, dest, inspectorMap) {
-    var render;
-    if (node instanceof Node) {
-        render = renderXmlTree;
-    } else {
-        render = renderJsonTree;
-    }
-    render(node, dest, inspectorMap);
-}
-
 
 var onResize = null;
 $(window).resize(function() {
@@ -242,8 +137,7 @@ var addParserModes = function(modes, parserClass, className, detail) {
         render: function(src, dest) {
             var parser = new parserClass();
             parser.parseToTree(src, function(tree, err) {
-                var target = $('<ul><li></li></ul>').appendTo(dest);
-                renderTree(tree, target.find('li'));
+				$(dest).nodeTree( tree );
             });
         }
 	};
@@ -269,7 +163,9 @@ var addParserModes = function(modes, parserClass, className, detail) {
             parser.parseToTree(src, function(tree, err) {
                 var target = makeInspectorColumns(dest);
                 var left = target.find('.left'), right = target.find('.right');
-                renderTree(tree, left, treeMap);
+				left.nodeTree( tree, function( node, el ) {
+					treeMap.put( node, el );
+				});
                 parser.treeToHtml(tree, function(node, err) {
                     right.append(node);
                     setupInspector(left, right, treeMap, renderMap);
