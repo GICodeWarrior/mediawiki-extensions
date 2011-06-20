8b0020f0950b09aee6e4f976a5517858390fd5c4
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-20 08:12:22 +0000

    Added documentation
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90443 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fundraiser-statistics/fundraiser-scripts/web_reporting/settings.py
+++ fundraiser-statistics/fundraiser-scripts/web_reporting/settings.py
@@ -97,6 +97,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'campaigns',
     'tests',
+    'LML',
+    'live_results',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- fundraiser-statistics/fundraiser-scripts/web_reporting/views.py
+++ fundraiser-statistics/fundraiser-scripts/web_reporting/views.py
@@ -1,4 +1,19 @@
-# Create your views here.
+"""
+    DJANGO VIEW DEFINITIONS:
+    ========================
+    
+    A very basic home url view for the Fundraiser Web Reporting
+    
+    Views:
+    
+    index           -- Simply calls the index template that links to the functional pieces of web reporting
+    
+"""
+
+__author__ = "Ryan Faulkner"
+__revision__ = "$Rev$"
+__date__ = "June 20th, 2011"
+
 from django.shortcuts import render_to_response
 from django.http import Http404
 from django.shortcuts import render_to_response, get_object_or_404
