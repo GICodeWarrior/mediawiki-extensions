5e90947c950d99ab129355ca7fd6e9e4e25bb171
nagelp <nagelp@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-21 05:00:05 +0000

    More escaping to reduce reviewer anxiety (love that word ;). Also escaping wfMsg() now, and thus had to remove <em> tags from i18n file.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90510 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Notificator/SpecialNotificator.php
+++ extensions/Notificator/SpecialNotificator.php
@@ -22,15 +22,17 @@ function execute( $par ) {
 	$receiver = $wgRequest->getText( 'receiver' );
 
 	if ( ! $pageId || ! $revId || ! $receiver ) {
-		$output = '<span class="error">' . wfMsg( 'notificator-special-page-accessed-directly' ) . '</span>';
+		$output = '<span class="error">' . htmlspecialchars(
+			wfMsg( 'notificator-special-page-accessed-directly' ) ) . '</span>';
 	} else {
 		$titleObj = Title::newFromID( $pageId );
 		$pageTitle = $titleObj->getFullText();
 		$linkToPage = $titleObj->getFullURL();
 
 		if ( ! Notificator::receiverIsValid( $receiver ) ) {
-			$output = '<span class="error">' . wfMsg( 'notificator-e-mail-address-invalid' ) . ' ' .
-				wfMsg( 'notificator-notification-not-sent' ) . '</span>';
+			$output = '<span class="error">' . htmlspecialchars(
+				wfMsg( 'notificator-e-mail-address-invalid' ) . ' ' .
+				wfMsg( 'notificator-notification-not-sent' ) ) . '</span>';
 			$output .= Notificator::getReturnToText( $linkToPage, $pageTitle );
 			$wgOut->addHTML( $output );
 			return;
@@ -41,37 +43,53 @@ function execute( $par ) {
 		if ( $oldRevId >= 0 ) {
 			if ( $oldRevId > 0 ) {
 				// Receiver has been notified before - send the diff to the last notified revision
-				$mailSubjectPrefix = '[' . wfMsg( 'notificator-change-tag' ) . '] ';
+				$mailSubjectPrefix = '[' . htmlspecialchars( wfMsg( 'notificator-change-tag' ) ) . '] ';
 
 				$wgOut->addModules( 'mediawiki.legacy.diff' );
 				$diff = Notificator::getNotificationDiffHtml( $oldRevId, $revId );
 				$notificationText = wfMsg( 'notificator-notification-text-changes',
-					htmlspecialchars( $wgUser->getName() ), '<a href="' . $linkToPage . '">' .
-					$pageTitle . '</a>' ) . '<div style="margin-top: 1em;">' . $diff . '</div>';
+					htmlspecialchars( $wgUser->getName() ),
+					Html::element(
+						'a',
+						array( 'href' => $linkToPage ),
+						$pageTitle
+						)
+					) .
+					Html::rawElement(
+						'div',
+						array( 'style' => 'margin-top: 1em' ),
+						$diff
+						);
 			} else {
 				// Receiver has never been notified about this page - so don't send a diff, just the link
-				$mailSubjectPrefix = '[' . wfMsg( 'notificator-new-tag' ) . '] ';
+				$mailSubjectPrefix = '[' . htmlspecialchars( wfMsg( 'notificator-new-tag' ) ) . '] ';
 				$notificationText = wfMsg( 'notificator-notification-text-new',
-					htmlspecialchars( $wgUser->getName() ), '<a href="' . $linkToPage . '">' .
-					$pageTitle . '</a>' );
+					htmlspecialchars( $wgUser->getName() ),
+					Html::element(
+						'a',
+						array( 'href' => $linkToPage ),
+						$pageTitle
+						)
+					);
 			}
 			$mailSubject = htmlspecialchars( $mailSubjectPrefix . $pageTitle );
 
 			if ( Notificator::sendNotificationMail( $receiver, $mailSubject, $notificationText ) ) {
-				$output = '<strong>' . wfMsg( 'notificator-following-e-mail-sent-to',
+				$output = '<strong>' . htmlspecialchars( wfMsg( 'notificator-following-e-mail-sent-to' ),
 					htmlspecialchars( $receiver ) ) . '</strong><div style="margin-top: 1em;"><h3>' .
 					wfMsg( 'notificator-subject' ) . ' ' . $mailSubject . '</h3><p>' . $notificationText .
 					'</p></div>';
 				Notificator::recordNotificationInDatabase( $pageId, $revId, $receiver );
 			} else {
-				$output = '<span class="error">' . wfMsg( 'notificator-error-sending-e-mail',
-					htmlspecialchars( $receiver ) ) . '</span>';
+				$output = '<span class="error">' . htmlspecialchars(
+					wfMsg( 'notificator-error-sending-e-mail', $receiver ) ) . '</span>';
 			}
 		} elseif ( $oldRevId == -1 ) {
-			$output = '<span class="error">' . wfMsg( 'notificator-error-parameter-missing' ) . '</span>';
+			$output = '<span class="error">' . htmlspecialchars(
+				wfMsg( 'notificator-error-parameter-missing' ) ) . '</span>';
 		} elseif ( $oldRevId == -2 ) {
-			$output = '<strong>' . wfMsg( 'notificator-notified-already', htmlspecialchars( $receiver ) ) .
-				' ' . wfMsg( 'notificator-notification-not-sent' ) . '</strong>';
+			$output = '<strong>' . htmlspecialchars ( wfMsg( 'notificator-notified-already',
+				$receiver ) . ' ' . wfMsg( 'notificator-notification-not-sent' ) ) . '</strong>';
 		}
 
 		$output .= Notificator::getReturnToText( $linkToPage, $pageTitle );
