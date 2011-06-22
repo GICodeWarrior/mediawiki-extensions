e9d0e97d209eb138172752374011f4d6cb8b2033
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-22 23:06:25 +0000

    used in 'campaigns' django view to display a list of campaigns with donation, timestamp, and report info
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90628 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fundraiser-statistics/fundraiser-scripts/sql/report_campaign_totals.sql
+++ fundraiser-statistics/fundraiser-scripts/sql/report_campaign_totals.sql
@@ -6,15 +6,15 @@ select
 faulkner.test.test_name,
 drupal.contribution_tracking.utm_campaign,
 sum(not isnull(contribution_tracking.contribution_id)) as donations,
-min(receive_date) as earliest_timestamp,
-if(not isnull(faulkner.test.test_name),1,0) as report_exists
+min(ts) as earliest_timestamp,
+if(not isnull(faulkner.test.test_name),1,NULL) as report_exists
 
 from 
 drupal.contribution_tracking left join civicrm.civicrm_contribution ON (drupal.contribution_tracking.contribution_id = civicrm.civicrm_contribution.id)
 left join faulkner.test on faulkner.test.utm_campaign = drupal.contribution_tracking.utm_campaign
 
-where receive_date >=  %s and receive_date < %s  
+where ts >= '%s' and ts < '%s'  
 
 group by 2
-order by 3 desc;
+order by 4 desc;
 
