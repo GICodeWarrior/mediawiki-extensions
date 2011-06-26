bfb450bdd774b4a6c024a289cd66097ebf295359
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-26 17:15:45 +0000

    Add i18n file Register extension to Translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90837 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SignupAPI.i18n.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalisation for SignupAPI
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'signupapi-desc' => 'Cleans up Special:UserLogin from signup related stuff & adds an API for signup',
+);
+
