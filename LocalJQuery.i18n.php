d4d7d04f83bb7f22ceb92980a8f30aa02ac160ec
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-27 20:37:08 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-06-27 20:21:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90906 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- LocalJQuery.i18n.php
+++ LocalJQuery.i18n.php
@@ -12,3 +12,52 @@ $messages['en'] = array(
 	'localjquery-desc' => 'Load jQuery from [[MediaWiki:JQuery.js]] (with ResourceLoader)',
 );
 
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author Wizardist
+ */
+$messages['be-tarask'] = array(
+	'localjquery-desc' => 'Загрузка jQuery з [[MediaWiki:JQuery.js]] (праз ResourceLoader)',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'localjquery-desc' => 'Ermöglicht das Laden einer lokalen [[MediaWiki:JQuery.js]] mithilfe des MediaWiki-ResourceLoader',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'localjquery-desc' => 'טעינת jQuery מתוך [[MediaWiki:JQuery.js]] (עם ResourceLoader)',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'localjquery-desc' => 'Cargar jQuery ab [[MediaWiki:JQuery.js]] (con ResourceLoader)',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'localjquery-desc' => 'Вчитај jQuery од [[MediaWiki:JQuery.js]] (со ResourceLoader)',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'localjquery-desc' => 'jQuery van [[MediaWiki:JQuery.js]] laden (met ResourceLoader)',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'localjquery-desc' => 'Carrega código jQuery de [[MediaWiki:JQuery.js]] (com o Carregador de Recursos)',
+);
+
