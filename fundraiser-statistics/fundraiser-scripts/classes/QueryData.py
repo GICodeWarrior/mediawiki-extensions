429dc0fd52405b0c9475e2d377a09994a5142aeb
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-28 21:21:03 +0000

    Added references for "report_bannerLP_metrics_minutely" and "report_bannerLP_confidence" queries
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91006 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fundraiser-statistics/fundraiser-scripts/classes/QueryData.py
+++ fundraiser-statistics/fundraiser-scripts/classes/QueryData.py
@@ -1,27 +1,32 @@
 
 """
 
-This module effectively functions as a Singleton class.
-
-This module contains and organizes query info.  Depends on the contents of ../sql/ where filenames are 
-coupled with query_name parameters
-
- METHODS:
+    This module effectively functions as a Singleton class.
     
-        format_query         
-        get_query             
-        get_query_header     
-        get_key_index
-        get_count_index
-        get_time_index
-        get_campaign_index
-        get_banner_index
-        get_landing_page_index
-        get_metric_index
-        get_plot_title
-        get_plot_ylabel
-        get_metric_full_name
+    This module contains and organizes query info.  Depends on the contents of ../sql/ where filenames are 
+    coupled with query_name parameters
+    
+     METHODS:
         
+            format_query         
+            get_query             
+            get_query_header     
+            get_key_index
+            get_count_index
+            get_time_index
+            get_campaign_index
+            get_banner_index
+            get_landing_page_index
+            get_metric_index
+            get_plot_title
+            get_plot_ylabel
+            get_metric_full_name
+       
+
+    QUERIES:
+        {report_campaign_metrics_minutely_all, report_banner_metrics_minutely_all, report_LP_metrics_minutely_all} ==> used for live reporting on donations and clicks .. over all campaigns, banners, and LPs
+        {report_campaign_metrics_minutely, report_banner_metrics_minutely, report_LP_metrics_minutely}             ==> used for interval reporting on campaign tests
+        {report_campaign_metrics_minutely_total}                                                                   ==> reports totals for a camapign -- combines with report_campaign_metrics_minutely
 """
 
 __author__ = "Ryan Faulkner"
@@ -114,6 +119,7 @@ def format_query(query_name, sql_stmnt, args):
         sql_stmnt = sql_stmnt % ('%','%','%','%', start, end)
     
     elif query_name == 'report_banner_confidence':
+        
         start = args[0]
         end = args[1]
         banner = args[2]
@@ -126,20 +132,29 @@ def format_query(query_name, sql_stmnt, args):
         
         sql_stmnt = sql_stmnt % (imp_start_time_obj_str, end, banner, start, end, campaign, start, end, banner, start, end, campaign, banner)
     
+    elif query_name == 'report_bannerLP_confidence':
+        
+        """ The artifact name is a composition of banner and landing page and must be split """
+        bannerlp_arg = args[2].split('-')
+        
+        start = args[0]
+        end = args[1]
+        utm_source = bannerlp_arg[0]
+        campaign = args[3]
+        
+        """ The start time for the impression portion of the query should be one second less"""
+        start_time_obj = TP.timestamp_to_obj(start,1)
+        imp_start_time_obj = start_time_obj + datetime.timedelta(seconds=-1)
+        imp_start_time_obj_str = TP.timestamp_from_obj(imp_start_time_obj, 1, 3)
+        
+        sql_stmnt = sql_stmnt % (imp_start_time_obj_str, end, utm_source, start, end, campaign, start, end, utm_source, start, end, campaign, utm_source)
+        
     elif query_name == 'report_LP_confidence':
         start = args[0]
         end = args[1]
         lp = args[2]
         campaign = args[3]
         sql_stmnt = sql_stmnt % (start, end, campaign, lp, start, end, campaign, lp)
-    
-    elif query_name == 'report_bannerLP_confidence':
-        start = args[0]
-        end = args[1]
-        banner = args[2]
-        lp = args[3]
-        campaign = args[4]
-        sql_stmnt = sql_stmnt % (start, end, banner, start, end, banner, campaign, start, end, banner, lp, campaign, start, end, banner, lp, campaign)
         
     elif query_name == 'report_ecomm_by_amount':
         start_time = args[0]
@@ -150,7 +165,7 @@ def format_query(query_name, sql_stmnt, args):
         where_str = args[0]
         sql_stmnt = sql_stmnt % ('%', '%', '%', '%', where_str)
     
-    elif query_name == 'report_banner_metrics_minutely':
+    elif query_name == 'report_banner_metrics_minutely' or query_name == 'report_bannerLP_metrics_minutely':
         start_time = args[0]
         end_time = args[1]
         campaign = args[2]
@@ -163,7 +178,7 @@ def format_query(query_name, sql_stmnt, args):
         
         sql_stmnt = sql_stmnt % ('%', '%', '%',  '%', interval, interval, imp_start_time_obj_str, end_time, '%', '%',  '%',  '%', interval, interval, start_time, end_time, campaign, \
                                 '%', '%',  '%',  '%', interval, interval, start_time, end_time, '%', '%',  '%',  '%', interval, interval, start_time, end_time, campaign, campaign)
-        
+    
     elif query_name == 'report_LP_metrics_minutely':
         start_time = args[0]
         end_time = args[1]
@@ -207,7 +222,14 @@ def format_query(query_name, sql_stmnt, args):
         utm_campaign = args[2]
         
         sql_stmnt = sql_stmnt % (start_time, end_time, utm_campaign)
-            
+    
+    elif query_name == 'report_campaign_metrics_minutely_all' or query_name == 'report_banner_metrics_minutely_all' or query_name == 'report_lp_metrics_minutely_all':
+        start_time = args[0]
+        end_time = args[1]
+        interval = args[3]
+        
+        sql_stmnt = sql_stmnt % ('%', '%', '%',  '%', interval, interval, start_time, end_time)
+        
     else:
         return 'no such table\n'
 
@@ -230,6 +252,8 @@ def get_query_header(query_name):
 def get_key_index(query_name):
     if query_name == 'report_banner_metrics_minutely':
         return 1
+    elif query_name == 'report_bannerLP_metrics_minutely':
+        return 1
     elif query_name == 'report_LP_metrics_minutely':
         return 1
     elif query_name == 'report_campaign_metrics_minutely':
@@ -242,7 +266,11 @@ def get_key_index(query_name):
         return 0
     elif query_name == 'report_campaign_lps':
         return 0
-
+    elif query_name == 'report_campaign_metrics_minutely_all' or query_name == 'report_banner_metrics_minutely_all' or query_name == 'report_lp_metrics_minutely_all':
+        return 1
+    else:
+        return 1
+    
 """     
     Returns the index of the timestamp for the query data 
 """
@@ -265,6 +293,8 @@ def get_time_index(query_name):
         return 0
     elif query_name == 'report_banner_metrics_minutely':
         return 0
+    elif query_name == 'report_bannerLP_metrics_minutely':
+        return 0
     elif query_name == 'report_LP_metrics_minutely':
         return 0
     elif query_name == 'report_campaign_metrics_minutely':
@@ -272,6 +302,8 @@ def get_time_index(query_name):
     elif query_name == 'report_campaign_metrics_minutely_total':
         return 0
     elif query_name == 'report_campaign_totals':
+        return 0
+    elif query_name == 'report_campaign_metrics_minutely_all' or query_name == 'report_banner_metrics_minutely_all' or query_name == 'report_lp_metrics_minutely_all':
         return 0
     else:
         return -1
@@ -400,6 +432,8 @@ def get_metric_index(query_name, metric_name):
             return 8
         elif metric_name == 'amt50_per_imp':
             return 10
+        elif metric_name == 'click_rate':
+            return 7
         else:
             return -1
     elif query_name == 'report_campaign_metrics_minutely':
@@ -425,6 +459,36 @@ def get_metric_index(query_name, metric_name):
             return 3
         else:
             return -1
+    
+    elif query_name == 'report_campaign_metrics_minutely_all' or query_name == 'report_banner_metrics_minutely_all' or query_name == 'report_lp_metrics_minutely_all':
+        if metric_name == 'donations':
+            return 2
+        elif metric_name == 'clicks':
+            return 3
+        else:
+            return -1
+    
+    elif query_name == 'report_bannerLP_metrics_minutely':
+        if metric_name == 'imp':
+            return 2
+        elif metric_name == 'views':
+            return 3
+        elif metric_name == 'donations':
+            return 4
+        elif metric_name == 'amount50':
+            return 6
+        elif metric_name == 'don_per_imp':
+            return 8
+        elif metric_name == 'amt50_per_imp':
+            return 10
+        elif metric_name == 'don_per_view':
+            return 11
+        elif metric_name == 'amt50_per_view':
+            return 13
+        elif metric_name == 'click_rate':
+            return 7
+        else:
+            return -1
         
     else:
         return 'no such table'
@@ -447,26 +511,43 @@ def get_plot_ylabel(query_name):
     
 def get_metric_full_name(metric_name):
     if metric_name == 'imp':
-        return 'BANNER IMPRESSIONS'
+        return 'Banner Impressions'
     elif metric_name == 'views':
-        return 'LANDING PAGE VIEWS'
+        return 'Landing Page Views'
     elif metric_name == 'don_per_imp':
-        return 'DONATIONS PER IMPRESSION'
+        return 'Donations per Impression'
     elif metric_name == 'don_per_view':
-        return 'DONATIONS PER VIEW'
+        return 'Donations per View'
     elif metric_name == 'amt50_per_imp':
-        return 'AMOUNT50 PER IMPRESSION'
+        return 'Amount50 per Impression'
     elif metric_name == 'amt50_per_view':
-        return 'AMOUNT50 PER VIEW'
+        return 'Amount50 per View'
     elif metric_name == 'amount50':
-        return 'AMOUNT50'
+        return 'Amount50'
     elif metric_name == 'donations':
-        return 'DONATIONS'
+        return 'Donations'
     elif metric_name == 'amt_per_imp':
-        return 'AMOUNT PER IMPRESSION'
+        return 'Amount per Impression'
     elif metric_name == 'amt_per_view':
-        return 'AMOUNT PER VIEW'
+        return 'Amount per View'
     elif metric_name == 'amount':
-        return 'AMOUNT'
+        return 'Amount'
+    elif metric_name == 'click_rate':
+        return 'Banner Click Rate'
     else:
         return'no such metric'
+
+
+def get_metric_data_type(metric_name, elem):
+    
+    if metric_name == 'ts' or metric_name == 'day_hr' or metric_name == 'utm_campaign' or metric_name == 'utm_source' or metric_name == 'landing_page':
+        return elem
+    elif metric_name == 'imp' or metric_name == 'impressions' or metric_name == 'views' or metric_name == 'donations' or metric_name ==  'clicks' or metric_name == 'total_clicks':
+        return str(int(elem))
+    elif metric_name == 'amt50_per_view' or metric_name == 'amt50_per_imp' or metric_name == 'don_per_view' or metric_name == 'don_per_imp' or metric_name == 'amt_per_imp' or metric_name == 'amt_per_view' or metric_name == 'click_rate':
+        return "%.6f" % elem
+    elif metric_name == 'amount' or metric_name == 'amount50' or metric_name == 'amount100':
+        return "%.2f" % elem
+    else:
+        return'no such metric'
+    
