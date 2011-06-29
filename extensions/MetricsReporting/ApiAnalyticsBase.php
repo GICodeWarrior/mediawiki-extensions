f8234255df2ee4bf62b2fcc3c73ecc1689d499c7
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-29 20:11:46 +0000

    Fleshing out description type stuffs
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91101 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MetricsReporting/ApiAnalyticsBase.php
+++ extensions/MetricsReporting/ApiAnalyticsBase.php
@@ -27,6 +27,128 @@ abstract class ApiAnalyticsBase extends ApiBase/*ApiQueryBase*/ {
 		return $this->mDb;
 	}
 
+	/**
+	 * @return array
+	 */
+	public function getAllowedFilters() {
+		return array();
+	}
+
+	public function getAllowedParams() {
+		$params = array(
+			'months' => 'string',
+			'normalized' => 'bool',
+			'data' => array(
+				ApiBase::PARAM_ISMULTI => true,
+				ApiBase::PARAM_TYPE => array(
+					'timeseries',
+					'timeseriesindexed',
+					'percentagegrowthlastmonth',
+					'percentagegrowthlasyyear',
+					'percentagegrowthfullperiod',
+				),
+			),
+			'reportlanguage' => array(
+				ApiBase::PARAM_DFLT => 'en',
+				ApiBase::PARAM_TYPE => array(
+					'en',
+				),
+			),
+		);
+
+		$select = array(
+			'selectregions' => array(
+				ApiBase::PARAM_ISMULTI => true,
+				ApiBase::PARAM_TYPE => array(
+					'as',
+					'c',
+					'eu',
+					'i',
+					'la',
+					'ma',
+					'na',
+					'us',
+					'w',
+				),
+			),
+			'selectcountries' => array(
+				ApiBase::PARAM_ISMULTI => false,
+				ApiBase::PARAM_TYPE => 'string',
+			),
+			'selectwebproperties' => array(
+				ApiBase::PARAM_ISMULTI => false,
+				ApiBase::PARAM_TYPE => 'string',
+			),
+			'selectprojects' => array(
+				ApiBase::PARAM_ISMULTI => false,
+				ApiBase::PARAM_TYPE => array(
+					'wb',
+					'wk',
+					'wm',
+					'wp',
+					'wq',
+					'ws',
+					'wv',
+					'co',
+					'wx',
+				),
+			),
+			'selectwikis' => array(
+				ApiBase::PARAM_ISMULTI => false,
+				ApiBase::PARAM_TYPE => 'string',
+			),
+			'selecteditors' => array(
+				ApiBase::PARAM_ISMULTI => false,
+				ApiBase::PARAM_TYPE => array(
+					'a',
+					'r',
+					'b',
+				),
+			),
+			'selectedits' => array(
+				ApiBase::PARAM_ISMULTI => false,
+				ApiBase::PARAM_TYPE => array(
+					'm',
+					'b',
+				),
+			),
+			'selectplatforms' => array(
+				ApiBase::PARAM_ISMULTI => false,
+				ApiBase::PARAM_TYPE => array(
+					'm',
+					'n'
+				),
+			),
+		);
+
+		return array_merge( $params, array_intersect_key( $select, $this->getAllowedFilters() ) );
+	}
+
+	public function getParamDescription() {
+		return array(
+			'months' => 'First and last month to include in time series',
+			'normalized' => array(
+
+			),
+			'data' => array(
+
+			),
+			'reportlanguage' => '',
+			'selectregions' => '',
+			'selectcountries' => '',
+			'selectwebproperties' => '',
+			'selectprojects' => '',
+			'selectwikis' => '',
+			'selecteditors' => '',
+			'selectedits' => '',
+			'selectplatform' => '',
+		);
+	}
+
+	//public abstract function getDescription();
+
+	//protected abstract function getExamples();
+
 	/*public function getPossibleErrors() {
 		return array_merge( parent::getPossibleErrors(), array(
 		) );
