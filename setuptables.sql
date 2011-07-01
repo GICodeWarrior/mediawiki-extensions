f584426fa70f96155ef66e5d52fa85f4623cd61a
kbrown <kbrown@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-01 22:57:10 +0000

    * follow up on r91317 to readd spider.php and setuptables.sql with the correct lowercase name to adhere to coding conventions * add function to spider.php to deal with replag on databases to prevent duplication by mulitple instances of the spider * fix setuptables.sql to actually have data instead of being empty (problem in r91047)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91319 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ setuptables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS `el_archive_blacklist` (
+  `bl_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `bl_type` tinyint(4) NOT NULL,
+  `bl_url` varchar(10000) NOT NULL,
+  `bl_expiry` int(11) unsigned NOT NULL,
+  `bl_reason` varchar(255) NOT NULL,
+  PRIMARY KEY (`bl_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+CREATE TABLE IF NOT EXISTS `el_archive_log` (
+  `log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `log_result` tinyint(4) NOT NULL,
+  `log_url` varchar(10000) NOT NULL,
+  `log_time` int(11) unsigned NOT NULL,
+  `log_http_code` varchar(255) NOT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `el_archive_queue` (
+  `queue_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `page_id` int(11) unsigned NOT NULL,
+  `url` varchar(10000) NOT NULL,
+  `delay_time` int(11) unsigned NOT NULL,
+  `insertion_time` int(11) unsigned NOT NULL,
+  `in_progress` varchar(50) NOT NULL,
+  PRIMARY KEY (`queue_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=105 ;
