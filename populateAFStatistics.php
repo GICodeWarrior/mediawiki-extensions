002a718fe08679122b4e2aaf50f77c54dd10a595
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-01 02:10:07 +0000

    Fix name collision with Page class in core
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91245 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- populateAFStatistics.php
+++ populateAFStatistics.php
@@ -12,65 +12,65 @@ class PopulateAFStatistics extends Maintenance {
 	 * @var int
 	 */
 	public $insert_batch_size = 100;
-	
+
 	/**
 	 * The period (in seconds) before now for which to gather stats
 	 * @var int
 	 */
 	public $polling_period = 86400;
-	
+
 	/**
 	 * The formatted timestamp from which to determine stats
 	 * @var int
 	 */
 	protected $lowerBoundTimestamp;
-	
+
 	/**
 	 * DB slave
 	 * @var object
 	 */
 	protected $dbr;
-	
+
 	/**
 	 * DB master
 	 * @var object
 	 */
 	protected $dbw;
-	
+
 	/**
 	 * Valid operations and their execution methods for this script to perform
-	 * 
+	 *
 	 * Operations are passed in as options during run-time - only valid options,
 	 * which are defined here, can be executed. Valid operations are mapped here
 	 * to a corresponding method ( array( 'operation' => 'method' ))
 	 * @var array
 	 */
-	protected $operation_map = array( 
-		'highslows' => 'populateHighsLows', 
+	protected $operation_map = array(
+		'highslows' => 'populateHighsLows',
 		'problems' => 'populateProblems',
 	);
-	
+
 	/**
 	 * Operations to execute
 	 * @var array
 	 */
 	public $operations = array();
-	
+
 	/**
 	 * The minimum number of rating sets required before taking some action
 	 * @var int
 	 */
 	public $rating_set_threshold = 10;
-	
+
 	public function __construct() {
 		parent::__construct();
 		$this->mDescription = "Populates the article feedback stats tables";
-		
+
 		$this->addOption( 'op', 'The ArticleFeedback stats gathering operation to run (eg "highslows").  Can specify multiple operations, separated by comma.', true, true );
 		$this->addOption( 'rating_sets', 'The minimum number of rating sets before taking an action.', false, true );
 		$this->addOption( 'poll_period', 'The polling period for fetching data, in seconds.', false, true );
 	}
-	
+
 	public function syncDBs() {
 		// FIXME: Copied from populateAFRevisions.php, which coppied from updateCollation.php, should be centralized somewhere
 		$lb = wfGetLB();
@@ -82,11 +82,11 @@ class PopulateAFStatistics extends Maintenance {
 			$lb->waitForAll( $pos );
 		}
 	}
-	
+
 	/**
 	 * Bootstrap this maintenance script
-	 * 
-	 * Performs operations necessary for this maintenance script to run which 
+	 *
+	 * Performs operations necessary for this maintenance script to run which
 	 * cannot or do not make sense to run in the constructor.
 	 */
 	public function bootstrap() {
@@ -120,18 +120,18 @@ class PopulateAFStatistics extends Maintenance {
 		} else {
 			$this->polling_period = $polling_period;
 		}
-		
+
 		// set db objects
 		$this->dbr = wfGetDB( DB_SLAVE );
 		$this->dbw = wfGetDB( DB_MASTER );
 	}
-	
+
 	/**
 	 * Check whether or not specified operations are valid.
-	 * 
+	 *
 	 * A specified operation is considered valid if it exists
 	 * as a key in the operation map.
-	 * 
+	 *
 	 * @param array $ops An array of operations to check
 	 * @return bool
 	 */
@@ -143,21 +143,21 @@ class PopulateAFStatistics extends Maintenance {
 		}
 		return true;
 	}
-	
+
 	public function execute() {
 		// finish bootstrapping the script
 		$this->bootstrap();
-		
+
 		// execute requested operations
 		foreach ( $this->operations as $operation ) {
 			$method = $this->operation_map[ $operation ];
 			$this->$method();
 		}
 	}
-	
+
 	public function populateProblems() {
 		global $wgMemc;
-		
+
 		/**
 		 * Chck to see if we already have a collection of pages to operate on.
 		 * If not, generate the collection of pages and their associated ratings.
@@ -174,19 +174,19 @@ class PopulateAFStatistics extends Maintenance {
 			if ( $page->rating_set_count < $this->rating_set_threshold ) {
 				continue;
 			}
-			
+
 			if ( $page->isProblematic() ) {
 				$problems[] = $page->page_id;
 			}
 		}
-		
+
 		// populate stats table with problem articles & associated data
 		// fetch stats type id - add stat type if it's non-existent
 		$stats_type_id = SpecialArticleFeedback::getStatsTypeId( 'problems' );
 		if ( !$stats_type_id ) {
 			$stats_type_id = $this->addStatType( 'problems' );
 		}
-		
+
 		$rows = array();
 		$cur_ts = $this->dbw->timestamp();
 		$count = 0;
@@ -199,7 +199,7 @@ class PopulateAFStatistics extends Maintenance {
 				'afs_ts' => $cur_ts,
 				'afs_stats_type_id' => $stats_type_id,
 			);
-			
+
 			$count++;
 			if ( $count >= 50 ) {
 				// No more than 50
@@ -208,7 +208,7 @@ class PopulateAFStatistics extends Maintenance {
 			}
 		}
 		$this->output( "Done.\n" );
-		
+
 		// Insert the problem rows into the database
 		$this->output( "Writing data to article_feedback_stats ...\n" );
 		$rowsInserted = 0;
@@ -216,7 +216,7 @@ class PopulateAFStatistics extends Maintenance {
 		$rowsCopy = $rows;
 		while( $rows ) {
 			$batch = array_splice( $rows, 0, $this->insert_batch_size );
-			$this->dbw->insert( 
+			$this->dbw->insert(
 				'article_feedback_stats',
 				$batch,
 				__METHOD__
@@ -226,7 +226,7 @@ class PopulateAFStatistics extends Maintenance {
 			$this->output( "Inserted " . $rowsInserted . " rows\n" );
 		}
 		$this->output( "Done.\n" );
-		
+
 		// populate cache with current problem articles
 		$this->output( "Caching latest problems (if cache present).\n" );
 		// grab the article feedback special page so we can reuse the data structure building code
@@ -237,15 +237,15 @@ class PopulateAFStatistics extends Maintenance {
 		$wgMemc->set( $key, $problems, 86400 );
 		$this->output( "Done.\n" );
 	}
-	
+
 	/**
 	 * Populate stats about highest/lowest rated articles
 	 */
 	public function populateHighsLows() {
 		global $wgMemc;
-		
+
 		$averages = array(); // store overall averages for a given page
-		
+
 		/**
 		 * Chck to see if we already have a collection of pages to operate on.
 		 * If not, generate the collection of pages and their associated ratings.
@@ -262,12 +262,12 @@ class PopulateAFStatistics extends Maintenance {
 			if ( $page->rating_set_count < $this->rating_set_threshold ) {
 				continue;
 			}
-			
+
 			// calculate the rating averages if they haven't already been calculated
 			if ( !count( $page->rating_averages )) {
 				$page->calculateRatingAverages();
 			}
-			
+
 			// store overall average rating seperately so we can easily sort
 			$averages[ $page->page_id ] = $page->overall_average;
 		}
@@ -285,13 +285,13 @@ class PopulateAFStatistics extends Maintenance {
 			$highest_and_lowest_page_ids += array_slice( $averages, -50, 50, true );
 		}
 		$this->output( "Done\n" );
-		
+
 		// fetch stats type id - add stat type if it's non-existant
 		$stats_type_id = SpecialArticleFeedback::getStatsTypeId( 'highs_and_lows' );
 		if ( !$stats_type_id ) {
 			$stats_type_id = $this->addStatType( 'highs_and_lows' );
 		}
-		
+
 		// prepare data for insert into db
 		$this->output( "Preparing data for db insertion ...\n");
 		$cur_ts = $this->dbw->timestamp();
@@ -315,7 +315,7 @@ class PopulateAFStatistics extends Maintenance {
 		$rowsCopy = $rows;
 		while( $rows ) {
 			$batch = array_splice( $rows, 0, $this->insert_batch_size );
-			$this->dbw->insert( 
+			$this->dbw->insert(
 				'article_feedback_stats',
 				$batch,
 				__METHOD__
@@ -325,7 +325,7 @@ class PopulateAFStatistics extends Maintenance {
 			$this->output( "Inserted " . $rowsInserted . " rows\n" );
 		}
 		$this->output( "Done.\n" );
-		
+
 		// loading data into cache
 		$this->output( "Caching latest highs/lows (if cache present).\n" );
 		$key = wfMemcKey( 'article_feedback_stats_highs_lows' );
@@ -336,10 +336,10 @@ class PopulateAFStatistics extends Maintenance {
 		$wgMemc->set( $key, $highs_lows, 86400 );
 		$this->output( "Done\n" );
 	}
-	
+
 	/**
 	 * Fetch ratings newer than a given time stamp.
-	 * 
+	 *
 	 * If no timestamp is provided, relies on $this->lowerBoundTimestamp
 	 * @param numeric $ts
 	 * @return database result object
@@ -348,60 +348,60 @@ class PopulateAFStatistics extends Maintenance {
 		if ( !$ts ) {
 			$ts = $this->getLowerBoundTimestamp();
 		}
-		
+
 		if ( !is_numeric( $ts )) {
 			throw new InvalidArgumentException( 'Timestamp expected to be numeric.' );
 		}
-		
+
 		$res = $this->dbr->select(
-			'article_feedback', 
-			array( 
+			'article_feedback',
+			array(
 				'aa_revision',
 				'aa_user_text',
 				'aa_rating_id',
 				'aa_user_anon_token',
-				'aa_page_id', 
+				'aa_page_id',
 				'aa_rating_value',
-			), 
+			),
 			array( 'aa_timestamp >= ' . $this->dbr->addQuotes( $this->dbr->timestamp( $ts ) ) ),
 			__METHOD__,
 			array()
 		);
-		
+
 		return $res;
 	}
-	
+
 	/**
 	 * Construct collection of pages and their ratings since a given time stamp
 	 * @param $ts
 	 * @return object The colelction of pages
 	 */
 	public function populatePageRatingsSince( $ts ) {
-		$pages = new Pages();
+		$pages = new AFPages();
 		// fetch the ratings since the lower bound timestamp
 		$this->output( 'Fetching page ratings between now and ' . date( 'Y-m-d H:i:s', strtotime( $ts )) . "...\n" );
 		$res = $this->fetchRatingsNewerThanTs( $ts );
 		$this->output( "Done.\n" );
-			
+
 		// assign the rating data to our data structure
 		$this->output( "Assigning fetched ratings to internal data structure ...\n" );
 		foreach ( $res as $row ) {
 			// fetch the page from the page store referentially so we can
 			// perform actions on it that will automagically be saved in the
 			// object for easy access later
-			
+
 			$page =& $pages->getPage( $row->aa_page_id );
-			
+
 			// determine the unique hash for a given rating set (page rev + user identifying info)
 			$rating_hash = $row->aa_revision . "|" . $row->aa_user_text . "|" . $row->aa_user_anon_token;
-			
+
 			// add rating data for this page
-			$page->addRating( $row->aa_rating_id, $row->aa_rating_value, $rating_hash );	
+			$page->addRating( $row->aa_rating_id, $row->aa_rating_value, $rating_hash );
 		}
 		$this->output( "Done.\n" );
 		return $pages;
 	}
-	
+
 	/**
 	 * Set $this->timestamp
 	 * @param int $ts
@@ -412,13 +412,13 @@ class PopulateAFStatistics extends Maintenance {
 		}
 		$this->lowerBoundTimestamp = $ts;
 	}
-	
+
 
 	/**
 	 * Get $this->lowerBoundTimestamp
-	 * 
+	 *
 	 * If it hasn't been set yet, set it based on the defined polling period.
-	 * 
+	 *
 	 * @return int
 	 */
 	public function getLowerBoundTimestamp() {
@@ -428,13 +428,13 @@ class PopulateAFStatistics extends Maintenance {
 		}
 		return $this->lowerBoundTimestamp;
 	}
-	
+
 	/**
 	 * Add stat type record to article_feedbak_stats_types
 	 * @param string $stat_type The identifying name of the stat type (eg 'highs_lows')
 	 */
 	public function addStatType( $stat_type ) {
-		$this->dbw->insert( 
+		$this->dbw->insert(
 			'article_feedback_stats',
 			array( 'afst_type' => $stat_type ),
 			__METHOD__
@@ -446,15 +446,15 @@ class PopulateAFStatistics extends Maintenance {
 /**
  * A class to represent a page and data about its ratings
  */
-class Page {
+class AFPage {
 	public $page_id;
-	
+
 	/**
 	 * The number of rating sets recorded for this page
 	 * @var int
 	 */
 	public $rating_set_count = 0;
-	
+
 	/**
 	 * An array of ratings for this page
 	 * @var array
@@ -466,27 +466,27 @@ class Page {
 	 * @var array
 	 */
 	public $rating_averages = array();
-	
+
 	/**
 	 * Mean of all ratings for this page
 	 * @var float
 	 */
 	public $overall_average;
-	
+
 	/**
 	 * An array of rating set hashes, which are used to identify unique sets of
 	 * ratings
 	 * @var array
 	 */
 	protected $rating_set_hashes = array();
-	
+
 	public function __construct( $page_id ) {
 		if ( !is_numeric( $page_id )) {
 			throw new Exception( 'Page id must be numeric.' );
 		}
 		$this->page_id = $page_id;
 	}
-	
+
 	/**
 	 * Add a new rating for this particular page
 	 * @param int $rating_id
@@ -495,15 +495,15 @@ class Page {
 	 */
 	public function addRating( $rating_id, $rating_value, $rating_set_hash = null ) {
 		$this->ratings[ $rating_id ][] = $rating_value;
-		
+
 		if ( $rating_set_hash ) {
-			$this->trackRatingSet( $rating_set_hash );	
+			$this->trackRatingSet( $rating_set_hash );
 		}
 	}
-	
+
 	/**
 	 * Keep track of rating sets
-	 * 
+	 *
 	 * Record when we see a new rating set and increment the set count
 	 * @param string $rating_set_hash
 	 */
@@ -511,11 +511,11 @@ class Page {
 		if ( isset( $this->rating_set_hashes[ $rating_set_hash ] )) {
 			return;
 		}
-		
+
 		$this->rating_set_hashes[ $rating_set_hash ] = 1;
 		$this->rating_set_count += 1;
 	}
-	
+
 	public function calculateRatingAverages() {
 		// determine averages for each rating type
 		foreach( $this->ratings as $rating_id => $rating ) {
@@ -523,8 +523,8 @@ class Page {
 			$rating_avg = $rating_sum / count( $rating );
 			$this->rating_averages[ $rating_id ] = $rating_avg;
 		}
-		
-		// determine overall rating average for this page		
+
+		// determine overall rating average for this page
 		if ( count( $this->rating_averages )) {
 			$overall_rating_sum = array_sum( $this->rating_averages );
 			$overall_rating_average = $overall_rating_sum / count( $this->rating_averages );
@@ -533,7 +533,7 @@ class Page {
 		}
 		$this->overall_average = $overall_rating_average;
 	}
-	
+
 	/**
 	 * Returns whether or not this page is considered problematic
 	 * @return bool
@@ -544,11 +544,11 @@ class Page {
 		}
 		return $this->problematic;
 	}
-	
+
 	/**
 	 * Determine whether this article is  'problematic'
 	 *
-	 * If a page has one or more rating categories where 70% of the ratings are 
+	 * If a page has one or more rating categories where 70% of the ratings are
 	 * <= 2, it is considered problematic.
 	 */
 	public function determineProblematicStatus() {
@@ -559,14 +559,14 @@ class Page {
 					$count += 1;
 				}
 			}
-				
+
 			$threshold = round( 0.7 * count( $ratings ));
 			if ( $count >= $threshold ) {
 				$this->problematic = true;
 				return;
 			}
 		}
-		
+
 		$this->problematic = false;
 		return;
 	}
@@ -574,27 +574,27 @@ class Page {
 
 /**
  * A storage class to keep track of PageRatings object by page
- * 
+ *
  * Iterable on array of pages.
  */
-class Pages implements IteratorAggregate {
+class AFPages implements IteratorAggregate {
 	/**
 	 * An array of page rating objects
 	 * @var array
 	 */
 	public $pages = array();
-	
+
 	public function &getPage( $page_id ) {
 		if ( !isset( $this->pages[ $page_id ] )) {
 			$this->addPage( $page_id );
 		}
 		return $this->pages[ $page_id ];
 	}
-	
+
 	public function addPage( $page_id ) {
-		$this->pages[ $page_id ] = new Page( $page_id );
+		$this->pages[ $page_id ] = new AFPage( $page_id );
 	}
-	
+
 	public function getIterator() {
 		return new ArrayIterator( $this->pages );
 	}
