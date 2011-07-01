6e384c682a6022d02f5ea81f3a25d0f061edf721
kbrown <kbrown@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-01 22:53:02 +0000

    * rename SpecialModifyBlacklist to SpecialModifyArchiveBlacklist to avoid confusion with other blacklists in mediawiki. * fix problems in r90691 with brackets in the i18n file, they are now directly in the class * (mostly) fix spaces instead of tabs in files for indentation, converted to adhere to style conventions * will rename Spider.php and SetupTables.sql to use lowercase letters instead of uppercase to adhere to coding conventions in next revision
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91317 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ArchiveLinks.i18n.php
+++ ArchiveLinks.i18n.php
@@ -1,23 +1,23 @@
 <?php
+
 /**
  * Internationalization File for Archive Links
  */
-
 $messages = array();
 
 //English
-$messages['en'] = array (
-    'archivelinks-cache-title' => '[cache]',
-    'ModifyBlacklist' => 'Modify Blacklist',
-    'archivelinks-modify-blacklist-desc' => 'This page allows you to blacklist or whitelist URLs for the ArchiveLinks extension.',
-    //'archivelinks-archive-blacklist-fieldset-label' => 'Blacklist a URL',
-    'archivelinks-modify-blacklist-url-field-label' => 'URL to Blacklist:',
-    'archivelinks-modify-blacklist-reason-field-label' => 'Reason for Blacklisting:',
-    'archivelinks-modify-blacklist-add-or-remove-field-label' => 'What would you like to do?',
-    'archivelinks-modify-blacklist-add-field-label' => 'Add',
-    'archivelinks-modify-blacklist-remove-field-label' => 'Remove',
-    'archivelinks-modify-blacklist-duration-field-label' => 'Expiry:',
-    'archivelinks-modify-blacklist-whitelist-field-label' => 'Blacklist',
-    'archivelinks-modify-blacklist-blacklist-field-label' => 'Whitelist',
-    'archivelinks-modify-blacklist-blacklist-or-whitelist-field-label' => 'Which list would you like to modify?',
+$messages['en'] = array(
+	'archivelinks-cache-title' => 'cache',
+	'ModifyArchiveBlacklist' => 'Modify Blacklist',
+	'archivelinks-modify-blacklist-desc' => 'This page allows you to blacklist or whitelist URLs for the ArchiveLinks extension.',
+	//'archivelinks-archive-blacklist-fieldset-label' => 'Blacklist a URL',
+	'archivelinks-modify-blacklist-url-field-label' => 'URL to Blacklist:',
+	'archivelinks-modify-blacklist-reason-field-label' => 'Reason for Blacklisting:',
+	'archivelinks-modify-blacklist-add-or-remove-field-label' => 'What would you like to do?',
+	'archivelinks-modify-blacklist-add-field-label' => 'Add',
+	'archivelinks-modify-blacklist-remove-field-label' => 'Remove',
+	'archivelinks-modify-blacklist-duration-field-label' => 'Expiry:',
+	'archivelinks-modify-blacklist-whitelist-field-label' => 'Blacklist',
+	'archivelinks-modify-blacklist-blacklist-field-label' => 'Whitelist',
+	'archivelinks-modify-blacklist-blacklist-or-whitelist-field-label' => 'Which list would you like to modify?',
 );
--- ArchiveLinks.php
+++ ArchiveLinks.php
@@ -1,31 +1,32 @@
 <?php
+
 /**
  * This is an extension to archive preemptively archive external links so that
  * in the even they go down a backup will be available.
  */
-
 if ( !defined( 'MEDIAWIKI' ) ) {
 	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
 	die( 1 );
 }
 
-error_reporting ( E_ALL | E_STRICT );
+error_reporting( E_ALL | E_STRICT );
 
 $path = dirname( __FILE__ );
 
-$wgExtensionMessagesFiles['ArchiveLinks'] =  "$path/ArchiveLinks.i18n.php";
-$wgExtensionMessagesFiles['ModifyBlacklist'] = "$path/ArchiveLinks.i18n.php";
+$wgExtensionMessagesFiles['ArchiveLinks'] = "$path/ArchiveLinks.i18n.php";
+$wgExtensionMessagesFiles['ModifyArchiveBlacklist'] = "$path/ArchiveLinks.i18n.php";
 
 $wgAutoloadClasses['ArchiveLinks'] = "$path/ArchiveLinks.class.php";
-$wgAutoloadClasses['SpecialModifyBlacklist'] = "$path/SpecialModifyBlacklist.php";
-    
+$wgAutoloadClasses['SpecialModifyArchiveBlacklist'] = "$path/SpecialModifyArchiveBlacklist.php";
+
 $wgHooks['ArticleSaveComplete'][] = 'ArchiveLinks::queueExternalLinks';
 $wgHooks['LinkerMakeExternalLink'][] = 'ArchiveLinks::rewriteLinks';
 
-$wgSpecialPages['ModifyBlacklist'] = 'SpecialModifyBlacklist';
+$wgSpecialPages['ModifyArchiveBlacklist'] = 'SpecialModifyArchiveBlacklist';
 
-$wgArchiveLinksConfig = array (
-    'archive_service' => 'wikiwix',
-    'use_multiple_archives' => false,
-    'run_spider_in_loop' => false,
+$wgArchiveLinksConfig = array(
+	'archive_service' => 'wikiwix',
+	'use_multiple_archives' => false,
+	'run_spider_in_loop' => false,
+	'in_progress_ignore_delay' => 7200,
 );
