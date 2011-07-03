d469f566bf4b5634bcf830559d65e884a0c09007
yuvipanda <yuvipanda@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-03 20:14:20 +0000

    Model to deal with reading stats from the db
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91382 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/GPoC/models/Statistics.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Has static methods to grab assessment statistics
+ **/
+
+class Statistics {
+	public static function getImportanceColumn( $importance ) {
+		$importanceColumnMapping = array(
+			'top' => 'ps_top_icount',
+			'high' => 'ps_high_icount',
+			'mid' => 'ps_mid_icount',
+			'low' => 'ps_mid_icount',
+			'no' => 'ps_no_icount',
+			'' => 'ps_unclassified_icount'
+		);
+
+		return $importanceColumnMapping[ strtolower( $importance ) ];
+	}
+
+	public static function getProjectStats( $project ) {
+		$dbr = wfGetDB( DB_SLAVE );
+		$query = $dbr->select(
+			"project_stats",
+			"*",
+			array(
+				"ps_project" => $project
+			),
+			__METHOD__
+		);
+
+		$project_statistics = array(
+			"top" => array(),
+			"high" => array(),
+			"mid" => array(),
+			"low" => array(),
+			"no" => array(),
+			"" => array()
+		);
+
+
+		foreach( $query as $row_object ) {
+			$data_row = (array)$row_object;
+			$quality = $data_row['ps_quality'];
+			foreach( $project_statistics as $importance => &$importance_row ) {
+				$importance_row[$quality] = $data_row[Statistics::getImportanceColumn( $importance )];
+			}
+		}
+
+		return $project_statistics;
+	}
+
+}
