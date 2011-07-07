9ca10de620a8d286ec6f4722d79319f6258ca05a
aigerim <aigerim@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-07 00:49:15 +0000

    Initial import of GSoC project.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91627 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ExtensionManagement/ExtensionManagement.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Special page aliases for the ExtensionManagement extension.
+ *
+ * @file ExtensionManagement.i18n.php
+ */
+
+$specialPageAliases = array();
+
+$specialPageAliases['en'] = array(
+	'ExtensionManagement' => array( 'ExtensionManagement' ),
+);
