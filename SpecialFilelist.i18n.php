eb1624b69f41da3d6c38ec75f89cbc5b36bdf576
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-09 20:12:43 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-07-09 19:33:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91807 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SpecialFilelist.i18n.php
+++ SpecialFilelist.i18n.php
@@ -1011,13 +1011,14 @@ $messages['yi'] = array(
 /** Simplified Chinese (‪中文(简体)‬)
  * @author Gzdavidwong
  * @author Liangent
+ * @author Xiaomingyan
  */
 $messages['zh-hans'] = array(
 	'filelist' => '文件列表',
 	'filelist-desc' => '在[[Special:Filelist|特殊页面]]列举上传的档案',
 	'filelist-show-list' => '显示列表',
 	'filelist-show-gallery' => '显示画廊',
-	'filelist-image-desc' => '递减',
+	'filelist-image-desc' => '降序',
 );
 
 /** Traditional Chinese (‪中文(繁體)‬)
