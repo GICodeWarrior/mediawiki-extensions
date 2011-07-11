b01c2ba7f469533e904c195f98b679d662a33d5d
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-11 21:48:10 +0000

    Reorganized some tests.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91916 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/tests/annotations/test.js
+++ parsers/wikidom/tests/annotations/test.js
@@ -55,33 +55,124 @@ var content = Content.newFromLines( lines );
 
 /* Tests */
 
-test( 'Content.substring returns correct plain text', function() {
-	equal( content.substring( 3, 39 ), 's is a test paragraph!\nParagraphs ca' );
-} );
-
-test( 'Content.substring uses data length if end is not given', function() {
-	equal( content.substring( 39 ), 'n have more than one line.\n' );
-} );
-
-test( 'Content.substring clamps out of range values', function() {
-	equal( content.substring( -10, 10 ), 'This is a ' );
-} );
-
-test( 'Content.substring called without arguments returns all text', function() {
+test( 'Content.substring', function() {
+	expect( 5 );
+	
+	equal(
+		content.substring( 3, 39 ),
+		's is a test paragraph!\nParagraphs ca',
+		'Returns correct plain text'
+	);
+	equal(
+		content.substring( 39 ),
+		'n have more than one line.\n',
+		'Uses data length if end is not given'
+	);
+	equal(
+		content.substring( -10, 10 ),
+		'This is a ',
+		'Clamps negetive start arguments'
+	);
+	equal(
+		content.substring( 39, 100000000000 ),
+		'n have more than one line.\n',
+		'Clamps out of range end arguments'
+	);
 	equal(
 		content.substring(),
-		'This is a test paragraph!\nParagraphs can have more than one line.\n'
+		'This is a test paragraph!\nParagraphs can have more than one line.\n',
+		'Called without arguments returns all text'
 	);
 } );
 
-test( 'getLength returns correct length', function() {
-	equals( content.getLength(), 66 );
-} );
+test( 'Content.getLength', function() {
+	equals( content.getLength(), 66, 'Returns correct length' );
+} )
 
-test( 'slice without arguments returns correct Content', function() {
-	deepEqual( content.slice().data, [["T",{"type":"italic"}],["h",{"type":"italic"}],["i",{"type":"italic"}],["s",{"type":"italic"}]," ","i","s"," ",["a",{"type":"xlink","data":{"url":"http://www.a.com"}}],[" ",{"type":"xlink","data":{"url":"http://www.a.com"}}],["t",{"type":"xlink","data":{"url":"http://www.a.com"}},{"type":"bold"}],["e",{"type":"xlink","data":{"url":"http://www.a.com"}},{"type":"bold"}],["s",{"type":"xlink","data":{"url":"http://www.a.com"}},{"type":"bold"}],["t",{"type":"xlink","data":{"url":"http://www.a.com"}},{"type":"bold"}]," ","p","a","r","a","g","r","a","p","h","!","\u000a","P","a","r","a","g","r","a","p","h","s"," ",["c",{"type":"italic"}],["a",{"type":"italic"}],["n",{"type":"italic"}]," ","h","a","v","e"," ",["m",{"type":"bold"}],["o",{"type":"bold"}],["r",{"type":"bold"}],["e",{"type":"bold"}]," ","t","h","a","n"," ","o","n","e"," ","l","i","n","e",".","\u000a"] );
-} );
-
-test( 'slice with arguments returns correct Content', function() {
-	deepEqual( content.slice(3, 10).data, [["s",{"type":"italic"}]," ","i","s"," ",["a",{"type":"xlink","data":{"url":"http://www.a.com"}}],[" ",{"type":"xlink","data":{"url":"http://www.a.com"}}]] );
+test( 'Content.slice', function() {
+	deepEqual(
+		content.slice().data,
+		[
+			["T", { "type": "italic" }],
+			["h", { "type": "italic" }],
+			["i", { "type": "italic" }],
+			["s", { "type": "italic" }],
+			" ",
+			"i",
+			"s",
+			" ",
+			["a", { "type": "xlink", "data": { "url":"http://www.a.com" } }],
+			[" ", { "type": "xlink", "data": { "url":"http://www.a.com" } }],
+			["t", { "type": "xlink", "data": { "url":"http://www.a.com" } }, { "type": "bold" }],
+			["e", { "type": "xlink", "data": { "url":"http://www.a.com" } }, { "type": "bold" }],
+			["s", { "type": "xlink", "data": { "url":"http://www.a.com" } }, { "type": "bold" }],
+			["t", { "type": "xlink", "data": { "url":"http://www.a.com" } }, { "type": "bold" }],
+			" ",
+			"p",
+			"a",
+			"r",
+			"a",
+			"g",
+			"r",
+			"a",
+			"p",
+			"h",
+			"!",
+			"\n",
+			"P",
+			"a",
+			"r",
+			"a",
+			"g",
+			"r",
+			"a",
+			"p",
+			"h",
+			"s",
+			" ",
+			["c", { "type": "italic" }],
+			["a", { "type": "italic" }],
+			["n", { "type": "italic" }],
+			" ",
+			"h",
+			"a",
+			"v",
+			"e",
+			" ",
+			["m", { "type": "bold" }],
+			["o", { "type": "bold" }],
+			["r", { "type": "bold" }],
+			["e", { "type": "bold" }],
+			" ",
+			"t",
+			"h",
+			"a",
+			"n",
+			" ",
+			"o",
+			"n",
+			"e",
+			" ",
+			"l",
+			"i",
+			"n",
+			"e",
+			".",
+			"\n"
+		],
+		'Called without arguments returns all data'
+	);
+	deepEqual(
+		content.slice( 3, 10 ).data,
+		[
+			["s", { "type": "italic" }],
+			" ",
+			"i",
+			"s",
+			" ",
+			["a", { "type": "xlink", "data": { "url":"http://www.a.com" } }],
+			[" ", { "type": "xlink", "data": { "url":"http://www.a.com" } }]
+		],
+		'Called with start and end returns range of data'
+	);
 } );
