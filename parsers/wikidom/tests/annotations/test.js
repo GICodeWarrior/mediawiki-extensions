3170264c437e88f1fe745abc4486db56d69af289
inez <inez@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-11 21:35:34 +0000

    Test for Content.slice without arguments passed
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91912 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/tests/annotations/test.js
+++ parsers/wikidom/tests/annotations/test.js
@@ -131,6 +131,10 @@ Content.prototype.remove = function( start, end ) {
 	this.data.splice( start, end - start );
 };
 
+Content.prototype.getLength = function() {
+	return this.data.length; 
+};
+
 var content = Content.newFromLines( lines );
 
 /* Tests */
@@ -152,4 +156,13 @@ test( 'Content.substring called without arguments returns all text', function() 
 		content.substring(),
 		'This is a test paragraph!\nParagraphs can have more than one line.\n'
 	);
+} );
+
+test( 'getLength returns correct length', function() {
+	equals( content.getLength(), 66 );
+} );
+
+test( '', function() {
+	var data = content.slice().data;
+	deepEqual( data, [["T",{"type":"italic"}],["h",{"type":"italic"}],["i",{"type":"italic"}],["s",{"type":"italic"}]," ","i","s"," ",["a",{"type":"xlink","data":{"url":"http://www.a.com"}}],[" ",{"type":"xlink","data":{"url":"http://www.a.com"}}],["t",{"type":"xlink","data":{"url":"http://www.a.com"}},{"type":"bold"}],["e",{"type":"xlink","data":{"url":"http://www.a.com"}},{"type":"bold"}],["s",{"type":"xlink","data":{"url":"http://www.a.com"}},{"type":"bold"}],["t",{"type":"xlink","data":{"url":"http://www.a.com"}},{"type":"bold"}]," ","p","a","r","a","g","r","a","p","h","!","\u000a","P","a","r","a","g","r","a","p","h","s"," ",["c",{"type":"italic"}],["a",{"type":"italic"}],["n",{"type":"italic"}]," ","h","a","v","e"," ",["m",{"type":"bold"}],["o",{"type":"bold"}],["r",{"type":"bold"}],["e",{"type":"bold"}]," ","t","h","a","n"," ","o","n","e"," ","l","i","n","e",".","\u000a"] );
 } );
