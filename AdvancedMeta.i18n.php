dff17271be768132dafd0904f6ce4ba43e4bd2eb
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-15 18:13:51 +0000

    Followup r92215: Tweak extension credits, sort messgaes by language code Add extension to Translatewiki
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@92263 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- AdvancedMeta.i18n.php
+++ AdvancedMeta.i18n.php
@@ -1,6 +1,10 @@
 <?php
-/*
- * Public Domain
+/**
+ * Internationalisation file for Advanced Meta extension.
+ *
+ * @file
+ * @ingroup Extensions
+ * @license Public Domain
  */
 
 $messages = array();
@@ -8,32 +12,40 @@ $messages = array();
 $messages['en'] = array(
 	'ameta-desc' => 'Allows per page meta settings (robots, keywords, description) and change title',
 	'ameta-metasettings' => 'Meta settings',
-	'ameta-titlealias' => 'Title Alias:',
-	'ameta-keywordsadd' => 'the following keywords are added to all pages: ',
+	'ameta-titlealias' => 'Title alias:',
+	'ameta-keywordsadd' => 'the following keywords are added to all pages:',
 	'ameta-keywordsmodify' => 'can be modified at Mediawiki:Globalkeywords',
-);
-
-$messages['fr'] = array(
-	'ameta-desc' => 'Permet le paramétrage des balises meta de chaque page (robots, mots clés, description) et le changement de titre',
-	'ameta-metasettings' => 'Paramètres des balises meta',
-	'ameta-titlealias' => 'Alias de titre:',
-	'ameta-keywordsadd' => 'Les mots clés suivant sont ajouté à toutes les pages: ',
-	'ameta-keywordsmodify' => 'peuvent être modifiées sur la page Mediawiki:Globalkeywords',
 );
 
 $messages['de'] = array(
 	'ameta-desc' => 'Ermöglicht das Festlegen der Meta-Elemente „robots“, „keywords“ und „description“ sowie das Anpassen des Seitentitels',
 	'ameta-metasettings' => 'Meta-Einstellungen',
 	'ameta-titlealias' => 'Title:',
-	'ameta-keywordsadd' => 'Die folgenden „keywords“ werden allen Seiten hinzugefügt: ',
+	'ameta-keywordsadd' => 'Die folgenden „keywords“ werden allen Seiten hinzugefügt:',
 	'ameta-keywordsmodify' => 'Sie können auf der Seite Mediawiki:Globalkeywords angepasst werden.',
+);
+
+$messages['fr'] = array(
+	'ameta-desc' => 'Permet le paramétrage des balises meta de chaque page (robots, mots clés, description) et le changement de titre',
+	'ameta-metasettings' => 'Paramètres des balises meta',
+	'ameta-titlealias' => 'Alias de titre:',
+	'ameta-keywordsadd' => 'Les mots clés suivant sont ajouté à toutes les pages:',
+	'ameta-keywordsmodify' => 'peuvent être modifiées sur la page Mediawiki:Globalkeywords',
+);
+
+$messages['nl'] = array(
+	'ameta-desc' => 'Maakt het mogelijk de meta-instellingen (robots, keywords, description) en de titel per pagina te veranderen',
+	'ameta-metasettings' => 'Meta-instellingen',
+	'ameta-titlealias' => 'Titel-alias:',
+	'ameta-keywordsadd' => 'de volgende trefwoorden worden aan elke pagina toegevoegd:',
+	'ameta-keywordsmodify' => 'kan worden gewijzigd op Mediawiki:Globalkeywords',
 );
 
 $messages['zh-hans'] = array(
 	'ameta-desc' => '允许为每个页面设置机器人政策、关键词和描述，以及修改标题',
 	'ameta-metasettings' => 'Meta设置',
 	'ameta-titlealias' => '修改标题:',
-	'ameta-keywordsadd' => '以下内容自动作为关键词添加: ',
+	'ameta-keywordsadd' => '以下内容自动作为关键词添加:',
 	'ameta-keywordsmodify' => '可在 Mediawiki:Globalkeywords 修改全局关键词',
 );
 
@@ -41,14 +53,6 @@ $messages['zh-hant'] = array(
 	'ameta-desc' => '允許為每個頁面設置機器人政策、關鍵字和描述，以及修改標題',
 	'ameta-metasettings' => 'Meta設置',
 	'ameta-titlealias' => '修改標題:',
-	'ameta-keywordsadd' => '以下內容自動作為關鍵字添加: ',
+	'ameta-keywordsadd' => '以下內容自動作為關鍵字添加:',
 	'ameta-keywordsmodify' => '可在 Mediawiki:Globalkeywords 修改全域關鍵字',
-);
-
-$messages['nl'] = array(
-	'ameta-desc' => 'Maakt het mogelijk de meta-instellingen (robots, keywords, description) en de titel per pagina te veranderen',
-	'ameta-metasettings' => 'Meta-instellingen',
-	'ameta-titlealias' => 'Titel-alias:',
-	'ameta-keywordsadd' => 'de volgende trefwoorden worden aan elke pagina toegevoegd: ',
-	'ameta-keywordsmodify' => 'kan worden gewijzigd op Mediawiki:Globalkeywords',
 );
