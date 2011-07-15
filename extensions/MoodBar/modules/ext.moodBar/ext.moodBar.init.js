34798791f4efd97fac29a417981c992290ddda61
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-15 17:35:07 +0000

    MoodBar front-end: * Init/show triger in init-module (position: top) * Bind and load the rest from the bottom in the core-module * Implement overlay div (with close-toggle) * Add i18n-messages * Makes use of new jquery.localize features
    Other fixes:
    * Only show when logged-in
    * Hook for mw.config
    * Merged the double i18n $messages['qqq'] with the first one
    
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@92256 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MoodBar/modules/ext.moodBar/ext.moodBar.init.js
+++ extensions/MoodBar/modules/ext.moodBar/ext.moodBar.init.js
@@ -1,36 +1,34 @@
 /**
- * Front-end scripting for the MoodBar MediaWiki extension
+ * Initialization script for the MoodBar MediaWiki extension
  *
  * @author Timo Tijhof, 2011
  */
 ( function( $ ) {
-
 	var mb = mw.moodBar = {
+
+		conf: mw.config.get( 'mbConfig' ),
 
 		ui: {
 			// jQuery objects
 			pMoodbar: null,
 			trigger: null,
-			overlay: null // @todo
+			overlay: null
 		},
-		event: {
-			trigger: function( e ) {
-				e.preventDefault();
-			}
-		},
+
 		init: function() {
 			var ui = mb.ui;
+
+			// Bucketing (@todo)
+			mb.conf.bucketKey = 'using';
 
 			// Create portlet
 			ui.pMoodbar = $( '<div id="p-moodbar"></div>' );
 
 			// Create trigger
-			ui.trigger = $( '<a>', {
-				href: '#',
-				title: mw.msg( 'tooltip-p-moodbar-trigger-using' ),
-				text: mw.msg( 'moodbar-trigger-using', mw.config.get( 'wgSiteName' ) ),
-				click: mb.event.trigger
-			});
+			ui.trigger = $( '<a>' )
+				.attr( 'href', '#' )
+				.attr( 'title', mw.msg( 'tooltip-p-moodbar-trigger-' + mb.conf.bucketKey ) )
+				.text( mw.msg( 'moodbar-trigger-' + mb.conf.bucketKey, mw.config.get( 'wgSiteName' ) ) );
 
 			// Insert trigger into portlet
 			ui.trigger
@@ -41,12 +39,13 @@
 			// Inject portlet into document, when document is ready
 			$( mb.inject );
 		},
+
 		inject: function() {
 			$( '#mw-head' ).append( mb.ui.pMoodbar );
 		}
 	
 	};
 
-	mw.moodBar.init();
+	mb.init();
 
 } )( jQuery );
