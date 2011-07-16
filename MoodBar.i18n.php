85f770ca558b28f63da2b8ec1785ca5e6f8e0f87
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-16 20:06:00 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-07-16 19:52:00 UTC) Ping r92057: Full export of core and ext-installer
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@92378 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MoodBar.i18n.php
+++ MoodBar.i18n.php
@@ -72,6 +72,26 @@ See also:
 	'moodbar-what-label' => 'Link text for the page where more info abut MoodBar can be found.',
 );
 
+/** Message documentation (Message documentation)
+ * @author SPQRobin
+ */
+$messages['qqq'] = array(
+	'moodbar-desc' => 'This is a feature in development. See [[mw:MoodBar 0.1/Design]] for background information.',
+	'moodbar-trigger-using' => "Link text of the MoodBar overlay trigger. \$1 is the SITENAME. The implied sentence is ''\"Using [Sitename] made me happy/sad/...\"''. See [[mw:MoodBar 0.1/Design]] for background development information.",
+	'moodbar-intro-using' => 'Intro title of the MoodBar overlay trigger. $1 is the SITENAME.',
+	'moodbar-trigger-feedback' => 'Link text of the MoodBar overlay trigger. $1 is the SITENAME.',
+	'moodbar-close' => 'Link text of the close-button. Make sure to include parentheses.
+
+See also:
+* {{msg|parentheses}}',
+	'moodbar-intro-feedback' => 'Intro title of the MoodBar overlay trigger. $1 is the SITENAME.',
+	'tooltip-moodbar-what' => 'Tooltip displayed when hovering the What-link.
+
+See also:
+* {{msg|moodbar-what-label}}',
+	'moodbar-what-label' => 'Link text for the page where more info abut MoodBar can be found.',
+);
+
 /** Afrikaans (Afrikaans)
  * @author Naudefj
  */
@@ -94,7 +114,32 @@ $messages['be-tarask'] = array(
 $messages['de'] = array(
 	'moodbar-desc' => 'Ermöglicht es Benutzern ihre aktuelle Stimmung dem Seitenbetreiber mitzuteilen',
 	'moodbar-trigger-using' => 'Nutze $1 …',
+	'moodbar-intro-using' => '$1 zu nutzen macht mich …',
 	'moodbar-trigger-feedback' => 'Rückmeldungen',
+	'moodbar-close' => '(schließen)',
+	'moodbar-intro-feedback' => 'Rückmeldung:',
+	'moodbar-type-happy-title' => 'glücklich',
+	'moodbar-type-sad-title' => 'traurig',
+	'moodbar-type-confused-title' => 'kirre',
+	'tooltip-moodbar-what' => 'Mehr zu dieser Funktion in Erfahrung bringen',
+	'moodbar-what-label' => 'Worum handelt es sich?',
+	'right-moodbar-view' => 'Rückmeldung zur Stimmung ansehen und exportieren',
+	'moodbar-admin-title' => 'Rückmeldung zur Stimmung',
+	'moodbar-admin-intro' => 'Auf dieser Seite können die Rückmeldungen zur Stimmung angesehen werden',
+	'moodbar-admin-download' => 'Als CSV-Datei herunterladen',
+	'moodbar-header-id' => 'Rückmeldungskennung',
+	'moodbar-header-timestamp' => 'Zeitstempel',
+	'moodbar-header-type' => 'Typ',
+	'moodbar-header-page' => 'Seite',
+	'moodbar-header-usertype' => 'Benutzerart',
+	'moodbar-header-user' => 'Benutzer',
+	'moodbar-header-editmode' => 'Bearbeitungsmodus',
+	'moodbar-header-bucket' => 'Testumgebung',
+	'moodbar-header-system' => 'Systemtyp',
+	'moodbar-header-locale' => 'Gebietsschema',
+	'moodbar-header-useragent' => 'Browser',
+	'moodbar-header-comment' => 'Kommentare',
+	'moodbar-header-user-editcount' => 'Bearbeitungszähler',
 );
 
 /** Spanish (Español)
@@ -135,7 +180,32 @@ $messages['he'] = array(
 $messages['ia'] = array(
 	'moodbar-desc' => 'Permitte al usatores specificate de inviar lor "humor" retro al operator del sito',
 	'moodbar-trigger-using' => 'Usante $1...',
-	'moodbar-trigger-feedback' => 'Commentos',
+	'moodbar-intro-using' => 'Le uso de $1 me rendeva...',
+	'moodbar-trigger-feedback' => 'Reaction',
+	'moodbar-close' => '(clauder)',
+	'moodbar-intro-feedback' => 'Reaction:',
+	'moodbar-type-happy-title' => 'Felice',
+	'moodbar-type-sad-title' => 'Triste',
+	'moodbar-type-confused-title' => 'Confuse',
+	'tooltip-moodbar-what' => 'Lege plus a proposito de iste function',
+	'moodbar-what-label' => 'Que es isto?',
+	'right-moodbar-view' => 'Vider e exportar reactiones del Barra de Humor',
+	'moodbar-admin-title' => 'Reactiones del Barra de Humor',
+	'moodbar-admin-intro' => 'Iste pagina permitte vider reactiones submittite con le Barra de Humor',
+	'moodbar-admin-download' => 'Discargar como CSV',
+	'moodbar-header-id' => 'ID del reaction',
+	'moodbar-header-timestamp' => 'Data e hora',
+	'moodbar-header-type' => 'Typo',
+	'moodbar-header-page' => 'Pagina',
+	'moodbar-header-usertype' => 'Typo de usator',
+	'moodbar-header-user' => 'Usator',
+	'moodbar-header-editmode' => 'Modo de modification',
+	'moodbar-header-bucket' => 'Situla de test',
+	'moodbar-header-system' => 'Typo de systema',
+	'moodbar-header-locale' => 'Region',
+	'moodbar-header-useragent' => 'Agente usator',
+	'moodbar-header-comment' => 'Commentos',
+	'moodbar-header-user-editcount' => 'Numero de modificationes del usator',
 );
 
 /** Luxembourgish (Lëtzebuergesch)
@@ -145,6 +215,18 @@ $messages['lb'] = array(
 	'moodbar-desc' => 'Erméiglecht et spezifesche Benotzer fir dem Responsabele vum Site hir Stëmmung ze schécken',
 	'moodbar-trigger-using' => 'Benotzt $1...',
 	'moodbar-trigger-feedback' => 'Feedback',
+	'moodbar-close' => '(zoumaachen)',
+	'moodbar-intro-feedback' => 'Feedback:',
+	'moodbar-type-happy-title' => 'Glécklech',
+	'moodbar-type-sad-title' => 'Traureg',
+	'moodbar-what-label' => 'Wat ass dat?',
+	'moodbar-admin-download' => 'Als CSV eroflueden',
+	'moodbar-header-type' => 'Typ',
+	'moodbar-header-page' => 'Säit',
+	'moodbar-header-usertype' => 'Benotzer-Typ',
+	'moodbar-header-user' => 'Benotzer',
+	'moodbar-header-comment' => 'Bemierkungen',
+	'moodbar-header-user-editcount' => 'Compteur vun den Ännerunge pro Benotzer',
 );
 
 /** Macedonian (Македонски)
@@ -153,7 +235,32 @@ $messages['lb'] = array(
 $messages['mk'] = array(
 	'moodbar-desc' => 'Им овозможува на одредени корисници да му го соопштуваат нивното „расположение“ на операторот на мрежното место',
 	'moodbar-trigger-using' => 'Користење на $1...',
+	'moodbar-intro-using' => '$1 ме...',
 	'moodbar-trigger-feedback' => 'Мислења',
+	'moodbar-close' => '(затвори)',
+	'moodbar-intro-feedback' => 'Мислења',
+	'moodbar-type-happy-title' => 'усреќи',
+	'moodbar-type-sad-title' => 'натажи',
+	'moodbar-type-confused-title' => 'збуни',
+	'tooltip-moodbar-what' => 'Дознајте повеќе за оваа функција',
+	'moodbar-what-label' => 'Што е ова?',
+	'right-moodbar-view' => 'Преглед и извоз на мислења од MoodBar',
+	'moodbar-admin-title' => 'Мислења со MoodBar',
+	'moodbar-admin-intro' => 'Оваа страница служи за преглед на мислења поднесени со MoodBar',
+	'moodbar-admin-download' => 'Преземи како CSV',
+	'moodbar-header-id' => 'Назнака (ID) на мислењето',
+	'moodbar-header-timestamp' => 'Време и датум',
+	'moodbar-header-type' => 'Вид',
+	'moodbar-header-page' => 'Страница',
+	'moodbar-header-usertype' => 'Вид на корисник',
+	'moodbar-header-user' => 'Корисник',
+	'moodbar-header-editmode' => 'Уреднички режим',
+	'moodbar-header-bucket' => 'Испробување',
+	'moodbar-header-system' => 'Вид на систем',
+	'moodbar-header-locale' => 'Место',
+	'moodbar-header-useragent' => 'Корисник-вршител',
+	'moodbar-header-comment' => 'Коментари',
+	'moodbar-header-user-editcount' => 'Бр. на кориснички уредувања',
 );
 
 /** Dutch (Nederlands)
@@ -162,7 +269,18 @@ $messages['mk'] = array(
 $messages['nl'] = array(
 	'moodbar-desc' => 'Laat bepaalde gebruikers toe hun "gemoedstoestand" naar de sitebeheerders te verzenden',
 	'moodbar-trigger-using' => 'Bij het gebruiken van $1...',
+	'moodbar-intro-using' => 'Het gebruiken van $1 maakte mij...',
 	'moodbar-trigger-feedback' => 'Terugkoppeling',
+	'moodbar-intro-feedback' => 'Terugkoppeling:',
+	'moodbar-type-happy-title' => 'Blij',
+	'moodbar-type-sad-title' => 'Triest',
+	'moodbar-type-confused-title' => 'Verward',
+	'tooltip-moodbar-what' => 'Meer informatie over deze functie',
+	'moodbar-what-label' => 'Wat is dit?',
+	'moodbar-admin-download' => 'Downloaden als CSV',
+	'moodbar-header-page' => 'Pagina',
+	'moodbar-header-user' => 'Gebruiker',
+	'moodbar-header-comment' => 'Opmerkingen',
 );
 
 /** Portuguese (Português)
