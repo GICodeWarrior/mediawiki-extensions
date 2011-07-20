4b1ea40ed2e125214010fa0087dff50d007809b0
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-20 20:21:18 +0000

    Add comments to SQL file, i18n
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@92675 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/EditPageTracking/EditPageTracking.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation file for EditPageTracking extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+ 
+$messages = array();
+ 
+/** English
+ * @author Andrew Garrett
+ */
+
+$messages['en'] = array(
+	'editpagetracking-desc' => 'Records the first time each user views the edit page',
+);
