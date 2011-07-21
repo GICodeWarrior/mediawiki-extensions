9688735fc9f5522a9f3d4a3ca0bdc4ed31bf9033
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-21 23:30:10 +0000

    MoodBar: Hide automatically after 3 seconds, add an ellipsis
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@92826 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MoodBar/MoodBar.i18n.php
+++ extensions/MoodBar/MoodBar.i18n.php
@@ -47,7 +47,7 @@ For more information, please visit the $1.',
 	'moodbar-loading-title' => 'Sharing...',
 	'moodbar-success-title' => 'Thanks!',
 	'moodbar-error-title' => 'Oops!',
-	'moodbar-loading-subtitle' => 'We are sharing your feedback.',
+	'moodbar-loading-subtitle' => 'We are sharing your feedback…',
 	'moodbar-success-subtitle' => 'Sharing your mood helps us make Wikipedia better.',
 	'moodbar-error-subtitle' => 'Something went wrong! Please try sharing your mood again later.',
 	// Special:MoodBar
