9ead91dd1e2aed0908d604065b7ff612fd95a0af
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-22 19:41:59 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-07-22 19:19:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@92881 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- EditPageTracking.i18n.php
+++ EditPageTracking.i18n.php
@@ -15,3 +15,46 @@ $messages = array();
 $messages['en'] = array(
 	'editpagetracking-desc' => 'Records the first time each user views the edit page',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'editpagetracking-desc' => 'Ermöglicht die Aufzeichnung des erstens Besuchs einer Bearbeitungsseite durch einen Benutzer',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'editpagetracking-desc' => 'שמירת הפעם הראשונה שעל משתמש רואה את דך העריכה',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'editpagetracking-desc' => 'Registra le prime vice que cata usator visualisa le pagina de modification',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'editpagetracking-desc' => 'Ја евидентира првата посета (погледнување) на страницата за уредување на секој корисник',
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Event
+ */
+$messages['no'] = array(
+	'editpagetracking-desc' => 'Det blir registrert første gang hver bruker ser på redigeringssiden',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'editpagetracking-desc' => 'Regista a primeira vez que cada utilizador visita a página de edição',
+);
+
