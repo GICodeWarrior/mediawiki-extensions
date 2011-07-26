601c2964f53cdcf1248eb25808f9496f3e2c41cd
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-26 15:01:01 +0000

    Tweak extension credits and message files for consisteny Add extension to Translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@93188 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ TreeAndMenu.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for the TreeAndMenu extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'treeandmenu-desc' => "Adds <code><nowiki>#tree</nowiki></code> and <code><nowiki>#menu</nowiki></code> parser functions which contain bullet-lists to be rendered as collapsible treeview's or dropdown menus.
+The treeview's use the [http://www.destroydrop.com/javascripts/tree dTree] JavaScript tree menu, and the dropdown menu's use [http://www.htmldog.com/articles/suckerfish/dropdowns/ Son of Suckerfish]",
+);
+
