16fe8ee1065abc7f6cf263e1a18ee08e37f6cab2
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-27 21:18:29 +0000

    More refactoring and adding some functionality to to es.Range
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@93315 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/lib/es/es.Range.js
+++ parsers/wikidom/lib/es/es.Range.js
@@ -4,8 +4,31 @@
  * @param start {Integer} Starting point
  * @param end {Integer} Ending point
  * @returns {es.Range}
+ * @property to {Integer}
+ * @property from {Integer}
+ * @property start {Integer}
+ * @property end {Integer}
  */
-es.Range = function( start, end ) {
-	this.start = start || null;
-	this.end = end || null;
+es.Range = function( from, to ) {
+	this.set( from, to );
+};
+
+es.Range.prototype.set = function( from, to ) {
+	this.from = from || 0;
+	this.to = to || from;
+	this.normalize();
 }
+
+es.Range.prototype.getLength = function() {
+	return Math.abs( this.from - this.to );
+};
+
+es.Range.prototype.normalize = function() {
+	if ( this.from < this.to ) {
+		this.start = this.from;
+		this.end = this.to;
+	} else {
+		this.start = this.to;
+		this.end = this.from;
+	}
+};
