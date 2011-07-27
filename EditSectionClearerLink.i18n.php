8ce1b506c3afd143f508ac32f4ab63b3f10a067e
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-27 19:26:23 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-07-27 19:08:00 UTC)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@93301 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- EditSectionClearerLink.i18n.php
+++ EditSectionClearerLink.i18n.php
@@ -15,10 +15,38 @@ $messages['en'] = array(
 	'editsectionclearerlink-desc' => 'Improve the usability of section edit links with section and link hover actions',
 );
 
+/** Message documentation (Message documentation)
+ * @author Purodha
+ */
+$messages['qqq'] = array(
+	'editsectionclearerlink-desc' => '{{desc}}',
+);
+
 /** German (Deutsch)
  * @author Kghbln
  */
 $messages['de'] = array(
 	'editsectionclearerlink-desc' => 'Verbessert die Nutzbarkeit der Links für die Bearbeitung von Seitenabschnitten durch zusätzliche Aktionen, die beim Überfahren mit der Maus eingeblendet werden',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'editsectionclearerlink-desc' => 'Määt dä Lenk för der Enhald vun Afschnedde ze ändere besser ze bruche, endämm dat mieh Müjjeleschkeite aanjezeisch wääde, wam_mer met der Muus drovver jöck.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'editsectionclearerlink-desc' => 'Подобрување на употребливоста на врските за уредување на поднаслови со лебдечки дејства',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'editsectionclearerlink-desc' => 'Melhorar a usabilidade dos links de edição de secções, disponibilizando operações ao passar o ponteiro do rato sobre secções e links',
 );
 
