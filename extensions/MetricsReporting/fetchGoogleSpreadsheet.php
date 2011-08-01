57f6ca6aab3fea34390afc6b4e5b44cc975b6956
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-01 14:10:47 +0000

    Do Google Account auth
    Numerous debugging things left purposely in for the moment
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@93619 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MetricsReporting/fetchGoogleSpreadsheet.php
+++ extensions/MetricsReporting/fetchGoogleSpreadsheet.php
@@ -15,8 +15,53 @@ class FetchGoogleSpreadsheet extends Maintenance {
 
 	public function execute() {
 		$url = $this->getArg( 0 );
-		$res = Http::request( 'GET', $url );
+
+		// Headers
+		// GData-Version: 3.0
+		$http = MWHttpRequest::factory( 'https://www.google.com/accounts/ClientLogin', array(
+													'method' => 'POST',
+													'postData' => array(
+														'accountType' => 'HOSTED_OR_GOOGLE',
+														'service' => 'wise', // Spreadsheet service is "wise"
+														'Email' => '',
+														'Passwd' => '',
+														'source' => Http::userAgent(),
+													)
+											)
+		);
+
+		$res = $http->execute();
+		if ( $http->getStatus() == 403 ) {
+			$this->error( '403', true );
+		}
+		//var_dump( $res );
+		//var_dump( $http->getResponseHeaders() );
+		$content = $http->getContent();
+		//var_dump( $content );
+
+		$authToken = null;
+		$pos = strpos( $content, 'Auth' );
+		if ( $pos !== false ) {
+			$authToken = rtrim( substr( $content, $pos + 6 ) );
+		}
+
+		if ( $authToken === null ) {
+			$this->error( 'No auth token', true );
+		}
+
+		//var_dump( $authToken );
+
+		$http = MWHttpRequest::factory( $url, array(
+												'method' => 'POST'
+											)
+										);
+		$http->setHeader( 'GData-Version', '3.0' );
+		$http->setHeader( 'GAuthorization', 'GoogleLogin auth=' . $authToken );
+
 		var_dump( $res );
+		var_dump( $http->getResponseHeaders() );
+		$content = $http->getContent();
+		var_dump( $content );
 	}
 }
 
