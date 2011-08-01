5339c6b985632cb48d35373aca35e40069e64ed1
yuvipanda <yuvipanda@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-01 14:54:15 +0000

    Formatting fixes
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@93624 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/GPoC/models/Selection.php
+++ extensions/GPoC/models/Selection.php
@@ -4,21 +4,21 @@
  * Represents an convenience methods for logging
  **/
 class Selection {
-    public static function addEntries( $name, $articles ) {
-        $dbw = wfGetDB( DB_MASTER );
-        $timestamp = wfTimestamp( TS_MW );
-        foreach( $articles as $article ) {
-            $success = $dbw->insert(
-                'selections',
-                array(
-                    's_selection_name' => $name,
-                    's_namespace' => $article['r_namespace'],
-                    's_article' => $article['r_article'],
-                    's_timestamp' => $timestamp
-                ),
-                __METHOD__,
-                array( 'IGNORE' )
-            );
-        }
-    }
+	public static function addEntries( $name, $articles ) {
+		$dbw = wfGetDB( DB_MASTER );
+		$timestamp = wfTimestamp( TS_MW );
+		foreach( $articles as $article ) {
+			$success = $dbw->insert(
+				'selections',
+				array(
+					's_selection_name' => $name,
+					's_namespace' => $article['r_namespace'],
+					's_article' => $article['r_article'],
+					's_timestamp' => $timestamp
+				),
+				__METHOD__,
+				array( 'IGNORE' )
+			);
+		}
+	}
 }
