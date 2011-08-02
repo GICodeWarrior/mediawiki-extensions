73887679a9dee10cd7b7e75a957b5a5c0128c22a
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-02 01:31:14 +0000

    Fix whitespace and documentation
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@93701 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MobileFrontend/library/WURFL/Storage/Mwmemcache.php
+++ extensions/MobileFrontend/library/WURFL/Storage/Mwmemcache.php
@@ -10,13 +10,21 @@ class WURFL_Storage_Mwmemcache extends WURFL_Storage_Base {
 		"expiration" => 0
 	);
 
-	public function __construct($params=array()) {
-		$currentParams = is_array($params) ? array_merge($this->defaultParams, $params) : $this->defaultParams;
-		$this->toFields($currentParams);
+	/**
+	 * @param $params array
+	 */
+	public function __construct( $params = array() ) {
+		$currentParams = is_array( $params )
+				? array_merge( $this->defaultParams, $params )
+				: $this->defaultParams;
+		$this->toFields( $currentParams );
 	}
 
-	private function toFields($params) {
-		foreach($params as $key => $value) {
+	/**
+	 * @param $params array
+	 */
+	private function toFields( $params ) {
+		foreach( $params as $key => $value ) {
 			$this->$key = $value;
 		}
 	}
@@ -24,29 +32,26 @@ class WURFL_Storage_Mwmemcache extends WURFL_Storage_Base {
 	/**
 	 * Saves the object.
 	 *
-	 * @param string $objectId
-	 * @param mixed $object
-	 * @return
+	 * @param $objectId string
+	 * @param $object mixed
+	 * @return mixed
 	 */
-	public function save($objectId, $object) {
+	public function save( $objectId, $object ) {
 		global $wgMemc;
 		return $wgMemc->set( $this->encode( $this->namespace, $objectId ), $object, $this->expiration );
 	}
 
-
 	/**
 	 * Load the object.
 	 *
-	 * @param string $objectId
-	 * @param mixed $object
-	 * @return
+	 * @param $objectId string
+	 * @return mixed
 	 */
-	public function load($objectId) {
+	public function load( $objectId ) {
 		global $wgMemc;
 		$value = $wgMemc->get( $this->encode( $this->namespace, $objectId ) );
 		return $value ? $value : null; // WURFL expects null on cache miss
 	}
-
 
 	public function clear() {
 	}
