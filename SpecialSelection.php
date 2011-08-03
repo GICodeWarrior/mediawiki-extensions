3086f58e4b3f901df1f513073665a0e794a735f1
yuvipanda <yuvipanda@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-03 17:10:37 +0000

    Export Selections as CSV
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@93837 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SpecialSelection.php
+++ SpecialSelection.php
@@ -13,20 +13,45 @@ class SpecialSelection extends SpecialPage {
 		parent::__construct( 'Selection' );
 	}
 
+	private function makeCSV( $articles, $name ) {
+		$outstream = fopen( "php://output", "w" );
+		$headers = array(
+			'article',
+			'added'
+		);
+		fputcsv( $outstream, $headers );
+		foreach( $articles as $article ) {
+			$row = array(
+				$article['title']->getFullText(),
+				wfTimeStamp( TS_ISO_8601, $article['s_timestamp'] )
+			);
+			fputcsv( $outstream, $row );
+		}
+		fclose( $outstream );
+	}
 	public function execute( $par ) {
         global $wgOut, $wgRequest;
 
 		$name = $par;
+		$action = $wgRequest->getVal('action'); 		
 
 		$entries = Selection::getSelection( $name );
 		$this->setHeaders();
 
 		$wgOut->setPageTitle("Selection");
 
+		if( $action == 'csv' ) {
+			$wgRequest->response()->header( 'Content-type: text/csv' );
+			// Is there a security issue in letting the name be arbitrary?
+			$wgRequest->response()->header(
+				"Content-Disposition: attachment; filename=$name.csv"
+			);
+			$wgOut->disable();
+			$this->makeCSV( $entries, $name );
+		}
 		$template = new SelectionTemplate();
 		$template->set( 'articles', $entries );
 
 		$wgOut->addTemplate( $template );
-
 	}
 }
