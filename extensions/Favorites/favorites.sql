fa4873e4155001af64153bdf7860a15b4b1d44af
jlemley <jlemley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-05 02:31:11 +0000

    Resolved table structure issue.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@93950 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Favorites/favorites.sql
+++ extensions/Favorites/favorites.sql
@@ -6,9 +6,8 @@
 CREATE TABLE IF NOT EXISTS /*_*/favoritelist (
   fl_user int(10) unsigned NOT NULL,
   fl_namespace int(11) NOT NULL DEFAULT '0',
-  fl_title varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+  fl_title varbinary(255) NOT NULL DEFAULT '',
   fl_notificationtimestamp varbinary(14) DEFAULT NULL,
   UNIQUE KEY fl_user (fl_user,fl_namespace,fl_title),
-  KEY namespace_title (fl_namespace,fl_title)
-) ;
-
+  KEY fl_namespace_title (fl_namespace,fl_title)
+) /*$wgDBTableOptions*/;
