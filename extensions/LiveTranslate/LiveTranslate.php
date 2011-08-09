a5d1239f20467d66bdd42029c93ea4e1ab5cc676
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-09 12:44:59 +0000

    1.2 RC1
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94104 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LiveTranslate/LiveTranslate.php
+++ extensions/LiveTranslate/LiveTranslate.php
@@ -24,7 +24,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define( 'LiveTranslate_VERSION', '1.2 alpha' );
+define( 'LiveTranslate_VERSION', '1.2 RC1' );
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
