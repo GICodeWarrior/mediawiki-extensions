e84a3f035a86b1fc4e82ba4d756535ababdccae6
kbrown <kbrown@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-09 16:13:27 +0000

    *add function for update.php schema updates
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94118 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ArchiveLinks/setuptables.sql
+++ extensions/ArchiveLinks/setuptables.sql
@@ -1,22 +1,22 @@
-CREATE TABLE IF NOT EXISTS `el_archive_blacklist` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_blacklist` (
   `bl_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `bl_type` tinyint(4) NOT NULL,
   `bl_url` varchar(10000) NOT NULL,
   `bl_expiry` int(11) unsigned NOT NULL,
   `bl_reason` varchar(255) NOT NULL,
   PRIMARY KEY (`bl_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) /*$wgDBTableOptions*/;
 
-CREATE TABLE IF NOT EXISTS `el_archive_log` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_log` (
   `log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `log_result` tinyint(4) NOT NULL,
   `log_url` varchar(10000) NOT NULL,
   `log_time` int(11) unsigned NOT NULL,
   `log_http_code` varchar(255) NOT NULL,
   PRIMARY KEY (`log_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) /*$wgDBTableOptions*/;
 
-CREATE TABLE IF NOT EXISTS `el_archive_queue` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_queue` (
   `queue_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `page_id` int(11) unsigned NOT NULL,
   `url` varchar(10000) NOT NULL,
@@ -24,12 +24,20 @@ CREATE TABLE IF NOT EXISTS `el_archive_queue` (
   `insertion_time` int(11) unsigned NOT NULL,
   `in_progress` varchar(50) NOT NULL,
   PRIMARY KEY (`queue_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=105 ;
+) /*$wgDBTableOptions*/;
 
-CREATE TABLE IF NOT EXISTS `el_archive_link_history` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_link_history` (
   `hist_id` int(11) unsigned NOT NULL,
   `hist_page_id` int(11) unsigned NOT NULL,
   `hist_url` varchar(10000) NOT NULL,
   `hist_insertion_time` int(11) unsigned NOT NULL,
   PRIMARY KEY (`hist_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) /*$wgDBTableOptions*/;
+
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_resource` (
+  `resource_id` int(11) NOT NULL,
+  `el_id` int(11) NOT NULL,
+  `resource_url` varchar(10000) NOT NULL,
+  `resource_location` varchar(10000) NOT NULL,
+  PRIMARY KEY (`resource_id`)
+) /*$wgDBTableOptions*/;
