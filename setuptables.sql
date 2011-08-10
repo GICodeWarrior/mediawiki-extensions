2848260e44543b738f5edc4515521780537be7ff
kbrown <kbrown@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-10 16:20:48 +0000

    fix partial excaping problem on setuptables.sql, follow up to r94118
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94164 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- setuptables.sql
+++ setuptables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_blacklist` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/el_archive_blacklist` (
   `bl_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `bl_type` tinyint(4) NOT NULL,
   `bl_url` varchar(10000) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_blacklist` (
   PRIMARY KEY (`bl_id`)
 ) /*$wgDBTableOptions*/;
 
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_log` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/el_archive_log` (
   `log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `log_result` tinyint(4) NOT NULL,
   `log_url` varchar(10000) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_log` (
   PRIMARY KEY (`log_id`)
 ) /*$wgDBTableOptions*/;
 
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_queue` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/el_archive_queue` (
   `queue_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `page_id` int(11) unsigned NOT NULL,
   `url` varchar(10000) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_queue` (
   PRIMARY KEY (`queue_id`)
 ) /*$wgDBTableOptions*/;
 
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_link_history` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/el_archive_link_history` (
   `hist_id` int(11) unsigned NOT NULL,
   `hist_page_id` int(11) unsigned NOT NULL,
   `hist_url` varchar(10000) NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_link_history` (
   PRIMARY KEY (`hist_id`)
 ) /*$wgDBTableOptions*/;
 
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/`el_archive_resource` (
+CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/el_archive_resource` (
   `resource_id` int(11) NOT NULL,
   `el_id` int(11) NOT NULL,
   `resource_url` varchar(10000) NOT NULL,
