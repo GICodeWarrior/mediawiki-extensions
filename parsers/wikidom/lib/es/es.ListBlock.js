4016ba51e3efb8f52c0af7ebc7d943a3ec61fb26
inez <inez@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-12 22:54:44 +0000

    Support for displaying cursor in lists
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94392 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/lib/es/es.ListBlock.js
+++ parsers/wikidom/lib/es/es.ListBlock.js
@@ -1,3 +1,13 @@
+/**
+ * Creates a list block.
+ * 
+ * @class
+ * @constructor
+ * @extends {es.Block}
+ * @param list {es.ListBlockList} Flat list to initialize with
+ * @property list {es.ListBlockList}
+ * @property $ {jQuery}
+ */
 es.ListBlock = function( list ) {
 	es.Block.call( this );
 
@@ -13,6 +23,16 @@ es.ListBlock = function( list ) {
 	} );
 };
 
+/* Static Methods */
+
+/**
+ * Creates a new list block object from WikiDom data.
+ * 
+ * @static
+ * @method
+ * @param wikidomListBlock {Object} WikiDom data to convert from
+ * @returns {es.ListBlock} EditSurface list block
+ */
 es.ListBlock.newFromWikiDomListBlock = function( wikidomListBlock ) {
 	if ( wikidomListBlock.type !== 'list' ) {
 		throw 'Invalid block type error. List block expected to be of type "list".';
@@ -24,8 +44,82 @@ es.ListBlock.prototype.renderContent = function( offset ) {
 	this.list.renderContent( offset );
 };
 
+/**
+ * Gets the offset of a position.
+ * 
+ * @method
+ * @param position {es.Position} Position to translate
+ * @returns {Integer} Offset nearest to position
+ */
+es.ListBlock.prototype.getOffset = function( position ) {
+	if ( position.top < 0 ) {
+		return 0;
+	} else if ( position.top >= this.$.height() ) {
+		return this.getLength();
+	}
+
+	var globalOffset = 0,
+		i,
+		itemOffset,
+		itemHeight,
+		blockOffset = this.$.offset();
+
+	position.top += blockOffset.top;
+	position.left += blockOffset.left;
+
+	for( i = 0; i < this.list.items.length; i++ ) {
+		itemOffset = this.list.items[i].$content.offset();
+		itemHeight = this.list.items[i].$content.height();
+
+		if ( position.top >= itemOffset.top && position.top < itemOffset.top + itemHeight ) {
+			position.top -= itemOffset.top;
+			position.left -= itemOffset.left;
+			return globalOffset + this.list.items[i].flow.getOffset( position );
+		}
+		
+		globalOffset += this.list.items[i].content.getLength() + 1;
+	}
+};
+
+/**
+ * Gets the position of an offset.
+ * 
+ * @method
+ * @param offset {Integer} Offset to translate
+ * @returns {es.Position} Position of offset
+ */
+es.ListBlock.prototype.getPosition = function( offset ) {
+	var globalOffset = 0,
+		i,
+		itemLength,
+		position,
+		contentOffset,
+		blockOffset = this.$.offset();
+	
+	for( i = 0; i < this.list.items.length; i++ ) {
+		itemLength = this.list.items[i].content.getLength();
+		if ( offset >= globalOffset && offset < globalOffset + itemLength ) {
+			position = this.list.items[i].flow.getPosition( offset - globalOffset );
+			contentOffset = this.list.items[i].$content.offset();
+			
+			position.top += contentOffset.top - blockOffset.top;
+			position.left += contentOffset.left - blockOffset.left;
+			position.bottom += contentOffset.top - blockOffset.top;
+			
+			position.line = i;
+			
+			return position;
+		}
+		globalOffset += itemLength + 1;
+	}
+};
+
+/* Registration */
+
+/**
+ * Extend es.Block to support list block creation with es.Block.newFromWikiDom
+ */
 es.Block.blockConstructors.list = es.ListBlock.newFromWikiDomListBlock;
 
-/* Inherit */
-
+/* Inheritance */
 es.extend( es.ListBlock, es.Block );
