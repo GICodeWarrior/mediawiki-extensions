3eb271f5d921a69bf5d87c6ab19932ffc698d267
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-12 18:22:39 +0000

    Fixed styles and levels for list items
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94366 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/lib/es/es.ListBlockItem.js
+++ parsers/wikidom/lib/es/es.ListBlockItem.js
@@ -2,15 +2,28 @@ es.ListBlockItem = function( content, style, level ) {
 	es.EventEmitter.call( this );
 
 	this.content = content || new es.Content();
-	this.$ = $( '<div class="editSurface-list-content"></div>' )
-			.addClass( 'editSurface-item-level' + level );
+	this.$ = $( '<div class="editSurface-listItem editSurface-listItem-content"></div>' )
+			.addClass( 'editSurface-listItem-level' + level )
+			.addClass( 'editSurface-listItem-' + style );
 	this.flow = new es.Flow( this.$, this.content );
-
+	// Listen to render events and trigger update event upstream
 	var listBlockItem = this;
 	this.flow.on( 'render', function() {
 		listBlockItem.emit( 'update' );
 	} );
 }
+
+es.ListBlockItem.prototype.setLevel = function( level ) {
+	this.$.removeClass( 'editSurface-listItem-level' + this.level );
+	this.level = level;
+	this.$.addClass( 'editSurface-listItem-level' + this.level );
+};
+
+es.ListBlockItem.prototype.setStyle = function( style ) {
+	this.$.removeClass( 'editSurface-listItem-' + this.style );
+	this.style = style;
+	this.$.addClass( 'editSurface-listItem-' + this.style );
+};
 
 es.ListBlockItem.prototype.renderContent = function( offset ) {
 	this.flow.render();
