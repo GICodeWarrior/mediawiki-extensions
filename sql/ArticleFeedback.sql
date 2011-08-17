66f73a12e2788e705c07f6f2b82130a70fe9ec29
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-17 19:40:44 +0000

    Bug 30426 - Complicated database schema update procedure will make enabling AF on a new wiki (that can't use update.php) a PITA
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94802 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- sql/ArticleFeedback.sql
+++ sql/ArticleFeedback.sql
@@ -32,10 +32,13 @@ CREATE TABLE IF NOT EXISTS /*_*/article_feedback (
   aa_rating_value int unsigned NOT NULL,
   -- Which rating widget the user was given. Default of 0 is the "old" design
   aa_design_bucket int unsigned NOT NULL DEFAULT 0,
+  aa_design_bucket int unsigned NOT NULL DEFAULT 0,
   -- 1 vote per user per revision
   PRIMARY KEY (aa_revision, aa_user_text, aa_rating_id, aa_user_anon_token)
 ) /*$wgDBTableOptions*/;
 CREATE INDEX /*i*/aa_user_page_revision ON /*_*/article_feedback (aa_user_id, aa_page_id, aa_revision);
+-- Create an index on the article_feedback.aa_timestamp field
+CREATE INDEX /*i*/article_feedback_timestamp ON /*_*/article_feedback (aa_timestamp);
 
 -- Aggregate rating table for a page
 CREATE TABLE IF NOT EXISTS /*_*/article_feedback_pages (
@@ -83,3 +86,26 @@ CREATE TABLE  IF NOT EXISTS /*_*/article_feedback_properties (
   afp_value_text varbinary(255) DEFAULT '' NOT NULL
 ) /*$wgDBTableOptions*/;
 CREATE UNIQUE INDEX /*i*/afp_rating_key ON /*_*/article_feedback_properties (afp_revision, afp_user_text, afp_user_anon_token, afp_key);
+
+CREATE TABLE IF NOT EXISTS /*_*/article_feedback_stats (
+	afs_page_id integer unsigned NOT NULL,
+	-- data point to be used for ordering this data
+	afs_orderable_data double unsigned NOT NULL,
+	-- json object of stat data
+	afs_data varbinary(255) NOT NULL,
+	afs_stats_type_id integer unsigned NOT NULL,
+	-- timestamp of insertion job
+	afs_ts binary(14) NOT NULL
+) /*$wgDBTableOptions*/;
+CREATE UNIQUE INDEX /*i*/afs_type_ts_page ON /*_*/article_feedback_stats(afs_stats_type_id, afs_ts, afs_page_id);
+CREATE INDEX /*i*/ afs_type_ts_orderable ON /*_*/article_feedback_stats (afs_stats_type_id, afs_ts, afs_orderable_data);
+
+CREATE TABLE IF NOT EXISTS /*_*/article_feedback_stats_types (
+	afst_id integer unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	afst_type varbinary(255) NOT NULL
+) /*$wgDBTableOptions*/;
+CREATE UNIQUE INDEX /*i*/afst_type ON /*_*/article_feedback_stats_types( afst_type );
+
+-- Pre-populate table with stat types
+INSERT INTO article_feedback_stats_types ( afst_type ) VALUES ( 'highs_and_lows' );
+INSERT INTO article_feedback_stats_types ( afst_type ) VALUES ( 'problems' );
