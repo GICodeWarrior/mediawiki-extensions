16d5ee75ec8eb8a41c2671c45ab5a0c684861419
nad <nad@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-17 21:45:51 +0000

    call each record forms onsubmit before merging into the edit form
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94824 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- recordadmin.js
+++ recordadmin.js
@@ -2,6 +2,7 @@ window.raRecordForms = function () {
 	for( i = 0; i < window.forms.length; i++ ) {
 		var type = forms[i];
 		var form = document.getElementById( type.toLowerCase() + '-form' );
+		form.onsubmit();
 		var tags = [ 'input', 'select', 'textarea' ];
 		for( j = 0; j < tags.length; j++ ) {
 			var inputs = form.getElementsByTagName( tags[j] );
