9e742743a1a76103375b469e4a51a4c8b4966c80
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-17 20:43:00 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94811 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- NamespacePaths.i18n.php
+++ NamespacePaths.i18n.php
@@ -15,3 +15,10 @@ $messages['en'] = array(
 	'namespacepaths-desc' => 'Allows custom article paths to be mapped to namespaces of the wiki. Eg: Mapping pages like Help:A to /help/Page instead of /wiki/Help:Page.',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'namespacepaths-desc' => 'Ermöglicht für Namensräume die Darstellung benutzerdefinierter Pfade zu Artikeln',
+);
+
