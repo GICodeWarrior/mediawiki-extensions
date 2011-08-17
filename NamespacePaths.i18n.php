3ac46f0ae186cdd319cdb2930fbf8aadf782feb1
dantman <dantman@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-17 01:32:49 +0000

    Commit NamespacePaths extension.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94721 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ NamespacePaths.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for NamespacePaths extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Friesen
+ */
+$messages['en'] = array(
+	'namespacepaths-desc' => 'Allows custom article paths to be mapped to namespaces of the wiki. Eg: Mapping pages like Help:A to /help/Page instead of /wiki/Help:Page.',
+);
+
