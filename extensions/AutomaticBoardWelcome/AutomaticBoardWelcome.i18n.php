c8dd9c1e7950ebb78dd148e0493d38e039a63463
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-18 08:36:13 +0000

    Add new extension (r93523) to translatewiki.net Add i18n file, tweak extension credits
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94878 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/AutomaticBoardWelcome/AutomaticBoardWelcome.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation file for extension Automatic Board Welcome.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Jack Phoenix <jack@countervandalism.net>
+ */
+$messages['en'] = array(
+	'automaticboardwelcome-desc' => "Automatically posts [[MediaWiki:User-board-welcome-message|a welcome message]] on new users' user boards after account creation",
+
+);
