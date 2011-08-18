ced9ab9f51979934833201925b7f6b093932f0ee
khorn <khorn@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-18 17:19:15 +0000

    Suggested changes to the contribution_tracking table definition (r94791).
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94911 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ContributionTracking.sql
+++ ContributionTracking.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS /*_*/contribution_tracking (
-  id int(10) unsigned NOT NULL auto_increment,
+  id int(10) unsigned NOT NULL PRIMARY KEY auto_increment,
   contribution_id int(10) unsigned default NULL,
   note text,
   referrer varchar(4096) default NULL,
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS /*_*/contribution_tracking (
   language varchar(8) default NULL,
   ts char(14) default NULL,
   owa_session varbinary(255) default NULL,
-  owa_ref int(11) default NULL,
-  PRIMARY KEY  (id),
-  UNIQUE KEY contribution_id (contribution_id),
-  KEY ts (ts)
+  owa_ref int(11) default NULL
 ) /*wgDBTableOptions*/; 
+
+CREATE UNIQUE INDEX /*i*/contribution_id ON /*_*/contribution_tracking (contribution_id);
+CREATE INDEX /*i*/ts ON /*_*/contribution_tracking (ts);
