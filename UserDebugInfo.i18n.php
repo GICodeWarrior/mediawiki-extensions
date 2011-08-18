9abc45fbc87cff25219db8cb6d0b46d2abe338d6
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-18 20:35:31 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94938 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UserDebugInfo.i18n.php
+++ UserDebugInfo.i18n.php
@@ -21,3 +21,45 @@ $messages['qqq'] = array(
 	'userdebuginfo-desc' => '{{desc}}',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'userdebuginfo' => 'Benutzerinformationen für die Fehlerermittlung',
+	'userdebuginfo-desc' => 'Ermöglicht die Anzeige von Informationen zum aktuellen Benutzer, die möglicherweise bei der Fehlerermittlung hilfreich sind',
+	'userdebuginfo-useragent' => 'HTTP: Programm des Benutzers',
+	'userdebuginfo-remotehost' => 'HTTP: Host des Benutzers',
+	'userdebuginfo-remoteaddr' => 'HTTP: IP-Adresse des Benutzers',
+	'userdebuginfo-key' => 'Schlüssel',
+	'userdebuginfo-value' => 'Wert',
+	'userdebuginfo-language' => 'Sprache',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'userdebuginfo' => 'Кориснички информации за отстранување грешки',
+	'userdebuginfo-desc' => 'Прикажува извесни потенцијално корисни информации за отстранување грешки на тековниот корисник',
+	'userdebuginfo-useragent' => 'HTTP - кориснички агент',
+	'userdebuginfo-remotehost' => 'HTTP - далечински домаќин',
+	'userdebuginfo-remoteaddr' => 'HTTP - далечинска адреса',
+	'userdebuginfo-key' => 'Клуч',
+	'userdebuginfo-value' => 'Вредност',
+	'userdebuginfo-language' => 'Јазик',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'userdebuginfo' => 'Debuggen van gebruikersgegevens',
+	'userdebuginfo-desc' => 'Geeft mogelijk interessante debuggegevens over de huidige gebruiker weer',
+	'userdebuginfo-useragent' => 'HTTP-gebruikersagent',
+	'userdebuginfo-remotehost' => 'HTTP-externe host',
+	'userdebuginfo-remoteaddr' => 'HTTP-extern adres',
+	'userdebuginfo-key' => 'Sleutel',
+	'userdebuginfo-value' => 'Waarde',
+	'userdebuginfo-language' => 'Taal',
+);
+
