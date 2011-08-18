a78c43564f45544cc0d89ab99219afd0d49d2547
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-18 20:35:31 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94938 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- AutomaticBoardWelcome.i18n.php
+++ AutomaticBoardWelcome.i18n.php
@@ -15,3 +15,25 @@ $messages['en'] = array(
 	'automaticboardwelcome-desc' => "Automatically posts [[MediaWiki:User-board-welcome-message|a welcome message]] on new users' user boards after account creation",
 
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'automaticboardwelcome-desc' => 'Ermöglicht, nach der Neuanlage eines Kontos, die Erstellung einer automatischen [[MediaWiki:User-board-welcome-message|Willkommensnachricht]] auf der entsprechenden Benutzerseite',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'automaticboardwelcome-desc' => 'Автоматски објевува [[MediaWiki:User-board-welcome-message|порака за добредојде]] на таблите на новите корисници откако ќе направат сметка',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'automaticboardwelcome-desc' => 'Plaatst een [[MediaWiki:User-board-welcome-message|welkomstbericht]] op het gebruikersbord van nieuwe gebruikers',
+);
+
