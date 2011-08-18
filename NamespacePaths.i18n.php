b741462d3244a435b3d27546d3531263aa967436
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-18 20:35:31 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94938 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- NamespacePaths.i18n.php
+++ NamespacePaths.i18n.php
@@ -22,3 +22,24 @@ $messages['de'] = array(
 	'namespacepaths-desc' => 'Ermöglicht für Namensräume die Darstellung benutzerdefinierter Pfade zu Artikeln',
 );
 
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'namespacepaths-desc' => 'אפשרות למפות נתיבי ערכים מותאמים אישית למרחבי שם של ויקי. למשל, למפות דפים כמו עזרה:דף לנתיב <span dir="ltr">/help/דף</span> במקום <span dir="ltr">/wiki/עזרה:דף</span>.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'namespacepaths-desc' => 'Овозможува патеки на статии по ваш избор да се пресликаат во именски простори во викито. На пр.: Прелискување на страници како Help:A to /help/Page наместо /wiki/Help:Page.',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'namespacepaths-desc' => 'Maakt het mogelijk aangepaste artikelpaden toe te wijzen aan naamruimten in de wiki. Bijvoorbeeld: pagina\'s als "Help:A" kunnen het pad "/help/Pagina" krijgen in plaats van "/wiki/Help:Pagina".',
+);
+
