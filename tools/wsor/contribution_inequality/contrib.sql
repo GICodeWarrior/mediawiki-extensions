290c4c2055e87075b0f8d6b27e4a99cdf986dc83
giovanni <giovanni@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-18 20:58:36 +0000

    fixed sort order
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94944 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wsor/contribution_inequality/contrib.sql
+++ tools/wsor/contribution_inequality/contrib.sql
@@ -1,9 +1,9 @@
 -- user ranking by contribution per namespace per year for main, talk,
 -- wikipedia, wikipedia talk
-describe
+
 select namespace, year, user_id, sum(edits) as total_contributions 
 from halfak.giovanni 
-where namespace in (0,1,4,5)
+where namespace in (0,1,4,5) and user_id > 0
 group by user_id, namespace, year 
--- having total_contributions > 10
-order by namespace asc, year asc, total_contributions desc;
+-- having total_contributions >= 1000
+order by namespace, year, total_contributions;
