7a52cb347ce788b335ad8ecc4cfca6570badb945
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-19 00:40:54 +0000

    Added es.Location.compareWith
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94975 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/lib/es/es.Location.js
+++ parsers/wikidom/lib/es/es.Location.js
@@ -11,4 +11,27 @@
 es.Location = function( block, offset ) {
 	this.block = block;
 	this.offset = offset || 0;
-}
+};
+
+/**
+ * Compares location with another.
+ * 
+ * @method
+ * @param location {es.Location} Location to compare with
+ * @returns {Integer} -1 if this is before the argument, 0 if at the same place or 1 if after
+ */
+es.Location.prototype.compareWith = function( location ) {
+	var a = this,
+		b = location;
+	if ( a === b ) {
+		return 0;
+	}
+	var aIndex = this.block.getIndex(),
+		bIndex = location.block.getIndex();
+	if ( aIndex < bIndex || ( aIndex === bIndex && a.offset < b.offset ) ) {
+		return -1;
+	} else if ( aIndex > bIndex || ( aIndex === bIndex && a.offset > b.offset )  ) {
+		return 1;
+	}
+	return 0;
+};
