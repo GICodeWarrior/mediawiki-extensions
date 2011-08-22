3ee176fba0ec498c10a3cbadf3582542f7c17756
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-22 19:04:31 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95225 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ShoutBox.i18n.php
+++ ShoutBox.i18n.php
@@ -22,3 +22,30 @@ $messages['en'] = array(
 	'shoutbox-url' => 'http://$1.shoutbox.us/?LANG=US',
 	'shoutbox-url-with-css' => 'http://$1.shoutbox.us/?LANG=US&extcss=$2',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'shoutbox-desc' => 'Fügt eine Parserfunktion zum Einbetten einer elektronischen Pinnwand hinzu',
+	'shoutbox-missing-params' => 'Es fehlt ein erforderlicher Parameter.',
+	'shoutbox-bad-params' => 'Es wurde ein ungültiger Parameter angegeben.',
+	'shoutbox-unparsable-param-string' => 'Es wurde der nicht verarbeitbarer Parameter „<tt>$1</tt>“ angegeben.',
+	'shoutbox-bad-id' => 'Es wurde die falsche Kennung „<tt>$1</tt>“ angegeben.',
+	'shoutbox-illegal-width' => 'Es wurde der ungültige <i>width</i>-Parameter „<tt>$1</tt>“ angegeben.',
+	'shoutbox-illegal-height' => 'Es wurde der ungültige <i>height</i>-Parameter „<tt>$1</tt>“ angegeben.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'shoutbox-desc' => 'Додава парсерска функција за ваш сопствена табла за пораки',
+	'shoutbox-missing-params' => 'Недостасува задолжителен параметар.',
+	'shoutbox-bad-params' => 'Зададовте лош параметар.',
+	'shoutbox-unparsable-param-string' => 'Зададовте нерасчленлива параметарска низа „<tt>$1</tt>“.',
+	'shoutbox-bad-id' => 'Зададовте лоша назнака (ID) „<tt>$1</tt>“',
+	'shoutbox-illegal-width' => 'Зададовте недопуштен параметар за ширина „$1“.',
+	'shoutbox-illegal-height' => 'Зададовте недопуштен параметар за висина „$1“.',
+);
+
