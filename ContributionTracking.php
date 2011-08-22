ddfd1af94beec7675721514b7c5b9bb6f6da4214
khorn <khorn@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-22 21:12:44 +0000

    r94809: This will now use the updater object for schema changes only if the extension's database matches the updater's database. Otherwise, we do the schema changes outside the updater. (Constructing a new updater from a main updater causes Problems) Also related to r94771, r94720
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95254 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ContributionTracking.php
+++ ContributionTracking.php
@@ -107,10 +107,28 @@ function efContributionTrackingLoadUpdates( $updater = null ){
 			$dir . 'patch-owa.sql',
 		);
 	} else {
-		$updater->addExtensionTable( 'contribution_tracking', $dir . 'ContributionTracking.sql' );
-		$updater->addExtensionTable( 'contribution_tracking_owa_ref', $dir . 'ContributionTracking_OWA_ref.sql' );
-		$updater->addExtensionUpdate( array( 'addField', 'contribution_tracking', 'owa_session',
-			$dir . 'patch-owa.sql', true ) );
+		global $wgContributionTrackingDBname;
+
+		if($updater->getDB()->getDBName() === $wgContributionTrackingDBname) {
+			$updater->addExtensionTable( 'contribution_tracking', $dir . 'ContributionTracking.sql' );
+			$updater->addExtensionTable( 'contribution_tracking_owa_ref', $dir . 'ContributionTracking_OWA_ref.sql' );
+			$updater->addExtensionUpdate( array( 'addField', 'contribution_tracking', 'owa_session',
+				$dir . 'patch-owa.sql', true ) );
+		} else { //We are configured not to use the main mediawiki db.
+			//Unless the updater is modified not to run
+			//'LoadExtensionSchemaUpdates' hooks in its constructor (or do so
+			//conditionally), we're going to have to do these manually.
+			$ctDB = ContributionTrackingProcessor::contributionTrackingConnection();
+			if (!$ctDB->tableExists('contribution_tracking')){
+				$ctDB->sourceFile($dir . 'ContributionTracking.sql');
+			}
+			if (!$ctDB->tableExists('contribution_tracking_owa_ref')){
+				$ctDB->sourceFile($dir . 'ContributionTracking_OWA_ref.sql');
+			}
+			if (!$ctDB->fieldExists('contribution_tracking', 'owa_session')){
+				$ctDB->sourceFile($dir . 'patch-owa.sql');
+			}
+		}
 	}
  	return true;
 
