7c1285cf95d7421660ee15120865242ebcadfeb0
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-23 21:17:12 +0000

    added i18n shizzle
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95334 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Survey.alias.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Aliases for the special pages of the Survey extension.
+ *
+ * @since 0.1
+ *
+ * @file Survey.alias.php
+ * @ingroup Survey
+ *
+ * @licence GNU GPL v3+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'Surveys' => array( 'Surveys' ),
+	'SurveyStats' => array( 'SurveyStats', 'SurveyStatistics' ),
+);
