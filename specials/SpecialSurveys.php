aacc08b859d90e8efff413a04ffa1286deecd7e4
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-23 08:12:30 +0000

    added base for specials and simple object wrappers
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95304 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ specials/SpecialSurveys.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Administration interface for surveys.
+ * 
+ * @since 0.1
+ * 
+ * @file SpecialSurveys.php
+ * @ingroup Survey
+ * 
+ * @licence GNU GPL v3 or later
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class SpecialSurveys extends SpecialPage {
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @since 0.1
+	 */
+	public function __construct() {
+		parent::__construct( 'Surveys', 'surveyadmin' );
+	}
+	
+	/**
+	 * @see SpecialPage::getDescription
+	 * 
+	 * @since 0.1
+	 */
+	public function getDescription() {
+		return wfMsg( 'special-' . strtolower( $this->getName() ) );
+	}
+	
+	/**
+	 * Sets headers - this should be called from the execute() method of all derived classes!
+	 * 
+	 * @since 0.1
+	 */
+	public function setHeaders() {
+		global $wgOut;
+		$wgOut->setArticleRelated( false );
+		$wgOut->setRobotPolicy( 'noindex,nofollow' );
+		$wgOut->setPageTitle( $this->getDescription() );
+	}	
+	
+	/**
+	 * Main method.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @param string $arg
+	 */
+	public function execute( $subPage ) {
+		global $wgOut, $wgUser;
+		
+		$this->setHeaders();
+		$this->outputHeader();
+		
+		// If the user is authorized, display the page, if not, show an error.
+		if ( !$this->userCanExecute( $wgUser ) ) {
+			$this->displayRestrictionError();
+			return;
+		}
+		
+	}
+	
+}
