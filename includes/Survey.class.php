14fd909c8c1981720f2d0ac7d7da299546241051
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-24 00:20:22 +0000

    added add survey api module
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95362 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/Survey.class.php
+++ includes/Survey.class.php
@@ -13,9 +13,37 @@
  */
 class Survey {
 	
+	/**
+	 * The ID of the surveys DB record, or null if not inserted yet.
+	 * 
+	 * @since 0.1
+	 * @var integer|null
+	 */
 	protected $id;
+	
+	/**
+	 * The name of the survey.
+	 * 
+	 * @since 0.1
+	 * @var string
+	 */
 	protected $name;
+	
+	/**
+	 * If the survey is enabled or not.
+	 * 
+	 * @since 0.1
+	 * @var boolean
+	 */
 	protected $enabled;
+	
+	/**
+	 * The questions that go with this survey.
+	 * 
+	 * @since 0.1
+	 * @var array of SurveyQuestion
+	 */
+	protected $questions;
 	
 	public static function newFromDBResult() {
 		
@@ -29,9 +57,114 @@ class Survey {
 	 * @param integer|null $id
 	 * @param string $name
 	 * @param boolean $enabled
+	 * @param array $questions
 	 */
-	public function __construct( $id, $name, $enabled ) {
+	public function __construct( $id, $name, $enabled, array $questions ) {
+		$this->id = $id;
+		$this->name = $name;
+		$this->enabled = $enabled;
+		$this->questions = $questions;
+	}
+	
+	/**
+	 * Writes the survey to the database, either updating it
+	 * when it already exists, or inserting it when it doesn't.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return boolean Success indicator
+	 */
+	public function writeToDB() {
+		if ( is_null( $this->id ) ) {
+			return $this->insertIntoDB();
+		}
+		else {
+			return  $this->updateInDB();
+		}
+	}
+	
+	/**
+	 * Updates the group in the database.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return boolean Success indicator
+	 */
+	protected function updateInDB() {
+		$dbr = wfGetDB( DB_MASTER );
 		
+		return $dbr->update(
+			'surveys',
+			$this->getWriteValues(),
+			array( 'survey_id' => $this->id )
+		);
+	}
+	
+	/**
+	 * Inserts the group into the database.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return boolean Success indicator
+	 */
+	protected function insertIntoDB() {
+		$dbr = wfGetDB( DB_MASTER );
+		
+		$result = $dbr->insert(
+			'surveys',
+			$this->getWriteValues()
+		);
+		
+		$this->id = $dbr->insertId();
+		
+		return $result;
+	}
+	
+	/**
+	 * Gets the fields => values to write to the surveys table. 
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return array
+	 */
+	protected function getWriteValues() {
+		return array(
+			'survey_enabled' => $this->enabled,
+			'survey_name' => $this->name, 
+		);
+	}
+	
+	/**
+	 * Returns the survey database id.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return integer|null
+	 */
+	public function getId() {
+		return $this->id;
+	}
+	
+	/**
+	 * Returns the survey name.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+	
+	/**
+	 * Returns the surveys questions.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return array of SurveyQuestion
+	 */
+	public function getQuestions() {
+		return $this->questions;
 	}
 	
 }
