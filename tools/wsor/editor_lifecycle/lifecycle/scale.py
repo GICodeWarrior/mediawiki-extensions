96e9090ed8c620010f1dc1a3146a3f18896ee2c9
giovanni <giovanni@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-24 19:04:17 +0000

    added GNU license information + missing docstring
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95417 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wsor/editor_lifecycle/lifecycle/scale.py
+++ tools/wsor/editor_lifecycle/lifecycle/scale.py
@@ -1,3 +1,24 @@
+''' power-linear axes scale for matplotlib '''
+
+
+'''
+Copyright (C) 2011 GIOVANNI LUCA CIAMPAGLIA, GCIAMPAGLIA@WIKIMEDIA.ORG
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+http://www.gnu.org/copyleft/gpl.html
+'''
+
 from matplotlib.scale import ScaleBase, register_scale
 from matplotlib.transforms import Transform, nonsingular
 from matplotlib.ticker import LinearLocator, Formatter
