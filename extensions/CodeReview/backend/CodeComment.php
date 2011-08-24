b78272ec9642be1cfadadb6fd8899803370f06e1
hashar <hashar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-24 19:53:17 +0000

    ability to create comment from a DB id (and a rev)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95431 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/backend/CodeComment.php
+++ extensions/CodeReview/backend/CodeComment.php
@@ -28,6 +28,36 @@ class CodeComment {
 	}
 
 	/**
+	 * Create a comment given its id AND a repository
+	 * @param int $cc_id Comment ID in the database
+	 * @param CodeRevision $rev A revision object to which the comment is
+	 * attached
+	 */
+	static function newFromID( $cc_id, CodeRevision $rev ) {
+		$dbr = wfGetDB( DB_SLAVE );
+		$row = $dbr->selectRow( 'code_comment',
+			array(
+				# fields needed to build a CodeRevision
+				'cc_rev_id',
+				'cc_repo_id',
+
+				# fields needed for self::newFromRow()
+				'cc_id',
+				'cc_text',
+				'cc_user',
+				'cc_user_text',
+				'cc_patch_line',
+				'cc_timestamp',
+				'cc_review',
+				'cc_sortkey'
+			),
+			array( 'cc_id' => (int) $cc_id ),
+			__METHOD__
+		);
+		return self::newFromRow( $rev, $row );
+	}
+
+	/**
 	 * @param $rev Revision
 	 * @param $data array
 	 * @return CodeComment
