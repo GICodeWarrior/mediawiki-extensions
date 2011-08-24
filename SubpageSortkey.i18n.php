2cd054232652c9265041e8310c7df3ec472a4079
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-24 07:37:37 +0000

    Add new extension (r95382) to translatewiki.net Remove dot at end of the desc message for consistency
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95384 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SubpageSortkey.i18n.php
+++ SubpageSortkey.i18n.php
@@ -1,10 +1,18 @@
 <?php
+/**
+ * Internationalisation file for SubpageSortkey extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
 $messages = array();
 
 $messages['en'] = array(
-	'subpagesortkey-desc' => 'Change the default sortkey of a page to be based on its subpage name instead of its full name.',
+	'subpagesortkey-desc' => 'Change the default sortkey of a page to be based on its subpage name instead of its full name',
 );
 
 $messages['qqq'] = array(
 	'subpagesortkey-desc' => '{{desc}}',
 );
+
