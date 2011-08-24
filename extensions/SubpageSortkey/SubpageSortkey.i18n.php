328ec27730a69cc6ccca0e7da348a5f57321dbd3
bawolff <bawolff@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-24 06:56:02 +0000

    Commit new extension - Allows making the default sortkey part of the subpage instead of the full page name
    Its meant as a way towards fixing bug 22911
    
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95382 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SubpageSortkey/SubpageSortkey.i18n.php
@@ -0,0 +1,10 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+	'subpagesortkey-desc' => 'Change the default sortkey of a page to be based on its subpage name instead of its full name.',
+);
+
+$messages['qqq'] = array(
+	'subpagesortkey-desc' => '{{desc}}',
+);
