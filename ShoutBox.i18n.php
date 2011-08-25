2db713ed4f674c1e94fdc74bee156d5940ee4e24
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-25 13:16:43 +0000

    Update punctuation.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95484 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ShoutBox.i18n.php
+++ ShoutBox.i18n.php
@@ -16,7 +16,7 @@ $messages['en'] = array(
 	'shoutbox-missing-params' => 'ShoutBox is missing a required parameter.',
 	'shoutbox-bad-params' => 'ShoutBox received a bad parameter.',
 	'shoutbox-unparsable-param-string' => 'ShoutBox received the unparsable parameter string "<tt>$1</tt>".',
-	'shoutbox-bad-id' => 'ShoutBox received the bad id "<tt>$1</tt>"',
+	'shoutbox-bad-id' => 'ShoutBox received the bad id "<tt>$1</tt>".',
 	'shoutbox-illegal-width' => 'ShoutBox received the illegal width parameter "$1".',
 	'shoutbox-illegal-height' => 'ShoutBox received the illegal height parameter "$1".',
 	'shoutbox-url' => 'http://$1.shoutbox.us/?LANG=US',
