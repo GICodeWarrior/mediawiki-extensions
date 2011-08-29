4764da65935e8f6369a9c54d6b53f23dbb545b3a
krinkle <krinkle@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-29 21:06:15 +0000

    Apply coding conventions: * Spaces to tabs * Spaces after a ',' in globals list. * Api-classes starting with "Api" * SpecialPage-classes with format Special{Name}. Renamed SignupForm class to SpecialUserSignup. * Filenames matching classes they define * No global functions, instead put hook functions inside a static hooks class
    Best practices:
    * Don't conditionally define a function
    * No need for the 'global' keyword in the config as this is running in the global context already
    * Move a file with 'svn move', not with delete and re-add. That way properties and commit history stays preserved. (I deleted ValidateSignup.php, and reverted r95213 to get the original validateSignup.php back and then renamed it to ApiValidateSignup.php)
    
    Issues fixed:
    * usersignup was listed on Special:SpecialPages under 'other'. Added it to 'login' group instead.
    * Removed usage of inexistent module 'mediawiki.special.usersignup'
    
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95698 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SignupAPI/includes/SignupAPI.hooks.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Hooks for SignupAPI
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+class SignupAPIHooks {
+
+	function onSignupAPIUseAjax() {
+		global $wgOut;
+		$wgOut->addModules( 'ext.SignupAPI' );
+		return true;
+	}
+
+	function onSourceTracking() {
+		global $wgExtNewTables;
+
+		$wgExtNewTables[] = array(
+			'sourcetracking',
+			dirname( __FILE__ ) . '/sourcetracking.sql'
+		);
+		return true;
+	}
+
+	function addSourceTracking( &$personal_urls, &$title ) {
+		global $wgRequest, $wgUser, $wgServer, $wgSecureLogin;
+
+		// Generate source tracking parameters
+		$sourceAction = $wgRequest->getVal( 'action' );
+		$sourceNS = $title->getNamespace();
+		$sourceArticle = $title->getArticleID();
+		$loggedin = $wgUser->isLoggedIn();
+		$thispage = $title->getPrefixedDBkey();
+		$thisurl = $title->getPrefixedURL();
+		$query = array();
+		if ( !$wgRequest->wasPosted() ) {
+			$query = $wgRequest->getValues();
+			unset( $query['title'] );
+			unset( $query['returnto'] );
+			unset( $query['returntoquery'] );
+		}
+		$thisquery = wfUrlencode( wfArrayToCGI( $query ) );
+
+		// Get the returnto and returntoquery parameters from the query string
+		// or fall back on $this->thisurl or $this->thisquery
+		// We can't use getVal()'s default value feature here because
+		// stuff from $wgRequest needs to be escaped, but thisurl and thisquery
+		// are already escaped.
+		$page = $wgRequest->getVal( 'returnto' );
+		if ( !is_null( $page ) ) {
+			$page = wfUrlencode( $page );
+		} else {
+			$page = $thisurl;
+		}
+		$query = $wgRequest->getVal( 'returntoquery' );
+		if ( !is_null( $query ) ) {
+			$query = wfUrlencode( $query );
+		} else {
+			$query = $thisquery;
+		}
+		$returnto = "returnto=$page";
+		if ( $query != '' ) {
+			$returnto .= "&returntoquery=$query";
+		}
+
+		if (isset ( $personal_urls['login'] ) ) {
+			$login_url = $personal_urls['login'];
+			$login_url['href'] = $login_url['href']."&source_action=$sourceAction&source_ns=$sourceNS&source_article=$sourceArticle";
+			$personal_urls['login'] = $login_url;
+		}
+
+		if ( isset ( $personal_urls['anonlogin'] ) ) {
+			$login_url = $personal_urls['anonlogin'];
+			$login_url['href'] = $login_url['href']."&source_action=$sourceAction&source_ns=$sourceNS&source_article=$sourceArticle";
+			$personal_urls['anonlogin'] = $login_url;
+		}
+
+		if ( isset ( $personal_urls['createaccount'] ) ) {
+			$page = $wgRequest->getVal( 'returnto' );
+			$is_signup = $wgRequest->getText( 'type' ) == "signup";
+			$createaccount_url = array(
+				'text' => wfMsg( 'createaccount' ),
+				'href' => SkinTemplate::makeSpecialUrl( 'UserSignup', "$returnto&type=signup&wpSourceAction=$sourceAction&wpSourceNS=$sourceNS&wpSourceArticle=$sourceArticle" ),
+				'active' => $title->isSpecial( 'Userlogin' ) && $is_signup
+			);
+
+			if ( substr( $wgServer, 0, 5 ) === 'http:' && $wgSecureLogin ) {
+				$title = SpecialPage::getTitleFor( 'UserSignup' );
+				$https_url = preg_replace( '/^http:/', 'https:', $title->getFullURL( "type=signup" ) );
+				$createaccount_url['href']	 = $https_url;
+				$createaccount_url['class'] = 'link-https';
+			}
+
+			$personal_urls['createaccount'] = $createaccount_url;
+
+		}
+
+		return true;
+
+	}
+
+}
