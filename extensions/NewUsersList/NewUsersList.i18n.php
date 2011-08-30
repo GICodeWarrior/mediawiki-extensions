1db2b9a7fff39a5c94f7a1e1bc655bc78b6ff28b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-30 08:32:36 +0000

    fu r95738: svn add forgotten.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95739 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/NewUsersList/NewUsersList.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation file for the extension NewUsersList
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/**
+ * @author Reedy
+ */
+$messages['en'] = array(
+	'newuserslist-desc' => 'Adds <tt>&lt;newusers&gt;</tt> parser tag to retrieve the list of new users and their avatars',
+);
+
