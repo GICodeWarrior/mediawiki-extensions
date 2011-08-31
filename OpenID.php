e34e9ac836f02778013b35650326f9cffdca0e99
wikinaut <wikinaut@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-31 04:49:16 +0000

    added preg_quote() in the the OpenID server module as suggested in code review comment; server module code tested (=ok); fixes cr-fixme and reset to cr-new
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@95833 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OpenID.php
+++ OpenID.php
@@ -27,7 +27,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit( 1 );
 }
 
-define( 'MEDIAWIKI_OPENID_VERSION', '0.935-beta 20110830' );
+define( 'MEDIAWIKI_OPENID_VERSION', '0.936-beta 20110831' );
 
 $path = dirname( __FILE__ );
 set_include_path( implode( PATH_SEPARATOR, array( $path ) ) . PATH_SEPARATOR . get_include_path() );
