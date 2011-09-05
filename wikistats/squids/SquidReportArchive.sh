99f8f727ebd621c8209e7b28c03b4360ebbc2f4c
ezachte <ezachte@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-05 17:41:01 +0000

    update packaging of results
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@96293 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- wikistats/squids/SquidReportArchive.sh
+++ wikistats/squids/SquidReportArchive.sh
@@ -2,19 +2,23 @@
 ulimit -v 4000000
 
 home="/a/ezachte"
-month=2011-02
+month=2011-08
 
 # perl $home/SquidReportArchive.pl -m 201007 > SquidReportArchiveLog.txt
-# after further automating SquidScanCountries.sh:
+# after further automating SquidScanCountries.sh
 
-#perl $home/SquidReportArchive.pl -c $month >> SquidReportArchiveLog.txt # -c for per country reports
-#perl $home/SquidReportArchive.pl -m $month >> SquidReportArchiveLog.txt
-#perl $home/SquidCountryScan.pl # start in July 2009
+# perl $home/SquidCountryScan.pl # start in July 2009
+# perl $home/SquidReportArchive.pl -c           # >> SquidReportArchiveLog.txt # -c for per country reports
+# perl $home/SquidReportArchive.pl -c -q 2010Q2 # >> SquidReportArchiveLog.txt # -c for per country reports
+perl $home/SquidReportArchive.pl -m $month           # >> SquidReportArchiveLog.txt
 
-cp /a/ezachte/$month/$month-html.tar ./reports_traffic_$month.tar 
-tar -cf reports_countries_$month.tar SquidReportPage*.htm 
+ls -l /a/ezachte/reports*$month*
+rm    /a/ezachte/reports*$month*
 
-bzip2 -f reports_traffic_$month.tar
-bzip2 -f reports_countries_$month.tar
-
-tar -cf reports_$month.tar reports_*_$month.tar.bz2
+tar -cf /a/ezachte/$month/$month-html.tar /a/ezachte/$month/*.htm
+cp /a/ezachte/$month/$month-html.tar ./reports-traffic-$month.tar 
+tar -cf reports-countries-$month.tar SquidReportPage*.htm 
+bzip2 -f reports-traffic-$month.tar
+bzip2 -f reports-countries-$month.tar
+tar -cf reports-$month.tar reports-*-$month.tar.bz2
+rm /a/ezachte/reports*$month*.bz2
