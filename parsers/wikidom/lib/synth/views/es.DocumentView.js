4736904fe669548a7317723bceda24ef249a3517
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-07 22:46:06 +0000

    Finished moving view creation to model items.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@96515 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/lib/synth/views/es.DocumentView.js
+++ parsers/wikidom/lib/synth/views/es.DocumentView.js
@@ -2,10 +2,6 @@ es.DocumentView = function( documentModel ) {
 	es.ViewContainer.call( this, documentModel, 'blocks' );
 };
 
-es.DocumentView.prototype.createItemView = function( itemModel ) {
-	return itemModel.createView();
-};
-
 /* Inheritance */
 
 es.extend( es.DocumentView, es.ViewContainer );
