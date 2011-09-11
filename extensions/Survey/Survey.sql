c8f12ce5122e6804f05f05b25559d29b22f8d07a
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-11 21:50:05 +0000

    added control to set survey ratio
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@96814 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Survey/Survey.sql
+++ extensions/Survey/Survey.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/surveys (
   survey_user_type         TINYINT             NOT NULL default '0', -- Type of users that can participate in the survey
   survey_namespaces        BLOB                NOT NULL, -- Namespaces on which the survey can be displayed
   survey_ratio             TINYINT unsigned    NOT NULL, -- Percentage of users to show the survey to
-  survey_expiry            INT unsigned        NOT NULL -- Coockie expiry time for the survey
+  survey_expiry            INT unsigned        NOT NULL, -- Coockie expiry time for the survey
 ) /*$wgDBTableOptions*/;
 
 -- Questions
