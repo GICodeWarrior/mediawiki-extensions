2cf8c33d3463a3e7ef8bd3fd36b83e91d16ab1fb
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-13 13:33:40 +0000

    Update for magic words and special pages localisation for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@96961 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UserDebugInfo.alias.php
+++ UserDebugInfo.alias.php
@@ -4,5 +4,20 @@ $specialPageAliases = array();
 
 /** English (English) */
 $specialPageAliases['en'] = array(
-	'UserDebugInfo' => array( 'UserDebugInfo', ),
+	'UserDebugInfo' => array( 'UserDebugInfo' ),
 );
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'UserDebugInfo' => array( 'معلومات_تصليح_المستخدم' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'UserDebugInfo' => array( 'ИнфоКорисникОтстранувањеГрешки' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
