08dbf184e0b1d75a5b9a358ab85578338c4d7150
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-13 13:23:30 +0000

    * Moved messages to a file linked from $wgExtensionMessagesFiles to use the common way to define messages and not the no-more existing $wgMessageCache * Removed now useless parts from WOMLanguage * Use descriptionmsg in $wgExtensionCredits with a message instead of description * Moved away all that was possible away from the extension function * Removed some code duplication
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@96960 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ languages/Messages.php
@@ -0,0 +1,9 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'objecteditor' => 'Object Editor',
+	'wom_editor' => 'Object Model',
+	'wom-desc' => 'Easy Page Object Model for wiki user',
+);
