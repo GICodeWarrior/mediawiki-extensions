d897fd73c4bcf1496f8f832bdeccfe767c25aabb
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-13 13:33:40 +0000

    Update for magic words and special pages localisation for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@96961 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- i18n/qp.alias.php
+++ i18n/qp.alias.php
@@ -13,6 +13,31 @@ $specialPageAliases['en'] = array(
 	'PollResults' => array( 'PollResults' ),
 );
 
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'PollResults' => array( 'نتائج_الاستقصاء' ),
+);
+
+/** Luxembourgish (Lëtzebuergesch) */
+$specialPageAliases['lb'] = array(
+	'PollResults' => array( 'Resultater_vun_der_Ëmfro' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'PollResults' => array( 'АнкетниРезултати' ),
+);
+
+/** Nedersaksisch (Nedersaksisch) */
+$specialPageAliases['nds-nl'] = array(
+	'PollResults' => array( 'Peilingsresultaoten' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'PollResults' => array( 'Peilingresultaten' ),
+);
+
 /** Russian (Русский) */
 $specialPageAliases['ru'] = array(
 	'PollResults' => array( 'Результаты_опросов' ),
