f5c1f51a9272ca16c5a0eb481c7c37e9d29c5c48
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-13 13:33:40 +0000

    Update for magic words and special pages localisation for extensions from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@96961 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Notificator.alias.php
+++ Notificator.alias.php
@@ -13,6 +13,31 @@ $specialPageAliases['en'] = array(
 	'Notificator' => array( 'Notificator' ),
 );
 
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'Notificator' => array( 'مخطر' ),
+);
+
+/** Franco-Provençal (Arpetan) */
+$specialPageAliases['frp'] = array(
+	'Notificator' => array( 'Notifior' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'Notificator' => array( 'Известувач' ),
+);
+
+/** Nedersaksisch (Nedersaksisch) */
+$specialPageAliases['nds-nl'] = array(
+	'Notificator' => array( 'Melder' ),
+);
+
+/** Traditional Chinese (‪中文(繁體)‬) */
+$specialPageAliases['zh-hant'] = array(
+	'Notificator' => array( '通知者' ),
+);
+
 /**
  * For backwards compatibility with MediaWiki 1.15 and earlier.
  */
