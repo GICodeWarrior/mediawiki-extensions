25d9b3545fe1db454b355c95a7df6d812396fece
ezachte <ezachte@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-14 15:49:03 +0000

    + view tracking
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@97070 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- wikistats/dumps/WikiReportsScripts.pm
+++ wikistats/dumps/WikiReportsScripts.pm
@@ -4,6 +4,22 @@ sub SetScriptImageFormat
 {
   my $format = shift ;
 
+$out_google_analytics = <<__GOOGLE_ANALYTICS__ ;
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25704186-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+__GOOGLE_ANALYTICS__
+
 $out_script_embedded_imageformat = <<__SCRIPT_EMBEDDED2__ ;
 
 <script>
