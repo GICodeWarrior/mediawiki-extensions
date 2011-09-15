24f7e8c06559e40e55c88101266307455abbda4d
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-15 00:18:37 +0000

    1.2.2
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@97125 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LiveTranslate/includes/jquery.replaceText.js
+++ extensions/LiveTranslate/includes/jquery.replaceText.js
@@ -8,16 +8,4 @@
  */
 ( function ( $ ) { 
 	$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})};
-} )( jQuery );
-
-/**
- * Regex text escaping function.
- * Borrowed from http://simonwillison.net/2006/Jan/20/escape/
- */
-RegExp.escape = function( text ) {
-	if ( !arguments.callee.sRE ) {
-		var specials = [  '/', '.', '*', '+', '?', '|', '(', ')', '[', ']', '{', '}', '\\' ];
-	    arguments.callee.sRE = new RegExp( '(\\' + specials.join('|\\') + ')', 'g' );
-	}
-	return text.replace(arguments.callee.sRE, '\\$1');
-}
+} )( window.jQuery );
