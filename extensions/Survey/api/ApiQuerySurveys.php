ea48d6259ac7161dac8a301f483888f919501635
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-15 16:09:06 +0000

    Follow up to r97128;
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@97170 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Survey/api/ApiQuerySurveys.php
+++ extensions/Survey/api/ApiQuerySurveys.php
@@ -153,7 +153,6 @@ class ApiQuerySurveys extends ApiQueryBase {
 				ApiBase::PARAM_MAX2 => ApiBase::LIMIT_BIG2
 			),
 			'continue' => null,
-			'token' => null,
 		);
 		
 	}
@@ -171,7 +170,6 @@ class ApiQuerySurveys extends ApiQueryBase {
 			'props' => 'Survey data to query',
 			'continue' => 'Offset number from where to continue the query',
 			'limit' => 'Max amount of words to return',
-			'token' => 'Edit token. You can get one of these through prop=info.',
 		);
 	}
 
