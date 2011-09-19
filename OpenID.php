ca3e81048e7e45e7278116b56550c6a8e5d00500
wikinaut <wikinaut@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-19 22:18:40 +0000

    follow up to r97447 : show OpenID registration timestamps in OpenID preference tab, if these are present
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@97566 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OpenID.php
+++ OpenID.php
@@ -27,7 +27,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit( 1 );
 }
 
-define( 'MEDIAWIKI_OPENID_VERSION', '0.937-beta 20110919' );
+define( 'MEDIAWIKI_OPENID_VERSION', '0.938-beta 20110920' );
 
 $path = dirname( __FILE__ );
 set_include_path( implode( PATH_SEPARATOR, array( $path ) ) . PATH_SEPARATOR . get_include_path() );
