b77e20baa08350e598798488765af5dafd8e8581
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-21 07:10:27 +0000

    moved MediaWikiPlayer support into extension resources for timed media handler
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@97700 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MwEmbedSupport/MwEmbedModules/MediaWikiSupport/MediaWikiSupport.php
+++ extensions/MwEmbedSupport/MwEmbedModules/MediaWikiSupport/MediaWikiSupport.php
@@ -1,12 +1,6 @@
 <?php
 
 return array(
-	"mw.MediaWikiPlayerSupport" => array(
-		'scripts' => 'resources/mw.MediaWikiPlayerSupport.js',
-		'dependencies'=> array( 
-			'mw.Api' 
-		)
-	),
 	'mw.Language.names' => array(
 		'scripts' => 'resources/mw.Language.names.js'
 	),
