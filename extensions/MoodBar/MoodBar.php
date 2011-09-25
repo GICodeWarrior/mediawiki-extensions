899958afc62193cb6eb693e43f046bac799353ea
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-25 17:59:01 +0000

    MoodBar: Display an error message when the AJAX call fails. Probably needs a better error message and/or better styling, but Brandon's not around to help me now
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98073 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MoodBar/MoodBar.php
+++ extensions/MoodBar/MoodBar.php
@@ -119,7 +119,7 @@ $wgResourceModules['ext.moodBar.core'] = $mbResourceTemplate + array(
 $wgResourceModules['ext.moodBar.dashboard'] = $mbResourceTemplate + array(
 	'scripts' => 'ext.moodBar.dashboard/ext.moodBar.dashboard.js',
 	'dependencies' => array( 'mediawiki.util' ),
-	'messages' => array( 'moodbar-feedback-nomore' ),
+	'messages' => array( 'moodbar-feedback-nomore', 'moodbar-feedback-ajaxerror' ),
 );
 
 $wgResourceModules['ext.moodBar.dashboard.styles'] = $mbResourceTemplate + array(
