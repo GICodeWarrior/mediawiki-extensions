ce0a4dbd22125992f5555160e7cb11f9827b9f63
sean_colombo <sean_colombo@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-26 22:50:40 +0000

    Adding ApiExplorer extension which uses the API to dynamically generate API documentation on Special:ApiExplorer.  Depends on the JavascriptAPI extension (currently in Wikia codebase). Would be nice to make this extension able to be aware of the examples that api.php submits & even to let it submit the examples.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98191 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ ApiExplorer.i18n.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Internationalisation file for extension ApiExplorer.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/** English
+ * @author Sean Colombo
+ */
+$messages['en'] = array(
+	'apiexplorer' => 'API Explorer',
+	'apiexplorer-desc' => 'Page for interactively exploring the documentation of the live version of the MediaWiki API running on this installation.',
+	'apiexplorer-intro' => 'This page shows documentation for the current wiki\'s API. Please click on a function below to see more info about what parameters can be used in each call.  For the general MediaWiki API documentation, see $1',
+	
+	'apiexplorer-loading' => 'Loading functions...',
+);
+
+/** Message documentation (Message documentation)
+ */
+$messages['qqq'] = array(
+	'apiexplorer-desc' => "Description of the extension (for Special:Version, etc.).",
+	'apiexplorer-intro' => "The intro at the top of the page to acquiant new users with what the page does."
+);
