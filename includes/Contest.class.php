9905b32d2f6d2f785158b32e8f01394aebded612
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-26 19:00:06 +0000

    created class for contests, using generic db object wrapper copied from Survey backported for PHP 5.2 and slightly simplified
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98157 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ includes/Contest.class.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Abstract base class for representing objects that are stored in some DB table.
+ * 
+ * @since 0.1
+ * 
+ * @file ContestDBObject.php
+ * @ingroup Contest
+ * 
+ * @licence GNU GPL v3 or later
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class Contest {
+	
+	/**
+	 * @see parent::getFieldTypes
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return string
+	 */
+	public function getDBTable() {
+		return 'contests';
+	}
+
+	/**
+	 * @see parent::getFieldTypes
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return string
+	 */
+	protected function getFieldPrefix() {
+		return 'contest_';
+	}
+	
+	/**
+	 * @see parent::getFieldTypes
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return array
+	 */
+	protected function getFieldTypes() {
+		return array(
+			'id' => 'id',
+			'name' => 'str',
+			'enabled' => 'bool',
+			'submission_count' => 'int'
+		);
+	}
+	
+	/**
+	 * @see parent::getDefaults
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return array
+	 */
+	public static function getDefaults() {
+		return array(
+			'name' => '',
+			'enabled' => false,
+			'submission_count' => 0,
+		);
+	}
+	
+}
