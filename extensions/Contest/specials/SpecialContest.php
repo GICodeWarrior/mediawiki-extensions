8fe026e822d9f4f7b0d074d4449964f1b70b718e
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-28 16:46:02 +0000

    work on contest special and landing page
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98331 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Contest/specials/SpecialContest.php
+++ extensions/Contest/specials/SpecialContest.php
@@ -34,15 +34,42 @@ class SpecialContest extends SpecialContestPage {
 			return;
 		}
 		
-
+		$out = $this->getOutput();
+		
+		$contest = Contest::s()->selectRow( null, array( 'name' => $subPage ) );
+		
+		if ( $contest === false ) {
+			$out->redirect( SpecialPage::getTitleFor( 'Contests' )->getLocalURL() );
+		}
+		else {
+			$out->setPageTitle( wfMsgExt( 'contest-contest-title', 'parseinline', $contest->getField( 'name' ) ) );
+			$this->showGeneralInfo( $contest );
+			$this->showContestants( $contest );
+		}
 	}
 	
 	protected function showGeneralInfo( Contest $contest ) {
-		
+		// TODO
 	}
 	
-	protected function showParticipants( Contest $contest ) {
+	// TODO: list scores and comment counts as well
+	protected function showContestants( Contest $contest ) {
+		$conds = array(
+			'contestant_contest_id' => $contest->getId()
+		);
 		
+		$pager = new ContestantPager( $this, $conds );
+		
+		if ( $pager->getNumRows() ) {
+			$this->getOutput()->addHTML(
+				$pager->getNavigationBar() .
+				$pager->getBody().
+				$pager->getNavigationBar()
+			);
+		}
+		else {
+			$this->getOutput()->addWikiMsg( 'contest-contest-no-results' );
+		}
 	}
 	
 }
