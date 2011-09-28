7bc1e903c651d95e3cfa1e6d2e3a173ccc1ce967
dasch <dasch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-28 19:06:07 +0000

    fixing r98338 (adding localisation)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98350 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/RelationLinks/RelationLinks.i18n.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Messages file for the RelationLinks extension
+ *
+ * @addtogroup Extensions
+ */
+
+/**
+ * Get all extension messages
+ *
+ * @return array
+ */
+$messages = array();
+
+$messages['qqq'] = array(
+	'relationlinks-desc'  => 'Description for Special:Version',
+);
+
+$messages['en'] = array(
+	'relationlinks-desc'  => 'Adds link rel to header, that can used for navigation and for SEO',
+);
+
+$messages['de'] = array(
+	'relationlinks-desc'  => 'Fügt im Header versteckte Navigationslinks hinzu die für die Navigation und für die Suchmaschinenoptimierung genutzt werden',
+);
+
