fd5c94efb15cf75d77fd48ef3464f7c79521a0f7
dasch <dasch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-28 19:06:45 +0000

    Adding Extensions FormatNum and ThumbParser
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98351 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ FormatNum.i18n.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Messages file for the FormatNum extension
+ *
+ * @addtogroup Extensions
+ */
+
+/**
+ * Get all extension messages
+ *
+ * @return array
+ */
+$messages = array();
+
+$messages['qqq'] = array(
+	'formatnum-desc'  => 'Description for Special:Version',
+);
+
+$messages['en'] = array(
+	'formatnum-desc'  => 'Passes formatnum to php number_format',
+);
+
+$messages['de'] = array(
+	'formatnum-desc'  => 'Ermöglicht das formatieren von Zahel mit php number_format',
+);
+
---
+++ FormatNum.php
@@ -0,0 +1,57 @@
+<?php
+/** 
+* @addtogroup Extensions 
+*/
+// Check environment
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
+	die( -1 );
+}
+
+/* Configuration */
+
+// Credits
+$wgExtensionCredits['parserhook'][] = array (
+	'path'=> __FILE__ ,
+	'name'=>'FormatNum',
+	'url'=>'http://www.mediawiki.org/wiki/Extension:FormatNum',
+	'description'=>'Passes formatnum to php number_format',
+	'descriptionmsg' => 'formatnum-desc',
+	'author'=>'[http://www.dasch-tour.de DaSch]',
+	'version'=>'0.1.1',
+);
+$dir = dirname( __FILE__ ) . '/';
+
+// Internationalization
+$wgExtensionMessagesFiles['FormatNum'] = $dir . 'FormatNum.i18n.php';
+
+# Define a setup function
+$wgHooks['ParserFirstCallInit'][] = 'efFormatNumParserFunction_Setup';
+# Add a hook to initialise the magic word
+$wgHooks['LanguageGetMagic'][]    = 'efFormatNumParserFunction_Magic';
+ 
+function efFormatNumParserFunction_Setup( $parser ) {
+	# Set a function hook associating the "example" magic word with our function
+	$parser->setFunctionHook( 'formatnum', 'efFormatNumParserFunction_Render' );
+	return true;
+}
+ 
+function efFormatNumParserFunction_Magic( &$magicWords, $langCode ) {
+        # Add the magic word
+        # The first array element is case sensitive, in this case it is not case sensitive
+        # All remaining elements are synonyms for our parser function
+        $magicWords['formatnum'] = array( 0, 'formatnum' );
+        # unless we return true, other parser functions extensions won't get loaded.
+        return true;
+}
+ 
+function efFormatNumParserFunction_Render( $parser, $param1 = 0, $param2 = 0, $param3 = '.', $param4 = ',' ) {
+        # The parser function itself
+        # The input parameters are wikitext with templates expanded
+        # The output should be wikitext too
+        if ($param4 == '_') {
+        	$param4 = ' ';
+        }
+		$output = number_format($param1, $param2, $param3, $param4);
+        return $output;
+}
