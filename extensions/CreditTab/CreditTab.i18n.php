aa0faca165b1ec68cde218c312454ea2bb49b98a
dasch <dasch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-28 16:01:43 +0000

    New Extension CreditTab
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98321 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/CreditTab/CreditTab.i18n.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Messages file for the InputBox extension
+ *
+ * @addtogroup Extensions
+ */
+
+/**
+ * Get all extension messages
+ *
+ * @return array
+ */
+$messages = array();
+
+$messages['en'] = array(
+	'credits-desc'  => 'Adds a link to Credits',
+	'credits-tab'  => 'Credits',
+);
+
+$messages['de'] = array(
+	'credits-desc'  => 'Fügt Link zu Autoren hinzu',
+	'credits-tab'  => 'Autoren',
+);
