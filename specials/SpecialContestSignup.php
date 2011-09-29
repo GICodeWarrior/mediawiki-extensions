2cd20f34a97cb426f85fa21622ad31a5ba321627
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-29 17:43:18 +0000

    replace underscores  by spaces in the name
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98419 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- specials/SpecialContestSignup.php
+++ specials/SpecialContestSignup.php
@@ -30,6 +30,8 @@ class SpecialContestSignup extends SpecialContestPage {
 	 * @param string $arg
 	 */
 	public function execute( $subPage ) {
+		$subPage = str_replace( '_', ' ', $subPage );
+		
 		if ( !parent::execute( $subPage ) ) {
 			return;
 		}
