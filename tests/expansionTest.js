2afc24fd208ea8f9adcf5eb7db52ac8644111330
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-29 00:23:17 +0000

    mwahahaha expand sorta working in parts in the ParserPlayground JS code. Too scary & debug-filled, though, and mostly fake. ;)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98393 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tests/expansionTest.js
+++ tests/expansionTest.js
@@ -1,4 +1,20 @@
 var pageDatabase = {
+	'Boring': 'Just some text and [[a link]].',
+	'Template:Parens': '({{{1}}})',
+	'ParenCaller': '{{Parens|bizbax}}'
+}
+var domDatabase = {
+	'Boring': {
+		type: 'root',
+		contents: [
+			'Just some text and ',
+			{
+				type: 'link',
+				target: 'a link'
+			},
+			'.'
+		]
+	},
 	'Template:Parens': {
 		type: 'root',
 		contents: [
@@ -52,15 +68,20 @@ var pageDatabase = {
 	}
 };
 
-var env = new MWParserEnvironment({
-	'domCache': pageDatabase
-});
-var frame = new PPFrame(env);
-frame.expand(pageDatabase['ParenCaller'], 0, function(node, err) {
-	if (err) {
-		console.log('error', err);
-	} else {
-		console.log(node);
-	}
-});
-
+$(function() {
+	var env = new MWParserEnvironment({
+		'pageCache': pageDatabase,
+		'domCache': domDatabase
+	});
+	env.debug = true;
+	var frame = new PPFrame(env);
+	//var victim = 'Boring';
+	var victim = 'ParenCaller';
+	frame.expand(domDatabase[victim], 0, function(node, err) {
+		if (err) {
+			console.log('error', err);
+		} else {
+			console.log(node);
+		}
+	});
+})
