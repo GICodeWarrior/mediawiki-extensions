933e30b323ed8a6670ded0351e4bf99b7080a2f1
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-30 21:46:43 +0000

    Added length adjustment methods that automatically propagate changes upstream
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98573 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/lib/hype/bases/es.DocumentModelNode.js
+++ parsers/wikidom/lib/hype/bases/es.DocumentModelNode.js
@@ -1,10 +1,13 @@
 /**
  * Creates an es.DocumentModelNode object.
  * 
+ * es.DocumentModelNode is a simple wrapper around es.ModelNode, which adds functionality for model
+ * nodes to be used as nodes in a space partitioning tree.
+ * 
  * @class
  * @constructor
- * @param {Integer} contentLength Length of contents
- * @property {Integer} contentLength Length of contents
+ * @param {Integer} contentLength Length of content
+ * @property {Integer} contentLength Length of content
  */
 es.DocumentModelNode = function( contentLength ) {
 	// Inheritance
@@ -16,6 +19,51 @@ es.DocumentModelNode = function( contentLength ) {
 
 /* Methods */
 
+/**
+ * Sets the content length.
+ * 
+ * @method
+ * @param {Integer} contentLength Length of content
+ * @throws Invalid content length error if contentLength is less than 0
+ */
+es.DocumentModelNode.setContentLength = function( contentLength ) {
+	if ( contentLength < 0 ) {
+		throw 'Invalid content length error. Content length can not be less than 0.';
+	}
+	var diff = contentLength - this.contentLength;
+	this.contentLength = contentLength;
+	if ( this.parent ) {
+		this.parent.adjustContentLength( diff );
+	}
+};
+
+/**
+ * Adjust the content length.
+ * 
+ * @method
+ * @param {Integer} adjustment Amount to adjust content length by
+ * @throws Invalid adjustment error if resulting length is less than 0
+ */
+es.DocumentModelNode.adjustContentLength = function( adjustment ) {
+	this.contentLength += adjustment;
+	// Make sure the adjustment was sane
+	if ( this.contentLength < 0 ) {
+		// Reverse the adjustment
+		this.contentLength -= adjustment;
+		// Complain about it
+		throw 'Invalid adjustment error. Content length can not be less than 0.';
+	}
+	if ( this.parent ) {
+		this.parent.adjustContentLength( adjustment );
+	}
+};
+
+/**
+ * Gets the content length.
+ * 
+ * @method
+ * @returns {Integer} Length of content
+ */
 es.DocumentModelNode.getContentLength = function() {
 	return this.contentLength;
 };
