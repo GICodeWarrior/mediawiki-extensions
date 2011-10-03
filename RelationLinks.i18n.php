cb7c1329679da55609913567e7ee2e1657d76de3
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-03 18:58:08 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98795 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- RelationLinks.i18n.php
+++ RelationLinks.i18n.php
@@ -29,6 +29,34 @@ $messages['de'] = array(
 	'relationlinks-desc' => 'Fügt im Headerbereich versteckte Navigationslinks hinzu, die für die Navigation wie auch für die Suchmaschinenoptimierung genutzt werden können',
 );
 
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'relationlinks-desc' => "Ajoute un lien lié à l'entête, qui peut être utilisé pour la navigation et pour le référencement",
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'relationlinks-desc' => 'Engade unha ligazón ligada á cabeceira que se pode empregar para a navegación e para as referencias',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'relationlinks-desc' => 'Adde un etiquetta "link rel" al capite, que pote esser usate pro navigation e pro optimisation al motores de recerca',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'relationlinks-desc' => "Setzt an den Header Linken derbäi déi fir d'Navigatioun an Optimiséierung vu Suchmaschinne benotzt kënne ginn.",
+);
+
 /** Macedonian (Македонски)
  * @author Bjankuloski06
  */
