aeabb0948279495053238f0624397e8ed59bc335
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-03 17:32:43 +0000

    FreqPatternTagCloud: cleanup the i18n file
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98781 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/FreqPatternTagCloud/FreqPatternTagCloud.i18n.php
+++ extensions/FreqPatternTagCloud/FreqPatternTagCloud.i18n.php
@@ -1,9 +1,8 @@
 <?php
-
 /**
  * Frequent Pattern Tag Cloud Plug-in
  * Internationalization
- * 
+ *
  * @author Tobias Beck, University of Heidelberg
  * @author Andreas Fay, University of Heidelberg
  * @version 1.0
@@ -11,59 +10,64 @@
 
 $messages = array();
 
+/** English */
 $messages['en'] = array(
-		"freqpatterntagcloud" => "Frequent Pattern Tag Cloud",
-		"freqpatterntagcloudmaintenance" => "Frequent Pattern Tag Cloud Maintenance",
-		"fptc-categoryname" => "Category",
-		"fptc-context-menu-browse" => "Browse pages with this value",
-		"fptc-context-menu-similar-tags" => "Similar tags",
-		"fptc-form-attribute-name" => "Property",
-		"fptc-form-submit-button" => "Submit",
-		"fptc-invalid-attribute" => "The entered property is invalid.",
-		"fptc-insufficient-rights-for-maintenance" => "You have to log in as system administrator to view this page.",
-		"fptc-refresh-frequent-patterns" => "Refresh data",
-		"fptc-refreshed-frequent-patterns" => "Frequent pattern rules refreshed.",
-		"fptc-search-attribute-name" => "Search property",
-		"fptc-search-button" => "Search",
-		"fptc-search-suggestion-value" => "Similar to \"%s\":",
-		"fptc-suggestion" => "Do you mean:",
-		"fptc-no-suggestion" => "No suggestions found"
-		);
-		
+	'freqpatterntagcloud' => 'Frequent Pattern Tag Cloud',
+	'freqpatterntagcloudmaintenance' => 'Frequent Pattern Tag Cloud Maintenance',
+	'fptc-categoryname' => 'Category',
+	'fptc-context-menu-browse' => 'Browse pages with this value',
+	'fptc-context-menu-similar-tags' => 'Similar tags',
+	'fptc-form-attribute-name' => 'Property',
+	'fptc-form-submit-button' => 'Submit',
+	'fptc-invalid-attribute' => 'The entered property is invalid.',
+	'fptc-insufficient-rights-for-maintenance' => 'You have to log in as system administrator to view this page.',
+	'fptc-refresh-frequent-patterns' => 'Refresh data',
+	'fptc-refreshed-frequent-patterns' => 'Frequent pattern rules refreshed.',
+	'fptc-search-attribute-name' => 'Search property',
+	'fptc-search-button' => 'Search',
+	'fptc-search-suggestion-value' => 'Similar to "%s":',
+	'fptc-suggestion' => 'Do you mean:',
+	'fptc-no-suggestion' => 'No suggestions found'
+);
+
+/** German (Deutsch)
+ * @author Tobias Beck, University of Heidelberg
+ * @author Andreas Fay, University of Heidelberg
+ */
 $messages['de'] = array(
-		"freqpatterntagcloud" => "Frequent Pattern Tag Cloud",
-		"freqpatterntagcloudmaintenance" => "Frequent Pattern Tag Cloud Maintenance",
-		"fptc-categoryname" => "Kategorie",
-		"fptc-context-menu-browse" => "Durchsuche Seiten mit diesem Wert",
-		"fptc-context-menu-similar-tags" => "�hnliche Tags",
-		"fptc-form-attribute-name" => "Attribut",
-		"fptc-form-submit-button" => "Eingabe",
-		"fptc-invalid-attribute" => "Das eingegebene Attribut ist ung&uuml;ltig.",
-		"fptc-insufficient-rights-for-maintenance" => "Um diese Seite sehen zu k&ouml;nnen m&uuml;ssen Sie als Systemadministrator angemeldet sein.",
-		"fptc-refresh-frequent-patterns" => "Aktualisierung der Daten",
-		"fptc-refreshed-frequent-patterns" => "Regeln f&uuml;r frequent pattern neu generiert.",
-		"fptc-search-attribute-name" => "Suche nach Attribut",
-		"fptc-search-button" => "Suche",
-		"fptc-search-suggestion-value" => "&Auml;hnlich zu \"%s\":",
-		"fptc-suggestion" => "Meinten Sie:",
-		"fptc-no-suggestion" => "Keine Vorschl&auml;ge gefunden"
-		);
-		
+	'freqpatterntagcloud' => 'Frequent Pattern Tag Cloud',
+	'freqpatterntagcloudmaintenance' => 'Frequent Pattern Tag Cloud Maintenance',
+	'fptc-categoryname' => 'Kategorie',
+	'fptc-context-menu-browse' => 'Durchsuche Seiten mit diesem Wert',
+	'fptc-context-menu-similar-tags' => 'Ähnliche Tags',
+	'fptc-form-attribute-name' => 'Attribut',
+	'fptc-form-submit-button' => 'Eingabe',
+	'fptc-invalid-attribute' => 'Das eingegebene Attribut ist ungültig.',
+	'fptc-insufficient-rights-for-maintenance' => 'Um diese Seite sehen zu können müssen Sie als Systemadministrator angemeldet sein.',
+	'fptc-refresh-frequent-patterns' => 'Aktualisierung der Daten',
+	'fptc-refreshed-frequent-patterns' => 'Regeln für frequent pattern neu generiert.',
+	'fptc-search-attribute-name' => 'Suche nach Attribut',
+	'fptc-search-button' => 'Suche',
+	'fptc-search-suggestion-value' => 'Ähnlich zu "%s":',
+	'fptc-suggestion' => 'Meinten Sie:',
+	'fptc-no-suggestion' => 'Keine Vorschläge gefunden'
+);
+
 $messages['qqq'] = array(
-		"freqpatterntagcloud" => "This message is the page title of the frequent-pattern-tag-cloud-specialpage and must not be translated.",
-		"freqpatterntagcloudmaintenance" => "This message is the page title of the frequent-pattern-tag-cloud-maintenace-specialpage and must not be translated.",
-		"fptc-categoryname" => "This message is the name of the category-attribut which is used in SemantikMediaWiki (e.g. category:example)",
-		"fptc-context-menu-browse" => "This message is used as a title in context menu of the tag cloud tags",
-		"fptc-context-menu-similar-tags" => "This message is used as a title in context menu of the tag cloud tags",
-		"fptc-form-attribute-name" => "This message describes the input box on the frequent-pattern-tag-cloud-specialpage",
-		"fptc-form-submit-button" => "This message is the text of the button which submits the value in the input box on the frequent-pattern-tag-cloud-specialpage",
-		"fptc-invalid-attribute" => "This message appears if the value in the input box on the frequent-pattern-tag-cloud-specialpage is invalid",
-		"fptc-insufficient-rights-for-maintenance" => "This message appears when calling the frequent-pattern-tag-cloud-maintenace-specialpage and the user is not a administrator.",
-		"fptc-refresh-frequent-patterns" => "This message is the text of the button to refresh the frequent patterns on the frequent-pattern-tag-cloud-maintenace-specialpage",
-		"fptc-refreshed-frequent-patterns" => "This message appears after successful update of the frequent patterns.",
-		"fptc-search-attribute-name" => "This message describes the search input box.",
-		"fptc-search-button" => "This message is the text of the button search",
-		"fptc-search-suggestion-value" => "This mesage is the title of the search suggestions (parameter %s = suggestion, do not translate)",
-		"fptc-suggestion" => "This message appears after submitting an invalid value in the input box on the frequent-pattern-tag-cloud-specialpage",
-		"fptc-no-suggestion" => "This message appears if no suggestions can be found."
-		);
+	'freqpatterntagcloud' => 'This message is the page title of the frequent-pattern-tag-cloud-specialpage and must not be translated.',
+	'freqpatterntagcloudmaintenance' => 'This message is the page title of the frequent-pattern-tag-cloud-maintenace-specialpage and must not be translated.',
+	'fptc-categoryname' => 'This message is the name of the category-attribut which is used in SemantikMediaWiki (e.g. category:example)',
+	'fptc-context-menu-browse' => 'This message is used as a title in context menu of the tag cloud tags',
+	'fptc-context-menu-similar-tags' => 'This message is used as a title in context menu of the tag cloud tags',
+	'fptc-form-attribute-name' => 'This message describes the input box on the frequent-pattern-tag-cloud-specialpage',
+	'fptc-form-submit-button' => 'This message is the text of the button which submits the value in the input box on the frequent-pattern-tag-cloud-specialpage',
+	'fptc-invalid-attribute' => 'This message appears if the value in the input box on the frequent-pattern-tag-cloud-specialpage is invalid',
+	'fptc-insufficient-rights-for-maintenance' => 'This message appears when calling the frequent-pattern-tag-cloud-maintenace-specialpage and the user is not a administrator.',
+	'fptc-refresh-frequent-patterns' => 'This message is the text of the button to refresh the frequent patterns on the frequent-pattern-tag-cloud-maintenace-specialpage',
+	'fptc-refreshed-frequent-patterns' => 'This message appears after successful update of the frequent patterns.',
+	'fptc-search-attribute-name' => 'This message describes the search input box.',
+	'fptc-search-button' => 'This message is the text of the button search',
+	'fptc-search-suggestion-value' => 'This mesage is the title of the search suggestions (parameter %s = suggestion, do not translate)',
+	'fptc-suggestion' => 'This message appears after submitting an invalid value in the input box on the frequent-pattern-tag-cloud-specialpage',
+	'fptc-no-suggestion' => 'This message appears if no suggestions can be found.'
+);
