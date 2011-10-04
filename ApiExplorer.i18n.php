764108598d7814b47109cf3c2f96951a8532b49c
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-04 20:02:01 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@98891 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ApiExplorer.i18n.php
+++ ApiExplorer.i18n.php
@@ -32,5 +32,34 @@ $messages['qqq'] = array(
 $messages['de'] = array(
 	'apiexplorer' => 'API-Explorer',
 	'apiexplorer-desc' => 'Ergänzt eine Spezialseite, mit der das interaktive Nutzen der Dokumentation zur API der MediaWiki-Installation möglich ist',
+	'apiexplorer-intro' => 'Diese Seite zeigt die Dokumention zur API dieses Wikis. Bitte unten auf eine der Funktionen klicken, um mehr Informationen zu den mit jedem Aufruf verfügbaren Parametern zu erhalten. Eine allgemeine Dokumentation zur API von MediaWiki ist auf der folgenden Webseite vorhanden: $1',
+	'apiexplorer-loading' => 'Funktionen werden geladen …',
+);
+
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'apiexplorer' => 'API Explorer',
+	'apiexplorer-desc' => "Page pour explorer de façon interactive la documentation de la version courante de l'API MediaWiki tournant sur cette installation.",
+	'apiexplorer-intro' => "Cette page affiche la documentation pour l'API du wiki actuel. Merci de cliquer sur une fonction ci-dessous pour voir plus d'information sur les paramètres qui peuvent être utilisés dans chaque appel. Pour la documentation générale de l'API MediaWiki, voyez $1",
+	'apiexplorer-loading' => 'Chargement des fonctions...',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'apiexplorer' => 'API-истражувач',
+	'apiexplorer-desc' => 'Страница за интерактивно истражување на документацијата на тековната активна верзија на API-прилогот на МедијаВики што работи на оваа инсталација.',
+	'apiexplorer-intro' => 'Оваа страница пркажува документација за API-прилогот на тековното вики. Стиснете на функција подолу за да добиете повеќе информации за тоа кои параметри можат да се употребат во секој повик. Општа документација за API-прилогот на МедијаВики ќе најдете на $1',
+	'apiexplorer-loading' => 'Вчитувам функции...',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'apiexplorer-loading' => 'Bezig met het laden van functies...',
 );
 
