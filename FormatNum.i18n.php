5d14961a0879852b3a7ecd9b8f05c159b51b7487
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-05 19:39:20 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99033 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- FormatNum.i18n.php
+++ FormatNum.i18n.php
@@ -27,3 +27,45 @@ $messages['de'] = array(
 	'formatnum-desc' => 'Ermöglicht das Formatieren von Zahlen mit php number_format',
 );
 
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'formatnum-desc' => 'Passe formatnum au number_format de php',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'formatnum-desc' => 'Pasa formatnum ao number_format do PHP',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'formatnum-desc' => 'Passa formatnum a number_format de PHP',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'formatnum-desc' => 'Löht en Zahle Fommatteere övver dem Wiki singe Funxjuhn <code lang="en">formatnum</code> vum PHP singe Funxjuhn <code lang="en">number_format</code> maache.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'formatnum-desc' => 'Ännert fromatnumm op php number_format',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'formatnum-desc' => 'Овозможува форматирање на бројки со php number_format',
+);
+
