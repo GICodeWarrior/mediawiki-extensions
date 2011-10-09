c6df45ad0a3a5b83dc418799a1f85e52b03b4900
kwisatz <kwisatz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-09 16:39:22 +0000

    implemented alias and messages separation as requested in code review c24014
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99358 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/TwitterLogin/TwitterLogin.i18n.php
+++ extensions/TwitterLogin/TwitterLogin.i18n.php
@@ -8,25 +8,17 @@
  * @author David Raison
  */
 
-$aliases = array();
 $messages = array();
-
-/** English
- * @author David Raison
- */
-$aliases['en'] = array(
-	'TwitterLogin' => array('TwitterLogin')
-);
 
 /** English
  * @author David Raison
  */
 $messages['en'] = array(
 	'twitterlogin' => 'Sign in using Twitter',
-	'signuptwitter' => 'You can register to this wiki using your twitter account',
-	'tietoaccount' => 'You are currently logged into this wiki as $1.<br/>You may sign in with Twitter to tie your twitter account to your existing mediawiki account.',
+	'twitterlogin-signup' => 'You can register to this wiki using your twitter account',
+	'twiterlogin-tietoaccount' => 'You are currently logged into this wiki as $1.<br/>You may sign in with Twitter to tie your twitter account to your existing mediawiki account.',
 	'twitterlogin-desc' => 'Register and log in to a mediawiki using your twitter account',
-	'alreadyloggedin' => 'You\'re already logged in.',
-	'couldnotconnect' => 'Could not connect to Twitter. Refresh the page or try again later.'
+	'twitterlogin-alreadyloggedin' => 'You\'re already logged in.',
+	'twitterlogin-couldnotconnect' => 'Could not connect to Twitter. Refresh the page or try again later.'
 );
 
