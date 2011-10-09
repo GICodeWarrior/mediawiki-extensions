GICodeWarrior/mediawiki-extensions

commit 1ffcdf55b95483e3594c8707e1089b938e80b687

Improve method documentation
Few bits of fixup

git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@82487 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/skins/Standard.php
@@ -18,9 +18,6 @@ class SkinStandard extends SkinLegacy {
 	var $skinname = 'standard', $stylename = 'standard',
 		$template = 'StandardTemplate';
 
-	/**
-	 *
-	 */
 	function setupSkinUserCss( OutputPage $out ){
 		parent::setupSkinUserCss( $out );
 		$out->AddModuleStyles( 'skins.standard' );

phase3/includes/Skin.php
@@ -360,7 +360,10 @@ abstract class Skin extends Linker {
 		$this->mTitle = $t;
 	}
 
-	/** Get the title */
+	/** Get the title
+	 *
+	 * @return Title
+	 */
 	public function getTitle() {
 		return $this->mTitle;
 	}

phase3/skins/Modern.php
@@ -32,6 +32,10 @@ class SkinModern extends SkinTemplate {
  * @ingroup Skins
  */
 class ModernTemplate extends MonoBookTemplate {
+
+	/**
+	 * @var Skin
+	 */
 	var $skin;
 	/**
 	 * Template filter callback for Modern skin.
@@ -44,7 +48,6 @@ class ModernTemplate extends MonoBookTemplate {
 	function execute() {
 		global $wgRequest;
 		$this->skin = $skin = $this->data['skin'];
-		$action = $wgRequest->getText( 'action' );
 
 		// Suppress warnings to prevent notices about missing indexes in $this->data
 		wfSuppressWarnings();

phase3/skins/MonoBook.php
@@ -52,7 +52,12 @@ class SkinMonoBook extends SkinTemplate {
  * @ingroup Skins
  */
 class MonoBookTemplate extends BaseTemplate {
+
+	/**
+	 * @var Skin
+	 */
 	var $skin;
+
 	/**
 	 * Template filter callback for MonoBook skin.
 	 * Takes an associative array of data set from a SkinTemplate-based

phase3/skins/Vector.php
@@ -61,7 +61,7 @@ class VectorTemplate extends BaseTemplate {
 	/* Members */
 
 	/**
-	 * @var Cached skin object
+	 * @var Skin Cached skin object
 	 */
 	var $skin;
 
@@ -80,7 +80,7 @@ class VectorTemplate extends BaseTemplate {
 		$nav = $this->data['content_navigation'];
 		
 		if ( $wgVectorUseIconWatch ) {
-			$mode = $this->skin->mTitle->userIsWatching() ? 'unwatch' : 'watch';
+			$mode = $this->skin->getTitle()->userIsWatching() ? 'unwatch' : 'watch';
 			if ( isset($nav['actions'][$mode]) ) {
 				$nav['views'][$mode] = $nav['actions'][$mode];
 				$nav['views'][$mode]['class'] = rtrim('icon ' . $nav['views'][$mode]['class'], ' ');
@@ -88,7 +88,8 @@ class VectorTemplate extends BaseTemplate {
 				unset($nav['actions'][$mode]);
 			}
 		}
-		
+
+		$xmlID = '';
 		foreach ( $nav as $section => $links ) {
 			foreach ( $links as $key => $link ) {
 				if ( $section == "views" && !(isset($link["primary"]) && $link["primary"]) ) {

commit 12663b483a0079fc3d1dc76441a027dd5c6c1681

Fix bug 31436. The collection extension is not array based, which caused an extra seperator to be inserted, but no content. This was broken in r82487 or before in the SkinLegacy rewrite.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99353 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/skins/Standard.php
@@ -116,10 +116,10 @@ class StandardTemplate extends LegacyTemplate {
 
 		$barnumber = 1;
 		foreach ( $bar as $browseLinks ) {
-			if ( $barnumber > 1 ) {
-				$s .= "\n<hr class='sep' />";
-			}
 			if ( is_array( $browseLinks ) ) {
+				if ( $barnumber > 1 ) {
+					$s .= "\n<hr class='sep' />";
+				}
 				foreach ( $browseLinks as $link ) {
 					if ( $link['text'] != '-' ) {
 						$s .= "<a href=\"{$link['href']}\">" .