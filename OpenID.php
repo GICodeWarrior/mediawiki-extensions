d6656195cd0b65d1069c46cc034581b566b65cd8
wikinaut <wikinaut@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-13 00:31:22 +0000

    maintenance patch of the updater hook
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99665 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OpenID.php
+++ OpenID.php
@@ -27,7 +27,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit( 1 );
 }
 
-define( 'MEDIAWIKI_OPENID_VERSION', '0.941-beta 20111011' );
+define( 'MEDIAWIKI_OPENID_VERSION', '0.942-beta 20111012' );
 
 $path = dirname( __FILE__ );
 set_include_path( implode( PATH_SEPARATOR, array( $path ) ) . PATH_SEPARATOR . get_include_path() );
