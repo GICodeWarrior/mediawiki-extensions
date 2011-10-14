627cc97fd93f1d6872aa8e609aae4cd07b14f66e
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-14 21:30:16 +0000

    Fix more comments
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99815 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- twitteroauth/twitteroauth.php
+++ twitteroauth/twitteroauth.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * Abraham Williams (abraham@abrah.am) http://abrah.am
  *
  * The first PHP Library to support OAuth for Twitter's REST API.
@@ -22,7 +22,7 @@ class TwitterOAuth {
   /* Set timeout default. */
   public $timeout = 30;
   /* Set connect timeout. */
-  public $connecttimeout = 30; 
+  public $connecttimeout = 30;
   /* Verify SSL Cert. */
   public $ssl_verifypeer = FALSE;
   /* Respons format. */
@@ -76,7 +76,7 @@ class TwitterOAuth {
     $parameters = array();
     if (!empty($oauth_callback)) {
       $parameters['oauth_callback'] = $oauth_callback;
-    } 
+    }
     $request = $this->oAuthRequest($this->requestTokenURL(), 'GET', $parameters);
     $token = OAuthUtil::parse_parameters($request);
     $this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret']);
@@ -127,7 +127,7 @@ class TwitterOAuth {
    *                "user_id" => "9436992",
    *                "screen_name" => "abraham",
    *                "x_auth_expires" => "0")
-   */  
+   */
   function getXAuthToken($username, $password) {
     $parameters = array();
     $parameters['x_auth_username'] = $username;
@@ -149,7 +149,7 @@ class TwitterOAuth {
     }
     return $response;
   }
-  
+
   /**
    * POST wrapper for oAuthRequest.
    */
