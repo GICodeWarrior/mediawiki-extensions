3994a220c71873d27d66804a71a43c666efe0286
cervidae <cervidae@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-14 15:37:30 +0000

    Adding GlobalUserGroups extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99773 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ GlobalUserGroups.i18n.extras.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Internationalization file for extra user groups if GlobalUserGroups extension is used
+ *
+ * @since 1.0
+ *
+ * @file GlobalUserGroups.i18n.extras.php
+ * @ingroup GlobalUserGroupsExtras
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @author Tim Weyer (SVG) <svg@tim-weyer.org>
+ */
+
+$messages = array();
+
+/** English
+ * @author SVG
+ */
+$messages['en'] = array(
+   'group-bot-global'                => 'Global Bots',
+   'group-bot-global-member'         => 'global bot',
+   'grouppage-bot-global'            => '{{ns:help}}:Global Bots',
+
+   'group-checkuser-global'          => 'Global Check users',
+   'group-checkuser-global-member'   => 'global check user',
+   'grouppage-checkuser-global'      => '{{ns:help}}:Global Checkusers',
+
+   'group-communitymanager'          => 'Community managers',
+   'group-communitymanager-member'   => 'community manager',
+   'grouppage-communitymanager'      => '{{ns:help}}:Community managers',
+
+   'group-helper'                    => 'Helpers',
+   'group-helper-member'             => 'helper',
+   'grouppage-helper'                => '{{ns:help}}:Helpers',
+
+   'group-helper-global'             => 'Global Helpers',
+   'group-helper-global-member'      => 'global helper',
+   'grouppage-helper-global'         => '{{ns:help}}:Global Helpers',
+
+   'group-ipblock-exempt'            => 'IP block exemptions',
+   'group-ipblock-exempt-member'     => 'ip block exemption',
+   'grouppage-ipblock-exempt'        => '{{ns:help}}:IP block exemptions',
+
+   'group-ipblock-exempt-g'          => 'Global IP block exemptions',
+   'group-ipblock-exempt-g-member'   => 'global ip block exemption',
+   'grouppage-ipblock-exempt-g'      => '{{ns:help}}:Global IP block exemptions',
+
+   'group-rollback-global'           => 'Global Rollbackers',
+   'group-rollback-global-member'    => 'global rollbacker',
+   'grouppage-rollback-global'       => '{{ns:help}}:Global Rollbackers',
+
+   'group-steward'                   => 'Stewards',
+   'group-steward-member'            => 'steward',
+   'grouppage-steward'               => '{{ns:help}}:Stewards',
+
+   'group-steward-global'            => 'Global Stewards',
+   'group-steward-global-member'     => 'global steward',
+   'grouppage-steward-global'        => '{{ns:help}}:Global Stewards',
+
+   'group-sysadmin'                  => 'System administrators',
+   'group-sysadmin-member'           => 'system administrator',
+   'grouppage-sysadmin'              => '{{ns:help}}:System administrators',
+
+   'group-sysadmin-global'           => 'Global System administrators',
+   'group-sysadmin-global-member'    => 'global system administrator',
+   'grouppage-sysadmin-global'       => '{{ns:help}}:Global System administrators',
+
+   'group-sysop-global'              => 'Global Administrators',
+   'group-sysop-global-member'       => 'global administrator',
+   'grouppage-sysop-global'          => '{{ns:help}}:Global Administrators',
+);
+
+/** German (Deutsch)
+ * @author SVG
+ */
+$messages['de'] = array(
+   'group-bot-global'                => 'Globale Bots',
+   'group-bot-global-member'         => 'Globaler Bot',
+   'grouppage-bot-global'            => '{{ns:help}}:Globale Bots',
+
+   'group-communitymanager'          => 'Community Manager',
+   'group-communitymanager-member'   => 'Community Manager',
+   'grouppage-communitymanager'      => '{{ns:help}}:Community Manager',
+
+   'group-checkuser-global'          => 'Globale Checkuser',
+   'group-checkuser-global-member'   => 'Globaler Checkuser',
+   'grouppage-checkuser-global'      => '{{ns:help}}:Globale Checkuser',
+
+   'group-helper'                    => 'Helfer',
+   'group-helper-member'             => 'Helfer',
+   'grouppage-helper'                => '{{ns:help}}:Helfer',
+
+   'group-helper-global'             => 'Globale Helfer',
+   'group-helper-global-member'      => 'Globaler Helfer',
+   'grouppage-helper-global'         => '{{ns:help}}:Globale Helfer',
+
+   'group-ipblock-exempt'            => 'IP-Sperre-Ausnahmen',
+   'group-ipblock-exempt-member'     => 'IP-Sperre-Ausnahme',
+   'grouppage-ipblock-exempt'        => '{{ns:help}}:IP-Sperre-Ausnahmen',
+
+   'group-ipblock-exempt-g'          => 'Globale IP-Sperre-Ausnahmen',
+   'group-ipblock-exempt-g-member'   => 'Globale IP-Sperre-Ausnahme',
+   'grouppage-ipblock-exempt-g'      => '{{ns:help}}:Globale IP-Sperre-Ausnahmen',
+
+   'group-rollback-global'           => 'Globale Zurücksetzer',
+   'group-rollback-global-member'    => 'Globaler Zurücksetzer',
+   'grouppage-rollback-global'       => '{{ns:help}}:Globale Zurücksetzer',
+
+   'group-steward'                   => 'Stewards',
+   'group-steward-member'            => 'Steward',
+   'grouppage-steward'               => '{{ns:help}}:Stewards',
+
+   'group-steward-global'            => 'Globale Stewards',
+   'group-steward-global-member'     => 'Globaler Steward',
+   'grouppage-steward-global'        => '{{ns:help}}:Globale Stewards',
+
+   'group-sysadmin'                  => 'Systemadministratoren',
+   'group-sysadmin-member'           => 'Systemadministrator',
+   'grouppage-sysadmin'              => '{{ns:help}}:Systemadministratoren',
+
+   'group-sysadmin-global'           => 'Globale Systemadministratoren',
+   'group-sysadmin-global-member'    => 'Globaler Systemadministrator',
+   'grouppage-sysadmin-global'       => '{{ns:help}}:Globale Systemadministratoren',
+
+   'group-sysop-global'              => 'Globale Administratoren',
+   'group-sysop-global-member'       => 'Globaler Administrator',
+   'grouppage-sysop-global'          => '{{ns:help}}:Globale Administratoren',
+);
