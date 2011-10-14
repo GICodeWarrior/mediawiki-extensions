34fc88728c6f491f763e0ed195e08ae1a782b18c
cervidae <cervidae@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-14 19:14:22 +0000

    Changing to gender specific -member translations
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99794 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- GlobalUserGroups.i18n.extras.php
+++ GlobalUserGroups.i18n.extras.php
@@ -18,55 +18,55 @@ $messages = array();
  */
 $messages['en'] = array(
    'group-bot-global'                => 'Global Bots',
-   'group-bot-global-member'         => 'global bot',
+   'group-bot-global-member'         => '{{GENDER:$1|global bot}}',
    'grouppage-bot-global'            => '{{ns:help}}:Global Bots',
 
    'group-checkuser-global'          => 'Global Check users',
-   'group-checkuser-global-member'   => 'global check user',
+   'group-checkuser-global-member'   => '{{GENDER:$1|global check user}}',
    'grouppage-checkuser-global'      => '{{ns:help}}:Global Checkusers',
 
    'group-communitymanager'          => 'Community managers',
-   'group-communitymanager-member'   => 'community manager',
+   'group-communitymanager-member'   => '{{GENDER:$1|community manager}}',
    'grouppage-communitymanager'      => '{{ns:help}}:Community managers',
 
    'group-helper'                    => 'Helpers',
-   'group-helper-member'             => 'helper',
+   'group-helper-member'             => '{{GENDER:$1|helper}}',
    'grouppage-helper'                => '{{ns:help}}:Helpers',
 
    'group-helper-global'             => 'Global Helpers',
-   'group-helper-global-member'      => 'global helper',
+   'group-helper-global-member'      => '{{GENDER:$1|global helper}}',
    'grouppage-helper-global'         => '{{ns:help}}:Global Helpers',
 
    'group-ipblock-exempt'            => 'IP block exemptions',
-   'group-ipblock-exempt-member'     => 'ip block exemption',
+   'group-ipblock-exempt-member'     => '{{GENDER:$1|ip block exemption}}',
    'grouppage-ipblock-exempt'        => '{{ns:help}}:IP block exemptions',
 
    'group-ipblock-exempt-g'          => 'Global IP block exemptions',
-   'group-ipblock-exempt-g-member'   => 'global ip block exemption',
+   'group-ipblock-exempt-g-member'   => '{{GENDER:$1|global ip block exemption}}',
    'grouppage-ipblock-exempt-g'      => '{{ns:help}}:Global IP block exemptions',
 
    'group-rollback-global'           => 'Global Rollbackers',
-   'group-rollback-global-member'    => 'global rollbacker',
+   'group-rollback-global-member'    => '{{GENDER:$1|global rollbacker}}',
    'grouppage-rollback-global'       => '{{ns:help}}:Global Rollbackers',
 
    'group-steward'                   => 'Stewards',
-   'group-steward-member'            => 'steward',
+   'group-steward-member'            => '{{GENDER:$1|steward|stewardess}}',
    'grouppage-steward'               => '{{ns:help}}:Stewards',
 
    'group-steward-global'            => 'Global Stewards',
-   'group-steward-global-member'     => 'global steward',
+   'group-steward-global-member'     => '{{GENDER:$1|global steward|global stewardess}}',
    'grouppage-steward-global'        => '{{ns:help}}:Global Stewards',
 
    'group-sysadmin'                  => 'System administrators',
-   'group-sysadmin-member'           => 'system administrator',
+   'group-sysadmin-member'           => '{{GENDER:$1|system administrator}}',
    'grouppage-sysadmin'              => '{{ns:help}}:System administrators',
 
    'group-sysadmin-global'           => 'Global System administrators',
-   'group-sysadmin-global-member'    => 'global system administrator',
+   'group-sysadmin-global-member'    => '{{GENDER:$1|global system administrator}}',
    'grouppage-sysadmin-global'       => '{{ns:help}}:Global System administrators',
 
    'group-sysop-global'              => 'Global Administrators',
-   'group-sysop-global-member'       => 'global administrator',
+   'group-sysop-global-member'       => '{{GENDER:$1|global administrator}}',
    'grouppage-sysop-global'          => '{{ns:help}}:Global Administrators',
 );
 
@@ -75,54 +75,55 @@ $messages['en'] = array(
  */
 $messages['de'] = array(
    'group-bot-global'                => 'Globale Bots',
-   'group-bot-global-member'         => 'Globaler Bot',
+   'group-bot-global-member'         => '{{GENDER:$1|Globaler Bot}}',
    'grouppage-bot-global'            => '{{ns:help}}:Globale Bots',
 
    'group-communitymanager'          => 'Community Manager',
-   'group-communitymanager-member'   => 'Community Manager',
+   'group-communitymanager-member'   => '{{GENDER:$1|Community Manager|Community Managerin}}',
    'grouppage-communitymanager'      => '{{ns:help}}:Community Manager',
 
    'group-checkuser-global'          => 'Globale Checkuser',
-   'group-checkuser-global-member'   => 'Globaler Checkuser',
+   'group-checkuser-global-member'   => '{{GENDER:$1|Globaler Checkuser|Globale Checkuserin}}',
    'grouppage-checkuser-global'      => '{{ns:help}}:Globale Checkuser',
 
    'group-helper'                    => 'Helfer',
-   'group-helper-member'             => 'Helfer',
+   'group-helper-member'             => '{{GENDER:$1|Helfer|Helferin}}',
    'grouppage-helper'                => '{{ns:help}}:Helfer',
 
    'group-helper-global'             => 'Globale Helfer',
-   'group-helper-global-member'      => 'Globaler Helfer',
+   'group-helper-global-member'      => '{{GENDER:$1|Globaler Helfer|Globale Helferin}}',
    'grouppage-helper-global'         => '{{ns:help}}:Globale Helfer',
 
    'group-ipblock-exempt'            => 'IP-Sperre-Ausnahmen',
-   'group-ipblock-exempt-member'     => 'IP-Sperre-Ausnahme',
+   'group-ipblock-exempt-member'     => '{{GENDER:$1|IP-Sperre-Ausnahme}}',
    'grouppage-ipblock-exempt'        => '{{ns:help}}:IP-Sperre-Ausnahmen',
 
    'group-ipblock-exempt-g'          => 'Globale IP-Sperre-Ausnahmen',
-   'group-ipblock-exempt-g-member'   => 'Globale IP-Sperre-Ausnahme',
+   'group-ipblock-exempt-g-member'   => '{{GENDER:$1|Globale IP-Sperre-Ausnahme}}',
    'grouppage-ipblock-exempt-g'      => '{{ns:help}}:Globale IP-Sperre-Ausnahmen',
 
    'group-rollback-global'           => 'Globale Zurücksetzer',
-   'group-rollback-global-member'    => 'Globaler Zurücksetzer',
+   'group-rollback-global-member'    => '{{GENDER:$1|Globaler Zurücksetzer|Globale Zurücksetzerin}}',
    'grouppage-rollback-global'       => '{{ns:help}}:Globale Zurücksetzer',
 
    'group-steward'                   => 'Stewards',
-   'group-steward-member'            => 'Steward',
+   'group-steward-member'            => '{{GENDER:$1|Steward|Stewardess}}',
    'grouppage-steward'               => '{{ns:help}}:Stewards',
 
    'group-steward-global'            => 'Globale Stewards',
-   'group-steward-global-member'     => 'Globaler Steward',
+   'group-steward-global-member'     => '{{GENDER:$1|Globaler Steward|Globale Stewardess}}',
    'grouppage-steward-global'        => '{{ns:help}}:Globale Stewards',
 
    'group-sysadmin'                  => 'Systemadministratoren',
-   'group-sysadmin-member'           => 'Systemadministrator',
+   'group-sysadmin-member'           => '{{GENDER:$1|Systemadministrator|Systemadministratorin}}',
    'grouppage-sysadmin'              => '{{ns:help}}:Systemadministratoren',
 
    'group-sysadmin-global'           => 'Globale Systemadministratoren',
-   'group-sysadmin-global-member'    => 'Globaler Systemadministrator',
+   'group-sysadmin-global-member'    => '{{GENDER:$1|Globaler Systemadministrator|Globale Systemadministratorin}}',
    'grouppage-sysadmin-global'       => '{{ns:help}}:Globale Systemadministratoren',
 
    'group-sysop-global'              => 'Globale Administratoren',
-   'group-sysop-global-member'       => 'Globaler Administrator',
+   'group-sysop-global-member'       => '{{GENDER:$1|Globaler Administrator|Globale Administratorin}}',
    'grouppage-sysop-global'          => '{{ns:help}}:Globale Administratoren',
 );
+
