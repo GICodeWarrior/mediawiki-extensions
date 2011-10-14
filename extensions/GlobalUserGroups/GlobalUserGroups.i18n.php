80c16b72c246417546b2e161500fc73d89815cff
cervidae <cervidae@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-14 15:37:30 +0000

    Adding GlobalUserGroups extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99773 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/GlobalUserGroups/GlobalUserGroups.i18n.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Internationalization file for the GlobalUserGroups extension
+ *
+ * @since 1.0
+ *
+ * @file GlobalUserGroups.i18n.php
+ * @ingroup GlobalUserGroups
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @author Tim Weyer (SVG) <svg@tim-weyer.org>
+ */
+
+$messages = array();
+
+/** English
+ * @author SVG
+ */
+$messages['en'] = array(
+	'globalusergroups-desc' => 'Allows the use of defined local user groups as global groups',
+);
+
+/** German (Deutsch)
+ * @author SVG
+ */
+$messages['de'] = array(
+	'globalusergroups-desc' => 'Ermöglicht das Nutzen von definierten lokalen Benutzergruppen als globale Gruppen',
+);
