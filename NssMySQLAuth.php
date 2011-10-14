16a9a0fce654dd01a2aa5ae3b1165ac23d68c9de
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-14 21:30:16 +0000

    Fix more comments
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99815 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- NssMySQLAuth.php
+++ NssMySQLAuth.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * A plugin to authenticate against a libnss-mysql database
  *
  * Copyright 2008 - Bryan Tong Minh / Delft Aerospace Rocket Engineering
@@ -10,7 +10,7 @@
  */
 
  ### READ BEFORE USING ###
-/*
+/**
  * This plugin allows authentication against an libnss-mysql database and thus
  * allows the use of the same login for MediaWiki as for shell.
  *
