5bea49db0e4a421de5d889466452d89922b514ec
smoke3723 <smoke3723@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-15 03:10:38 +0000

    Adding Sternograph extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99842 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Sternograph/Sternograph.php
@@ -0,0 +1,20 @@
+<?php
+$wgHooks['ParserFirstCallInit'][] = 'efSternograph';
+$wgExtensionMessagesFiles['Sternograph'] = dirname( __FILE__ ) . "/Sternograph.i18n.php";
+$wgAutoloadClasses['Sternograph'] = dirname( __FILE__ ) . "/Sternograph.body.php";
+
+$wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'Sternograph',
+	'version' => '0.0',
+	'author' =>'Smoke 003723', 
+	'url' => 'http://www.mediawiki.org/wiki/Extension:Sternograph', 
+	'descriptionmsg' => 'descriptionmsg'
+       );
+ 
+function efSternograph(){
+	new Sternograph;
+	return true;
+}
+
+
