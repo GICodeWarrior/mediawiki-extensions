32abe37d0c037cc412ca3a5733ba39b5f3e4217b
smoke3723 <smoke3723@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-15 03:10:38 +0000

    Adding Sternograph extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99842 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Sternograph.i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Internationalisation file for Sternograph extension.
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'descriptionmsg'=>'&lt;sterno> tags for transcripts of spokens words (plays, interviews, etc)',
+	'empty' => '&lt;sterno> tag cannot be empty',
+	'nested'=>'&lt;sterno> tag cannot be nested',
+	'speakerIs' => '&lt;sterno> speaker block lines must have '.Sternograph::SPEAKER_IS.' character',
+
+	'speakerPre'=>'',
+	'speakerPost'=>':&nbsp;',
+	'blockPre'=>'',
+	'blockPost'=>'',
+	'inlinePre'=>'[',
+	'inlinePost'=>']',
+	'contextPre'=>'&mdash;',
+	'contextPost'=>''
+);
