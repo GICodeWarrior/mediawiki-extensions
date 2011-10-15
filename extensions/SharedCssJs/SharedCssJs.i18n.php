3feda404d4f278cac6c20a2c6b5c12b5b0f01cb8
cervidae <cervidae@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-15 15:42:28 +0000

    Adding SharedCssJs extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99887 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SharedCssJs/SharedCssJs.i18n.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Localisation file for the SharedCssJs extension
+ *
+ * @since 1.0
+ *
+ * @file SharedCssJs.i18n.php
+ * @ingroup SharedCssJs
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @author Tim Weyer (SVG) <svg@tim-weyer.org>
+ */
+
+$messages = array();
+
+/** English
+ * @author SVG
+ */
+$messages['en'] = array(
+	'sharedcssjs-desc'    => 'Enables fetching of global wiki and user CSS and JS from central wiki',
+	'sharedcssjs-error'   => "This page is included from central wiki by another way and can only editing there.",
+	'global.css'          => "/* CSS placed here will be applied to all skins on all wikis of the wiki farm */",
+	'global.js'           => "/* JavaScript placed here will be applied to all skins on all wikis of the wiki farm */",
+);
+
+/** German (Deutsch)
+ * @author SVG
+ */
+$messages['de'] = array(
+	'sharedcssjs-desc'    => 'Ermöglicht das Nutzen von globalen Wiki- und Benutzer-CSS und JS-Dateien aus dem Zentral Wiki',
+	'sharedcssjs-error'   => 'Diese Seite ist aus dem Zentral Wiki auf einem anderen Weg eingebunden und kann auch nur dort bearbeitet werden.',
+	'global.css'          => "/* Das folgende CSS wird für alle Benutzeroberflächen auf allen Wikis der Wiki-Farm geladen */",
+	'global.js'           => "/* Das folgende JavaScript wird für alle Benutzeroberflächen auf allen Wikis der Wiki-Farm geladen */",
+);
