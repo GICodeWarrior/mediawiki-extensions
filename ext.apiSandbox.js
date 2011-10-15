fc8d705b55c98089ac865b491a3fe970e267cada
maxsem <maxsem@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-15 17:05:09 +0000

    Major rewrite, added support for generators, pageset and other tasty stuf
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@99891 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ext.apiSandbox.js
+++ ext.apiSandbox.js
@@ -1,4 +1,139 @@
 jQuery( function( $ ) {
+	/**
+	 * Class that creates inputs for a query and builds request data
+	 *
+	 * Constructor
+	 * @param $container {jQuery} Container to put UI into
+	 * @param info {Object} Query information
+	 * @param prefix {String} Additional prefix for parameter names
+	 */
+	function UiBuilder( $container, info, prefix ) {
+		this.$container = $container;
+		this.info = info;
+		this.prefix = prefix + info.prefix;
+
+		this.createInputs();
+	}
+
+	UiBuilder.prototype = {
+		/**
+		 * Creates inputs and places them into container
+		 */
+		createInputs: function() {
+			var s = '<table class="api-sandbox-options">\n<tbody>';
+			for ( var i = 0; i < this.info.parameters.length; i++ ) {
+				var param = this.info.parameters[i],
+					name = this.prefix + param.name;
+
+				s += '<tr><td class="api-sandbox-label"><label for="param-' + name + '">' + name + '=</label></td>'
+					+ '<td class="api-sandbox-value">' + this.input( param, name )
+					+ '</td><td>' + smartEscape( param.description ) + '</td></tr>';
+			}
+			s += '\n</tbody>\n</table>\n';
+			this.$container.html( s );
+			this.$container.show();
+		},
+
+		/**
+		 * Adds module help to a container
+		 * @param $container {jQuery} Container to  use
+		 */
+		setHelp: function( $container ) {
+			var desc = smartEscape( this.info.description );
+			if ( isset( this.info.helpurls ) && isset( this.info.helpurls[0] ) && this.info.helpurls[0] ) {
+				desc = desc.replace( /^([^\r\n\.]*)/, 
+					'<a target="_blank" href="' + mw.html.escape( this.info.helpurls[0] ) + '">$1</a>'
+				);
+			}
+			$container.html( desc );
+		},
+
+		input: function( param, name ) {
+			var s,
+				value = '';
+			switch ( param.type ) {
+				case 'limit':
+					value = 10;
+				case 'user':
+				case 'timestamp':
+				case 'integer':
+				case 'string':
+					s = '<input class="api-sandbox-input" id="param-' + name + '" value="' + value + '"/>';
+					break;
+				case 'bool':
+					param.type = 'boolean'; // normalisation for later use
+				case 'boolean':
+					s = '<input id="param-' + name + '" type="checkbox"/>';
+					break;
+				case 'namespace':
+					param.type = namespaces;
+				default:
+					if ( typeof param.type == 'object' ) {
+						var id = 'param-' + name,
+							attributes = { 'id': id };
+						if ( isset( param.multi ) ) {
+							attributes.multiple = 'multiple';
+							s = this.select( param.type, attributes, false );
+						} else {
+							s = this.select( param.type, attributes, true );
+						}
+					} else {
+						s = mw.html.element( 'code', [], mw.msg( 'parentheses', param.type ) );
+					}
+			}
+			return s;
+		},
+
+		select: function( values, attributes, selected ) {
+			attributes['class'] = 'api-sandbox-input';
+			if ( isset( attributes.multiple ) ) {
+				attributes['size'] = Math.min( values.length, 10 );
+			}
+			var s = '';
+			if ( typeof selected != 'array' ) {
+				if ( selected ) {
+					s += mw.html.element( 'option', { value: '', selected: 'selected' }, mw.msg( 'apisb-select-value' ) );
+				}
+				selected = [];
+			}
+			for ( var i = 0; i < values.length; i++ ) {
+				var value = typeof values[i] == 'object' ? values[i].key : values[i],
+					face = typeof values[i] == 'object' ? values[i].value : values[i],
+					attrs = { 'value': value };
+				if ( $.inArray( value, selected ) >= 0 ) {
+					attrs.selected = 'selected';
+				}
+				s += '\n' + mw.html.element( 'option', attrs, face );
+			}
+			s = mw.html.element( 'select', attributes, new mw.html.Raw( s ) );
+			return s;
+		},
+
+		getRequestData: function() {
+			var params = '';
+			for ( var i = 0; i < this.info.parameters.length; i++ ) {
+				var param = this.info.parameters[i],
+					name = this.prefix + param.name,
+					$node = $( '#param-' + name );
+				if ( param.type == 'boolean' ) {
+					if ( $node.is( ':checked' ) ) {
+						params += '&' + name;
+					}
+				} else {
+					var value = $node.val();
+					if ( !isset( value ) || value == '' || value == null ) {
+						continue;
+					}
+					if ( $.isArray( value ) ) {
+						value = value.join( '|' );
+					}
+					params += '&' + name + '=' + encodeURIComponent( value );
+				}
+			}
+			return params;
+		}
+	} // end of UiBuilder.prototype
+
 	var $content = $( '#api-sandbox-content' );
 	if ( !$content.length ) {
 		return;
@@ -11,18 +146,23 @@ jQuery( function( $ ) {
 	    $query = $( '#api-sandbox-query' ),
 	    $queryRow = $( '#api-sandbox-query-row' ),
 	    $help = $( '#api-sandbox-help' ),
-	    $further = $( '#api-sandbox-further-inputs' ),
+	    $mainContainer = $( '#api-sandbox-main-inputs' ),
+		$genericContainer, // will be set later
+		$generatorContainer = $( '#api-sandbox-generator-inputs' ),
 	    $submit = $( '#api-sandbox-submit' ),
 	    $requestUrl = $( '#api-sandbox-url' ),
 	    $requestPost = $( '#api-sandbox-post' ),
 	    $output = $( '#api-sandbox-output' ),
 	    $postRow = $( '#api-sandbox-post-row' );
 
+	// UiBuilder objects
+	var mainQuery,
+		genericQuery,
+		generatorQuery;
+
 	// cached stuff
-	var actionCache = [],
-	    propCache = [],
-	    namespaces = [],
-	    currentInfo = {};
+	var paramInfo = { modules: {}, querymodules: {} },
+	    namespaces = [];
 
 	// load namespaces
 	$.getJSON( mw.util.wikiScript( 'api' ),
@@ -40,43 +180,58 @@ jQuery( function( $ ) {
 					namespaces.push( { key: id, value: ns } );
 				}
 			} else {
-				showLoadError( $further, 'apisb-namespaces-error' );
+				showLoadError( $mainContainer, 'apisb-namespaces-error' );
 			}
 		}
 	);
 
-	$action.change( updateBasics );
-	$query.change( updateBasics );
+	// load stuff we need from the beginning
+	getParamInfo( { mainmodule: 1, pagesetmodule: 1, modules: 'query' },
+		function() {},
+		function() {
+			paramInfo.mainmodule.parameters.shift(); // remove format
+			paramInfo.mainmodule.parameters.shift(); // ...and action
+			$genericContainer = $( '#api-sandbox-generic-inputs > div' );
+			genericQuery = new UiBuilder( $genericContainer, paramInfo.mainmodule, '' );
+		},
+		function() {}
+	);
+
+	$action.change( updateUI );
+	$query.change( updateUI );
+	
+	$( '#param-generator' ).live( 'change', function() {
+		var generator = $( '#param-generator' ).val();
+		if ( generator == '' ) {
+			$generatorContainer.hide();
+		} else {
+			getParamInfo( { querymodules: generator },
+				function() { showLoading( $generatorContainer ); },
+				function() {
+					generatorQuery = new UiBuilder( $generatorContainer, paramInfo.querymodules[generator], 'g' );
+				},
+				function() {}
+			);
+		}
+	} );
 
 	$submit.click( function() {
-		var url = mw.util.wikiScript( 'api' ) + '?action=' + $action.val(),
-		    info = currentInfo; // in case it changes later
+		var url = mw.util.wikiScript( 'api' ) + '?action=' + $action.val();
 		if ( $action.val() == 'query' ) {
 			url += '&' + $query.val();
 		}
 		url += '&format=' + $format.val();
-		var params = '';
-		for ( var i = 0; i < info.parameters.length; i++ ) {
-			var param = info.parameters[i],
-			    name = info.prefix + param.name,
-			    $node = $( '#param-' + name );
-			if ( param.type == 'boolean' ) {
-				if ( $node.is( ':checked' ) ) {
-					params += '&' + name;
-				}
-			} else {
-				var value = $node.val();
-				if ( !isset( value ) || value == '' || value == null ) {
-					continue;
-				}
-				if ( $.isArray( value ) ) {
-					value = value.join( '|' );
-				}
-				params += '&' + name + '=' + encodeURIComponent( value );
-			}
+
+		var params = mainQuery.getRequestData(),
+		    mustBePosted = isset( mainQuery.info.mustbeposted );
+		
+		params += genericQuery.getRequestData();
+		if ( $( '#param-generator' ).val() != '' ) {
+			params += generatorQuery.getRequestData();
 		}
+
 		showLoading( $output );
-		if ( isset( info.mustbeposted ) ) {
+		if ( mustBePosted ) {
 			$requestUrl.val( url );
 			$requestPost.val( params );
 			$postRow.show();
@@ -89,7 +244,7 @@ jQuery( function( $ ) {
 			url: url,
 			data: params,
 			dataType: 'text',
-			type: isset( info.mustbeposted ) ? 'POST' : 'GET',
+			type: mustBePosted ? 'POST' : 'GET',
 			success: function( data ) {
 				var match = data.match( /<pre>[\s\S]*<\/pre>/ );
 				if ( $.isArray( match ) ) {
@@ -114,72 +269,118 @@ jQuery( function( $ ) {
 		return typeof x != 'undefined';
 	}
 
-	function showLoading( element ) {
-		element.html(
+	/**
+	 * Merges several objects into one that gets returned
+	 */
+	function merge( /* ... */ ) {
+		var res = {};
+		for ( var i = 0; i < arguments.length; i++ ) {
+			$.extend( res, arguments[i] );
+		}
+		return res;
+	}
+
+	function showLoading( $element ) {
+		$element.html(
 			mw.html.element( 'img', 
 				{ src: mw.config.get( 'stylepath' ) + '/common/images/spinner.gif', alt: '' } )
 			+ mw.html.escape( mw.msg( 'apisb-loading' ) ) );
 	}
 
-	function showLoadError( element, message ) {
-		element.html( mw.html.element( 'span', { 'class': 'error' }, mw.msg( message ) ) );
+	function showLoadError( $element, message ) {
+		$element.html( mw.html.element( 'span', { 'class': 'error' }, mw.msg( message ) ) );
 	}
 
-	function parseParamInfo( data ) {
-		$further.text( '' );
-		if ( !isset( data.paraminfo ) 
-			|| ( !isset( data.paraminfo.modules ) && !isset( data.paraminfo.querymodules ) )
-			)
-		{
-			showLoadError( $further, 'apisb-load-error' );
-			return;
+	function getParamInfo( what, loadCallback, completeCallback, errorCallback ) {
+		var needed = {};
+		for ( var param in what ) {
+			if ( !isset( paramInfo[param] ) ) {
+				needed[param] = what[param];
+			} else if (typeof needed[param] == 'object' ) {
+				for ( var subParam in param ) {
+					if ( !isset( paramInfo[param][subParam] ) ) {
+						needed[param][subParam] = what[param][subParam];
+					}
+				}
+			} else {
+				needed[param] = what[param];
+			}
 		}
-		if ( isset( data.paraminfo.modules ) ) {
-			actionCache[data.paraminfo.modules[0].name] = data.paraminfo.modules[0];
-			createInputs( actionCache[data.paraminfo.modules[0].name] );
+		if ( $.isEmptyObject( needed ) ) {
+			completeCallback();
 		} else {
-			propCache[data.paraminfo.querymodules[0].name] = data.paraminfo.querymodules[0];
-			createInputs( propCache[data.paraminfo.querymodules[0].name] );
+			loadCallback();
+			needed.format = 'json';
+			needed.action = 'paraminfo';
+			$.getJSON(
+				mw.util.wikiScript( 'api' ),
+				needed,
+				function( data ) {
+					if ( data.error || !data.paraminfo ) {
+						errorCallback();
+						return;
+					}
+					for ( var prop in data.paraminfo ) {
+						if ( !isset( paramInfo[prop] ) ) {
+							paramInfo[prop] = data.paraminfo[prop];
+						} else {
+							for ( var i = 0; i < data.paraminfo[prop].length; i++ ) {
+								var info = data.paraminfo[prop][i];
+								if ( !paramInfo[prop][info.name] ) {
+									paramInfo[prop][info.name] = info;
+								}
+							}
+						}
+					}
+					completeCallback();
+				}
+			).error( errorCallback );
 		}
-		$submit.removeAttr( 'disabled' );
 	}
 
-	function getQueryInfo( action, query ) {
+	function updateQueryInfo( action, query ) {
 		var isQuery = action == 'query';
 		if ( action == '-' || ( isQuery && query == '-' ) ) {
 			$submit.attr( 'disabled', 'disabled' );
 			return;
 		}
 		query = query.replace( /^.*=/, '' );
-		var cached;
-		if ( isQuery ) {
-			cached = propCache[query];
+		//if ( typeof cached != 'object' ) { // stupid FF adds watch() everywhere
+		data = {};
+		if (isQuery ) {
+			data.querymodules = query;
 		} else {
-			cached = actionCache[action];
+			data.modules = action;
 		}
-		if ( typeof cached != 'object' ) { // stupid FF adds watch() everywhere
-			showLoading( $further );
-			var data = {
-				format: 'json',
-				action: 'paraminfo'
-			};
-			if (isQuery ) {
-				data.querymodules = query;
-			} else {
-				data.modules = action;
+		getParamInfo( data,
+			function() {
+				showLoading( $mainContainer );
+				$submit.attr( 'disabled', 'disabled' );
+			},
+			function() {
+				var info;
+				if ( isQuery ) {
+					info = merge( paramInfo.querymodules[query], paramInfo.modules.query, paramInfo.pagesetmodule );
+				} else {
+					info = paramInfo.modules[action];
+				}
+				mainQuery = new UiBuilder( $mainContainer, info, '' );
+				mainQuery.setHelp( $help );
+				$submit.removeAttr( 'disabled' );
+			},
+			function() {
+				$submit.removeAttr( 'disabled' );
+				showLoadError( $mainContainer, 'apisb-load-error' );
 			}
-			$submit.attr( 'disabled', 'disabled' );
-			$.getJSON(
-				mw.config.get( 'wgScriptPath' ) + '/api' + mw.config.get( 'wgScriptExtension' ),
-				data,
-				parseParamInfo
-			);
-		} else {
-			$submit.removeAttr( 'disabled' );
-			createInputs( cached );
-		}
+		);
 	}
 
+	/**
+	 * HTML-escapes and pretty-formats an API description string
+	 *
+	 * @param s {String} String to escape
+	 * @return {String}
+	 */
 	function smartEscape( s ) {
 		s = mw.html.escape( s );
 		if ( s.indexOf( '\n ' ) >= 0 ) {
@@ -190,101 +391,24 @@ jQuery( function( $ ) {
 		return s;
 	}
 
-	function createInputs( info ) {
-		currentInfo = info;
-		var desc = smartEscape( info.description );
-		if ( isset( info.helpurls ) && isset( info.helpurls[0] ) && info.helpurls[0] ) {
-			desc = desc.replace( /^([^\r\n\.]*)/, 
-				'<a target="_blank" href="' + mw.html.escape( info.helpurls[0] ) + '">$1</a>'
-			);
-		}
-		$help.html( desc );
-		var s = '<table class="api-sandbox-options">\n<tbody>';
-		for ( var i = 0; i < info.parameters.length; i++ ) {
-			var param = info.parameters[i],
-			    name = info.prefix + param.name;
-
-			s += '<tr><td class="api-sandbox-label"><label for="param-' + name + '">' + name + '=</label></td>'
-				+ '<td class="api-sandbox-value">' + input( param, name )
-				+ '</td><td>' + smartEscape( param.description ) + '</td></tr>';
-		}
-		s += '\n</tbody>\n</table>\n';
-		$further.html( s );
-	}
-
-	function input( param, name ) {
-		var s,
-		    value = '';
-		switch ( param.type ) {
-			case 'limit':
-				value = 10;
-			case 'user':
-			case 'timestamp':
-			case 'integer':
-			case 'string':
-				s = '<input class="api-sandbox-input" id="param-' + name + '" value="' + value + '"/>';
-				break;
-			case 'bool':
-				param.type = 'boolean'; // normalisation for later use
-			case 'boolean':
-				s = '<input id="param-' + name + '" type="checkbox"/>';
-				break;
-			case 'namespace':
-				param.type = namespaces;
-			default:
-				if ( typeof param.type == 'object' ) {
-					var id = 'param-' + name,
-					    attributes = { 'id': id };
-					if ( isset( param.multi ) ) {
-						attributes.multiple = 'multiple';
-						s = select( param.type, attributes, false );
-					} else {
-						s = select( param.type, attributes, true );
-					}
-				} else {
-                    s = mw.html.element( 'code', [], mw.msg( 'parentheses', param.type ) );
-                }
-		}
-		return s;
-	}
-
-	function select( values, attributes, selected ) {
-		attributes['class'] = 'api-sandbox-input';
-		if ( isset( attributes.multiple ) ) {
-			attributes['size'] = Math.min( values.length, 10 );
-		}
-		var s = '';
-		if ( typeof selected != 'array' ) {
-			if ( selected ) {
-				s += mw.html.element( 'option', { value: '', selected: 'selected' }, mw.msg( 'apisb-select-value' ) );
-			}
-			selected = [];
-		}
-		for ( var i = 0; i < values.length; i++ ) {
-			var value = typeof values[i] == 'object' ? values[i].key : values[i],
-			    face = typeof values[i] == 'object' ? values[i].value : values[i],
-			    attrs = { 'value': value };
-			if ( $.inArray( value, selected ) >= 0 ) {
-				attrs.selected = 'selected';
-			}
-			s += '\n' + mw.html.element( 'option', attrs, face );
-		}
-		s = mw.html.element( 'select', attributes, new mw.html.Raw( s ) );
-		return s;
-	}
-	
-	function updateBasics() {
+	/**
+	 * Updates UI after basic query parameters have been changed
+	 */
+	function updateUI() {
 		var a = $action.val(),
 		    q = $query.val(),
 		    isQuery = a == 'query';
 		if ( isQuery ) {
 			$queryRow.show();
+			$( '#api-sandbox-generator-parameters' ).show();
 		} else {
 			$queryRow.hide();
+			$( '#api-sandbox-generator-parameters' ).hide();
 		}
-		$further.text( '' );
+		$mainContainer.text( '' );
 		$help.text( '' );
-		getQueryInfo( a, q );
+		updateQueryInfo( a, q );
+		$generatorContainer.hide();
 	}
 
 });
