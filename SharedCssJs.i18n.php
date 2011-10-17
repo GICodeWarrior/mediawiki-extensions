941290fa1e39894b9482f6015b12e2d64a9606d2
cervidae <cervidae@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-17 13:43:32 +0000

    Adding message documentation for Translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100026 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SharedCssJs.i18n.php
+++ SharedCssJs.i18n.php
@@ -17,18 +17,30 @@ $messages = array();
  * @author SVG
  */
 $messages['en'] = array(
-	'sharedcssjs-desc'    => 'Enables fetching of global wiki and user CSS and JS from central wiki',
+	'sharedcssjs-desc'    => "Enables fetching of global wiki and user CSS and JS from central wiki",
 	'sharedcssjs-error'   => "This page is included from central wiki by another way and can only editing there.",
 	'global.css'          => "/* CSS placed here will be applied to all skins on all wikis of the wiki farm */",
 	'global.js'           => "/* JavaScript placed here will be applied to all skins on all wikis of the wiki farm */",
+);
+
+/** Message documentation (Message documentation)
+ * @author SVG
+ */
+$messages['qqq'] = array(
+	'sharedcssjs-desc'    => "{{desc}}",
+	'sharedcssjs-error'   => "{{msg-mw|sharedcssjs-error}}
+
+Similar to {{msg-mw|protectedpagetext}}",
+	'global.css'          => "{{msg-mw|global.css}}",
+	'global.js'           => "{{msg-mw|global.js}}",
 );
 
 /** German (Deutsch)
  * @author SVG
  */
 $messages['de'] = array(
-	'sharedcssjs-desc'    => 'Ermöglicht das Nutzen von globalen Wiki- und Benutzer-CSS und JS-Dateien aus dem Zentral Wiki',
-	'sharedcssjs-error'   => 'Diese Seite ist aus dem Zentral Wiki auf einem anderen Weg eingebunden und kann auch nur dort bearbeitet werden.',
+	'sharedcssjs-desc'    => "Ermöglicht das Nutzen von globalen Wiki- und Benutzer-CSS und JS-Dateien aus dem Zentral Wiki",
+	'sharedcssjs-error'   => "Diese Seite ist aus dem Zentral Wiki auf einem anderen Weg eingebunden und kann auch nur dort bearbeitet werden.",
 	'global.css'          => "/* Das folgende CSS wird für alle Benutzeroberflächen auf allen Wikis der Wiki-Farm geladen */",
 	'global.js'           => "/* Das folgende JavaScript wird für alle Benutzeroberflächen auf allen Wikis der Wiki-Farm geladen */",
 );
