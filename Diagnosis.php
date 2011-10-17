6af0c3bb41e84fd9f980a3cfc0db88a9a181715a
dasch <dasch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-17 17:03:36 +0000

    all tabs improved PHP check
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100053 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Diagnosis.php
+++ Diagnosis.php
@@ -1,20 +1,20 @@
 <?php
 # Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
 if (!defined('MEDIAWIKI')) {
-        echo <<<EOT
+		echo <<<EOT
 To install my extension, put the following line in LocalSettings.php:
 require_once( "\$IP/extensions/Diagnosis/Diagnosis.php" );
 EOT;
-        exit( 1 );
+		exit( 1 );
 }
  
 $wgExtensionCredits['specialpage'][] = array(
 		'path' => __FILE__,
-        'name' => 'Diagnosis',
-        'author' => '[http://www.dasch-tour.de DaSch]',
-        'url' => 'http://www.mediawiki.org/wiki/Extension:Diagnosis',
-        'descriptionmsg' => 'diagnosis-desc',
-        'version' => '0.0.1',
+		'name' => 'Diagnosis',
+		'author' => '[http://www.dasch-tour.de DaSch]',
+		'url' => 'http://www.mediawiki.org/wiki/Extension:Diagnosis',
+		'descriptionmsg' => 'diagnosis-desc',
+		'version' => '0.0.1',
 );
  
 $dir = dirname(__FILE__) . '/';
