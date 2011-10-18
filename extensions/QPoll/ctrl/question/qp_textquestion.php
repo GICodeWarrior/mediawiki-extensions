ad03d3a355442c09df5de16d0a929609c0330e40
questpc <questpc@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-18 07:33:10 +0000

    Fixing edge cases with improperly nested category definitions / templates in question type text. Added message documentation to the most of newly introduced messages.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100109 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/QPoll/ctrl/question/qp_textquestion.php
+++ extensions/QPoll/ctrl/question/qp_textquestion.php
@@ -161,6 +161,24 @@ class qp_TextQuestion extends qp_StubQuestion {
 	# regexp for separation of proposal line tokens
 	static $propCatPattern = null;
 
+	# source "raw" tokens (preg_split)
+	var $rawtokens;
+
+	/**
+	 * array with parsed braces pairs
+	 * every element may have the following keys:
+	 *   'type' : type of brace string _key_values_ of self::$matching_braces
+	 *   'closed_at' : indicates opening brace;
+	 *     false when no matching closing brace was found
+	 *     int  key of $this->rawtokens a "link" to matching closing brace
+	 *   'opened_at' : indicates closing brace;
+	 *     false when no matching opening brace was found
+	 *     int  key of $this->rawtokens a "link" to matching opening brace
+	 *   'iscat'     : indicates brace that belongs to self::$input_braces_types AND
+	 *                 has a proper match (both 'closed_at' and 'opened_at' are int)
+	 */
+	var $brace_matches;
+
 	# $propview is an instance of qp_TextQuestionProposalView
 	#             which contains parsed tokens for combined
 	#             proposal/category view
@@ -258,6 +276,111 @@ class qp_TextQuestion extends qp_StubQuestion {
 	}
 
 	/**
+	 * Builds $this->brace_matches array which contains the list of matching braces
+	 * from $this->rawtokens array.
+	 */
+	private function findMatchingBraces() {
+		$brace_stack = array();
+		$this->brace_matches = array();
+		$matching_closed_brace = '';
+		# building $this->brace_matches
+		foreach ( $this->rawtokens as $tkey => $token ) {
+			if ( array_key_exists( $token, self::$matching_braces ) ) {
+				# opening braces
+				$this->brace_matches[$tkey] = array(
+					'closed_at' => false,
+					'type' => $token
+				);
+				$match = self::$matching_braces[$token];
+				# create new brace_stack element:
+				$last_brace_def = array(
+					'match' => $match,
+					'idx' => $tkey
+				);
+				if ( array_key_exists( $token, self::$input_braces_types ) &&
+						count( $brace_stack ) == 0 ) {
+					# will try to start category definiton (on closing)
+					$matching_closed_brace = $match;
+				}
+				array_push( $brace_stack, $last_brace_def );
+			} elseif ( in_array( $token, self::$matching_braces ) ) {
+				# closing braces
+				$this->brace_matches[$tkey] = array(
+					'opened_at' => false,
+					# we always put opening brace in 'type'
+					'type' => array_search( $token, self::$matching_braces, true )
+				);
+				if ( count( $brace_stack ) > 0 ) {
+					$last_brace_def = array_pop( $brace_stack );
+					if ( $last_brace_def['match'] != $token ) {
+						# braces didn't match
+						array_push( $brace_stack, $last_brace_def );
+						continue;
+					}
+					$idx = $last_brace_def['idx'];
+					# link opening / closing braces to each other
+					$this->brace_matches[$tkey]['opened_at'] = $idx;
+					$this->brace_matches[$idx]['closed_at'] = $tkey;
+					if ( count( $brace_stack ) > 0 || $token !== $matching_closed_brace ) {
+						# brace does not belong to self::$input_braces_types
+						continue;
+					}
+					# stack level 1 and found a matching_closed_brace;
+					# indicate end of category in $this->brace_matches
+					$this->brace_matches[$tkey]['iscat'] = true;
+					# indicate begin of category in $this->brace_matches
+					$this->brace_matches[$idx]['iscat'] = true;
+					# clear match
+					$matching_closed_brace = '';
+				}
+			}
+		}
+		# trying to backtrack non-closed braces only these which belong to
+		# self::$input_braces_types
+		$brace_keys = array_keys( $this->brace_matches, true );
+		for ( $i = count( $brace_keys ) - 1; $i >= 0; $i-- ) {
+			$brace_match = &$this->brace_matches[$brace_keys[$i]];
+			# match non-closed brace which belongs to self::$input_braces_types
+			# (non-closed category definitions)
+			if ( array_key_exists( 'opened_at', $brace_match ) &&
+					$brace_match['opened_at'] === false &&
+					array_key_exists( $brace_match['type'], self::$input_braces_types ) ) {
+				# try to find matching opening brace for current non-closed closing brace
+				for ( $j = $i - 1; $j >= 0; $j-- ) {
+					$checked_brace = &$this->brace_matches[$brace_keys[$j]];
+					if ( array_key_exists( 'iscat', $checked_brace ) ) {
+						# category definitions cannot be nested
+						break;
+					}
+					if ( array_key_exists( 'closed_at', $checked_brace ) ) {
+						# opening brace
+						if ( $checked_brace['closed_at'] === false ) {
+							# opening brace that has no matching closing brace
+							if ( $checked_brace['type'] === $brace_match['type'] ) {
+								# opening brace of the same type that $brace_match have
+								# link $brace_match and $checked_brace to each other:
+								# found matching non-closed opening brace; "link" both to each other
+								$brace_match['opened_at'] = $brace_keys[$j];
+								$brace_match['iscat'] = true;
+								$checked_brace['closed_at'] = $brace_keys[$i];
+								$checked_brace['iscat'] = true;
+								break;
+							}
+						} elseif ( $checked_brace['closed_at'] > $i ) {
+							# found opening brace that is closed at higher position than our
+							# $brace_match has;
+							# cross-closing is not allowed, the following code cannot be
+							# category definition and template at the same time:
+							# "<[ {{ ]> }}"
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
 	 * Creates question view which should be renreded and
 	 * also may be altered during the poll generation
 	 */
@@ -275,44 +398,41 @@ class qp_TextQuestion extends qp_StubQuestion {
 			$this->dbtokens = $brace_stack = array();
 			$catId = 0;
 			$last_brace = '';
-			$tokens = preg_split( self::$propCatPattern, $raw, -1, PREG_SPLIT_DELIM_CAPTURE );
+			$this->rawtokens = preg_split( self::$propCatPattern, $raw, -1, PREG_SPLIT_DELIM_CAPTURE );
 			$matching_closed_brace = '';
-			foreach ( $tokens as $token ) {
-				try {
-					# $toBeStored == true when current $token has to be stored into
-					# category / proposal list (depending on $opt->isCatDef)
-					$toBeStored = true;
-					if ( $token === '|' ) {
-						# parameters separator
-						if ( $opt->isCatDef ) {
-							if ( count( $brace_stack ) == 1 && $brace_stack[0] === $matching_closed_brace ) {
-								# pipe char starts new option only at top brace level,
-								# with matching input brace
-								$opt->addEmptyOption();
-								$toBeStored = false;
-							}
+			$this->findMatchingBraces();
+			foreach ( $this->rawtokens as $tkey => $token ) {
+				# $toBeStored == true when current $token has to be stored into
+				# category / proposal list (depending on $opt->isCatDef)
+				$toBeStored = true;
+				if ( $token === '|' ) {
+					# parameters separator
+					if ( $opt->isCatDef ) {
+						if ( count( $brace_stack ) == 1 && $brace_stack[0] === $matching_closed_brace ) {
+							# pipe char starts new option only at top brace level,
+							# with matching input brace
+							$opt->addEmptyOption();
+							$toBeStored = false;
 						}
-					} elseif ( array_key_exists( $token, self::$matching_braces ) ) {
-						# opening braces
+					}
+				} elseif ( array_key_exists( $tkey, $this->brace_matches ) ) {
+					# brace
+					$brace_match = &$this->brace_matches[$tkey];
+					if ( array_key_exists( 'closed_at', $brace_match ) &&
+							$brace_match['closed_at'] !== false ) {
+						# valid opening brace
 						array_push( $brace_stack, self::$matching_braces[$token] );
-						if ( array_key_exists( $token, self::$input_braces_types ) &&
-								count( $brace_stack ) == 1 ) {
-							# start category definiton
+						if ( array_key_exists( 'iscat', $brace_match ) ) {
+							# start category definition
 							$matching_closed_brace = self::$matching_braces[$token];
 							$opt->startOptionsList( self::$input_braces_types[$token] );
 							$toBeStored = false;
 						}
-					} elseif ( in_array( $token, self::$matching_braces ) ) {
-						# closing braces
-						if ( count( $brace_stack ) > 0 ) {
-							$last_brace = array_pop( $brace_stack );
-							if ( $last_brace != $token ) {
-								array_push( $brace_stack, $last_brace );
-								throw new Exception( 'break' );
-							}
-							if ( count( $brace_stack ) > 0 || $token !== $matching_closed_brace ) {
-								throw new Exception( 'break' );
-							}
+					} elseif ( array_key_exists( 'opened_at', $brace_match ) &&
+						$brace_match['opened_at'] !== false ) {
+						# valid closing brace
+						array_pop( $brace_stack );
+						if ( array_key_exists( 'iscat', $brace_match ) ) {
 							$matching_closed_brace = '';
 							# add new category input options for the storage
 							$this->dbtokens[] = $opt->input_options;
@@ -324,10 +444,6 @@ class qp_TextQuestion extends qp_StubQuestion {
 							$catId++;
 							$toBeStored = false;
 						}
-					}
-				} catch ( Exception $e ) {
-					if ( $e->getMessage() !== 'break' ) {
-						throw new MWException( $e->getMessage() );
 					}
 				}
 				if ( $toBeStored ) {
