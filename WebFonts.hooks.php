493b010247838dee55835019bced5f7a523f2b22
santhosh <santhosh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-18 15:22:13 +0000

    have a configuraiton parameter $wgEnableWebFontsDefault , when set false, dont show the extension for anonymous users. For logged in users, use the user's preference page settings.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100116 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WebFonts.hooks.php
+++ WebFonts.hooks.php
@@ -17,7 +17,37 @@ class WebFontsHooks {
 
 		return true; // Hooks must return value
 	}
+	
+	public static function addConfig( &$vars ) {
+		global $wgUser;
 
+		if ( $wgUser->getOption( 'webfontsDisable' ) ) {
+			wfDebugLog( 'webfonts', 'User disabled WebFonts: '  , true  );
+			// User disabled WebFonts
+			return true;
+		}
+
+		
+		return true;
+	}
+
+	public static function addVariables( &$vars ) {
+		global $wgWebFontsEnabledByDefault, $wgUser;
+
+		if ( $wgUser->getOption( 'webfontsDisable' ) ) {
+			// User disabled WebFonts
+			return true;
+		}
+
+		if ( $wgUser->isAnon() ) {
+			// If user enabled webfonts from preference page, 
+			// wgWebFontsEnabledByDefault is overridden by that.
+			$vars['wgWebFontsEnabledByDefault'] = $wgWebFontsEnabledByDefault;
+		}
+
+		return true;
+	}
+	
 	public static function addPreference( $user, &$preferences ) {
 		// A checkbox in preferences to disable WebFonts
 		$preferences['webfontsDisable'] = array(
