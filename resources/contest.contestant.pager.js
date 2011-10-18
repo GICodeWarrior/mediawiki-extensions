6b255ef0201383154e9f3c0a3310cf99f69890fd
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-18 21:06:23 +0000

    Trim trailing whitespace
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100161 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- resources/contest.contestant.pager.js
+++ resources/contest.contestant.pager.js
@@ -1,19 +1,19 @@
 /**
  * JavasSript for the Contest MediaWiki extension.
  * @see https://www.mediawiki.org/wiki/Extension:Contest
- * 
+ *
  * @licence GNU GPL v3 or later
  * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
  */
 
-(function( $, mw ) { 
-	
+(function( $, mw ) {
+
 	$( document ).ready( function() {
 
 		$( '.contestant-row' ).click( function() {
 			window.location = $( this ).attr( 'data-contestant-target' );
 		} );
-		
+
 	} );
 
 })( window.jQuery, window.mediaWiki );
