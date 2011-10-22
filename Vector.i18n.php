b6cdc5b36e514496f8cea916f4e315b8fa573e13
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-22 20:29:38 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100514 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Vector.i18n.php
+++ Vector.i18n.php
@@ -877,9 +877,16 @@ $messages['ig'] = array(
  * @author Lam-ang
  */
 $messages['ilo'] = array(
+	'vector' => 'Napasayat nga UI iti Vector',
+	'vector-desc' => 'Napasaayat kadagiti "interface elements" ti agar-aramat no agusar iti kudil nga Vector.',
+	'vector-collapsiblenav-preference' => 'Pabalin mo ti pagpabassit kadagita adda dita  "sidebar" ti Vector nga kudil',
 	'vector-collapsiblenav-more' => 'Adu pay nga pagsasao',
+	'vector-editwarning-warning' => 'No pumanaw ka ditoy nga panid baka agpukaw dagiti binalbaliwam.
+No nakastrekka, mabalin mo nga ikkaten daytoy nga ballaag idiay "Pagbaliwan" a benneg kadagiti kaykayatmo.',
+	'vector-editwarning-preference' => 'Pagballaagannak ket no pumanawak iti pabaliwan a panid nga saan nga naidulin ti sinukatak',
 	'vector-simplesearch-search' => 'Biroken',
 	'vector-simplesearch-containing' => 'nagyan na nga...',
+	'vector-noexperiments-preference' => 'Saannak nga iraman kadagiti experinto ti ramramit',
 );
 
 /** Ido (Ido)
