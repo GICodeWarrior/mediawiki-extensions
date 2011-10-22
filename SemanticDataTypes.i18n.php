8ed6f6a441757d6febf43353990609cd5a93bf4f
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-22 00:37:21 +0000

    importing SemanticDataTypes extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100480 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticDataTypes.i18n.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Internationalization file for the Semantic Data Types extension.
+ *
+ * @since 0.1
+ *
+ * @file SemanticDataTypes.i18n.php
+ * @ingroup SDT
+ *
+ * @licence GNU GPL v3+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'sdt-desc' => 'Additional data types for Semantic MediaWiki and extensions',
+
+	'sdt-duration' => 'Duration',
+);
