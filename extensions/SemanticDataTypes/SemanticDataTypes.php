c2a52d73f0d870288bb86a2bd8b95233a7b49c30
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-22 00:46:14 +0000

    added version check
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100481 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SemanticDataTypes/SemanticDataTypes.php
+++ extensions/SemanticDataTypes/SemanticDataTypes.php
@@ -28,6 +28,15 @@ if ( version_compare( $wgVersion, '1.16c', '<' ) ) { // Needs to be 1.16c becaus
 	die( '<b>Error:</b> the Semantic Data Types requires MediaWiki 1.16 or above.' );
 }
 
+// Show a warning if Semantic MediaWiki is not loaded.
+if ( ! defined( 'SMW_VERSION' ) ) {
+	die( '<b>Error:</b> You need to have <a href="http://semantic-mediawiki.org/wiki/Semantic_MediaWiki">Semantic MediaWiki</a> installed in order to use <a href="http://www.mediawiki.org/wiki/Extension:Semantic_Data_Types">Semantic Data Types</a>.<br />' );
+}
+
+if ( version_compare( SMW_VERSION, '1.6 alpha', '<' ) ) {
+	die( '<b>Error:</b> Semantic Data Types requires Semantic MediaWiki 1.6 or above.' );
+}
+
 define( 'SDT_VERSION', '0.1 alpha' );
 
 $wgExtensionCredits['semantic'][] = array(
