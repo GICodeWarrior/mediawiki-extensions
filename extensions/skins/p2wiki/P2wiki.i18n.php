c88468ea34943dda938d75d0c2f63ab1a2a454a7
platonides <platonides@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-23 20:57:57 +0000

    i18n files don't need defined( 'MEDIAWIKI' ) guards.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100563 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/skins/p2wiki/P2wiki.i18n.php
+++ extensions/skins/p2wiki/P2wiki.i18n.php
@@ -7,8 +7,6 @@
  * @author Daniel Friesen
  */
 
-if( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
-
 $messages = array();
 
 /** English
