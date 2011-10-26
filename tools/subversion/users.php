2ca8761635ed80d42671c36e10e108bb4883853f
mah <mah@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-26 02:01:26 +0000

    From Bug #31794 - [Users.php] HTML5 \o/
    Patch from p858snake.
    
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100770 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/subversion/users.php
+++ tools/subversion/users.php
@@ -1,17 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
-<head>
-<title>Wikimedia Subversion user list</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-</head>
-<body>
-<h1>Wikimedia Subversion user list</h1>
-<table border="1">
-<tr>
-<th>Username</th>
-<th>Real name</th>
-<th>Ready for git?</th>
-</tr>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Wikimedia Subversion user list</title>
+	</head>
+
+	<body>
+		<h1>Wikimedia Subversion user list</h1>
+			<table border="1">
+				<tr>
+					<th>Username</th>
+					<th>Real name</th>
+					<th>Ready for git?</th>
+				</tr>
 <?php
 
 $time = microtime( true );
