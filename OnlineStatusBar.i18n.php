6afcc0ba3eb07e9f6433eae5a82ef18e5a45eb7e
petrb <petrb@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-26 19:26:12 +0000

    added messages qqq
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100850 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OnlineStatusBar.i18n.php
+++ OnlineStatusBar.i18n.php
@@ -13,10 +13,25 @@ $messages = array();
  * @author Petr Bena
  */
 $messages['en'] = array(
+	// Description 
 	'onlinestatusbar-desc' => "Status bar which shows whether a user is online, based on preferences, on their user page",
+	// Status bar text line (User is now Offline) etc.
 	'onlinestatusbar-line' => "$1 is now $2 $3",
+	// Message in config asking user if they want to enable it
 	'onlinestatusbar-used' => 'Do you want to let others see if you are online?',
+	// Message in config asking what status they want to use
 	'onlinestatusbar-status' => 'What is the default status you wish to use:',
+	// Title for config
 	'prefs-onlinestatusbar' => 'Online status bar',
+	// Section for config
 	'prefs-onlinestatus' => 'Online Status',
 );
+
+$messages['qqq'] = array(
+	'onlinestatusbar-desc' => '{{desc}}',
+        'onlinestatusbar-line' => 'Status bar text line (User is now Offline) etc.',
+        'onlinestatusbar-used' => 'Message in config asking user if they want to enable it',
+        'onlinestatusbar-status' => 'Message in config asking what status they want to use',
+        'prefs-onlinestatusbar' => 'Title for config',
+        'prefs-onlinestatus' => 'Section for config',
+};
