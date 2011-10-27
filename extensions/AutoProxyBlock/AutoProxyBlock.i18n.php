53e32fe968613c2fad01334bab35058a7f730051
cryptocoryne <cryptocoryne@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-27 13:23:16 +0000

    Followup r100863, some improves and fixes
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100972 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/AutoProxyBlock/AutoProxyBlock.i18n.php
+++ extensions/AutoProxyBlock/AutoProxyBlock.i18n.php
@@ -1,17 +1,22 @@
-﻿<?php
+<?php
 $messages = array();
 
 $messages['en'] = array(
 	'autoproxyblock-desc' => 'Allows to block open proxies from performing actions in wiki',
-	'proxy-blocked' => '$1 is listed as proxy, performed action aborted.',
+	'proxy-blocked' => 'Your IP address is listed as proxy, performed action aborted.',
 	'abusefilter-edit-builder-vars-is-proxy' => 'True if this action was performed through a proxy',
 	'tag-proxy' => 'action through proxy',
 	'right-notagproxychanges' => 'protect from tagging edits through proxies as "proxy"',
+	'right-autoproxyblock-log' => 'view log of automatically blocked changes through proxies',
+	'proxyblock-log-name' => 'Auto proxy block log',
+	'proxyblock-log-header' => 'List of automatically blocked changes through proxies',
+	'proxyblock-logentry' => '',
+	'proxyblock-logentry-blocked' => 'User [[User:$3|$3]]\'s action "$2" on page [[$1]] automatically blocked.',
 );
 
 $messages['ru'] = array(
 	'autoproxyblock-desc' => 'Позволяет запрещать совершать действия в вики c открытых прокси',
-	'proxy-blocked' => '$1 находится в списках прокси, действие отменено.',
+	'proxy-blocked' => 'Ваш IP-адрес находится в списках прокси, действие отменено.',
 	'abusefilter-edit-builder-vars-is-proxy' => 'Истинно, если действие совершено через прокси',
 	'tag-proxy' => 'совершено через прокси',
 	'right-notagproxychanges' => 'правки через прокси не отмечаются меткой',	
