7d14cc68ea37bbf6eba2325c784e02e1344b7e0f
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-27 20:00:45 +0000

    Added more tests for es.DocumentNode
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@101032 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/tests/hype/es.DocumentNode.test.js
+++ parsers/wikidom/tests/hype/es.DocumentNode.test.js
@@ -12,11 +12,17 @@ DocumentNodeStub.prototype.getElementLength = function() {
 };
 
 test( 'es.DocumentNode', function() {
+
+	// Stub test
+
 	strictEqual(
 		( new DocumentNodeStub( [], 'a', 0 ) ).getElementLength(),
 		2,
 		'DocumentNodeStub.getElementLength() returns initialized length plus 2 for elements'
 	);
+
+	// Stubs
+
 	var a = new DocumentNodeStub( [], 'a', 0 ),
 		b = new DocumentNodeStub( [], 'b', 1 ),
 		c = new DocumentNodeStub( [], 'c', 2 ),
@@ -24,6 +30,27 @@ test( 'es.DocumentNode', function() {
 		e = new DocumentNodeStub( [], 'e', 4 ),
 		root1 = new DocumentNodeStub( [a, b, c, d, e], 'root1', 20 ),
 		i;
+
+	// getRangeFromNode tests
+
+	var getRangeFromNodeTests = [
+		{ 'input': a, 'output': new es.Range( 0, 2 ) },
+		{ 'input': b, 'output': new es.Range( 2, 5 ) },
+		{ 'input': c, 'output': new es.Range( 5, 9 ) },
+		{ 'input': d, 'output': new es.Range( 9, 14 ) },
+		{ 'input': e, 'output': new es.Range( 14, 20 ) },
+		{ 'input': null, 'output': null }
+	];
+	
+	for ( i = 0; i < getRangeFromNodeTests.length; i++ ) {
+		deepEqual(
+			root1.getRangeFromNode( getRangeFromNodeTests[i].input ),
+			getRangeFromNodeTests[i].output,
+			'getRangeFromNode returns the correct range or null if item is not found'
+		);
+	}
+
+	// getNodeFromOffset tests
 
 	var getNodeFromOffsetTests = [
 		{ 'input': -1, 'output': null },
@@ -55,6 +82,25 @@ test( 'es.DocumentNode', function() {
 			root1.getNodeFromOffset( getNodeFromOffsetTests[i].input ),
 			getNodeFromOffsetTests[i].output,
 			'getNodeFromOffset finds the right item or returns null when out of range'
+		);
+	}
+
+	// getOffsetFromNode tests
+
+	var getOffsetFromNodeTests = [
+		{ 'input': a, 'output': 0 },
+		{ 'input': b, 'output': 2 },
+		{ 'input': c, 'output': 5 },
+		{ 'input': d, 'output': 9 },
+		{ 'input': e, 'output': 14 },
+		{ 'input': null, 'output': -1 }
+	];
+
+	for ( i = 0; i < getOffsetFromNodeTests.length; i++ ) {
+		strictEqual(
+			root1.getOffsetFromNode( getOffsetFromNodeTests[i].input ),
+			getOffsetFromNodeTests[i].output,
+			'getOffsetFromNode finds the right offset or returns -1 when node is not found'
 		);
 	}
 } );
