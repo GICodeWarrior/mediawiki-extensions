e86230016c45fd2c886504a9e8b9d15203bfe724
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-27 17:38:23 +0000

    Some message tweaks for consistenxy. Preparation for adding to Translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@101004 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/OnlineStatusBar/OnlineStatusBar.i18n.php
+++ extensions/OnlineStatusBar/OnlineStatusBar.i18n.php
@@ -14,15 +14,15 @@ $messages = array();
  */
 $messages['en'] = array(
 	// Description 
-	'onlinestatusbar-desc' => "Status bar which shows whether a user is online, based on preferences, on their user page",
+	'onlinestatusbar-desc' => 'Status bar which shows whether a user is online, based on preferences, on their user page',
 	// Status bar text line (User is now Offline) etc.
-	'onlinestatusbar-line' => "$1 is now $2 $3",
+	'onlinestatusbar-line' => '$1 is now $2 $3',
 	// Message in config asking user if they want to enable it
 	'onlinestatusbar-used' => 'Do you want to let others see if you are online?',
 	// Message in config asking what status they want to use
 	'onlinestatusbar-status' => 'What is the default status you wish to use:',
 	// Section for config
-	'prefs-onlinestatus' => 'Online Status',
+	'prefs-onlinestatus' => 'Online status',
 	// Message in config
 	'onlinestatusbar-hide' => 'Do you want to hide the status bar in order to use just the magic word? (For advanced users)',
 );
@@ -35,3 +35,4 @@ $messages['qqq'] = array(
         'onlinestatusbar-status' => 'Message in config asking what status they want to use, option box',
         'prefs-onlinestatus' => 'Section for config, located in preferences - misc',
 );
+
