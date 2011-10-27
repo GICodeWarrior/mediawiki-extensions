2d85f2024335690022883e5b2274596316006a22
johnduhart <johnduhart@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-27 16:20:48 +0000

    Moved hooks into their own class This undoes something from r100996 but I need to run
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@100997 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OnlineStatusBar.php
+++ OnlineStatusBar.php
@@ -1,9 +1,4 @@
 <?php
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo "This is a part of mediawiki and can't be started separately";
-	die();
-}
-
 /**
  * Insert a special box on user page showing their status.
  *
@@ -13,6 +8,11 @@ if ( !defined( 'MEDIAWIKI' ) ) {
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  * @link http://www.mediawiki.org/wiki/Extension:OnlineStatusBar Documentation
  */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a part of mediawiki and can't be started separately";
+	die();
+}
 
 $wgExtensionCredits[version_compare( $wgVersion, '1.17', '>=' ) ? 'userpage tools' : 'other'][] = array(
 	'path' => __FILE__,
@@ -30,9 +30,10 @@ $wgResourceModules['ext.OnlineStatusBar'] = array (
 	'styles' => 'OnlineStatusBar.css',
 	'localBasePath' => dirname ( __FILE__ ),
 	'remoteExtPath' => 'OnlineStatusBar',
-	);
+);
 
 $wgAutoloadClasses['OnlineStatusBar'] = "$dir/OnlineStatusBar.body.php";
+$wgAutoloadClasses['OnlineStatusBarHooks'] = "$dir/OnlineStatusBarHooks.php";
 
 // Configuration
 // Those values can be overriden in LocalSettings, do not change it here
@@ -69,83 +70,13 @@ $wgOnlineStatusBarDefaultEnabled = false;
 // how long to wait until user is considered as offline
 $wgOnlineStatusBar_LogoutTime = 3600;
 
-$wgHooks['LoadExtensionSchemaUpdates'][] = 'wfOnlineStatusBar_CkSchema';
-function wfOnlineStatusBar_CkSchema( $updater = null ) {
-	if ( $updater !== null ){
-		$updater->addExtensionUpdate( array( 'addtable', 'online_status', dirname( __FILE__ ) . '/OnlineStatusBar.sql', true ) );
-	} else {
-		global $wgExtNewTables;
-		$wgExtNewTables[] = array(
-			'online_status', dirname( __FILE__ ) . '/OnlineStatusBar.sql' );
-	}
-	return true;
-}
-
-$wgHooks['UserLogoutComplete'][] = 'wfOnlineStatusBar_Logout';
-function wfOnlineStatusBar_Logout( &$user, &$inject_html, $old_name ) {
-	OnlineStatusBar::DeleteStatus( $old_name );
-	return true;
-}
-
-$wgHooks['ArticleViewHeader'][] = 'wfOnlineStatusBar_RenderBar';
-function wfOnlineStatusBar_RenderBar( &$article, &$outputDone, &$pcache ) {
-	global $wgOnlineStatusBar_Template, $messages, $wgOnlineStatusBarModes, $wgOut;
-	OnlineStatusBar::UpdateStatus();
-	$ns = $article->getTitle()->getNamespace();
-	if ( ( $ns == NS_USER_TALK ) || ( $ns == NS_USER ) ) {
-		$user = OnlineStatusBar::GetOwnerFromTitle ( $article->getTitle() );
-		if ( $user == null ) {
-			return true;
-		}
-		$username = $user->getName();
-		if ( $user->getOption ( "OnlineStatusBar_hide" ) == true ) {
-			return true;
-		}
-		if ( OnlineStatusBar::IsValid( $username ) ) {
-			$mode = OnlineStatusBar::GetStatus( $username );
-			$modetext = $wgOnlineStatusBarModes[$mode];
-			$image = OnlineStatusBar::getImageHtml( $mode );
-			$text = wfMessage( 'onlinestatusbar-line', $username )->rawParams( $image )->params( $modetext )->escaped();
-			$wgOut->addHtml( OnlineStatusBar::Get_Html( $text, $mode ) );
-		}
-	}
-	return true;
-}
-
-$wgHooks['UserLoginComplete'][] = 'wfOnlineStatusBar_UpdateStatus';
-function wfOnlineStatusBar_UpdateStatus() {
-	OnlineStatusBar::UpdateDb();
-	return true;
-}
-
-$wgHooks['GetPreferences'][] = 'wfOnlineStatusBar_PreferencesHook';
-function wfOnlineStatusBar_PreferencesHook( $user, &$preferences ) {
-	global $wgOnlineStatusBarDefaultOnline, $wgOnlineStatusBarDefaultEnabled, $wgOnlineStatusBarModes;
-	$preferences['OnlineStatusBar_active'] = array( 'type' => 'toggle', 'label-message' => 'onlinestatusbar-used', 'section' => 'misc/onlinestatus' );
-	$preferences['OnlineStatusBar_hide'] = array( 'type' => 'toggle', 'label-message' => 'onlinestatusbar-hide', 'section' => 'misc/onlinestatus' );
-	$preferences['OnlineStatusBar_status'] = array( 'type' => 'radio', 'label-message' => 'onlinestatusbar-status', 'section' => 'misc/onlinestatus',
-		'options' => array(
-			$wgOnlineStatusBarModes['online'] => 'online',
-			$wgOnlineStatusBarModes['busy'] => 'busy',
-			$wgOnlineStatusBarModes['away'] => 'away',
-			$wgOnlineStatusBarModes['hidden'] => 'hidden'
-		),
-	);
-	return true;
-}
-
-$wgHooks['UserGetDefaultOptions'][] = 'wfOnlineStatusBar_SetDefaultOptions';
-function wfOnlineStatusBar_SetDefaultOptions( &$defaultOptions ) {
-	global $wgOnlineStatusBarDefaultOnline, $wgOnlineStatusBarDefaultEnabled;
-	// set defaults
-	$defaultOptions['OnlineStatusBar_status'] = $wgOnlineStatusBarDefaultOnline;
-	$defaultOptions['OnlineStatusBar_active'] = $wgOnlineStatusBarDefaultEnabled;
-	$defaultOptions['OnlineStatusBar_hide'] = false;
-	// quit
-	return true;	
-}
-
-$wgHooks['LanguageGetMagic'][] = 'OnlineStatusBar::MagicWordVar';
-$wgHooks['BeforePageDisplay'][] = 'OnlineStatusBar::StylePage';
-$wgHooks['MagicWordwgVariableIDs'][] = 'OnlineStatusBar::MagicWordSet';
-$wgHooks['ParserGetVariableValueSwitch'][] = 'OnlineStatusBar::ParserGetVariable';
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'OnlineStatusBarHooks::ckSchema';
+$wgHooks['UserLogoutComplete'][] = 'OnlineStatusBarHooks::logout';
+$wgHooks['ArticleViewHeader'][] = 'OnlineStatusBarHooks::renderBar';
+$wgHooks['UserLoginComplete'][] = 'OnlineStatusBarHooks::updateStatus';
+$wgHooks['GetPreferences'][] = 'OnlineStatusBarHooks::preferencesHook';
+$wgHooks['UserGetDefaultOptions'][] = 'OnlineStatusBarHooks::setDefaultOptions';
+$wgHooks['LanguageGetMagic'][] = 'OnlineStatusBarHooks::magicWordVar';
+$wgHooks['BeforePageDisplay'][] = 'OnlineStatusBarHooks::stylePage';
+$wgHooks['MagicWordwgVariableIDs'][] = 'OnlineStatusBarHooks::magicWordSet';
+$wgHooks['ParserGetVariableValueSwitch'][] = 'OnlineStatusBarHooks::parserGetVariable';
