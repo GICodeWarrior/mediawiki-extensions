c3f32f974854dfd73aba827baaaf6186c48f8034
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-28 18:45:03 +0000

    New extension 'Regex Fun', introducing some parser functions for regular expression handling within wiki articles. Version 1.0 alpha.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@101166 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ RegexFun.i18n.php
@@ -0,0 +1,30 @@
+<?php
+#coding: utf-8
+
+/**
+ * Internationalisation file of the 'Regex Fun' extension
+ *
+ * @since 1.0
+ * 
+ * @file RegexFun.i18n.php
+ * @ingroup RegexFun
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Werner
+ */
+$messages['en'] = array(
+	'regexfun-desc' => 'Adds parser functions allowing the use of regular expressions within the wiki.',
+	'regexfun-invalid' => 'The regular expression "$1" is invalid.',
+);
+
+/** German (Deutsch)
+ * @author Daniel Werner
+ */
+$messages['de'] = array(
+	'regexfun-desc' => 'Fügt Parser-Funktionen hinzu um reguläre Ausdrücke im Wiki verwenden zu können.',
+	'regexfun-invalid' => '„$1“ ist kein gültiger regulärer Ausdruck.',
+);
