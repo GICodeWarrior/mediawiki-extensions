7fd9f659beb649f84e383c7a2dcd515cad88f1d9
johnduhart <johnduhart@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-28 15:12:25 +0000

    Code improvements and cleanup to OnlineStatusBar
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@101137 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/OnlineStatusBar/OnlineStatusBar.i18n.php
+++ extensions/OnlineStatusBar/OnlineStatusBar.i18n.php
@@ -11,6 +11,7 @@ $messages = array();
 /**
  * English
  * @author Petr Bena
+ * @author John Du Hart
  */
 $messages['en'] = array(
 	// Description 
@@ -25,9 +26,18 @@ $messages['en'] = array(
 	'prefs-onlinestatus' => 'Online status',
 	// Message in config
 	'onlinestatusbar-hide' => 'Do you want to hide the status bar in order to use just the magic word? (For advanced users)',
+
+	'onlinestatusbar-status-online' => 'On-line',
+	'onlinestatusbar-status-busy' => 'Busy',
+	'onlinestatusbar-status-away' => 'Away',
+	'onlinestatusbar-status-offline' => 'Offline',
+	'onlinestatusbar-status-hidden' => 'Hidden',
 );
 
-/** Message documentation (Message documentation) */
+/** Message documentation (Message documentation)
+ * @author Petr Bena
+ * @author John Du Hart
+ */
 $messages['qqq'] = array(
 	'onlinestatusbar-desc' => '{{desc}}',
 	'onlinestatusbar-line' => 'Status bar text line (User is now Offline), parameters:
@@ -38,6 +48,12 @@ $messages['qqq'] = array(
 	'onlinestatusbar-status' => 'Message in config asking what status they want to use, option box',
 	'prefs-onlinestatus' => 'Section for config, located in preferences - misc',
 	'onlinestatusbar-hide' => 'Ask user if they want to hide status bar this is useful when they are using custom template but need to check if they are online',
+
+	'onlinestatusbar-status-online' => 'Status for users who mark themselves as active',
+	'onlinestatusbar-status-busy' => 'Status for users who mark themselves as busy',
+	'onlinestatusbar-status-away' => 'Status for users who mark themselves as away',
+	'onlinestatusbar-status-offline' => 'Status for users who are offline',
+	'onlinestatusbar-status-hidden' => 'Status for users who mark themselves as hidden (used on preferences only)',
 );
 
 /** German (Deutsch)
