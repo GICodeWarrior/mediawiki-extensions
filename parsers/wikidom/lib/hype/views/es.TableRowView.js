76132b7b28a0e56afb5f2127b98e0cedc2c6fe67
inez <inez@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-28 18:19:04 +0000

    Support for placing cursor inside table cells
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@101162 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- parsers/wikidom/lib/hype/views/es.TableRowView.js
+++ parsers/wikidom/lib/hype/views/es.TableRowView.js
@@ -11,3 +11,31 @@ es.TableRowView = function( model ) {
 	view.$.attr( 'style', model.getElementAttribute( 'html/style' ) );
 	return view;
 };
+
+/**
+ * Gets the nearest offset of a rendered position.
+ * 
+ * The only difference between this method and one in DocumentViewBranchNode is that it checks
+ * 'left' property of position instead of 'top'. If such case will become more frequent lets
+ * consider having private property in DocumentViewBranchNode which defines if horizontal
+ * or vertical scanning should be executed.
+ * 
+ * @method
+ * @param {es.Position} position Position to get offset for
+ * @returns {Integer} Offset of position
+ */
+es.TableRowView.prototype.getOffsetFromRenderedPosition = function( position ) {
+	if ( this.length === 0 ) {
+		return 0;
+	}
+	var node = this[0];
+	for ( var i = 1; i < this.length; i++ ) {
+		if ( this[i].$.offset().left > position.left ) {
+			break;
+		}
+		node = this[i];
+	}
+	return node.getParent().getOffsetFromNode( node, true ) +
+		node.getOffsetFromRenderedPosition( position );	
+};
+
