0f82204f0d55da95ee200b20c8c1fd4a9f22384e
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-28 16:14:22 +0000

    Moving my extension 'HashTables' from mediawiki.org into svn. Starting with new version '0.6.4 alpha'.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@101146 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ HashTables.i18n.php
@@ -0,0 +1,28 @@
+<?php
+#coding: utf-8
+
+/**
+ * Internationalization file of the 'HashTables' extension.
+ *
+ * @since 0.6.3
+ *
+ * @file HashTables.i18n.php
+ * @ingroup HashTables
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Werner
+ */
+$messages['en'] = array(
+	'hashtables-desc' => 'Parser functions allowing to work with hash tables in an article scoped context',
+);
+
+/** German
+ * @author Daniel Werner
+ */
+$messages['de'] = array(
+	'hashtables-desc' => 'Bietet Parser-Funktionen zur Verwendung Assoziativer Datenfelder (Hashes) in Wiki-Artikeln',
+);
