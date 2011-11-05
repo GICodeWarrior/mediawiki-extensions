0217cb1df25303a910dc14294718326e79eeb3c1
petrb <petrb@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-05 14:57:47 +0000

    new extension created initial folder + some stuff, it's work in progress
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102099 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/InteractiveBlockMessage/InteractiveBlockMessage.i18n.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Internationalisation file for Interactive block message extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/**
+ * English
+ * @author Petr Bena
+ */
+$messages['en'] = array(
+	// Description 
+	'interactiveblockmessage-desc' => 'Creates magic word {{userblocked}} which if on userspace indicates if user is blocked or not.',
+);
+
+/** Message documentation (Message documentation)
+ * @author Petr Bena
+ */
+$messages['qqq'] = array(
+	'interactiveblockmessage-desc' => '{{desc}}',
+);
---
+++ extensions/InteractiveBlockMessage/InteractiveBlockMessageHooks.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Hooks for Interactive block message
+ *
+ * @group Extensions
+ */
+class InteractiveBlockMessageHooks {
+	/**
+	 * @param $magicWords array
+	 * @param $ln string (language)
+	 * @return bool
+	 */
+	public static function magicWordVar( array &$magicWords, $ln ) {
+		$magicWords['userblocked'] = array( 0, 'userblocked' );
+		return true;
+	}
+
+	/**
+	 * @param $vars array
+	 * @return bool
+	 */
+	public static function magicWordSet( &$vars ) {
+		$vars[] = 'userblocked';
+		return true;
+	}
+
+	/**
+	 * @param $parser Parser
+	 * @param $varCache ??
+	 * @param $index ??
+	 * @param $ret string?
+	 * @return bool
+	 */
+	public static function parserGetVariable( &$parser, &$varCache, &$index, &$ret ) {
+		if ( $index != 'userblocked' ) {
+			return true;
+		}
+		
+		if ( $parser->getTitle->getNamespace() != NS_USER && $parser->getTitle->getNamespace() != NS_USER_TALK ) {
+			$ret = "unknown";
+                        return true;
+                }
+
+		$user = User::newFromName( $parser->getTitle()->getBaseText() ); 
+		if ($user instanceof User) {
+			if ($user->isBlocked()) {
+				$ret = "true";
+				return true;
+			} else {
+				$ret = "false";
+				return true;
+			}
+		} else {
+			$ret = "unknown";
+			return true;
+		}
+	 	$ret = "unknown";	
+		return true;
+	}
+}
