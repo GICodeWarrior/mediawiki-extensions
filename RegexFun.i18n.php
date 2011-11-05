fb43d4ef7ed5be6845fc5e5fadadce045035e2fd
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-05 21:03:17 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102118 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- RegexFun.i18n.php
+++ RegexFun.i18n.php
@@ -27,3 +27,36 @@ $messages['de'] = array(
 	'regexfun-desc' => 'Fügt Parser-Funktionen hinzu, um reguläre Ausdrücke auf Wiki-Seiten verwenden zu können',
 	'regexfun-invalid' => '„$1“ ist kein gültiger regulärer Ausdruck.',
 );
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'regexfun-desc' => 'Engade funcións analíticas que permiten o uso de expresións regulares nas páxinas wiki',
+	'regexfun-invalid' => 'A expresión regular "$1" non é válida.',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'regexfun-desc' => 'Adder functiones al analysator syntactic que permitte le uso de expressiones regular intra paginas wiki',
+	'regexfun-invalid' => 'Le expression regular "$1" es invalide.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'regexfun-desc' => 'Додава парсерски функции што овозможуваат употреба на регуларни изрази во вики-страници',
+	'regexfun-invalid' => 'Регуларниот израз „$1“ е неважечки.',
+);
+
+/** Dutch (Nederlands)
+ * @author Tjcool007
+ */
+$messages['nl'] = array(
+	'regexfun-desc' => "Voegt parser functies toe die toelaten om reguliere expressies te gebruiken in wiki pagina's",
+	'regexfun-invalid' => 'De reguliere expressie "$1" is ongeldig.',
+);
+
