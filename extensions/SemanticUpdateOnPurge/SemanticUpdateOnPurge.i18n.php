f69d9c6048dbc59ae4abe04e9f831b060be631cc
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-06 02:27:13 +0000

    Version 0.4 of my extension 'SemanticUpdateOnPurge' moved from mediawiki.org
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102143 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SemanticUpdateOnPurge/SemanticUpdateOnPurge.i18n.php
@@ -0,0 +1,28 @@
+<?php
+#coding: utf-8
+
+/**
+ * Internationalisation file of the 'SemanticUpdateOnPurge' extension
+ *
+ * @since 0.4
+ * 
+ * @file SemanticUpdateOnPurge.i18n.php
+ * @ingroup SemanticUpdateOnPurge
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Werner
+ */
+$messages['en'] = array(
+	'suop-desc' => 'Updates semantic attributes of a wiki page when purging the page.',
+);
+
+/** German (Deutsch)
+ * @author Daniel Werner
+ */
+$messages['de'] = array(
+	'suop-desc' => 'Aktualisiert semantische Attribute wenn der Seitencache durch „purge“ geleert wird.',
+);
