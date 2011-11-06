e033ac304cb59b129e69406c85e59ee08223c981
cryptocoryne <cryptocoryne@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-06 01:17:04 +0000

    Added experimental version of new extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102139 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ db_tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE /*$wgDBprefix*/pm_queue (
+	pmq_id BIGINT unsigned NOT NULL AUTO_INCREMENT,
+	pmq_page_last_id INT unsigned NOT NULL,
+	pmq_page_ns INT NOT NULL,
+	pmq_page_title VARCHAR(255) BINARY NOT NULL,
+	pmq_user INT unsigned NOT NULL DEFAULT 0,
+	pmq_user_text VARCHAR(255) BINARY NOT NULL DEFAULT '',
+	pmq_timestamp BINARY(14) NOT NULL DEFAULT '',
+	pmq_minor TINYINT unsigned NOT NULL DEFAULT 0,	
+	pmq_summary TINYBLOB NOT NULL,
+	pmq_len INT unsigned,
+	pmq_text MEDIUMBLOB NOT NULL,
+	pmq_flags tinyblob NOT NULL,
+	pmq_ip VARBINARY(40) NOT NULL DEFAULT '',
+	pmq_updated BINARY(14) DEFAULT NULL,
+	pmq_updated_user INT unsigned DEFAULT NULL,
+	pmq_updated_user_text VARCHAR(255) BINARY DEFAULT NULL,
+	pmq_status VARBINARY(40) NOT NULL DEFAULT '',
+	
+	PRIMARY KEY (pmq_id),
+	KEY (pmq_user)
+) /*$wgDBTableOptions*/;
+
+CREATE TABLE /*$wgDBprefix*/pm_whitelist (
+	pmw_ip VARBINARY(40) NOT NULL DEFAULT '',
+
+	PRIMARY KEY (pmw_ip)
+) /*$wgDBTableOptions*/;
