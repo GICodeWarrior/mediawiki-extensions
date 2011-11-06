6b38ee1c24adfc5e30c0a69abe68c36f583f729a
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-06 01:09:07 +0000

    Version 1.0.1, configuration variables and settings file added, some cleanup done.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102136 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/RegexFun/RegexFun.php
+++ extensions/RegexFun/RegexFun.php
@@ -20,6 +20,7 @@
 
 if ( ! defined( 'MEDIAWIKI' ) ) { die( ); }
 
+
 /**** extension info ****/ 
 
 $wgExtensionCredits['parserhook'][] = array(
@@ -36,7 +37,10 @@ $wgExtensionMessagesFiles['RegexFunMagic'] = ExtRegexFun::getDir() . '/RegexFun.
 
 $wgHooks['ParserFirstCallInit'][] = 'ExtRegexFun::init';
 $wgHooks['ParserClearState'   ][] = 'ExtRegexFun::onParserClearState';
+$wgHooks['ParserLimitReport'  ][] = 'ExtRegexFun::onParserLimitReport';
 
+// Include the settings file:
+require_once ExtRegexFun::getDir() . '/RegexFun_Settings.php';
 
 /**
  * Extension class with all the regex functions functionality
@@ -54,18 +58,29 @@ class ExtRegexFun {
 	 */
 	const VERSION = '1.0.1';
 	
-    /**
-     * Sets up parser functions
-     */
-	public static function init( &$parser ) {		
-    	$parser->setFunctionHook( 'regex',       array( __CLASS__, 'regex'       ) );
-		$parser->setFunctionHook( 'regex_var',   array( __CLASS__, 'regex_var'   ) );
-    	$parser->setFunctionHook( 'regexall',    array( __CLASS__, 'regexall'    ) );
-    	$parser->setFunctionHook( 'regexquote',  array( __CLASS__, 'regexquote'  ) );
-    	$parser->setFunctionHook( 'regexascii',  array( __CLASS__, 'regexascii'  ) );
+	/**
+	 * Sets up parser functions
+	 */
+	public static function init( Parser &$parser ) {
+		self::initFunction( $parser, 'regex' );
+		self::initFunction( $parser, 'regex_var' );
+		self::initFunction( $parser, 'regexquote' );
+		self::initFunction( $parser, 'regexall' );
 		
 		return true;		
-    }
+	}	
+	private static function initFunction( Parser &$parser, $name, $functionCallback = null ) {
+		if( $functionCallback === null ) {
+			$functionCallback = array( __CLASS__, $name );
+		}
+		
+		global $egRegexFunDisabledFunctions;
+		
+		// only register function if not disabled by configuration
+		if( ! in_array( $name, $egRegexFunDisabledFunctions ) ) {
+			$parser->setFunctionHook( $name, $functionCallback );
+		}
+	}
 	
 	/**
 	 * Returns the extensions base installation directory.
@@ -93,6 +108,7 @@ class ExtRegexFun {
 	 * @var array
 	 */
 	private static $tmpRegexCB;
+	
 	
 	/**
 	 * Checks whether the given regular expression is valid or would cause an error.
@@ -179,6 +195,15 @@ class ExtRegexFun {
 		return false;
 	}
 	
+	private static function limitHandler( Parser &$parser ) {		
+		// is the limit exceeded for this parsers parse() process?
+		if( self::limitExceeded( $parser ) ) {
+			return false;
+		}		
+		self::increaseRegexCount( $parser );
+		return true;
+	}
+	
 	/**
 	 * Returns a valid parser function output that the given pattern is not valid for a regular
 	 * expression. The message can be displayed in the wiki and is wrapped in an error-class span
@@ -188,9 +213,15 @@ class ExtRegexFun {
 	 * 
 	 * @return Array
 	 */
-	public static function invalidRegexParsingOutput( $pattern ) {
-		$msg = '<span class="error">' . wfMsgExt( 'regexfun-invalid', array( 'content' ), "<tt><nowiki>$pattern</nowiki></tt>" ). '</span>';
-		return array( $msg, 'noparse' => true, 'isHTML' => false ); // isHTML must be false for #iferror!
+	protected static function msgInvalidRegex( $pattern ) {
+		$msg = '<span class="error">' . wfMsgForContent( 'regexfun-invalid', "<tt><nowiki>$pattern</nowiki></tt>" ). '</span>';
+		return array( $msg, 'noparse' => false, 'isHTML' => false ); // 'noparse' true for <nowiki>, 'isHTML' false for #iferror!
+	}
+	
+	protected static function msgLimitExceeded() {
+		global $egRegexFunMaxRegexPerParse;
+		$msg = '<span class="error">' . wfMsgForContent( 'regexfun-limit-exceed', $egRegexFunMaxRegexPerParse ). '</span>';
+		return array( $msg, 'noparse' => true, 'isHTML' => false ); // 'isHTML' must be false for #iferror!
 	}
 	
 	/**
@@ -212,21 +243,26 @@ class ExtRegexFun {
 	}
 	
     /**
-     * Performs a regular expression search or replacement
+	 * Performs a regular expression search or replacement
 	 * 
-     * @param $parser Parser instance of running Parse
-     * @param $subject String input string to evaluate
-     * @param $pattern String regular expression pattern - must use /, | or % delimiter
-     * @param $replace String regular expression replacement
+	 * @param $parser Parser instance of running Parse
+	 * @param $subject String input string to evaluate
+	 * @param $pattern String regular expression pattern - must use /, | or % delimiter
+	 * @param $replace String regular expression replacement
 	 * 
-     * @return String Result of replacing pattern with replacement in string, or matching text if replacement was omitted
-     */
+	 * @return String Result of replacing pattern with replacement in string, or matching text if replacement was omitted
+	 */
     public static function regex( Parser &$parser, $subject = '', $pattern = '', $replace = null, $limit = -1 ) {
+		// check whether limit exceeded:
+		if( self::limitExceeded( $parser ) ) {
+			return self::msgLimitExceeded();
+		}
+		self::increaseRegexCount( $parser );
 		
 		// validate, initialise and check for wrong input:
 		$continue = self::validateRegexCall( $parser, $subject, $pattern, $specialFlags, true );
 		if( ! $continue ) {
-			return self::invalidRegexParsingOutput( $pattern );;
+			return self::msgInvalidRegex( $pattern );
 		}
 		
 		if( $replace === null ) {
@@ -302,11 +338,17 @@ class ExtRegexFun {
 	 * 
 	* @return String result of all matching text parts separated by a string
 	*/
-    public static function regexall( &$parser , $subject = '' , $pattern = '' , $separator = ', ' , $offset = 0 , $length = '' ) {
+	public static function regexall( &$parser , $subject = '' , $pattern = '' , $separator = ', ' , $offset = 0 , $length = '' ) {
+		// check whether limit exceeded:
+		if( self::limitExceeded( $parser ) ) {
+			return self::msgLimitExceeded();
+		}
+		self::increaseRegexCount( $parser );
+		
 		// validate and check for wrong input:
 		$continue = self::validateRegexCall( $parser, $subject, $pattern, $specialFlags, false );
 		if( ! $continue ) {
-			return self::invalidRegexParsingOutput( $pattern );;
+			return self::msgInvalidRegex( $pattern );;
 		}
 		
 		// adjust default values:
@@ -334,13 +376,13 @@ class ExtRegexFun {
 		return '';
     }
 	
-    /**
-     * Returns a value from the last performed regex match
+	/**
+	 * Returns a value from the last performed regex match
 	 * 
-     * @index $parser Parser instance of running Parser
-     * @param $index Integer index of the last match which should be returnd or a string containing $n as indexes to be replaced
-     * @param $defaultVal Integer default value which will be returned when the result with the given index doesn't exist or is a void string
-     */
+	 * @index $parser Parser instance of running Parser
+	 * @param $index Integer index of the last match which should be returnd or a string containing $n as indexes to be replaced
+	 * @param $defaultVal Integer default value which will be returned when the result with the given index doesn't exist or is a void string
+	 */
 	public static function regex_var( &$parser, $index = 0, $defaultVal = '' ) {
 		// get matches from last #regex
 		$lastMatches = self::getLastMatches( $parser );
@@ -360,6 +402,13 @@ class ExtRegexFun {
 			}
 		} else {
 			// complex string is given, something like "$1, $2 and $3":			
+			
+			// limit check, only in complex mode:
+			if( self::limitExceeded( $parser ) ) {
+				return self::msgLimitExceeded();
+			}
+			self::increaseRegexCount( $parser );
+			
 			/*
 			 * replace all back-references with their number increased by 1!
 			 * this way we can also handle $0 in the right way!
@@ -396,16 +445,16 @@ class ExtRegexFun {
 		}
 		return preg_replace( '%\d+%', (int)$index + 1, $full );	
 	}	
-	
-    /**
-     * takes $str and puts a backslash in front of each character that is part of the regular expression syntax
+
+	/**
+	 * takes $str and puts a backslash in front of each character that is part of the regular expression syntax
 	 * 
-     * @param $parser Parser instance of running Parser
-     * @param $str String input string to change
-     * @param $delimiter String delimiter which also will be escaped within $str (default is set to '/')
+	 * @param $parser Parser instance of running Parser
+	 * @param $str String input string to change
+	 * @param $delimiter String delimiter which also will be escaped within $str (default is set to '/')
 	 * 
-     * @return String Returns the quoted string
-     */
+	 * @return String Returns the quoted string
+	 */
 	public static function regexquote( &$parser, $str = null, $delimiter = '/' ) {		
 		if( $str === null ) {
 			return '';
@@ -432,11 +481,26 @@ class ExtRegexFun {
 		return $str;
 	}
 	
+    public static function onParserLimitReport( $parser, &$report ) {
+		global $egRegexFunMaxRegexPerParse;
+		$count = self::getLimitCount( $parser );
+		
+		$report .= 'ExtRegexFun count: ';
+		
+		if( $egRegexFunMaxRegexPerParse !== -1 ) {
+			$report .= "{$count}/{$egRegexFunMaxRegexPerParse}\n";
+		}
+		else {
+			$report .= "{$count}\n";
+		}
+        return true;
+    }
 	
-	/*********************************
-	 ****  HELPER - For Store of  ****
-	 **** regex_var within Parser ****
-	 *********************************
+	
+	/***********************************
+	 ****   HELPER - For store of   ****
+	 **** regex stuff within Parser ****
+	 ***********************************
 	 ****
 	 **
 	 * Adding the info to each Parser object makes it invulnerable to new Parser objects being created
@@ -454,11 +518,38 @@ class ExtRegexFun {
 	
 	public static function onParserClearState( &$parser ) {
 		//cleanup to avoid conflicts with job queue or Special:Import
+		$parser->mExtRegexFun = array();
 		self::setLastMatches( $parser, null );
 		self::setLastPattern( $parser, '' );
-		self::setLastSubject( $parser, '' );		
+		self::setLastSubject( $parser, '' );
+		$parser->mExtRegexFun['counter'] = 0;
 		
 		return true;
+	}
+	
+	/**
+	 * Returns whether the maximum limit of regular expression has been exceeded
+	 * for the given parser objects current Parser::parse() process.
+	 * 
+	 * @return boolean
+	 */
+	public static function limitExceeded( Parser &$parser ) {
+		global $egRegexFunMaxRegexPerParse;
+		return (
+			$egRegexFunMaxRegexPerParse !== -1
+			&& $parser->mExtRegexFun['counter'] >= $egRegexFunMaxRegexPerParse
+		);
+	}
+	
+	public static function getLimitCount( Parser &$parser ) {
+		if( isset( $parser->mExtRegexFun['counter'] ) ) {
+			return $parser->mExtRegexFun['counter'];
+		}
+		return 0;
+	}
+	
+	private static function increaseRegexCount( Parser &$parser ) {
+		$parser->mExtRegexFun['counter']++;
 	}
 	
 	/**
