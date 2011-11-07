47c6b4fe7c704e2230638fb43fbfa10d110561ac
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-07 15:52:11 +0000

    r101295: Consistency tweaks in preparation for adding extension to translatewiki.net: Sort languages: 1. 'en', 2. 'qqq' change description message
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102293 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Sternograph.i18n.php
+++ Sternograph.i18n.php
@@ -5,10 +5,29 @@
 $messages = array();
 
 /**
+ * English
+ */
+$messages['en'] = array(
+	'sternograph-desc' => '<nowiki><sterno></nowiki> tags for transcripts of spokens words (plays, interviews, etc)',
+	'sternograph-empty' => '<$1> tag cannot be empty.',
+	'sternograph-nested' => '<$1> tag cannot be nested.',
+	'sternograph-speaker-is' => '<$1> speaker block lines must have $2 character.',
+
+	'sternograph-speaker-pre' => '',
+	'sternograph-speaker-post' => ':&nbsp;',
+	'sternograph-block-pre' => '',
+	'sternograph-block-post' => '',
+	'sternograph-inline-pre' => '[',
+	'sternograph-inline-post' => ']',
+	'sternograph-context-pre' => '&mdash;',
+	'sternograph-context-post' => ''
+);
+
+/**
  * Translation documentation.
  */
 $messages['qqq'] = array(
-	'sternograph-descriptionmsg'=>'Description of the Sternograph extension; displayed on the Special:Extensions page only.',
+	'sternograph-desc'=>'Description of the Sternograph extension; displayed on the Special:Extensions page only.',
 	'sternograph-empty' => 'Error message when an empty tag is detected on the wiki-formatted page.  The tag name to display is parsed to this function as the first parameter.',
 	'sternograph-nested'=>'Error message when a sternograph tag occurs within an existing sternograph tag on the wiki-formatted page.  The tag name to display is the first parameter.',
 	'sternograph-speaker-is' => 'Error message when a speaker block does not contain the speaker delimeter on the wiki-formatted page.  The tag name to display is the first parameter; the character that is required is the second parameter.',
@@ -23,21 +42,3 @@ $messages['qqq'] = array(
 	'sternograph-context-post'=>'Text that will always occur after the context block in the formatted text.  This text is formatted by the sternographContext CSS class.'
 );
 
-/**
- * English
- */
-$messages['en'] = array(
-	'sternograph-descriptionmsg'=>'<sterno> tags for transcripts of spokens words (plays, interviews, etc).',
-	'sternograph-empty' => '<$1> tag cannot be empty.',
-	'sternograph-nested'=>'<$1> tag cannot be nested.',
-	'sternograph-speaker-is' => '<$1> speaker block lines must have $2 character.',
-
-	'sternograph-speaker-pre'=>'',
-	'sternograph-speaker-post'=>':&nbsp;',
-	'sternograph-block-pre'=>'',
-	'sternograph-block-post'=>'',
-	'sternograph-inline-pre'=>'[',
-	'sternograph-inline-post'=>']',
-	'sternograph-context-pre'=>'&mdash;',
-	'sternograph-context-post'=>''
-);
