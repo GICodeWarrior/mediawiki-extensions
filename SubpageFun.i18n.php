a53355276db41334becc89f1a242c032fbd9a824
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-07 20:46:13 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102323 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SubpageFun.i18n.php
+++ SubpageFun.i18n.php
@@ -26,3 +26,32 @@ $messages['en'] = array(
 $messages['de'] = array(
 	'subpagefun-desc' => 'Fügt Parser-Funktionen zum Ermitteln umfangreicher Informationen über [http://www.mediawiki.org/wiki/Help:Subpages Unterseiten] hinzu',
 );
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'subpagefun-desc' => 'Engade funcións analíticas que permiten obter información máis ampla acerca das [http://www.mediawiki.org/wiki/Help:Subpages subpáxinas]',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'subpagefun-desc' => 'Adde functiones al analysator syntactic que permitte ganiar information extense a proposito de [http://www.mediawiki.org/wiki/Help:Subpages subpaginas]',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'subpagefun-desc' => 'Додава парсерски функции што даваат приказ на опширни информации за [www.mediawiki.org/wiki/Help:Subpages?uselang=mk потстраници]',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'subpagefun-desc' => "A gionta funsion ëd parser ch'a përmëtto ëd pijé anformassion estèise su le [http://www.mediawiki.org/wiki/Help:Subpages sotpagine]",
+);
+
