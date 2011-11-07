9015b198fe2d6ebc93cf339d332a251145636725
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-07 03:33:21 +0000

    Version 0.5 of my extension 'Subpage Fun', moved from mediawiki.org into svn repository
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102246 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SubpageFun.i18n.php
@@ -0,0 +1,28 @@
+<?php
+#coding: utf-8
+
+/**
+ * Internationalisation file of the 'Subpage Fun' extension
+ *
+ * @since 0.5
+ * 
+ * @file SubpageFun.i18n.php
+ * @ingroup SubpageFun
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Werner
+ */
+$messages['en'] = array(
+	'subpagefun-desc' => 'Adds parser functions allowing to gain extensive information about [http://www.mediawiki.org/wiki/Help:Subpages subpages]',
+);
+
+/** German (Deutsch)
+ * @author Daniel Werner
+ */
+$messages['de'] = array(
+	'subpagefun-desc' => 'Fügt Parser-Funktionen zum Ermitteln umfangreicher Informationen über [http://www.mediawiki.org/wiki/Help:Subpages Unterseiten] hinzu',
+);
