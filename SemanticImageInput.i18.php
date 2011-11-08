d3609544acff5d7f76aa224055d7313043689679
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-08 10:45:07 +0000

    importing SemanticImageInput extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102386 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticImageInput.i18.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Internationalization file for the Semantic Image Input extension.
+ *
+ * @since 0.1
+ *
+ * @file SemanticImageInput.i18n.php
+ * @ingroup SII
+ *
+ * @licence GNU GPL v3+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'sii-desc' => 'Adds an image input that pulls images from Wikipedia to [https://www.mediawiki.org/wiki/Extension:Semantic_Forms Semantic Forms].',
+
+	'sii-imageinput-loading' => 'Loading image...'
+);
