2fb733b87c1796b22ab3a3c7ccec7152f3cfd5ab
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-08 14:31:30 +0000

    r102386: Consistency tweaks in preparation for adding extension to translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102400 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticImageInput.i18.php
+++ SemanticImageInput.i18.php
@@ -18,7 +18,8 @@ $messages = array();
  * @author Jeroen De Dauw
  */
 $messages['en'] = array(
-	'sii-desc' => 'Adds an image input that pulls images from Wikipedia to [https://www.mediawiki.org/wiki/Extension:Semantic_Forms Semantic Forms].',
+	'sii-desc' => 'Adds an image input that pulls images from Wikipedia to [https://www.mediawiki.org/wiki/Extension:Semantic_Forms Semantic Forms]',
 
 	'sii-imageinput-loading' => 'Loading image...'
 );
+
