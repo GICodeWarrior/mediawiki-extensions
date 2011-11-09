e722128623c1ecc4e2e0d7a9f313f53685224098
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-09 00:38:15 +0000

    My new extension ParserFun, rc for initial release 0.1, depends on current 'Validator' extension in trunk.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102471 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ ParserFun.i18n.php
@@ -0,0 +1,28 @@
+<?php
+#coding: utf-8
+
+/**
+ * Internationalization file of the 'Parser Fun' extension.
+ *
+ * @since 0.1
+ *
+ * @file ParserFun.i18n.php
+ * @ingroup ParserFun
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Werner
+ */
+$messages['en'] = array(
+	'parserfun-desc' => "Adds a parser function <code>#parse</code> for parsing wikitext and introduces <code>THIS:</code> prefix for page information related magic variables",
+);
+
+/** German (Deutsch)
+ * @author Daniel Werner
+ */
+$messages['de'] = array(
+	'parserfun-desc' => "Macht eine Parser-Funktion <code>#parse</code> zum Parsen von Wikitext verfügbar und führt das <code>THIS:</code> Präfix für von Wiki-Seiten Information beziehende Variablen ein"
+);
