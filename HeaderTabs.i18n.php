e03452fc87578f92715c69caed3195416209c923
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-09 20:18:41 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@102563 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- HeaderTabs.i18n.php
+++ HeaderTabs.i18n.php
@@ -13,3 +13,24 @@ $messages['en'] = array(
 	'headertabs-desc' => 'Adds tabs to the page separating top-level sections',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'headertabs-desc' => 'Ermöglicht Reiter zur alternativen Darstellung der obersten Gliederungsebene von Inhaltsverzeichnissen',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'headertabs-desc' => 'Adde schedas al pagina, separante le sectiones de prime nivello',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'headertabs-desc' => 'Во страницата додава јазичиња што ги одделуваат првостепените поднаслови',
+);
+
