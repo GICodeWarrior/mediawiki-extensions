0ac7522a8ef721c8b7f774534ed6c8456e55dcf1
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-19 14:32:52 +0000

    Localisation update for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@103680 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- InteractiveBlockMessage.i18n.php
+++ InteractiveBlockMessage.i18n.php
@@ -24,3 +24,38 @@ $messages['qqq'] = array(
 	'interactiveblockmessage-desc' => '{{desc}}',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'interactiveblockmessage-desc' => 'Ergänzt das „magische Wort“ <nowiki>{{USERBLOCKED}}</nowiki>, das anzeigt, ob ein Benutzer gesperrt ist oder nicht',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'interactiveblockmessage-desc' => 'Wutworja nowe magiske słowo <nowiki>{{USERBLOCKED}}</nowiki>, kotrež podawa, hač wužiwar je zablokowany abo nic',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'interactiveblockmessage-desc' => 'Crea un nove parola magic <nowiki>{{USERBLOCKED}}</nowiki> le qual, si in spatio de usator, indica si le usator es blocate o non',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'interactiveblockmessage-desc' => 'Создава нов волшебен збор <nowiki>{{USERBLOCKED}}</nowiki> кој покажува дали корисникот е блокиран кога ќе го внесете во кориснички простор',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'interactiveblockmessage-desc' => 'Mewujudkan kata sakti baru <nowiki>{{USERBLOCKED}}</nowiki> untuk menunjukkan sama ada seseorang pengguna disekat atau tidak di sesebuah ruang pengguna',
+);
+
