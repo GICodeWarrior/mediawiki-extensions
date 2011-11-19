132c52e51295d51a3f2623f924e04a17ce32e6a5
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-19 14:32:52 +0000

    Localisation update for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@103680 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ParserFun.i18n.php
+++ ParserFun.i18n.php
@@ -21,8 +21,30 @@ $messages['en'] = array(
 
 /** German (Deutsch)
  * @author Daniel Werner
+ * @author Kghbln
  */
 $messages['de'] = array(
-	'parserfun-desc' => "Macht eine Parser-Funktion <code>#parse</code> zum Parsen von Wikitext verfügbar und führt das <code>THIS:</code> Präfix für von Wiki-Seiten Information beziehende Variablen ein"
+	'parserfun-desc' => 'Ergänzt die Parserfunktion <code>#parse</code> zum Parsen von Wikitext, sowie das <code>THIS:</code>-Präfix für Variablen, die von Wikiseiten Informationen abrufen',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'parserfun-desc' => 'Adde al analysator syntactic un function <code>#parse</code> pro interpretar wikitexto e introduce le prefixo <code>THIS:</code> pro variabiles magic connexe a information de paginas',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'parserfun-desc' => 'Додава парсерска функција <code>#parse</code> за парсирање на викитекст и го воведува префиксот <code>THIS:</code> за волшебни променливи што се однесуваат на информации за страници',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'parserfun-desc' => 'Menambahkan fungsi penghurai <code>#parse</code> untuk menghuraikan teks wiki serta memperkenalkan awalan <code>THIS:</code> untuk pembolehubah sakti berkenaan maklumat laman',
 );
 
