d02deaf94075a1f41b37b95afaef1de177c9eb54
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-22 23:34:46 +0000

    My new extension SimpleFarm v0.1rc, will be released soon.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@103982 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SimpleFarm.i18n.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Internationalization file of the 'Simple Farm' extension.
+ *
+ * @since 0.1
+ *
+ * @file SimpleFarm.i18n.php
+ * @ingroup SimpleFarm
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Werner
+ */
+$messages['en'] = array(
+	'parserfun-desc' => 'Simple, yet powerfull wiki farm extension without any fancy configuration pages.',
+);
+
+/** German (Deutsch)
+ * @author Daniel Werner
+ */
+$messages['de'] = array(
+	'parserfun-desc' => 'Einfache, jedoch trotzdem mächtige Wiki-Farm-Erweiterung ohne großartige Konfigurations-Seiten.',
+);
