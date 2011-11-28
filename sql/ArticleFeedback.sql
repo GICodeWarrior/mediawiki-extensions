2c0645a1e10fd92fbf9b0de50be85a66d6ddc72b
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-28 21:21:05 +0000

    Add forgotten /*_*/
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@104491 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- sql/ArticleFeedback.sql
+++ sql/ArticleFeedback.sql
@@ -94,5 +94,5 @@ CREATE TABLE IF NOT EXISTS /*_*/article_feedback_stats_types (
 CREATE UNIQUE INDEX /*i*/afst_type ON /*_*/article_feedback_stats_types( afst_type );
 
 -- Pre-populate table with stat types
-INSERT INTO article_feedback_stats_types ( afst_type ) VALUES ( 'highs_and_lows' );
-INSERT INTO article_feedback_stats_types ( afst_type ) VALUES ( 'problems' );
+INSERT INTO /*_*/article_feedback_stats_types ( afst_type ) VALUES ( 'highs_and_lows' );
+INSERT INTO /*_*/article_feedback_stats_types ( afst_type ) VALUES ( 'problems' );
