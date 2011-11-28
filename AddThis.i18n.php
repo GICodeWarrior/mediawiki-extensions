5fbb8af8ab1a8deb225c20e77c4efd2713b649ac
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-28 21:01:31 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@104485 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- AddThis.i18n.php
+++ AddThis.i18n.php
@@ -16,10 +16,67 @@ $messages['en'] = array(
 	'addthis-desc' => 'Adds [http://www.addthis.com AddThis widget] to the sidebar and creates toolbar displayed on page header or by using <nowiki><addthis /></nowiki> tag',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'addthis' => 'Teilen',
+	'addthis-desc' => 'Ergänzt ein [http://www.addthis.com AddThis-Widget] in der Seitenleiste und ermöglicht das Einfügen einer Funktionsleiste mit dem <nowiki><addthis /></nowiki>-Tag',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'addthis' => 'Źěliś',
+	'addthis-desc' => 'Pśidawa [http://www.addthis.com rozšyrjenje AddThis] bocnicy a napórajo rědowu kšomu, kótaraž pokazujo se w głowje boka, abo z pomocu elementa <nowiki><addthis /></nowiki>',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'addthis' => 'Compartir',
+	'addthis-desc' => 'Engade o [http://www.addthis.com widget AddThis] á barra lateral e crea unha barra de ferramentas mostrada na cabeceira das páxinas ou mediante a etiqueta <nowiki><addthis /></nowiki>',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'addthis' => 'Divider',
+	'addthis-desc' => 'Adde un [http://www.addthis.com widget "Adder isto"] al barra lateral e insere un barra de utensiles in alto del paginas o per usar le etiquetta <nowiki><addthis /></nowiki>',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'addthis' => 'Сподели',
+	'addthis-desc' => 'Го додава посредничкиот елемент [http://www.addthis.com AddThis] во страничната лента и создава алатник во заглавието на страницата или користејќи ознака <nowiki><addthis /></nowiki>',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'addthis' => 'Delen',
+	'addthis-desc' => 'Voegt een [http://www.addthis.com AddThis-widget] toe aan de sidebar en maakt een werkbalk aan die wordt weergegeven op de paginakop of waar de tag <nowiki><addthis /></nowiki> wordt gebruikt',
+);
+
 /** Russian (Русский)
  * @author Unikum111
  */
-$messages['ru'] = array (
-	'addthis'      => 'Поделиться',
-	'addthis-desc' => 'Добавляет [http://www.addthis.com кнопку AddThis] на боковую панель и в заголовок статьи'
+$messages['ru'] = array(
+	'addthis' => 'Поделиться',
+	'addthis-desc' => 'Добавляет [http://www.addthis.com кнопку AddThis] на боковую панель и в заголовок статьи',
 );
+
+/** Ukrainian (Українська)
+ * @author Gucci Mane Burrr
+ */
+$messages['uk'] = array(
+	'addthis' => 'Поділитися',
+	'addthis-desc' => 'Додає [http://www.addthis.com кнопку AddThis] на бічну панель і створює панель інструментів, що відображається у заголовку сторінки або за допомогою тегу <nowiki><addthis /></nowiki>',
+);
+
