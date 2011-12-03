91e6b3556c41d5a6ad0389574de9c42312895922
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-03 14:56:00 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@105068 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UserImages.i18n.php
+++ UserImages.i18n.php
@@ -288,7 +288,7 @@ $messages['fr'] = array(
  * @author ChrisPtDe
  */
 $messages['frp'] = array(
-	'userimages-desc' => 'Fât des galeries d’émâges tèlèchargiês per un utilisator avouéc <code><nowiki><userimage /></nowiki></code>.',
+	'userimages-desc' => 'Fât des galeries d’émâges tèlèchargiês per un usanciér avouéc <code><nowiki><userimage /></nowiki></code>.',
 	'userimages-caption' => 'Émâges tèlèchargiês per $1',
 	'userimages-noname' => 'Nom d’utilisator envalido ou ben manquent.',
 	'userimages-noimages' => '$1 at tèlèchargiê gins d’émâge.',
