209eeda203824e47e956a886f208da6deebaabb3
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-05 21:02:57 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@105224 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticWebBrowser.i18n.php
+++ SemanticWebBrowser.i18n.php
@@ -27,16 +27,33 @@ $messages['en'] = array(
 
 /** Message documentation (Message documentation)
  * @author Benedikt Kämpgen
+ * @author SPQRobin
  */
 $messages['qqq'] = array(
-	'browsewiki'                 => 'Title of the SWB page',
-	'swb_browse_semantic_web'    => 'Title for browsing',
-	'swb_browse_article'         => 'Text above "go" window',
-	'swb_browse_go'              => 'Title of button "go"',
-	'swb_browse_show_incoming'   => 'Title of properties',
-	'swb_browse_hide_incoming'   => 'Title of properties',
+	'browsewiki' => 'Title of the SWB page',
+	'swb_desc' => '{{desc}}',
+	'swb_browse_article' => 'Text above "go" window',
+	'swb_browse_go' => 'Title of button "go"',
+	'swb_browse_show_incoming' => 'Title of properties',
+	'swb_browse_hide_incoming' => 'Title of properties',
 	'swb_inverse_label_property' => 'Name of a special property',
-	'swb_inverse_label_default'  => 'Inverse label default, $1 is a place marker', 
-	'swb_browse_more'            => 'Browse more details',
+	'swb_inverse_label_default' => 'Inverse label default, $1 is a place marker',
+	'swb_browse_more' => 'Browse more details',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'browsewiki' => 'Das Wiki sowie das semantische Web browsen',
+	'swb_desc' => 'Ergänzt eine [[Special:BrowseWiki|Spezialseite]] zum Browsen des Wikis sowie des semantischen Webs',
+	'swb_browse_article' => 'Bitte den Namen einer Seite angeben, um mit dem Browsen zu beginnen.',
+	'swb_browse_go' => 'Los',
+	'swb_browse_show_incoming' => 'zeige Attribute die hierhin verlinken',
+	'swb_browse_hide_incoming' => 'verstecke Attribute die hierhin verlinken',
+	'swb_browse_no_outgoing' => 'Diese Seite enthält keine Attribute.',
+	'swb_browse_no_incoming' => 'Keine Attribute verlinken auf diese Seite.',
+	'swb_inverse_label_property' => 'Bezeichnung des inversen Attributs',
+	'swb_inverse_label_default' => '$1 von',
 );
 
