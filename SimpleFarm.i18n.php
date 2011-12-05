a2654ce7c537b2ce5019aff4871e8d4e84bd79c9
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-05 21:02:57 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@105224 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SimpleFarm.i18n.php
+++ SimpleFarm.i18n.php
@@ -19,10 +19,24 @@ $messages['en'] = array(
 	'simplefarm-desc' => 'Simple, yet powerful wiki farm extension without any fancy configuration pages',
 );
 
+/** Message documentation (Message documentation)
+ * @author SPQRobin
+ */
+$messages['qqq'] = array(
+	'simplefarm-desc' => '{{desc}}',
+);
+
 /** German (Deutsch)
  * @author Daniel Werner
  */
 $messages['de'] = array(
 	'simplefarm-desc' => 'Einfache, jedoch trotzdem mächtige Wiki-Farm-Erweiterung ohne großartige Konfigurations-Seiten',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'simplefarm-desc' => "Eenvoudige maar toch krachtige wikifarm-uitbreiding zonder grootse configuratiepagina's",
 );
 
