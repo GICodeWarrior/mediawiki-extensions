e95b4ec98e6af24c20fd231a963c7a91647c875a
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-06 15:28:18 +0000

    Few more bits of minor refactoring
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@105303 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- InteractiveBlockMessageHooks.php
+++ InteractiveBlockMessageHooks.php
@@ -39,31 +39,31 @@ class InteractiveBlockMessageHooks {
 			return true;
 		}
 
-		if ( $parser->getTitle()->getNamespace() != NS_USER && $parser->getTitle()->getNamespace() != NS_USER_TALK ) {
+		$title = $parser->getTitle();
+		if ( $title->getNamespace() != NS_USER && $title->getNamespace() != NS_USER_TALK ) {
 			$ret = 'unknown';
 			return true;
 		}
 
-		$user = User::newFromName( $parser->getTitle()->getBaseText() );
+		$user = User::newFromName( $title->getBaseText() );
 		if ( $user instanceof User ) {
-			if ( $user->isBlocked() ) {
-				// if user is blocked it's pretty much possible they will be unblocked one day :)
-				// so we enable cache for shorter time only so that we can recheck later
-				// if they weren't already unblocked - if there is a better way to do that, fix me
-				$expiry = $user->getBlock()->mExpiry;
-				if ( is_numeric ($expiry) ) { // sometimes this is 'infinityinfinity'. in that case, use the default cache expiry time.
-					$expiry = wfTimestamp( TS_UNIX, $expiry ) - wfTimestamp( TS_UNIX );
-					if ( $expiry > 0 ) {
-						// just to make sure
-						$parser->getOutput()->updateCacheExpiry( $expiry );
-					}
-				}
-				$ret = 'true';
-				return true;
-			} else {
+			if ( !$user->isBlocked() ) {
 				$ret = 'false';
 				return true;
 			}
+			// if user is blocked it's pretty much possible they will be unblocked one day :)
+			// so we enable cache for shorter time only so that we can recheck later
+			// if they weren't already unblocked - if there is a better way to do that, fix me
+			$expiry = $user->getBlock()->mExpiry;
+			if ( is_numeric ( $expiry ) ) { // sometimes this is 'infinityinfinity'. in that case, use the default cache expiry time.
+				$expiry = wfTimestamp( TS_UNIX, $expiry ) - wfTimestamp( TS_UNIX );
+				if ( $expiry > 0 ) {
+					// just to make sure
+					$parser->getOutput()->updateCacheExpiry( $expiry );
+				}
+			}
+			$ret = 'true';
+			return true;
 		}
 		$ret = 'unknown';
 		return true;
