91900f4128d4aa90efe49565039eab59f8a881ba
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-06 15:25:23 +0000

    Remove unused $wgInteractiveBlockMessageCacheTimeout
    Fix some minor style issues
    
    Remove one redundant else block
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@105302 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- InteractiveBlockMessageHooks.php
+++ InteractiveBlockMessageHooks.php
@@ -35,19 +35,18 @@ class InteractiveBlockMessageHooks {
 	 * @return bool
 	 */
 	public static function parserGetVariable( &$parser, &$varCache, &$index, &$ret ) {
-		global $wgInteractiveBlockMessageCacheTimeout;
 		if ( $index != 'USERBLOCKED' ) {
 			return true;
 		}
-		
+
 		if ( $parser->getTitle()->getNamespace() != NS_USER && $parser->getTitle()->getNamespace() != NS_USER_TALK ) {
 			$ret = 'unknown';
 			return true;
 		}
-	
+
 		$user = User::newFromName( $parser->getTitle()->getBaseText() );
-		if ($user instanceof User) {
-			if ($user->isBlocked()) {
+		if ( $user instanceof User ) {
+			if ( $user->isBlocked() ) {
 				// if user is blocked it's pretty much possible they will be unblocked one day :)
 				// so we enable cache for shorter time only so that we can recheck later
 				// if they weren't already unblocked - if there is a better way to do that, fix me
@@ -56,7 +55,7 @@ class InteractiveBlockMessageHooks {
 					$expiry = wfTimestamp( TS_UNIX, $expiry ) - wfTimestamp( TS_UNIX );
 					if ( $expiry > 0 ) {
 						// just to make sure
-						$parser->getOutput()->updateCacheExpiry($expiry);
+						$parser->getOutput()->updateCacheExpiry( $expiry );
 					}
 				}
 				$ret = 'true';
@@ -65,11 +64,8 @@ class InteractiveBlockMessageHooks {
 				$ret = 'false';
 				return true;
 			}
-		} else {
-			$ret = 'unknown';
-			return true;
 		}
-	 	$ret = 'unknown';
+		$ret = 'unknown';
 		return true;
 	}
 }
