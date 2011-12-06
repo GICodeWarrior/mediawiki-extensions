26fe4e86a749981785adaabb1565f58032b4fbb9
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-06 20:11:21 +0000

    Stylize, start documentation update
    Unfortunately, PHP isn't a strongly typed language, and using comments that show you wish it was, doesn't help either
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@105345 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- CrowdAuthentication.php
+++ CrowdAuthentication.php
@@ -31,10 +31,13 @@ $wgExtensionCredits['other'][] = array(
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:CrowdAuthentication'
 );
 
-$wgExtensionMessagesFiles['CrowdAuthentication'] = dirname(__FILE__) . '/CrowdAuthentication.i18n.php';
+$wgExtensionMessagesFiles['CrowdAuthentication'] = dirname( __FILE__ ) . '/CrowdAuthentication.i18n.php';
 
 class caPasswordCredential {
-	public /*string*/ $credential;
+	/**
+	 * @var string
+	 */
+	public $credential;
 }
 
 class caAuthenticatedToken {
@@ -50,38 +53,60 @@ class caSOAPGroup {
 }
 
 class caSOAPAttribute {
-	public /*string*/ $name;
-	public /*string[]*/ $values;
 
-	public function __construct(/*string*/ $name, /*string*/ $value) {
+	/**
+	 * @var string
+	 */
+	public $name;
+
+	/**
+	 * @var array
+	 */
+	public $values;
+
+	/**
+	 * @param $name string
+	 * @param $value string
+	 */
+	public function __construct( $name, $value ) {
 		$this->name = $name;
-		$this->values = array($value);
+		$this->values = array( $value );
 	}
 }
 
 class caApplicationAuthenticationContext {
-	public /*PasswordCredential*/ $credential;
+	/**
+	 * @var PasswordCredential
+	 */
+	public /**/ $credential;
 	public /*string*/ $name;
 	public /*ValidationFactor[]*/ $validationFactors = null;
-};
+}
 
 class caPrincipalAuthenticationContext {
 	public /*string*/ $application;
 	public /*PasswordCredential*/ $credential;
 	public /*string*/ $name;
 	public /*ValidationFactor[]*/ $validationFactors = null;
-};
+}
 
 class CrowdAuthenticator extends AuthPlugin {
+
+	/**
+	 * @var null|SoapClient
+	 */
 	private $crowd = null;
 	private $token = null;
 
-	private function /*SoapClient*/ getCrowd() {
-	global	$caCrowdServerUrl, $caApplicationName, $caApplicationPassword;
+	/**
+	 * @return SoapClient
+	 */
+	private function getCrowd() {
+		global $caCrowdServerUrl, $caApplicationName, $caApplicationPassword;
 
-		if (is_null($this->crowd)) {
-			$this->crowd = new SoapClient($caCrowdServerUrl . '/SecurityServer?wsdl',
-					array('classmap' =>
+		if ( is_null( $this->crowd ) ) {
+			$this->crowd = new SoapClient( $caCrowdServerUrl . '/SecurityServer?wsdl',
+					array( 'classmap' =>
 						array(
 							'ApplicationAuthenticationContext' => 'caApplicationAuthenticationContext',
 							'PrincipalAuthenticationContext' => 'caPrincipalAuthenticationContext',
@@ -99,30 +124,34 @@ class CrowdAuthenticator extends AuthPlugin {
 			$authctx = new caApplicationAuthenticationContext();
 			$authctx->credential = $cred;
 			$authctx->name = $caApplicationName;
-			$t = $this->crowd->authenticateApplication(array("in0" => $authctx));
+			$t = $this->crowd->authenticateApplication( array( "in0" => $authctx ) );
 			$this->token = $t->out;
 		}
 
 		return $this->crowd;
 	}
 
-	private function /*bool*/ findUsername(/*string*/ $name) {
+	/**
+	 * @param $name string
+	 * @return bool
+	 */
+	private function findUsername( $name ) {
 		/*
 		 * Need to check several variations, e.g. lowercase initial letter,
 		 * _ instead of ' ', etc.
 		 */
 		$variations = array(
 			$name,
-			strtolower($name[0]) . substr($name, 1),
-			str_replace(" ", "_", $name),
+			strtolower( $name[0] ) . substr( $name, 1 ),
+			str_replace( " ", "_", $name ),
 		);
 
 		$crowd = $this->getCrowd();
-		foreach ($variations as $v) {
+		foreach ( $variations as $v ) {
 			try {
-				$crowd->findPrincipalByName(array("in0" => $this->token, "in1" => $v));
+				$crowd->findPrincipalByName( array( "in0" => $this->token, "in1" => $v ) );
 				return $v;
-			} catch (Exception $e) {
+			} catch ( Exception $e ) {
 				continue;
 			}
 		}
@@ -130,33 +159,33 @@ class CrowdAuthenticator extends AuthPlugin {
 		return null;
 	}
 
-	public function /*bool*/ userExists(/*string*/ $name) {
-		return !is_null($this->findUsername($name));
+	public function /*bool*/ userExists( /*string*/ $name ) {
+		return !is_null( $this->findUsername( $name ) );
 	}
 
-	public function /*bool*/ authenticate(/*string*/ $username, /*string*/ $password) {
+	public function /*bool*/ authenticate( /*string*/ $username, /*string*/ $password ) {
 	global	$caApplicationName, $caImportGroups, $caOverwriteLocalGroups;
 
 		$crowd = $this->getCrowd();
 		$cred = new caPasswordCredential();
 		$cred->credential = $password;
 		$authctx = new caPrincipalAuthenticationContext();
-		$authctx->name = $this->findUsername($username);
+		$authctx->name = $this->findUsername( $username );
 		$authctx->credential = $cred;
 		$authctx->application = $caApplicationName;
 
 		try {
-			$crowd->authenticatePrincipal(array("in0" => $this->token, "in1" => $authctx));
+			$crowd->authenticatePrincipal( array( "in0" => $this->token, "in1" => $authctx ) );
 			return true;
-		} catch (Exception $e) {
+		} catch ( Exception $e ) {
 			return false;
 		}
 	}
 
-	public function /*void*/ updateUser(/*User*/ &$user) {
+	public function /*void*/ updateUser( /*User*/ &$user ) {
 	global	$caImportGroups, $caOverwriteLocalGroups;
 
-		if (!$caImportGroups)
+		if ( !$caImportGroups )
 			return true;
 
 		/*
@@ -165,89 +194,89 @@ class CrowdAuthenticator extends AuthPlugin {
 
 		$restr = new caSearchRestriction();
 		$restr->name = "group.principal.member";
-		$restr->value = $this->findUsername($user->getName());
-		$groups = $this->crowd->searchGroups(array("in0" => $this->token, "in1" => array($restr)));
+		$restr->value = $this->findUsername( $user->getName() );
+		$groups = $this->crowd->searchGroups( array( "in0" => $this->token, "in1" => array( $restr ) ) );
 		$groups = $groups->out->SOAPGroup;
 
-		$dbw = wfGetDB(DB_MASTER);
-		if ($caOverwriteLocalGroups)
-			$dbw->delete('user_group', array('ug_user' => $user->getId()));
+		$dbw = wfGetDB( DB_MASTER );
+		if ( $caOverwriteLocalGroups )
+			$dbw->delete( 'user_group', array( 'ug_user' => $user->getId() ) );
 
-		foreach ($groups as $group) {
-			$user->addGroup($group->name);
+		foreach ( $groups as $group ) {
+			$user->addGroup( $group->name );
 		}
 	}
 
-	public function /*bool*/ autoCreate(/*void*/) {
+	public function /*bool*/ autoCreate( /*void*/ ) {
 		return true;
 	}
 
-	public function /*bool*/ strict(/*void*/) {
+	public function /*bool*/ strict( /*void*/ ) {
 		return true;
 	}
 
-	public function /*bool*/ allowPasswordChange(/*void*/) {
+	public function /*bool*/ allowPasswordChange( /*void*/ ) {
 		return true;
 	}
 
-	public function /*bool*/ setPassword(/*User*/ $user, /*string*/ $password) {
+	public function /*bool*/ setPassword( /*User*/ $user, /*string*/ $password ) {
 		$newcred = new caPasswordCredential;
 		$newcred->credential = $password;
-		$username = $this->findUsername($user->getName());
+		$username = $this->findUsername( $user->getName() );
 		$crowd = $this->getCrowd();
 		try {
-			$crowd->updatePrincipalCredential(array(
+			$crowd->updatePrincipalCredential( array(
 						"in0" => $this->token,
 						"in1" => $username,
-						"in2" => $newcred));
+						"in2" => $newcred ) );
 			return true;
-		} catch (Exception $e) {
+		} catch ( Exception $e ) {
 			return false;
 		}
 	}
 
-	public function /*bool*/ canCreateAccounts(/*void*/) {
+	public function /*bool*/ canCreateAccounts( /*void*/ ) {
 		return true;
 	}
 
-	public function /*bool*/ addUser(/*User*/ $user, /*string*/ $password,
-					 /*string*/ $email = '', /*string*/ $realname = '') {
+	public function /*bool*/ addUser( /*User*/ $user, /*string*/ $password,
+					 /*string*/ $email = '', /*string*/ $realname = '' ) {
 	global	$caDefaultGroups;
 		$crowd = $this->getCrowd();
-		$nameparts = split(" ", $realname, 2);
+		$nameparts = split( " ", $realname, 2 );
 		$firstname = $user->getName();
 		$lastname = "";
-		if (count($nameparts) > 0)
+		if ( count( $nameparts ) > 0 )
 			$firstname = $nameparts[0];
-		if (count($nameparts) > 1)
+		if ( count( $nameparts ) > 1 )
 			$lastname = $nameparts[1];
 		$cred = new caPasswordCredential();
 		$cred->credential = $password;
 		$principal = new caPrincipal();
 		$principal->name = $user->getName();
 		$principal->attributes = array(
-			new caSOAPAttribute("mail", $email),
-			new caSOAPAttribute("givenName", $firstname),
-			new caSOAPAttribute("sn", $lastname),
-			new caSOAPAttribute("invalidPasswordAttempts", 0),
-			new caSOAPAttribute("lastAuthenticated", 0),
-			new caSOAPAttribute("passwordLastChanged", 0),
-			new caSOAPAttribute("requiresPasswordChange", 0),
+			new caSOAPAttribute( "mail", $email ),
+			new caSOAPAttribute( "givenName", $firstname ),
+			new caSOAPAttribute( "sn", $lastname ),
+			new caSOAPAttribute( "invalidPasswordAttempts", 0 ),
+			new caSOAPAttribute( "lastAuthenticated", 0 ),
+			new caSOAPAttribute( "passwordLastChanged", 0 ),
+			new caSOAPAttribute( "requiresPasswordChange", 0 ),
 		);
 		$principal->active = true;
 		$principal->conception = 0;
 		$principal->lastModified = 0;
 
 		try {
-			$crowd->addPrincipal(array("in0" => $this->token,
+			$crowd->addPrincipal( array( "in0" => $this->token,
 						   "in1" => $principal,
-						   "in2" => $cred));
-			foreach ($caDefaultGroups as $group)
-				$crowd->addPrincipalToGroup(array("in0" => $this->token, "in1" => $user->getName(), "in2" => $group));
+						   "in2" => $cred ) );
+			foreach ( $caDefaultGroups as $group )
+				$crowd->addPrincipalToGroup( array( "in0" => $this->token, "in1" => $user->getName(), "in2" => $group ) );
 
 			return true;
-		} catch (Exception $e) {
+		} catch ( Exception $e ) {
 			return false;
 		}
 	}
-};
+}
