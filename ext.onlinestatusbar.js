e199248c2732043dc99d7b104d950e10e1642d7b
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-07 01:33:29 +0000

    whitespace, local variable fixes
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@105405 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ext.onlinestatusbar.js
+++ ext.onlinestatusbar.js
@@ -2,32 +2,32 @@
 
 $(function() {
 
-	$statusbar = $('#status-top');
+var $statusbar = $('#status-top'),
 	$iconbar = $('.onlinestatusbaricon');
 
-	// Only do the rest if we have the statusbar!
-	if ($statusbar.length > 0) {
-		function updateOnlineStatusBar() {
-			// ... code to fetch and update
-			$.ajax({
+// Only do the rest if we have the statusbar!
+if ($statusbar.length > 0) {
+	function updateOnlineStatusBar() {
+		// ... code to fetch and update
+		$.ajax({
 			url: mw.config.get('wgScriptPath') + '/api' + mw.config.get('wgScriptExtension'),
 			data: {
-			action: "query",
-			prop: "onlinestatus",
-			onlinestatususer: mw.config.get('wgTitle'),
-			format: 'json'
-		  },
-		success: function(data) {
-		// code to update the statusbar based on the returned message
-		$icon = $("<img>").attr("src",mw.config.values.wgScriptPath+"/extensions/OnlineStatusBar/status"+({offline:'red',online:'green',away:'orange'})[data.onlinestatus.result]+'.png');
-		$statusbar.html(mw.msg('onlinestatusbar-line', wgTitle,$icon,data.onlinestatus.result));
-		//$statusbar.text(mw.config.values.wgTitle +" is now ").append($("<img>").attr("src",mw.config.values.wgScriptPath+"/extensions/OnlineStatusBar/status"+({offline:'red',online:'green',away:'orange'})[data.onlinestatus.result]+'.png')).append(data)
-  }
-});
-		}
+				action: "query",
+				prop: "onlinestatus",
+				onlinestatususer: mw.config.get('wgTitle'),
+				format: 'json'
+			},
+			success: function(data) {
+				// code to update the statusbar based on the returned message
+				var $icon = $("<img>").attr("src",mw.config.values.wgScriptPath+"/extensions/OnlineStatusBar/status"+({offline:'red',online:'green',away:'orange'})[data.onlinestatus.result]+'.png');
+				$statusbar.html(mw.msg('onlinestatusbar-line', wgTitle,$icon,data.onlinestatus.result));
+				//$statusbar.text(mw.config.values.wgTitle +" is now ").append($("<img>").attr("src",mw.config.values.wgScriptPath+"/extensions/OnlineStatusBar/status"+({offline:'red',online:'green',away:'orange'})[data.onlinestatus.result]+'.png')).append(data)
+			}
+		});
+	}
 
-		// Update the status every couple minutes if we leave the page open
-		window.setInterval(updateOnlineStatusBar, 120 * 1000);
+	// Update the status every couple minutes if we leave the page open
+	window.setInterval(updateOnlineStatusBar, 120 * 1000);
 	updateOnlineStatusBar();
 }
 
