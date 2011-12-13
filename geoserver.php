7d0afec0fc4bf5cd7db3311bd2efaebdaf278057
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-13 23:49:33 +0000

    Update a butt-ton of extension about URLs to point to https://www.mediawiki.org/
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106136 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- geoserver.php
+++ geoserver.php
@@ -9,7 +9,7 @@ $wgExtensionCredits['parserhook']['geoserver'] = array(
 	'path' => __FILE__,
 	'name' => 'geoserver',
 	'author' => 'Jens Frank',
-	'url' => 'http://www.mediawiki.org/wiki/Extension:geoserver',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:geoserver',
 	'description' => 'Allows geotagging using the <nowiki><geo></nowiki> tag. Saves geodata in a WFS-T server, e.g. geoserver.',
 );
 
