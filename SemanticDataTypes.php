93c39c85c38c171e082ecafb755140f229ec9e0b
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-13 23:49:33 +0000

    Update a butt-ton of extension about URLs to point to https://www.mediawiki.org/
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106136 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticDataTypes.php
+++ SemanticDataTypes.php
@@ -46,7 +46,7 @@ $wgExtensionCredits['semantic'][] = array(
 	'author' => array(
 		'[http://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
 	),
-	'url' => 'http://www.mediawiki.org/wiki/Extension:Semantic_Data_Types',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:Semantic_Data_Types',
 	'descriptionmsg' => 'sdt-desc'
 );
 
