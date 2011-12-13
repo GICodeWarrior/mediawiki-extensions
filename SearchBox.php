7b8a366ac8ad4e66b105ac693350f862fc447eb8
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-13 23:49:33 +0000

    Update a butt-ton of extension about URLs to point to https://www.mediawiki.org/
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106136 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SearchBox.php
+++ SearchBox.php
@@ -29,7 +29,7 @@ $wgExtensionCredits['parserhook'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'SearchBox',
 	'author'         => array( 'Trevor Parscal' ),
-	'url'            => 'http://www.mediawiki.org/wiki/Extension:SearchBox',
+	'url'            => 'https://www.mediawiki.org/wiki/Extension:SearchBox',
 	'descriptionmsg' => 'searchbox-desc',
 );
 
