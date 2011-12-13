87c9e56d7c30dc920909aada739b247717bd5eb6
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-13 23:49:33 +0000

    Update a butt-ton of extension about URLs to point to https://www.mediawiki.org/
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106136 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticACL.php
+++ SemanticACL.php
@@ -8,7 +8,7 @@ $wgExtensionCredits['other'][] = array(
 	'name'           => 'Semantic ACL',
 	'author'         => array( 'Andrew Garrett' ),
 	'descriptionmsg' => 'sacl-desc',
-	'url' 		 => 'http://www.mediawiki.org/wiki/Extension:SemanticACL',
+	'url' 		 => 'https://www.mediawiki.org/wiki/Extension:SemanticACL',
 );
 
 $wgExtensionMessagesFiles['SemanticACL'] = dirname(__FILE__) . '/SemanticACL.i18n.php';
