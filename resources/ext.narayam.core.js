28f0f5bbf8728d63d95dca9cf7ad4d17f2f59b64
santhosh <santhosh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-15 11:23:29 +0000

    Remove the unnesessary hide(), It will be hidden using css. Do not hide menu when clicked on the menu items.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106316 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- resources/ext.narayam.core.js
+++ resources/ext.narayam.core.js
@@ -660,7 +660,6 @@ $.narayam = new ( function() {
 		var positionFunction = rtlEnv ? "append" : "prepend";
 		$( '#p-personal ul:first' )[positionFunction]( $li );
 		$( 'body' ).prepend( $menu );
-		$menu.hide();
 		$li.click( function( event ) {
 			var menuSide, menuOffset, distanceToEdge;
 
@@ -700,7 +699,9 @@ $.narayam = new ( function() {
 			$menu.removeClass( 'open' );
 			$menu.hide();
 		} );
-
+		$menu.click( function( event ) {
+			event.stopPropagation();
+		} );
 		// Workaround for IE bug - activex components like input fields
 		// coming on top of everything.
 		// TODO: is there a better solution other than hiding it on hover?
