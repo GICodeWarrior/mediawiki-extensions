716830768227ee549e216fa91f738adecdb0d2fd
mah <mah@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-17 19:03:29 +0000

    followup r106235 - Add message documentation
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106519 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OpenStackManager.i18n.php
+++ OpenStackManager.i18n.php
@@ -349,6 +349,7 @@ $messages['qqq'] = array(
 	'openstackmanager-projectlist' => 'Page title',
 	'openstackmanager-deleteproject' => 'Page title',
 	'openstackmanager-novaproject-project' => '{{Identical|Project}}',
+	'openstackmanager-novaproject-membership' => 'The members in a project',
 	'openstackmanager-roles' => '{{Identical|Role}}',
 	'openstackmanager-rolelist' => 'It is a list of roles that are assigned to all instances (i.e. global).',
 	'openstackmanager-securitygroupdescription' => '{{Identical|Description}}',
