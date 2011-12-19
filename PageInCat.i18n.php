8b39a99459dbfe699b89221bb70af4538ee630a8
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-19 20:15:36 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106683 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PageInCat.i18n.php
+++ PageInCat.i18n.php
@@ -16,3 +16,45 @@ $messages['qqq'] = array(
 	'pagesincat-desc' => '{{desc}}',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'pageincat-desc' => 'Ergänzt die Parserfunktion <code>#incat:</code> mit der ermittelt werden kann, ob sich die aktuelle Seite in einer angegebenen Kategorie befindet',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'pageincat-desc' => 'Pśidawa parserowu funkciju <code><nowiki>{{#incat:...}}</nowiki></code>, aby zwěsćiło, lěc aktualny bok jo w pódanej kategoriji',
+);
+
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'pageincat-desc' => "Ajoute une fonction de l'analyseur <code><nowiki>{{#incat:...}}</nowiki></code> pour déterminer si la page courante est dans une catégorie spécifiée",
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'pageincat-desc' => 'Engade unha función analítica, <code><nowiki>{{#incat:...}}</nowiki></code>, para determinar se a páxina actual está presente nunha categoría especificada',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'pageincat-desc' => 'Přidawa parserowu funkciju <code><nowiki>{{#incat...}}</nowiki></code>, zo by zwěsćiło, hač aktualna strona je w podatej kategoriji',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'pageincat-desc' => 'Voegt de parserfunctie <code><nowiki>{{#incat:...}}</nowiki></code> toe om te bepalen of de huidige pagina in een bepaalde categorie valt',
+);
+
