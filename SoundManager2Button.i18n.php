31895caba9c42292198dd1303651779ff546489d
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-19 20:15:36 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106683 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SoundManager2Button.i18n.php
+++ SoundManager2Button.i18n.php
@@ -20,3 +20,60 @@ $messages['qqq'] = array(
 	'soundmanager2button-play' => 'Play button text, primarily for screenreaders.',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'soundmanager2button-desc' => 'Ermöglicht das Einbetten des MP3-Players SoundManager 2 in eine Wikiseite',
+	'soundmanager2button-play' => 'Abspielen',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'soundmanager2button-desc' => 'Zasajźijo MP3-wótegrawak SoundManager 2 do wikijowego boka',
+	'soundmanager2button-play' => 'Wótegraś',
+);
+
+/** French (Français)
+ * @author Seb35
+ */
+$messages['fr'] = array(
+	'soundmanager2button-desc' => 'Inclut un lecteur MP3 SoundManager 2 dans une page wiki',
+	'soundmanager2button-play' => 'Lire',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'soundmanager2button-desc' => 'Incorpora un reprodutor MP3 SoundManager 2 ás páxinas do wiki',
+	'soundmanager2button-play' => 'Reproducir',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'soundmanager2button-desc' => 'Zasadźi MP3-wothrawak SoundManager 2 do wikijoweje strony',
+	'soundmanager2button-play' => 'Wothrać',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'soundmanager2button-desc' => 'Го вметнува MP3-изведувачот SoundManager 2 во викистраница',
+	'soundmanager2button-play' => 'Пушти',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'soundmanager2button-desc' => 'Maakt het mogelijk een SoundManager 2 MP3-speler op te nemen in een wikipagina',
+	'soundmanager2button-play' => 'Afspelen',
+);
+
