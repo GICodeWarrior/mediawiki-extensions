f4347be03db5bc880d83586b712ef877bdcf086a
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-19 09:54:42 +0000

    r106628: Consistency tweaks in preparation for adding extension to translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106631 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PageInCat.i18n.php
+++ PageInCat.i18n.php
@@ -9,9 +9,10 @@
 $messages = array();
 
 $messages['en'] = array(
-	'pageincat-desc' => 'Adds a parser function <code><nowiki>{{#incat:...}}</nowiki></code> to determine if the current page is in a specified category.',
+	'pageincat-desc' => 'Adds a parser function <code><nowiki>{{#incat:...}}</nowiki></code> to determine if the current page is in a specified category',
 );
 
 $messages['qqq'] = array(
 	'pagesincat-desc' => '{{desc}}',
 );
+
