ea5a1923e41b519897110b11b0b9cedf8b069c40
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-20 20:47:29 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106858 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MarkAsHelpful.i18n.php
+++ MarkAsHelpful.i18n.php
@@ -28,5 +28,76 @@ This is a feature in development. See [[mw:MarkAsHelpful]] for background inform
 	'mah-mark-text' => 'Text to prompt the user to mark this item as helpful',
 	'mah-you-marked-text' => 'Text displayed to the logged in user if they mark an item helpful',
 	'mah-someone-marked-text' => 'Text displayed as to who marked this is helpful, shown if not the user who marked {$1 is the username}',
-	'mah-undo-mark-text' => 'Text for the the undo mark link',	
+	'mah-undo-mark-text' => 'Text for the the undo mark link',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'markashelpful-desc' => 'Ermöglicht eine Benutzeroberfläche mit der Kommentare als hilfreich markiert werden können',
+	'mah-mark-text' => 'Als hilfreich markieren',
+	'mah-you-marked-text' => 'Du meinst, dass dieser Kommentar hilfreich ist.',
+	'mah-someone-marked-text' => '$1 meint, dass dieser Kommentar hilfreich ist.',
+	'mah-undo-mark-text' => 'rückgängig machen',
+);
+
+/** German (formal address) (‪Deutsch (Sie-Form)‬)
+ * @author Kghbln
+ */
+$messages['de-formal'] = array(
+	'mah-you-marked-text' => 'Sie meinen, dass dieser Kommentar hilfreich ist.',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'markashelpful-desc' => 'Proporciona unha interface de usuario para marcar comentarios como útiles',
+	'mah-mark-text' => 'Marcar isto como útil',
+	'mah-you-marked-text' => 'Pensa que isto é útil',
+	'mah-someone-marked-text' => '$1 pensa que isto é útil',
+	'mah-undo-mark-text' => 'desfacer',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'markashelpful-desc' => 'Staja wužiwarski powjerch k dispoziciji, zo bychu so komentary jako pomocliwe markěrowali',
+	'mah-mark-text' => 'Jako pomocliwy markěrować',
+	'mah-you-marked-text' => 'Maš to za pomocliwe',
+	'mah-someone-marked-text' => '$1 ma to za pomocliwe',
+	'mah-undo-mark-text' => 'cofnyć',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'mah-mark-text' => 'Dëst als nëtzlech markéieren',
+	'mah-undo-mark-text' => 'réckgängeg maachen',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'markashelpful-desc' => 'Овозможува кориснички посредник за означување на коментари како корисни (т.е. од помош)',
+	'mah-mark-text' => 'Означете го ова како корисно',
+	'mah-you-marked-text' => 'Сметате дека ова е корисно',
+	'mah-someone-marked-text' => '$1 смета дека ова е корисно',
+	'mah-undo-mark-text' => 'врати',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'markashelpful-desc' => 'Biedt een gebruikersinterface om opmerkingen als nuttig te markeren',
+	'mah-mark-text' => 'Als nuttig markeren',
+	'mah-you-marked-text' => 'U denkt dat dit nuttig is',
+	'mah-someone-marked-text' => '{{GENDER:$1|$1}} denkt dat dit nuttig is',
+	'mah-undo-mark-text' => 'ongedaan maken',
+);
+
