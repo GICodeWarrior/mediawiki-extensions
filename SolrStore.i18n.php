f9fafc5df73c29f725741e7efbaea44f5f402343
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-20 20:47:29 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106858 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SolrStore.i18n.php
+++ SolrStore.i18n.php
@@ -16,18 +16,66 @@ $messages['en'] = array(
 	'solrstore-searchFieldSets-title' => 'SolrSearch: SearchSet select',
 );
 
+/** Message documentation (Message documentation) */
 $messages['qqq'] = array(
-	'solrconnector-desc' =>  '{{desc}}',
-	'solrsearch' => 'The title of the Special:SolrSearch page as appearing in Special:SpecialPages. Should probably the same as message \'\'searchFieldSets\'\'',
+	'solrsearch' => "The title of the Special:SolrSearch page as appearing in Special:SpecialPages. Should probably the same as message ''searchFieldSets''",
 	'solrstore-searchFieldSets' => 'The pagetitle (heading) of the Special:SolrSearch page',
 	'solrstore-searchFieldSets-select' => 'A message',
 	'solrstore-searchFieldSets-title' => 'The title of the Special:SolrSearch page',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
 $messages['de'] = array(
+	'solrstore-desc' => 'Ermöglicht die Suche nach semantischen Attributen mit einer Volltextsuchmaschine',
 	'solrsearch' => 'SolrSuche',
 	'solrstore-searchFieldSets' => 'SolrSuche',
 	'solrstore-searchFieldSets-select' => 'Bitte wähle ein SearchSet aus',
 	'solrstore-searchFieldSets-title' => 'SolrSearch: SearchSet Auswahl',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'solrstore-desc' => 'Procurar propiedades semánticas co poder dun motor de procuras de texto',
+	'solrsearch' => 'SolrSearch',
+	'solrstore-searchFieldSets' => 'SolrSearch',
+	'solrstore-searchFieldSets-select' => 'Seleccione un conxunto de procuras',
+	'solrstore-searchFieldSets-title' => 'SolrSearch: Selección do conxunto de procuras',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'solrstore-desc' => 'Pytanje za semantiskimi atributami z połnotekstowej pytawu',
+	'solrsearch' => 'SolrSearch',
+	'solrstore-searchFieldSets' => 'SolrSearch',
+	'solrstore-searchFieldSets-select' => 'Prošu wubjer pytansku sadźbu',
+	'solrstore-searchFieldSets-title' => 'SolrSearch: Wuběr pytanskich sadźbow',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'solrstore-desc' => 'Пребарување на семантички својства со способностите на пребарувачот на полн текст',
+	'solrsearch' => 'SolrSearch',
+	'solrstore-searchFieldSets' => 'SolrSearch',
+	'solrstore-searchFieldSets-select' => 'Изберете SearchSet',
+	'solrstore-searchFieldSets-title' => 'SolrSearch: Избор на SearchSet',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'solrstore-desc' => 'Semantische eigenschappen doorzoeken met de kracht van een fulltext zoekmachine',
+	'solrsearch' => 'SolrSearch',
+	'solrstore-searchFieldSets' => 'SolrSearch',
+	'solrstore-searchFieldSets-select' => 'Selecteer een SearchSet',
+	'solrstore-searchFieldSets-title' => 'SolrSearch: SearchSet selecteren',
 );
 
