GICodeWarrior/mediawiki-extensions

commit 5da5afafa4373d8dc52c2f5f5419914ba71bd625

Add an option option to not perform a global login (bug 20852). This also provides a solution to bug 24471 as clarified in bug 14407#c30
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@75041 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

CentralAuthHooks.php
@@ -101,7 +101,7 @@ class CentralAuthHooks {
 	}
 
 	static function onUserLoginComplete( &$user, &$inject_html ) {
-		global $wgCentralAuthCookies;
+		global $wgCentralAuthCookies, $wgRequest;
 		if ( !$wgCentralAuthCookies ) {
 			// Use local sessions only.
 			return true;
@@ -118,6 +118,11 @@ class CentralAuthHooks {
 		if ( !$wgCentralAuthAutoLoginWikis ) {
 			wfLoadExtensionMessages( 'SpecialCentralAuth' );
 			$inject_html .= wfMsgExt( 'centralauth-login-no-others', 'parsemag' );
+			return true;
+		}
+
+		if ( !$wgRequest->getCheck( 'wpCentralLogin' ) ) {
+			// The user requested to log in just on this wiki
 			return true;
 		}
 

CentralAuthPlugin.php
@@ -249,4 +249,21 @@ class CentralAuthPlugin extends AuthPlugin {
 	public function getUserInstance( User &$user ) {
 		return CentralAuthUser::getInstance( $user );
 	}
+	
+	public function modifyUITemplate( &$template, &$type ) {
+		global $wgCentralAuthCookies;
+
+		$template->set( 'usedomain', false );
+
+		if ( !$wgCentralAuthCookies ) return;
+		
+		$label = Xml::checkLabel( wfMsg( 'centralauth-login-global' ), 'wpCentralLogin', 'wpCentralLogin', true, array( 'tabindex' => '4' ) );
+		$field = <<<HTML
+		<tr id="mw-centralauth-login">
+			<td></td>
+			<td class="mw-input">$label</td>
+		</tr>
+HTML;
+		$template->set( 'extrafields', $field );
+	}
 }

CentralAuth.i18n.php
@@ -35,6 +35,7 @@ No changes will be made until you have confirmed that things are in order.',
 	'centralauth-merge-step2-detail' => "Some of the accounts could not be automatically matched to the designated home wiki.
 If these accounts belong to you, you can confirm that they are yours by providing the password for them.",
 	'centralauth-merge-step2-submit' => 'Confirm login information',
+	'centralauth-login-global'       => 'Log me globally',
 
 	// Dry run messages
 	'centralauth-merge-dryrun-complete'   => 'All existing accounts can be automatically unified!

commit 25f8ed31629825e1e6bc9176385fb1dc3a30e1f0

* (bug 30671) CentralAuth should globally autologin user on account creation
Fixed per Jeroens comment on the bug

Problem was introduced in r75041 when adding the awful "Also log me in to other wikis of the Wikimedia Foundation" checkbox...

git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106801 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

CentralAuthHooks.php
@@ -164,8 +164,11 @@ class CentralAuthHooks {
 			return true;
 		}
 
-		if ( !$wgRequest->getCheck( 'wpCentralLogin' ) ) {
+		if ( !$wgRequest->getCheck( 'wpCentralLogin' ) && !$wgRequest->getVal( 'wpCreateaccount' ) ) {
 			// The user requested to log in just on this wiki
+			// Or they just created an account
+			// Thankfully, we don't have the awful "Also log me in to other wikis of the Wikimedia Foundation"
+			// on the registration form too... Though, that would've caused this to not happen
 			return true;
 		}
 

CentralAuthPlugin.php
@@ -260,7 +260,9 @@ class CentralAuthPlugin extends AuthPlugin {
 
 		$template->set( 'usedomain', false );
 
-		if ( !$wgCentralAuthCookies ) return;
+		if ( !$wgCentralAuthCookies ) {
+			return;
+		}
 
 		$label = Xml::checkLabel( wfMsg( 'centralauth-login-global' ), 'wpCentralLogin', 'wpCentralLogin', true, array( 'tabindex' => '4' ) );
 		$field = <<<HTML