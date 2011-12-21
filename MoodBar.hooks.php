1abc51e0538385fce071b17d55ddcdbe7a76756c
bsitu <bsitu@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-21 23:44:09 +0000

    follow up to -r106844 - alter the logic for Mark As Helpful hook function in Moodbar
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107008 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MoodBar.hooks.php
+++ MoodBar.hooks.php
@@ -22,43 +22,45 @@ class MoodBarHooks {
 	 * @param $type string - the object type to be marked
 	 * @param $item int - an item of $type to be marked 
 	 * @param $User User Object - the User in current session
+	 * @param $isAbleToMark bool - determine whether the user is able to mark the item
 	 * @return bool
 	 */
-	public static function onMarkItemAsHelpful( $mahaction, $type, $item, $User ) {
+	public static function onMarkItemAsHelpful( $mahaction, $type, $item, $User, &$isAbleToMark ) {
 		
-		$isAbleToMark = true;
-		
-		switch ( $mahaction ) {
+		if ( $type == 'mbresponse' ) {
 			
-			case 'mark':
-				$dbr = wfGetDB( DB_SLAVE );
+			switch ( $mahaction ) {
 				
-				$conds = array( 'mbf_id = mbfr_mbf_id', 'mbfr_id' => intval( $item ) );
+				case 'mark':
+					$dbr = wfGetDB( DB_SLAVE );
+					
+					$conds = array( 'mbf_id = mbfr_mbf_id', 'mbfr_id' => intval( $item ) );
+					
+					if ( !$User->isAnon() ) {
+						$conds['mbf_user_id'] = $User->getId();		
+					}
+					else {
+						$conds['mbf_user_ip'] = $User->getName();
+					}
+					
+					$res = $dbr->selectRow( array( 'moodbar_feedback', 'moodbar_feedback_response' ), 
+								array( 'mbf_id' ),
+								$conds,
+								__METHOD__ );	
+					
+					if ( $res === false ) {
+						$isAbleToMark = false;
+					}
+				break;
 				
-				if ( !$User->isAnon() ) {
-					$conds['mbf_user_id'] = $User->getId();		
-				}
-				else {
-					$conds['mbf_user_ip'] = $User->getName();
-				}
-				
-				$res = $dbr->selectRow( array( 'moodbar_feedback', 'moodbar_feedback_response' ), 
-			             			array( 'mbf_id' ),
-			             			$conds,
-			             			__METHOD__ );	
-			        
-			        if ( $res === false ) {
-			        	$isAbleToMark = false;
-			        }
-			break;
-			
-			case 'unmark':
-			default:
-				//We will leve the MarkAsHelpFul extension to check if the user has unmark right
-		        break;
+				case 'unmark':
+				default:
+					//We will leve the MarkAsHelpFul extension to check if the user has unmark right
+				break;
+			}
 		}
-
-		return $isAbleToMark;
+		
+		return true;
 		
 	}
 
