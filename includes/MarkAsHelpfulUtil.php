5a67d88015a66f7439ebe5eb71a822ef2b09f429
bsitu <bsitu@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-21 00:35:56 +0000

    updated the template and moved template generation for different cases to individual functions
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@106906 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/MarkAsHelpfulUtil.php
+++ includes/MarkAsHelpfulUtil.php
@@ -1,58 +1,73 @@
 <?php
 
+/**
+ * Utility class for Mark As Helpful
+ */
 class MarkAsHelpfulUtil {
 
 	public static function getMarkAsHelpfulTemplate( $user, $isAbleToMark, $helpfulUserList, $type, $item ) {
+		
 		if ( $user->isAnon() ) {
-
-			$userList = '';
-
-			foreach ( $helpfulUserList as $val ) {
-				$userList .= $val['user_name'] . ' ';
-			}
-
-			$data = '';
-
-			if ( $userList ) {
-				$data = wfMessage( 'mah-someone-marked-text' )->params( $userList )->escaped();
-			}
-
-			return <<<HTML
-			<div class="mw-mah-wrapper">
-				$data
-			</div>
-HTML;
+			$html = self::otherMarkedTemplate( $helpfulUserList );
 		} else {
-			$userList = '';
 			$userId = $user->getId();
 
 			if ( isset( $helpfulUserList[$userId] ) ) {
-				$mahMarkedText = wfMessage( 'mah-you-marked-text' )->escaped();
-				$undoLinkText = wfMessage( 'mah-undo-mark-text' )->escaped();
-
-				return <<<HTML
-				<div class="mw-mah-wrapper">
-					<span class='mah-helpful-marked-state'>
-						$mahMarkedText
-					</span>
-					&nbsp;(<a class='markashelpful-undo'>$undoLinkText</a>)
-				</div>
-HTML;
+				$html = self::ownerMarkedTemplate();
 			} else {
 				if ( $isAbleToMark ) {
-					$mahLinkText = wfMessage( 'mah-mark-text' )->escaped();
-					return <<<HTML
-					<div class="mw-mah-wrapper">
-						<a class='mah-helpful-state markashelpful-mark'>
-							$mahLinkText
-						</a>
-					</div>
-HTML;
+					$html = self::requestToMarkTemplate();
+				}
+				else {
+					$html = self::otherMarkedTemplate( $helpfulUserList );
 				}
 
 			}
 		}
 
+		return '<div class="mw-mah-wrapper">' . $html . '</div>';
+		
+	}
+	
+	private static function otherMarkedTemplate( $helpfulUserList ) {
+		$userList = '';
+
+		foreach ( $helpfulUserList as $val ) {
+			$userList .= $val['user_name'] . ' ';
+		}
+
+		$data = '';
+
+		if ( $userList ) {
+			$data = wfMessage( 'mah-someone-marked-text' )->params( $userList )->escaped();
+		}
+
+		return <<<HTML
+				<span class='mah-helpful-marked-state'>
+					$data
+				</span>
+HTML;
+	}
+	
+	private static function ownerMarkedTemplate() {
+		$mahMarkedText = wfMessage( 'mah-you-marked-text' )->escaped();
+		$undoLinkText = wfMessage( 'mah-undo-mark-text' )->escaped();
+
+		return <<<HTML
+			<span class='mah-helpful-marked-state'>
+				$mahMarkedText
+			</span>
+			&nbsp;(<a class='markashelpful-undo'>$undoLinkText</a>)
+HTML;
 	}
 
+	private static function requestToMarkTemplate() {
+		$mahLinkText = wfMessage( 'mah-mark-text' )->escaped();
+		return <<<HTML
+			<a class='mah-helpful-state markashelpful-mark'>
+				$mahLinkText
+			</a>
+HTML;
+	}
+	
 }
