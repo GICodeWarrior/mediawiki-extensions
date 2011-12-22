947af4f5fee7bce7da74adb2365a2afb2b6ee1c1
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-22 20:15:58 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107106 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- NoBogusUserpages.i18n.php
+++ NoBogusUserpages.i18n.php
@@ -41,3 +41,55 @@ $messages['qqq'] = array(
 	'right-createbogususerpage' => '{{doc-right|createbogususerpage}}',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'nobogususerpages-desc' => 'Verhindert das Erstellen von Benutzerseiten zu denen kein Benutzerkonto vorhanden ist',
+	'badaccess-bogususerpage' => 'Zu dieser Benutzerseite ist kein Benutzerkonto vorhanden. Du bist nicht berechtigt diese Benutzerseite zu erstellen.',
+	'right-createbogususerpage' => 'Benutzerseiten zu nicht vorhandenen Benutzerkonten erstellen',
+);
+
+/** German (formal address) (‪Deutsch (Sie-Form)‬)
+ * @author Kghbln
+ */
+$messages['de-formal'] = array(
+	'badaccess-bogususerpage' => 'Zu dieser Benutzerseite ist kein Benutzerkonto vorhanden. Sie sine nicht berechtigt diese Benutzerseite zu erstellen.',
+);
+
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'nobogususerpages-desc' => "Empêcher la création de pages utilisateur pour lesquelles il n'y a pas d'utilisateur existant",
+	'badaccess-bogususerpage' => "L'utilisateur de cette page utilisateur n'existe pas. Vous n'avez pas le droit de créer une fausse page utilisateur.",
+	'right-createbogususerpage' => 'Créer des pages utilisateur pour des utilisateurs qui ne sont pas enregistrés sur ce wiki',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'nobogususerpages-desc' => 'Zadźěwa wutworjenju wužiwarskich stronow, za kotrež wužiwar njeeksistuje',
+	'badaccess-bogususerpage' => 'Wužiwar tuteje wužiwarskeje strony njeeksistuje. Nimaš prawa, zo by wopačnu wužiwarsku stronu wutworił.',
+	'right-createbogususerpage' => 'Wužiwarske strony za wužiwarjow wutowrić, kotřiž njejsu na tutym wikiju zregistrowani',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'nobogususerpages-desc' => 'Го спречува создавањето на кориснички страници за непостоечки корисници',
+	'badaccess-bogususerpage' => 'Корисникот на оваа страница не постои. Немате права да создадете божемна корисничка страница.',
+	'right-createbogususerpage' => 'Создавање на кориснички страници за нерегистрирани корисници',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'nobogususerpages-desc' => "Beperkt het aanmaken van gebruikerspagina's waar een gebruiker niet bestaat",
+	'badaccess-bogususerpage' => 'De gebruiker voor deze gebruikerspagina bestaat niet. U hebt geen rechten om deze gebruikerspagina aan te maken.',
+	'right-createbogususerpage' => "Gebruikerspagina's aanmaken voor gebruikers die niet bekend zijn in deze wiki",
+);
+
