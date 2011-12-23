0c559090e0a049e270ff8b881f39c0153e83c3d9
nikerabbit <nikerabbit@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-23 08:45:55 +0000

    Stylize
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107140 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/MarkAsHelpfulItem.php
+++ includes/MarkAsHelpfulItem.php
@@ -39,7 +39,7 @@ class MarkAsHelpfulItem {
 	 * @param $key string - the name of a property
 	 */
 	public function getProperty( $key ) {
-		if( array_key_exists( $key, $this->property) ) {
+		if ( array_key_exists( $key, $this->property ) ) {
 			return $this->property[$key];
 		} else {
 			return null;
@@ -48,11 +48,11 @@ class MarkAsHelpfulItem {
 
 	/**
 	 * Setter method
-	 * @param $key string - the name of the property 
+	 * @param $key string - the name of the property
 	 * @param $value mixed - the valud of the property
 	 */
 	public function setProperty( $key, $value ) {
-		if( array_key_exists( $key, $this->property) ) {
+		if ( array_key_exists( $key, $this->property ) ) {
 			$this->property[$key] = $value;
 		}
 	}
@@ -137,23 +137,23 @@ class MarkAsHelpfulItem {
 	 * @exception MWMarkAsHelpFulItemSearchKeyException
 	 */
 	public function loadFromDatabase( $conds ) {
-		
+
 		$searchKey = array_keys ( $conds );
-		
+
 		$flag = sort( $searchKey );
-		
+
 		if ( !$flag ) {
 			return false;
 		}
-		
-		$searchKey = implode(',', $searchKey );	
-	
+
+		$searchKey = implode( ',', $searchKey );
+
 		$allowableSearchKey = array ( 'mah_id', 'mah_item,mah_type,mah_user_id,mah_user_ip' );
-		
+
 		if ( !in_array( $searchKey, $allowableSearchKey ) ) {
 			throw new MWMarkAsHelpFulItemSearchKeyException( 'Invalid search key!' );
 		}
-		
+
 		$dbr = wfGetDB( DB_SLAVE );
 
 		$res = $dbr->selectRow(
@@ -163,8 +163,8 @@ class MarkAsHelpfulItem {
 			__METHOD__
 		);
 
-		if( $res !== false ) {
-			foreach( $this->property as $key => $val ) {
+		if ( $res !== false ) {
+			foreach ( $this->property as $key => $val ) {
 				$this->setProperty( $key, $res->$key );
 			}
 
@@ -178,7 +178,7 @@ class MarkAsHelpfulItem {
 	/**
 	 * To mark an item as helpful, this function should be called after either loadFromRequest() or setProperty()
 	 * data must be validated if called from setProperty()
-	 * 
+	 *
 	 */
 	public function mark() {
 		if ( $this->userHasMarked() ) {
@@ -205,15 +205,15 @@ class MarkAsHelpfulItem {
 	 * @param $currentUser Object - the current user who is browsing the site
 	 */
 	public function unmark( $currentUser ) {
-		
+
 		if ( $currentUser->isAnon() ) {
 			return;
 		}
-		
+
 		if ( $this->getProperty( 'mah_id' ) ) {
-			
+
 			if ( !$this->getProperty( 'mah_type' ) ) {
-				if( !$this->loadFromDatabase( array( 'mah_id' => $this->getProperty( 'mah_id' ) ) ) ) {
+				if ( !$this->loadFromDatabase( array( 'mah_id' => $this->getProperty( 'mah_id' ) ) ) ) {
 					return;
 				}
 			}
@@ -221,11 +221,11 @@ class MarkAsHelpfulItem {
 			$user = $this->getUser();
 
 			if ( $user ) {
-				
+
 				if ( !$user->isAnon() ) {
-					
+
 					if (  $currentUser->getId() == $user->getId() ) {
-					
+
 						$dbw = wfGetDB( DB_MASTER );
 
 						$dbw->delete(
@@ -233,11 +233,11 @@ class MarkAsHelpfulItem {
 							array( 'mah_id' => $this->getProperty( 'mah_id' ) ),
 							__METHOD__
 						);
-						
+
 					}
-						
+
 				}
-				
+
 			}
 		}
 
@@ -310,9 +310,9 @@ class MarkAsHelpfulItem {
 
 		$list = array();
 
-		foreach( $res AS $val ) {
-			$list[$val->user_id] = array( 'user_name' => $val->user_name, 
-				                      'user_id' => $val->user_id, 
+		foreach ( $res AS $val ) {
+			$list[$val->user_id] = array( 'user_name' => $val->user_name,
+				                      'user_id' => $val->user_id,
 				                      'user_ip' => $val->mah_user_ip );
 		}
 
