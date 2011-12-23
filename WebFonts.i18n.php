66ed33ad81c5627ba5ae62fabc4fadc65c39809d
santhosh <santhosh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-23 08:40:45 +0000

    Correct  the page link since it is moved in mediawiki.org.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107138 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WebFonts.i18n.php
+++ WebFonts.i18n.php
@@ -19,7 +19,7 @@ $messages['en'] = array(
 	'webfonts-enable-preference' => 'Enable font embedding (WebFonts)',
 	'webfonts-menu-tooltip' => 'Select a font for the page',
 	'webfonts-help' => 'Help',
-	'webfonts-help-page' => '//mediawiki.org/wiki/Special:MyLanguage/Help:Web_fonts',
+	'webfonts-help-page' => '//mediawiki.org/wiki/Special:MyLanguage/Help:Extension:WebFonts',
 );
 
 /** Message documentation (Message documentation)
