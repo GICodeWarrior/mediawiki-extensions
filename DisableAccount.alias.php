64388debe8a6d46cfb8c67dc25925b0e9eb25b74
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 22:19:29 +0000

    Localisation updates for special pages and magic words for extensions from http://translatewiki.net.
    Backwards compatibility with MediaWiki 1.15 and earlier dropped for special page aliases.
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107294 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- DisableAccount.alias.php
+++ DisableAccount.alias.php
@@ -7,21 +7,37 @@
 
 $specialPageAliases = array();
 
-/** English
- * @author Andrew Garrett
- */
+/** English (English) */
 $specialPageAliases['en'] = array(
 	'DisableAccount' => array( 'DisableAccount' ),
 );
 
-/** German (Deutsch)
- * @author SVG
- */
+/** German (Deutsch) */
 $specialPageAliases['de'] = array(
 	'DisableAccount' => array( 'Benutzerkonto_deaktivieren' ),
 );
 
-/**
- * For backwards compatibility with MediaWiki 1.15 and earlier.
- */
-$aliases =& $specialPageAliases;
+/** 湘语 (湘语) */
+$specialPageAliases['hsn'] = array(
+	'DisableAccount' => array( '关闭账户' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'DisableAccount' => array( 'DeaktiveKont' ),
+);
+
+/** Khmer (ភាសាខ្មែរ) */
+$specialPageAliases['km'] = array(
+	'DisableAccount' => array( 'បិទគណនី' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'DisableAccount' => array( 'GebruikerUitschakelen' ),
+);
+
+/** Serbian (Cyrillic script) (‪Српски (ћирилица)‬) */
+$specialPageAliases['sr-ec'] = array(
+	'DisableAccount' => array( 'Онемогући_налог' ),
+);
