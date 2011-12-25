1074fcc03dd7d02895ef90350ddb83ca4cb82fe7
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 09:42:00 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107261 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Checkpoint.i18n.php
+++ Checkpoint.i18n.php
@@ -384,7 +384,7 @@ $messages['nl'] = array(
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Nghtwlkr
  */
-$messages['no'] = array(
+$messages['nb'] = array(
 	'checkpoint' => 'Lagre og fortsett redigeringen',
 	'checkpoint-desc' => 'Gjør det mulig å lagre en endring og fortsette redigeringen',
 	'checkpoint-tooltip' => 'Lagre fremgangen din og fortsett redigeringen av siden',
