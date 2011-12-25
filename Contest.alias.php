fc3199e91d05b8790f9aff200a731f314d7c6221
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 22:19:29 +0000

    Localisation updates for special pages and magic words for extensions from http://translatewiki.net.
    Backwards compatibility with MediaWiki 1.15 and earlier dropped for special page aliases.
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107294 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Contest.alias.php
+++ Contest.alias.php
@@ -24,3 +24,69 @@ $specialPageAliases['en'] = array(
 	'EditContest' => array( 'EditContest' ),
 	'MyContests' => array( 'MyContests', 'ContestSubmission', 'My contests' ),
 );
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'Contest' => array( 'Wettbewerb' ),
+	'Contestant' => array( 'Wettbewerbsteilnehmer' ),
+	'Contests' => array( 'Wettbewerbe' ),
+	'ContestSignup' => array( 'Wettbewerbsanmeldung' ),
+	'ContestWelcome' => array( 'Wettbewerbsdarstellung' ),
+	'EditContest' => array( 'Wettbewerb_bearbeiten' ),
+	'MyContests' => array( 'Meine_Wettbewerbe' ),
+);
+
+/** Galician (Galego) */
+$specialPageAliases['gl'] = array(
+	'Contest' => array( 'Concurso' ),
+	'Contestant' => array( 'Concursante' ),
+	'Contests' => array( 'Concursos' ),
+	'ContestSignup' => array( 'Rexistro_no_concurso' ),
+	'ContestWelcome' => array( 'Benvida_ao_concurso' ),
+	'EditContest' => array( 'Editar_o_concurso' ),
+	'MyContests' => array( 'Os_meus_concursos' ),
+);
+
+/** Interlingua (Interlingua) */
+$specialPageAliases['ia'] = array(
+	'Contest' => array( 'Concurso' ),
+	'Contestant' => array( 'Concurrente' ),
+	'Contests' => array( 'Concursos' ),
+	'ContestSignup' => array( 'Inscriber_pro_concurso' ),
+	'ContestWelcome' => array( 'Benvenite_al_concurso' ),
+	'EditContest' => array( 'Modificar_concurso' ),
+	'MyContests' => array( 'Mi_concursos' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'Contest' => array( 'Конкурс' ),
+	'Contestant' => array( 'Учесник' ),
+	'Contests' => array( 'Конкурси' ),
+	'ContestSignup' => array( 'КонкурсПријава' ),
+	'ContestWelcome' => array( 'КонкурсДобредојдовте' ),
+	'EditContest' => array( 'УредиКонкурс' ),
+	'MyContests' => array( 'МоиКонкурси' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'Contest' => array( 'മത്സരം' ),
+	'Contestant' => array( 'മത്സരാർത്ഥി' ),
+	'Contests' => array( 'മത്സരങ്ങൾ' ),
+	'ContestSignup' => array( 'മത്സരത്തിൽപേരുചേർക്കൽ' ),
+	'ContestWelcome' => array( 'മത്സരത്തിലേയ്ക്ക്സ്വാഗതം' ),
+	'EditContest' => array( 'മത്സരംതിരുത്തുക' ),
+	'MyContests' => array( 'എന്റെമത്സരങ്ങൾ', 'മത്സരസമർപ്പണങ്ങൾ', 'എന്റെ_മത്സരങ്ങൾ' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'Contest' => array( 'Wedstrijd' ),
+	'Contestant' => array( 'Deelnemer' ),
+	'Contests' => array( 'Wedstrijden' ),
+	'ContestSignup' => array( 'WedstrijdDeelnemen' ),
+	'ContestWelcome' => array( 'WedstrijdWelkom' ),
+	'EditContest' => array( 'WedstrijdBewerken' ),
+	'MyContests' => array( 'MijnWedstrijden' ),
+);
