d81b8285d6510bbf94f57ff0ed7b9ed67c002500
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 09:42:00 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107261 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SemanticGallery.i18n.php
+++ SemanticGallery.i18n.php
@@ -239,7 +239,7 @@ $messages['nn'] = array(
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Nghtwlkr
  */
-$messages['no'] = array(
+$messages['nb'] = array(
 	'semanticgallery-desc' => 'Legger til en galleriutdataformatering på SMW-spørringer',
 );
 
