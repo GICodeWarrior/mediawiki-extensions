e93b8d00d3e660b580fb1a1346bda0240f74146b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 20:25:45 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107287 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Livelets.i18n.php
+++ Livelets.i18n.php
@@ -163,18 +163,18 @@ $messages['ms'] = array(
 	'livelets-desc' => 'Membolehkan laman untuk ditransklusi yang dimuatkan selepas kandungan utama laman dan boleh dikemas kini secara dinamik dengan Ajax',
 );
 
-/** Dutch (Nederlands)
- * @author McDutchie
- */
-$messages['nl'] = array(
-	'livelets-desc' => "Staat het transcluderen van pagina's toe die geladen worden na de hoofdinhoud van de pagina en via AJAX dynamisch bijgewerkt kunnen worden",
-);
-
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Nghtwlkr
  */
 $messages['nb'] = array(
 	'livelets-desc' => 'Tillater sider å bli transkludert som gjør at de laster etter hovedsideinnholdet og kan oppdateres dynamisk med Ajax',
+);
+
+/** Dutch (Nederlands)
+ * @author McDutchie
+ */
+$messages['nl'] = array(
+	'livelets-desc' => "Staat het transcluderen van pagina's toe die geladen worden na de hoofdinhoud van de pagina en via AJAX dynamisch bijgewerkt kunnen worden",
 );
 
 /** Polish (Polski)
