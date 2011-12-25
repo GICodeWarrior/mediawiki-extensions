99c990531cb984210a3e16211ce258f1d5bbd1c4
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 09:42:00 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107261 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OpenSearchXml.i18n.php
+++ OpenSearchXml.i18n.php
@@ -337,7 +337,7 @@ $messages['nn'] = array(
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Jon Harald Søby
  */
-$messages['no'] = array(
+$messages['nb'] = array(
 	'opensearchxml-desc' => 'Grensesnitt for [http://opensearch.org/ OpenSearch]',
 );
 
