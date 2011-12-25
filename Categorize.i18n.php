4ce6497f2afdcc0f4e21268cdce07e7ea2c8ab63
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 09:42:00 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107261 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Categorize.i18n.php
+++ Categorize.i18n.php
@@ -209,7 +209,7 @@ $messages['nl'] = array(
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Event
  */
-$messages['no'] = array(
+$messages['nb'] = array(
 	'categorize-desc' => 'Legger til inndatafelt for redigerings- og opplastingsside for å tillate brukere å knytte kategorier til siden',
 	'categorize-title' => 'Kunnskapskvalifisering',
 	'categorize-footer' => 'Hvis du ønsker å legge til en etikett, vennligst kontakt en [[Special:ListUsers/sysop|administrator]].',
