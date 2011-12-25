c4a532e3fe4c02e59d2b1a4a3a2d823e079be51b
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 23:09:26 +0000

    Bye, bye $wgExtensionAliasesFiles (deprecated in 1.16).
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107299 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Farmer.php
+++ Farmer.php
@@ -76,7 +76,7 @@ $root = dirname( __FILE__ ) . '/';
 
 $wgExtensionMessagesFiles['MediaWikiFarmer'] = $root . 'Farmer.i18n.php';
 
-$wgExtensionAliasesFiles['MediaWikiFarmer'] = $root . 'Farmer.alias.php';
+$wgExtensionMessagesFiles['MediaWikiFarmerAlias'] = $root . 'Farmer.alias.php';
 
 $wgAutoloadClasses['MediaWikiFarmer'] = $root . 'MediaWikiFarmer.php';
 $wgAutoloadClasses['MediaWikiFarmer_Extension'] = $root . 'MediaWikiFarmer_Extension.php';
