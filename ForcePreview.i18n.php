d7371ed775953bffebdefb5f2449732a3995f4a5
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 09:42:00 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107261 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ForcePreview.i18n.php
+++ ForcePreview.i18n.php
@@ -440,7 +440,7 @@ $messages['nn'] = array(
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Jon Harald Søby
  */
-$messages['no'] = array(
+$messages['nb'] = array(
 	'forcepreview' => 'Lagre side (forhåndsvis først)',
 	'forcepreview-desc' => 'Tvinger brukere til å forhåndsvise siden dersom de ikke har riktige rettigheter',
 	'right-forcepreviewexempt' => 'Kan lagre uten å måtte se forhåndsvisning først',
