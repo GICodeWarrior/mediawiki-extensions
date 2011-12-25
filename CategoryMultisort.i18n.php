fb7b8a3d962a007b2469cb8e894de13f1fd3ef08
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-25 09:42:00 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107261 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- CategoryMultisort.i18n.php
+++ CategoryMultisort.i18n.php
@@ -547,7 +547,7 @@ $messages['nn'] = array(
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Nghtwlkr
  */
-$messages['no'] = array(
+$messages['nb'] = array(
 	'categorymultisort-desc' => 'Tillater kategorier å bli vist med forskjellige sorteringsnøkler',
 	'categorymultisort-defaultsortkey-name' => 'Standard',
 	'categorymultisort-sortkey' => 'Sorteringsnøkkel:',
