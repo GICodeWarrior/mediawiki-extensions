008672ac863f8a8793aca254e2672ad9ee525886
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-26 15:13:56 +0000

    Kill some <= 1.15 back compat
    Deletion of:
    
    /**
     * For backwards compatibility with MediaWiki 1.15 and earlier.
     */
    $aliases =& $specialPageAliases;
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107322 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiBhasha.alias.php
+++ WikiBhasha.alias.php
@@ -32,7 +32,3 @@ $specialPageAliases['en'] = array(
 	'WikiBhasha' => array( 'WikiBhasha' ),
 );
 
-/**
- * For backwards compatibility with MediaWiki 1.15 and earlier.
- */
-$aliases =& $specialPageAliases;
