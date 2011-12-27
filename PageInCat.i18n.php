070a75fcb60b56a8e571de1aa81e82582da8c304
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-27 20:23:52 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107412 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PageInCat.i18n.php
+++ PageInCat.i18n.php
@@ -20,10 +20,10 @@ $messages['qqq'] = array(
 
 *$1 is the list of categories (in a localized comma separated list with the last two items separated by {{msg-mw|and}}. The individual category names will be italicized).
 *$2 is how many categories',
-	'pageincat-very-wrong-warn' => 'Warning displayed during preview when editing a page if #incat parser function acted incorrectly (Acting incorrectly means acting as if page was not in category, but page actually is) . This can happen if someone does something like \'\'put this page in category foo only if its not in category foo\'\' or more generally when people include category links inside <code>#incat</code> functions. Compare this to {{msg-mw|pageincat-wrong-warn}}. Generally this error message can happen when support for checking actual categories in the preview is enabled (but the category functions still behave incorrectly), the other error message will be triggered when such support is disabled.
+	'pageincat-very-wrong-warn' => "Warning displayed during preview when editing a page if #incat parser function acted incorrectly (Acting incorrectly means acting as if page was not in category, but page actually is) . This can happen if someone does something like ''put this page in category foo only if its not in category foo'' or more generally when people include category links inside <code>#incat</code> functions. Compare this to {{msg-mw|pageincat-wrong-warn}}. Generally this error message can happen when support for checking actual categories in the preview is enabled (but the category functions still behave incorrectly), the other error message will be triggered when such support is disabled.
 
 *$1 is the list of categories (in a localized comma separated list with the last two items separated by {{msg-mw|and}}. The individual category names will be italicized).
-*$2 is how many categories',
+*$2 is how many categories",
 );
 
 /** German (Deutsch)
@@ -32,7 +32,8 @@ $messages['qqq'] = array(
  */
 $messages['de'] = array(
 	'pageincat-desc' => 'Ergänzt die Parserfunktion <code>#incat:</code> mit der ermittelt werden kann, ob sich die aktuelle Seite in einer angegebenen Kategorie befindet',
-	'pageincat-wrong-warn' => "'''Achtung:''' Die {{PLURAL:$2|Kategorie $1 wurde|Kategorien $1 wurden}} durch <code><nowiki>{{#incat:…}}</nowiki></code> falsch erkannt. Dadurch ist die Vorschau falsch. Die gespeicherte Version dieser Seite sollte korrekt angezeigt werden.",
+	'pageincat-wrong-warn' => "'''Achtung:''' Die {{PLURAL:$2|Kategorie $1 wurde|Kategorien $1 wurden}} durch <code>#incat:</code> falsch erkannt. Deswegen könnte die Vorschau fehlerhaft sein. Die gespeicherte Version dieser Seite sollte korrekt angezeigt werden.",
+	'pageincat-very-wrong-warn' => "'''Achtung:''' Die {{PLURAL:$2|Kategorie $1 wurde|Kategorien $1 wurden}} durch <code>#incat:</code> falsch erkannt. Deswegen könnte die Vorschau fehlerhaft sein. Dies kann durch die Angabe von Kategorien innerhalb der Funktionsangabe <code><nowiki>{{#incat:...}}</nowiki></code> verursacht werden und könnte daher zu einer inkonsistenten Anzeige führen.",
 );
 
 /** Lower Sorbian (Dolnoserbski)
@@ -49,6 +50,7 @@ $messages['dsb'] = array(
 $messages['fr'] = array(
 	'pageincat-desc' => "Ajoute une fonction de l'analyseur <code><nowiki>{{#incat:...}}</nowiki></code> pour déterminer si la page courante est dans une catégorie spécifiée",
 	'pageincat-wrong-warn' => "'''Attention:''' {{PLURAL:$2|La catégorie $1a mal été détectée|Les catégories $1 ont mal été détectées}} par <code><nowiki>{{#incat:...}}</nowiki></code>, et par conséquent, cet aperçu peut être incorrect. La version enregistrée de cette page devrait être affichée correctement.",
+	'pageincat-very-wrong-warn' => "'''Attention:''' {{PLURAL:$2|La catégorie $1a été mal détectée|Les catégories $1ont été mal détectées}} par <code><nowiki>{{#incat:...}}</nowiki></code>, et par conséquent cet aperçu peut être incorrect. Cela peut être causé par des catégories incluses à l'intérieur des déclarations <code><nowiki>{{#incat:...}}</nowiki></code>, et peut provoquer un affichage incohérent.",
 );
 
 /** Galician (Galego)
@@ -57,6 +59,7 @@ $messages['fr'] = array(
 $messages['gl'] = array(
 	'pageincat-desc' => 'Engade unha función analítica, <code><nowiki>{{#incat:...}}</nowiki></code>, para determinar se a páxina actual está presente nunha categoría especificada',
 	'pageincat-wrong-warn' => "'''Atención:''' <code><nowiki>{{#incat:...}}</nowiki></code> detectou incorrectamente {{PLURAL:$2|a categoría $1|as categorías $1}}. Debido a isto, esta vista previa pode non ser correcta. A versión gardada da páxina debería mostrarse correctamente.",
+	'pageincat-very-wrong-warn' => "'''Atención:''' <code><nowiki>{{#incat:...}}</nowiki></code> detectou incorrectamente {{PLURAL:$2|a categoría $1|as categorías $1}}. Debido a isto, esta vista previa pode non ser correcta. Poida que incluíse categorías dentro de declaracións <code><nowiki>{{#incat:...}}</nowiki></code>, o que pode provocar que non se mostre correctamente.",
 );
 
 /** Upper Sorbian (Hornjoserbsce)
@@ -65,6 +68,7 @@ $messages['gl'] = array(
 $messages['hsb'] = array(
 	'pageincat-desc' => 'Přidawa parserowu funkciju <code><nowiki>{{#incat...}}</nowiki></code>, zo by zwěsćiło, hač aktualna strona je w podatej kategoriji',
 	'pageincat-wrong-warn' => "'''Warnowanje:''' {{PLURAL:$2|Kategorija $1 je|Kategoriji $1 stej|Kategorije $1 su|Kategorije $1 su}} so přez <code><nowiki>{{#incat:...}}</nowiki></code> wopak {{PLURAL:$2|spóznała|spóznałoj|spóznali|spóznali}}, a tohodla móhł přehlad wopak być. Składowana wersija strony měła so korektnje zwobraznić.",
+	'pageincat-very-wrong-warn' => "'''Warnowanje:''' {{PLURAL:$2|Kategorija $1 je|Kategoriji $1 stej|Kategorije $1 su|Kategorije $1 su}} so přez <code><nowiki>{{#incat:...}}</nowiki></code> wopak {{PLURAL:$2|spóznała|spóznałoj|spóznali|spóznali}}, a tohodla móhł přehlad wopak być. Přičina móhła być, zo su so kategorije do wurazow <code><nowiki>{{#incat:...}}</nowiki></code> zapřijeli, štož móhło k inkonsistentnemu zwobraznjenju wjesć.",
 );
 
 /** Luxembourgish (Lëtzebuergesch)
@@ -80,7 +84,8 @@ $messages['lb'] = array(
  */
 $messages['mk'] = array(
 	'pageincat-desc' => 'Додава парсерска функција <code><nowiki>{{#incat:...}}</nowiki></code> за да се утврди дали тековната страница стои во назначена категорија',
-	'pageincat-wrong-warn' => "'''ПРЕДУПРЕДУВАЊЕ:''' {{PLURAL:$2|Категоријата $1 е погрешно утврдена|Категориите $1 се погрешно утврдени}} од страна на <code><nowiki>{{#incat:...}}</nowiki></code>. Ппоради тоа, овој преглед може да е неисправен. Зачуваната верзија на страницава треба да се прикажува на правилен начин.",
+	'pageincat-wrong-warn' => "'''ПРЕДУПРЕДУВАЊЕ:''' {{PLURAL:$2|Категоријата $1 е погрешно утврдена|Категориите $1 се погрешно утврдени}} од страна на <code><nowiki>{{#incat:...}}</nowiki></code>. Поради тоа, овој преглед може да е неисправен. Зачуваната верзија на страницава треба да се прикажува на правилен начин.",
+	'pageincat-very-wrong-warn' => "'''Предупредување:''' {{PLURAL:$2|Категоријата $1 е погрешно утврдена|Категориите $1 се погрешно утврдени}} од страна на <code><nowiki>{{#incat:...}}</nowiki></code>. Поради тоа, овој преглед може да е неисправен. Една можна причина е ако има категории ставени во искази со <code><nowiki>{{#incat:...}}</nowiki></code>. Проблемот може да произлегува и од недоследен приказ.",
 );
 
 /** Dutch (Nederlands)
