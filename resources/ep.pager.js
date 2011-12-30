291610db372f8f662eac7513e1ccd47896d2813a
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-30 23:58:56 +0000

    style fix
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107672 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- resources/ep.pager.js
+++ resources/ep.pager.js
@@ -6,36 +6,38 @@
  * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
  */
 
-(function( $, mw, ep ) { 
-	
-	$( document ).ready( function() {
-		
-		$( '.ep-pager-clear' ).click( function() {
+(function ( $, mw, ep ) {
+
+	$( document ).ready( function () {
+
+		$( '.ep-pager-clear' ).click( function () {
 			var $form = $( this ).closest( 'form' );
 			$form.find( 'select' ).val( '' );
 			$form.submit();
 			return false;
 		} );
-		
-		$( '.ep-pager-delete' ).click( function() {
+
+		$( '.ep-pager-delete' ).click( function () {
 			if ( confirm( mw.msg( 'ep-pager-confirm-delete' ) ) ) {
-				$this = $( this );
-				
+				var $this = $( this );
+
 				ep.api.remove(
 					{
 						'type': $this.attr( 'data-type' ),
 						'ids': [ $this.attr( 'data-id' ) ]
 					},
-					function( result ) {
+					function ( result ) {
 						if ( result.success ) {
-							$tr = $this.closest( 'tr' );
-							$table = $tr.closest( 'table' );
-							
+							var $tr = $this.closest( 'tr' );
+							var $table = $tr.closest( 'table' );
+
 							if ( $table.find( 'tr' ).length > 2 ) {
-								$tr.slideUp( 'slow', function() { $tr.remove(); } );
+								$tr.slideUp( 'slow', function () {
+									$tr.remove();
+								} );
 							}
 							else {
-								$table.slideUp( 'slow', function() {
+								$table.slideUp( 'slow', function () {
 									$table.remove();
 								} );
 							}
@@ -47,19 +49,19 @@
 				);
 			}
 		} );
-		
-		$( '.ep-pager-select-all' ).change( function() {
-			var a = $( this ).closest( 'table' ).find( 'input:checkbox' ).prop( 'checked', $( this ).is( ':checked' ) );
+
+		$( '.ep-pager-select-all' ).change( function () {
+			$( this ).closest( 'table' ).find( 'input:checkbox' ).prop( 'checked', $( this ).is( ':checked' ) );
 		} );
 
-		$( '.ep-pager-delete-selected' ).click( function() {
-            var $deleteButton = $( this );
+		$( '.ep-pager-delete-selected' ).click( function () {
+			var $deleteButton = $( this );
 			var $selectAllCheckbox = $( '#ep-pager-select-all-' + $( this ).attr( 'data-pager-id' ) );
-            var $table = $selectAllCheckbox.closest( 'table' );
+			var $table = $selectAllCheckbox.closest( 'table' );
 
 			var ids = [];
 
-            $table.find( 'input[type=checkbox]:checked' ).each( function( i, element ) {
+			$table.find( 'input[type=checkbox]:checked' ).each( function ( i, element ) {
 				ids.push( $( element ).val() );
 			} );
 
@@ -74,21 +76,21 @@
 					'type': $( this ).attr( 'data-type' ),
 					'ids': ids
 				},
-				function( result ) {
+				function ( result ) {
 					if ( result.success ) {
-                        if ( ids.length > 0 && ( $table.find( 'tr' ).length - ids.length > 1 ) ) {
-                            for ( i in ids ) {
-                                if ( ids.hasOwnProperty( i ) ) {
-                                    $( '#select-' + pagerId + '-' + ids[i] ).closest( 'tr' ).remove();
-                                }
-                            }
-                        }
-                        else {
-                            $table.slideUp( 'slow', function() {
-                                $table.remove();
-                                $deleteButton.closest( 'fieldset' ).remove();
-                            } );
-                        }
+						if ( ids.length > 0 && ( $table.find( 'tr' ).length - ids.length > 1 ) ) {
+							for ( i in ids ) {
+								if ( ids.hasOwnProperty( i ) ) {
+									$( '#select-' + pagerId + '-' + ids[i] ).closest( 'tr' ).remove();
+								}
+							}
+						}
+						else {
+							$table.slideUp( 'slow', function () {
+								$table.remove();
+								$deleteButton.closest( 'fieldset' ).remove();
+							} );
+						}
 					}
 					else {
 						alert( mw.msg( 'ep-pager-delete-selected-fail' ) ); // TODO
@@ -96,7 +98,7 @@
 				}
 			);
 		} );
-		
+
 	} );
-	
-})( window.jQuery, window.mediaWiki, window.educationProgram )
+
+})( window.jQuery, window.mediaWiki, window.educationProgram );
