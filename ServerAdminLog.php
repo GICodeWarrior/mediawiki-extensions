4acb3256e8d834f5997dabd2ac886a061dc16841
johnduhart <johnduhart@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-31 05:51:30 +0000

    Comitting new extension, ServerAdminLog. Meant as a replacement for the [[wikitech:Server Admin Log]] and SAL in labsconsole. Still an early work in progress.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107710 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ ServerAdminLog.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Server Admin Log: Let everyone know you 'rm -rf /' enwiki
+ *
+ * @ingroup Extensions
+ * @file
+ */
+
+// TODO: Database setup
+
+
+/*
+ * Exit if called outside of MediaWiki
+ */
+if( !defined( 'MEDIAWIKI' ) ) {
+	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
+	die( 1 );
+}
+
+/*
+ * Greets to all mah homies
+ */
+$wgExtensionCredits['special'][] = array(
+	'path' => __FILE__,
+	'name' => 'Server Admin Log',
+	'version' => 1.0,
+	'author' => array( 'John Du Hart' ),
+	'url' => '//www.mediawiki.org/wiki/Extension:ServerAdminLog',
+	'descriptionmsg' => 'serveradminlog-desc',
+);
+
+/*
+ * Introduce our files to the autoloader
+ */
+$dir = dirname( __FILE__ ) . '/';
+
+$wgAutoloadClasses['ServerAdminLogEntryPager'] = $dir . '/ServerAdminLogEntryPager.php';
+$wgAutoloadClasses['ServerAdminLogChannel'] = $dir . '/ServerAdminLogChannel.php';
+$wgAutoloadClasses['SpecialAdminLog'] = $dir . '/specials/SpecialAdminLog.php';
+
+$wgSpecialPages['AdminLog'] = 'SpecialAdminLog';
+
+$wgExtensionMessagesFiles['ServerAdminLog'] = $dir . 'ServerAdminLog.i18n.php';
