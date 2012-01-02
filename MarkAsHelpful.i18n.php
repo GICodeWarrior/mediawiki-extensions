04ba09722a06652a5c627340763bec738429c4e1
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-02 20:46:12 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107851 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MarkAsHelpful.i18n.php
+++ MarkAsHelpful.i18n.php
@@ -162,10 +162,12 @@ $messages['hsb'] = array(
  * @author Dj
  */
 $messages['hu'] = array(
+	'markashelpful-desc' => 'Felhasználói felületet biztosít a hasznos megjegyzések megjelölésére',
 	'mah-mark-text' => 'Megjelelés hasznosként',
 	'mah-you-marked-text' => 'Szerinted hasznos',
 	'mah-someone-marked-text' => '{{GENDER:$1|$1 szerint hasznos}}',
 	'mah-undo-mark-text' => 'visszavonás',
+	'mah-action-error' => 'Hiba történt a művelet végrehajtása során',
 );
 
 /** Interlingua (Interlingua)
