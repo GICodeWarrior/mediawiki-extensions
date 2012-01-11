112f59c652677da518891c39d61b3a558f06b74b
siebrand <siebrand@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-11 21:58:15 +0000

    Simplify extension description.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@108684 fd97a983-eea8-4f9b-9b74-e50290b97401

--- WindowsAzureSDK.i18n.php
+++ WindowsAzureSDK.i18n.php
@@ -2,9 +2,7 @@
 $messages = array();
 
 $messages['en'] = array(
-	'windowsazuresdk-desc' => 'Provides the [http://phpazure.codeplex.com/ PHPAzure] SDK created by [http://www.realdolmen.com/ REALDOLMEN].
-It is the foundation for other extensions that need to interact with Windows Azure services.
-The integration with MediaWiki was made by [http://www.hallowelt.biz Hallo Welt! Medienwerkstatt GmbH].'
+	'windowsazuresdk-desc' => 'Provides the [http://phpazure.codeplex.com/ PHPAzure] SDK created by [http://www.realdolmen.com/ REALDOLMEN].'
 );
 
 /** German (Deutsch) */
