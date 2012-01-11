4c23b23dda1d00cfbef55db8df2cf76d9a42462b
raymond <raymond@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-11 20:17:10 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@108669 fd97a983-eea8-4f9b-9b74-e50290b97401

--- WindowsAzureSDK.i18n.php
+++ WindowsAzureSDK.i18n.php
@@ -7,6 +7,8 @@ It is the foundation for other extensions that need to interact with Windows Azu
 The integration with MediaWiki was made by [http://www.hallowelt.biz Hallo Welt! Medienwerkstatt GmbH].'
 );
 
+/** German (Deutsch) */
 $messages['de'] = array(
-	'windowsazuresdk-desc' => 'Diese Erweiterung stellt das [http://phpazure.codeplex.com/ PHPAzure] SDK bereit, welches von [http://www.realdolmen.com/ REALDOLMEN] entwickelt wird. Sie ist die Basis für andere Erweiterungen, welche mit Windows Azure Diensten interagieren müssen. Die MediaWiki-Integration wurde von [http://www.hallowelt.biz Hallo Welt! Medienwerkstatt GmbH] durchgeführt.'
+	'windowsazuresdk-desc' => 'Diese Erweiterung stellt das [http://phpazure.codeplex.com/ PHPAzure] SDK bereit, welches von [http://www.realdolmen.com/ REALDOLMEN] entwickelt wird. Sie ist die Basis für andere Erweiterungen, welche mit Windows Azure Diensten interagieren müssen. Die MediaWiki-Integration wurde von [http://www.hallowelt.biz Hallo Welt! Medienwerkstatt GmbH] durchgeführt.',
 );
+
