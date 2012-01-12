8f3e2945b3f5c15faadab8bd410ad88378f397f5
raymond <raymond@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-12 21:22:22 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@108761 fd97a983-eea8-4f9b-9b74-e50290b97401

--- ZeroRatedMobileAccess.i18n.php
+++ ZeroRatedMobileAccess.i18n.php
@@ -35,7 +35,6 @@ $messages['en'] = array (
 $messages['qqq'] = array(
 	'zero-rated-mobile-access-desc' => '{{desc}}',
 	'zero-rated-mobile-access-search' => 'Text for search action on the search button',
-	'zero-rated-mobile-access-language-options-wiki-page' => 'The name of the wiki page in the MediaWiki namespace to store the country language data',
 	'zero-rated-mobile-access-language-selection' => 'Text to show the user when they select a language option',
 	'zero-rated-mobile-access-home-page-selection' => 'Name of language followed by text to show user in local link to Wikipedia',
 	'zero-rated-mobile-access-home-page-selection-text' => 'Text to show user prior to selection of language option',
@@ -48,11 +47,135 @@ $messages['qqq'] = array(
 	'zero-rated-mobile-access-click-to-view-image' => 'Text for viewing an image link',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'zero-rated-mobile-access-desc' => 'Stellt Mobilfunkanbietern Funktionen bereit, mit denen diese kostenfreien Zugriff auf das Wiki ermöglichen können',
+	'zero-rated-mobile-access-search' => 'Suche',
+	'zero-rated-mobile-access-language-selection' => 'Alle Sprachen',
+	'zero-rated-mobile-access-home-page-selection' => '$1 Startseite',
+	'zero-rated-mobile-access-home-page-selection-text' => 'Oder besuche die Startseite in der folgenden Sprache:',
+	'zero-rated-mobile-access-banner-text' => 'Kostenloser Zugriff auf Wikipedia über $1',
+	'zero-rated-mobile-access-banner-text-data-charges' => 'Datenübertragungsgebühren können anwendbar sein: $1 oder $2',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Ja',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'Nein',
+	'zero-rated-mobile-access-click-to-view-image' => 'Hier klicken, um das Bild von „$1“ zu sehen …',
+);
+
+/** German (formal address) (‪Deutsch (Sie-Form)‬)
+ * @author Kghbln
+ */
+$messages['de-formal'] = array(
+	'zero-rated-mobile-access-home-page-selection-text' => 'Oder besuchen Sie die Startseite in der folgenden Sprache:',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'zero-rated-mobile-access-desc' => 'Staja póbitowarjam mobilneje telefonije funkcije k dispoziciji, kótarež zmóžnjaju dermotny pśistup',
+	'zero-rated-mobile-access-search' => 'Pytaś',
+	'zero-rated-mobile-access-language-selection' => 'Wšykne rěcy',
+	'zero-rated-mobile-access-home-page-selection' => '$1 startowy bok',
+	'zero-rated-mobile-access-home-page-selection-text' => 'Abo źi k startowemu bokoju w slědujucej rěcy:',
+	'zero-rated-mobile-access-banner-text' => 'Lichotna Wikipedija wót $1',
+	'zero-rated-mobile-access-banner-text-data-charges' => 'Pśi pśenjasenju datow mógu se płaśonki wuběraś: $1 abo $2',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Jo',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'Ně',
+	'zero-rated-mobile-access-click-to-view-image' => 'Klikni how, aby wobraz wót "$1" wiźeł...',
+);
+
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'zero-rated-mobile-access-desc' => 'Fournit les fonctionnalités pour les fournisseurs de mobile qui proposent un accès libre',
+	'zero-rated-mobile-access-search' => 'Rechercher',
+	'zero-rated-mobile-access-language-selection' => 'Toutes les langues',
+	'zero-rated-mobile-access-home-page-selection' => "page d'accueil $1",
+	'zero-rated-mobile-access-home-page-selection-text' => "Ou allez à la page d'accueil dans la langue suivante:",
+	'zero-rated-mobile-access-banner-text' => 'Wikipedia libre depuis $1',
+	'zero-rated-mobile-access-banner-text-data-charges' => "Les frais de données peuvent s'appliquer de façon permanente: $1 ou $2",
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Oui',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'Non',
+	'zero-rated-mobile-access-click-to-view-image' => 'Cliquez pour voir l\'image de, "$1"...',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'zero-rated-mobile-access-desc' => 'Staja poskićowarjam mobilneho škričkowanja funkcije k dispoziciji, kotrež darmotny přistup zmóžnjeja',
+	'zero-rated-mobile-access-search' => 'Pytać',
+	'zero-rated-mobile-access-language-selection' => 'Wšě rěče',
+	'zero-rated-mobile-access-home-page-selection' => '$1 startowa strona',
+	'zero-rated-mobile-access-home-page-selection-text' => 'Abo wopytaj startowu stronu w slědowacej rěči:',
+	'zero-rated-mobile-access-banner-text' => 'Swobodna Wikipedija wot $1',
+	'zero-rated-mobile-access-banner-text-data-charges' => 'Při přenjesenju datow móža so popłatki zběrać: $1 abo $2',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Haj',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'Ně',
+	'zero-rated-mobile-access-click-to-view-image' => 'Klikń tu, zo by wobraz wot "$1" widźał...',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'zero-rated-mobile-access-desc' => 'Provide functionalitate pro fornitores mobile que offere accesso libere',
+	'zero-rated-mobile-access-search' => 'Cercar',
+	'zero-rated-mobile-access-language-selection' => 'Tote le linguas',
+	'zero-rated-mobile-access-home-page-selection' => 'Pagina principal in $1',
+	'zero-rated-mobile-access-home-page-selection-text' => 'O va al pagina principal in lingua:',
+	'zero-rated-mobile-access-banner-text' => 'Wikipedia gratuite de $1',
+	'zero-rated-mobile-access-banner-text-data-charges' => 'Costos de datos pote esser applicate. Continuar ($1 o $2)?',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Si',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'No',
+	'zero-rated-mobile-access-click-to-view-image' => 'Clicca pro vider le imagine de "$1"...',
+);
+
 /** Japanese (日本語)
  * @author Patrick Reilly
  */
 $messages['ja'] = array(
 	'zero-rated-mobile-access-desc' => 'ゼロ格モバイルアクセス',
 	'zero-rated-mobile-access-search' => '検索する',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'zero-rated-mobile-access-desc' => 'Овозможува функции за мобилни оператори што даваат бесплатен пристап',
+	'zero-rated-mobile-access-search' => 'Пребарај',
+	'zero-rated-mobile-access-language-selection' => 'Сите јазици',
+	'zero-rated-mobile-access-home-page-selection' => 'Главна страница на $1',
+	'zero-rated-mobile-access-home-page-selection-text' => 'Или пак посетете ја главната страница на следниов јазик:',
+	'zero-rated-mobile-access-banner-text' => 'Бесплатна Википедија од $1',
+	'zero-rated-mobile-access-banner-text-data-charges' => 'Може да ви биде побарана доплата за податоците. Сакате да продолжите? $1 / $2',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Да',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'Не',
+	'zero-rated-mobile-access-click-to-view-image' => 'Стиснете за да ја погледате сликата на „$1“...',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'zero-rated-mobile-access-search' => 'Zoeken',
+	'zero-rated-mobile-access-language-selection' => 'Alle talen',
+	'zero-rated-mobile-access-banner-text' => 'Gratis Wikipedia van $1',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Ja',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'Nee',
+);
+
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages['te'] = array(
+	'zero-rated-mobile-access-search' => 'వెతుకు',
+	'zero-rated-mobile-access-language-selection' => 'అన్ని భాషలు',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'అవును',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'కాదు',
 );
 
