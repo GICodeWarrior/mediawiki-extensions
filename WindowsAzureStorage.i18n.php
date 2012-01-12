5888d830ef403d859c0f45293b12e1dc023c52fd
raymond <raymond@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-12 21:22:22 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@108761 fd97a983-eea8-4f9b-9b74-e50290b97401

--- WindowsAzureStorage.i18n.php
+++ WindowsAzureStorage.i18n.php
@@ -6,7 +6,38 @@ $messages['en'] = array(
 	'windowsazurestorage-desc' => 'Store files in the Windows Azure cloud, using the Windows Azure Storage Service'
 );
 
-/** German (Deutsch) */
+/** German (Deutsch)
+ * @author Kghbln
+ */
 $messages['de'] = array(
-	'windowsazurestorage-desc' => 'Speichern von Dateien in der Windows Azure Cloud über den Windows Azure Storage Service'
+	'windowsazurestorage-desc' => 'Ermöglicht das Speichern von Dateien in der Windows-Azure-Cloud über den Windows-Azure-Storage-Service',
 );
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'windowsazurestorage-desc' => 'Dataje w mrokawje Windows Azure z pomocu słužby Windows Azure Storage składowaś',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'windowsazurestorage-desc' => 'Almacena ficheiros na nube Windows Azure, grazas ao servizo de almacenamento Windows Azure',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'windowsazurestorage-desc' => 'Dataje w mróčeli Windows Azure z pomocu słužby Windows Azure Storage składować',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'windowsazurestorage-desc' => 'Immagazinar files in "Windows Azure cloud", usante le servicio de immagazinage "Windows Azure"',
+);
+
