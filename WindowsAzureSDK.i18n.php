4c0b8e102624ee54ea4f3e78a02a2d83978d4b52
raymond <raymond@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-12 21:22:22 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@108761 fd97a983-eea8-4f9b-9b74-e50290b97401

--- WindowsAzureSDK.i18n.php
+++ WindowsAzureSDK.i18n.php
@@ -5,8 +5,59 @@ $messages['en'] = array(
 	'windowsazuresdk-desc' => 'Provides the [http://phpazure.codeplex.com/ PHPAzure] SDK created by [http://www.realdolmen.com/ REALDOLMEN]'
 );
 
-/** German (Deutsch) */
+/** German (Deutsch)
+ * @author Kghbln
+ */
 $messages['de'] = array(
-	'windowsazuresdk-desc' => 'Diese Erweiterung stellt das [http://phpazure.codeplex.com/ PHPAzure] SDK bereit, welches von [http://www.realdolmen.com/ REALDOLMEN] entwickelt wird. Sie ist die Basis für andere Erweiterungen, welche mit Windows Azure Diensten interagieren müssen. Die MediaWiki-Integration wurde von [http://www.hallowelt.biz Hallo Welt! Medienwerkstatt GmbH] durchgeführt.',
+	'windowsazuresdk-desc' => 'Stellt das von [http://www.realdolmen.com/ RealDolmen] entwickelte [http://phpazure.codeplex.com/ Windows Azure SDK für PHP] bereit',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'windowsazuresdk-desc' => 'Staja [http://phpazure.codeplex.com/ PHPAzure]-SDK napórany wót [http://www.realdolmen.com/ REALDOLMEN] k dispoziciji.',
+);
+
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'windowsazuresdk-desc' => 'Fournit le SDK [http://phpazure.codeplex.com/ PHPAzure] créé par [http://www.realdolmen.com/ REALDOLMEN].',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'windowsazuresdk-desc' => 'Proporciona o SDK [http://phpazure.codeplex.com/ PHPAzure] creado por [http://www.realdolmen.com/ REALDOLMEN]',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'windowsazuresdk-desc' => 'Staja [http://phpazure.codeplex.com/ PHPAzure]-SDK wutworjeny wot [http://www.realdolmen.com/ REALDOLMEN] k dispoziciji.',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'windowsazuresdk-desc' => 'Provide le SDK de [http://phpazure.codeplex.com/ PHPAzure] create per [http://www.realdolmen.com/ REALDOLMEN].',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'windowsazuresdk-desc' => 'Го дава приборот за програмски развој [http://phpazure.codeplex.com/ PHPAzure] од [http://www.realdolmen.com/ REALDOLMEN].',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'windowsazuresdk-desc' => 'Biedt de [http://phpazure.codeplex.com/ PHPAzure] SDK gemaakt door [http://www.realdolmen.com/ REALDOLMEN]',
 );
 
