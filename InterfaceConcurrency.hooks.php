b9a6e7cbbfe86c342ffd3b26f6dcadae53a8da96
raindrift <raindrift@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-13 22:10:14 +0000

    moved ConcurrencyCheck code into its own extension
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@108863 fd97a983-eea8-4f9b-9b74-e50290b97401

---
+++ InterfaceConcurrency.hooks.php
@@ -0,0 +1,18 @@
+<?php
+
+class InterfaceConcurrencyHooks {
+
+	/**
+	 * Runs InterfaceConcurrency schema updates#
+	 *
+	 * @param $updater DatabaseUpdater
+	 */
+	public static function onLoadExtensionSchemaUpdates( $updater = null ) {
+		$dir = dirname(__FILE__) . '/sql';
+
+		$updater->addExtensionTable( 'concurrencycheck', "$dir/concurrencycheck.sql" );
+		
+		return true;
+	}
+	
+}
---
+++ InterfaceConcurrency.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Internationalisation file for extension InterfaceConcurrency
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Ian Baker
+ */
+$messages['en'] = array(
+	'interfaceconcurrency-desc' => 'Provides backend for UI that shows which resources are in use',
+);
+
+$messages['qqq'] = array(
+	'interfaceconcurrency-desc' => 'Description of this extension',
+);
