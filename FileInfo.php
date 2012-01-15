52b7cee223cd7ada2a245a5cf3d6082846a04ad0
siebrand <siebrand@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-15 20:07:13 +0000

    Remove use of deprecated LanguageGetMagic hook. Add FIXME for FlaggedRevs, because it loads the i18n resources in a weird way. Some misc. whitespace updates.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@108997 fd97a983-eea8-4f9b-9b74-e50290b97401

--- FileInfo.php
+++ FileInfo.php
@@ -6,26 +6,18 @@
 
 // @todo add other file metadata types via {{#fileinfo:...}}
 
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['FileInfoMagic'] = $dir . 'FileInfo.i18n.magic.php';
+
 # Define a setup function
 $wgExtensionFunctions[] = 'efFilesizeParserFunction_Setup';
-# Add a hook to initialise the magic word
-$wgHooks['LanguageGetMagic'][]       = 'efFilesizeParserFunction_Magic';
- 
+
 function efFilesizeParserFunction_Setup() {
 	global $wgParser;
 	# Set a function hook associating the "filesize" magic word with our function
 	$wgParser->setFunctionHook( 'filesize', 'efFilesizeParserFunction_Render' );
 }
- 
-function efFilesizeParserFunction_Magic( &$magicWords, $langCode ) {
-	# Add the magic word
-	# The first array element is case sensitive, in this case it is not case sensitive
-	# All remaining elements are synonyms for our parser function
-	$magicWords['filesize'] = array( 0, 'filesize' );
-	# unless we return true, other parser functions extensions won't get loaded.
-	return true;
-}
- 
+
 function efFilesizeParserFunction_Render( &$parser, $filename = '' ) {
 	# The parser function itself
 	# The input parameters are wikitext with templates expanded
