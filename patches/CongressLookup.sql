d70cf29c204856c4aa09bc4452ba37d40d12fa66
khorn <khorn@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-16 22:24:44 +0000

    followup r109023, r109055 Cleaning up the update sql.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@109068 fd97a983-eea8-4f9b-9b74-e50290b97401

--- patches/CongressLookup.sql
+++ patches/CongressLookup.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cl_senate (
   `ss_fax` varchar(32) NOT NULL,
   `ss_contactform` varchar(255) NOT NULL
 ) /*$wgDBTableOptions*/;
-CREATE INDEX /*i*/ss_state ON /*$wgDBprefix*/sopa_senate (ss_state);
+CREATE INDEX /*i*/ss_state ON /*$wgDBprefix*/cl_senate (ss_state);
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cl_house (
   `sh_id` int(10) unsigned NOT NULL PRIMARY KEY,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cl_house (
 ) /*$wgDBTableOptions*/;
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cl_zip3 (
-  `sz3_zip` int(3) unsigned DEFAULT NULL PRIMARY KEY,
+  `sz3_zip` int(3) unsigned NOT NULL PRIMARY KEY,
   `sz3_state` varchar(2) DEFAULT NULL
 ) /*$wgDBTableOptions*/;
 
