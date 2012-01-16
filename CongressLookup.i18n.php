8bb62493c97e16782f4d56aab484ca48cd32364f
kaldari <kaldari@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-16 20:34:03 +0000

    checking in some messages so we can start them being translated
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@109057 fd97a983-eea8-4f9b-9b74-e50290b97401

--- CongressLookup.i18n.php
+++ CongressLookup.i18n.php
@@ -14,6 +14,11 @@ $messages = array();
 $messages['en'] = array(
 	'congresslookup-desc' => 'Look up congressional representatives based on a U.S. zip code',
 	'congresslookup' => 'CongressLookup',
+	'congresslookup-submit' => 'Submit',
+	'congresslookup-submit-zip' => 'Submit zip',
+	'congresslookup-phone' => 'Phone: $1',
+	'congresslookup-fax' => 'Fax: $1',
+	'congresslookup-contact-form' => 'Contact form',
 );
 
 /** Message documentation (Message documentation)
@@ -22,4 +27,9 @@ $messages['en'] = array(
 $messages['qqq'] = array(
 	'congresslookup-desc' => '{{desc}}',
 	'congresslookup' => 'The name of the extension CongressLookup.',
+	'congresslookup-submit' => 'Submit button',
+	'congresslookup-submit-zip' => 'Submit button',
+	'congresslookup-phone' => 'Phone number listing. $1 is the phone number.',
+	'congresslookup-fax' => 'Fax number listing. $1 is the fax number.',
+	'congresslookup-contact-form' => "Label for a link to the representative's contact form",
 );
