bb93da066d3f49d47cdbc832c4e15c7d7dcd3342
raymond <raymond@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-17 22:10:16 +0000

    Localisation updates for core and extension messages from translatewiki.net
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@109231 fd97a983-eea8-4f9b-9b74-e50290b97401

--- InterfaceConcurrency.i18n.php
+++ InterfaceConcurrency.i18n.php
@@ -15,6 +15,50 @@ $messages['en'] = array(
 	'interfaceconcurrency-desc' => 'Provides a backend for the user interface that shows which resources are in use',
 );
 
+/** Message documentation (Message documentation) */
 $messages['qqq'] = array(
 	'interfaceconcurrency-desc' => '{{desc}}',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'interfaceconcurrency-desc' => 'Ermöglicht ein Backend für die Benutzeroberfläche, das anzeigt, welche Quellen genutzt werden',
+);
+
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'interfaceconcurrency-desc' => "Fournit une IHM pour l'interface utilisateur qui montre les ressources qui sont utilisées",
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'interfaceconcurrency-desc' => 'Proporciona unha infraestrutura para a interface de usuario que mostra os recursos en uso',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'interfaceconcurrency-desc' => 'Staja backend za wužiwarski powjerch k dispoziciji, kotryž pokazuje, kotre resursy so wužiwaja',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'interfaceconcurrency-desc' => 'Provide un back-end pro le interfacie de usator que monstra le ressources que es in uso',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'interfaceconcurrency-desc' => 'Дава подлога за корисничкиот посредник што прикажува кои ресурси се во употреба',
+);
+
