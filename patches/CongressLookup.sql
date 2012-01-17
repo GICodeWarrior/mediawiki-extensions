f10699793e5bd0bed5fe3d5c8c5833d79ffe3b3e
awjrichards <awjrichards@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-17 20:48:08 +0000

    Added auto-increment id field for cl_zip5 table. clz5_zip cannot be primary key as there is a 1:many relationship between clz5_zip and clz5_rep_id
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@109210 fd97a983-eea8-4f9b-9b74-e50290b97401

--- patches/CongressLookup.sql
+++ patches/CongressLookup.sql
@@ -32,9 +32,11 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cl_zip3 (
 ) /*$wgDBTableOptions*/;
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cl_zip5 (
-  `clz5_zip` int(5) unsigned NOT NULL PRIMARY KEY,
+  `clz5_id` int(10) unsigned NOT NULL PRIMARY KEY auto_increment,
+  `clz5_zip` int(5) unsigned NOT NULL,
   `clz5_rep_id` int(10) unsigned DEFAULT NULL
 ) /*$wgDBTableOptions*/;
+CREATE INDEX /*i*/clz5_zip ON /*$wgDBprefix*/cl_zip5 (clz5_zip);
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cl_zip9 (
   `clz9_zip` int(9) unsigned NOT NULL PRIMARY KEY,
