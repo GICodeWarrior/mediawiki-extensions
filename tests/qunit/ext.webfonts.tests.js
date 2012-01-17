346d0b7dccbd94398c974fb58b0028d7ebe07696
nikerabbit <nikerabbit@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-17 13:51:08 +0000

    Modify directly the style attr, .css didn't work - more tests passing now Some whitespace and test comment cleanups
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@109144 fd97a983-eea8-4f9b-9b74-e50290b97401

--- tests/qunit/ext.webfonts.tests.js
+++ tests/qunit/ext.webfonts.tests.js
@@ -16,7 +16,7 @@ test( '-- Dynamic font loading', function() {
 	assertTrue( mw.webfonts.addFont( 'Lohit Devanagari' ) , 'Add the Lohit Devanagari font again' );
 	assertTrue( loadedFontsSize === mw.webfonts.fonts.length , 'Already loaded fonts not loaded again.' );
 	assertFalse( mw.webfonts.addFont( 'Some Non existing font' ), 'addFont should return false if font not found' );
-	assertTrue( cssRulesLength + 1 === document.styleSheets.length, 'No new  css rules added at this stage' );
+	assertTrue( cssRulesLength + 1 === document.styleSheets.length, 'No new css rules added at this stage' );
 } );
 
 test( '-- Dynamic font loading based on lang attribute', function() {
@@ -24,14 +24,14 @@ test( '-- Dynamic font loading based on lang attribute', function() {
 
 	ok( $( 'body' ).append( "<p class='webfonts-testing-lang-attr'>Some Content</p>") );
 	$testElement =  $( 'p.webfonts-testing-lang-attr' )
-	assertTrue( $testElement !== undefined, 'Test element added' ) ;
+	assertTrue( $testElement !== [], 'Test element added' ) ;
 
 	ok( mw.webfonts.loadFontsForLangAttr() );
 	assertFalse( $testElement.hasClass( 'webfonts-lang-attr' ), 'The element has no webfonts-lang-attr class since there is no lang attribute' ) ;
 
 	ok( $testElement.attr( 'lang' , 'en' ) , 'Set lang attribute as english' );
 	ok( mw.webfonts.loadFontsForLangAttr() );
-	assertFalse( $testElement.hasClass( 'webfonts-lang-attr' ), 'The element has no webfonts-lang-attr class since there is en lang has no fonts available' ) ;
+	assertFalse( $testElement.hasClass( 'webfonts-lang-attr' ), 'The element has no webfonts-lang-attr class since en lang has no fonts available' ) ;
 
 	ok( $testElement.attr( 'lang' , 'ta' ) , 'Set lang attribute as Tamil' );
 	var cssRulesLength = document.styleSheets.length;
@@ -49,24 +49,24 @@ test( '-- Dynamic font loading based on lang attribute', function() {
 test( '-- Dynamic font loading based on font-family style attribute', function() {
 	expect( 14 )
 
-	ok( $( 'body' ).append( "<p class='webfonts-testing-font-family-style'>Some Content</p>") );
+	ok( $( 'body' ).append( "<p class='webfonts-testing-font-family-style'>Some Content</p>" ) );
 	var $testElement = $( 'p.webfonts-testing-font-family-style' );
-	assertTrue(  $testElement !== undefined, 'Test element added' ) ;
+	assertTrue(  $testElement !== [], 'Test element added' ) ;
 
-	$testElement.css( 'font-family', 'RufScript, Arial, Helvetica, sans');
+	$testElement.attr( 'style','font-family: RufScript, Arial, Helvetica, sans' );
 	var cssRulesLength = document.styleSheets.length;
 	assertTrue( $.inArray( 'RufScript', mw.webfonts.fonts ) === -1 , 'RufScript Font not loaded yet' );
 	ok( mw.webfonts.loadFontsForFontFamilyStyle() );
 	assertTrue( $.inArray( 'RufScript', mw.webfonts.fonts ) >= 0 , 'Font loaded' );
 	assertTrue( cssRulesLength +1 === document.styleSheets.length, 'New css rule added to the document' );
 
-	$testElement.css( 'font-family', 'NonExistingFont, Arial, Helvetica, sans' );
+	$testElement.attr( 'style','font-family: NonExistingFont, Arial, Helvetica, sans' );
 	cssRulesLength = document.styleSheets.length;
 	ok( mw.webfonts.loadFontsForFontFamilyStyle() );
 	assertTrue( $.inArray( 'NonExistingFont', mw.webfonts.fonts ) === -1 , 'Font not loaded since it is not existing, including fallback fonts' );
 	assertTrue( cssRulesLength === document.styleSheets.length, 'No new css rule added to the document' );
 
-	$testElement.css( 'font-family', 'NonExistingFont, AnjaliOldLipi, Arial, Helvetica, sans');
+	$testElement.attr( 'style','font-family: NonExistingFont, AnjaliOldLipi, Arial, Helvetica, sans' );
 	cssRulesLength = document.styleSheets.length;
 	assertTrue( $.inArray( 'AnjaliOldLipi', mw.webfonts.fonts ) === -1 , 'Fallback font AnjaliOldLipi not loaded yet' );
 	ok( mw.webfonts.loadFontsForFontFamilyStyle() );
