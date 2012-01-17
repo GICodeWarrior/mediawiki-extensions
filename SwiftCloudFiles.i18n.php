b069ef92c111f564f690d458285aad1fa4df9f37
aaron <aaron@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-17 22:00:51 +0000

    Added php-cloudfiles (verbatim) with a thin MediaWiki extension wrapper around it. Custom modifications will also be maintained here.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@109227 fd97a983-eea8-4f9b-9b74-e50290b97401

---
+++ SwiftCloudFiles.i18n.php
@@ -0,0 +1,7 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'swiftcloudfiles-desc' => "Includes and registers the ''php-cloudfiles'' API (https://github.com/rackspace/php-cloudfiles), allowing MediaWiki to interface with an OpenStack Swift storage backend."
+);
