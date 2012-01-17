ce63a0b3f5ea3d4a629cafc938bb91fa69fb3554
jeroendedauw <jeroendedauw@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-17 01:12:38 +0000

    attempt to get around weird 1.18 issue
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@109098 fd97a983-eea8-4f9b-9b74-e50290b97401

--- resources/ep.js
+++ resources/ep.js
@@ -8,10 +8,43 @@
 
 window.educationProgram = new( function() {
 	
-	this.compatMode = true;
+	this.compatMode = null;
 	
 	this.msg = function () {
-		if ( window.educationProgram.compatMode ) {
+		// Yeah, doing it here, since "mw.loader.using( 'mediawiki.language',"
+		// does not have mediaWiki.language loaded.
+		if ( this.compatMode === null ) {
+			this.compatMode = typeof window.mediaWiki.language.gender === 'undefined';
+			
+			if ( window.educationProgram.compatMode ) {
+				mw.language.gender = function( gender, forms ) {
+					if ( !forms || forms.length === 0 ) {
+						return '';
+					}
+					forms = mw.language.preConvertPlural( forms, 2 );
+					if ( gender === 'male' ) {
+						return forms[0];
+					}
+					if ( gender === 'female' ) {
+						return forms[1];
+					}
+					return ( forms.length === 3 ) ? forms[2] : forms[0];
+				};
+				
+				mw.jqueryMsg.htmlEmitter.prototype.gender = function( nodes ) { 
+					var gender;
+					if  ( nodes[0] && nodes[0].options instanceof mw.Map ){
+						gender = nodes[0].options.get( 'gender' );
+					} else {
+						gender = nodes[0];
+					}
+					var forms = nodes.slice(1);
+					return this.language.gender( gender, forms );
+				};
+			}
+		}
+		
+		if ( this.compatMode ) {
 			return gM.apply( this, arguments );
 		}
 		else {
@@ -22,37 +55,5 @@ window.educationProgram = new( function() {
 	this.msge = function () {
 		return mw.html.escape( this.msg.apply( this, arguments ) );
 	};
-	
-} );
-
-mw.loader.using( 'mediawiki.language', function() {
-	window.educationProgram.compatMode = typeof window.mediaWiki.language === 'undefined';
-	
-	if ( window.educationProgram.compatMode ) {
-		mw.language.gender = function( gender, forms ) {
-			if ( !forms || forms.length === 0 ) {
-				return '';
-			}
-			forms = mw.language.preConvertPlural( forms, 2 );
-			if ( gender === 'male' ) {
-				return forms[0];
-			}
-			if ( gender === 'female' ) {
-				return forms[1];
-			}
-			return ( forms.length === 3 ) ? forms[2] : forms[0];
-		};
-		
-		mw.jqueryMsg.htmlEmitter.prototype.gender = function( nodes ) { 
-			var gender;
-			if  ( nodes[0] && nodes[0].options instanceof mw.Map ){
-				gender = nodes[0].options.get( 'gender' );
-			} else {
-				gender = nodes[0];
-			}
-			var forms = nodes.slice(1);
-			return this.language.gender( gender, forms );
-		};
-	}	
 	
 } );
