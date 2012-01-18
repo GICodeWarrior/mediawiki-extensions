5acb46a95e9f4d956a46dff186600170351188c9
raindrift <raindrift@fd97a983-eea8-4f9b-9b74-e50290b97401>
2012-01-18 08:15:41 +0000

    contains a form, stores things in the database, is very web 1.0
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@109344 fd97a983-eea8-4f9b-9b74-e50290b97401

--- SpecialCongressFail.php
+++ SpecialCongressFail.php
@@ -15,15 +15,43 @@ class SpecialCongressFail extends UnlistedSpecialPage {
 	public function execute( $subPage ) {
 		global $wgRequest, $wgUser;
 
-		$out = $this->getOutput();
+		$zip = (int)$wgRequest->getVal( 'zip' );
 
+		$out = $this->getOutput();
 		$this->setHeaders();
 		$this->outputHeader();
+		
+		if( $wgRequest->getVal( 'store' ) ) {
+			$dbw = wfGetDb( DB_MASTER );
 
-		$out->addHtml('<body>');
+			$res = $dbw->insert(
+				'cl_errors',
+				array(
+					'cle_zip' => $zip,
+					'cc_comment' => $wgRequest->getVal( 'comment' ),
+				),
+				__METHOD__,
+				array()
+			);
+			
+			
+		} else {
+			$out->addHtml('<div>');
+			$out->addHtml('ZIP Code (will be recorded): ' . $zip);
+			$out->addHtml('</div>');
 
+			$out->addHtml('<form method=post action="/wiki/Special:CongressFail">');
 
-		$out->addHtml('</body>');
+			$out->addHtml('<div>');
+			$out->addHtml('<input type=hidden name=zip value=' . $zip. '>');
+			$out->addHtml('Tell us what went wrong: <input type=text size=100 name=comment>');
+			$out->addHtml('</div>');
+
+			$out->addHtml('<div>');
+			$out->addHtml('<input type=submit name=store value="Send Error Report">');
+			$out->addHtml('</div>');
+			$out->addHtml('</form>');
+		}
 	}
 
 }
